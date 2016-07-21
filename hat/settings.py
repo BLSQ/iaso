c2084eed258760158fd488cc07e86115be564820
francesco palazzini <palazzini.francesco@gmail.com>
2016-07-21 17:07:35 +0200

    fix: allow hosts for production environment

--- hat/settings.py
+++ hat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG", "false") == "true")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

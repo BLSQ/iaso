0295ada4d16f87b54da282541ceb1fca69b46ae7
Karl Westin <karl.westin@gmail.com>
2016-08-12 17:49:02 +0200

    feat(translations): add translation tags in all files

--- hat/import_export/forms.py
+++ hat/import_export/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 
 class UploadMdbFilesForm(forms.Form):
     file = forms.FileField(
+        label=_("File"),
         widget=forms.FileInput(attrs={'multiple': True, 'accept': '.mdb,.accdb,.enc'}))
 
 

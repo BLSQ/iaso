16b6c817faf705dda4067de998db2ef5d876df65
Karl Westin <karl.westin@gmail.com>
2016-08-15 17:36:52 +0200

    chore(translations): always download translations, upload from dev

---
+++ build_scripts/tfx_download.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+set -o nounset
+
+target="hat/locale/fr/LC_MESSAGES"
+mkdir -p "$target"
+
+url="https://www.transifex.com/api/2/project/hat/resource/sense-hat-historicalpo/translation/fr?file=true"
+curl -L --user "$TRANSIFEX_USER:$TRANSIFEX_PASSWORD" -X GET "$url" > "$target/django.po"
+
+# compile messages for django
+docker-compose run web manage compilemessages -l fr

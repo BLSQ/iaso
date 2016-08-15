7978fe8ef6833d0e2e092bec99cdb770c1ec6989
Till Reitemeyer <till@skoni.de>
2016-08-15 23:07:26 +0200

    fix: use textfield instead of charfield in schema
    CharField was initially choosen with the assumptions, that it
    is more performance and/or space efficient. That is not true.
    In postgres varchar and text are stored alike and its safer
    here to use text, because some fields  might contain long values
    and break the input if they don't fit the max_length constraint.

--- hat/participants/models.py
+++ hat/participants/models.py
@@ -6,40 +6,40 @@ class HatParticipant(models.Model):
         ('historic', 'Historic'),
         ('mobile_backup', 'Mobile backup')
     )
-    source = models.CharField(max_length=16, choices=SOURCE_CHOICES, null=True)
+    source = models.TextField(choices=SOURCE_CHOICES, null=True)
 
     document_date = models.DateTimeField(null=True)
     # The id is currently a hash over the row to be able to
     # catch duplicates
-    document_id = models.CharField(max_length=32)
-    hat_id = models.CharField(max_length=64)
+    document_id = models.TextField()
+    hat_id = models.TextField()
     entry_date = models.DateTimeField(null=True)
-    entry_name = models.CharField(max_length=128, null=True)
+    entry_name = models.TextField(null=True)
 
-    name = models.CharField(max_length=128, null=True)
-    lastname = models.CharField(max_length=128, null=True)
-    prename = models.CharField(max_length=128, null=True)
+    name = models.TextField(null=True)
+    lastname = models.TextField(null=True)
+    prename = models.TextField(null=True)
     SEX_CHOICES = (
         ('female', 'Female'),
         ('male', 'Male'),
         ('unknown', 'Unknown'),
     )
-    sex = models.CharField(max_length=16, choices=SEX_CHOICES)
+    sex = models.TextField(choices=SEX_CHOICES)
     age = models.PositiveSmallIntegerField(null=True)
     year_of_birth = models.PositiveSmallIntegerField(null=True)
-    mothers_surname = models.CharField(max_length=128, null=True)
+    mothers_surname = models.TextField(null=True)
 
-    village = models.CharField(max_length=64, null=True)
-    province = models.CharField(max_length=64, null=True)
-    ZS = models.CharField(max_length=64, null=True)
-    AZ = models.CharField(max_length=64, null=True)
+    village = models.TextField(null=True)
+    province = models.TextField(null=True)
+    ZS = models.TextField(null=True)
+    AZ = models.TextField(null=True)
 
-    mobile_unit = models.CharField(max_length=128, null=True)
+    mobile_unit = models.TextField(null=True)
 
-    treatment_center = models.CharField(max_length=128, null=True)
+    treatment_center = models.TextField(null=True)
     treatment_start_date = models.DateTimeField(null=True)
     treatment_end_date = models.DateTimeField(null=True)
-    treatment_prescribed = models.CharField(max_length=128, null=True)
+    treatment_prescribed = models.TextField(null=True)
     treatment_secondary_effects = models.NullBooleanField()
     TREATMENT_RESULT_CHOICES = (
         ('recovered', 'Recovered'),
@@ -50,7 +50,7 @@ class HatParticipant(models.Model):
         ('transferred', 'Transferred'),
         ('other', 'Other'),
     )
-    treatment_result = models.CharField(max_length=16, choices=TREATMENT_RESULT_CHOICES, null=True)
+    treatment_result = models.TextField(choices=TREATMENT_RESULT_CHOICES, null=True)
 
     # mobile and historic data sources
     test_rdt = models.NullBooleanField(null=True)
@@ -64,9 +64,9 @@ class HatParticipant(models.Model):
     test_pl = models.NullBooleanField(null=True)
 
     # from historic data
-    test_catt_total_blood = models.CharField(max_length=16, null=True)
+    test_catt_total_blood = models.TextField(null=True)
 
-    test_catt_dilution = models.CharField(max_length=16, null=True)
+    test_catt_dilution = models.TextField(null=True)
     test_lymph_node_puncture = models.NullBooleanField(null=True)
     test_sf = models.NullBooleanField(null=True)
     test_woo = models.NullBooleanField(null=True)
@@ -82,32 +82,32 @@ class HatParticipant(models.Model):
 
     test_clinical_sickness = models.NullBooleanField(null=True)
     test_other = models.NullBooleanField(null=True)
-    test_pl_liquid = models.CharField(max_length=16, null=True)
-    test_pl_trypanosome = models.CharField(max_length=128, null=True)
-    test_pl_gb_mm3 = models.CharField(max_length=128, null=True)
-    test_pl_albumine = models.CharField(max_length=128, null=True)
-    test_pl_lcr = models.CharField(max_length=16, null=True)
-    test_pl_comments = models.CharField(max_length=128, null=True)
+    test_pl_liquid = models.TextField(null=True)
+    test_pl_trypanosome = models.TextField(null=True)
+    test_pl_gb_mm3 = models.TextField(null=True)
+    test_pl_albumine = models.TextField(null=True)
+    test_pl_lcr = models.TextField(null=True)
+    test_pl_comments = models.TextField(null=True)
     PL_TEST_RESULT_CHOICES = (
         ('stage1', 'Stage1'),
         ('stage2', 'Stage2'),
         ('unknown', 'Unknown')
     )
-    test_pl_result = models.CharField(max_length=16, choices=PL_TEST_RESULT_CHOICES, null=True)
+    test_pl_result = models.TextField(choices=PL_TEST_RESULT_CHOICES, null=True)
 
     followup_done = models.NullBooleanField(null=True)
 
     # fields for followup tests
-    test_followup_pg = models.CharField(max_length=128, null=True)
-    test_followup_sf = models.CharField(max_length=128, null=True)
-    test_followup_ge = models.CharField(max_length=128, null=True)
-    test_followup_woo = models.CharField(max_length=128, null=True)
-    test_followup_maect = models.CharField(max_length=128, null=True)
-    test_followup_woo_maect = models.CharField(max_length=128, null=True)
-    test_followup_pl = models.CharField(max_length=128, null=True)
-    test_followup_pl_trypanosome = models.CharField(max_length=128, null=True)
-    test_followup_pl_gb = models.CharField(max_length=128, null=True)
-    test_followup_decision = models.CharField(max_length=128, null=True)
+    test_followup_pg = models.TextField(null=True)
+    test_followup_sf = models.TextField(null=True)
+    test_followup_ge = models.TextField(null=True)
+    test_followup_woo = models.TextField(null=True)
+    test_followup_maect = models.TextField(null=True)
+    test_followup_woo_maect = models.TextField(null=True)
+    test_followup_pl = models.TextField(null=True)
+    test_followup_pl_trypanosome = models.TextField(null=True)
+    test_followup_pl_gb = models.TextField(null=True)
+    test_followup_decision = models.TextField(null=True)
 
     class Meta:
         permissions = (

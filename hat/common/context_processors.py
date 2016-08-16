ec514a25cdc7c301378a31aed3a0949eb19a592a
Karl Westin <karl.westin@gmail.com>
2016-08-16 15:19:28 +0200

    fix: limit commit to 7 chars, don't show ...

--- hat/common/context_processors.py
+++ hat/common/context_processors.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 def appversions(request):
     return {
-        'version_commit': settings.HAT_COMMIT or 'local-dev'
+            'version_commit': settings.HAT_COMMIT[0:7] or 'local-dev'
     }

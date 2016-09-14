f782066651865b9eeeffb4583a9b72bffd2e28ad
Karl Westin <karl.westin@gmail.com>
2016-09-14 16:26:09 +0200

    chore(playground): enable hot-reloading
    the root component needs to be in a separate file to be hot-reloadable

--- hat/assets/js/playground.js
+++ hat/assets/js/playground.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { Route, useRouterHistory } from 'react-router'
 import { createHistory } from 'history'
@@ -6,90 +6,7 @@ import { createHistory } from 'history'
 import createStore from './redux/createStore'
 import App from './apps/App'
 
-import JsonSchema from './apps/playground/json-schema'
-import ResourceList from './apps/playground/resource-list'
-import VegaLiteVis from './components/vega-lite-vis'
-import ValueVis from './components/value-vis'
-import VISUALIZATIONS from '../json/visualizations.json'
-
-function createQueryString (params) {
-  return Object.keys(params)
-    .map((name) => encodeURIComponent(name) + '=' + encodeURIComponent(params[name]))
-    .join('&')
-    .replace(/%20/g, '+')
-}
-
-class Playground extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      visName: '',
-      datasetConfig: null,
-      dataset: null,
-      params: null
-    }
-    this.handleVisSelect = this.handleVisSelect.bind(this)
-    this.handleDataSelect = this.handleDataSelect.bind(this)
-    this.handleParamsChange = this.handleParamsChange.bind(this)
-  }
-
-  handleVisSelect (event) {
-    this.setState({visName: event.target.value})
-  }
-
-  handleDataSelect (config) {
-    this.setState({datasetConfig: config})
-    window.fetch(config.url, {headers: {'Accept': 'application/json'}, credentials: 'include'})
-      .then((resp) => resp.json())
-      .then((json) => {
-        this.setState({dataset: json})
-      })
-  }
-
-  handleParamsChange (params) {
-    this.setState({params: params})
-    const qs = createQueryString(params)
-    const url = this.state.datasetConfig.url + '?' + qs
-    window.fetch(url, {headers: {'Accept': 'application/json'}, credentials: 'include'})
-      .then((resp) => resp.json())
-      .then((json) => {
-        this.setState({dataset: json})
-      })
-  }
-
-  render () {
-    const {visName, datasetConfig, dataset} = this.state
-    let paramsSchema
-    if (datasetConfig && datasetConfig.params_schema) {
-      paramsSchema = datasetConfig.params_schema
-    }
-    let vis
-    if (dataset && visName) {
-      const visConfig = VISUALIZATIONS[visName]
-      switch (visConfig.type) {
-        case 'vega-lite': vis = <VegaLiteVis data={dataset} spec={VISUALIZATIONS[visName].spec} />; break
-        case 'value': vis = <ValueVis data={dataset} />; break
-      }
-    }
-    return <div>
-      <div>
-        <label>Visualizations:</label>
-        <select value={visName} onChange={this.handleVisSelect}>
-          <option key='none' value=''>None</option>
-          {Object.keys(VISUALIZATIONS).map((name, i) => {
-            return <option key={i} value={name}>{name}</option>
-          })}
-        </select>
-      </div>
-      <br />
-      <ResourceList title='Datasets' url='/api/datasets' onSelect={this.handleDataSelect} />
-      <br />
-      <JsonSchema name='Dataset parameters' schema={paramsSchema} onChange={this.handleParamsChange} />
-      <hr />
-      {vis}
-    </div>
-  }
-}
+import Playground from './apps/playground/playground'
 
 const routes = [
   <Route path='/' component={Playground} />

fee303476ddd0e7a12ecda903d59dc14302e6bd0
Till Reitemeyer <till@skoni.de>
2016-10-26 11:49:41 +0200

    fix: hat-id empty birthyear placeholder

--- hat/import_export/tests/test_utils.py
+++ hat/import_export/tests/test_utils.py
@@ -4,6 +4,28 @@ from .. import utils
 
 
 class UtilsTests(TestCase):
+    def test_hat_id(self):
+        r1 = pandas.Series({
+            'lastname': 'Foo',
+            'name': 'Bar',
+            'prename': 'Baz',
+            'sex': 'Male',
+            'year_of_birth': '1999',
+            'mothers_surname': 'Mo',
+        })
+        self.assertEqual(utils.hat_id(r1), 'FOBABAM1999M')
+
+        r2 = pandas.Series({})
+        self.assertEqual(utils.hat_id(r2), 'XXXXXXXXXXXX')
+
+        r3 = pandas.Series({
+            'lastname': 'ÀZ',
+            'name': 'ÈÉÊ',
+            'prename': 'ÛZ',
+            'mothers_surname': 'M'
+        })
+        self.assertEqual(utils.hat_id(r3), 'AZEEUZXXXXXM')
+
     def test_capitalize(self):
         self.assertEqual(utils.capitalize(None), None)
         self.assertEqual(utils.capitalize('foo bar'), 'Foo Bar')

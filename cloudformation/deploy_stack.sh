ae09d4b25170282a1b6f1db5028ffaf0d4e84d54
Francesco Palazzini <palazzini.francesco@gmail.com>
2016-11-04 14:14:05 +0100

    test

--- cloudformation/deploy_stack.sh
+++ cloudformation/deploy_stack.sh
@@ -1,45 +1,35 @@
 #!/usr/bin/env bash
-#
 
 set -e -x
 
 script_dir="$(cd "$(dirname "$0")"; pwd)"
+source "${script_dir}/functions.sh"
 source "${script_dir}/configrc"
 
-echo "validating templates syntax"
-aws cloudformation validate-template --template-body file://${script_dir}/cloudformation.json
-aws cloudformation validate-template --template-body file://${script_dir}/cloudformation-env.json
+parse_args "${*}"
 
-args_1="${1}"
+validate_template "${script_dir}/cloudformation.yml"
+validate_template "${script_dir}/cloudformation-env.yml"
 
-# if the script is executed without args create a new stack
-if [ -z $args_1 ]; then
+if [[ "${cf_action}" == "create-stack" ]]; then
     if [ -z "${created_by}" ] || [ -z "${requested_by}" ]; then
-        echo 'created_by and requested_by envs expected to be set in the env'
-        exit 1
+        _error 'created_by and requested_by envs expected to be set in the env'
     fi
-    cf_action='create-stack'
+
     # define array of parameters which can only be passed while creating stack
     create_parameters=(--tags)
     create_parameters+=(Key=StackName,Value="${stack_name}")
     create_parameters+=(Key=BILLING_PROJECT,Value="${billing_project}")
     create_parameters+=(Key=RequestedBy,Value="${requested_by}")
     create_parameters+=(Key=CreatedBy,Value="${created_by}" )
-
-# if the script is executed with args == 'update' update an existing stack
-elif [[ $args_1 == 'update' ]]; then
-    cf_action='update-stack'
-else
-    echo 'not recognized parameter'
-    exit 1
 fi
 
-aws s3 cp "cloudformation-env.json" "s3://${cf_s3_bucket}/${stack_name}-cloudformation-env.json"
+aws s3 cp "cloudformation-env.yml" "s3://${cf_s3_bucket}/${stack_name}-cloudformation-env.yml"
 
 echo "starting stack ${cf_action} operation"
-aws cloudformation ${cf_action} \
+aws cloudformation "${cf_action}" \
     --stack-name "${stack_name}" \
-    --template-body "file://${script_dir}/cloudformation.json" \
+    --template-body "file://${script_dir}/cloudformation.yml" \
     --capabilities CAPABILITY_IAM \
     --parameters \
     ParameterKey=ProjectName,ParameterValue="${stack_name}" \
@@ -51,4 +41,5 @@ aws cloudformation ${cf_action} \
     ParameterKey=SNSTopic,ParameterValue="${sns_topic}" \
     ParameterKey=SSLCertARN,ParameterValue="${ssl_cert}" \
     ParameterKey=ImageId,ParameterValue="${image_id}" \
-    ${create_parameters[*]}
+    ParameterKey=GetOrCreateRecordLambda,ParameterValue="${get_or_create_record_lambda}" \
+${create_parameters[*]}

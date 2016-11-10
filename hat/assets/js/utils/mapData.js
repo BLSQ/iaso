7081e86fb73dd277c0801b534967b619c770ceac
obdulia <obdulia.losantos@gmail.com>
2016-11-10 13:48:35 +0100

    feat(microplanning): use UCLA files (1st version)

--- hat/assets/js/utils/mapData.js
+++ hat/assets/js/utils/mapData.js
@@ -1,45 +1,66 @@
 //
-// build a clean list with Zone de Sante boundaries and its villages
+// build clean lists with areas boundaries and villages
 //
 
 import * as topojson from 'topojson'
-import topoData from '../../json/ZS-Bandundu-topo.json'
-import locations from '@ehealthafrica/sense-hat-locations'
-import villagesATLAS from '../../json/HAT-ATLAS-villages.json'
-
-//
-// Leaflet doesn't support topoJSON format
-// transform them as geoJSON layers.
-//
-const geoData = {}
-for (let key in topoData.objects) {
-  geoData[key] = topojson.feature(topoData, topoData.objects[key])
-}
-
-//
-// Extract all the villages from the locations list
-// TO BE CHANGED WITH CANONICAL LIST
-//
-const villages = locations.reduce((flattenZones, zone) => {
-  return flattenZones.concat(zone.areas.reduce((flattenAreas, area) => {
-    return flattenAreas.concat(area.villages
-      .filter((village) => (village.lon * village.lat !== 0))
-      .map((village) => ({
-        zone: zone.name,
-        area: area.name,
-        village: village.name,
-        lat: village.lat,
-        lon: village.lon
-      })))
-  }, []))
-}, [])
+import areasTopo from '../../json/areas.json'
+import villages from '../../json/villages.json'
 
 const MAP_CENTER = [ -4.4233379, 16.2113064 ]
 const MAP_ZOOM = 7
 
+//
+// Leaflet doesn't support topoJSON format, transform them to geoJSON layers.
+//
+const areas = topojson.feature(areasTopo, areasTopo.objects.areas)
+
+//
+// modify villages list based on other properties
+//
+villages.forEach((item) => {
+  // create fake `_id` based on assumed uniqueness location
+  item._id = item.lat.toString() + ':' + item.lon.toString()
+
+  // indicate `type` based on `official` and `village` values
+  if (item.official === 'YES') {
+    item.type = 'official'
+  } else if (item.official === 'NO') {
+    item.type = 'other'
+  } else if (item.village === 'Inconnu') {
+    item.type = 'unknown'
+  } else {
+    item.type = '---' // this should never happen
+  }
+})
+
+//
+// calculate aggregated population, number of villages...
+//
+const aggregateBy = (keys) => {
+  return (item) => {
+    const props = item.properties
+    const list = villages.filter((entry) => (
+      keys.every((key) => entry[key] === props[key])
+    ))
+
+    if (list.length) {
+      props.population = list.reduce((prev, curr) => (prev + (curr.population || 0)), 0)
+      props.villages = {
+        list: list,
+        official: list.filter((village) => (village.type === 'official')).length,
+        other: list.filter((village) => (village.type === 'other')).length,
+        unknown: list.filter((village) => (village.type === 'unknown')).length
+      }
+    }
+  }
+}
+
+// include aggregated data from villages
+areas.features.forEach(aggregateBy(['zone', 'area']))
+
 export default {
   center: MAP_CENTER,
   zoom: MAP_ZOOM,
-  zones: geoData.zones,
-  villages: villagesATLAS.concat(villages)
+  areas: areas,
+  villages: villages
 }

5c43d349d71816ad77b0acd832852d87c6c9b582
obdulia <obdulia.losantos@gmail.com>
2016-11-24 15:17:48 +0100

    feat(microplanning): move village filters to MapLegend

--- hat/assets/js/apps/Microplanning/components/MapLegend.js
+++ hat/assets/js/apps/Microplanning/components/MapLegend.js
@@ -1,25 +1,70 @@
-import React, {Component} from 'react'
+import React, {Component, PropTypes} from 'react'
 import { FormattedMessage, injectIntl } from 'react-intl'
 
 class MapLegend extends Component {
   render () {
+    const { filter, bufferSize, filterChange, bufferChange } = this.props
+    const bufferCheck = (bufferSize > 0)
+
     return (
-      <div className='legend'>
-        <div className='points'>
-          <ul>
-            <li key='highlight'>
-              <i className='fa fa-circle highlight' />
-              <FormattedMessage id='microplanning.legend.highlight' defaultMessage='Villages with confirmed cases' />
-            </li>
-            <li key='selected'>
-              <i className='fa fa-circle selected' />
-              <FormattedMessage id='microplanning.legend.selected' defaultMessage='Selected villages' />
-            </li>
-          </ul>
+      <div>
+        <form className='widget__toggle-group'>
+          <span className='widget__toggle-group__legend'>
+            <FormattedMessage id='microplanning.display.villages.types' defaultMessage='Village types' />
+          </span>
+          <label htmlFor='official' className='widget__filterpluslabel__item--official'>
+            <input type='checkbox' name='official' checked={filter.official} onChange={filterChange} className='widget__filterpluslabel__input' />
+            <span className='widget__filterpluslabel__text--official'>
+              <FormattedMessage id='microplanning.display.official' defaultMessage='Official villages' />
+            </span>
+          </label>
+          <label htmlFor='other' className='widget__filterpluslabel__item--other'>
+            <input type='checkbox' name='other' checked={filter.other} onChange={filterChange} className='widget__filterpluslabel__input' />
+            <span className='widget__filterpluslabel__text--other'>
+              <FormattedMessage id='microplanning.display.other' defaultMessage='Non official villages' />
+            </span>
+          </label>
+          <label htmlFor='unknown' className='widget__filterpluslabel__item--unknown'>
+            <input type='checkbox' name='unknown' checked={filter.unknown} onChange={filterChange} className='widget__filterpluslabel__input' />
+            <span className='widget__filterpluslabel__text--unknown'>
+              <FormattedMessage id='microplanning.display.unknown' defaultMessage='Unknown villages' />
+            </span>
+          </label>
+
+          <label htmlFor='buffer-check' className='widget__filterpluslabel__item--buffer'>
+            <input type='checkbox' name='buffer-check' checked={bufferCheck} onChange={bufferChange} className='widget__filterpluslabel__input' />
+            <span className='widget__filterpluslabel__text--buffer'>
+              <FormattedMessage id='microplanning.buffer' defaultMessage='Buffer zone on confirmed cases' />
+              <input type='number' className='small' disabled={!bufferCheck} name='buffer-value' value={bufferSize} onChange={bufferChange} />
+              {'m'}
+            </span>
+          </label>
+        </form>
+
+        <div className='legend'>
+          <div className='points'>
+            <ul>
+              <li key='highlight'>
+                <i className='fa fa-circle highlight' />
+                <FormattedMessage id='microplanning.legend.highlight' defaultMessage='Villages with confirmed cases in the indicated period' />
+              </li>
+              <li key='selected'>
+                <i className='fa fa-circle selected' />
+                <FormattedMessage id='microplanning.legend.selected' defaultMessage='Selected villages' />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     )
   }
+}
+
+Map.MapLegend = {
+  filter: PropTypes.object,
+  filterChange: PropTypes.func,
+  bufferSize: PropTypes.number,
+  bufferChange: PropTypes.func
 }
 
 export default injectIntl(MapLegend)

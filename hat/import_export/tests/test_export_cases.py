1e652894193aaec1daf1ce8ac8519362bdf84664
Till Reitemeyer <till@skoni.de>
2017-02-22 12:29:26 +0100

    fix(export-tests): use fixtures with transaction-test-case

--- hat/import_export/tests/test_export_cases.py
+++ hat/import_export/tests/test_export_cases.py
@@ -1,57 +1,61 @@
 from datetime import datetime
 import pandas
-from django.test import TestCase
+from django.test import TransactionTestCase
 from django.utils import timezone
-from ..import_cases import import_cases_file
 from ..export_csv import export_csv
-from . import TEST_DATA
+
+# The number of types of cases in the fixtures
+NUM_CASES = 6
+NUM_HISTORIC_CASES = 2
+NUM_BACKUP_CASES = 3
+NUM_PV_CASES = 1
+NUM_SUSPECTS = 1
+NUM_ON_2016_1_2 = 2
 
 
-class ExportTests(TestCase):
-    def setUp(self):
-        import_cases_file('historic', TEST_DATA['historic']['file'])
-        import_cases_file('pv', TEST_DATA['pv']['file'])
-        import_cases_file('backup', TEST_DATA['mobile_backup']['file'])
+class ExportTests(TransactionTestCase):
+    fixtures = ['cases']
 
     def test_export(self):
+        from hat.cases.models import Case
         csv_file = export_csv()
         df = pandas.read_csv(csv_file)
-        self.assertEqual(len(df), TEST_DATA['total_count'])
+        self.assertEqual(len(df), NUM_CASES)
 
     def test_export_anon(self):
         csv_file = export_csv(anon=True)
         df = pandas.read_csv(csv_file)
-        self.assertEqual(len(df), TEST_DATA['total_count'])
+        self.assertEqual(len(df), NUM_CASES)
         self.assertTrue('name' not in df)
 
     def test_export_sources(self):
         csv_file = export_csv(sources=['historic'])
         df = pandas.read_csv(csv_file)
-        self.assertEqual(len(df), TEST_DATA['historic']['count'])
+        self.assertEqual(len(df), NUM_HISTORIC_CASES)
 
         csv_file = export_csv(sources=['pv'])
         df = pandas.read_csv(csv_file)
-        self.assertEqual(len(df), TEST_DATA['pv']['count'])
+        self.assertEqual(len(df), NUM_PV_CASES)
 
         csv_file = export_csv(sources=['mobile_backup'])
         df = pandas.read_csv(csv_file)
-        self.assertEqual(len(df), TEST_DATA['mobile_backup']['count'])
+        self.assertEqual(len(df), NUM_BACKUP_CASES)
 
         csv_file = export_csv(sources=['historic', 'pv'])
         df = pandas.read_csv(csv_file)
-        self.assertEqual(len(df), TEST_DATA['historic']['count'] + TEST_DATA['pv']['count'])
+        self.assertEqual(len(df), NUM_HISTORIC_CASES + NUM_PV_CASES)
 
     def test_export_suspects(self):
         csv_file = export_csv(only_suspects=True)
         df = pandas.read_csv(csv_file)
-        self.assertEqual(len(df), 2)
+        self.assertEqual(len(df), NUM_SUSPECTS)
 
     def test_export_dates(self):
-        historic_date = datetime(2016, 7, 14)
+        historic_date = datetime(2016, 1, 2)
         d = timezone.make_aware(historic_date)
         csv_file = export_csv(date_from=d, date_to=d)
         df = pandas.read_csv(csv_file)
-        self.assertTrue(len(df), TEST_DATA['historic']['count'])
+        self.assertTrue(len(df), NUM_ON_2016_1_2)
 
     def test_export_sep(self):
         def read_file(filename):

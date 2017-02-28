ff85e59be64a071e6ea13128824339745f8ba59a
Till Reitemeyer <till@skoni.de>
2017-02-28 15:34:26 +0100

    refactor(event_log): single sql template for all events

--- hat/cases/queries/event_log.sql
+++ hat/cases/queries/event_log.sql
@@ -13,67 +13,23 @@
 
 
 {% sql 'insert_event', note='create event' %}
-  {# This is the base event that forms one part of every event log.
-     Every event consists of two rows, one in the general event table and one
-     in the table with specific event data. Do not use this by itself #}
-  INSERT INTO hat_event
+  {# This is the query used for inserting events of any type. It will insert two rows
+     into two tables. One into the general event table and one into the specific
+     event table with a FK pointing to the corresponding row in the general table. #}
+  WITH E AS (INSERT INTO hat_event
       (table_name, created, updated, deleted, total)
-  VALUES (
-      {{ table_name|guards.string }}
+    VALUES (
+      {{ details_table|guards.string }}
     , {{ created|guards.integer }}
     , {{ updated|guards.integer }}
     , {{ deleted|guards.integer }}
     , {{ total|guards.integer }}
+    )
+    RETURNING id
   )
-  RETURNING id
-{% endsql %}
-
-
-{% sql 'insert_cases_file_import', note='create cases file import entry' %}
-  WITH e AS ({{ insert_event }})
-  INSERT INTO hat_import_cases_file_event
-      (id, filename, file_hash, contents, source_type)
-  SELECT e.id
-       , {{ name|guards.string }}
-       , {{ hash|guards.string }}
-       , {{ contents|guards.string }}
-       , {{ source_type|guards.string }}
-  FROM e
-  RETURNING id
-{% endsql %}
-
-
-{% sql 'insert_reconciled_file_import', note='create reconciled cases import entry' %}
-  WITH e AS ({{ insert_event }})
-  INSERT INTO hat_import_reconciled_file_event
-      (id, filename, file_hash, contents)
-  SELECT e.id
-       , {{ name|guards.string }}
-       , {{ hash|guards.string }}
-       , {{ contents|guards.string }}
-  FROM e
-  RETURNING id
-{% endsql %}
-
-
-{% sql 'insert_cases_merge', note='create merge entry' %}
-  WITH e AS ({{ insert_event }})
-  INSERT INTO hat_merge_cases_event
-      (id, documents)
-  SELECT e.id
-       , '{{ documents }}'
-  FROM e
-  RETURNING id
-{% endsql %}
-
-
-{% sql 'insert_cases_sync', note='insert sync entry' %}
-  WITH e AS ({{ insert_event }})
-  INSERT INTO hat_sync_cases_event
-      (id, documents, device_id)
-  SELECT e.id
-       , '{{ documents }}'
-       , {{ device_id|guards.string }}
-  FROM e
+  INSERT INTO {{ details_table }}
+         (id, {{ details_fields }})
+  SELECT E.id, {{ details_values }}
+  FROM E
   RETURNING id
 {% endsql %}

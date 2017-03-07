f49b7bde2981d973f28804429ad6f441871d3033
obdulia-losantos <obdulia.losantos@gmail.com>
2017-03-07 18:04:40 +0100

    feat(paginator): add more info and options

--- hat/common/paginator.py
+++ hat/common/paginator.py
@@ -2,33 +2,50 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def paginate(request, objects, prefix_url: str, page_size=25) -> dict:
-    paginator = Paginator(objects, page_size)
+    def get_page_url(p):
+        qs = request.GET.copy()
+        qs['page'] = p
+        return prefix_url + qs.urlencode()
 
+    paginator = Paginator(objects, page_size)
+    count = objects.count()
     page = request.GET.get('page')
+    total = paginator.num_pages
+
     try:
         items = paginator.page(page)
     except PageNotAnInteger:
         # If page is not an integer, deliver first page.
+        page = 1
         items = paginator.page(1)
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
-        items = paginator.page(paginator.num_pages)
+        page = paginator.num_pages
+        items = paginator.page(total)
 
-    next_url = None
+    first_url = None
     prev_url = None
+    next_url = None
+    last_url = None
 
-    if items.has_next():
-        qs = request.GET.copy()
-        qs['page'] = items.next_page_number()
-        next_url = prefix_url + qs.urlencode()
-    if items.has_previous():
-        qs = request.GET.copy()
-        qs['page'] = items.previous_page_number()
-        prev_url = prefix_url + qs.urlencode()
+    if count:
+        page = int(page)
+        if page > 1:
+            first_url = get_page_url(1)
+        if page > 2:
+            prev_url = get_page_url(page - 1)
+        if page < total - 1:
+            next_url = get_page_url(page + 1)
+        if page < total:
+            last_url = get_page_url(total)
 
     return {
         'items': items,
-        'count': objects.count(),
-        'next_url': next_url,
+        'count': count,
+        'current': page,
+        'total': total,
+        'first_url': first_url,
         'prev_url': prev_url,
+        'next_url': next_url,
+        'last_url': last_url,
     }

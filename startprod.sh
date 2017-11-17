a4eb1ce84534b74c40055f96299e19a74fccd52b
Martin De Wulf <madewulf@multitasked.net>
2017-11-17 15:04:34 +0100

    Iterations on prod config.

--- startprod.sh
+++ startprod.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 export RDS_DB_NAME="trypelim"
 export RDS_USERNAME="sense_hat"
 export RDS_PASSWORD="BhEzVaGy2njTjVvrJV)ETw"
-export RDS_HOSTNAME="sense-hat-dev-db.cfkkcjedzein.eu-west-1.rds.amazonaws.com"
+export RDS_HOSTNAME="trypelim.ct6zilwq9jpp.eu-central-1.rds.amazonaws.com"
 export RDS_PORT=5432
 
 docker-compose -f docker-compose-prod.yml up

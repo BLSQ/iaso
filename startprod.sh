fb6da41f319824ca2e424ca4f8365f8dfa2b523f
madewulf <mdewulf@bluesquarehub.com>
2017-11-17 11:54:37 +0000

    Adding prod docker config

---
+++ startprod.sh
@@ -0,0 +1,7 @@
+export RDS_DB_NAME="trypelim"
+export RDS_USERNAME="sense_hat"
+export RDS_PASSWORD="BhEzVaGy2njTjVvrJV)ETw"
+export RDS_HOSTNAME="sense-hat-dev-db.cfkkcjedzein.eu-west-1.rds.amazonaws.com"
+export RDS_PORT=5432
+
+docker-compose -f docker-compose-prod.yml up

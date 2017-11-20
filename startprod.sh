7d2b5d42b7d4267b149a01d2c7940c1cc4937aa4
Ubuntu <ubuntu@ip-172-31-32-155.eu-central-1.compute.internal>
2017-11-20 11:50:54 +0000

    more prod tweaks

--- startprod.sh
+++ startprod.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-./set-prod-env.sh
-docker-compose  -f docker-compose-prod.yml up 
+
+export RDS_DB_NAME="trypelim"
+export RDS_USERNAME="sense_hat"
+export RDS_PASSWORD="BhEzVaGy2njTjVvrJV)ETw"
+export RDS_HOSTNAME="trypelim.ct6zilwq9jpp.eu-central-1.rds.amazonaws.com"
+export RDS_PORT="5432"
+export COUCHDB_PASSWORD="q>sv2tBYDTcdVDvgipLCDN"
+
+docker-compose  -f docker-compose-prod.yml restart

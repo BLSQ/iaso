d171e7c2ea80a5fbad63c78ee2a4d6e4097da475
Martin De Wulf <madewulf@multitasked.net>
2018-01-10 16:58:39 +0100

    Memory efficient linkcasestovillage (to be able to run them on our tiny ec2 instances)

--- hat/cases/management/commands/linkcasestovillage.py
+++ hat/cases/management/commands/linkcasestovillage.py
@@ -2,6 +2,28 @@ from django.core.management.base import BaseCommand
 
 from hat.cases.models import Case
 from hat.geo.models import Village
+import gc
+
+
+def queryset_iterator(queryset, chunksize=1000):
+    '''''
+    Iterate over a Django Queryset ordered by the primary key
+
+    This method loads a maximum of chunksize (default: 1000) rows in it's
+    memory at the same time while django normally would load all rows in it's
+    memory. Using the iterator() method only causes it to not preload all the
+    classes.
+
+    Note that the implementation of the iterator does not support ordered query sets.
+    '''
+    pk = 0
+    last_pk = queryset.order_by('-pk')[0].pk
+    queryset = queryset.order_by('pk')
+    while pk < last_pk:
+        for row in queryset.filter(pk__gt=pk)[:chunksize]:
+            pk = row.pk
+            yield row
+        gc.collect()
 
 
 class Command(BaseCommand):
@@ -11,7 +33,7 @@ class Command(BaseCommand):
         cases = Case.objects.filter(normalized_village__isnull=True, village__isnull=False, AS__isnull=False,
                                     ZS__isnull=False, province__isnull=False)
         count = 0
-        for case in cases:
+        for case in queryset_iterator(cases):
             attempted_villages = list(Village.objects.filter(name__iexact=case.village.strip(),
                                                         AS__name__iexact=case.AS.strip(),
                                                         AS__ZS__name__iexact=case.ZS.strip(),

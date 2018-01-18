50cfcb68d4317301509fd036ddd3ca6265c58391
Martin De Wulf <madewulf@multitasked.net>
2018-01-18 11:53:49 +0100

    Removing dead code

--- hat/assets/js/utils/fetchData.js
+++ hat/assets/js/utils/fetchData.js
@@ -32,20 +32,6 @@ export function checkLocation (params, results, dispatch) {
   return true
 }
 
-export function getFromCookie (name) {
-  // https://docs.djangoproject.com/en/dev/ref/csrf/#ajax
-  if (document.cookie && document.cookie !== '') {
-    const cookies = document.cookie.split(';')
-    for (var i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim()
-      // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
-        return decodeURIComponent(cookie.substring(name.length + 1))
-      }
-    }
-  }
-}
-
 export function fetchUrls (urls, params, oldParams, dispatch, checkResults) {
   if (deepEqual(oldParams, params, true)) {
     return

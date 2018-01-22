3cc73ef3253fbdd624072fbff24e472b44faf839
Beygorghor <c@eduz.be>
2018-01-22 14:54:12 +0100

    Auto stash before merge of "development" and "origin/development"

--- hat/settings.py
+++ hat/settings.py
@@ -326,9 +326,9 @@ SECURE_SSL_REDIRECT = not DEBUG
 
 #############
 
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',

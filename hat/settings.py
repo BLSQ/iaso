1b546b960eb3204a6daa1a95aee77d0f24065e3e
Beygorghor <c@eduz.be>
2018-01-23 10:41:29 +0100

    roll back settings.py

--- hat/settings.py
+++ hat/settings.py
@@ -326,9 +326,8 @@ SECURE_SSL_REDIRECT = not DEBUG
 
 #############
 
-if not DEBUG:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',

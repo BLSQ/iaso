33cfed8c1a88fe00061f55f323e2e14e2b02212a
Martin De Wulf <madewulf@multitasked.net>
2018-04-09 16:27:57 +0200

    Fixing webpack.prod.js + committing assets

--- hat/assets/webpack/styles-29ad909db376c1022673.js
+++
@@ -1,1 +0,0 @@
-var HAT=HAT||{};HAT.styles=webpackJsonpHAT__name_([7],{542:function(n,A){}},[542]);
---
+++ hat/assets/webpack/styles-9a1d1261a586747a960a.js
@@ -0,0 +1,1 @@
+var HAT=HAT||{};HAT.styles=webpackJsonpHAT__name_([7],{558:function(n,A){}},[558]);

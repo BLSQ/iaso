c40f260d90f2e4a8d10b145502e0dcf73da09249
Martin De Wulf <madewulf@multitasked.net>
2018-05-11 13:21:49 +0200

    Generating way better plannings for teams.

--- hat/api/coordination.py
+++ hat/api/coordination.py
@@ -1,14 +1,53 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-
+from tsp_solver.greedy import solve_tsp
+from geopy.distance import vincenty
 from hat.planning.models import Planning, Assignation
-from hat.geo.models import ZS
+from hat.geo.models import ZS, Village
 from hat.users.models import Coordination
 from .authentication import CsrfExemptSessionAuthentication
 from rest_framework.authentication import BasicAuthentication
 from collections import defaultdict
-from math import ceil
+
+
+def optimize_path(assignation_list):
+    matrix = [[] for _ in assignation_list]
+
+    village_ids = [obj['village_id'] for obj in assignation_list]
+    village_queryset = Village.objects.filter(id__in=village_ids)
+    villages = {v.id: v for v in village_queryset}
+    total_population = sum([village.population for village in village_queryset])
+    i = 0
+    for assignation1 in assignation_list:
+        j = 0
+        village_1 = villages[assignation1['village_id']]
+        for assignation2 in assignation_list:
+            village_2 = villages[assignation2['village_id']]
+            village_1_coordinates = (village_1.longitude, village_1.latitude)
+            village_2_coordinates = (village_2.longitude, village_2.latitude)
+            matrix[i].append(vincenty(village_1_coordinates, village_2_coordinates).km)
+            j += 1
+        i += 1
+    path = solve_tsp(matrix)
+    res = []
+    current_population = 0
+    current_month = 1
+
+    i = 0
+
+    for index in path:
+        assignation_dict = assignation_list[index]
+        current_population += villages[assignation_dict['village_id']].population
+        if current_population > (total_population / 12.0) * current_month:
+            current_month += 1
+        assignation_dict['month'] = min(current_month, 12)
+        assignation_dict['index'] = i
+        res.append(assignation_list[index])
+        i += 1
+
+    return res
+
 
 class CoordinationViewSet(viewsets.ViewSet):
     """
@@ -55,14 +94,14 @@ class CoordinationViewSet(viewsets.ViewSet):
 
             for team_id in teams_dict:
                 assignation_list = teams_dict[team_id]
-                size = len(assignation_list)
-                for index, obj in enumerate(assignation_list):
+                ordered = optimize_path(assignation_list)
+                for index, obj in enumerate(ordered):
 
                     Assignation.objects.filter(planning=planning, village_id=obj['village_id']).delete()
                     assignation = Assignation()
                     assignation.planning = planning
-                    assignation.index = index
-                    assignation.month = max(1, int(ceil((index * 12.0)/size)))
+                    assignation.index = obj['index']
+                    assignation.month = obj['month']
                     assignation.village_id = obj['village_id']
                     assignation.team_id = obj['team_id']
                     assignation.save()
@@ -74,8 +113,8 @@ class CoordinationViewSet(viewsets.ViewSet):
                     coordination.save()
                 coordination.ZS.clear()
                 for location in locations:
-                    newLocation = get_object_or_404(ZS, pk=location['id'])
-                    coordination.ZS.add(newLocation)
+                    new_location = get_object_or_404(ZS, pk=location['id'])
+                    coordination.ZS.add(new_location)
             coordination.save()
 
         return Response(coordination.as_dict())

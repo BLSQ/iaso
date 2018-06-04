3b36bc06e626123290e5f07b109334516c93c69a
Martin De Wulf <madewulf@multitasked.net>
2018-06-04 17:58:17 +0200

    Removing the limit of the 5 last years for the locator tool.

--- hat/api/cases.py
+++ hat/api/cases.py
@@ -20,7 +20,7 @@ class CasesViewSet(viewsets.ViewSet):
 
         orders = request.GET.get("order", 'ZS').split(',')
         queryset = Case.objects.filter(normalized_village=None, normalized_village_not_found=False, confirmed_case=True)\
-            .exclude(source='mobile_sync').exclude(source='mobile_backup').exclude(document_date__year__lte=2013)\
+            .exclude(source='mobile_sync').exclude(source='mobile_backup')\
             .exclude(province__icontains='kas').exclude(province__icontains='kinsh').exclude(province__icontains='bas')\
             .exclude(province__icontains='maniema').exclude(province__icontains='k.').exclude(province__icontains='equateur') \
             .order_by(*orders)

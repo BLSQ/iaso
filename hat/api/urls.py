be3e7aa7c3bf81affdb330c7ff9d5ecbcb906f98
Martin De Wulf <madewulf@multitasked.net>
2018-06-22 17:31:31 +0200

    [SLEEP-194] Adding an API to retrieve the list of villages where cases have been recorded by a device.

--- hat/api/urls.py
+++ hat/api/urls.py
@@ -19,28 +19,27 @@ from .qc_check_stats import QCCheckStatsViewSet
 from .traps import TrapsViewSet
 from .targets import TargetsViewSet
 from .metrics import MetricsViewSet
-
+from .device_locations import DeviceVillageViewSet
 
 router = routers.DefaultRouter()
-router.register(r'datasets', DatasetViewSet, base_name='datasets')
-router.register(r'plannings', PlanningViewSet, base_name='planning')
-router.register(r'assignations', AssignationViewSet, base_name='assignations')
-router.register(r'provinces', ProvinceViewSet, base_name='provinces')
-router.register(r'as', ASViewSet, base_name='as')
-router.register(r'zs', ZSViewSet, base_name='zs')
-router.register(r'coordinations', CoordinationViewSet, base_name='coordinations')
-router.register(r'teams', TeamViewSet, base_name='teams')
-router.register(r'villages', VillageViewSet, base_name='village')
-router.register(r'algo', AlgoViewSet, base_name='algo')
-router.register(r'cases', CasesViewSet, base_name='cases')
-router.register(r'qcstats', QCStatsViewSet, base_name='qcstats')
-router.register(r'qctests', QCTestsViewSet, base_name='qctests')
-router.register(r'checks', QCChecksViewSet, base_name='checks')
-router.register(r'qccheckstats', QCCheckStatsViewSet, base_name='qc_check_stats')
-router.register(r'traps', TrapsViewSet, base_name='traps')
-router.register(r'targets', TargetsViewSet, base_name='targets')
-router.register(r'metrics', MetricsViewSet, base_name='metrics')
+router.register(r"datasets", DatasetViewSet, base_name="datasets")
+router.register(r"plannings", PlanningViewSet, base_name="planning")
+router.register(r"assignations", AssignationViewSet, base_name="assignations")
+router.register(r"provinces", ProvinceViewSet, base_name="provinces")
+router.register(r"as", ASViewSet, base_name="as")
+router.register(r"zs", ZSViewSet, base_name="zs")
+router.register(r"coordinations", CoordinationViewSet, base_name="coordinations")
+router.register(r"teams", TeamViewSet, base_name="teams")
+router.register(r"villages", VillageViewSet, base_name="village")
+router.register(r"algo", AlgoViewSet, base_name="algo")
+router.register(r"cases", CasesViewSet, base_name="cases")
+router.register(r"qcstats", QCStatsViewSet, base_name="qcstats")
+router.register(r"qctests", QCTestsViewSet, base_name="qctests")
+router.register(r"checks", QCChecksViewSet, base_name="checks")
+router.register(r"qccheckstats", QCCheckStatsViewSet, base_name="qc_check_stats")
+router.register(r"traps", TrapsViewSet, base_name="traps")
+router.register(r"targets", TargetsViewSet, base_name="targets")
+router.register(r"metrics", MetricsViewSet, base_name="metrics")
+router.register(r"devicevillages", DeviceVillageViewSet, base_name="devicevillages")
 
-urlpatterns = [
-    url(r'^', include(router.urls)),
-]
+urlpatterns = [url(r"^", include(router.urls))]

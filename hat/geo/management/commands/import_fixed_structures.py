9677013d97387b3037e0c08154f986f06a38231e
madewulf <madewulf@multitasked.net>
2018-11-13 14:25:00 +0100

    Fixing the import of fixed structures.

--- hat/geo/management/commands/import_fixed_structures.py
+++ hat/geo/management/commands/import_fixed_structures.py
@@ -32,71 +32,73 @@ class Command(BaseCommand):
         organisation_units = j['organisationUnits']
         counter = 0
         all_structures_not_found = []
-        for org_unit in organisation_units:
-            level = org_unit.get("level", None)
-            cleaned_name = clean(org_unit['name'])
-            identifier = org_unit['id']
-            if level == 2:
-                try:
-                    province = Province.objects.get(name=cleaned_name)
-                    if not province.source_ref:
-                        province.source_ref = identifier
-                        province.save()
-                except:
-                    print("NOT FOUND LEVEL 2", clean(org_unit['name']))
-
-            if level == 3:
-                province = Province.objects.get(source_ref=org_unit["parent"]["id"])
-                zones = get_zones_by_name_or_alias(cleaned_name, [province])
-                if len(zones) == 1:
-                    zone = zones[0]
-                    if not zone.source_ref:
-                        zone.source_ref = identifier
-                        zone.save()
-                else:
-                    print("NOT FOUND LEVEL 3", clean(org_unit['name']))
-                    print("zone problem", zones)
-
-            if level == 4:
-                try:
-                    zone = ZS.objects.get(source_ref=org_unit["parent"]["id"])
-                    areas = get_areas_by_name_or_alias(cleaned_name, [zone])
-                    if len(areas) == 1:
-                        area = areas[0]
-                        if not area.source_ref:
-                            area.source_ref = identifier
-                            area.save()
-                    else:
-                        print("NOT FOUND LEVEL 4", clean(org_unit['name']))
-                        print("area problem", cleaned_name, areas)
-                except:
-                    print("exception: zone Not Found")
-
-            if level == 5:
-                try:
-                    HealthStructure.objects.get(source_ref=identifier)
-                except:
-                    area = None
+        for current_level in [2, 3, 4, 5]:
+            print('current_level', current_level)
+            for org_unit in organisation_units:
+                level = org_unit.get("level", None)
+                cleaned_name = clean(org_unit['name'])
+                identifier = org_unit['id']
+                if level == current_level and current_level == 2:
                     try:
-                        area = AS.objects.get(source_ref=org_unit["parent"]["id"])
+                        province = Province.objects.get(name__iexact=cleaned_name)
+                        if not province.source_ref:
+                            province.source_ref = identifier
+                            province.save()
                     except:
-                        print("NOT FOUND *******")
-                        print(org_unit)
-                        all_structures_not_found.append(org_unit['path'])
+                        print("NOT FOUND LEVEL 2", cleaned_name)
 
-                        counter = counter + 1
-                    if area is not None:
-                        hs = HealthStructure()
-                        hs.name = cleaned_name
-                        hs.AS = area
-                        hs.source_ref = identifier
-                        hs.source = 'snis'
-                        coordinates = org_unit.get("coordinates", None)
-                        if coordinates is not None:
-                            tuple = list(reversed(json.loads(coordinates)))
-                            hs.location = Point(tuple)
-                        hs.save()
-                        print("structure created", cleaned_name)
+                if level == current_level and current_level == 3:
+                    province = Province.objects.get(source_ref=org_unit["parent"]["id"])
+                    zones = get_zones_by_name_or_alias(cleaned_name, [province])
+                    if len(zones) == 1:
+                        zone = zones[0]
+                        if not zone.source_ref:
+                            zone.source_ref = identifier
+                            zone.save()
+                    else:
+                        print("NOT FOUND LEVEL 3", clean(org_unit['name']))
+                        print("zone problem", zones)
+
+                if level == current_level and current_level == 4:
+                    try:
+                        zone = ZS.objects.get(source_ref=org_unit["parent"]["id"])
+                        areas = get_areas_by_name_or_alias(cleaned_name, [zone])
+                        if len(areas) == 1:
+                            area = areas[0]
+                            if not area.source_ref:
+                                area.source_ref = identifier
+                                area.save()
+                        else:
+                            print("NOT FOUND LEVEL 4", clean(org_unit['name']))
+                            print("area problem", cleaned_name, areas)
+                    except:
+                        print("exception: zone Not Found")
+
+                if level == current_level and current_level == 5:
+                    try:
+                        HealthStructure.objects.get(source_ref=identifier)
+                    except:
+                        area = None
+                        try:
+                            area = AS.objects.get(source_ref=org_unit["parent"]["id"])
+                        except:
+                            print("NOT FOUND *******")
+                            print(org_unit)
+                            all_structures_not_found.append(org_unit['path'])
+
+                            counter = counter + 1
+                        if area is not None:
+                            hs = HealthStructure()
+                            hs.name = cleaned_name
+                            hs.AS = area
+                            hs.source_ref = identifier
+                            hs.source = 'snis'
+                            coordinates = org_unit.get("coordinates", None)
+                            if coordinates is not None:
+                                tuple = list(reversed(json.loads(coordinates)))
+                                hs.location = Point(tuple)
+                            hs.save()
+                            print("structure created", cleaned_name)
 
         print("Structures NOT created", counter)
 

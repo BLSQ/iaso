24bb4b1e24f8f6f51f99b33cada70ed21e2ddd78
Eric Darchis <edarchis@bluesquare.com>
2018-12-06 23:28:04 +0100

    Huge perf gain on patient duplicates check by removing p1_id>p2_id
    The view constraint forcing patient1_id to be greater than patient2_id makes the view extremely slow on new records.
    The constraint is only enforced in the mass rebuild to avoid adding both sides of a match.

--- hat/patient/migrations/views/patient_matching_namesim.sql
+++ hat/patient/migrations/views/patient_matching_namesim.sql
@@ -14,6 +14,6 @@ create or replace view patient_matching_namesimilarity as
   and p1.mothers_surname % p2.mothers_surname
   and (p1.year_of_birth is null or p2.year_of_birth is null
        or abs(p2.year_of_birth-p1.year_of_birth)<5)
-  and p1.id > p2.id
+  and p1.id <> p2.id
   and not exists (select 1 from patient_patientignoredpair i
     where i.patient1_id=p1.id and i.patient2_id=p2.id)

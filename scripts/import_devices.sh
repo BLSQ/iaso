16f8ec2a565ccc26ad86222a1cbb9e50aff9f566
Eric Darchis <edarchis@bluesquare.com>
2019-03-12 00:07:37 +0100

    Prevent multiple instances of import_devices in cron

--- scripts/import_devices.sh
+++ scripts/import_devices.sh
@@ -2,5 +2,7 @@
 cd /opt/python/current/app
 source /opt/python/current/env
 source /opt/python/run/venv/bin/activate
-date |gzip -c>> /home/ec2-user/import_devices.log.gz
-./manage.py importdevices 2>&1 |gzip -c>> /home/ec2-user/import_devices.log.gz
+if [[ $(ps aux|grep "manage.py importdevices"|grep -v grep|wc -l) -eq 0 ]]; then
+  date |gzip -c>> /home/ec2-user/import_devices.log.gz
+  ./manage.py importdevices 2>&1 |gzip -c>> /home/ec2-user/import_devices.log.gz
+fi

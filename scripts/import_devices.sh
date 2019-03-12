522be8453a5a3a3ecd12e780e02ea743a0658cfa
Eric Darchis <edarchis@bluesquare.com>
2019-03-12 10:47:43 +0100

    Improved logging in import_devices.sh

--- scripts/import_devices.sh
+++ scripts/import_devices.sh
@@ -2,7 +2,11 @@
 cd /opt/python/current/app
 source /opt/python/current/env
 source /opt/python/run/venv/bin/activate
+LOGDIR=/home/ec2-user/import_devices_logs
+LOGFILE=${LOGDIR}/import_devices_$(date +%Y%m%d%H%M%S)_$$.log
+mkdir -p /home/ec2-user/import_devices_logs
 if [[ $(ps aux|grep "manage.py importdevices"|grep -v grep|wc -l) -eq 0 ]]; then
-  date |gzip -c>> /home/ec2-user/import_devices.log.gz
-  ./manage.py importdevices 2>&1 |gzip -c>> /home/ec2-user/import_devices.log.gz
+  date >> $LOGFILE
+  ./manage.py importdevices 2>&1 >> ${LOGFILE}
 fi
+gzip -9 ${LOGFILE}

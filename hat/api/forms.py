8567c69246ad87fefff5fc600b749b4eef8ad230
madewulf <madewulf@multitasked.net>
2019-07-30 17:50:01 +0200

    Fixing the counter on the list of forms

--- hat/api/forms.py
+++ hat/api/forms.py
@@ -23,23 +23,34 @@ class FormsViewSet(viewsets.ViewSet):
         queryset = Form.objects
         limit = request.GET.get("limit", None)
         page_offset = request.GET.get("page", 1)
-        order = request.GET.get("order", "instance_updated_at").split(",")  # TODO: allow this only from a predefined list for security purpose
+        order = request.GET.get("order", "instance_updated_at").split(
+            ","
+        )  # TODO: allow this only from a predefined list for security purpose
         from_date = request.GET.get("date_from", None)
         to_date = request.GET.get("date_to", None)
         csv_format = request.GET.get("csv", None)
         xlsx_format = request.GET.get("xlsx", None)
 
         queryset = queryset.annotate(instance_updated_at=Max("instance__updated_at"))
-        queryset = queryset.annotate(instances_count=Count("instance"))
+        queryset = queryset.annotate(
+            instances_count=Count("instance", filter=(~Q(instance__file="")))
+        )
+
         additional_fields = ["instance_updated_at", "instances_count"]
         queryset = queryset.order_by(*order)
         if from_date:
             queryset = queryset.filter(
-                Q(instance_updated_at__gte=from_date) | Q(created_at__gte=from_date) | Q(updated_at__gte=from_date))
+                Q(instance_updated_at__gte=from_date)
+                | Q(created_at__gte=from_date)
+                | Q(updated_at__gte=from_date)
+            )
 
         if to_date:
             queryset = queryset.filter(
-                Q(instance_updated_at__lte=to_date) | Q(created_at__lte=to_date) | Q(updated_at__lte=to_date))
+                Q(instance_updated_at__lte=to_date)
+                | Q(created_at__lte=to_date)
+                | Q(updated_at__lte=to_date)
+            )
 
         if csv_format is None and xlsx_format is None:
             if limit:
@@ -52,7 +63,9 @@ class FormsViewSet(viewsets.ViewSet):
                     page_offset = paginator.num_pages
                 page = paginator.page(page_offset)
 
-                res["forms"] = map(lambda x: x.as_dict(additional_fields), page.object_list)
+                res["forms"] = map(
+                    lambda x: x.as_dict(additional_fields), page.object_list
+                )
                 res["has_next"] = page.has_next()
                 res["has_previous"] = page.has_previous()
                 res["page"] = page_offset
@@ -60,7 +73,9 @@ class FormsViewSet(viewsets.ViewSet):
                 res["limit"] = limit
                 return Response(res)
             else:
-                return Response({"forms": [form.as_dict(additional_fields) for form in queryset]})
+                return Response(
+                    {"forms": [form.as_dict(additional_fields) for form in queryset]}
+                )
         else:
             columns = [
                 {"title": "ID du formulaire", "width": 20},
@@ -75,9 +90,14 @@ class FormsViewSet(viewsets.ViewSet):
             def get_row(qsform, **kwargs):
                 fdict = qsform.as_dict(additional_fields)
                 created_at = timestamp_to_datetime(fdict.get("created_at"))
-                updated_at = fdict.get("instance_updated_at").strftime("%Y-%m-%d %H:%M:%S") if fdict.get(
-                    "instance_updated_at") else "2019-01-01 00:00:00"
-                org_unit_types = ", ".join([o['name'] for o in fdict.get("org_unit_types") if o is not None])
+                updated_at = (
+                    fdict.get("instance_updated_at").strftime("%Y-%m-%d %H:%M:%S")
+                    if fdict.get("instance_updated_at")
+                    else "2019-01-01 00:00:00"
+                )
+                org_unit_types = ", ".join(
+                    [o["name"] for o in fdict.get("org_unit_types") if o is not None]
+                )
                 return [
                     fdict.get("form_id"),
                     fdict.get("name"),

0e3cfc4f98808ad5281142a84c1023dd047ec872
madewulf <madewulf@multitasked.net>
2019-08-02 16:12:54 +0200

    Improvements to permissions (getting rid of the infamous “anounymous view”

--- hat/menupermissions/models.py
+++ hat/menupermissions/models.py
@@ -9,7 +9,7 @@ ROUTES = _("Routes")
 GRAPHS = _("Graphs")
 REPORTS = _("Reports")
 CASES = _("Cases")
-CASE_ANALYSIS  = _("Cases analysis")
+CASE_ANALYSIS = _("Cases analysis")
 RECONCILIATION = _("Reconciliation")
 DEVICES = _("Devices")
 PLANNINGS = _("Plannings")
@@ -23,7 +23,7 @@ LOCATOR = _("Locator")
 VECTOR_CONTROL = _("Vector control")
 VECTOR_CONTROL_UPLOAD = _("Vector control import Gpx")
 QUALITY_CONTROL = _("Quality control")
-ANONYMOUS_VIEW = _("Anonymous view")
+ANONYMOUS_VIEW = "Anonymisation des patients"
 EDIT_PATIENT = _("Edition d'un patient")
 DOWNLOAD_DATAS = _("Téléchargement de données")
 DUPLICATES = _("Doublons")
@@ -33,33 +33,33 @@ class CustomPermissionSupport(models.Model):
     class Meta:
 
         managed = False  # No database table creation or deletion operations \
-                         # will be performed for this model.
+        # will be performed for this model.
 
         permissions = (
-            ('x_datasets_datauploads', UPLOAD_OF_CASE_FILES),
-            ('x_datasets_villageuploads', UPLOAD_OF_VILLAGES),
-            ('x_plannings_macroplanning', MACROPLANNING),
-            ('x_plannings_microplanning', MICROPLANNING),
-            ('x_plannings_routes', ROUTES),
-            ('x_stats_graphs', GRAPHS),
-            ('x_stats_reports', REPORTS),
-            ('x_case_cases', CASES),
-            ('x_case_analysis', CASE_ANALYSIS),
-            ('x_case_reconciliation', RECONCILIATION),
-            ('x_management_devices', DEVICES),
-            ('x_management_plannings', PLANNINGS),
-            ('x_management_plannings_template', PLANNINGS_TEMPLATE),
-            ('x_management_coordinations', COORDINATIONS),
-            ('x_management_workzones', WORKZONES),
-            ('x_management_teams', TEAMS),
-            ('x_management_users', USERS),
-            ('x_management_villages', VILLAGES),
-            ('x_locator', LOCATOR),
-            ('x_vectorcontrol', VECTOR_CONTROL),
-            ('x_vectorcontrolupload', VECTOR_CONTROL_UPLOAD),
-            ('x_qualitycontrol', QUALITY_CONTROL),
-            ('x_anonymous', ANONYMOUS_VIEW),
-            ('x_datas_patient_edition', EDIT_PATIENT),
-            ('x_datas_download', DOWNLOAD_DATAS),
-            ('x_duplicates', DUPLICATES),
+            ("x_datasets_datauploads", UPLOAD_OF_CASE_FILES),
+            ("x_datasets_villageuploads", UPLOAD_OF_VILLAGES),
+            ("x_plannings_macroplanning", MACROPLANNING),
+            ("x_plannings_microplanning", MICROPLANNING),
+            ("x_plannings_routes", ROUTES),
+            ("x_stats_graphs", GRAPHS),
+            ("x_stats_reports", REPORTS),
+            ("x_case_cases", CASES),
+            ("x_case_analysis", CASE_ANALYSIS),
+            ("x_case_reconciliation", RECONCILIATION),
+            ("x_management_devices", DEVICES),
+            ("x_management_plannings", PLANNINGS),
+            ("x_management_plannings_template", PLANNINGS_TEMPLATE),
+            ("x_management_coordinations", COORDINATIONS),
+            ("x_management_workzones", WORKZONES),
+            ("x_management_teams", TEAMS),
+            ("x_management_users", USERS),
+            ("x_management_villages", VILLAGES),
+            ("x_locator", LOCATOR),
+            ("x_vectorcontrol", VECTOR_CONTROL),
+            ("x_vectorcontrolupload", VECTOR_CONTROL_UPLOAD),
+            ("x_qualitycontrol", QUALITY_CONTROL),
+            ("x_anonymous", ANONYMOUS_VIEW),
+            ("x_datas_patient_edition", EDIT_PATIENT),
+            ("x_datas_download", DOWNLOAD_DATAS),
+            ("x_duplicates", DUPLICATES),
         )
--- hat/settings.py
+++ hat/settings.py
@@ -15,7 +15,7 @@ import os
 STAGING = os.environ.get("STAGING", "").lower() == "true"
 TESTING = os.environ.get("TESTING", "").lower() == "true"
 FLAVOR = os.environ.get("FLAVOR", "trypelim")  # alternative is "iaso"
-
+print("FLAVOR", FLAVOR)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 

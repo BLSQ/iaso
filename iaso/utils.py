e2d5e7e1079a6052ec88b62977a24267ef38c80b
madewulf <madewulf@multitasked.net>
2019-08-07 19:54:24 +0200

    PEP 8

--- iaso/utils.py
+++ iaso/utils.py
@@ -1,35 +1,37 @@
 from bs4 import BeautifulSoup as Soup
 from datetime import datetime
-from django.utils import timezone
+
 
 def timestamp_to_datetime(timestamp):
     date = datetime.fromtimestamp(timestamp)
     return date.strftime("%Y-%m-%d %H:%M:%S")
 
 
-def getflatChildrenTree(el, flatXmlDict):
+def get_flat_children_tree(el, flat_xml_dict):
     for children in el.findChildren(None, {}, False):
         if len(children.findChildren(None, {}, False)) > 0:
-            getflatChildrenTree(children, flatXmlDict)
+            get_flat_children_tree(children, flat_xml_dict)
         else:
-            flatXmlDict[children.name] = children.text
+            flat_xml_dict[children.name] = children.text
 
 
-def getChildrenTree(el):
-    xmlDict = {}
+def get_children_tree(el):
+    xml_dict = {}
     for children in el.findChildren(None, {}, False):
         if len(children.findChildren(None, {}, False)) > 0:
-            xmlDict[children.name] = getChildrenTree(children)
+            xml_dict[children.name] = get_children_tree(children)
         else:
-            xmlDict[children.name] = children.text
-    return xmlDict
+            xml_dict[children.name] = children.text
+    return xml_dict
 
-def parseXMLFile(file):
-    soup = Soup(file.read(), features="html.parser")
-    return getChildrenTree(soup)
 
-def flatParseXMLFile(file):
+def parse_xml_file(file):
     soup = Soup(file.read(), features="html.parser")
-    flatXmlDict = {}
-    getflatChildrenTree(soup, flatXmlDict)
-    return flatXmlDict
+    return get_children_tree(soup)
+
+
+def flat_parse_xml_file(file):
+    soup = Soup(file.read(), features="html.parser")
+    flat_xml_dict = {}
+    get_flat_children_tree(soup, flat_xml_dict)
+    return flat_xml_dict

ee2187733c0642849c23fa2582cf0cc309e1a4a0
madewulf <madewulf@multitasked.net>
2019-09-04 15:53:53 +0200

    Committing assets

--- hat/assets/js/apps/Management/pages/ManagementVillages.js
+++ hat/assets/js/apps/Management/pages/ManagementVillages.js
@@ -251,7 +251,7 @@ class ManagementVillages extends React.Component {
                             onClick={() => this.props.selectVillage(newItem)}
                         >
                             <i className="fa fa-plus" />
-                            <FormattedMessage id="main.label.new" defaultMessage="Nouveau" />
+                            <FormattedMessage id="main.label.new" defaultMessage="New" />
                         </button>
                     </SearchButton>
                 </div>

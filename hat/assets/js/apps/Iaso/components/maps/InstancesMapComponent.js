4f83af2bb7bd8f7fd816d7a421bb3ac147373731
Christophe Gerard <cgerard@bluesquarehub.com>
2019-09-09 14:03:39 +0200

    put back image on map

--- hat/assets/js/apps/Iaso/components/maps/InstancesMapComponent.js
+++ hat/assets/js/apps/Iaso/components/maps/InstancesMapComponent.js
@@ -5,8 +5,14 @@ import {
 } from 'react-leaflet';
 import { FormattedMessage } from 'react-intl';
 
-import { withStyles } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
+import {
+    withStyles,
+    Grid,
+    CardMedia,
+    CardContent,
+    Card,
+    Hidden,
+} from '@material-ui/core';
 
 import PropTypes from 'prop-types';
 import moment from 'moment';
@@ -19,6 +25,15 @@ import commonStyles from '../../styles/common';
 
 const styles = theme => ({
     ...commonStyles(theme),
+    popup: {
+        margin: 0,
+        '& .leaflet-popup-content': {
+            margin: 0,
+        },
+        '& .leaflet-popup-close-button': {
+            color: 'white !important',
+        },
+    },
     link: {
         wordBreak: 'break-all',
     },
@@ -30,6 +45,29 @@ const styles = theme => ({
     },
     listItem: {
         textAlign: 'left',
+    },
+    card: {
+        height: '100%',
+        width: '100%',
+        borderRadius: 12,
+        overflow: 'hidden',
+        boxShadow: 'none',
+    },
+    cardMedia: {
+        height: 120,
+    },
+    cardContent: {
+        margin: theme.spacing(2),
+        overflow: 'hidden',
+        wordBreak: 'break-all',
+        padding: '0 !important',
+    },
+    fileList: {
+        paddingLeft: theme.spacing(2),
+    },
+    fileItem: {
+        position: 'relative',
+        left: -theme.spacing(1) / 2,
     },
 });
 
@@ -55,79 +93,93 @@ class InstancesMap extends Component {
                                 if (!i.latitude || !i.longitude || !isValidCoordinate(i.latitude, i.longitude)) return null;
                                 return (
                                     <Marker position={[i.latitude, i.longitude]} key={i.id}>
-                                        <Popup>
-                                            <Grid container spacing={0}>
-                                                <Grid item xs={4} className={classes.listItemLabel}>
-                                                    <FormattedMessage id="iaso.label.name" defaultMessage="Name" />
-                                                    :
-                                                </Grid>
-                                                <Grid item xs={8} className={classes.listItem}>
-                                                    {i.org_unit.name}
-                                                </Grid>
-                                            </Grid>
-                                            <Grid container spacing={0}>
-                                                <Grid item xs={4} className={classes.listItemLabel}>
-                                                    <FormattedMessage id="iaso.label.type" defaultMessage="Type" />
-                                                    :
-                                                </Grid>
-                                                <Grid item xs={8} className={classes.listItem}>
-                                                    {i.org_unit.org_unit_type_name}
-                                                </Grid>
-                                            </Grid>
-                                            <Grid container spacing={0}>
-                                                <Grid item xs={4} className={classes.listItemLabel}>
-                                                    <FormattedMessage id="iaso.instance.device" defaultMessage="IMEI appareil" />
-                                                    :
-                                                </Grid>
-                                                <Grid item xs={8} className={classes.listItem}>
-                                                    {i.device_id ? i.device_id : '/'}
-                                                </Grid>
-                                            </Grid>
-                                            <Grid container spacing={1}>
-                                                <Grid item xs={4} className={classes.listItemLabel}>
-                                                    <FormattedMessage id="iaso.instance.coordinate" defaultMessage="Coordinates" />
-                                                    :
-                                                </Grid>
-                                                <Grid item xs={8} className={classes.listItem}>
-                                                    {' '}
-                                                    Long:
-                                                    {' '}
-                                                    {i.longitude}
-                                                    {' '}
-                                                    <br />
-                                                    {' '}
-                                                    Lat:
-                                                    {' '}
-                                                    {i.latitude}
-                                                </Grid>
-                                            </Grid>
-                                            <Grid container spacing={0}>
-                                                <Grid item xs={4} className={classes.listItemLabel}>
-                                                    <FormattedMessage id="iaso.instance.created_at" defaultMessage="Created at" />
-                                                    :
-                                                </Grid>
-                                                <Grid item xs={8} className={classes.listItem}>
-                                                    {moment.unix(i.created_at).format('DD/MM/YYYY HH:mm')}
-                                                </Grid>
-                                            </Grid>
-                                            <Grid container spacing={0}>
-                                                <Grid item xs={4} className={classes.listItemLabel}>
-                                                    <FormattedMessage id="iaso.instance.file" defaultMessage="File" />
-                                                    :
-                                                </Grid>
-                                                <Grid item xs={8} className={classes.listItem}>
-                                                    <a target="_blank" rel="noopener noreferrer" href={i.file_url}>{i.file_name}</a>
-                                                </Grid>
-                                            </Grid>
-                                            <Grid container spacing={0}>
-                                                <Grid item xs={4} className={classes.listItemLabel}>
-                                                    <FormattedMessage id="iaso.instance.otherFiles" defaultMessage="Other files" />
-                                                    :
-                                                </Grid>
-                                                <Grid item xs={8} className={classes.listItem}>
-                                                    <a target="_blank" rel="noopener noreferrer" href={i.file_url}>{i.file_name}</a>
-                                                </Grid>
-                                            </Grid>
+                                        <Popup className={classes.popup}>
+                                            <Card className={classes.card}>
+                                                {
+                                                    i.files && i.files.length > 0
+                                                    && (
+                                                        <CardMedia
+                                                            className={classes.cardMedia}
+                                                            image={i.files[0]}
+                                                            href={i.files[0]}
+                                                        />
+                                                    )
+                                                }
+                                                <CardContent className={classes.cardContent}>
+                                                    <Grid container spacing={0}>
+                                                        <Grid item xs={4} className={classes.listItemLabel}>
+                                                            <FormattedMessage id="iaso.label.name" defaultMessage="Name" />
+                                                            :
+                                                        </Grid>
+                                                        <Grid item xs={8} className={classes.listItem}>
+                                                            {i.org_unit.name}
+                                                        </Grid>
+                                                    </Grid>
+                                                    <Grid container spacing={0}>
+                                                        <Grid item xs={4} className={classes.listItemLabel}>
+                                                            <FormattedMessage id="iaso.label.type" defaultMessage="Type" />
+                                                            :
+                                                        </Grid>
+                                                        <Grid item xs={8} className={classes.listItem}>
+                                                            {i.org_unit.org_unit_type_name}
+                                                        </Grid>
+                                                    </Grid>
+                                                    <Grid container spacing={0}>
+                                                        <Grid item xs={4} className={classes.listItemLabel}>
+                                                            <FormattedMessage id="iaso.instance.device" defaultMessage="IMEI device" />
+                                                            :
+                                                        </Grid>
+                                                        <Grid item xs={8} className={classes.listItem}>
+                                                            {i.device_id ? i.device_id : '/'}
+                                                        </Grid>
+                                                    </Grid>
+                                                    <Grid container spacing={1}>
+                                                        <Grid item xs={4} className={classes.listItemLabel}>
+                                                            <FormattedMessage id="iaso.instance.coordinate" defaultMessage="Coordinates" />
+                                                            :
+                                                        </Grid>
+                                                        <Grid item xs={8} className={classes.listItem}>
+                                                            {' '}
+                                                            Long:
+                                                            {' '}
+                                                            {i.longitude}
+                                                            {' '}
+                                                            <br />
+                                                            {' '}
+                                                            Lat:
+                                                            {' '}
+                                                            {i.latitude}
+                                                        </Grid>
+                                                    </Grid>
+                                                    <Grid container spacing={0}>
+                                                        <Grid item xs={4} className={classes.listItemLabel}>
+                                                            <FormattedMessage id="iaso.instance.created_at" defaultMessage="Created at" />
+                                                            :
+                                                        </Grid>
+                                                        <Grid item xs={8} className={classes.listItem}>
+                                                            {moment.unix(i.created_at).format('DD/MM/YYYY HH:mm')}
+                                                        </Grid>
+                                                    </Grid>
+                                                    <Grid container spacing={0}>
+                                                        <Grid item xs={4} className={classes.listItemLabel}>
+                                                            <FormattedMessage id="iaso.instance.files" defaultMessage="Files" />
+                                                            :
+                                                        </Grid>
+                                                        <Grid item xs={8} className={classes.listItem}>
+                                                            <ul className={classes.fileList}>
+                                                                <li>
+                                                                    <a className={classes.fileItem} target="_blank" rel="noopener noreferrer" href={i.file_url}>{i.file_name}</a>
+                                                                </li>
+                                                                {i.files.map(f => (
+                                                                    <li key={f}>
+                                                                        <a className={classes.fileItem} target="_blank" rel="noopener noreferrer" href={f}>{f.toString()}</a>
+                                                                    </li>
+                                                                ))}
+                                                            </ul>
+                                                        </Grid>
+                                                    </Grid>
+                                                </CardContent>
+                                            </Card>
                                         </Popup>
                                     </Marker>
                                 );
@@ -142,6 +194,7 @@ class InstancesMap extends Component {
         );
     }
 }
+
 
 InstancesMap.propTypes = {
     classes: PropTypes.object.isRequired,

47feb68cdf895302975b4fa8d1d58c79ae04cc88
madewulf <madewulf@multitasked.net>
2019-11-24 09:26:26 +0100

    Improvements to the admin: avoid that details load full village list, or full cases list

--- hat/cases/admin.py
+++ hat/cases/admin.py
@@ -4,25 +4,30 @@ from .models import Case, Location, TestGroup
 
 
 class CaseAdmin(admin.ModelAdmin):
-    date_hierarchy = 'document_date'
-    list_filter = ('test_catt', 'source', 'confirmed_case')
-    search_fields = ('name', 'lastname', 'prename')
-    raw_id_fields = ('normalized_patient', 'normalized_AS', 'normalized_village')
+    date_hierarchy = "document_date"
+    list_filter = ("test_catt", "source", "confirmed_case")
+    search_fields = ("name", "lastname", "prename")
+    raw_id_fields = (
+        "normalized_patient",
+        "normalized_AS",
+        "normalized_village",
+        "infection_location",
+    )
 
 
 admin.site.register(Case, CaseAdmin)
 
 
 class LocationAdmin(admin.ModelAdmin):
-    search_fields = ('village',)
+    search_fields = ("village",)
 
 
 admin.site.register(Location, LocationAdmin)
 
 
 class TestGroupAdmin(admin.ModelAdmin):
-    date_hierarchy = 'created_at'
-    raw_id_fields = ('cases',)
+    date_hierarchy = "created_at"
+    raw_id_fields = ("cases",)
 
 
 admin.site.register(TestGroup, TestGroupAdmin)
--- hat/sync/admin.py
+++ hat/sync/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import MobileUser, DeviceDB, ImageUpload, VideoUpload
 from .models import DeviceStatus, DeviceAction, DeviceEvent, JSONDocument
 
+
+class JSONDocumentAdmin(admin.ModelAdmin):
+    search_fields = ("doc",)
+    raw_id_fields = ("case", "population")
+
+
 admin.site.register(MobileUser)
 admin.site.register(DeviceDB)
 admin.site.register(ImageUpload)
@@ -10,4 +16,4 @@ admin.site.register(VideoUpload)
 admin.site.register(DeviceEvent)
 admin.site.register(DeviceStatus)
 admin.site.register(DeviceAction)
-admin.site.register(JSONDocument)
+admin.site.register(JSONDocument, JSONDocumentAdmin)

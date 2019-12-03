5214249aac22c920dc3e0e613cc00bacff8660fc
Stéphan Mestach <smestach@bluesquare.org>
2019-12-03 11:18:37 +0100

    Add support parent update

--- iaso/diffing/exporter.py
+++ iaso/diffing/exporter.py
@@ -14,21 +14,56 @@ def all_slices(iterables, size: int):
     return slices
 
 
+def sort_by_path(diffs):
+    def by_path(ou):
+        path = ou.path()
+        return path if path else ""
+
+    # make sure we create new parent first
+    return sorted(diffs, key=lambda d: by_path(d.org_unit))
+
+
+def assign_dhis2_ids(to_create_diffs):
+    # ideally should run on diff
+    # assign source_ref for orgunits to avoid double creation
+    for to_create in to_create_diffs:
+        if to_create.org_unit.source_ref is None:
+            to_create.org_unit.source_ref = generate_id_for_dhis_2()
+            to_create.org_unit.save()
+
+    for to_create in to_create_diffs:
+        to_create.org_unit.refresh_from_db()
+
+
+def to_dhis2_feature_type(type):
+    # TODO better way : to snake case upper case ?
+    if type == "Point":
+        return "POINT"
+
+    if type == "Polygon":
+        return "POLYGON"
+
+    if type == "MultiPolygon":
+        return "MULTI_POLYGON"
+
+
 class Exporter:
     def __init__(self, logger):
         self.iaso_logger = logger
 
     def export_to_dhis2(self, api, diffs):
-        self.iaso_logger.ok("   ------ New ----")
+        self.iaso_logger.ok("   ------ New org units----")
         self.create_missings(api, diffs)
-        self.iaso_logger.ok("   ------ Modified ----")
+        self.iaso_logger.ok("   ------ Modified org units----")
+        self.update_orgunits(api, diffs)
+        self.iaso_logger.ok("   ------ Modified groups----")
         self.update_orgunits(api, diffs)
 
     def create_missings(self, api, diffs):
         to_create_diffs = list(filter(lambda x: x.status == "new", diffs))
 
-        self.assign_dhis2_ids(to_create_diffs)
-        to_create_diffs = self.sort_by_path(to_create_diffs)
+        assign_dhis2_ids(to_create_diffs)
+        to_create_diffs = sort_by_path(to_create_diffs)
 
         # build the "minimal" payloads for creation, groups only done at later stage
         for to_create in to_create_diffs:
@@ -41,58 +76,23 @@ class Exporter:
                 "shortName": name_comparison.after,
                 "openingDate": "1960-08-03T00:00:00.000",
             }
-
-            self.fill_parent_id(to_create, payload)
-            self.fill_geometry_or_coordinates(to_create, payload)
+            if to_create.org_unit.parent:
+                payload["parent"] = {"id": to_create.org_unit.parent.source_ref}
+            self.fill_geometry_or_coordinates(to_create.comparison("geometry"), payload)
 
             self.iaso_logger.info("will post ", payload)
             resp = api.post("organisationUnits", payload)
             self.iaso_logger.info("received ", resp.json())
 
-    def fill_parent_id(self, to_create, payload):
-        if to_create.org_unit.parent:
-            payload["parent"] = {"id": to_create.org_unit.parent.source_ref}
-
-    def fill_geometry_or_coordinates(self, to_create, payload):
-        geometry_comparison = to_create.comparison("geometry")
-        if geometry_comparison.after:
-            point_or_shape = GEOSGeometry(geometry_comparison.after)
+    def fill_geometry_or_coordinates(self, comparison, payload):
+        if comparison.after:
+            point_or_shape = GEOSGeometry(comparison.after)
             geometry = json.loads(point_or_shape.geojson)
             # if dhis2 >= 2.32
             payload["geometry"] = geometry
             # if dhis2 < 2.32
             payload["coordinates"] = geometry["coordinates"]
-            payload["featureType"] = self.to_dhis2_feature_type(geometry["type"])
-
-    def sort_by_path(self, diffs):
-        def by_path(ou):
-            path = ou.path()
-            return path if path else ""
-
-        # make sure we create new parent first
-        return sorted(diffs, key=lambda d: by_path(d.org_unit))
-
-    def assign_dhis2_ids(self, to_create_diffs):
-        # ideally should run on diff
-        # assign source_ref for orgunits to avoid double creation
-        for to_create in to_create_diffs:
-            if to_create.org_unit.source_ref is None:
-                to_create.org_unit.source_ref = generate_id_for_dhis_2()
-                to_create.org_unit.save()
-
-        for to_create in to_create_diffs:
-            to_create.org_unit.refresh_from_db()
-
-    def to_dhis2_feature_type(self, type):
-        # TODO better way : to snake case upper case ?
-        if type == "Point":
-            return "POINT"
-
-        if type == "Polygon":
-            return "POLYGON"
-
-        if type == "MultiPolygon":
-            return "MULTI_POLYGON"
+            payload["featureType"] = to_dhis2_feature_type(geometry["type"])
 
     def update_orgunits(self, api, diffs):
         support_by_update_fields = ("name", "parent", "geometry")
@@ -103,11 +103,11 @@ class Exporter:
                 diffs,
             )
         )
-        print("to_update_diffs", len(to_update_diffs))
-        to_update_diffs = self.sort_by_path(to_update_diffs)
+        self.iaso_logger.info("to_update_diffs", len(to_update_diffs))
+        to_update_diffs = sort_by_path(to_update_diffs)
         self.iaso_logger.info("modified", len(to_update_diffs))
 
-        slices = all_slices(to_update_diffs, 5)
+        slices = all_slices(to_update_diffs, 4)
         index = 0
         for current_slice in slices:
             index = index + 1
@@ -126,10 +126,10 @@ class Exporter:
 
                 if len(diff) == 0:
                     raise Exception(
-                        "orgunit can't be loaded : "
+                        "orgunit can't be loaded from dhis2 : "
                         + dhis2_payload["id"]
-                        + "  "
-                        + str(dhis2_payloads)
+                        + " vs in page loaded "
+                        + ", ".join(list(map(lambda x: x["id"], dhis2_payloads)))
                     )
                 diff = diff[0]
                 for comparison in diff.comparisons:
@@ -142,15 +142,20 @@ class Exporter:
             self.iaso_logger.info(resp, resp.json())
 
     def apply_comparison(self, payload, comparison):
+        # TODO ideally move to FieldTypes in comparisons.py
         if comparison.field == "name":
             payload[comparison.field] = comparison.after
             return
-        if comparison.field == "geometry":
-            point_or_shape = GEOSGeometry(comparison.after)
-            geometry = json.loads(point_or_shape.geojson)
-            payload["geometry"] = geometry
 
-            payload["coordinates"] = geometry["coordinates"]
-            payload["featureType"] = self.to_dhis2_feature_type(geometry["type"])
+        if comparison.field == "geometry":
+            self.fill_geometry_or_coordinates(comparison, payload)
+            return
+
+        if comparison.field == "parent" and comparison.after:
+            self.fill_parent_id(comparison, payload)
             return
         raise Exception("unsupported field", comparison.field)
+
+    def fill_parent_id(self, comparison, payload):
+        if comparison.after:
+            payload["parent"] = {"id": comparison.after}

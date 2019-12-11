e993ef08da59088150ff495006eb7d50cf6fb677
Stéphan Mestach <smestach@bluesquare.org>
2019-12-11 09:07:54 +0100

    Add actually export events

--- iaso/management/commands/export_events.py
+++ iaso/management/commands/export_events.py
@@ -3,7 +3,7 @@ import json
 from iaso.models import OrgUnit, OrgUnitType, Instance
 from django.db import connection
 from django.core.paginator import Paginator
-from dhis2 import Api
+from dhis2 import Api, RequestException
 
 """
 https://docs.dhis2.org/2.30/en/developer/html/dhis2_developer_manual_full.html#webapi_csv_data_elements
@@ -35,6 +35,25 @@ n        TRUE_ONLY              Yes only	        True values, renders as check-b
 """
 
 
+def translate_optionset(data_element, raw_value):
+    if "optionSet" in data_element:
+        if raw_value == "":
+            return None
+
+        for options in data_element["optionSet"]["options"]:
+            if options.get("odk") == raw_value:
+                return options["code"]
+
+        for options in data_element["optionSet"]["options"]:
+            if options["code"] == raw_value:
+                return raw_value
+
+        raise Exception(
+            "no value matching : '" + raw_value + "' in " + str(data_element)
+        )
+    return raw_value
+
+
 def format_value(data_element, raw_value):
     data_element_type = None
     if "valueType" in data_element:
@@ -42,11 +61,13 @@ def format_value(data_element, raw_value):
     else:
         raise Exception("no valueType for ", data_element)
 
+    translated_value = translate_optionset(data_element, raw_value)
+
     if data_element_type == "NUMBER":
-        if raw_value == "":
+        if translated_value == "":
             return None
         try:
-            return float(raw_value)
+            return float(translated_value)
         except:
             raise Exception("Bad value for float", raw_value, data_element)
 
@@ -56,37 +77,49 @@ def format_value(data_element, raw_value):
         or data_element_type == "INTEGER_POSITIVE"
         or data_element_type == "INTEGER_NEGATIVE"
     ):
-        if raw_value == "":
+        if translated_value == "":
             return None
         try:
-            return int(raw_value)
+            return int(translated_value)
         except:
             raise Exception("Bad value for int", raw_value, data_element)
 
     if data_element_type == "TEXT" or data_element_type == "LONG_TEXT":
-        if "option_set" in data_element:
-            for options in data_element["option_set"]["options"]:
-                if options["odk"] == raw_value:
-                    return options["code"]
-
-            for options in data_element["option_set"]["options"]:
-                if options["code"] == raw_value:
-                    return raw_value
-        return str(raw_value)
+        return str(translated_value)
 
     if data_element_type == "BOOLEAN":
-        if raw_value == "1":
+        if translated_value == "1":
             return True
-        elif raw_value == "0":
+        elif translated_value == "0":
             return False
-        elif raw_value == "":
+        elif translated_value == "":
             return None
         else:
             raise Exception(
                 raw_value + " bad value for '" + data_element_type + "'", raw_value
             )
 
-    raise Exception("unsupported data element type : " + data_element, raw_value)
+    raise Exception("unsupported data element type : " + str(data_element), raw_value)
+
+
+def uniquify(seq, idfun=None):  # Alex Martelli ******* order preserving
+    if idfun is None:
+
+        def idfun(x):
+            return x
+
+    seen = {}
+    result = []
+    for item in seq:
+        marker = idfun(item)
+        # in old Python versions:
+        # if seen.has_key(marker)
+        # but in new ones:
+        if marker not in seen:
+            seen[marker] = 1
+            result.append(item)
+
+    return result
 
 
 class Command(BaseCommand):
@@ -141,10 +174,14 @@ class Command(BaseCommand):
         print(json.dumps(mapping, indent=4))
 
     def handle(self, *args, **options):
+        # mapping_file = "./testdata/form-ihp.json"
+        export = False
+        mapping_file = "./testdata/form-play.json"
+        form_id = "RDC_Data_CS"
 
         instances_qs = (
             Instance.objects.filter(
-                form__form_id="RDC_Data_CS"
+                form__form_id=form_id
                 # , org_unit__validated= True ?
             )
             .prefetch_related("org_unit")
@@ -153,10 +190,11 @@ class Command(BaseCommand):
             .all()
         )
         paginator = Paginator(instances_qs, 100)
-        mapping = self.load_mapping()
+        mapping = self.load_mapping(mapping_file)
 
         print("How to create a mapping file from a program id")
-        self.seed_mapping(self.get_api(options), mapping["program_id"])
+        api = self.get_api(options)
+        self.seed_mapping(api, "q04UBOqq3rp")  # mapping["program_id"])
 
         events = []
         errors = []
@@ -167,7 +205,7 @@ class Command(BaseCommand):
                     event = {
                         "program": mapping["program_id"],
                         "event": instance.export_id,
-                        "orgUnit": instance.org_unit.source_ref,
+                        "orgUnit": instance.org_unit.source_ref,  # play "Rp268JB6Ne4",
                         "eventDate": instance.created_at.strftime("%Y-%m-%d"),
                         "status": "COMPLETED",
                         "dataValues": [],
@@ -183,28 +221,66 @@ class Command(BaseCommand):
                     for question_key in instance.json.keys():
                         if question_key in question_mappings:
                             try:
-                                question_mappings["question_key"] = question_key
                                 data_element = question_mappings[question_key]
+                                data_element["question_key"] = question_key
                                 raw_value = instance.json[question_key]
                                 data_value = {
                                     "dataElement": data_element["id"],
                                     "value": format_value(data_element, raw_value),
+                                    # "debug": str(raw_value)+" "+str(data_element)
+                                    "debug": str(raw_value) + " " + question_key,
                                 }
                                 event["dataValues"].append(data_value)
                             except Exception as error:
                                 errored = True
                                 event_errors.append([question_key, error])
                                 print("ERROR", error, question_key)
+
                     if not errored:
                         events.append(event)
                     else:
                         errors.append(event_errors)
-
+            if export:
+                try:
+                    resp = api.post("events", {"events": events}).json()
+                    print(resp)
+                except RequestException as dhis2_exception:
+                    resp = json.loads(dhis2_exception.description)
+                    if resp["status"] == "ERROR":
+                        message = "error while processing page %d/%d" % (
+                            page,
+                            paginator.num_pages,
+                        )
+                        for count_type in ("imported", "updated", "deleted", "ignored"):
+                            print(message, count_type, resp["response"][count_type])
+                        import_summaries = resp["response"]["importSummaries"]
+                        descriptions = [
+                            m["description"]
+                            for m in import_summaries
+                            if "description" in m
+                        ]
+                        conflicts = [
+                            m["conflicts"] for m in import_summaries if "conflicts" in m
+                        ]
+                        descriptions = uniquify(descriptions)
+                        print(
+                            "---------------------------------------------------------"
+                        )
+                        print(
+                            "----------------------- EXPORT ERROR --------------------"
+                        )
+                        print(
+                            "Failed to create events got", descriptions, conflicts, resp
+                        )
             print(
                 "done processing page %d/%d" % (page, paginator.num_pages), len(events)
             )
+        print(errors)
         print(json.dumps(events, indent=4))
+        print("instances", paginator.count)
+        print("events", len(events))
+        print("errors", len(errors))
 
-    def load_mapping(self):
-        with open("./testdata/form-ihp.json") as json_file:
+    def load_mapping(self, mapping_file):
+        with open(mapping_file) as json_file:
             return json.load(json_file)

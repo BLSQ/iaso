1a89d1bcf6e3395c56df131d411ba346dc4d6533
Stéphan Mestach <smestach@bluesquare.org>
2019-12-16 15:19:25 +0100

    Add command to seed a mapping file

--- iaso/dhis2/form_mapping.py
+++ iaso/dhis2/form_mapping.py
@@ -1,22 +1,35 @@
 import json
 
 
-def seed_mapping(self, api, program_id):
+def seed_mapping(api, program_id):
     program = api.get(
         "programs/" + program_id,
         params={
-            "fields": ":all,programStages[id,name,programStageDataElements[dataElement[id,code,name,valueType,optionSet[id,name,options[id,name,code]]]]]"
+            "fields": ":all,programStages[id,name,programStageDataElements[dataElement[id,code,name,shortName,valueType,optionSet[id,name,options[id,name,code]]]]]"
         },
     )
     question_mappings = {}
+    missing_data_elements = []
     for program_stage in program.json()["programStages"]:
         for psde in program_stage["programStageDataElements"]:
-            question_mappings[psde["dataElement"]["code"]] = psde["dataElement"]
+            de = psde["dataElement"]
+            code_or_name = None
+            if "code" in de:
+                code_or_name = de.get("code")
+            elif "shortName":
+                code_or_name = de.get("shortName")
+                missing_data_elements.append(de)
+            else:
+                code_or_name = de.get("name")
+                missing_data_elements.append(de)
+
+            psde["dataElement"]["code"] = code_or_name
+            question_mappings[code_or_name] = psde["dataElement"]
 
     mapping = {
         "type": "simple_event",
         "program_id": program_id,
         "question_mappings": question_mappings,
     }
-    print(json.dumps(mapping, indent=4))
-    return mapping
+
+    return (mapping, missing_data_elements)

1278eae0803f3a1e41f634f257c9435d3961d14f
Eric Darchis <edarchis@bluesquarehub.com>
2020-05-12 20:10:31 +0200

    Black

--- hat/api/planning.py
+++ hat/api/planning.py
@@ -40,31 +40,53 @@ class PlanningViewSet(viewsets.ViewSet):
     conflict with the body of the request. For example, if you associate village 3 with team 3 and it was previously
     associated with team 2, this assignation to team 2 will be deleted.
     """
+
     authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
     permission_required = [
-        'menupermissions.x_management_plannings',
-        'menupermissions.x_management_workzones',
-        'menupermissions.x_plannings_macroplanning',
-        'menupermissions.x_plannings_microplanning',
-        'menupermissions.x_plannings_routes'
+        "menupermissions.x_management_plannings",
+        "menupermissions.x_management_workzones",
+        "menupermissions.x_plannings_macroplanning",
+        "menupermissions.x_plannings_microplanning",
+        "menupermissions.x_plannings_routes",
     ]
 
     def list(self, request):
-        orders = request.GET.get('order', '-created_at')
-        with_template = request.GET.get('with_template', False)
-        orders = ('-is_template,' + orders).split(',')
+        orders = request.GET.get("order", "-created_at")
+        with_template = request.GET.get("with_template", False)
+        orders = ("-is_template," + orders).split(",")
 
         queryset = Planning.objects.all()
         if not with_template:
             queryset = queryset.filter(is_template=False)
-            return Response(queryset.values('name', 'id', 'year', 'updated_at', 'is_template', 'years_coverage', 'months', 'month_start').order_by(*orders))
+            return Response(
+                queryset.values(
+                    "name",
+                    "id",
+                    "year",
+                    "updated_at",
+                    "is_template",
+                    "years_coverage",
+                    "months",
+                    "month_start",
+                ).order_by(*orders)
+            )
         else:
-            res = {"datas": queryset.values(
-                'name', 'id', 'year', 'updated_at', 'is_template', 'years_coverage', 'months', 'month_start').order_by(*orders)}
-            if request.user.has_perm('menupermissions.x_management_plannings_template'):
-                res['can_make_template'] = True
+            res = {
+                "datas": queryset.values(
+                    "name",
+                    "id",
+                    "year",
+                    "updated_at",
+                    "is_template",
+                    "years_coverage",
+                    "months",
+                    "month_start",
+                ).order_by(*orders)
+            }
+            if request.user.has_perm("menupermissions.x_management_plannings_template"):
+                res["can_make_template"] = True
             else:
-                res['can_make_template'] = False
+                res["can_make_template"] = False
             return Response(res)
 
     def retrieve(self, request, pk=None):
@@ -73,12 +95,12 @@ class PlanningViewSet(viewsets.ViewSet):
         return Response(planning.as_dict())
 
     def create(self, request):
-        planning_to_copy_id = request.data.get('planning_to_copy', None)
-        year = request.data.get('year', None)
-        name = request.data.get('name', None)
-        years_coverage = request.data.get('years_coverage', None)
-        months = request.data.get('months', None)  # defaults in Model
-        month_start = request.data.get('month_start', None)
+        planning_to_copy_id = request.data.get("planning_to_copy", None)
+        year = request.data.get("year", None)
+        name = request.data.get("name", None)
+        years_coverage = request.data.get("years_coverage", None)
+        months = request.data.get("months", None)  # defaults in Model
+        month_start = request.data.get("month_start", None)
 
         if planning_to_copy_id:
             planning = get_object_or_404(Planning, pk=planning_to_copy_id)
@@ -105,22 +127,22 @@ class PlanningViewSet(viewsets.ViewSet):
         else:
             planning = get_object_or_404(Planning, pk=pk)
         Assignation.objects.filter(planning=planning).delete()
-        village_id = request.data.get('village_id', -1)
-        team_id = request.data.get('team_id', -1)
+        village_id = request.data.get("village_id", -1)
+        team_id = request.data.get("team_id", -1)
         if village_id != -1 and team_id != -1:
             for obj in request.data:
                 assignation = Assignation()
                 assignation.planning = planning
-                assignation.village_id = obj['village_id']
-                assignation.team_id = obj['team_id']
+                assignation.village_id = obj["village_id"]
+                assignation.team_id = obj["team_id"]
                 assignation.save()
-        newYear = request.data.get('year', '')
-        if newYear != '':
+        newYear = request.data.get("year", "")
+        if newYear != "":
             planning.year = newYear  # Bonne année !
-        newName = request.data.get('name', '')
-        if newName != '':
+        newName = request.data.get("name", "")
+        if newName != "":
             planning.name = newName
-        is_template = request.data.get('is_template', False)
+        is_template = request.data.get("is_template", False)
         newMonths = request.data.get("months", None)
         if newMonths:
             planning.months = newMonths
@@ -130,9 +152,11 @@ class PlanningViewSet(viewsets.ViewSet):
 
         planning.is_template = is_template
 
-        planning.years_coverage = request.data.get('years_coverage', None)
-        if is_template and not request.user.has_perm('menupermissions.x_management_plannings_template'):
-            return Response('Unauthorized', status=401)
+        planning.years_coverage = request.data.get("years_coverage", None)
+        if is_template and not request.user.has_perm(
+            "menupermissions.x_management_plannings_template"
+        ):
+            return Response("Unauthorized", status=401)
         else:
             planning.save()
             return Response(planning.as_dict())
@@ -140,22 +164,29 @@ class PlanningViewSet(viewsets.ViewSet):
     def partial_update(self, request, pk=None):
         planning = get_object_or_404(Planning, pk=pk)
 
-        if planning.is_template and not request.user.has_perm('menupermissions.x_management_plannings_template'):
-            return Response('Unauthorized', status=401)
+        if planning.is_template and not request.user.has_perm(
+            "menupermissions.x_management_plannings_template"
+        ):
+            return Response("Unauthorized", status=401)
         else:
             for obj in request.data:
                 Assignation.objects.filter(
-                    planning=planning, village_id=obj['village_id']).delete()
-                if obj.get('team_id', 'none') != 'none':
-                    Assignation.objects.get_or_create(planning=planning,
-                                                      village_id=obj['village_id'],
-                                                      team_id=obj['team_id'])
+                    planning=planning, village_id=obj["village_id"]
+                ).delete()
+                if obj.get("team_id", "none") != "none":
+                    Assignation.objects.get_or_create(
+                        planning=planning,
+                        village_id=obj["village_id"],
+                        team_id=obj["team_id"],
+                    )
             return Response(planning.as_dict())
 
     def delete(self, request, pk=None):
         planning = get_object_or_404(Planning, pk=pk)
-        if planning.is_template and not request.user.has_perm('menupermissions.x_management_plannings_template'):
-            return Response('Unauthorized', status=401)
+        if planning.is_template and not request.user.has_perm(
+            "menupermissions.x_management_plannings_template"
+        ):
+            return Response("Unauthorized", status=401)
         else:
             planning.delete()
             return Response(True)

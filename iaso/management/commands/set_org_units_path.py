e6d893ed5cbb225a75cbc34ea7d1ed73ca11a134
pvanliefland <pierre.vanliefland@gmail.com>
2020-05-26 14:09:40 +0200

    IA-254 Create path hierarchical property on Org Units
    New org units should have a path, unless they have a pathless parent

--- iaso/management/commands/set_org_units_path.py
+++ iaso/management/commands/set_org_units_path.py
@@ -12,10 +12,10 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):  # TODO: remove me
         parser.add_argument(
-            '--test-seed',
-            action='store_true',
+            "--test-seed",
+            action="store_true",
             default=False,
-            help='Create a bunch of org units for load testing',
+            help="Create a bunch of org units for load testing",
         )
 
     def handle(self, *args, **options):
@@ -28,21 +28,42 @@ class Command(BaseCommand):
 
         self.stdout.write("Setting path on org units...")
 
-        no_path_count = OrgUnit.objects.filter(path=None).count()
-        self.stdout.write(f"Found {no_path_count} without path")
+        no_path_count_before = OrgUnit.objects.filter(path=None).count()
+        self.stdout.write(f"Found {no_path_count_before} without path")
 
-        root_org_units = OrgUnit.objects.filter(parent=None)
-        self.stdout.write(f"Found {len(root_org_units)} root org units")
+        root_org_units = OrgUnit.objects.filter(parent=None, path=None)
+        self.stdout.write(f"Found {len(root_org_units)} root org units without path")
 
         for org_unit in root_org_units:
             with transaction.atomic():
                 org_unit.save(force_calculate_path=True, update_fields=["path"])
 
-        no_path_count = OrgUnit.objects.filter(path=None).count()
-        if no_path_count == 0:
+        # Cheating - simulating an org unit created in the meantime, TODO: remove me
+        OrgUnit.objects.create(
+            parent=OrgUnit.objects.first(), name="set_org_units_path test"
+        )
+
+        no_path_count_after = OrgUnit.objects.filter(path=None).count()
+        if no_path_count_after == 0:
             self.stdout.write(f"After this operation, all org units have a path")
         else:
-            self.stdout.write(f"After this operation, {no_path_count} org units are left without path")
+            self.stdout.write(
+                f"After this operation, {no_path_count_after} org units are left without path"
+            )
+            self.stdout.write("Attempting fix")
+
+            for org_unit in OrgUnit.objects.filter(path=None):
+                with transaction.atomic():
+                    org_unit.save(force_calculate_path=True, update_fields=["path"])
+
+            no_path_count_final = OrgUnit.objects.filter(path=None).count()
+            if no_path_count_final == 0:
+                self.stdout.write(f"After this fix, all org units have a path")
+            else:
+                self.stdout.write(
+                    f"After fix attempt, still {no_path_count_final} org units are left without path"
+                )
+                self.stdout.write("Should be fixed manually")
 
         stop = perf_counter()
 
@@ -55,17 +76,19 @@ class Command(BaseCommand):
         with transaction.atomic():
             created = self._create_org_unit_batch(0)
 
+        OrgUnit.objects.filter(name="set_org_units_path test").update(path=None)
+
         self.stdout.write(f"Created {created} test org units!")
 
     def _create_org_unit_batch(self, level, parent=None):  # TODO: remove me
         created = 0
 
         for _ in range(10):
-            new_parent = OrgUnit.objects.create(parent=parent, name="set_org_units_path test")
+            new_parent = OrgUnit.objects.create(
+                parent=parent, name="set_org_units_path test"
+            )
             created += 1
             if level < self.max_seed_level:
                 created += self._create_org_unit_batch(level + 1, new_parent)
 
         return created
-
-

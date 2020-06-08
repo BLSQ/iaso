83c06dc020522537def32adecad0339ed50d59e6
pvanliefland <pierre.vanliefland@gmail.com>
2020-06-08 18:30:33 +0200

    IA-113 ORG UNITS types create/delete
    Black

--- iaso/api/org_unit_types/viewsets.py
+++ iaso/api/org_unit_types/viewsets.py
@@ -17,7 +17,16 @@ class OrgUnitTypeViewSet(ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = OrgUnitTypeSerializer
     results_key = "orgUnitTypes"
-    http_method_names = ["get", "post", "patch", "put", "delete", "head", "options", "trace"]
+    http_method_names = [
+        "get",
+        "post",
+        "patch",
+        "put",
+        "delete",
+        "head",
+        "options",
+        "trace",
+    ]
 
     def get_queryset(self):
         queryset = OrgUnitType.objects.all()
@@ -25,8 +34,8 @@ class OrgUnitTypeViewSet(ModelViewSet):
         search = self.request.query_params.get("search", None)
         if search:
             queryset = queryset.filter(
-                Q(name__icontains=search)
-                | Q(short_name__icontains=search))
+                Q(name__icontains=search) | Q(short_name__icontains=search)
+            )
 
         if not self.request.user.is_anonymous:
             profile = self.request.user.iaso_profile

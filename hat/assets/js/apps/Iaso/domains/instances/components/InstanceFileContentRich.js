c6bc65cac1d6081740b70db52574020e315a778f
pvanliefland <pierre.vanliefland@gmail.com>
2020-06-23 11:00:17 +0200

    IA-268 Display questions labels in form detail
    Better fixtures, improved detail screen

--- hat/assets/js/apps/Iaso/domains/instances/components/InstanceFileContentRich.js
+++ hat/assets/js/apps/Iaso/domains/instances/components/InstanceFileContentRich.js
@@ -7,12 +7,16 @@ import {
     TableRow,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import FunctionsIcon from '@material-ui/icons/Functions';
+import CommentIcon from '@material-ui/icons/Comment';
+import isPlainObject from 'lodash/isPlainObject';
+
 import { textPlaceholder } from '../../../constants/uiConstants';
 
 const useStyle = makeStyles(theme => ({
     tableCellHead: {
         fontWeight: 'bold',
-        backgroundColor: 'transparent',
+        backgroundColor: theme.palette.gray,
         borderTop: 'none !important',
         borderLeft: 'none !important',
         borderRight: 'none !important',
@@ -26,12 +30,55 @@ const useStyle = makeStyles(theme => ({
         borderBottom: `1px solid ${theme.palette.ligthGray.border}  !important`,
         minWidth: '200px',
     },
+    tableCellLabelWrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
     tableCellLabel: {
-        width: '100%',
+        display: 'inline-block',
         whiteSpace: 'normal',
         wordBreak: 'break-word',
+        marginLeft: 5,
     },
 }));
+
+/**
+ * Translate the provided label if it is translatable
+ *
+ * @param label
+ * @returns {*}
+ */
+function translateLabel(label) {
+    // TODO: pick current locale - not trivial since keys could be "FR", "French", "En français"...
+    if (isPlainObject(label)) {
+        return label[Object.keys(label)[0]];
+    }
+
+    return label;
+}
+
+/**
+ * Extract the value from data using the descriptor
+ * (handles the different scenarios, such as select fields)
+ *
+ * @param descriptor
+ * @param data
+ * @returns {string|*}
+ */
+function getDisplayedValue(descriptor, data) {
+    const value = data[descriptor.name];
+    if (value === undefined) {
+        return textPlaceholder;
+    }
+
+    if (descriptor.type === 'select one') {
+        const choice = descriptor.children.find(c => c.name === value);
+        return choice !== undefined ? translateLabel(choice.label) : textPlaceholder;
+    }
+
+    return value !== '' ? value : textPlaceholder;
+}
 
 export default function InstanceFileContentRich({
     instanceData,
@@ -40,7 +87,7 @@ export default function InstanceFileContentRich({
     return (
         <Table>
             <TableBody>
-                {formDescriptor.children.map(childDescriptor => (
+                {formDescriptor.children.filter(c => c.name !== 'meta').map(childDescriptor => (
                     <FormChild key={childDescriptor.name} descriptor={childDescriptor} data={instanceData} />
                 ))}
             </TableBody>
@@ -57,7 +104,20 @@ function FormChild({ descriptor, data }) {
     switch (descriptor.type) {
         case 'group':
             return <FormGroup descriptor={descriptor} data={data} />;
+        case 'start':
+        case 'end':
+        case 'today':
+        case 'deviceid':
+        case 'subscriberid':
+        case 'simserial':
+        case 'phonenumber':
+            return <FormMetaField descriptor={descriptor} data={data} />;
+        case 'note':
+            return <FormNoteField descriptor={descriptor} />;
+        case 'calculate':
+            return <FormField descriptor={descriptor} data={data} icon={<FunctionsIcon color="disabled" />} />;
         default:
+            console.log(descriptor.type, descriptor, data);
             return <FormField descriptor={descriptor} data={data} />;
     }
 }
@@ -72,7 +132,7 @@ function FormGroup({ descriptor, data }) {
     return (
         <>
             <TableRow>
-                <TableCell colSpan={3} align="center" className={classes.tableCellHead}>
+                <TableCell colSpan={2} align="center" className={classes.tableCellHead}>
                     <Label descriptor={descriptor} />
                 </TableCell>
             </TableRow>
@@ -93,36 +153,86 @@ FormGroup.propTypes = {
     data: PropTypes.object.isRequired,
 };
 
-function FormField({ descriptor, data }) {
+function FormField({ descriptor, data, icon }) {
     const classes = useStyle();
-    const value = descriptor.name in data ? data[descriptor.name] : textPlaceholder;
 
     return (
         <TableRow>
             <TableCell className={classes.tableCell}>
-                <Label descriptor={descriptor} />
+                <div className={classes.tableCellLabelWrapper}>
+                    {icon !== null && icon}
+                    <Label descriptor={descriptor} />
+                </div>
             </TableCell>
-            <TableCell className={classes.tableCell} align="right">{value}</TableCell>
+            <TableCell className={classes.tableCell} align="right">
+                {getDisplayedValue(descriptor, data)}
+            </TableCell>
         </TableRow>
     );
 }
+FormField.defaultProps = {
+    icon: null,
+};
 FormField.propTypes = {
+    descriptor: PropTypes.object.isRequired,
+    data: PropTypes.object.isRequired,
+    icon: PropTypes.element,
+};
+
+function FormMetaField({ descriptor, data }) {
+    const classes = useStyle();
+
+    return (
+        <TableRow>
+            <TableCell className={classes.tableCell} colSpan={2}>
+                <Label descriptor={descriptor} value={getDisplayedValue(descriptor, data)} />
+            </TableCell>
+        </TableRow>
+    );
+}
+FormMetaField.propTypes = {
     descriptor: PropTypes.object.isRequired,
     data: PropTypes.object.isRequired,
 };
 
-function Label({ descriptor }) {
+function FormNoteField({ descriptor }) {
     const classes = useStyle();
 
-    let labelText;
+    return (
+        <TableRow>
+            <TableCell className={classes.tableCell} colSpan={2}>
+                <div className={classes.tableCellLabelWrapper}>
+                    <CommentIcon color="disabled" />
+                    <Label descriptor={descriptor} />
+                </div>
+            </TableCell>
+        </TableRow>
+    );
+}
+FormNoteField.propTypes = {
+    descriptor: PropTypes.object.isRequired,
+};
+
+function Label({ descriptor, value }) {
+    const classes = useStyle();
+
+    let label;
     if ('label' in descriptor) {
-        labelText = descriptor.label;
+        label = translateLabel(descriptor.label);
+
+        if (value !== null) { // useful for meta questions, whose labels are like "subscriberid ${subscriberid}"
+            label = label.replace(`\${${descriptor.name}}`, value);
+        }
     } else {
-        labelText = descriptor.name;
+        label = descriptor.name;
     }
 
-    return <div className={classes.tableCellLabel}>{labelText}</div>;
+    return <div className={classes.tableCellLabel}>{label}</div>;
 }
+Label.defaultProps = {
+    value: null,
+};
 Label.propTypes = {
     descriptor: PropTypes.object.isRequired,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
--- iaso/management/commands/seed_simple_form.py
+++ iaso/management/commands/seed_simple_form.py
@@ -10,71 +10,50 @@ from iaso.odk import parsing
 class Command(BaseCommand):
     help = "Create a simple form with a few versions and some instances."
 
-    def add_arguments(self, parser):  # TODO: remove me
+    def add_arguments(self, parser):
         parser.add_argument(
             "account_id",
             help="The project to which the form should be linked",
         )
 
     def handle(self, *args, **options):
-
-        Instance.objects.filter(name="FOSA_PLAN_CAR (ssf)").delete()
-        Form.objects.filter(name="FOSA_PLAN_CAR (ssf)").delete()
-
-        Instance.objects.filter(name="CSI - Quantite - Valide (ssf)").delete()
-        Form.objects.filter(name="CSI - CS - Quantite - Valide (ssf)").delete()
-        Device.objects.filter(imei="ssf1230403").delete()
-        OrgUnit.objects.filter(name="Centre de santé quelque part (ssf)").delete()
-        OrgUnitType.objects.filter(name="Centre de santé (ssf)").delete()
+        Instance.objects.filter(name="Test form 1 (ssf)").delete()
+        Form.objects.filter(name="Test form 1 (ssf)").delete()
+        Device.objects.filter(imei="ssf123456").delete()
+        OrgUnit.objects.filter(name="Test org unit (ssf)").delete()
+        OrgUnitType.objects.filter(name="Test org unit type (ssf)").delete()
         Project.objects.filter(name="Test project (ssf)").delete()
 
         project = Project.objects.create(name="Test project (ssf)",
                                          account=Account.objects.get(pk=options["account_id"]))
 
-        device = Device.objects.create(imei="ssf1230403")
+        device = Device.objects.create(imei="ssf123456")
         device.projects.add(project)
 
-        health_center = OrgUnitType.objects.create(name="Centre de santé (ssf)", short_name="Centre")
-        health_center.projects.add(project)
+        org_unit_type = OrgUnitType.objects.create(name="Test org unit type (ssf)", short_name="Ou")
+        org_unit_type.projects.add(project)
 
-        health_center_somewhere = OrgUnit.objects.create(name="Centre de santé quelque part (ssf)",
-                                                         org_unit_type=health_center)
+        org_unit = OrgUnit.objects.create(name="Test org unit (ssf)", org_unit_type=org_unit_type)
 
-        form = Form.objects.create(name="CSI - CS - Quantite - Valide (ssf)", form_id="ne_csi_quantite", device_field="deviceid",
-                                   period_type=periods.PERIOD_TYPE_MONTH, periods_before_allowed=15, periods_after_allowed=3, single_per_period=True)
-        form.org_unit_types.add(health_center)
+        form = Form.objects.create(name="Test form 1 (ssf)", form_id="QOC-HD-00000_v6", device_field="deviceid",
+                                   period_type=periods.PERIOD_TYPE_QUARTER, periods_before_allowed=6,
+                                   periods_after_allowed=6, single_per_period=True)
+        form.org_unit_types.add(org_unit_type)
         form.projects.add(project)
 
-        with open("iaso/fixtures/csi_quantite_valide_2020051401.xlsx", "rb") as form_version_1_file:
+        with open("iaso/fixtures/form_1.xlsx", "rb") as form_version_1_file:
             survey = parsing.parse_xls_form(form_version_1_file)
             form_version_1 = FormVersion.objects.create_for_form_and_survey(
                 form=form, survey=survey, xls_file=File(form_version_1_file)
             )
-            form_version_1.version_id = "2020051401"  # force version to match instance files
+            form_version_1.version_id = "2020060801"  # force version to match instance files
             form_version_1.save()
 
-        with open("iaso/fixtures/csi_quantite_valide_2020051402.xlsx", "rb") as form_version_2_file:
-            survey = parsing.parse_xls_form(form_version_2_file, previous_version=form_version_1.version_id)
-            form_version_2 = FormVersion.objects.create_for_form_and_survey(
-                form=form, survey=survey, xls_file=File(form_version_2_file)
-            )
-            form_version_2.version_id="2020051402"
-            form_version_2.save()
-
-        with open("iaso/fixtures/79_3_2020-05-08_15-01-54.xml", "rb") as instance_1_file:
-            Instance.objects.create(uuid=uuid4(), export_id=uuid4(), name="CSI - Quantite - Valide (ssf)",
+        with open("iaso/fixtures/instance_form_1_1.xml", "rb") as instance_1_file:
+            Instance.objects.create(uuid=uuid4(), export_id=uuid4(), name="Test form 1 (ssf)",
                                     file=File(instance_1_file),
-                                    file_name="79_3_2020-05-08_15-01-54.xml",
+                                    file_name="instance_form_1_1.xml",
                                     location="SRID=4326;POINT Z (-17.4586282 14.7456688 0)",
-                                    org_unit=health_center_somewhere, form=form, project=project, accuracy=3000.00,
+                                    org_unit=org_unit, form=form, project=project, accuracy=25.00,
                                     device=device,
-                                    period="202002")
-
-        with open("iaso/fixtures/79_2020-04-07_14-47-25.xml", "rb") as instance_2_file:
-            Instance.objects.create(uuid=uuid4(), export_id=uuid4(), name="CSI - Quantite - Valide (ssf)",
-                                    file=File(instance_2_file),
-                                    file_name="79_2020-04-07_14-47-25.xml",
-                                    location="SRID=4326;POINT Z (-17.4590048 14.7449495 0)",
-                                    org_unit=health_center_somewhere, form=form, project=project, accuracy=20.00,
-                                    device=device,
-                                    period="202001")
+                                    period="2020Q1")

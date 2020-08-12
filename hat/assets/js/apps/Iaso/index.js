fc5c697ebba00df80cba3d2e09a398dde733ab82
madewulf <mdewulf@bluesquarehub.com>
2020-08-12 17:17:23 +0200

    Fixing default urls

--- hat/assets/js/apps/Iaso/index.js
+++ hat/assets/js/apps/Iaso/index.js
@@ -67,7 +67,7 @@ export default function iasoApp(element, baseUrl) {
 
     routes = routes.concat([
         <Redirect path="/" to={baseUrls.forms} />,
-        <Redirect path={baseUrls.orgUnits} to={`${baseUrls.orgUnits}/locationLimit/${locationLimitMax}/order/id/pageSize/50/page/1/searchTabIndex/0/searches/[{"validated":"both", "color":"${getChipColors(0).replace('#', '')}"}]`} />,
+        <Redirect path={baseUrls.orgUnits} to={`${baseUrls.orgUnits}/locationLimit/${locationLimitMax}/order/id/pageSize/50/page/1/searchTabIndex/0/searches/[{"validation_status":"all", "color":"${getChipColors(0).replace('#', '')}"}]`} />,
         <Redirect path={baseUrls.mappings} to={`${baseUrls.mappings}/order/form_version__form__name,form_version__version_id,mapping__mapping_type/pageSize/20/page/1`} />,
         <Redirect path={baseUrls.users} to={`${baseUrls.users}/order/user__username/pageSize/20/page/1`} />,
         <Redirect path={baseUrls.groups} to={`${baseUrls.groups}/order/name/pageSize/20/page/1`} />,

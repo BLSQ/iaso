882f3c61f1a156f4163690b55e2b71f1ad2b86ab
madewulf <mdewulf@bluesquarehub.com>
2020-09-15 22:36:00 +0200

    Improved admin for api imports

--- hat/vector_control/admin.py
+++ hat/vector_control/admin.py
@@ -5,8 +5,8 @@ from .models import APIImport
 
 class APIImportAdmin(admin.GeoModelAdmin):
     date_hierarchy = "created_at"
-    search_fields = ("json",)
-    list_filter = ("has_problem",)
+    search_fields = ("json_body", "headers", "exception")
+    list_filter = ("has_problem", "import_type")
 
 
 admin.site.register(APIImport, APIImportAdmin)

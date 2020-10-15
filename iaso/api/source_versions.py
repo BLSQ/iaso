4f435f09be899b4fc187a0504470abb812b29c6b
madewulf <mdewulf@bluesquarehub.com>
2020-10-15 18:16:04 +0200

    Better source versions API

--- iaso/api/source_versions.py
+++ iaso/api/source_versions.py
@@ -1,32 +1,60 @@
-from rest_framework import viewsets, permissions
-from rest_framework.response import Response
 from iaso.models import SourceVersion
+from .common import ModelViewSet
+from iaso.models import DataSource, OrgUnit
+from rest_framework import serializers, permissions
+from .common import HasPermission
 
 
-class SourceVersionViewSet(viewsets.ViewSet):
+class SourceVersionSerializer(serializers.ModelSerializer):
     """Source versions API
 
     This API is restricted to authenticated users (no specific permission check)
 
     GET /api/sourceversions/
     """
+    class Meta:
+        model = SourceVersion
+        fields = ["id", "data_source", "number", "description", "created_at", "updated_at"]
 
-    permission_classes = [permissions.IsAuthenticated]
+    def validate_data_source(self, value):
+        """
+        Check that data source belongs to the account
+        """
+        account = self.context["request"].user.iaso_profile.account
+        sources = DataSource.objects.filter(projects__account=account)
 
-    def list(self, request):
-        versions = SourceVersion.objects.all()
-        profile = request.user.iaso_profile
-        versions = versions.filter(data_source__projects__account=profile.account)
+        if value not in sources:
+            raise serializers.ValidationError("Source does not belong to this account ")
+        return value
 
-        source_id = request.GET.get("source", None)
+
+class SourceVersionViewSet(ModelViewSet):
+    """ Data source API
+
+    This API is restricted to authenticated users having at least one of the "menupermissions.iaso_mappings",
+    "menupermissions.iaso_org_units", and "menupermissions.iaso_links" permissions
+
+    GET /api/datasources/
+    GET /api/datasources/<id>
+    """
+
+    permission_classes = [permissions.IsAuthenticated,
+                         HasPermission("menupermissions.iaso_org_units"),
+    ]
+    serializer_class = SourceVersionSerializer
+    results_key = "sources"
+    queryset = DataSource.objects.all()
+    http_method_names = ["get", "post", "put", "head", "options", "trace", "delete"]
+
+    def get_queryset(self):
+        profile = self.request.user.iaso_profile
+
+        versions = SourceVersion.objects.filter(data_source__projects__account=profile.account)
+
+        source_id = self.kwargs.get("source", None)
         if source_id:
             versions = versions.filter(data_source_id=source_id)
 
-        return Response(
-            {
-                "versions": [
-                    version.as_dict()
-                    for version in versions.order_by("data_source_id", "number")
-                ]
-            }
-        )
+        return versions.order_by("id")
+
+

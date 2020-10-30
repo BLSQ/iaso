d563e6d348b0f716ca93308066e468514f7302d5
Christophe Gerard <cgerard@bluesquarehub.com>
2020-10-30 14:49:51 +0100

    test app loading and install coverage tools

---
+++ hat/assets/js/test/helpers.js
@@ -0,0 +1,22 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { mount, render, shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+global.expect = expect;
+
+global.sinon = sinon;
+
+global.mount = mount;
+global.render = render;
+global.shallow = shallow;
+
+const mock = require('mock-require');
+
+// mock Translations
+mock('__intl/messages/fr', []);
+mock('__intl/messages/en', []);
+mock('__intl/localeData/fr', []);
+mock('__intl/localeData/en', []);

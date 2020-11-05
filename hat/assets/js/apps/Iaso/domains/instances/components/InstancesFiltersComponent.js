eba8ce5e310019d601f929cb0040c52e46663499
Christophe Gerard <cgerard@bluesquarehub.com>
2020-11-05 11:02:11 +0100

    worst commit ever

--- hat/assets/js/apps/Iaso/domains/instances/components/InstancesFiltersComponent.js
+++ hat/assets/js/apps/Iaso/domains/instances/components/InstancesFiltersComponent.js
@@ -202,6 +202,7 @@ class InstancesFiltersComponent extends Component {
                             onLatestIdChanged={() =>
                                 setInstancesFilterUpdated(true)
                             }
+                            defaultVersion
                             params={params}
                             baseUrl={baseUrl}
                         />

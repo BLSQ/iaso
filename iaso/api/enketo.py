a3919600305aac6884ede772b7fb8a9a3399ee5a
madewulf <mdewulf@bluesquarehub.com>
2020-11-06 07:46:39 +0100

    New api to create instances using enketo.

--- iaso/api/enketo.py
+++ iaso/api/enketo.py
@@ -7,9 +7,12 @@ from rest_framework.response import Response
 from rest_framework import status
 from iaso.enketo import (
     enketo_settings,
-    enketo_url,
+    enketo_url_for_edition,
+    enketo_url_for_creation,
     to_xforms_xml,
     inject_userid_and_version,
+    inject_instance_id_in_form,
+    inject_instance_id_in_instance,
     EnketoError,
     ENKETO_FORM_ID_SEPARATOR,
 )
@@ -20,6 +23,7 @@ from hat.audit.models import log_modification, INSTANCE_API
 from iaso.models import User
 from uuid import uuid4
 
+
 def public_url_for_enketo(request, path):
     resolved_path = request.build_absolute_uri(path)
 
@@ -29,55 +33,31 @@ def public_url_for_enketo(request, path):
     return resolved_path
 
 
-sample_xml = """<data xmlns:jr="http://openrosa.org/javarosa" xmlns:orx="http://openrosa.org/xforms" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:odk="http://www.opendatakit.org/xforms" id="%(odk_form_id)s" version="%(version)s">
-          <meta>
-            <instanceID>uuid:%(uuid)s</instanceID>
-          </meta>
-        </data>
-"""
-
-sample_xml = """<%(odk_form_id)s xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:orx="http://openrosa.org/xforms" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:odk="http://www.opendatakit.org/xforms" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:jr="http://openrosa.org/javarosa" id="%(odk_form_id)s" version="%(version)s"><meta><instanceID>uuid:%(uuid)s</instanceID></meta></%(odk_form_id)s>"""
-
 @api_view(["POST"])
 @permission_classes([permissions.IsAuthenticated])
 def enketo_create_url(request):
     form_id = request.data.get("form_id")
     period = request.data.get("period", None)
-    org_unit_id =  request.data.get("org_unit_id")
-    return_url = request.data.get("return_url")
+    org_unit_id = request.data.get("org_unit_id")
 
     uuid = str(uuid4())
     form = Form.objects.get(id=form_id)
-    latest_version = form.latest_version
-    latest_version_xml = latest_version.file.read()
-    latest_version_soup = Soup(latest_version_xml, "xml")
 
-    #print("latest_version_xml", latest_version_xml)
-    filled_xml = sample_xml % {"form_id": form.form_id, "uuid": uuid, "odk_form_id": form.form_id, "version": latest_version.version_id }
-    #print("-----------------------------------filled_xml", filled_xml)
-    filled_soup = Soup(filled_xml, 'xml')
-    #print("-----------------------------------filled_soup", filled_soup)
-    #print("-----------------------------------latest_version_soup", latest_version_soup)
-    instance_xml = latest_version_soup.head.model.instance.data
-    #print("-----------------------------------instance_xml", instance_xml)
-    main_element = getattr(filled_soup, form.form_id)
-    main_element.append(instance_xml)
-    print("-----------------------------------filled_soup", filled_soup)
-
-    i = Instance(form_id=form_id, period=period, uuid=uuid)
+    i = Instance(
+        form_id=form_id,
+        period=period,
+        uuid=uuid,
+        org_unit_id=org_unit_id,
+        project=form.projects.first(),
+        file_name=str(uuid) + "xml",
+    )  # warning for access rights here
     i.save()
 
     try:
-        edit_url = enketo_url(
-            public_url_for_enketo(request, "/api/enketo"),
-            form_id_string=form.form_id
-            + ENKETO_FORM_ID_SEPARATOR
-            + str(form.id)
-            + ENKETO_FORM_ID_SEPARATOR
-            + str(latest_version.version_id),
-            instance_xml=filled_soup,
-            instance_id=uuid,
-            return_url=request.GET.get("return_url", public_url_for_enketo(request, "")),
+        edit_url = enketo_url_for_creation(
+            server_url=public_url_for_enketo(request, "/api/enketo"),
+            uuid=uuid,
+            return_url=public_url_for_enketo(request, "/dashboard/instance/instanceId/%s" % i.id),
         )
 
         return JsonResponse({"edit_url": edit_url}, status=201)
@@ -97,19 +77,20 @@ def enketo_edit_url(request, instance_uuid):
     try:
         instance_xml = instance.file.read()
         soup = Soup(instance_xml, "xml")
-        instanceid = soup.meta.instanceID.contents[0].strip().replace("uuid:", "")
+        instance_id = soup.meta.instanceID.contents[0].strip().replace("uuid:", "")
 
         # inject editUserID in the meta section of the xml
         # to allow assign Modification to the user
         instance_xml = inject_userid_and_version(
             instance_xml.decode("utf-8"), request.user.id, instance.form.latest_version.version_id
         )
+        instance_xml = inject_instance_id_in_instance(instance_xml, instance.id)
 
-        edit_url = enketo_url(
+        edit_url = enketo_url_for_edition(
             public_url_for_enketo(request, "/api/enketo"),
             form_id_string=instance.uuid,
             instance_xml=instance_xml,
-            instance_id=instanceid,
+            instance_id=instance_id,
             return_url=request.GET.get("return_url", public_url_for_enketo(request, "")),
         )
 
@@ -124,28 +105,33 @@ def enketo_edit_url(request, instance_uuid):
 def enketo_form_list(request):
     form_id_str = request.GET["formID"]
     i = Instance.objects.get(uuid=form_id_str)
-
     lastest_form_version = i.form.latest_version
     # will it work through s3, what about "signing" infos if they expires ?
-    downloadurl = public_url_for_enketo(request, lastest_form_version.file.url)
-    downloadurl = public_url_for_enketo(request, "/api/enketo/formDownload/?instance_id=%s" % i.id)
-    downloadurl = "http://localhost:8081/api/enketo/formDownload/?instance_id=%s" % i.id
-    xforms = to_xforms_xml(
-        i.form,
-        download_url=downloadurl,
-        version=lastest_form_version.version_id,
-        md5checksum=calculate_file_md5(lastest_form_version.file),
-    )
+    downloadurl = public_url_for_enketo(request, "/api/enketo/formDownload/?uuid=%s" % i.uuid)
 
-    return HttpResponse(xforms, content_type="application/xml")
+    if request.method == "GET":
+        xforms = to_xforms_xml(
+            i.form,
+            download_url=downloadurl,
+            version=lastest_form_version.version_id,
+            md5checksum=calculate_file_md5(lastest_form_version.file),
+            new_form_id=form_id_str,
+        )
+
+        return HttpResponse(xforms, content_type="application/xml")
+    else:
+        return HttpResponse(content_type="application/xml")
+
 
 @api_view(["GET", "HEAD"])
 @permission_classes([permissions.AllowAny])
 def enketo_form_download(request):
-    instance_id = request.GET.get("instance_id")
-    i = Instance.objects.get(pk=instance_id)
+    uuid = request.GET.get("uuid")
+    i = Instance.objects.get(uuid=uuid)
+    xml_string = i.form.latest_version.file.read().decode("utf-8")
+    injected_xml = inject_instance_id_in_form(xml_string, i.id)
+    return HttpResponse(injected_xml, content_type="application/xml")
 
-    return HttpResponse(i.form.latest_version.file, content_type="application/xml")
 
 class EnketoSubmissionAPIView(APIView):
     permission_classes = [permissions.AllowAny]
@@ -165,13 +151,14 @@ class EnketoSubmissionAPIView(APIView):
             xml = main_file.read()
             soup = Soup(xml, "xml")
             # should we add form_id criteria or instanceID is enough ?
-
-            instanceid = soup.meta.deprecatedID.contents[0].strip()
-            user_id = soup.meta.editUserID.contents[0].strip()
-            user = User.objects.filter(id=user_id).first()
-            original = Instance.objects.filter(json__instanceID=instanceid).first()
-
-            instance = Instance.objects.filter(json__instanceID=instanceid).first()
+            instanceid = soup.data["iasoInstance"]
+            try:
+                user_id = soup.meta.editUserID.contents[0].strip()
+                user = User.objects.filter(id=user_id).first()
+            except:
+                user = None
+            original = Instance.objects.get(id=instanceid)
+            instance = Instance.objects.get(id=instanceid)
 
             instance.file = main_file
             instance.json = {}
--- iaso/enketo/enketo_xml.py
+++ iaso/enketo/enketo_xml.py
@@ -3,7 +3,7 @@ from lxml import etree
 ENKETO_FORM_ID_SEPARATOR = "-"
 
 
-def inject_userid_and_version(xml_str, userId, version_id):
+def inject_userid_and_version(xml_str, user_id, version_id):
     root = etree.fromstring(xml_str)
 
     root.set("version", str(version_id))
@@ -12,32 +12,63 @@ def inject_userid_and_version(xml_str, userId, version_id):
 
     edit_user_id_tags = [c for c in meta_tag if c.tag == "editUserID"]
     if len(edit_user_id_tags) > 0:
-        edit_user_id_tags[0].text = str(userId)
+        edit_user_id_tags[0].text = str(user_id)
     else:
         edit_user_id_tag = etree.Element("editUserID")
-        edit_user_id_tag.text = str(userId)
+        edit_user_id_tag.text = str(user_id)
         meta_tag.append(edit_user_id_tag)
 
     instance_xml = etree.tostring(root, pretty_print=False, encoding="UTF-8")
     return instance_xml.decode("utf-8")
 
 
-def to_xforms_xml(form, download_url, version, md5checksum):
+def inject_instance_id_in_instance(xml_str, instance_id):
+    root = etree.fromstring(xml_str)
+    root.set("iasoInstance", str(instance_id))
+    instance_xml = etree.tostring(root, pretty_print=False, encoding="UTF-8")
+    return instance_xml.decode("utf-8")
+
+
+def inject_instance_id_in_form(xml_str, instance_id):
+    root = etree.fromstring(xml_str)
+    root.set("iasoInstance", str(instance_id))
+
+    head = root[0]
+    model = [c for c in head if c.tag == "{http://www.w3.org/2002/xforms}model"][0]
+    instance = [c for c in model if c.tag == "{http://www.w3.org/2002/xforms}instance"][
+        0
+    ]
+    data = [c for c in instance if c.tag == "{http://www.w3.org/2002/xforms}data"][0]
+
+    instance_id_tags = [c for c in data if c.tag == "iasoInstanceId"]
+    if len(instance_id_tags) > 0:
+        instance_id_tags[0].text = str(instance_id)
+    else:
+        instance_id_tag = etree.Element("iasoInstanceId")
+        instance_id_tag.text = str(instance_id)
+        data.set("iasoInstance", str(instance_id))
+    instance_xml = etree.tostring(root, pretty_print=False, encoding="UTF-8")
+    return instance_xml.decode("utf-8")
+
+
+def to_xforms_xml(form, download_url, version, md5checksum, new_form_id=None):
     # create XML
     ns = {"xmlns": "http://openrosa.org/xforms/xformsList"}
     root = etree.Element("xforms", ns)
 
     xform = etree.Element("xform")
     root.append(xform)
-
     form_id = etree.Element("formID")
-    form_id.text = (
-        form.form_id
-        + ENKETO_FORM_ID_SEPARATOR
-        + str(form.id)
-        + ENKETO_FORM_ID_SEPARATOR
-        + str(form.latest_version.version_id)
-    )
+    if new_form_id is not None:
+        form_id.text = new_form_id
+    else:
+        form_id.text = (
+            form.form_id
+            + ENKETO_FORM_ID_SEPARATOR
+            + str(form.id)
+            + ENKETO_FORM_ID_SEPARATOR
+            + str(form.latest_version.version_id)
+        )
 
     xform.append(form_id)
 

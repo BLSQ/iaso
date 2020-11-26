276459762603d36b13b7fd65edb34fd4d54f5695
Martin De Wulf <mdewulf@bluesquarehub.com>
2020-11-26 17:19:41 +0100

    Allowing to create and update an app

--- iaso/api/apps/serializers.py
+++ iaso/api/apps/serializers.py
@@ -13,57 +13,24 @@ class AppSerializer(ProjectSerializer):
 
     class Meta(ProjectSerializer.Meta):
         model = Project
-        fields = ["id", "name","app_id", "forms", "feature_flags", "needs_authentication", "created_at", "updated_at"]
+        fields = ["id", "name", "app_id", "forms", "feature_flags", "needs_authentication", "created_at", "updated_at"]
         read_only_fields = ["id", "created_at", "updated_at"]
-        # extra_kwargs = {'name': {'required': True}}
-        # validators = [
-        #     RequiredValidator(
-        #         fields=('name')
-        #     )
-        # ]
-    @staticmethod
-    def get_feature_flags(obj: FeatureFlag):
-        return ([v.as_dict() for v in obj.all()],)
 
     id = serializers.CharField(read_only=True, source="app_id")
 
-    def create(self, request):
-        # app = Project(**request)
-        app = Project()
-        logger.error(request)
-        # org_unit.groups.set(new_groups)
+    def update(self, instance, validated_data):
+        feature_flags = validated_data.pop('feature_flags', None)
+        app_id = validated_data.pop('app_id', None)
+        name = validated_data.pop('name', None)
+        if app_id is not None:
+            instance.app_id = app_id
+        if name is not None:
+            instance.name = name
+        instance.save()
+        if feature_flags is not None:
+            instance.feature_flags.clear()
+            for f in feature_flags:
+                f_object = FeatureFlag.objects.get(code=f["code"])
+                instance.feature_flags.add(f_object)
 
-        forms = []
-        for form in request.forms:
-            temp_form = get_object_or_404(Form, id=form)
-            forms.append(temp_form)
-        app.forms.set(forms)
-
-        audit_models.log_modification(None, app, source="app_api", user=request.user)
-
-        app.save()
-        # account = self.context["request"].user.iaso_profile.account
-        # project = account.project_set.first()  # not wonderful, there should maybe be a default project rather than this
-        # ds.projects.add(project)
-        return app
-
-    def update(self, request, pk=None):
-        # breakpoint()
-        logger.error(request)
-        # print(request)
-
-        # algo_id = request.data.get("algoId", -1)
-        # source_origin_id = request.data.get("sourceOriginId", -1)
-        # version_origin = request.data.get("versionOrigin", -1)
-        # source_destination_id = request.data.get("sourceDestinationId", -1)
-        # version_destination = request.data.get("versionDestination", -1)
-        #
-        # algorithm = MatchingAlgorithm.objects.get(id=algo_id)
-        # source_1 = DataSource.objects.get(id=source_origin_id)
-        # version_1 = SourceVersion.objects.get(number=version_origin, data_source=source_1)
-        # source_2 = DataSource.objects.get(id=source_destination_id)
-        # version_2 = SourceVersion.objects.get(number=version_destination, data_source=source_2)
-        # algo_module = importlib.import_module(algorithm.name)
-        # algo = algo_module.Algorithm()
-        # algo.match(version_1, version_2, request.user)
-        return Response(True)
+        return instance

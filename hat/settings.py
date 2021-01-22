f3555908d3456287467463bf5d3ddd0361608c74
Martin De Wulf <mdewulf@bluesquarehub.com>
2021-01-22 14:55:03 +0100

    Fixing the static root for dev server

--- hat/settings.py
+++ hat/settings.py
@@ -263,7 +263,7 @@ if USE_S3:
 else:
     MEDIA_URL = "/media/"
     STATIC_URL = "/static/"
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "iaso/static"), os.path.join(BASE_DIR, "hat/assets/webpack"))

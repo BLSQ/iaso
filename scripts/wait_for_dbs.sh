e12e6138d5b5ac9c44e8f1fa4ba7c9cd237bdfce
Kaio Brito <me@kaiobrito.com>
2021-04-21 10:45:27 -0300

    Fix missing database password on wait_for_dbs.sh

--- scripts/wait_for_dbs.sh
+++ scripts/wait_for_dbs.sh
@@ -3,7 +3,7 @@ set -e
 
 # Simple script to wait for DBs to have been started
 
-until psql -h "db" -U "postgres" -c '\l' > /dev/null; do
+until PGPASSWORD="postgres" psql -h "db" -U "postgres" -c '\l' > /dev/null; do
   >&2 echo "Waiting for postgres..."
   sleep 1
 done

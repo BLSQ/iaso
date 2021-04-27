e584a5a833ae8d0a5ab937a6669b81d97ea19608
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-04-27 13:52:58 +0200

    Remove specific js code in plugin

--- plugins/polio/js/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const HelloWorld = () => {
-  return <h1>My name is react</h1>;
-};
-
-ReactDOM.render(<HelloWorld />, document.getElementById("app"));

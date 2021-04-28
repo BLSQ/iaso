efebe39da01fd602960d014f0ed5dc66c3548804
Kaio Brito <me@kaiobrito.com>
2021-04-28 18:24:14 +0000

    Add layout component

---
+++ plugins/polio/js/src/componetnt/Layout.jsx
@@ -0,0 +1,39 @@
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+export const Layout = ({ children }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <AppBar position="relative" color="primary">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Polio Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {children}
+    </>
+  );
+};

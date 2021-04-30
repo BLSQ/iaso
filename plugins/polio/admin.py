5f164e429709e1ccc33a868c81aba2e2224defa0
Kaio Brito <me@kaiobrito.com>
2021-04-30 14:44:41 +0000

    Add CampaignAdmin

--- plugins/polio/admin.py
+++ plugins/polio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Campaign
 
-# Register your models here.
+
+class CampaignAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['initial_org_unit']
+    list_filter = ['virus', 'vacine', 'detection_status', 'risk_assessment_status', 'budget_status']
+
+
+admin.site.register(Campaign, CampaignAdmin)

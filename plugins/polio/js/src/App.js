96cc007bbecd76d60f797466bc339fa366642476
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-04-30 13:57:45 +0200

    Remove unused import

--- plugins/polio/js/src/App.js
+++ plugins/polio/js/src/App.js
@@ -1,8 +1,6 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Dashboard } from './components/Dashboard';
 
-import { Layout } from './components/Layout';
-
 function App() {
     return (
         <Router>

b80ba4522f7648911d091ccf399cde3787b5e036
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-04-30 11:19:09 +0200

    Add dummy table

--- plugins/polio/js/src/components/Dashboard.js
+++ plugins/polio/js/src/components/Dashboard.js
@@ -1,3 +1,89 @@
+import { Box, makeStyles } from '@material-ui/core';
+import { useMemo } from 'react';
+import { useTable } from 'react-table';
+
+import commonStyles from '../styles/common';
+
+const useStyles = makeStyles(theme => ({
+    ...commonStyles(theme),
+}));
+
 export const Dashboard = () => {
-    return <div>Dashboard</div>;
+    const classes = useStyles();
+
+    const data = useMemo(
+        () => [
+            {
+                col1: 'Hello',
+                col2: 'World',
+            },
+            {
+                col1: 'react-table',
+                col2: 'rocks',
+            },
+            {
+                col1: 'whatever',
+                col2: 'you want',
+            },
+        ],
+        [],
+    );
+
+    const columns = useMemo(
+        () => [
+            {
+                Header: 'Column 1',
+                accessor: 'col1',
+            },
+            {
+                Header: 'Column 2',
+                accessor: 'col2',
+            },
+        ],
+        [],
+    );
+
+    const tableInstance = useTable({ columns, data });
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = tableInstance;
+
+    return (
+        <Box className={classes.containerFullHeightNoTabPadded}>
+            <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th {...column.getHeaderProps()}>
+                                    {column.render('Header')}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map(row => {
+                        prepareRow(row);
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td {...cell.getCellProps()}>
+                                            {cell.render('Cell')}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </Box>
+    );
 };

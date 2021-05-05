8879d453b37f053e24543766c4600044636771d4
Geoffrey Dhuyvetters <geoffrey.dhuyvetters@gmail.com>
2021-05-05 09:32:55 +0200

    Use async function

--- plugins/polio/js/src/utils/networking.js
+++ plugins/polio/js/src/utils/networking.js
@@ -1,12 +1,13 @@
-export const sendRequest = (method, path, body) => {
+export const sendRequest = async (method, path, body) => {
     const requestInit = {
         method,
         headers: {
-            'Accept': 'application/json',
+            Accept: 'application/json',
             'Content-Type': 'application/json',
         },
         body: body ? JSON.stringify(body) : undefined,
     };
 
-    return fetch(path, requestInit).then(response => response.json());
+    const response = await fetch(path, requestInit);
+    return await response.json();
 };

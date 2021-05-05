e895f71402ae87653a2323804953116ca2608907
Quang Son Le <qle@bluesquarehub.com>
2021-05-05 10:47:00 +0200

    IA-538 refactor: remove dead code
    - delete request.js
    - delete unused methods in fetchData.js
    - delete related tests

--- hat/assets/js/apps/Iaso/utils/fetchData.spec.js
+++ hat/assets/js/apps/Iaso/utils/fetchData.spec.js
@@ -1,16 +1,6 @@
 /* global describe, it, beforeEach, afterEach */
 import assert from 'assert';
-import nock from 'nock';
-import { createUrl, fetchUrls } from './fetchData';
-import { LOAD, LOAD_SUCCESS } from '../redux/load';
-
-function createNockScope(urls) {
-    const ns = nock('http://localhost');
-    urls.forEach(config => {
-        ns.get(new RegExp(`^${config.url}`)).reply(200, config.mock);
-    });
-    return ns;
-}
+import { createUrl } from './fetchData';
 
 describe('createUrl', () => {
     it('should create an url', () => {
@@ -21,45 +11,4 @@ describe('createUrl', () => {
         });
         assert(url === '/charts/foo/11/bar/12/baz/hello');
     });
-});
-
-describe('fetchUrls', () => {
-    const urls = [
-        { name: 'foo', url: '/api/foo', mock: { foo: true } },
-        { name: 'bar', url: '/api/bar', mock: { bar: true } },
-    ];
-    const params = { test: true };
-    const oldParams = { test: false };
-    let nockScope = null;
-
-    let actions = null;
-    const dispatch = action => {
-        actions[action.type] = action.payload || true;
-    };
-
-    beforeEach(() => {
-        nockScope = createNockScope(urls);
-        actions = {};
-    });
-
-    afterEach(() => {
-        nock.cleanAll();
-    });
-
-    it('should fetch urls', () =>
-        fetchUrls(urls, params, oldParams, dispatch).then(() => {
-            assert(actions[LOAD], 'Load action has been called');
-            assert(actions[LOAD_SUCCESS], 'Success action has been called');
-            assert(nockScope.isDone(), 'The urls have been requested');
-        }));
-
-    it('should abort when checking the result', () =>
-        fetchUrls(urls, params, oldParams, dispatch, () => false).then(() => {
-            assert(actions[LOAD], 'Load action has been called');
-            assert(
-                !actions[LOAD_SUCCESS],
-                'Success action has been not called',
-            );
-            assert(nockScope.isDone(), 'The urls have been requested');
-        }));
 });

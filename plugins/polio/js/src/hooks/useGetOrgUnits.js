8309f21780afa4f15b15c4870dfda08b69038bef
Xavier Barbosa <xavierbarbosa@gmail.com>
2021-05-06 11:21:46 +0100

    Pass urls params as a object to the query key

--- plugins/polio/js/src/hooks/useGetOrgUnits.js
+++ plugins/polio/js/src/hooks/useGetOrgUnits.js
@@ -2,14 +2,20 @@ import { useQuery } from 'react-query';
 import { sendRequest } from '../utils/networking';
 
 export const useGetOrgUnits = (parent, source) =>
-    useQuery(['polio', 'org_units', parent, source], () => {
-        const params = new URLSearchParams(
-            `parent_id=${parent}&validation_status=VALID`,
-        );
+    useQuery(
+        [
+            'org_units',
+            { parent_id: parent, source: source, validation_status: 'VALID' },
+        ],
+        () => {
+            const params = new URLSearchParams(
+                `parent_id=${parent}&validation_status=VALID`,
+            );
 
-        if (source !== undefined) {
-            params.set('source', source);
-        }
+            if (source !== undefined) {
+                params.set('source', source);
+            }
 
-        return sendRequest('GET', '/api/orgunits/?' + params.toString());
-    });
+            return sendRequest('GET', '/api/orgunits/?' + params.toString());
+        },
+    );

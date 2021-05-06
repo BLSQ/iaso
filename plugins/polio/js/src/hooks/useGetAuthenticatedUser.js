01f08c67356baef5afcae8fb2d7bac37ab50e223
Xavier Barbosa <xavierbarbosa@gmail.com>
2021-05-06 11:18:09 +0100

    Follow better convention for naming use query keys

--- plugins/polio/js/src/hooks/useGetAuthenticatedUser.js
+++ plugins/polio/js/src/hooks/useGetAuthenticatedUser.js
@@ -2,4 +2,4 @@ import { useQuery } from 'react-query';
 import { sendRequest } from '../utils/networking';
 
 export const useGetAuthenticatedUser = () =>
-    useQuery(['polio', 'user'], () => sendRequest('GET', '/api/profiles/me'));
+    useQuery(['profile', 'me'], () => sendRequest('GET', '/api/profiles/me'));

e5a132a1afa4d4324e5ac6d0884a9a4513726bfd
Xavier Barbosa <xavierbarbosa@gmail.com>
2021-05-06 11:33:11 +0100

    Reuse query params

--- plugins/polio/js/src/hooks/useGetOrgUnits.js
+++ plugins/polio/js/src/hooks/useGetOrgUnits.js
@@ -1,21 +1,20 @@
 import { useQuery } from 'react-query';
 import { sendRequest } from '../utils/networking';
 
-export const useGetOrgUnits = (parent, source) =>
-    useQuery(
-        [
-            'org_units',
-            { parent_id: parent, source: source, validation_status: 'VALID' },
-        ],
-        () => {
-            const params = new URLSearchParams(
-                `parent_id=${parent}&validation_status=VALID`,
-            );
+export const useGetOrgUnits = (parent, source) => {
+    const params = {
+        parent_id: parent,
+        source: source,
+        validation_status: 'VALID',
+    };
 
-            if (source !== undefined) {
-                params.set('source', source);
-            }
+    return useQuery(['org_units', params], () => {
+        const queryString = new URLSearchParams(params);
 
-            return sendRequest('GET', '/api/orgunits/?' + params.toString());
-        },
-    );
+        if (source === undefined) {
+            params.delete('source');
+        }
+
+        return sendRequest('GET', '/api/orgunits/?' + queryString.toString());
+    });
+};

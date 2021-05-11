5da8a60c998694fc1077ef178d360fb8d1e4c31e
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-05-11 12:55:05 +0200

    Constraint: use EXISTS() instead of count()

--- iaso/migrations/0086_add_version_constraints.py
+++ iaso/migrations/0086_add_version_constraints.py
@@ -14,10 +14,10 @@ class Migration(migrations.Migration):
 CREATE OR REPLACE FUNCTION iaso_group_org_units_same_source_version() RETURNS trigger AS
 $F$
 BEGIN
-    IF ((select count(*) from iaso_group_org_units go
+    IF (EXISTS (select count(*) from iaso_group_org_units go
             join iaso_group on (iaso_group.id = go.group_id)
             join iaso_orgunit on (iaso_orgunit.id = go.orgunit_id)
-            where iaso_orgunit.version_id != iaso_group.source_version_id) > 0)
+            where iaso_orgunit.version_id != iaso_group.source_version_id))
     THEN
         RAISE EXCEPTION 'Constraint violation iaso_group_org_units_same_source_version_constraint';
     END IF;

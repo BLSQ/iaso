1f5a977c5642b626365c7bdcba3510ee6144c5aa
Kaio Brito <me@kaiobrito.com>
2021-05-20 09:43:48 +0000

    Linting

--- plugins/polio/migrations/0005_auto_20210519_0953.py
+++ plugins/polio/migrations/0005_auto_20210519_0953.py
@@ -8,31 +8,62 @@ import uuid
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('polio', '0004_auto_20210505_1813'),
+        ("polio", "0004_auto_20210505_1813"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Preparedness',
+            name="Preparedness",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('planning_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Planning, coordination and funding')),
-                ('training_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Training for SIAs quality')),
-                ('monitoring_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Monitoring and Supervision')),
-                ('vaccine_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Vaccine, cold chain and logistics')),
-                ('advocacy_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Advocacy, social mobilization and communication')),
-                ('adverse_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Adverse Event Following Immunization (AEFI)')),
-                ('status_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Status of preparedness')),
+                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                (
+                    "planning_score",
+                    models.DecimalField(
+                        decimal_places=2, max_digits=10, verbose_name="Planning, coordination and funding"
+                    ),
+                ),
+                (
+                    "training_score",
+                    models.DecimalField(decimal_places=2, max_digits=10, verbose_name="Training for SIAs quality"),
+                ),
+                (
+                    "monitoring_score",
+                    models.DecimalField(decimal_places=2, max_digits=10, verbose_name="Monitoring and Supervision"),
+                ),
+                (
+                    "vaccine_score",
+                    models.DecimalField(
+                        decimal_places=2, max_digits=10, verbose_name="Vaccine, cold chain and logistics"
+                    ),
+                ),
+                (
+                    "advocacy_score",
+                    models.DecimalField(
+                        decimal_places=2, max_digits=10, verbose_name="Advocacy, social mobilization and communication"
+                    ),
+                ),
+                (
+                    "adverse_score",
+                    models.DecimalField(
+                        decimal_places=2, max_digits=10, verbose_name="Adverse Event Following Immunization (AEFI)"
+                    ),
+                ),
+                (
+                    "status_score",
+                    models.DecimalField(decimal_places=2, max_digits=10, verbose_name="Status of preparedness"),
+                ),
             ],
         ),
         migrations.AddField(
-            model_name='campaign',
-            name='spreadsheet_url',
+            model_name="campaign",
+            name="spreadsheet_url",
             field=models.URLField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='campaign',
-            name='preperadness_national_score',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='polio.Preparedness'),
+            model_name="campaign",
+            name="preperadness_national_score",
+            field=models.ForeignKey(
+                blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to="polio.Preparedness"
+            ),
         ),
     ]

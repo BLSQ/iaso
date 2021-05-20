b0d451fc1c805b299aa161c2a93eccc57472f7f6
Kaio Brito <me@kaiobrito.com>
2021-05-20 19:23:38 +0000

    Refresh preparedness data

--- plugins/polio/js/src/forms/PreparednessForm.js
+++ plugins/polio/js/src/forms/PreparednessForm.js
@@ -1,32 +1,51 @@
-import { Grid, Typography, Button } from '@material-ui/core';
-import { Field } from 'formik';
-import { DateInput, Select, TextInput } from '../components/Inputs';
-import { polioVacines, polioViruses } from '../constants/virus';
-import { OrgUnitsLevels } from '../components/Inputs/OrgUnitsSelect';
+import { Grid, Button } from '@material-ui/core';
+import { Field, useFormikContext } from 'formik';
+import { CircularProgress } from '@material-ui/core';
+import { TextInput } from '../components/Inputs';
 import { useStyles } from '../styles/theme';
+import { useGetPreparednessData } from '../hooks/useGetPreparednessData';
 
 export const PreparednessForm = () => {
     const classes = useStyles();
+    const { values } = useFormikContext();
+    const { mutate, isLoading, isError, error } = useGetPreparednessData();
+
+    const refreshData = () => {
+        mutate(values.spreadsheet_url);
+    };
 
     return (
         <>
             <Grid container spacing={2}>
                 <Grid container direction="row" item spacing={2}>
-                    <Grid xs={12} md={6} item>
+                    <Grid xs={12} md={8} item>
                         <Field
                             label="Google Sheet URL"
                             name={'spreadsheet_url'}
                             component={TextInput}
+                            disabled={isLoading}
                             className={classes.input}
                         />
                     </Grid>
-                    <Grid xs={6} md={3} item>
-                        <Button color="primary">Access data</Button>
+                    <Grid xs={6} md={2} item>
+                        <Button target="_blank" href={values.spreadsheet_url} color="primary">Access data</Button>
                     </Grid>
-                    <Grid xs={6} md={3} item>
-                        <Button variant="contained" color="primary">
+                    <Grid xs={6} md={2} item>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            disabled={isLoading}
+                            onClick={refreshData}
+                        >
                             Refresh Data
                         </Button>
+                    </Grid>
+
+                    <Grid xd={12} item>
+                        {isLoading && <CircularProgress />}
+                        {isError && (
+                            <div>An error occurred: {error.non_field_errors}</div>
+                        )}
                     </Grid>
                 </Grid>
             </Grid>

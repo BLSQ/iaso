6e80878c792bfde5e56c07477f168e38df84c5b1
Kaio Brito <me@kaiobrito.com>
2021-05-21 15:17:24 +0000

    Reducing number API requests when getting scores

--- plugins/polio/preparedness/google_sheet.py
+++ plugins/polio/preparedness/google_sheet.py
@@ -21,33 +21,69 @@ def _get_client():
     return gspread.authorize(creds)
 
 
-def get_national_level_preparedness_by_url(spreadsheet_url):
+def parse_value(value: str):
+    try:
+        return int(value.replace("%", ""))
+    except ValueError:
+        return 0
     client = _get_client()
-    sheet = client.open_by_url(spreadsheet_url)
-    return get_national_level_preparedness(sheet)
+def _get_scores(worksheet, initial_cell):
+    """
+    The scores are fetched using [A1 Notation](https://developers.google.com/sheets/api/guides/concepts).
+    Starting at the "Summary of {Regional|National} Level Preparedness" cell, the scores will be one column ahead and one row below.
+    For example, given the summary cell is at A1, the score's range will be B2:B8.
+
+    Regardless type of content (national, regional or district) the summary
+    should always have the same structure and the scores must be in this order:
+
+    1. Planning, coordination and funding
+    2. Training for SIAs quality
+    3. Monitoring and Supervision
+    4. Vaccine, cold chain and logistics
+    5. Advocacy, social mobilization and communication
+    6. Adverse Event Following Immunization (AEFI)*
+    Status of preparedness
+
+    * The Adverse Event Following Immunization (AEFI) is not required, when this happens the last value
+    in the array will be the status score.
+    """
+
+    first_row = initial_cell.row + 1
+    first_col = initial_cell.col + 1
+    last_row = initial_cell.row + 7
+    last_col = first_col
+
+    [
+        planning_coordination_financing_score,
+        training_sias_score,
+        monitoring_supervision_score,
+        vaccine_cold_chain_logistics_score,
+        advocacy_social_mob_commu_score,
+        adverse_event_score,
+        status_score,
+    ] = worksheet.range(first_row, first_col, last_row, last_col)
+
+    return {
+        "planning_score": parse_value(planning_coordination_financing_score.value),
+        "training_score": parse_value(training_sias_score.value),
+        "monitoring_score": parse_value(monitoring_supervision_score.value),
+        "vaccine_score": parse_value(vaccine_cold_chain_logistics_score.value),
+        "advocacy_score": parse_value(advocacy_social_mob_commu_score.value),
+        "adverse_score": parse_value(adverse_event_score.value if bool(status_score.value) else "0"),
+        "status_score": parse_value(status_score.value if bool(status_score.value) else adverse_event_score.value),
+    }
 
 
-def get_national_level_preparedness(sheet):
+def get_national_level_preparedness(sheet: gspread.Spreadsheet):
     for worksheet in sheet.worksheets():
         try:
             cell = worksheet.find("Summary of National Level Preparedness")
             print(f"Data found on worksheet: {worksheet.title}")
-            overall_scores = worksheet.cell(cell.row, cell.col + 1)
-            planning_coordination_financing_score = worksheet.cell(overall_scores.row + 1, overall_scores.col)
-            training_sias_score = worksheet.cell(
-                planning_coordination_financing_score.row + 1, planning_coordination_financing_score.col
-            )
-            monitoring_supervision_score = worksheet.cell(training_sias_score.row + 1, training_sias_score.col)
-            vaccine_cold_chain_logistics_score = worksheet.cell(
-                monitoring_supervision_score.row + 1, monitoring_supervision_score.col
-            )
-            advocacy_social_mob_commu_score = worksheet.cell(
-                vaccine_cold_chain_logistics_score.row + 1, vaccine_cold_chain_logistics_score.col
-            )
-            adverse_event_score = worksheet.cell(
-                advocacy_social_mob_commu_score.row + 1, advocacy_social_mob_commu_score.col
-            )
-            status_score = worksheet.cell(adverse_event_score.row + 1, adverse_event_score.col)
+            return _get_scores(worksheet, cell)
+
+        except gspread.CellNotFound:
+            print(f"No data found on worksheet: {worksheet.title}")
+    raise InvalidFormatError("Summary of National Level Preparedness` was not found in this document")
 
             return {
                 "planning_score": planning_coordination_financing_score.value,

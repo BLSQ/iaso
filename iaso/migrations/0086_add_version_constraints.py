81b078cc09aa806b19963d42ee32716291547dee
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-05-21 15:53:51 +0200

    Trigger: Only check modified rows
    Performenec now on par with before: 18s

--- iaso/migrations/0086_add_version_constraints.py
+++ iaso/migrations/0086_add_version_constraints.py
@@ -18,7 +18,9 @@ BEGIN
     IF ((select count(*) from iaso_group_org_units go
             join iaso_group on (iaso_group.id = go.group_id)
             join iaso_orgunit on (iaso_orgunit.id = go.orgunit_id)
-            where iaso_orgunit.version_id != iaso_group.source_version_id) > 0)
+            where go.group_id = new.group_id
+            and go.orgunit_id = new.orgunit_id
+            and iaso_orgunit.version_id != iaso_group.source_version_id) > 0)
     THEN
         RAISE EXCEPTION 'Constraint violation iaso_group_org_units_same_source_version_constraint';
     END IF;
@@ -29,39 +31,76 @@ $F$ LANGUAGE plpgsql;""",
         ),
         migrations.RunSQL(
             sql="""
+        CREATE CONSTRAINT TRIGGER iaso_group_org_units_same_source_version_constraint
+        AFTER INSERT OR UPDATE
+        ON iaso_group_org_units
+        DEFERRABLE INITIALLY DEFERRED
+        FOR EACH ROW
+        EXECUTE PROCEDURE iaso_group_org_units_same_source_version();
+        """,
+            reverse_sql="""
+        DROP TRIGGER iaso_group_org_units_same_source_version_constraint
+        ON iaso_group_org_units;""",
+        ),
+        migrations.RunSQL(
+            sql="""
+    CREATE OR REPLACE FUNCTION iaso_group_org_units_same_source_version_group() RETURNS trigger AS
+    $F$
+    BEGIN
+        IF ((select count(*) from iaso_group_org_units go
+                join iaso_group on (iaso_group.id = go.group_id)
+                join iaso_orgunit on (iaso_orgunit.id = go.orgunit_id)
+                where go.group_id = new.id
+
+                and iaso_orgunit.version_id != iaso_group.source_version_id) > 0)
+        THEN
+            RAISE EXCEPTION 'Constraint violation iaso_group_org_units_same_source_version_constraint';
+        END IF;
+        RETURN NEW;
+    END;
+    $F$ LANGUAGE plpgsql;""",
+            reverse_sql="""DROP FUNCTION iaso_group_org_units_same_source_version;""",
+        ),
+        migrations.RunSQL(
+            sql="""
             CREATE CONSTRAINT TRIGGER iaso_group_same_source_version_as_org_unit_constraint
-            AFTER INSERT OR UPDATE OR DELETE
+            AFTER INSERT OR UPDATE
             ON iaso_group
             DEFERRABLE INITIALLY DEFERRED
             FOR EACH ROW
-            EXECUTE PROCEDURE iaso_group_org_units_same_source_version();""",
+            EXECUTE PROCEDURE iaso_group_org_units_same_source_version_group();""",
             reverse_sql="""
             DROP TRIGGER iaso_group_same_source_version_as_org_unit_constraint
             ON iaso_group;""",
         ),
         migrations.RunSQL(
             sql="""
-            CREATE CONSTRAINT TRIGGER iaso_org_units_same_source_version_constraint
-            AFTER INSERT OR UPDATE OR DELETE
-            ON iaso_orgunit
-            DEFERRABLE INITIALLY DEFERRED
-            FOR EACH ROW
-            EXECUTE PROCEDURE iaso_group_org_units_same_source_version();
-            """,
-            reverse_sql="""
-            DROP TRIGGER iaso_org_units_same_source_version_constraint ON iaso_orgunit;""",
+    CREATE OR REPLACE FUNCTION iaso_group_org_units_same_source_version_orgunit() RETURNS trigger AS
+    $F$
+    BEGIN
+        IF ((select count(*) from iaso_group_org_units go
+                join iaso_group on (iaso_group.id = go.group_id)
+                join iaso_orgunit on (iaso_orgunit.id = go.orgunit_id)
+                where go.orgunit_id = new.id
+                and iaso_orgunit.version_id != iaso_group.source_version_id) > 0)
+        THEN
+            RAISE EXCEPTION 'Constraint violation iaso_group_org_units_same_source_version_constraint';
+        END IF;
+        RETURN NEW;
+    END;
+    $F$ LANGUAGE plpgsql;""",
+            reverse_sql="""DROP FUNCTION iaso_group_org_units_same_source_version_orgunit;""",
         ),
         migrations.RunSQL(
             sql="""
-            CREATE CONSTRAINT TRIGGER iaso_group_org_units_same_source_version_constraint
-            AFTER INSERT OR UPDATE OR DELETE
-            ON iaso_group_org_units
+            CREATE CONSTRAINT TRIGGER iaso_org_units_same_source_version_constraint
+            AFTER INSERT OR UPDATE
+            ON iaso_orgunit
             DEFERRABLE INITIALLY DEFERRED
             FOR EACH ROW
-            EXECUTE PROCEDURE iaso_group_org_units_same_source_version();
+            EXECUTE PROCEDURE iaso_group_org_units_same_source_version_orgunit();
             """,
             reverse_sql="""
-            DROP TRIGGER iaso_group_org_units_same_source_version_constraint
-            ON iaso_group_org_units;""",
+            DROP TRIGGER iaso_org_units_same_source_version_constraint ON iaso_orgunit;""",
         ),
     ]

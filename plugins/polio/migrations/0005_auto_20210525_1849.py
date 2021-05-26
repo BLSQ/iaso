dc0c1b018889f43ed433c7cc80f37212fb211502
Kaio Brito <me@kaiobrito.com>
2021-05-26 11:47:55 +0000

    Linting

--- plugins/polio/migrations/0005_auto_20210525_1849.py
+++ plugins/polio/migrations/0005_auto_20210525_1849.py
@@ -9,26 +9,26 @@ import uuid
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('polio', '0004_auto_20210505_1813'),
+        ("polio", "0004_auto_20210505_1813"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='campaign',
-            name='preperadness_spreadsheet_url',
+            model_name="campaign",
+            name="preperadness_spreadsheet_url",
             field=models.URLField(blank=True, null=True),
         ),
         migrations.CreateModel(
-            name='Preparedness',
+            name="Preparedness",
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
-                ('spreadsheet_url', models.URLField()),
-                ('national_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='National Score')),
-                ('regional_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='Regional Score')),
-                ('district_score', models.DecimalField(decimal_places=2, max_digits=10, verbose_name='District Score')),
-                ('payload', django.contrib.postgres.fields.jsonb.JSONField()),
-                ('created_at', models.DateTimeField(auto_now_add=True, db_index=True)),
-                ('campaign', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polio.Campaign')),
+                ("id", models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, serialize=False)),
+                ("spreadsheet_url", models.URLField()),
+                ("national_score", models.DecimalField(decimal_places=2, max_digits=10, verbose_name="National Score")),
+                ("regional_score", models.DecimalField(decimal_places=2, max_digits=10, verbose_name="Regional Score")),
+                ("district_score", models.DecimalField(decimal_places=2, max_digits=10, verbose_name="District Score")),
+                ("payload", django.contrib.postgres.fields.jsonb.JSONField()),
+                ("created_at", models.DateTimeField(auto_now_add=True, db_index=True)),
+                ("campaign", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="polio.Campaign")),
             ],
         ),
     ]

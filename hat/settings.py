5978eed6bc8253858ed4000694b02c8fdb313073
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-05-31 12:23:44 +0200

    Remove middleware
    I can't find a use for it anymore

--- hat/settings.py
+++ hat/settings.py
@@ -136,7 +136,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "hat.users.middleware.ThreadLocalMiddleware",
 ]
 
 ROOT_URLCONF = "hat.urls"

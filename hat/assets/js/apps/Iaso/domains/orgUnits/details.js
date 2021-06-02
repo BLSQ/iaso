90d3b94233ba7d54abf1867ac2cdea719ab00aef
Quang Son Le <qle@bluesquarehub.com>
2021-06-02 14:49:06 +0200

    IA-565 fix remarks from PR

--- hat/assets/js/apps/Iaso/domains/orgUnits/details.js
+++ hat/assets/js/apps/Iaso/domains/orgUnits/details.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import omit from 'lodash/omit';
 import { connect } from 'react-redux';
 import { push, replace } from 'react-router-redux';

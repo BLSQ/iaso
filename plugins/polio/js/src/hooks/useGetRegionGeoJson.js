ca78148f35b9d89ba036ed8e4c9912319ab3d045
Kaio Brito <me@kaiobrito.com>
2021-06-08 18:32:12 +0000

    Add useGetRegionGeoJson

---
+++ plugins/polio/js/src/hooks/useGetRegionGeoJson.js
@@ -0,0 +1,18 @@
+import { useQuery } from 'react-query';
+import { sendRequest } from '../utils/networking';
+
+export const useGetRegionGeoJson = region => {
+    const params = {
+        source: 6,
+        validation_status: 'all',
+        asLocation: true,
+        limit: 3000,
+        order: 'id',
+        parent_id: region,
+    };
+
+    return useQuery(['geo_json', params], () => {
+        const queryString = new URLSearchParams(params);
+        return sendRequest('GET', `/api/orgunits/?${queryString.toString()}`);
+    });
+};

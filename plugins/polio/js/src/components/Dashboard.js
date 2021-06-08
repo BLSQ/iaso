0f5ed67573d91cb82092f9c2b0d5b6d398b1a817
Quang Son Le <qle@bluesquarehub.com>
2021-06-08 12:10:31 +0200

    IA-574 style: format import

--- plugins/polio/js/src/components/Dashboard.js
+++ plugins/polio/js/src/components/Dashboard.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useMemo, useState, useCallback } from 'react';
-import {Table,textPlaceholder, IconButton as IconButtonComponent,ColumnText, LoadingSpinner} from 'bluesquare-components';
+import {
+    Table,
+    textPlaceholder,
+    IconButton as IconButtonComponent,
+    ColumnText,
+    LoadingSpinner
+} from 'bluesquare-components';
 import 'react-table/react-table.css';
 
 import {

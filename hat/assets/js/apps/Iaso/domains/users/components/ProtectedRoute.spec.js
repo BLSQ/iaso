2ef345623936a6fcea3d526b68e2ec7f3dae8a31
Quang Son Le <qle@bluesquarehub.com>
2021-06-08 22:06:43 +0200

    IA581 refactor ProtectedRoute test

--- hat/assets/js/apps/Iaso/domains/users/components/ProtectedRoute.spec.js
+++ hat/assets/js/apps/Iaso/domains/users/components/ProtectedRoute.spec.js
@@ -13,7 +13,6 @@ import { redirectTo as redirectToAction } from '../../../routing/actions';
 import SidebarMenu from '../../app/components/SidebarMenuComponent';
 
 let component;
-let baseComponent;
 
 const user = {
     id: 40,
@@ -26,28 +25,41 @@ const user = {
     org_units: [],
     language: '',
 };
+
 const updatedUser = {
-    id: 40,
-    first_name: '',
-    user_name: 'son',
-    last_name: '',
-    email: '',
-    permissions: [],
-    is_superuser: true,
-    org_units: [],
+    ...user,
     language: 'en',
 };
 const unauthorizedUser = {
-    id: 40,
-    first_name: '',
-    user_name: 'son',
-    last_name: '',
-    email: '',
+    ...user,
     permissions: ['iaso_users'],
     is_superuser: false,
-    org_units: [],
     language: 'en',
 };
+
+const makeMockedStore = value =>
+    mockedStore({
+        app: { locale: { code: 'fr', label: 'Version française' } },
+        users: { current: value },
+    });
+
+const store = makeMockedStore(user);
+
+const updatedStore = makeMockedStore(updatedUser);
+
+const storeWithUnauthorizedUser = makeMockedStore(unauthorizedUser);
+
+const storeWithNoUser = makeMockedStore(null);
+
+const localStorageSpy = sinon.spy(localStorage, 'setItem');
+
+const updatedDispatchSpy = sinon.spy(updatedStore, 'dispatch');
+
+const unauthorizedDispatchSpy = sinon.spy(
+    storeWithUnauthorizedUser,
+    'dispatch',
+);
+const redirectSpy = sinon.spy(redirectToAction);
 
 const fakeGetItem = key => {
     if (key === 'iaso_locale') {
@@ -62,56 +74,17 @@ const localStorageStub = sinon
 
 const stubComponent = () => <div>I am a stub</div>;
 
-const store = mockedStore({
-    app: { locale: { code: 'fr', label: 'Version française' } },
-    users: { current: user },
-});
-const updatedStore = mockedStore({
-    app: { locale: { code: 'fr', label: 'Version française' } },
-    users: { current: updatedUser },
-});
-
-const storeWithUnauthorizedUser = mockedStore({
-    app: { locale: { code: 'fr', label: 'Version française' } },
-    users: { current: unauthorizedUser },
-});
-
-const storeWithNoUser = mockedStore({
-    app: { locale: { code: 'fr', label: 'Version française' } },
-    users: { current: null },
-});
-
-const localStorageSpy = sinon.spy(localStorage, 'setItem');
-const updatedDispatchSpy = sinon.spy(updatedStore, 'dispatch');
-const unauthorizedDispatchSpy = sinon.spy(
-    storeWithUnauthorizedUser,
-    'dispatch',
-);
-const redirectSpy = sinon.spy(redirectToAction);
-// const userHasPermissionSpy = sinon.spy(userHasPermission);
-// const getFirstAllowedUrlSpy = sinon.spy(getFirstAllowedUrl);
-
-// const getFirstAllowedUrlStub = sinon
-//     .stub({ getFirstAllowedUrl }, 'getFirstAllowedUrl')
-//     .returns(true);
-
-// const userHasPermissionStub = sinon
-//     .stub({ userHasPermission }, 'userHasPermission')
-//     .returns(false);
-
 const renderComponent = () => {
-    baseComponent = (
-        <ProtectedRoute
-            component={stubComponent()}
-            permission="permission"
-            isRootUrl
-        />
-    );
     component = mount(
         renderWithMutableStore(
             <ErrorBoundary>
                 <MuiThemeProvider theme={theme}>
-                    {baseComponent}
+                    <ProtectedRoute
+                        component={stubComponent()}
+                        permission="permission"
+                        isRootUrl
+                        location={{ location: 'here' }}
+                    />
                 </MuiThemeProvider>
             </ErrorBoundary>,
             store,
@@ -123,8 +96,8 @@ describe.only('ProtectedRoutes', () => {
     beforeEach(() => {
         localStorageSpy.resetHistory();
         updatedDispatchSpy.resetHistory();
+        unauthorizedDispatchSpy.resetHistory();
         redirectSpy.resetHistory();
-        // getFirstAllowedUrlStub.resetHistory();
         nock.cleanAll();
         nock.abortPendingRequests();
         mockRequest('get', '/api/profiles/me', user);
@@ -135,34 +108,31 @@ describe.only('ProtectedRoutes', () => {
     it('renders', () => {
         expect(component.exists()).to.equal(true);
     });
-    it('uses the languages option in localstorage if it exists', () => {
+    it('uses the languages option in localstorage if it exists', async () => {
+        // updating store to trigger componentDidUpdate
         component.setProps({ store: updatedStore });
-        component.update();
-        // expect(updatedDispatchSpy).to.not.have.been.called;
+        await component.update();
         expect(localStorageSpy).to.not.have.been.called;
     });
-    it('uses the language option from backend if none exist in localstorage', () => {
+    it('uses the language option from backend if none exist in localstorage', async () => {
         localStorageStub.returns(null);
         component.setProps({ store: updatedStore });
-        component.update();
+        await component.update();
         expect(updatedDispatchSpy).to.have.been.calledOnce;
         // expect 2 calls because localStorage is set again by action dispatched
         expect(localStorageSpy).to.have.been.calledTwice;
     });
-    it('redirects unauthorized user to first authorized route', () => {
+    it('redirects unauthorized user to first authorized route', async () => {
         // put a value in localStorage to prevent saving language option (which would trigger a second dispatch call)
         localStorageStub.returns('en');
         component.setProps({ store: storeWithUnauthorizedUser });
-        component.update();
-        // expect(userHasPermissionSpy).to.have.been.calledOnce;
-        // expect(getFirstAllowedUrlSpy).to.have.been.calledOnce;
-        // expect(redirectSpy).to.have.been.calledOnce;
+        await component.update();
         // Spying on dispatch as imported functions cannot be stubbed or spied on
         expect(unauthorizedDispatchSpy).to.have.been.calledOnce;
     });
-    it('does not render anything if there is no user', () => {
+    it('does not render anything if there is no user', async () => {
         component.setProps({ store: storeWithNoUser });
-        component.update();
+        await component.update();
         const element = component.find(SidebarMenu).at(0);
         expect(element.exists()).to.equal(false);
     });

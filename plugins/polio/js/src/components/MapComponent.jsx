00f11c63a2453b26aa1ede50287fafee9bb772f7
Kaio Brito <me@kaiobrito.com>
2021-06-10 14:16:21 +0000

    Handle map clicks

--- plugins/polio/js/src/components/MapComponent.jsx
+++ plugins/polio/js/src/components/MapComponent.jsx
@@ -1,10 +1,10 @@
 import { MapContainer, TileLayer, GeoJSON, useMap } from 'react-leaflet';
 import * as L from 'leaflet';
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useMemo, useCallback } from 'react';
 
 import 'leaflet/dist/leaflet.css';
 
-const InnerMap = ({ orgUnits, bounds }) => {
+const InnerMap = ({ orgUnits, bounds, onClick }) => {
     const map = useMap();
 
     useEffect(() => {
@@ -14,19 +14,26 @@ const InnerMap = ({ orgUnits, bounds }) => {
     return (
         <>
             {orgUnits.map(orgUnit => (
-                <GeoJSON key={`${orgUnit.id}`} data={orgUnit.geo_json} />
+                <GeoJSON
+                    key={`${orgUnit.id}`}
+                    data={orgUnit.geo_json}
+                    eventHandlers={{
+                        click() {
+                            onClick(orgUnit);
+                        },
+                    }}
+                />
             ))}
         </>
     );
 };
 
 export const MapComponent = ({ orgUnits = [] }) => {
-    const mapRef = useRef();
-
     const bounds = useMemo(() => {
         const firstOrgUnit = orgUnits[0];
         if (firstOrgUnit) return L.geoJSON(firstOrgUnit.geo_json);
     }, [orgUnits]);
+    const onClick = useCallback(arg => console.log(arg), []);
 
     if (!bounds) {
         return null;
@@ -34,7 +41,6 @@ export const MapComponent = ({ orgUnits = [] }) => {
 
     return (
         <MapContainer
-            ref={mapRef}
             style={{ height: 500 }}
             center={[0, 0]}
             bounds={bounds}
@@ -45,7 +51,11 @@ export const MapComponent = ({ orgUnits = [] }) => {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <InnerMap orgUnits={orgUnits} bounds={bounds.getBounds()} />
+            <InnerMap
+                orgUnits={orgUnits}
+                bounds={bounds.getBounds()}
+                onClick={onClick}
+            />
         </MapContainer>
     );
 };

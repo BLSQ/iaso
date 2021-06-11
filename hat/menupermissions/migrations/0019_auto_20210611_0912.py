b501ccb04948027ee19b4e9e1a0d454652bc2381
Quang Son Le <qle@bluesquarehub.com>
2021-06-11 11:31:49 +0200

    IA543 build: format with black

--- hat/menupermissions/migrations/0019_auto_20210611_0912.py
+++ hat/menupermissions/migrations/0019_auto_20210611_0912.py
@@ -6,12 +6,28 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('menupermissions', '0018_auto_20210112_2007'),
+        ("menupermissions", "0018_auto_20210112_2007"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='custompermissionsupport',
-            options={'managed': False, 'permissions': (('x_modifications', 'Modifications'), ('x_management_teams', 'Teams'), ('x_management_users', 'Utilisateurs'), ('iaso_forms', 'Formulaires'), ('iaso_mappings', 'Correspondances avec DHIS2'), ('iaso_completeness', 'Complétude des données'), ('iaso_org_units', "Unités d'organisations"), ('iaso_links', 'Correspondances sources'), ('iaso_users', 'Utilisateurs'), ('iaso_projects', 'Projets'), ('iaso_sources', 'Sources'), ('iaso_data_tasks', 'Tâches'))},
+            name="custompermissionsupport",
+            options={
+                "managed": False,
+                "permissions": (
+                    ("x_modifications", "Modifications"),
+                    ("x_management_teams", "Teams"),
+                    ("x_management_users", "Utilisateurs"),
+                    ("iaso_forms", "Formulaires"),
+                    ("iaso_mappings", "Correspondances avec DHIS2"),
+                    ("iaso_completeness", "Complétude des données"),
+                    ("iaso_org_units", "Unités d'organisations"),
+                    ("iaso_links", "Correspondances sources"),
+                    ("iaso_users", "Utilisateurs"),
+                    ("iaso_projects", "Projets"),
+                    ("iaso_sources", "Sources"),
+                    ("iaso_data_tasks", "Tâches"),
+                ),
+            },
         ),
     ]

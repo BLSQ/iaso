735815ecf11d3c2c898e7ef3a9b3042313ec84f4
Xavier Barbosa <xavierbarbosa@gmail.com>
2021-06-11 16:26:21 +0100

    Update default query for campaigns to take into account the user org unit access level

--- plugins/polio/api.py
+++ plugins/polio/api.py
@@ -1,5 +1,8 @@
-from plugins.polio.serializers import CampaignSerializer, PreparednessPreviewSerializer, SurgePreviewSerializer
-from rest_framework import viewsets, routers
+from plugins.polio.serializers import SurgePreviewSerializer
+import itertools
+from iaso.models import OrgUnit
+from plugins.polio.serializers import CampaignSerializer, PreparednessPreviewSerializer
+from rest_framework import routers
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.decorators import action
@@ -8,7 +11,6 @@ from iaso.api.common import ModelViewSet
 
 
 class CampaignViewSet(ModelViewSet):
-    queryset = Campaign.objects.all().order_by("obr_name")
     serializer_class = CampaignSerializer
     results_key = "campaigns"
     remove_results_key_if_paginated = True
@@ -28,6 +30,17 @@ class CampaignViewSet(ModelViewSet):
         else:
             return Response(serializer.data)
 
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.iaso_profile.org_units.count():
+            org_units = map(lambda org_unit: OrgUnit.objects.hierarchy(org_unit), user.iaso_profile.org_units.all())
+            org_units = list(itertools.chain.from_iterable(org_units))
+
+            return Campaign.objects.filter(initial_org_unit__in=org_units).order_by("obr_name")
+        else:
+            return Campaign.objects.order_by("obr_name")
+
     @action(methods=["POST"], detail=False, serializer_class=PreparednessPreviewSerializer)
     def preview_preparedness(self, request, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -42,4 +55,4 @@ class CampaignViewSet(ModelViewSet):
 
 
 router = routers.SimpleRouter()
-router.register(r"polio/campaigns", CampaignViewSet)
+router.register(r"polio/campaigns", CampaignViewSet, basename="Campaign")

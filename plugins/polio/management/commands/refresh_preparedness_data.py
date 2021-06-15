74a03654e69d06f7f7883be5306e0de95ab532c6
Kaio Brito <me@kaiobrito.com>
2021-06-15 11:33:57 +0000

    Add refresh_preparedness_data command

---
+++ plugins/polio/management/commands/refresh_preparedness_data.py
@@ -0,0 +1,38 @@
+from plugins.polio.preparedness.quota_manager import QuotaManager
+from django.core.management.base import BaseCommand
+from plugins.polio.models import Campaign, Preparedness
+from plugins.polio.preparedness.parser import (
+    get_national_level_preparedness,
+    get_regional_level_preparedness,
+    open_sheet_by_url,
+)
+from plugins.polio.preparedness.calculator import get_preparedness_score
+
+
+class Command(BaseCommand):
+    help = ""
+
+    def handle(self, *args, **options):
+        manager = QuotaManager()
+        campaigns_with_spreadsheet = Campaign.objects.only("id", "preperadness_spreadsheet_url").filter(
+            preperadness_spreadsheet_url__isnull=False
+        )
+        for campaign in campaigns_with_spreadsheet:
+            print(f"Campaign {campaign.pk} refresh started")
+            sheet = open_sheet_by_url(campaign.preperadness_spreadsheet_url)
+            preparedness_data = {
+                "national": get_national_level_preparedness(sheet, manager=manager),
+                **get_regional_level_preparedness(sheet, manager=manager),
+            }
+            preparedness_data["totals"] = get_preparedness_score(preparedness_data)
+
+            Preparedness.objects.create(
+                campaign=campaign,
+                spreadsheet_url=campaign.preperadness_spreadsheet_url,
+                national_score=preparedness_data["totals"]["national_score"],
+                district_score=preparedness_data["totals"]["district_score"],
+                regional_score=preparedness_data["totals"]["regional_score"],
+                payload=preparedness_data,
+            )
+            print(f"Campaign {campaign.pk} refreshed")
+            print(f"Requests sent {manager.total_requests}")

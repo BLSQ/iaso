84bb81d874ea245aa588b0428b14d18b3a75a5e8
Kaio Brito <me@kaiobrito.com>
2021-06-21 14:46:08 +0000

    Fix issue while loading authenticated user data

--- plugins/polio/js/src/components/Inputs/OrgUnitsSelect.js
+++ plugins/polio/js/src/components/Inputs/OrgUnitsSelect.js
@@ -44,8 +44,9 @@ export const OrgUnitsSelect = props => {
 
 export const OrgUnitsLevels = ({ field = {}, form, ...props }) => {
     const { data = {} } = useGetAuthenticatedUser();
+    const { org_units = [] } = data;
     const source = data?.account?.default_version?.data_source?.id;
-    const startOrgUnit = data?.org_units[0].id ?? null;
+    const startOrgUnit = org_units[0]?.id ?? null;
     const initialOrgUnit =
         form?.initialValues?.initial_org_unit ?? startOrgUnit;
 

106f00e65a15f26ae2c84a0fbf7052411997f752
Martin De Wulf <mdewulf@bluesquarehub.com>
2021-06-29 17:25:41 +0200

    Fix: misconfiguration in admin of campaigns

--- plugins/polio/admin.py
+++ plugins/polio/admin.py
@@ -3,10 +3,10 @@ from .models import Campaign, Preparedness, Surge
 
 
 class CampaignAdmin(admin.ModelAdmin):
-    autocomplete_fields = ["initial_org_unit"]
+    raw_id_fields = ("initial_org_unit",)
     list_filter = ["virus", "vacine", "detection_status", "risk_assessment_status", "budget_status"]
 
 
-admin.site.register(Campaign)
+admin.site.register(Campaign, CampaignAdmin)
 admin.site.register(Preparedness)
 admin.site.register(Surge)

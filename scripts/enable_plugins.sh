44f542598bfa2a8f5a22a4c0610513ae16328254
Xavier Barbosa <xavier@madewithlove.com>
2021-06-29 15:51:21 +0100

    Add version for polio install bash script

--- scripts/enable_plugins.sh
+++ scripts/enable_plugins.sh
@@ -1,5 +1,7 @@
 if [ "$PLUGIN_POLIO_ENABLED" == 'true' ]; then
     cd plugins/polio/js
+    node --version && npm --version
+    which npm
     npm install
     SKIP_PREFLIGHT_CHECK=true npm run build
 fi;

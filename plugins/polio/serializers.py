a33e71d3762e6ea8632640cab68837f8a44351bf
Kaio Brito <me@kaiobrito.com>
2021-06-30 14:33:47 +0000

    Add country_parent

--- plugins/polio/serializers.py
+++ plugins/polio/serializers.py
@@ -2,7 +2,7 @@ from plugins.polio.preparedness.calculator import get_preparedness_score
 from django.db.models import fields
 from django.db.transaction import atomic
 from rest_framework import serializers
-from iaso.models import Group, OrgUnit
+from iaso.models import Group, OrgUnit, org_unit
 from .models import Preparedness, Round, Campaign, Surge
 from .preparedness.parser import (
     open_sheet_by_url,
@@ -109,15 +109,27 @@ class SurgePreviewSerializer(serializers.Serializer):
 
 
 class OrgUnitSerializer(serializers.ModelSerializer):
+    country_parent = serializers.SerializerMethodField()
     root = serializers.SerializerMethodField()
+
+    def __init__(self, **kwargs):
+        for field in kwargs.pop("hidden_fields", []):
+            self.fields.pop(field)
+        super().__init__(**kwargs)
+
+    def get_country_parent(self, instance: OrgUnit):
+        countries = instance.country_ancestors()
+        if countries is not None and len(countries) > 0:
+            country = countries[0]
+            return OrgUnitSerializer(instance=country, hidden_fields=["country_parent", "root"]).data
 
     def get_root(self, instance: OrgUnit):
         root = instance.root()
-        return OrgUnitSerializer(instance=root).data if root else None
+        return OrgUnitSerializer(instance=root, hidden_fields=["country_parent", "root"]).data if root else None
 
     class Meta:
         model = OrgUnit
-        fields = ["id", "name", "root"]
+        fields = ["id", "name", "root", "country_parent"]
 
 
 class CampaignSerializer(serializers.ModelSerializer):

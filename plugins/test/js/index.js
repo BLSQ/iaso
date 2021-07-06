a03b485844430cf6274f15cba694da9c9f23c34b
Christophe Gerard <cgerard@bluesquarehub.com>
2021-07-06 18:21:21 +0200

    adding fake table to test app

--- plugins/test/js/index.js
+++ plugins/test/js/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { makeStyles, Box } from '@material-ui/core';
+import {
+    useSafeIntl,
+    LoadingSpinner,
+    commonStyles,
+    Table,
+} from 'bluesquare-components';
 import TopBar from '../../../hat/assets/js/apps/Iaso/components/nav/TopBarComponent';
 
-import { useSafeIntl } from 'bluesquare-components';
+import tableColumns from './columns';
 import MESSAGES from './messages';
 
-const test = () => {
-    const intl = useSafeIntl();
-    return <>
-        <TopBar
-            title={intl.formatMessage(MESSAGES.title)}
-            displayBackButton={false}
-        />
-    </>;
-};
+import { redirectTo } from '../../../hat/assets/js/apps/Iaso/routing/actions';
+import {
+    getRequest,
+} from '../../../hat/assets/js/apps/Iaso/libs/Api';
 
-export default test;
+const baseUrl = 'test';
+
+const useStyles = makeStyles(theme => ({
+    ...commonStyles(theme),
+    reactTable: {
+        ...commonStyles(theme).reactTable,
+        marginTop: theme.spacing(4),
+    },
+}));
+
+const TestApp = () => {
+    const dispatch = useDispatch();
+    const classes = useStyles();
+    const intl = useSafeIntl();
+    const [fetching, setFetching] = useState(true);
+    const [data, setData] = useState([]);
+    const [count, setCount] = useState(0);
+    const fetchData = async () => {
+        const tempData = await getRequest('/api/test/blogpost/');
+        setData(tempData);
+        setCount(tempData.length);
+        setFetching(false);
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            {fetching && <LoadingSpinner />}
+            <TopBar
+                title={intl.formatMessage(MESSAGES.title)}
+                displayBackButton={false}
+            />
+            <Box className={classes.containerFullHeightNoTabPadded}>
+                <Table
+                    data={data}
+                    pages={1}
+                    defaultSorted={[{ id: 'title', desc: false }]}
+                    columns={tableColumns(intl.formatMessage, this)}
+                    count={count}
+                    baseUrl={baseUrl}
+                    params={{}}
+                    redirectTo={(key, params)=> dispatch(redirectTo(key, params))}
+                />
+            </Box>
+        </>
+    );
+}
+
+export default TestApp;
--- plugins/test/js/messages.js
+++ plugins/test/js/messages.js
@@ -9,6 +9,26 @@ const MESSAGES = defineMessages({
         defaultMessage: 'Test App',
         id: 'iaso.plugin.title',
     },
+    titleCol: {
+        defaultMessage: 'Title',
+        id: 'iaso.label.title',
+    },
+    name: {
+        id: 'iaso.label.name',
+        defaultMessage: 'Name',
+    },
+    updatedAt: {
+        id: 'iaso.forms.updated_at',
+        defaultMessage: 'Updated at',
+    },
+    createdAt: {
+        id: 'iaso.forms.created_at',
+        defaultMessage: 'Created at',
+    },
+    author: {
+        id: 'iaso.plugin.author',
+        defaultMessage: 'Author',
+    },
 });
 
 export default MESSAGES;

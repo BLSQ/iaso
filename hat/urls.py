215b1580f0549679428c5648f97b15e56d2ed329
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-07-06 14:13:24 +0200

    add _health endpoint for health checking

--- hat/urls.py
+++ hat/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin, auth
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from iaso.views import page
+from iaso.views import page, health
 
 admin.site.site_header = "Administration de Iaso"
 admin.site.site_title = "Iaso"
@@ -12,6 +12,7 @@ admin.site.index_title = "Administration de Iaso"
 urlpatterns = [
     url(r"^$", RedirectView.as_view(pattern_name="dashboard:iaso", permanent=False), name="index"),
     url(r"^accounts/", include("django.contrib.auth.urls")),
+    url(r"^_health/", health),
     url(r"^admin/", admin.site.urls),
     url(r"^api/", include("iaso.urls")),
     # url(r"^api/", include("hat.api.urls")),

c333ee9873a1788d59d984537869ec498b69d6ca
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-07-06 14:13:24 +0200

    add _health endpoint for health checking

--- iaso/views.py
+++ iaso/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required, resolve_url
 from django.contrib.auth.views import redirect_to_login
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.conf import settings
-from iaso.models import Page
+from iaso.models import Page, Account
 
 
 @login_required
@@ -15,3 +15,18 @@ def page(request, page_slug):
         return redirect_to_login(path, resolved_login_url, "next")
 
     return HttpResponse(page.content)
+
+
+def health(request):
+    """This is used by aws health check to verify the environment is up
+
+    it just looks at the 200 status code and not at the content.
+    """
+    res = {"up": "ok", "env": settings.ENVIRONMENT}
+    try:
+        # mostly to check we can connect to the db
+        res["account_count"] = Account.objects.count()
+    except:
+        res["error"] = "db_fail"
+
+    return JsonResponse(res)

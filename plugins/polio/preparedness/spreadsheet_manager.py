9328a6d4a1ee3217f2724c16fb382de5e5a48e6a
kaiobrito <kaio.brito92@gmail.com>
2021-07-12 08:13:15 -0300

    Make spreadsheet open to everyone

--- plugins/polio/preparedness/spreadsheet_manager.py
+++ plugins/polio/preparedness/spreadsheet_manager.py
@@ -4,4 +4,5 @@ from plugins.polio.preparedness.client import get_client
 def create_spreadsheet(title):
     client = get_client()
     spreadsheet = client.create(title)
+    spreadsheet.share(None, perm_type='anyone', role='writer')
     return spreadsheet

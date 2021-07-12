a5fa5afe1e2080d392d7878e7b8c5153a01d6dcf
Xavier Barbosa <xavier@madewithlove.com>
2021-07-12 12:27:08 +0100

    Add get pages hook

---
+++ plugins/polio/js/src/hooks/useGetPages.js
@@ -0,0 +1,32 @@
+import { useQuery } from 'react-query';
+import { sendRequest } from '../utils/networking';
+
+export const useGetPages = options => {
+    const params = {
+        limit: options.pageSize,
+        page: options.page,
+    };
+
+    const getURL = params => {
+        const filteredParams = Object.entries(params).filter(
+            ([key, value]) => value !== undefined,
+        );
+
+        const queryString = new URLSearchParams(
+            Object.fromEntries(filteredParams),
+        );
+
+        return `/api/pages/?${queryString.toString()}`;
+    };
+
+    return {
+        query: useQuery(
+            ['polio', 'pages', params],
+            async () => {
+                // additional props are WIP
+                return sendRequest('GET', getURL(params));
+            },
+            { cacheTime: 0, structuralSharing: false },
+        ),
+    };
+};

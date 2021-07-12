d728ee556f77d23cec7bd5de692cbc82c6e06386
kaiobrito <kaio.brito92@gmail.com>
2021-07-12 09:28:12 -0300

    Generate spreadsheet from a template

--- plugins/polio/preparedness/client.py
+++ plugins/polio/preparedness/client.py
@@ -6,7 +6,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 DIRNAME = os.path.dirname(__file__)
-SCOPES = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive.file",
+SCOPES = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive",
           "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/spreadsheets"]
 
 
--- plugins/polio/preparedness/spreadsheet_manager.py
+++ plugins/polio/preparedness/spreadsheet_manager.py
@@ -3,6 +3,7 @@ import os
 from plugins.polio.preparedness.client import get_client
 from plugins.polio.preparedness.exceptions import TemplateNotFound
 
+
 PREPAREDNESS_TEMPLATE_ID = os.environ.get("PREPAREDNESS_TEMPLATE_ID", None)
 
 
@@ -10,6 +11,6 @@ def create_spreadsheet(title):
     if not PREPAREDNESS_TEMPLATE_ID:
         raise TemplateNotFound()
     client = get_client()
-    spreadsheet = client.create(title)
+    spreadsheet = client.copy(PREPAREDNESS_TEMPLATE_ID, title, copy_permissions=True)
     spreadsheet.share(None, perm_type='anyone', role='writer')
     return spreadsheet

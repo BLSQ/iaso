8c5d0142d188d57bcdb16dff3f4ecb4996c403b4
Xavier Barbosa <xavier@madewithlove.com>
2021-07-13 17:22:24 +0100

    Add navigation back to pages list

--- hat/assets/js/apps/Iaso/domains/pages/viewPage.js
+++ hat/assets/js/apps/Iaso/domains/pages/viewPage.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { commonStyles, useSafeIntl } from 'bluesquare-components';
+import Box from '@material-ui/core/Box';
+import { withRouter } from 'react-router';
+import PropTypes from 'prop-types';
+import TopBar from '../../components/nav/TopBarComponent';
+import MESSAGES from '../forms/messages';
+import { baseUrls } from '../../constants/urls';
 
-const ViewPage = () => {
-    return <div>VIEW PAGE</div>;
+const ViewPage = ({ router }) => {
+    const intl = useSafeIntl();
+
+    return (
+        <>
+            <TopBar
+                title={intl.formatMessage(MESSAGES.title)}
+                displayBackButton
+                goBack={() => {
+                    router.push(baseUrls.pages);
+                }}
+            />
+            <Box>
+                <iframe
+                    title="powerbi dashboard"
+                    style={{
+                        height: '100vh',
+                    }}
+                    width="100%"
+                    height="100%"
+                    src="https://app.powerbi.com/view?r=eyJrIjoiMWFjNTI5NWItOWI2Yi00NDNjLWExYzQtOGNiMTMwNWMxYTM5IiwidCI6IjIxZmZhYjVkLWJlNzEtNGQ2ZS05YzRjLTFmYWZkYWUwNjdhOCIsImMiOjl9&pageName=ReportSection"
+                    frameBorder="0"
+                    allowFullScreen="true"
+                />
+            </Box>
+        </>
+    );
 };
 
-export default ViewPage;
+ViewPage.propTypes = {
+    router: PropTypes.any.isRequired,
+};
+
+export default withRouter(ViewPage);

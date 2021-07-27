3c3d966c37dbeaa6ca7bd0d609f726efebd82484
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-07-27 18:07:35 +0200

    tree add map

--- hat/assets/js/apps/Iaso/domains/orgUnits/tree.js
+++ hat/assets/js/apps/Iaso/domains/orgUnits/tree.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, {
+    useCallback,
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
+} from 'react';
 
 import { useQuery } from 'react-query';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { TreeItem, TreeView } from '@material-ui/lab';
+import Grid from '@material-ui/core/Grid';
+import { Map, TileLayer, GeoJSON } from 'react-leaflet';
+import { geoJSON } from 'leaflet';
 import { sendRequest } from '../pages/networking';
 
-export const useGetDSTree = () =>
+const useGetDSTree = () =>
     useQuery(
         ['datasource.tree'],
         async () => {
-            // additional props are WIP
             return sendRequest('GET', `/api/orgunits/tree_source_data`);
         },
         { staleTime: 5, refetchOnWindowFocus: false },
     );
 
-export const useGetTree = slug =>
+const useGetTree = slug =>
     useQuery(
         ['orgunittree', slug],
         async () => {
-            // additional props are WIP
             return sendRequest('GET', `/api/orgunits/tree?version_id=${slug}`);
         },
         { staleTime: 5, refetchOnWindowFocus: false },
+    );
+
+const useGetShape = slug =>
+    useQuery(
+        ['orgunitshape', slug],
+        async () => {
+            return sendRequest('GET', `/api/orgunits/${slug}/`);
+        },
+        { staleTime: 5, cacheTime: 5, refetchOnWindowFocus: false, enabled: Boolean(slug) },
     );
 
 const Label = node => (
@@ -42,35 +58,80 @@ const renderTree = node => (
     </TreeItem>
 );
 
-const TreeComponent = ({ data }) => {
+const TreeComponent = ({ data, onNodeSelected }) => {
+    if (!data) return null;
     return (
         <TreeView
             // className={classes.root}
             defaultCollapseIcon={<ExpandMoreIcon />}
             defaultExpanded={['0']}
             defaultExpandIcon={<ChevronRightIcon />}
+            onNodeSelect={onNodeSelected}
         >
             {renderTree(data)}
         </TreeView>
     );
 };
 
+const MapComponent = ({ children, bounds }) => {
+    const map = useRef();
+    useEffect(() => {
+        if (bounds && bounds.isValid()) {
+            map.current.leafletElement.fitBounds(bounds);
+        }
+    }, [bounds]);
+
+    return (
+        <Map
+            ref={map}
+            // style={{ height: '100%' }}
+            center={[0, 0]}
+            zoom={3}
+            scrollWheelZoom={false}
+            // bounds={bounds}
+        >
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {children}
+        </Map>
+    );
+};
+
 const TreePage = () => {
     // const { data, isFetching, error } = useGetDSTree();
-    const { data, isFetching, error } = useGetTree(4);
+    const { data, isFetching, error } = useGetTree(39);
+    const [selectedNode, setSelectedNode] = useState();
+    const { data: shape } = useGetShape(selectedNode);
 
     if (error) {
         return error.toString();
     }
-    if (isFetching) {
-        return 'Loading';
-    }
+    const handleSelect = (event, nodeIds) => {
+        setSelectedNode(nodeIds);
+    };
 
-    if (data === undefined) return 'data is undefined';
+    const bounds = useMemo(() => {
+        if (!(shape && shape.geo_json)) return null;
+        return geoJSON(shape.geo_json).getBounds();
+    }, [shape]);
+
     return (
         <>
             {isFetching && 'Loading ...'}
-            <TreeComponent data={data} />
+            <Grid container>
+                <Grid item xs={6}>
+                    <TreeComponent data={data} onNodeSelected={handleSelect} />
+                </Grid>
+                <Grid item xs={6}>
+                    <MapComponent bounds={bounds}>
+                        {shape && Boolean(shape.geo_json) && (
+                            <GeoJSON data={shape.geo_json} />
+                        )}
+                    </MapComponent>
+                </Grid>
+            </Grid>
         </>
     );
 };

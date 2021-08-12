39dd01ffcf191098b79981bd1ee0e310358f865b
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-08-12 21:01:04 +0200

    fix polio OrgUnit selector not displaying anything

--- plugins/polio/js/src/components/Inputs/TextInput.js
+++ plugins/polio/js/src/components/Inputs/TextInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TextField } from '@material-ui/core';
 import get from 'lodash.get';
 
-export const TextInput = ({ field = {}, form = {}, ...props } = {}) => {
+export const TextInput = ({ field = {}, form = {}, value, ...props } = {}) => {
     return (
         <TextField
             InputLabelProps={{
@@ -13,7 +13,7 @@ export const TextInput = ({ field = {}, form = {}, ...props } = {}) => {
             size="medium"
             {...props}
             {...field}
-            value={field.value || ''}
+            value={field.value ?? value ?? ''}
             error={form.errors && Boolean(get(form.errors, field.name))}
             helperText={form.errors && get(form.errors, field.name)}
         />

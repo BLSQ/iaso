4a6e2e6552be379a6b56f4ba9c0eaa1d58c97100
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-08-27 12:45:18 +0200

    AddTask port to react query and setup test
    SetUp testing framework so we can task component with react-query

--- hat/assets/js/apps/Iaso/domains/dataSources/components/AddTaskComponent.js
+++ hat/assets/js/apps/Iaso/domains/dataSources/components/AddTaskComponent.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { Grid } from '@material-ui/core';
 import isEqual from 'lodash/isEqual';
+import { useMutation } from 'react-query';
 import MESSAGES from '../messages';
 import ConfirmCancelDialogComponent from '../../../components/dialogs/ConfirmCancelDialogComponent';
 import { EditableTextFields } from '../../../components/forms/EditableTextFields';
 import { Checkboxes } from '../../../components/forms/Checkboxes';
 import { redirectTo } from '../../../routing/actions';
 import { baseUrls } from '../../../constants/urls';
-import { useDhisOuImporterRequest } from '../requests';
+import { sendDhisOuImporterRequest } from '../requests';
 import { useFormState } from '../../../hooks/form';
 
 const initialFormState = sourceCredentials => {
@@ -33,92 +34,33 @@ const AddTask = ({
     const [form, setFormField, _, setFormState] = useFormState(
         initialFormState(sourceCredentials),
     );
-    const [allowConfirm, setAllowConfirm] = useState(false);
-    const [redirect, setRedirect] = useState(false);
-    const [requestBody, setRequestBody] = useState();
-    const [closeDialogCallback, setCloseDialogCallback] = useState(null);
     const [withExistingDhis2Settings, setWithExistingDhis2Settings] = useState(
         !isEqual(sourceCredentials, {}),
     );
     const dispatch = useDispatch();
-    // TODO add and return reset function
-    const dhisOu = useDhisOuImporterRequest(requestBody);
+    const mutation = useMutation(sendDhisOuImporterRequest);
 
-    const submit = useCallback(() => {
-        setAllowConfirm(false);
-        const body = {
-            source_id: sourceId,
-            source_version_number: sourceVersion,
-            force: false,
-            validate_status: form.validate_status.value,
-            continue_on_error: form.continue_on_error.value,
-        };
-        if (!withExistingDhis2Settings) {
-            body.dhis2_password = form.dhis2_password.value;
-            body.dhis2_url = form.dhis2_url.value;
-            body.dhis2_login = form.dhis2_login.value;
-            setWithExistingDhis2Settings(true);
-        }
-        setRequestBody(body);
-    }, [
-        sourceId,
-        sourceVersion,
-        form.dhis2_url.value,
-        form.dhis2_login.value,
-        form.dhis2_password.value,
-        form.validate_status.value,
-        form.continue_on_error.value,
-        withExistingDhis2Settings,
-    ]);
-
-    // TODO check if sourceCredentials doesn't make reset refresh too many times
     const reset = useCallback(() => {
-        setRequestBody(null);
         setFormState(initialFormState(sourceCredentials));
     }, [sourceCredentials]);
 
-    const onConfirm = useCallback(
-        closeDialog => {
-            submit();
-            setCloseDialogCallback(() => closeDialog);
-        },
-        [submit],
-    );
-
-    const onRedirect = useCallback(
-        closeDialog => {
-            onConfirm(closeDialog);
-            setRedirect(true);
-        },
-        [onConfirm],
-    );
-    useEffect(() => {
-        if (!withExistingDhis2Settings) {
-            if (
-                form.dhis2_url.value &&
-                form.dhis2_login.value &&
-                form.dhis2_password.value
-            ) {
-                setAllowConfirm(true);
-            } else {
-                setAllowConfirm(false);
+    const submit = useCallback(
+        async (closeDialogCallBack, redirect = false) => {
+            const body = {
+                source_id: sourceId,
+                source_version_number: sourceVersion,
+                force: false,
+                validate_status: form.validate_status.value,
+                continue_on_error: form.continue_on_error.value,
+            };
+            if (!withExistingDhis2Settings) {
+                body.dhis2_password = form.dhis2_password.value;
+                body.dhis2_url = form.dhis2_url.value;
+                body.dhis2_login = form.dhis2_login.value;
+                setWithExistingDhis2Settings(true);
             }
-        } else {
-            setAllowConfirm(true);
-        }
-    }, [
-        withExistingDhis2Settings,
-        form.dhis2_url.value,
-        form.dhis2_login.value,
-        form.dhis2_password.value,
-        // this dep to unlock buttons after successful request
-        // TODO include this behaviour in reset() func
-        dhisOu,
-    ]);
-
-    useEffect(() => {
-        if (dhisOu) {
-            closeDialogCallback();
+            await mutation.mutateAsync(body);
+            closeDialogCallBack();
             if (redirect) {
                 dispatch(
                     redirectTo(baseUrls.tasks, {
@@ -127,8 +69,44 @@ const AddTask = ({
                 );
             }
             reset();
-        }
-    }, [closeDialogCallback, dhisOu, reset, redirect]);
+        },
+        [
+            sourceId,
+            sourceVersion,
+            form.dhis2_url.value,
+            form.dhis2_login.value,
+            form.dhis2_password.value,
+            form.validate_status.value,
+            form.continue_on_error.value,
+            withExistingDhis2Settings,
+        ],
+    );
+
+    const onConfirm = useCallback(
+        async closeDialog => {
+            await submit(closeDialog);
+        },
+        [submit],
+    );
+
+    const onRedirect = useCallback(
+        async closeDialog => {
+            await submit(closeDialog, true);
+        },
+        [submit],
+    );
+    let allowConfirm;
+    if (mutation.isLoading) {
+        allowConfirm = false;
+    } else if (!withExistingDhis2Settings) {
+        allowConfirm = !!(
+            form.dhis2_url.value &&
+            form.dhis2_login.value &&
+            form.dhis2_password.value
+        );
+    } else {
+        allowConfirm = true;
+    }
 
     const renderDefaultLayout = showDefaultOverride => {
         const checkboxes = [

a20621a80324a1038f289c8abf2aef0b4fc04cef
Christophe Gerard <cgerard@bluesquarehub.com>
2021-09-01 14:52:01 +0200

    black

--- hat/menupermissions/migrations/0021_auto_20210901_1228.py
+++ hat/menupermissions/migrations/0021_auto_20210901_1228.py
@@ -6,12 +6,30 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('menupermissions', '0020_pages_permissions'),
+        ("menupermissions", "0020_pages_permissions"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='custompermissionsupport',
-            options={'managed': False, 'permissions': (('x_modifications', 'Modifications'), ('x_management_teams', 'Teams'), ('x_management_users', 'Utilisateurs'), ('iaso_forms', 'Formulaires'), ('iaso_mappings', 'Correspondances avec DHIS2'), ('iaso_completeness', 'Complétude des données'), ('iaso_org_units', "Unités d'organisations"), ('iaso_links', 'Correspondances sources'), ('iaso_users', 'Utilisateurs'), ('iaso_pages', 'Pages'), ('iaso_projects', 'Projets'), ('iaso_sources', 'Sources'), ('iaso_data_tasks', 'Tâches'), ('iaso_polio', 'Polio'))},
+            name="custompermissionsupport",
+            options={
+                "managed": False,
+                "permissions": (
+                    ("x_modifications", "Modifications"),
+                    ("x_management_teams", "Teams"),
+                    ("x_management_users", "Utilisateurs"),
+                    ("iaso_forms", "Formulaires"),
+                    ("iaso_mappings", "Correspondances avec DHIS2"),
+                    ("iaso_completeness", "Complétude des données"),
+                    ("iaso_org_units", "Unités d'organisations"),
+                    ("iaso_links", "Correspondances sources"),
+                    ("iaso_users", "Utilisateurs"),
+                    ("iaso_pages", "Pages"),
+                    ("iaso_projects", "Projets"),
+                    ("iaso_sources", "Sources"),
+                    ("iaso_data_tasks", "Tâches"),
+                    ("iaso_polio", "Polio"),
+                ),
+            },
         ),
     ]

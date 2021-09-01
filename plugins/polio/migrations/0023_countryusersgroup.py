02af5e8258a70b6cb1a25c1ea308cace1429e9d7
Quang Son Le <qle@bluesquarehub.com>
2021-09-01 12:27:24 +0200

    IA-734 merge migrations

--- plugins/polio/migrations/0023_countryusersgroup.py
+++ plugins/polio/migrations/0023_countryusersgroup.py
@@ -24,4 +24,20 @@ class Migration(migrations.Migration):
                 ("users", models.ManyToManyField(blank=True, to=settings.AUTH_USER_MODEL)),
             ],
         ),
+        migrations.CreateModel(
+            name="LineListImport",
+            fields=[
+                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("file", models.FileField(upload_to="uploads/linelist/% Y/% m/% d/")),
+                ("import_result", models.JSONField()),
+                ("created_at", models.DateTimeField(auto_now_add=True, db_index=True)),
+                ("updated_at", models.DateTimeField(auto_now=True)),
+                (
+                    "created_by",
+                    models.ForeignKey(
+                        blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL
+                    ),
+                ),
+            ],
+        ),
     ]

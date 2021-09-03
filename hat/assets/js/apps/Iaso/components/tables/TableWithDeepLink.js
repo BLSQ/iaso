180ff07cffbe5a6324971d97d2722448e1cf4e59
Quang Son Le <qle@bluesquarehub.com>
2021-09-03 12:01:54 +0200

    IA-734 code cleanup

--- hat/assets/js/apps/Iaso/components/tables/TableWithDeepLink.js
+++ hat/assets/js/apps/Iaso/components/tables/TableWithDeepLink.js
@@ -4,9 +4,6 @@ import { string } from 'prop-types';
 import { handleTableDeepLink } from '../../utils/table';
 
 export const TableWithDeepLink = ({ baseUrl, ...props }) => {
-    const onTableParamsChange = handleTableDeepLink(baseUrl);
-    const newProps = { ...props };
-    newProps.onTableParamsChange = onTableParamsChange;
     return (
         // eslint-disable-next-line react/jsx-props-no-spreading
         <Table {...props} onTableParamsChange={handleTableDeepLink(baseUrl)} />

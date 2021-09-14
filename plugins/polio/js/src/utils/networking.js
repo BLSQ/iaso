7a4305ddf94cf7e4f69909ec32acd264725df450
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-09-14 18:57:01 +0200

    document

--- plugins/polio/js/src/utils/networking.js
+++ plugins/polio/js/src/utils/networking.js
@@ -46,6 +46,26 @@ const MESSAGES = defineMessages({
     },
 });
 
+/**
+ * Mix a useMutation from react-query and snackbar message as well as
+ * cache invalidation.
+ *
+ * Per default will display a message on success and on error.
+ * Success message can be disabled
+ *
+ * @param mutationFn
+ * @param snackSuccessMessage
+ *   Translatable Formatjs Message object.
+ *   pass null to supress, undefined for default.
+ * @param snackErrorMsg
+ *   idem
+ * @param invalidateQueryKey
+ *   optional query key to invalidate
+ * @param options
+ *   standard useMutation Options
+ * @returns {UseMutationResult<mutationFn, options, void, unknown>}
+ */
+
 export const useSnackMutation = (
     mutationFn,
     snackSuccessMessage = MESSAGES.defaultMutationApiSuccess,

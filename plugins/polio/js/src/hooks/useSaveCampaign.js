4fc10c5867e117a2f07574830f090fce18e6e0ce
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-09-14 18:56:48 +0200

    Polio mod: useSaveCampaign : useSnackMutation to invalidate query key

--- plugins/polio/js/src/hooks/useSaveCampaign.js
+++ plugins/polio/js/src/hooks/useSaveCampaign.js
@@ -1,24 +1,13 @@
-import { useQueryClient } from 'react-query';
 import { sendRequest, useSnackMutation } from '../utils/networking';
 
-export const useSaveCampaign = () => {
-    const queryClient = useQueryClient();
-
-    const { mutate, ...result } = useSnackMutation(body => {
-        const method = body.id ? 'PUT' : 'POST';
-        const path = `/api/polio/campaigns/${body.id ? `${body.id}/` : ''}`;
-        return sendRequest(method, path, body);
-    });
-
-    return {
-        ...result,
-        mutate: (variables, { onSuccess, ...options }) =>
-            mutate(variables, {
-                ...options,
-                onSuccess: async (...args) => {
-                    await queryClient.invalidateQueries(['polio', 'campaigns']);
-                    onSuccess(...args);
-                },
-            }),
-    };
-};
+export const useSaveCampaign = () =>
+    useSnackMutation(
+        body => {
+            const method = body.id ? 'PUT' : 'POST';
+            const path = `/api/polio/campaigns/${body.id ? `${body.id}/` : ''}`;
+            return sendRequest(method, path, body);
+        },
+        undefined,
+        undefined,
+        ['polio', 'campaigns'],
+    );

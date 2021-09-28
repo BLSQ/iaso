b53ed15b365455439d2755fb130634709a187cd0
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-09-28 17:32:57 +0200

    mod simplify useGetGeoJson
    we don't need the source since we specify the parent country
    this will remove us one uneeded api call to /profile/me

--- plugins/polio/js/src/hooks/useGetAuthenticatedUser.js
+++
@@ -1,5 +0,0 @@
-import { useSnackQuery } from '../../../../../hat/assets/js/apps/Iaso/libs/apiHooks';
-import { getRequest } from '../../../../../hat/assets/js/apps/Iaso/libs/Api';
-
-export const useGetAuthenticatedUser = () =>
-    useSnackQuery(['profile', 'me'], () => getRequest('/api/profiles/me/'));
--- plugins/polio/js/src/hooks/useGetGeoJson.js
+++ plugins/polio/js/src/hooks/useGetGeoJson.js
@@ -1,12 +1,8 @@
-import { useGetAuthenticatedUser } from './useGetAuthenticatedUser';
 import { getRequest } from '../../../../../hat/assets/js/apps/Iaso/libs/Api';
 import { useSnackQuery } from '../../../../../hat/assets/js/apps/Iaso/libs/apiHooks';
 
 export const useGetGeoJson = (country, orgUnitCategory) => {
-    const { data: user = {}, isFetching } = useGetAuthenticatedUser();
-    const source = user?.account?.default_version?.data_source?.id || '';
     const baseParams = {
-        source,
         validation_status: 'all',
         asLocation: true,
         limit: 3000,
@@ -30,8 +26,7 @@ export const useGetGeoJson = (country, orgUnitCategory) => {
         () => getRequest(`/api/orgunits/?${queryString.toString()}`),
         undefined,
         {
-            enabled: Boolean(country) && isFetching,
-            refetchOnWindowFocus: false,
+            enabled: Boolean(country),
             staleTime: 1000 * 60 * 15, // in MS
             cacheTime: 1000 * 60 * 5,
         },

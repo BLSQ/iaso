385c2226bfd8f869ed9a08395a084031a4c66f66
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-10-05 16:04:45 +0200

    polio mod useGetGeoJson

--- plugins/polio/js/src/hooks/useGetGeoJson.js
+++ plugins/polio/js/src/hooks/useGetGeoJson.js
@@ -2,23 +2,15 @@ import { getRequest } from '../../../../../hat/assets/js/apps/Iaso/libs/Api';
 import { useSnackQuery } from '../../../../../hat/assets/js/apps/Iaso/libs/apiHooks';
 
 export const useGetGeoJson = (country, orgUnitCategory) => {
-    const baseParams = {
+    const params = {
         validation_status: 'all',
         asLocation: true,
         limit: 3000,
         order: 'id',
         orgUnitParentId: country,
+        orgUnitTypeCategory: orgUnitCategory,
     };
-    const districtParam = {
-        orgUnitTypeCategory: 'DISTRICT',
-    };
-    const provinceParam = {
-        orgUnitTypeCategory: 'REGION',
-    };
-    const params =
-        orgUnitCategory === 'DISTRICT'
-            ? { ...baseParams, ...districtParam }
-            : { ...baseParams, ...provinceParam };
+
     const queryString = new URLSearchParams(params);
 
     return useSnackQuery(

b671c7e5b0fb77423149ab69a95c6797afccb411
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-10-21 11:18:36 +0200

    IA-867 Fix int display as date in submissions

--- hat/assets/js/apps/Iaso/domains/instances/utils.js
+++ hat/assets/js/apps/Iaso/domains/instances/utils.js
@@ -9,6 +9,7 @@ import MESSAGES from './messages';
 import DeleteDialog from './components/DeleteInstanceDialog';
 import ExportInstancesDialogComponent from './components/ExportInstancesDialogComponent';
 import { getCookie } from '../../utils/cookies';
+import { apiDateFormat, shortApiDateFormat } from '../../utils/dates';
 
 const NO_VALUE = '/';
 const hasNoValue = value => !value || value === '';
@@ -21,8 +22,21 @@ const KeyValueFields = ({ entry }) =>
         </>
     ));
 
+/**
+ * Pretty Format value for display
+ * Try to guess if it is a date or datetime to display in appropriate locale
+ * @param value string
+ */
 const formatValue = value => {
-    if (moment(value).isValid()) return moment(value).format('LTS');
+    // use strict mode so it doesn't try to interpret number as timestamp.
+    const asDay = moment(value, shortApiDateFormat, true);
+    if (asDay.isValid()) {
+        return asDay.format('L');
+    }
+    const asDT = moment(value, apiDateFormat, true);
+    if (asDT.isValid()) {
+        return asDT.format('LTS');
+    }
     return value;
 };
 const renderValue = (settings, c) => {

f0cbab17de7de500f16288320056c7bcd21d8b83
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-11-22 16:51:44 +0100

    Period helper class

--- iaso/periods.py
+++ iaso/periods.py
@@ -18,3 +18,112 @@ def detect(dhis2_period):
         return PERIOD_TYPE_MONTH
 
     raise Exception("unsupported dhis2 period format for '" + dhis2_period + "'")
+
+
+class Period:
+    def __init__(self, value: str):
+        self.value = value
+
+    @staticmethod
+    def from_string(period_string):
+        period_type = detect(period_string)
+        if period_type == PERIOD_TYPE_YEAR:
+            return YearPeriod(period_string)
+        elif period_type == PERIOD_TYPE_MONTH:
+            return MonthPeriod(period_string)
+        elif period_type == PERIOD_TYPE_QUARTER:
+            return QuarterPeriod(period_string)
+        elif period_type == PERIOD_TYPE_SIX_MONTH:
+            return SemesterPeriod(period_string)
+        raise ValueError("unsuported period type: {period_type}")
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.value == other
+        if not isinstance(other, Period):
+            raise ValueError("unsupported type")
+        return self.value == other.value
+
+    def __str__(self):
+        return self.value
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} {self.value}>"
+
+    def __le__(self, other):
+        assert type(self) == type(other)
+        return self.value <= other.value
+
+    def __lt__(self, other):
+        assert type(self) == type(other)
+        return self.value < other.value
+
+    @property
+    def period_type(self):
+        return detect(self.value)
+
+    def next_period(self):
+        raise NotImplemented()
+
+    def range_period_to(self, other):
+        if not type(self) == type(other):
+            raise ValueError(f"{self} not the same type as {other}")
+        if not self <= other:
+            raise ValueError(f"{self} not greater than {other}")
+        r = []
+        n = self
+        while n <= other:
+            r.append(n)
+            n = n.next_period()
+        return r
+
+
+class QuarterPeriod(Period):
+    def next_period(self):
+        p = self.value
+        year, quarter = p.split("Q")
+        quarter = int(quarter)
+        year = int(year)
+        if quarter >= 4:
+            n_year = year + 1
+            n_quarter = 1
+        else:
+            n_quarter = quarter + 1
+            n_year = year
+        return QuarterPeriod(f"{n_year:04}Q{n_quarter}")
+
+
+class YearPeriod(Period):
+    def next_period(self):
+        n_p = int(self.value) + 1
+        return YearPeriod(f"{n_p:04}")
+
+
+class SemesterPeriod(Period):
+    def next_period(self):
+        p = self.value
+        year, semester = p.split("S")
+        semester = int(semester)
+        year = int(year)
+        if semester >= 2:
+            n_year = year + 1
+            n_semester = 1
+        else:
+            n_year = year
+            n_semester = semester + 1
+        return SemesterPeriod(f"{n_year:04}S{n_semester}")
+
+
+class MonthPeriod(Period):
+    "Month start at 1"
+
+    def next_period(self):
+        year, month = int(self.value[:4]), int(self.value[4:])
+        if month == 12:
+            n_month = 1
+            n_year = year + 1
+        else:
+            n_month = month + 1
+            n_year = year
+
+        return MonthPeriod(f"{n_year:04}{n_month:02}")
---
+++ iaso/tests/test_periods.py
@@ -0,0 +1,40 @@
+from unittest import TestCase
+from iaso.periods import QuarterPeriod, MonthPeriod, YearPeriod, SemesterPeriod, period_from_string, Period
+
+
+class PeriodTests(TestCase):
+    def test_comparison(self):
+        self.assertTrue(QuarterPeriod("2021Q2") < QuarterPeriod("2021Q3"))
+        self.assertFalse(QuarterPeriod("2021Q2") < QuarterPeriod("2021Q2"))
+        self.assertTrue(QuarterPeriod("2021Q2") <= QuarterPeriod("2021Q2"))
+
+    def test_period(self):
+        self.assertEqual(repr(QuarterPeriod("2021Q2")), "<QuarterPeriod 2021Q2>", repr(QuarterPeriod("2021Q2")))
+        self.assertTrue(QuarterPeriod("2021Q2"))
+        self.assertEqual(QuarterPeriod("2021Q2").next_period(), QuarterPeriod("2021Q3"))
+        self.assertNotEqual(QuarterPeriod("2021Q1").next_period(), QuarterPeriod("2021Q3"))
+        self.assertEqual(QuarterPeriod("2021Q4").next_period(), QuarterPeriod("2022Q1"))
+        periods = QuarterPeriod("2021Q4").range_period_to(QuarterPeriod("2021Q4"))
+        self.assertEqual(periods, ["2021Q4"], periods)
+        periods = QuarterPeriod("2021Q4").range_period_to(QuarterPeriod("2022Q4"))
+        self.assertEqual(periods, ["2021Q4", "2022Q1", "2022Q2", "2022Q3", "2022Q4"])
+        self.assertEqual(YearPeriod("2014").range_period_to(YearPeriod("2017")), ["2014", "2015", "2016", "2017"])
+        self.assertRaises(ValueError, lambda: YearPeriod("2014").range_period_to(QuarterPeriod("2022Q1")))
+        self.assertEqual(MonthPeriod("202112").next_period(), MonthPeriod("202201"))
+        self.assertEqual(MonthPeriod("202101").next_period(), MonthPeriod("202102"))
+        self.assertEqual(MonthPeriod("202101").range_period_to(MonthPeriod("202101")), [MonthPeriod("202101")])
+        self.assertEqual(
+            MonthPeriod("202109").range_period_to(MonthPeriod("202202")),
+            [MonthPeriod("202109"), "202110", "202111", "202112", "202201", "202202"],
+        )
+
+    def test_semester(self):
+        self.assertEqual(
+            SemesterPeriod("2018S1").range_period_to(SemesterPeriod("2021S1")),
+            ["2018S1", "2018S2", "2019S1", "2019S2", "2020S1", "2020S2", "2021S1"],
+        )
+
+    def test_detection(self):
+        self.assertEqual(type(Period.from_string("2021Q2")), QuarterPeriod)
+        self.assertEqual(type(Period.from_string("2021")), YearPeriod)
+        self.assertEqual(type(Period.from_string("2021")), YearPeriod)

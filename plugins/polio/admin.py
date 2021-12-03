934e6dc123f6b61923f8f0c3b00e7a5418995852
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-12-03 16:32:01 +0100

    polio: view table in admin

--- plugins/polio/admin.py
+++ plugins/polio/admin.py
@@ -1,4 +1,6 @@
+import gspread.utils
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import Campaign, Preparedness, Surge, Round, Config, CountryUsersGroup, URLCache, SpreadSheetImport
 
@@ -16,9 +18,46 @@ class PreparednessAdmin(admin.ModelAdmin):
 class SpreadSheetImportAdmin(admin.ModelAdmin):
     list_filter = ["spread_id", "created_at"]
     list_display = ["spread_id", "title", "created_at", "url"]
+    readonly_fields = ["title", "table"]
 
     def title(self, obj: SpreadSheetImport):
         return obj.content["title"]
+
+    def table(self, obj: SpreadSheetImport):
+        # Write a get-method for a list of module names in the class Profile
+        # return HTML string which will be display in the form
+        # for sheet in self.content['sheets']:
+        html = ""
+
+        for sheet in obj.content["sheets"]:
+            html += f'<details open><summary><b>{sheet["title"]}</b></summary><table>'
+            try:
+                if not sheet["values"]:
+                    html += "Empty</table></details>"
+                    continue
+
+                values = gspread.utils.fill_gaps(sheet["values"])
+
+                html += "<tr><td></td>"
+                for col_num in range(len(values[0])):
+                    html += f"<td>{col_num}</td>"
+                html += "</tr>"
+
+                for row_num, row in enumerate(values):
+                    html += f"<tr><td>{row_num}</td>"
+
+                    for col in row:
+                        html += f"<td>{col}</td>\n"
+                    html += "</tr>"
+
+            except Exception as e:
+                print(e)
+                html += f"<error>render error: {e}</error>"
+                html += f'<pre>{sheet["values"]}</pre>'
+            html += "</table></details>"
+
+        # print(html)
+        return mark_safe(html)
 
 
 admin.site.register(Campaign, CampaignAdmin)

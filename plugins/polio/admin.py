9b8c67f7cf809ef549d73328f5b065ccc0a4518d
Olivier Le Thanh Duong <olivier@lethanh.be>
2021-12-03 15:55:00 +0100

    polio: admin for SpreadSheetImport

--- plugins/polio/admin.py
+++ plugins/polio/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Campaign, Preparedness, Surge, Round, Config, CountryUsersGroup, URLCache
+from .models import Campaign, Preparedness, Surge, Round, Config, CountryUsersGroup, URLCache, SpreadSheetImport
 
 
 class CampaignAdmin(admin.ModelAdmin):
@@ -13,6 +13,14 @@ class PreparednessAdmin(admin.ModelAdmin):
     list_display = ["campaign", "created_at"]
 
 
+class SpreadSheetImportAdmin(admin.ModelAdmin):
+    list_filter = ["spread_id", "created_at"]
+    list_display = ["spread_id", "title", "created_at", "url"]
+
+    def title(self, obj: SpreadSheetImport):
+        return obj.content["title"]
+
+
 admin.site.register(Campaign, CampaignAdmin)
 admin.site.register(Preparedness, PreparednessAdmin)
 admin.site.register(Config)
@@ -20,3 +28,4 @@ admin.site.register(Surge)
 admin.site.register(Round)
 admin.site.register(CountryUsersGroup)
 admin.site.register(URLCache)
+admin.site.register(SpreadSheetImport, SpreadSheetImportAdmin)

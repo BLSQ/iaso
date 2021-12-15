fcdcaf571163ca9ec7f181c401f48d2bc8c1b9af
Simon Dusausoy <dusausoy.simon@gmail.com>
2021-12-15 17:14:47 +0100

    Formatting with black

--- plugins/polio/migrations/0038_campaign_deleted_at.py
+++ plugins/polio/migrations/0038_campaign_deleted_at.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('polio', '0037_campaign_enable_send_weekly_email'),
+        ("polio", "0037_campaign_enable_send_weekly_email"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='campaign',
-            name='deleted_at',
+            model_name="campaign",
+            name="deleted_at",
             field=models.DateTimeField(blank=True, default=None, null=True),
         ),
     ]

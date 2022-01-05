c883ad45bec7394507c49d18a9899a8454122511
Quang Son Le <qle@bluesquarehub.com>
2022-01-05 14:59:28 +0100

    IA-984 move data fetching to ImMap - fetch "converted data - fetch shapes - calculate scope

--- plugins/polio/js/src/pages/IM/ImMap.js
+++ plugins/polio/js/src/pages/IM/ImMap.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { object, oneOf, array, string, bool } from 'prop-types';
+import { oneOf, string, array, number } from 'prop-types';
 import { Box } from '@material-ui/core';
 import { useSafeIntl, LoadingSpinner } from 'bluesquare-components';
 import { MapComponent } from '../../components/MapComponent/MapComponent';
@@ -9,23 +9,28 @@ import { makePopup } from '../../utils/LqasIm.tsx';
 import { LqasImMapHeader } from '../../components/LQAS-IM/LqasImMapHeader.tsx';
 import { determineStatusForDistrict, makeImMapLegendItems } from './utils.ts';
 import { districtColors } from './constants';
-import { getScopeStyle, findDataForShape } from '../../utils/index';
+import { getScopeStyle, findDataForShape, findScope } from '../../utils/index';
 import MESSAGES from '../../constants/messages';
+import { useConvertedIMData } from './requests';
+import { useGetGeoJson } from '../../hooks/useGetGeoJson';
 
 export const ImMap = ({
-    imData,
-    shapes,
+    imType,
     round,
-    campaign,
-    scope,
-    isLoading,
+    selectedCampaign,
+    countryId,
+    campaigns,
 }) => {
     const { formatMessage } = useSafeIntl();
     const [renderCount, setRenderCount] = useState(0);
+    const { data: imData, isLoading } = useConvertedIMData(imType);
+    const { data: shapes = [] } = useGetGeoJson(countryId, 'DISTRICT');
+
+    const scope = findScope(selectedCampaign, campaigns, shapes);
 
     const legendItems = makeImMapLegendItems(formatMessage)(
         imData,
-        campaign,
+        selectedCampaign,
         round,
     );
 
@@ -36,19 +41,19 @@ export const ImMap = ({
                     shape,
                     data: imData,
                     round,
-                    campaign,
+                    campaign: selectedCampaign,
                 }),
             );
             if (status) return districtColors[status];
             return getScopeStyle(shape, scope);
         },
-        [scope, campaign, round, imData],
+        [scope, selectedCampaign, round, imData],
     );
 
     // force Map render when campaign changes, otherwise, shape colors are off
     useEffect(() => {
         setRenderCount(count => count + 1);
-    }, [campaign]);
+    }, [selectedCampaign]);
 
     return (
         <>
@@ -75,7 +80,11 @@ export const ImMap = ({
                                 main: 'District',
                                 background: 'Region',
                             }}
-                            makePopup={makePopup(imData, round, campaign)}
+                            makePopup={makePopup(
+                                imData,
+                                round,
+                                selectedCampaign,
+                            )}
                             height={600}
                         />
                     </Box>
@@ -87,16 +96,13 @@ export const ImMap = ({
 
 ImMap.propTypes = {
     round: oneOf(['round_1', 'round_2']).isRequired,
-    imData: object,
-    shapes: array,
-    campaign: string,
-    scope: array,
-    isLoading: bool,
+    campaigns: array,
+    selectedCampaign: string,
+    imType: oneOf(['imGlobal', 'imOHH', 'imIHH']).isRequired,
+    countryId: number,
 };
 ImMap.defaultProps = {
-    imData: {},
-    shapes: {},
-    campaign: '',
-    scope: [],
-    isLoading: false,
+    campaigns: [],
+    selectedCampaign: '',
+    countryId: null,
 };

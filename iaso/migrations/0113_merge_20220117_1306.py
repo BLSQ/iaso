c74cb528521dc8cc4d05aa957b35203525ccb683
Simon Dusausoy <sdusausoy@bluesquarehub.com>
2022-01-17 14:25:33 +0100

    black formatting

--- iaso/migrations/0113_merge_20220117_1306.py
+++ iaso/migrations/0113_merge_20220117_1306.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('iaso', '0112_auto_20220110_0925'),
-        ('iaso', '0112_auto_20220110_1503'),
+        ("iaso", "0112_auto_20220110_0925"),
+        ("iaso", "0112_auto_20220110_1503"),
     ]
 
-    operations = [
-    ]
+    operations = []

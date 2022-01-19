b9050242f6904611a1d58006abf1df08de569fcc
Olivier Le Thanh Duong <olivier@lethanh.be>
2022-01-19 16:58:52 +0100

    not working since user before d is defined

--- plugins/polio/api.py
+++ plugins/polio/api.py
@@ -1030,7 +1030,6 @@ class LQASStatsViewSet(viewsets.ViewSet):
                     district = find_district(district_name, region_name, district_dict)
                     if not district:
                         district_long_name = "%s - %s" % (district_name, region_name)
-                        d["region_name"] = region_name
 
                         if district_long_name not in campaign_stats[campaign_name]["districts_not_found"]:
                             campaign_stats[campaign_name]["districts_not_found"].append(district_long_name)

82e77a0fe6e5a36bd1ca8527b1a809b9695445fb
Olivier Le Thanh Duong <olivier@lethanh.be>
2022-01-24 11:20:25 +0100

    Add as a snackbar

--- hat/assets/js/apps/Iaso/domains/instances/components/UserOrgUnitRestriction.js
+++ hat/assets/js/apps/Iaso/domains/instances/components/UserOrgUnitRestriction.js
@@ -2,6 +2,7 @@ import { useSafeIntl } from 'bluesquare-components';
 import React from 'react';
 import { Alert } from '@material-ui/lab';
 import { useCurrentUser } from 'Iaso/utils/usersUtils';
+import { Snackbar } from '@material-ui/core';
 import MESSAGES from '../messages';
 
 export const UserOrgUnitRestriction = () => {
@@ -12,9 +13,11 @@ export const UserOrgUnitRestriction = () => {
         return null;
     }
     return (
-        <Alert severity="info">
-            {formatMessage(MESSAGES.restricted_submissions_by_orgunits)}{' '}
-            {currentUser.org_units.map(ou => ou.name).join(', ')}
-        </Alert>
+        <Snackbar open>
+            <Alert severity="info">
+                {formatMessage(MESSAGES.restricted_submissions_by_orgunits)}{' '}
+                {currentUser.org_units.map(ou => ou.name).join(', ')}
+            </Alert>
+        </Snackbar>
     );
 };

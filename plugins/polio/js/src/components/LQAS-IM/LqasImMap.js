dde5989933cd533da4209184f3917661f308718e
Quang Son Le <qle@bluesquarehub.com>
2022-01-25 13:37:31 +0100

    POLIO-181 improve map loading spinner

--- plugins/polio/js/src/components/LQAS-IM/LqasImMap.js
+++ plugins/polio/js/src/components/LQAS-IM/LqasImMap.js
@@ -34,7 +34,10 @@ export const LqasImMap = ({
     const { formatMessage } = useSafeIntl();
     const [renderCount, setRenderCount] = useState(0);
     const { data, isLoading } = useConvertedLqasImData(type);
-    const { data: shapes = [] } = useGetGeoJson(countryId, 'DISTRICT');
+    const { data: shapes = [], isFetching: isFetchingGeoJson } = useGetGeoJson(
+        countryId,
+        'DISTRICT',
+    );
 
     const scope = findScope(selectedCampaign, campaigns, shapes);
 
@@ -84,55 +87,40 @@ export const LqasImMap = ({
 
     return (
         <>
-            {isLoading && <LoadingSpinner />}
-            {!isLoading && (
-                <>
-                    <Box position="relative">
-                        <Paper elevation={2}>
-                            <MapLegendContainer>
-                                <MapLegend
-                                    title={title}
-                                    legendItems={legendItems}
-                                    width="lg"
-                                />
-                                {/* {type !== 'lqas' && (
-                                    <AccordionMapLegend
-                                        title={MESSAGES.collectionStats}
-                                        noDataMsg={MESSAGES.noDataFound}
-                                        data={accordionItems}
-                                        defaultExpanded
-                                        width="lg"
-                                    />
-                                )} */}
-                            </MapLegendContainer>
-                            <MapComponent
-                                // Use the key to force render
-                                key={`LQASIMMap${round}${renderCount}-${type}`}
-                                name={`LQASIMMap${round}-${type}`}
-                                mainLayer={shapes}
-                                onSelectShape={() => null}
-                                getMainLayerStyle={getShapeStyles}
-                                tooltipLabels={{
-                                    main: 'District',
-                                    background: 'Region',
-                                }}
-                                makePopup={makePopup(
-                                    data,
-                                    round,
-                                    selectedCampaign,
-                                )}
-                                height={600}
-                            />
-                            {selectedCampaign && (
-                                <ScopeAndDNFDisclaimer
-                                    type={type}
-                                    campaign={selectedCampaign}
-                                />
-                            )}
-                        </Paper>
-                    </Box>
-                </>
-            )}
+            <Box position="relative">
+                <Paper elevation={2}>
+                    <MapLegendContainer>
+                        <MapLegend
+                            title={title}
+                            legendItems={legendItems}
+                            width="lg"
+                        />
+                    </MapLegendContainer>
+                    {(isLoading || isFetchingGeoJson) && (
+                        <LoadingSpinner fixed={false} />
+                    )}
+                    <MapComponent
+                        // Use the key to force render
+                        key={`LQASIMMap${round}${renderCount}-${type}`}
+                        name={`LQASIMMap${round}-${type}`}
+                        mainLayer={shapes}
+                        onSelectShape={() => null}
+                        getMainLayerStyle={getShapeStyles}
+                        tooltipLabels={{
+                            main: 'District',
+                            background: 'Region',
+                        }}
+                        makePopup={makePopup(data, round, selectedCampaign)}
+                        height={600}
+                    />
+                    {selectedCampaign && (
+                        <ScopeAndDNFDisclaimer
+                            type={type}
+                            campaign={selectedCampaign}
+                        />
+                    )}
+                </Paper>
+            </Box>
         </>
     );
 };

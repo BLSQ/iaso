71aef9fe6b834a680c24f6d87504e2af022b3113
Christophe Gerard <cgerard@bluesquarehub.com>
2022-02-04 16:51:41 +0100

    it's friday ...

--- hat/assets/js/cypress/integration/2 - forms/list.spec.js
+++ hat/assets/js/cypress/integration/2 - forms/list.spec.js
@@ -41,7 +41,7 @@ const goToPage = (
     }
 
     cy.intercept('GET', '/api/orgunittypes/**', {
-        fixture: 'orgUnittypes/list.json',
+        fixture: 'orgunittypes/list.json',
     });
     cy.intercept('GET', '/api/projects/**', {
         fixture: 'projects/list.json',

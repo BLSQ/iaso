8d91c6df59217538a007811f39b7600ce1bafc30
Christophe Gerard <cgerard@bluesquarehub.com>
2022-02-22 10:41:44 +0100

    improve tests

--- hat/assets/js/cypress/integration/8 - projects/list.spec.js
+++ hat/assets/js/cypress/integration/8 - projects/list.spec.js
@@ -5,6 +5,7 @@ import listfeatureFlags from '../../fixtures/featureflags/list.json';
 import superUser from '../../fixtures/profiles/me/superuser.json';
 
 const siteBaseUrl = Cypress.env('siteBaseUrl');
+let interceptFlagProjects = false;
 
 const baseUrl = `${siteBaseUrl}/dashboard/settings/projects`;
 
@@ -54,6 +55,63 @@ const openDialogForIndex = index => {
     editButton.click();
     cy.get('#project-dialog').should('be.visible');
 };
+
+const testRowContent = (index, p = listFixture.projects[index]) => {
+    cy.get('table').as('table');
+    cy.get('@table').find('tbody').find('tr').eq(index).as('row');
+    cy.get('@row').find('td').should('have.length', 4);
+    cy.get('@row').find('td').eq(0).should('contain', p.name);
+    cy.get('@row').find('td').eq(1).should('contain', p.app_id);
+    cy.get('@row').find('td').eq(2).as('ffCol');
+    p.feature_flags.forEach(ff => {
+        cy.get('@ffCol').should('contain', ff.name);
+    });
+};
+
+const testDialogContent = (p, ffIndexes, clearff = true) => {
+    cy.get('#input-text-name').clear().type(p.name);
+    cy.testInputValue('#input-text-name', p.name);
+    cy.get('#input-text-app_id').clear().type(p.app_id);
+    cy.testInputValue('#input-text-name', p.name);
+    cy.selectTab(1, '#project-dialog');
+    cy.fillMultiSelect('#feature_flags', ffIndexes, clearff);
+};
+
+const mockListCall = (keyName, body) => {
+    interceptFlagProjects = false;
+    cy.intercept(
+        {
+            method: 'GET',
+            pathname: '/api/projects',
+            query: defautlQuery,
+        },
+        req => {
+            interceptFlagProjects = true;
+            req.reply({
+                statusCode: 200,
+                body,
+            });
+        },
+    ).as(keyName);
+};
+const mockSaveCall = (method, i, pathname, p) => {
+    interceptFlag = false;
+    cy.intercept(
+        {
+            method,
+            pathname,
+        },
+        req => {
+            interceptFlag = true;
+            expect(req.body).to.deep.equal(p);
+            req.reply({
+                statusCode: 200,
+                body: listFixture.projects[i],
+            });
+        },
+    ).as('saveProject');
+};
+
 describe('Projects', () => {
     describe('Page', () => {
         it('should not be accessible if user does not have permission', () => {
@@ -71,11 +129,14 @@ describe('Projects', () => {
         it('should render results', () => {
             goToPage();
             cy.wait('@getProjects').then(() => {
-                table = cy.get('table');
-                table.should('have.length', 1);
-                const rows = table.find('tbody').find('tr');
-                rows.should('have.length', listFixture.projects.length);
-                rows.eq(0).find('td').should('have.length', 4);
+                cy.get('table').as('table');
+                cy.get('@table').should('have.length', 1);
+                cy.get('@table').find('tbody').find('tr').as('rows');
+                cy.get('@rows').should(
+                    'have.length',
+                    listFixture.projects.length,
+                );
+                testRowContent(0);
             });
         });
 
@@ -90,7 +151,7 @@ describe('Projects', () => {
         });
     });
 
-    describe('Dialog', () => {
+    describe.only('Dialog', () => {
         it('should display empty project dialog', () => {
             // this will be tested when creation will be enabled
             goToPage();
@@ -102,7 +163,7 @@ describe('Projects', () => {
                 cy.testInputValue('#input-text-app_id', '');
             });
         });
-        it('should display correct entity infos', () => {
+        it('should display correct project infos', () => {
             goToPage();
             cy.wait('@getProjects').then(() => {
                 const theIndex = 0;
@@ -118,72 +179,44 @@ describe('Projects', () => {
                 );
             });
         });
-        it('should call api list and api create', () => {
+        it('should create correctly', () => {
             goToPage();
             cy.wait('@getProjects').then(() => {
                 const theIndex = 0;
                 cy.get('#add-button-container').find('button').click();
-                interceptFlag = false;
                 const newProject = {
-                    name: 'superman',
-                    app_id: 'pacman',
+                    name: 'create',
+                    app_id: 'project',
                     feature_flags: [
-                        listfeatureFlags.featureflags[2],
-                        listfeatureFlags.featureflags[3],
+                        listfeatureFlags.featureflags[0],
+                        listfeatureFlags.featureflags[1],
                     ],
                 };
-                cy.get('#input-text-name').clear().type(newProject.name);
-                cy.testInputValue('#input-text-name', newProject.name);
-                cy.get('#input-text-app_id').clear().type(newProject.app_id);
-                cy.testInputValue('#input-text-name', newProject.name);
-                cy.selectTab(1, '#project-dialog');
-                cy.fillMultiSelect('#feature_flags', [2, 3], false);
-                cy.intercept(
-                    {
-                        method: 'POST',
-                        pathname: '/api/apps/',
-                    },
-                    req => {
-                        interceptFlag = true;
-                        expect(req.body).to.deep.equal(newProject);
-                        req.reply({
-                            statusCode: 200,
-                            body: listFixture.projects[theIndex],
-                        });
-                    },
-                ).as('saveProject');
+                const newList = {
+                    ...listFixture,
+                };
+                newList.projects.push(newProject);
 
-                let interceptFlagProjects = false;
-                cy.intercept(
-                    {
-                        method: 'GET',
-                        pathname: '/api/projects',
-                        query: defautlQuery,
-                    },
-                    req => {
-                        interceptFlagProjects = true;
-                        req.reply({
-                            statusCode: 200,
-                            body: listFixture.projects[theIndex],
-                        });
-                    },
-                ).as('getProjectsAfterCreate');
+                testDialogContent(newProject, [0, 1], false);
+                mockSaveCall('POST', theIndex, '/api/apps/', newProject);
+                mockListCall('getProjectsAfterCreate', newList);
+
                 cy.get('.MuiDialogActions-root').find('button').last().click();
                 cy.wait('@saveProject').then(() => {
                     cy.wrap(interceptFlag).should('eq', true);
                 });
                 cy.wait('@getProjectsAfterCreate').then(() => {
                     cy.wrap(interceptFlagProjects).should('eq', true);
+                    testRowContent(2, newProject);
                 });
             });
         });
 
-        it('should call api list and api save', () => {
+        it('should save correctly', () => {
             goToPage();
             cy.wait('@getProjects').then(() => {
                 const theIndex = 0;
                 openDialogForIndex(theIndex);
-                interceptFlag = false;
                 const newProject = {
                     id: listFixture.projects[theIndex].app_id,
                     name: 'superman',
@@ -194,48 +227,32 @@ describe('Projects', () => {
                     ],
                     old_app_id: listFixture.projects[theIndex].app_id,
                 };
-                cy.get('#input-text-name').clear().type(newProject.name);
-                cy.testInputValue('#input-text-name', newProject.name);
-                cy.get('#input-text-app_id').clear().type(newProject.app_id);
-                cy.testInputValue('#input-text-name', newProject.name);
-                cy.selectTab(1, '#project-dialog');
-                cy.fillMultiSelect('#feature_flags', [2, 3]);
-                cy.intercept(
-                    {
-                        method: 'PUT',
-                        pathname: `/api/apps/${listFixture.projects[theIndex].app_id}/`,
-                    },
-                    req => {
-                        interceptFlag = true;
-                        expect(req.body).to.deep.equal(newProject);
-                        req.reply({
-                            statusCode: 200,
-                            body: listFixture.projects[theIndex],
-                        });
-                    },
-                ).as('saveProject');
+                const newProjects = [...listFixture.projects];
+                newProjects[theIndex] = {
+                    ...listFixture.projects[theIndex],
+                    ...newProject,
+                };
+                const newList = {
+                    ...listFixture,
+                    projects: newProjects,
+                };
 
-                let interceptFlagProjects = false;
-                cy.intercept(
-                    {
-                        method: 'GET',
-                        pathname: '/api/projects',
-                        query: defautlQuery,
-                    },
-                    req => {
-                        interceptFlagProjects = true;
-                        req.reply({
-                            statusCode: 200,
-                            body: listFixture.projects[theIndex],
-                        });
-                    },
-                ).as('getProjectsAfterSave');
+                mockSaveCall(
+                    'PUT',
+                    theIndex,
+                    `/api/apps/${listFixture.projects[theIndex].app_id}/`,
+                    newProject,
+                );
+                testDialogContent(newProject, [2, 3]);
+                mockListCall('getProjectsAfterSave', newList);
+
                 cy.get('.MuiDialogActions-root').find('button').last().click();
                 cy.wait('@saveProject').then(() => {
                     cy.wrap(interceptFlag).should('eq', true);
                 });
                 cy.wait('@getProjectsAfterSave').then(() => {
                     cy.wrap(interceptFlagProjects).should('eq', true);
+                    testRowContent(0, newProject);
                 });
             });
         });

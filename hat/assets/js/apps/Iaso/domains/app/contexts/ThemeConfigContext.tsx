d18eb0da0490baad689f63e23f53b5640302e0db
Quang Son Le <qle@bluesquarehub.com>
2022-03-24 14:06:15 +0100

    POLIO-245: update context

--- hat/assets/js/apps/Iaso/domains/app/contexts/ThemeConfigContext.tsx
+++ hat/assets/js/apps/Iaso/domains/app/contexts/ThemeConfigContext.tsx
@@ -6,6 +6,8 @@ type ThemeConfig = {
     THEME_SECONDARY_COLOR: string;
     THEME_PRIMARY_BACKGROUND_COLOR: string;
     LOGO_PATH: string;
+    FAVICON_PATH: string;
+    SHOW_LOGO_WITH_NAME: 'yes' | 'no';
 };
 
 const defaultContextValue: ThemeConfig = {
@@ -14,6 +16,8 @@ const defaultContextValue: ThemeConfig = {
     THEME_SECONDARY_COLOR: '',
     THEME_PRIMARY_BACKGROUND_COLOR: '',
     LOGO_PATH: '',
+    FAVICON_PATH: '',
+    SHOW_LOGO_WITH_NAME: 'yes',
 };
 
 export const ThemeConfigContext =

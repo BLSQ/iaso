f3cf83c9e4bc99bcf047394a0290bb3b3ef0a6aa
Christophe Gerard <cgerard@bluesquarehub.com>
2022-03-25 13:55:26 +0100

    project id

--- hat/assets/js/apps/Iaso/domains/app/index.spec.js
+++ hat/assets/js/apps/Iaso/domains/app/index.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import App from './index';
 
-describe('App', () => {
+describe.only('App', () => {
     it('render properly', () => {
         const wrapper = shallow(
             <App

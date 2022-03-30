cda280b1218c4512dec6eb8782bdad70b63189aa
Quang Son Le <38907762+quang-le@users.noreply.github.com>
2022-03-30 17:34:26 +0200

    IA-1158 copy version to different source (#706)
    * POLIO-257: display dates used for calculation
    
    * POLIO-257: align LqasImMapHeader dates
    
    * POLIO-257: add tooltip, styling to lqas-im dates
    
    * POLIO-257: dry up code
    
    - create LqasImDates component
    
    * IA-1158: add missing API options
    
    - add ui fields for destination source, destination version and force
    -  copy_version manages empty source and empty version separately
    - update request to pass additional params
    
    * IA-1158 warning and description message
    
    - move CopyVersion and related components to dedicated folder
    - Added dedicated component for messages
    
    * IA-1158: change destination version to select box
    
    * IA-1158: add translation key
    
    * IA-1158: show next version number in warning message
    
    * IA-1158: center warning message
    
    * IA-1158: add onCancel
    
    * IA-1158: add translations, cleanup comments
    
    * IA-1158: polish UI
    
    - adapt version number when changing datasource with default version nr
    - use datasourceName in warning when destinationSourceName is null
    - Add code for error management (nor working though)
    
    * POLIO-255: make enddate inclusive
    
    * IA-1158: add error state
    
    - block confirmation if forec overwrite not selected for existing destination
    - add error state on select box
    - add translations
    - clean up code
    
    * IA-1158: remove force overwrite option
    
    * IA-1158: disable confirm button if source empty
    
    * IA-1158: light refactor

---
+++ hat/assets/js/apps/Iaso/domains/dataSources/components/CopySourceVersion/CopyVersionWarnings.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { FunctionComponent } from 'react';
+import { Grid, Typography, makeStyles } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
+import MESSAGES from '../../messages';
+import { useDataSourceAsDropDown } from '../../requests';
+
+type Props = {
+    dataSourceName: string;
+    dataSourceVersionNumber: string | number | null;
+    destinationSourceId: string | number | null;
+    destinationVersionNumber: string | number | null;
+};
+
+const warningStyle = {
+    source: { fontWeight: 'bold' },
+    destination: { fontWeight: 'bold' },
+};
+
+const useWarningStyles = makeStyles(warningStyle);
+
+export const WarningMessage: FunctionComponent<Props> = ({
+    dataSourceName,
+    dataSourceVersionNumber,
+    destinationSourceId,
+    destinationVersionNumber,
+}) => {
+    const { data: dataSources } = useDataSourceAsDropDown();
+    const classes = useWarningStyles();
+    const destinationSourceName = destinationSourceId
+        ? dataSources.filter(
+              dataSource => dataSource.value === destinationSourceId,
+          )[0]?.label
+        : null;
+    return (
+        <Grid container item spacing={1} justifyContent="center">
+            <Grid item>
+                <Typography className={classes.source} variant="h6">
+                    <FormattedMessage
+                        {...MESSAGES.copiedVersion}
+                        values={{
+                            sourceName: dataSourceName,
+                            versionNumber: dataSourceVersionNumber,
+                        }}
+                    />
+                </Typography>
+            </Grid>
+            <Grid item>
+                <Typography variant="h6">
+                    <FormattedMessage {...MESSAGES.willBeCopied} />
+                </Typography>
+            </Grid>
+            <Grid item>
+                <Typography className={classes.destination} variant="h6">
+                    <FormattedMessage
+                        {...MESSAGES.copyToSourceWithVersion}
+                        values={{
+                            sourceName: destinationSourceName ?? dataSourceName,
+                            versionNumber: destinationVersionNumber,
+                        }}
+                    />{' '}
+                </Typography>
+            </Grid>
+        </Grid>
+    );
+};

ef285b9b0d1e9aff275fa3285f8ebb4786309460
SimonDus <45785235+SimonDus@users.noreply.github.com>
2022-03-31 12:14:57 +0200

    IA-1113 Read only perm on submissions (#725)
    * IA-1113 WIP add write perm to enkteto instances update
    
    * IA-1113 WIP
    
    * IA-1113 Add write permission to instances
    
    * IA-1113: remove enketo action in submission detail
    
    Co-authored-by: Simon Dusausoy <sdusausoy@bluesquarehub.com>
    Co-authored-by: Quang Son Le <qle@bluesquarehub.com>

--- iaso/tests/api/test_enketo.py
+++ iaso/tests/api/test_enketo.py
@@ -33,7 +33,10 @@ class EnketoAPITestCase(APITestCase):
         star_wars.default_version = sw_version
         star_wars.save()
 
-        cls.yoda = cls.create_user_with_profile(username="yoda", account=star_wars, permissions=["iaso_forms"])
+        cls.yoda = cls.create_user_with_profile(
+            username="yoda", account=star_wars, permissions=["iaso_forms", "iaso_update_submission"]
+        )
+        cls.gunther = cls.create_user_with_profile(username="gunther", account=star_wars, permissions=["iaso_forms"])
 
         cls.jedi_council = m.OrgUnitType.objects.create(name="Jedi Council", short_name="Cnc")
 
@@ -88,6 +91,7 @@ class EnketoAPITestCase(APITestCase):
     def test_when_authenticated_edit_url_should_return_an_enketo_url(self):
         """GET /api/enketo/edit/{uuid}/"""
         instance = self.form_1.instances.first()
+
         self.client.force_authenticate(self.yoda)
         enketo_contents = []
 
@@ -111,6 +115,29 @@ class EnketoAPITestCase(APITestCase):
             enketo_contents[0],
             f'form_id={instance.uuid}&server_url=http://testserver/api/enketo&instance=<PLAN_INT_CAR_FOSA+xmlns:h="http://www.w3.org/1999/xhtml"+xmlns:jr="http://openrosa.org/javarosa"+xmlns:xsd="http://www.w3.org/2001/XMLSchema"+xmlns:ev="http://www.w3.org/2001/xml-events"+xmlns:orx="http://openrosa.org/xforms"+xmlns:odk="http://www.opendatakit.org/xforms"+id="PLAN_CAR_FOSA"+version="1"+iasoInstance="{instance.id}"><formhub><uuid>385689b3b55f4739b80dcba5540c5f87</uuid></formhub><start>2019-12-02T14:07:52.465+01:00</start><end>2019-12-02T14:10:11.380+01:00</end><today>2019-12-02</today><deviceid>358544083104930</deviceid><subscriberid>206300001285696</subscriberid><imei>358544083104930</imei><simserial>8932030000106638166</simserial><phonenumber/><user_name>Tttt</user_name><region>UnCrC8p12UN</region><prefecture>IJoQdfGfYsC</prefecture><district>tSs16aZvMD4</district><sous-prefecture>drMs7e3pDFZ</sous-prefecture><fosa>FeNjVewpswJ</fosa><year>2019</year><quarter>1</quarter><Ident_type_structure>ce</Ident_type_structure><Ident_type_services>serv_prot</Ident_type_services><Ident_type_serv_medical>0</Ident_type_serv_medical><Ident_type_serv_protect>1</Ident_type_serv_protect><Ident_type_serv_jurid>0</Ident_type_serv_jurid><Ident_type_serv_psycho>0</Ident_type_serv_psycho><Ident_type_serv_educ>0</Ident_type_serv_educ><Ident_type_serv_recope>0</Ident_type_serv_recope><Ident_type_serv_club>0</Ident_type_serv_club><Ident_statut>ong</Ident_statut><Ident_eau_courante>1</Ident_eau_courante><Ident_electricite>0</Ident_electricite><Ident_nom_responsable>Chggh</Ident_nom_responsable><Ident_telephone>256</Ident_telephone><fermeture_structure>sam</fermeture_structure><Ident_ferm_lundi>0</Ident_ferm_lundi><Ident_ferm_mardi>0</Ident_ferm_mardi><Ident_ferm_mercredi>0</Ident_ferm_mercredi><Ident_ferm_jeudi>0</Ident_ferm_jeudi><Ident_ferm_vendredi>0</Ident_ferm_vendredi><Ident_ferm_samedi>1</Ident_ferm_samedi><Ident_ferm_dim>0</Ident_ferm_dim><Ident_ferm_aucun>0</Ident_ferm_aucun><Ident_serv_cout>0</Ident_serv_cout><Ident_type_batiment>sem_dur</Ident_type_batiment><imgUrl>1575292156137.jpg</imgUrl><gps>50.8367386+4.40093901+123.56201171875+49.312</gps><meta><instanceID>uuid:7ff9b3b4-9404-4702-bbe4-efe2407aef02</instanceID><editUserID>{self.yoda.id}</editUserID></meta></PLAN_INT_CAR_FOSA>&instance_id=7ff9b3b4-9404-4702-bbe4-efe2407aef02&return_url=http://testserver/api/enketo/edit/uuid-1/',
         )
+
+    @override_settings(ENKETO=enketo_test_settings)
+    @responses.activate
+    def test_unauthorized_user_cant_edit_submission(self):
+        """GET /api/enketo/edit/{uuid}/"""
+        instance = self.form_1.instances.first()
+        self.client.force_authenticate(self.gunther)
+        enketo_contents = []
+
+        def request_callback(request):
+            enketo_contents.append(urllib.parse.unquote(request.body))
+            return (200, {}, json.dumps({"edit_url": "https://enketo_url.host.test/something"}))
+
+        responses.add_callback(
+            responses.POST,
+            "https://enketo_url.host.test/api_v2/instance",
+            callback=request_callback,
+            content_type="application/json",
+        )
+
+        response = self.client.get(f"/api/enketo/edit/{instance.uuid}/")
+
+        self.assertEqual(response.status_code, 403)
 
     def test_when_anonymous_head_submission_should_work(self):
         instance = self.form_1.instances.first()
@@ -141,7 +168,6 @@ class EnketoAPITestCase(APITestCase):
         self.assertEquals(response.content.decode("utf-8"), expected_list)
 
     def test_when_anonymous_get_formList_should_work_2(self):
-
         with open("iaso/tests/fixtures/hydroponics_test_upload_modified.xml") as modified_xml:
             instance = self.form_1.instances.first()
             f = SimpleUploadedFile(

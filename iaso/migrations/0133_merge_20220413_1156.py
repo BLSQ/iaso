247a003de74494a8a8bf1dec10883d29012055c4
Simon Dusausoy <sdusausoy@bluesquarehub.com>
2022-04-13 14:00:57 +0200

    IA-933 Black formatting

--- iaso/migrations/0133_merge_20220413_1156.py
+++ iaso/migrations/0133_merge_20220413_1156.py
@@ -6,10 +6,9 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('iaso', '0130_merge_20220401_1428'),
-        ('iaso', '0130_merge_20220410_1802'),
-        ('iaso', '0132_merge_20220412_1136'),
+        ("iaso", "0130_merge_20220401_1428"),
+        ("iaso", "0130_merge_20220410_1802"),
+        ("iaso", "0132_merge_20220412_1136"),
     ]
 
-    operations = [
-    ]
+    operations = []

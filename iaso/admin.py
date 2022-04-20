4cc7b5b3f067a9bcd7bf55766eafb841e7c45641
HAKIZIMANA Franck <hakifranckmana@gmail.com>
2022-04-20 09:27:45 +0200

    fill the instance reference id, instead of loading all instance in select box

--- iaso/admin.py
+++ iaso/admin.py
@@ -44,7 +44,7 @@ from .models import (
 
 
 class OrgUnitAdmin(admin.GeoModelAdmin):
-    raw_id_fields = ("parent",)
+    raw_id_fields = ("parent", "instance_defining")
     list_filter = ("org_unit_type", "custom", "validated", "sub_source", "version")
     search_fields = ("name", "source_ref", "uuid")
     readonly_fields = ("path",)

80ae9aeb7ae080de1b57ded61ec0a86b20357200
Quang Son Le <qle@bluesquarehub.com>
2022-04-29 17:12:32 +0200

    IA-1285: add FilterButton, SearchButton

---
+++ hat/assets/js/apps/Iaso/components/FilterButton.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { FunctionComponent } from 'react';
+import FiltersIcon from '@material-ui/icons/FilterList';
+import { Button, Box } from '@material-ui/core';
+import { FormattedMessage, defineMessages } from 'react-intl';
+
+const MESSAGES = defineMessages({
+    filter: {
+        defaultMessage: 'Filter',
+        id: 'iaso.label.filter',
+    },
+});
+
+type Props = {
+    disabled: boolean;
+    onFilter: () => void;
+};
+
+export const FilterButton: FunctionComponent<Props> = ({
+    disabled,
+    onFilter,
+}) => {
+    return (
+        <Button
+            disabled={disabled}
+            variant="contained"
+            color="primary"
+            onClick={onFilter}
+        >
+            <Box mr={1} top={3} position="relative">
+                <FiltersIcon />
+            </Box>
+            <FormattedMessage {...MESSAGES.filter} />
+        </Button>
+    );
+};

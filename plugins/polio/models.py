b8a27722bfda574998aa54c0a85cd2815ea63998
Olivier Le Thanh Duong <olivier@lethanh.be>
2022-05-02 14:39:42 +0200

    set default order for Round

--- plugins/polio/models.py
+++ plugins/polio/models.py
@@ -80,6 +80,9 @@ ROUNDSTATUS = [
 
 
 class Round(models.Model):
+    class Meta:
+        ordering = ["number", "started_at"]
+
     started_at = models.DateField(null=True, blank=True)
     number = models.IntegerField(null=True, blank=True)
     campaign = models.ForeignKey("Campaign", related_name="rounds", on_delete=models.PROTECT, null=True)

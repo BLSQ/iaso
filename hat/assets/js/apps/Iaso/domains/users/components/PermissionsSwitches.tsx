71a377a75c34accd321839e03eb17df4e3d2351a
Olivier Le Thanh Duong <olivier@lethanh.be>
2022-05-27 11:06:57 +0200

    Move Permissions switch to FunctionComponent and react-query

--- hat/assets/js/apps/Iaso/domains/users/components/PermissionsSwitches.tsx
+++ hat/assets/js/apps/Iaso/domains/users/components/PermissionsSwitches.tsx
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { injectIntl } from 'react-intl';
+import React from 'react';
 import {
-    withStyles,
     FormControlLabel,
     Switch,
     Typography,
+    makeStyles,
 } from '@material-ui/core';
 
-import { fetchPermissions as fetchPermissionsAction } from '../actions';
+import { useSafeIntl, LoadingSpinner } from 'bluesquare-components';
 
 import MESSAGES from '../messages';
+import { useSnackQuery } from '../../../libs/apiHooks';
+import { getRequest } from '../../../libs/Api';
+import snackMessages from '../../../components/snackBars/messages';
 
 const styles = theme => ({
     admin: {
@@ -20,16 +19,29 @@ const styles = theme => ({
     },
 });
 
-class PermissionsSwitches extends Component {
-    componentDidMount() {
-        const { fetchPermissions, permissions } = this.props;
-        if (permissions.length === 0) {
-            fetchPermissions();
-        }
-    }
+const useStyles = makeStyles(styles);
 
-    setPermissions(codeName, isChecked) {
-        const { currentUser, handleChange } = this.props;
+type Props = {
+    isSuperUser?: boolean;
+    currentUser: any;
+    // eslint-disable-next-line no-unused-vars
+    handleChange: (newValue: any) => void;
+};
+
+const PermissionsSwitches: React.FunctionComponent<Props> = ({
+    isSuperUser,
+    currentUser,
+    handleChange,
+}) => {
+    const { formatMessage } = useSafeIntl();
+    const classes = useStyles();
+    const { data, isLoading } = useSnackQuery(
+        ['permissions'],
+        () => getRequest('/api/permissions/'),
+        snackMessages.fetchPermissions,
+    );
+
+    const setPermissions = (codeName: string, isChecked: boolean) => {
         const newUserPerms = [...currentUser.permissions.value];
         if (!isChecked) {
             const permIndex = newUserPerms.indexOf(codeName);
@@ -38,97 +50,68 @@ class PermissionsSwitches extends Component {
             newUserPerms.push(codeName);
         }
         handleChange(newUserPerms);
-    }
+    };
 
     // We need the intl object inside the component to be able to perform sort the strings
     // as <FormattedMessage/> returns a span, not a string
-    permissionLabel(permissionCodeName) {
-        const { intl } = this.props;
+    const permissionLabel = permissionCodeName => {
         return MESSAGES[permissionCodeName]
-            ? intl.formatMessage(MESSAGES[permissionCodeName])
+            ? formatMessage(MESSAGES[permissionCodeName])
             : permissionCodeName;
-    }
+    };
+    const permissions = data?.permissions ?? [];
 
-    render() {
-        const { classes, permissions, currentUser, isSuperUser } = this.props;
-        return (
-            <>
-                {isSuperUser && (
-                    <Typography
-                        id="superuser-permission-message"
-                        variant="body1"
-                        className={classes.admin}
-                    >
-                        {this.props.intl.formatMessage(MESSAGES.isSuperUser)}
-                    </Typography>
-                )}
-                {!isSuperUser &&
-                    permissions
-                        .sort((a, b) =>
-                            this.permissionLabel(a.codename).localeCompare(
-                                this.permissionLabel(b.codename),
-                            ),
-                        )
-                        .map(p => (
-                            <div key={p.id}>
-                                <FormControlLabel
-                                    control={
-                                        <Switch
-                                            className="permission-checkbox"
-                                            id={`permission-checkbox-${p.codename}`}
-                                            checked={Boolean(
-                                                currentUser.permissions.value.find(
-                                                    up => up === p.codename,
-                                                ),
-                                            )}
-                                            onChange={e =>
-                                                this.setPermissions(
-                                                    p.codename,
-                                                    e.target.checked,
-                                                )
-                                            }
-                                            name={p.codename}
-                                            color="primary"
-                                        />
-                                    }
-                                    label={this.permissionLabel(p.codename)}
-                                />
-                            </div>
-                        ))}
-            </>
-        );
-    }
-}
+    return (
+        <>
+            {isLoading && <LoadingSpinner />}
+            {isSuperUser && (
+                <Typography
+                    id="superuser-permission-message"
+                    variant="body1"
+                    className={classes.admin}
+                >
+                    {formatMessage(MESSAGES.isSuperUser)}
+                </Typography>
+            )}
+            {!isSuperUser &&
+                permissions
+                    .sort((a, b) =>
+                        permissionLabel(a.codename).localeCompare(
+                            permissionLabel(b.codename),
+                        ),
+                    )
+                    .map(p => (
+                        <div key={p.id}>
+                            <FormControlLabel
+                                control={
+                                    <Switch
+                                        className="permission-checkbox"
+                                        id={`permission-checkbox-${p.codename}`}
+                                        checked={Boolean(
+                                            currentUser.permissions.value.find(
+                                                up => up === p.codename,
+                                            ),
+                                        )}
+                                        onChange={e =>
+                                            setPermissions(
+                                                p.codename,
+                                                e.target.checked,
+                                            )
+                                        }
+                                        name={p.codename}
+                                        color="primary"
+                                    />
+                                }
+                                label={permissionLabel(p.codename)}
+                            />
+                        </div>
+                    ))}
+        </>
+    );
+};
 
 PermissionsSwitches.defaultProps = {
     isSuperUser: false,
 };
 
-PermissionsSwitches.propTypes = {
-    classes: PropTypes.object.isRequired,
-    fetchPermissions: PropTypes.func.isRequired,
-    permissions: PropTypes.array.isRequired,
-    currentUser: PropTypes.object.isRequired,
-    handleChange: PropTypes.func.isRequired,
-    isSuperUser: PropTypes.bool,
-    intl: PropTypes.object.isRequired,
-};
-
-const MapStateToProps = state => ({
-    permissions: state.users.permissions,
-});
-
-const mapDispatchToProps = dispatch => ({
-    ...bindActionCreators(
-        {
-            fetchPermissions: fetchPermissionsAction,
-        },
-        dispatch,
-    ),
-});
-
-export default injectIntl(
-    withStyles(styles)(
-        connect(MapStateToProps, mapDispatchToProps)(PermissionsSwitches),
-    ),
-);
+export default PermissionsSwitches;

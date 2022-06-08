e9aeaa3844e3f328207fef973a988d05e3b698aa
Quang Son Le <qle@bluesquarehub.com>
2022-06-08 15:45:36 +0200

    POLIO-360 fix navigation from details screen

--- plugins/polio/js/src/pages/Budget/BudgetDetails.tsx
+++ plugins/polio/js/src/pages/Budget/BudgetDetails.tsx
@@ -5,20 +5,22 @@ import {
     // @ts-ignore
     useSkipEffectOnMount,
 } from 'bluesquare-components';
-import { Box, Grid, Paper } from '@material-ui/core';
+import { Box, Divider, Grid, Paper } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
 import TopBar from '../../../../../../hat/assets/js/apps/Iaso/components/nav/TopBarComponent';
 import MESSAGES from '../../constants/messages';
 import { convertObjectToString } from '../../utils';
 import { useStyles } from '../../styles/theme';
 import { TableWithDeepLink } from '../../../../../../hat/assets/js/apps/Iaso/components/tables/TableWithDeepLink';
 import { useGetBudgetDetails } from '../../hooks/useGetBudgetDetails';
-import { BUDGET_DETAILS } from '../../constants/routes';
+import { BUDGET, BUDGET_DETAILS } from '../../constants/routes';
 import { useBudgetDetailsColumns } from './config';
 import { useGetTeams } from '../../hooks/useGetTeams';
 import { useGetProfiles } from '../../components/CountryNotificationsConfig/requests';
 import { GraphTitle } from '../../components/LQAS-IM/GraphTitle';
 import { BudgetStatus } from './BudgetStatus';
 import { CreateBudgetEvent } from './CreateBudgetEvent';
+import { redirectToReplace } from '../../../../../../hat/assets/js/apps/Iaso/routing/actions';
 
 type Props = {
     router: any;
@@ -29,6 +31,9 @@ export const BudgetDetails: FunctionComponent<Props> = ({ router }) => {
     const classes = useStyles();
     const { campaignName, campaignId, ...apiParams } = router.params;
     const { formatMessage } = useSafeIntl();
+    // @ts-ignore
+    const prevPathname = useSelector(state => state.routerCustom.prevPathname);
+    const dispatch = useDispatch();
 
     const { data: budgetDetails, isFetching } = useGetBudgetDetails({
         ...apiParams,
@@ -55,14 +60,22 @@ export const BudgetDetails: FunctionComponent<Props> = ({ router }) => {
             <TopBar
                 title={formatMessage(MESSAGES.budgetDetails, { campaignName })}
                 displayBackButton
-                goBack={() => router.goBack()}
+                goBack={() => {
+                    if (prevPathname) {
+                        router.goBack();
+                    } else {
+                        dispatch(redirectToReplace(BUDGET, {}));
+                    }
+                }}
             />
             {/* @ts-ignore */}
             <Box className={classes.containerFullHeightNoTabPadded}>
                 <Box mb={4} ml={2} mr={2}>
                     <Grid container justifyContent="space-between">
                         <Grid container item xs={6}>
-                            <BudgetStatus budgetDetails={budgetDetails} />
+                            {!isFetching && (
+                                <BudgetStatus budgetDetails={budgetDetails} />
+                            )}
                         </Grid>
                         <Grid>
                             <CreateBudgetEvent campaignId={campaignId} />
@@ -75,6 +88,9 @@ export const BudgetDetails: FunctionComponent<Props> = ({ router }) => {
                             text={formatMessage(MESSAGES.steps)}
                             displayTrigger
                         />
+                        <Box mt={2} mb={1}>
+                            <Divider />
+                        </Box>
                         <TableWithDeepLink
                             data={budgetDetails?.results ?? []}
                             count={budgetDetails?.count}

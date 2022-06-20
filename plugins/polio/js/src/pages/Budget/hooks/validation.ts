76045bdb3e12ecc3a0d94a0665da5483bbcb7f25
Quang Son Le <qle@bluesquarehub.com>
2022-06-20 15:41:47 +0200

    restore budget form validation

--- plugins/polio/js/src/pages/Budget/hooks/validation.ts
+++ plugins/polio/js/src/pages/Budget/hooks/validation.ts
@@ -1,33 +1,30 @@
 import { useMemo } from 'react';
 // @ts-ignore
 import { useSafeIntl } from 'bluesquare-components';
-import { array, mixed, object, string } from 'yup';
+import { array, mixed, object, string, bool } from 'yup';
 import MESSAGES from '../../../constants/messages';
-// import { emailRegex } from '../../../../../../../hat/assets/js/apps/Iaso/libs/utils';
+import {
+    makeRegexValidator,
+    urlRegex,
+} from '../../../../../../../hat/assets/js/apps/Iaso/libs/utils';
 
-// const multipleEmailsValidator = (message: string) => ({
-//     name: 'multiple email validation',
-//     message,
-//     test: (value: string) => {
-//         const emails = value.split(', ');
-//         for (let i = 0; i < emails.length; i += 1) {
-//             if (!emails[i].match(emailRegex)) {
-//                 return false;
-//             }
-//         }
-//         return true;
-//     },
-// });
+const multipleUrlsValidator = makeRegexValidator(urlRegex);
 
 export const useBudgetEvenValidation = () => {
     const { formatMessage } = useSafeIntl();
     const fieldRequired = formatMessage(MESSAGES.requiredField);
-    // const emailFormat = formatMessage(MESSAGES.emailFormat);
+    const urlFormat = formatMessage(MESSAGES.urlFormat);
     const typeError = formatMessage(MESSAGES.budgetTypeError);
     return useMemo(() => {
         return object().shape({
             file: mixed().nullable(),
+            links: string()
+                .nullable()
+                .test(
+                    multipleUrlsValidator(urlFormat, 'multiple url validation'),
+                ),
             comments: string().nullable(),
+            internal: bool().nullable().required(fieldRequired),
             target_teams: array()
                 .min(1, fieldRequired)
                 .nullable()
@@ -38,5 +35,5 @@ export const useBudgetEvenValidation = () => {
                 .required(fieldRequired)
                 .typeError(typeError),
         });
-    }, [fieldRequired, typeError]);
+    }, [fieldRequired, typeError, urlFormat]);
 };

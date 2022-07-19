234e8374831aa7be17f6d6aca10d142469fc209c
axel <tavormina.axel@gmail.com>
2022-07-19 17:33:38 +0200

    refactor hooks to get the right data

--- hat/assets/js/apps/Iaso/domains/instances/compare/hooks/useGetInstanceLogs.ts
+++ hat/assets/js/apps/Iaso/domains/instances/compare/hooks/useGetInstanceLogs.ts
@@ -3,7 +3,12 @@ import moment from 'moment';
 import { UseQueryResult } from 'react-query';
 import { getRequest } from '../../../../libs/Api';
 import { useSnackQuery } from '../../../../libs/apiHooks';
-import { Instance, InstanceLog, InstanceLogData } from '../../types/instance';
+import {
+    Instance,
+    InstanceLog,
+    InstanceLogData,
+    FormDescriptor,
+} from '../../types/instance';
 
 const getInstanceLog = (instanceId: string | undefined): Promise<Instance> => {
     return getRequest(`/api/logs/?objectId=${instanceId}&order=-created_at`);
@@ -12,14 +17,14 @@ const getInstanceLog = (instanceId: string | undefined): Promise<Instance> => {
 const getInstanceLogDetail = (
     logId: string | undefined,
 ): Promise<InstanceLogData> => {
-    console.log('logId', logId);
     return getRequest(`/api/logs/${logId}`);
 };
 
-// const getFormVersion = versionId => {
-//     console.log('version id', versionId);
-//     return getRequest(`/api/formVersions/${versionId}/?fields=descriptor`);
-// };
+const getFormId = formId => {
+    // TO DO : add versionId in parameters waiting change in backend to filter on version_id
+    // TO DO : replace call api below by : /api/formversions/?version_id=versionId&form_id=formID
+    return getRequest(`/api/formversions/${formId}/?fields=descriptor`);
+};
 
 export const useGetInstanceLogs = (
     instanceId: string | undefined,
@@ -59,8 +64,9 @@ export const useGetInstanceLogDetail = (
             enabled: Boolean(logId),
             select: (data: InstanceLog | undefined) => {
                 if (data) {
-                    data = data.new_value[0].json;
-                    return data;
+                    const instanceLogData = data.new_value[0].fields;
+
+                    return instanceLogData;
                 }
 
                 return undefined;
@@ -69,24 +75,20 @@ export const useGetInstanceLogDetail = (
     );
 };
 
-// export const useGetFormVersion = (
-//     formVersionId: string | undefined,
-// ): UseQueryResult<FormDescritor, Error> => {
-//     const queryKey: any[] = ['instanceLogDetail', formVersionId];
-//     // @ts-ignore
-//     return useSnackQuery(
-//         queryKey,
-//         () => getFormVersion(formVersionId),
-//         undefined,
-//         {
-//             enabled: Boolean(formVersionId),
-//             select: (data: FormDescriptor | undefined) => {
-//                 if (data) {
-//                     return data;
-//                 }
+export const useGetFormDescriptor = (
+    formId: number | undefined,
+): UseQueryResult<FormDescriptor, Error> => {
+    const queryKey: any[] = ['instanceDescriptor', formId];
+    // @ts-ignore
+    return useSnackQuery(queryKey, () => getFormId(formId), undefined, {
+        enabled: Boolean(formId),
+        select: (data: FormDescriptor | undefined) => {
+            if (data) {
+                data = data.descriptor;
+                return data;
+            }
 
-//                 return undefined;
-//             },
-//         },
-//     );
-// };
+            return undefined;
+        },
+    });
+};

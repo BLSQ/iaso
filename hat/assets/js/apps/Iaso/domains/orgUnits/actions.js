d50cabd80d89399a2aa4601c4b762851cacff0ab
Christophe Gerard <c@eduz.be>
2022-07-22 17:45:25 +0200

    remove unused reducers and actions

--- hat/assets/js/apps/Iaso/domains/orgUnits/actions.js
+++ hat/assets/js/apps/Iaso/domains/orgUnits/actions.js
@@ -1,37 +1,8 @@
-export const SET_ORG_UNITS = 'SET_ORG_UNITS';
-export const SET_ORG_UNITS_LOCATIONS = 'SET_ORG_UNITS_LOCATIONS';
 export const RESET_ORG_UNITS = 'RESET_ORG_UNITS';
 export const SET_SUB_ORG_UNIT = 'SET_SUB_ORG_UNIT';
-export const SET_FETCHING = 'SET_FETCHING';
 export const SET_ORG_UNIT_TYPES = 'SET_ORG_UNIT_TYPES';
 export const SET_SOURCES = 'SET_SOURCES';
-export const SET_ORG_UNITS_LIST_FETCHING = 'SET_ORG_UNITS_LIST_FETCHING';
 export const SET_FETCHING_ORG_UNITS_TYPES = 'SET_FETCHING_ORG_UNITS_TYPES';
-export const SET_FILTERS_UPDATED = 'SET_FILTERS_UPDATED';
-
-export const setOrgUnits = (
-    list,
-    showPagination,
-    params,
-    count,
-    pages,
-    counts,
-) => ({
-    type: SET_ORG_UNITS,
-    payload: {
-        list,
-        showPagination,
-        params,
-        count,
-        pages,
-        counts,
-    },
-});
-
-export const setOrgUnitsLocations = orgUnitsLocations => ({
-    type: SET_ORG_UNITS_LOCATIONS,
-    payload: orgUnitsLocations,
-});
 
 export const resetOrgUnits = () => ({
     type: RESET_ORG_UNITS,
@@ -52,22 +23,7 @@ export const setSources = sources => ({
     payload: sources,
 });
 
-export const setOrgUnitsListFetching = currentSubOrgUnit => ({
-    type: SET_ORG_UNITS_LIST_FETCHING,
-    payload: currentSubOrgUnit,
-});
-
-export const setFetching = fetching => ({
-    type: SET_FETCHING,
-    payload: fetching,
-});
-
 export const setFetchingOrgUnitTypes = fetching => ({
     type: SET_FETCHING_ORG_UNITS_TYPES,
     payload: fetching,
-});
-
-export const setFiltersUpdated = filtersUpdated => ({
-    type: SET_FILTERS_UPDATED,
-    payload: filtersUpdated,
 });
--- hat/assets/js/apps/Iaso/domains/orgUnits/reducer.js
+++ hat/assets/js/apps/Iaso/domains/orgUnits/reducer.js
@@ -1,57 +1,23 @@
 import {
-    SET_ORG_UNITS,
-    SET_ORG_UNITS_LOCATIONS,
     RESET_ORG_UNITS,
-    SET_FETCHING,
     SET_ORG_UNIT_TYPES,
     SET_SOURCES,
-    SET_ORG_UNITS_LIST_FETCHING,
     SET_FETCHING_ORG_UNITS_TYPES,
-    SET_FILTERS_UPDATED,
     SET_SUB_ORG_UNIT,
 } from './actions';
 
 export const orgUnitsInitialState = {
     currentSubOrgUnit: null,
-    fetchingList: false,
     fetchingOrgUnitTypes: true,
-    orgUnitsPage: {
-        list: [],
-        showPagination: false,
-        params: {},
-        count: 0,
-        pages: 0,
-        counts: [],
-    },
     orgUnitTypes: [],
     sources: null,
     orgUnitLevel: [],
-    orgUnitsLocations: {
-        locations: [],
-        shapes: [],
-    },
     filtersUpdated: false,
     groups: [],
 };
 
 export const orgUnitsReducer = (state = orgUnitsInitialState, action = {}) => {
     switch (action.type) {
-        case SET_ORG_UNITS: {
-            const { list, showPagination, params, count, pages, counts } =
-                action.payload;
-            return {
-                ...state,
-                orgUnitsPage: {
-                    list,
-                    showPagination,
-                    params,
-                    count,
-                    pages,
-                    counts,
-                },
-            };
-        }
-
         case SET_SUB_ORG_UNIT: {
             const currentSubOrgUnit = action.payload;
             return { ...state, currentSubOrgUnit };
@@ -74,30 +40,11 @@ export const orgUnitsReducer = (state = orgUnitsInitialState, action = {}) => {
             };
         }
 
-        case SET_ORG_UNITS_LIST_FETCHING: {
-            const fetchingList = action.payload;
-            return { ...state, fetchingList };
-        }
-
-        case SET_FETCHING: {
-            const fetchingSubOrgUnits = action.payload;
-            return { ...state, fetchingSubOrgUnits };
-        }
-
-        case SET_ORG_UNITS_LOCATIONS: {
-            const orgUnitsLocations = action.payload;
-            return { ...state, orgUnitsLocations };
-        }
-
         case SET_FETCHING_ORG_UNITS_TYPES: {
             const fetchingOrgUnitTypes = action.payload;
             return { ...state, fetchingOrgUnitTypes };
         }
 
-        case SET_FILTERS_UPDATED: {
-            const filtersUpdated = action.payload;
-            return { ...state, filtersUpdated };
-        }
         default:
             return state;
     }

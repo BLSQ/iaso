67da50b469585c5b0e5965f8c3fdb5e36bbe0482
axel <tavormina.axel@gmail.com>
2022-08-11 17:50:33 +0200

    create new Filters component for forms

---
+++ hat/assets/js/apps/Iaso/domains/forms/components/Filters.tsx
@@ -0,0 +1,107 @@
+import React, { useState, FunctionComponent, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { Grid, Button, makeStyles } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+import { commonStyles, useSafeIntl } from 'bluesquare-components';
+
+import InputComponent from '../../../components/forms/InputComponent';
+import { redirectTo } from '../../../routing/actions';
+import MESSAGES from '../messages';
+
+import { baseUrl } from '../config';
+
+const useStyles = makeStyles(theme => ({
+    ...commonStyles(theme),
+}));
+
+type Params = {
+    pageSize: string;
+    order: string;
+    page: string;
+    search?: string;
+    entityTypes?: string;
+};
+
+type Props = {
+    params: Params;
+};
+
+const Filters: FunctionComponent<Props> = ({ params }) => {
+    const [filtersUpdated, setFiltersUpdated] = useState(false);
+    const classes: Record<string, string> = useStyles();
+    const { formatMessage } = useSafeIntl();
+    const dispatch = useDispatch();
+    const [filters, setFilters] = useState({
+        search: params.search,
+    });
+
+    const handleSearch = useCallback(() => {
+        if (filtersUpdated) {
+            setFiltersUpdated(false);
+            const tempParams = {
+                ...params,
+                ...filters,
+            };
+            tempParams.page = '1';
+            dispatch(redirectTo(baseUrl, tempParams));
+        }
+    }, [filtersUpdated, dispatch, filters, params]);
+
+    const handleChange = useCallback(
+        (key, value) => {
+            setFiltersUpdated(true);
+            setFilters({
+                ...filters,
+                [key]: value,
+            });
+        },
+        [filters],
+    );
+
+    return (
+        <>
+            <Grid container spacing={4}>
+                <Grid item xs={3}>
+                    <InputComponent
+                        keyValue="search"
+                        onChange={handleChange}
+                        value={filters.search}
+                        type="search"
+                        label={MESSAGES.search}
+                        onEnterPressed={handleSearch}
+                    />
+                </Grid>
+            </Grid>
+            <Grid
+                container
+                spacing={4}
+                justifyContent="flex-end"
+                alignItems="center"
+            >
+                <Grid
+                    item
+                    xs={2}
+                    container
+                    justifyContent="flex-end"
+                    alignItems="center"
+                >
+                    <Button
+                        data-test="search-button"
+                        disabled={!filtersUpdated}
+                        variant="contained"
+                        className={classes.button}
+                        color="primary"
+                        onClick={() => handleSearch()}
+                    >
+                        <SearchIcon className={classes.buttonIcon} />
+                        {formatMessage(MESSAGES.search)}
+                    </Button>
+                </Grid>
+            </Grid>
+        </>
+    );
+};
+
+export { Filters };

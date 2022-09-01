52fe0994742d4ba8e3f17932f8a6ac535222919c
Christophe Gerard <c@eduz.be>
2022-09-01 13:13:37 +0200

    black

--- iaso/migrations/0162_merge_20220901_1052.py
+++ iaso/migrations/0162_merge_20220901_1052.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('iaso', '0161_merge_20220829_0920'),
-        ('iaso', '0161_rename_fields_detail_view_entitytype_fields_detail_info_view'),
+        ("iaso", "0161_merge_20220829_0920"),
+        ("iaso", "0161_rename_fields_detail_view_entitytype_fields_detail_info_view"),
     ]
 
-    operations = [
-    ]
+    operations = []

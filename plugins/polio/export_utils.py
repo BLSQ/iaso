4d46c9cd8068ed5ea417403e9b511619739faba7
HAKIZIMANA Franck <hakifranckmana@gmail.com>
2022-09-23 14:13:00 +0200

    wif: structure of data to be displayed

--- plugins/polio/export_utils.py
+++ plugins/polio/export_utils.py
@@ -8,7 +8,11 @@ def generate_xlsx(filename, columns, datas):
     sheet.append(columns)
     for index in range(len(datas)):
         for key in datas[index]:
-            sheet.append([datas[index][key]["country_name"]])
-            
+            data_to_display = []
+            data_to_display.append(datas[index][key]["country_name"])
+            for x in datas[index][key]["rounds"]:
+                data_to_display.append('-'.join(datas[index][key]["rounds"][x]))    
+            sheet.append(data_to_display)
+
     file.save(filename+".xlsx")
     return file

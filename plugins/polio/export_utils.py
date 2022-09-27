d3bde4d46989abc9724a6c1e50cca51726de8be0
HAKIZIMANA Franck <hakifranckmana@gmail.com>
2022-09-27 13:43:46 +0200

    wip: align data in cells

--- plugins/polio/export_utils.py
+++ plugins/polio/export_utils.py
@@ -1,21 +1,30 @@
 from openpyxl import Workbook
 from openpyxl.writer.excel import save_virtual_workbook
 from openpyxl.utils import get_column_letter
+from openpyxl.styles import Alignment  
 
 def generate_xlsx(filename, columns, datas):
     file = Workbook()
     sheet = file.active
     sheet.title = filename
-    sheet.append(columns)
+    # display columns in the xlsx file
+    for row in range(1,2):
+        for column in range(1, len(columns) + 1):
+            cell = sheet.cell(column=column, row=row, value=columns[column-1])
+            cell.alignment = Alignment(horizontal='left', vertical='top') 
+    # display calendar data in the xlsx file
     for data in datas:
-        data_to_display = []
+        # loop over each row representing a country campaign rounds
         for row in range(1, len(datas)):
-            _ = sheet.cell(column=1, row=row+1, value=datas[row-1]["country_name"].format(get_column_letter(1)))
+            cell = sheet.cell(column=1, row=row+1, value=datas[row-1]["country_name"])
+            cell.alignment = Alignment(horizontal='left', vertical='top') 
+            # loop over rounds for each month
             for month in range(1,13):
                 if str(month) in datas[row-1]["rounds"].keys():
-                    _ = sheet.cell(column=month+1, row=row+1, value=get_cell_data(datas[row-1]["rounds"][str(month)]).format(get_column_letter(month+1)))
+                    cell = sheet.cell(column=month+1, row=row+1, value=get_cell_data(datas[row-1]["rounds"][str(month)]))
                 else:
-                    _ = sheet.cell(column=month+1, row=row+1, value="".format(get_column_letter(month+1)))
+                    cell = sheet.cell(column=month+1, row=row+1, value="")
+                cell.alignment = Alignment(horizontal='left', vertical='top')  
     file.save(filename+".xlsx")
     return file
 

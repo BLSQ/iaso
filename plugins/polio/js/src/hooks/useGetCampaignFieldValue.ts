d6ae0114c0aba995fec86b5dab6dc2fa617fda93
axel <tavormina.axel@gmail.com>
2022-10-06 13:59:20 +0200

    create new hook for get values campaign log detail

---
+++ plugins/polio/js/src/hooks/useGetCampaignFieldValue.ts
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+import moment from 'moment';
+// @ts-ignore
+import { useSafeIntl } from 'bluesquare-components';
+
+import { CampaignLogData, CampaignFieldType } from '../constants/types';
+
+import MESSAGES from '../constants/messages';
+
+const textPlaceholder = '--';
+
+export const useGetCampaignFieldValue = (): ((
+    fieldKey: string,
+    newValue: CampaignLogData,
+    type: CampaignFieldType,
+) => string) => {
+    const { formatMessage } = useSafeIntl();
+    const getValue = (fieldKey, newValue, type): string => {
+        switch (type) {
+            case 'object': {
+                return 'TO DO';
+            }
+            case 'string':
+            case 'number': {
+                return newValue[fieldKey] || textPlaceholder;
+            }
+            case 'date': {
+                return newValue[fieldKey]
+                    ? moment(newValue[fieldKey]).format('L')
+                    : textPlaceholder;
+            }
+            case 'boolean': {
+                return newValue[fieldKey].toString();
+            }
+            case 'dateTime': {
+                return newValue[fieldKey]
+                    ? moment(newValue[fieldKey]).format('LTS')
+                    : textPlaceholder;
+            }
+            case 'time': {
+                return newValue[fieldKey]
+                    ? moment(newValue[fieldKey]).format('T')
+                    : textPlaceholder;
+            }
+            default:
+                return formatMessage(MESSAGES.typeNotSupported, { type });
+        }
+    };
+    return getValue;
+};

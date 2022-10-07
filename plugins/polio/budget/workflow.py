7123455a696b747e10995a61b2d9722315fb0304
Quang Son Le <qle@bluesquarehub.com>
2022-10-07 13:06:55 +0200

    POLIO-619: change color for feedback step

--- plugins/polio/budget/workflow.py
+++ plugins/polio/budget/workflow.py
@@ -98,7 +98,7 @@ transition_defs = [
         "displayed_fields": ["comment"],
         "from_node": "budget_submitted",
         "to_node": "rejected",
-        "color": "red",
+        "color": "primary",
     },
 ]
 

2ed2f18168d6c55945e27b2cc493f9cb9883f375
Quang Son Le <qle@bluesquarehub.com>
2022-10-07 13:34:08 +0200

    POLIO-619: fix webpack build

--- plugins/polio/js/src/pages/Budget/BudgetDetailsFilters.tsx
+++ plugins/polio/js/src/pages/Budget/BudgetDetailsFilters.tsx
@@ -2,14 +2,16 @@
 /* eslint-disable camelcase */
 import React, { FunctionComponent } from 'react';
 import { Box, Grid, useMediaQuery, useTheme } from '@material-ui/core';
+// @ts-ignore
+import { useSafeIntl } from 'bluesquare-components';
 import { UrlParams } from '../../../../../../hat/assets/js/apps/Iaso/types/table';
 import { BUDGET_DETAILS } from '../../constants/routes';
 import InputComponent from '../../../../../../hat/assets/js/apps/Iaso/components/forms/InputComponent';
 import { useFilterState } from '../../../../../../hat/assets/js/apps/Iaso/hooks/useFilterState';
 import MESSAGES from '../../constants/messages';
 import { FilterButton } from '../../../../../../hat/assets/js/apps/Iaso/components/FilterButton';
-// import { useGetTeamsDropDown } from '../../hooks/useGetTeams';
-import { useAllEventsOption } from './CreateEditBudgetEvent/utils';
+import { BudgetEventType } from '../../constants/types';
+import { DropdownOptions } from '../../../../../../hat/assets/js/apps/Iaso/types/utils';
 
 type Props = {
     params: UrlParams & {
@@ -23,6 +25,41 @@ type Props = {
         type?: string;
     };
     buttonSize?: 'medium' | 'small' | 'large' | undefined;
+};
+
+// TODO import all step types from API
+export const useAllEventsOption = (): DropdownOptions<BudgetEventType>[] => {
+    const { formatMessage } = useSafeIntl();
+    return [
+        {
+            label: formatMessage(MESSAGES.submission) as string,
+            value: 'submission',
+        },
+        {
+            label: formatMessage(MESSAGES.comments) as string,
+            value: 'comments',
+        },
+        {
+            label: formatMessage(MESSAGES.request) as string,
+            value: 'request',
+        },
+        {
+            label: formatMessage(MESSAGES.feedback) as string,
+            value: 'feedback',
+        },
+        {
+            label: formatMessage(MESSAGES.transmission) as string,
+            value: 'transmission',
+        },
+        {
+            label: formatMessage(MESSAGES.review) as string,
+            value: 'review',
+        },
+        {
+            value: 'validation',
+            label: formatMessage(MESSAGES.validation),
+        },
+    ];
 };
 
 export const BudgetDetailsFilters: FunctionComponent<Props> = ({
@@ -46,30 +83,6 @@ export const BudgetDetailsFilters: FunctionComponent<Props> = ({
                 spacing={isXSLayout ? 0 : 2}
                 justifyContent="flex-end"
             >
-                {/* <Grid item xs={12} sm={6} md={3}>
-                    <InputComponent
-                        keyValue="senderTeam"
-                        onChange={handleChange}
-                        type="select"
-                        options={teams}
-                        multi={false}
-                        value={filters.senderTeam}
-                        label={MESSAGES.sentByTeam}
-                        loading={isFetching}
-                    />
-                </Grid>
-                <Grid item xs={12} sm={6} md={3}>
-                    <InputComponent
-                        keyValue="recipient"
-                        onChange={handleChange}
-                        type="select"
-                        options={teams}
-                        multi={false}
-                        value={filters.recipient}
-                        label={MESSAGES.sentToTeam}
-                        loading={isFetching}
-                    />
-                </Grid> */}
                 <Grid item xs={12} sm={6} md={3}>
                     <InputComponent
                         keyValue="type"

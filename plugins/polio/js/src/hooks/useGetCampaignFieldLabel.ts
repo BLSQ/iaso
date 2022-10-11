ba6bcc339abb9c8e455469d171dc38873c55cd84
axel <tavormina.axel@gmail.com>
2022-10-11 14:10:14 +0200

    refactor hook to display label

--- plugins/polio/js/src/hooks/useGetCampaignFieldLabel.ts
+++ plugins/polio/js/src/hooks/useGetCampaignFieldLabel.ts
@@ -11,16 +11,8 @@ export const useGetCampaignFieldLabel = (): ((
 ) => string) => {
     const { formatMessage } = useSafeIntl();
     const getLabel = (fieldKey, messages): any => {
-        if (!messages[fieldKey]) {
-            // TO FIX
-            const field = Object.entries(messages).find(message =>
-                message.filter(obj => obj.key === fieldKey),
-            );
-            // TOF FIX
-            return formatMessage(messages[field[0]]);
-        }
-
-        return formatMessage(messages[fieldKey]);
+        // temporary displaying key instead of label not yet added to translations
+        return formatMessage(messages[fieldKey]) || fieldKey;
     };
 
     return getLabel;

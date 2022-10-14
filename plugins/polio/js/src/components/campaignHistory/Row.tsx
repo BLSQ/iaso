4cb5fbd3fc15b05af20d09768546698b1353a3b2
axel <tavormina.axel@gmail.com>
2022-10-14 18:29:10 +0200

    truncate value if link

--- plugins/polio/js/src/components/campaignHistory/Row.tsx
+++ plugins/polio/js/src/components/campaignHistory/Row.tsx
@@ -4,7 +4,13 @@ import {
     commonStyles,
 } from 'bluesquare-components';
 
-import { TableRow, TableCell, makeStyles, Theme } from '@material-ui/core';
+import {
+    TableRow,
+    TableCell,
+    makeStyles,
+    Theme,
+    Typography,
+} from '@material-ui/core';
 
 import { useGetCampaignFieldLabel } from '../../hooks/useGetCampaignFieldLabel';
 
@@ -21,19 +27,47 @@ const useStyles = makeStyles((theme: Theme) => ({
     value: {
         maxWidth: '300px',
     },
+    link: {
+        color: theme.palette.primary.main,
+        textAlign: 'right',
+        flex: '1',
+        cursor: 'pointer',
+        display: 'flex',
+        justifyContent: 'flex-start',
+    },
 }));
+
+const redirectToUrl = url => {
+    window.open(url);
+};
+
+const truncate = (string, limit) => {
+    return `${string.slice(0, limit)}...`;
+};
 
 export const Row: FunctionComponent<RowProps> = ({ fieldKey, value }) => {
     const classes: Record<string, string> = useStyles();
     const getLabel = useGetCampaignFieldLabel();
+    const regex = '^(http|https):\\/\\/';
     return (
-        <TableRow>
+        <TableRow className={classes.round}>
             {fieldKey && (
                 <TableCell className={classes.tableCellLabel}>
                     {getLabel(fieldKey)}
                 </TableCell>
             )}
-            <TableCell className={classes.tableCell}>{value}</TableCell>
+            <TableCell className={classes.tableCell}>
+                {typeof value === 'string' && value.match(regex) ? (
+                    <Typography
+                        className={classes.link}
+                        onClick={() => redirectToUrl(value)}
+                    >
+                        {truncate(value, 35)}
+                    </Typography>
+                ) : (
+                    value
+                )}
+            </TableCell>
         </TableRow>
     );
 };

5f57c4d83c6a38ba6104a2f8c9957fcfcd767763
Olivier Le Thanh Duong <olivier@lethanh.be>
2022-10-28 15:12:21 +0200

    Refactor jsonlogic for future casting

--- iaso/utils/jsonlogic.py
+++ iaso/utils/jsonlogic.py
@@ -15,6 +15,7 @@ def jsonlogic_to_q(jsonlogic: Dict[str, Any], field_prefix: str = "") -> Q:
 
     :return: A Django Q object.
     """
+
     if "and" in jsonlogic:
         return reduce(
             operator.and_,
@@ -28,17 +29,35 @@ def jsonlogic_to_q(jsonlogic: Dict[str, Any], field_prefix: str = "") -> Q:
 
     elif "!" in jsonlogic:
         return ~jsonlogic_to_q(jsonlogic["!"], field_prefix)
-    elif "==" in jsonlogic:
-        return Q(**{f"{field_prefix}{jsonlogic['=='][0]['var']}__exact": jsonlogic["=="][1]})
-    elif "!=" in jsonlogic:
-        return ~Q(**{f"{field_prefix}{jsonlogic['!='][0]['var']}__exact": jsonlogic["!="][1]})
-    elif ">" in jsonlogic:
-        return Q(**{f"{field_prefix}{jsonlogic['>'][0]['var']}__gt": jsonlogic[">"][1]})
-    elif ">=" in jsonlogic:
-        return Q(**{f"{field_prefix}{jsonlogic['>='][0]['var']}__gte": jsonlogic[">="][1]})
-    elif "<" in jsonlogic:
-        return Q(**{f"{field_prefix}{jsonlogic['<'][0]['var']}__lt": jsonlogic["<"][1]})
-    elif "<=" in jsonlogic:
-        return Q(**{f"{field_prefix}{jsonlogic['<='][0]['var']}__lte": jsonlogic["<="][1]})
-    else:
-        raise ValueError(f"Unknown JsonLogic operator: {jsonlogic}")
+
+    if not jsonlogic.keys():
+        return Q()
+
+    # Binary operators
+    op = list(jsonlogic.keys())[0]
+    params = jsonlogic[op]
+    if len(params) != 2:
+        raise ValueError(f"Unsuported JsonLogic: {jsonlogic}")
+    if "var" not in params[0]:
+        raise ValueError(f"Unsuported JsonLogic: {jsonlogic}")
+
+    lookups = {
+        "==": "exact",
+        "!=": "exact",
+        ">": "gt",
+        ">=": "gte",
+        "<": "lt",
+        "<=": "lte",
+    }
+
+    if op not in lookups.keys():
+        raise ValueError(f"Unsuported JsonLogic: {jsonlogic}")
+
+    field_name = params[0]["var"]
+    lookup = lookups[op]
+    f = f"{field_prefix}{field_name}__{lookup}"
+    q = Q(**{f: params[1]})
+    if op == "!=":
+        # invert the filter
+        q = ~q
+    return q

90a75efce38095f36bbe9b52561e493c96b371b3
axel <tavormina.axel@gmail.com>
2022-10-28 16:45:01 +0200

    wip: add stepper from material UI + custom design

--- plugins/polio/js/src/pages/Budget/BudgetDetails/BudgetTimeline.tsx
+++ plugins/polio/js/src/pages/Budget/BudgetDetails/BudgetTimeline.tsx
@@ -1,9 +1,12 @@
-import { makeStyles } from '@material-ui/core';
-import React, { FunctionComponent } from 'react';
+import { Box, makeStyles } from '@material-ui/core';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 // @ts-ignore
 import { useSafeIntl } from 'bluesquare-components';
 import classnames from 'classnames';
-import WidgetPaperComponent from '../../../../../../../hat/assets/js/apps/Iaso/components/papers/WidgetPaperComponent';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import { CheckBox } from '@material-ui/icons';
 import MESSAGES from '../../../constants/messages';
 import { Categories } from '../types';
 
@@ -11,15 +14,46 @@ type Props = {
     categories?: Categories;
 };
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        '& .MuiStepConnector-lineHorizontal': {
+            borderTopWidth: '12px',
+            borderColor: 'grey',
+        },
+    },
+    item: {
+        marginTop: '20px',
+    },
+}));
+
 export const BudgetTimeline: FunctionComponent<Props> = ({
     categories = [],
 }) => {
+    const [activeStep, setActiveStep] = useState(0);
+    const classes = useStyles();
+
     return (
         <>
-            {categories?.map(category => (
-                <h1>{category.key}</h1>
-            ))}
-            ;
+            <Stepper classes={classes} activeStep={activeStep} alternativeLabel>
+                {categories?.map(category => (
+                    <Step key={category.key}>
+                        <StepLabel>{category.label}</StepLabel>
+                        <Box className={classes.item}>
+                            {category.items.map(item => (
+                                <Box
+                                    display="flex"
+                                    alignItems="center"
+                                    justifyContent="center"
+                                >
+                                    <CheckBox />
+                                    {item.label}
+                                </Box>
+                            ))}
+                        </Box>
+                    </Step>
+                ))}
+            </Stepper>
         </>
     );
 };

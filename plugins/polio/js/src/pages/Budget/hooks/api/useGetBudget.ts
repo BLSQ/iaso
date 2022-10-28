6533b0b5110a25609f388f308e1ef9001ed5f2ca
axel <tavormina.axel@gmail.com>
2022-10-28 10:33:40 +0200

    add mock for timeline

--- plugins/polio/js/src/pages/Budget/hooks/api/useGetBudget.ts
+++ plugins/polio/js/src/pages/Budget/hooks/api/useGetBudget.ts
@@ -9,7 +9,7 @@ import {
     DropdownOptions,
     Optional,
 } from '../../../../../../../../hat/assets/js/apps/Iaso/types/utils';
-import { Budget, Workflow } from '../../types';
+import { Budget, Workflow, Categories } from '../../types';
 
 const getBudgets = (params: any) => {
     const filteredParams = Object.entries(params).filter(
@@ -72,12 +72,146 @@ export const useGetBudgetForCampaign = (
     id: Optional<string>,
 ): UseQueryResult<Partial<Budget>> => {
     const params = {
+        // add timeline field
         fields: 'id,obr_name,current_state,next_transitions,possible_transitions',
     };
+
+    const mockBudgetTimeline: Categories = [
+        {
+            key: 'step_1',
+            label: 'MoH, GPEI, UNICEF & WHO COs',
+            color: 'green',
+            items: [
+                {
+                    label: 'WHO Co Budget Request',
+                    performed_on: '2022-05-18',
+                    performed_by: {
+                        username: 'Spike',
+                        first_name: 'Spike',
+                        last_name: 'Spiegel',
+                    },
+                },
+                {
+                    label: 'UNICEF Co Budget Request',
+                    performed_on: '2022-05-19',
+                    performed_by: {
+                        username: 'Faye',
+                        first_name: 'Faye',
+                        last_name: 'Valentine',
+                    },
+                },
+                {
+                    label: 'GPEI Budget Consolidation',
+                    performed_on: '2022-05-21',
+                    performed_by: {
+                        username: 'Ed',
+                        first_name: 'Edward',
+                        last_name: 'Wong',
+                    },
+                },
+            ],
+        },
+        {
+            key: 'step_2',
+            label: 'RRT finances',
+            color: 'green',
+            items: [
+                {
+                    label: 'Budget Submitted to RRT',
+                    performed_on: '2022-05-23',
+                    performed_by: {
+                        username: 'Jet',
+                        first_name: 'Jet',
+                        last_name: 'Black',
+                    },
+                },
+                {
+                    label: 'Feedback received by GPEI Co Budget Request',
+                    performed_on: '2022-05-25',
+                    performed_by: {
+                        username: ' Oni',
+                        first_name: 'Onizuka',
+                        last_name: 'Eikichi',
+                    },
+                },
+            ],
+        },
+        {
+            key: 'step_3',
+            label: 'ORPG',
+            color: 'green',
+            items: [
+                {
+                    label: 'Budget Submitted to ORPG',
+                    performed_on: '2022-05-28',
+                    performed_by: {
+                        username: 'Aizawa',
+                        first_name: 'Aizawa',
+                        last_name: 'Miyabi',
+                    },
+                },
+                {
+                    label: 'Feedback received by RRT',
+                    performed_on: '2022-06-03',
+                    performed_by: {
+                        username: ' Uehara',
+                        first_name: 'Uehara',
+                        last_name: 'Anko',
+                    },
+                },
+            ],
+        },
+        {
+            key: 'step_4',
+            label: 'WHO/UNICEF',
+            color: 'grey',
+            items: [
+                {
+                    label: 'Budget Submitted to WHO',
+                },
+                {
+                    label: 'Budget Sumbitted to UNICEF',
+                },
+                {
+                    label: 'Budget Sumbitted by ORPG (WHO)',
+                },
+                {
+                    label: 'Budget Sumbitted to ORPG (UNICEF)',
+                },
+            ],
+        },
+        {
+            key: 'step_5',
+            label: 'Funding Release',
+            color: 'grey',
+            items: [
+                {
+                    label: 'Budget Final Approval',
+                },
+                {
+                    label: 'Funds release to COs',
+                },
+                {
+                    label: 'Funds release to Operations',
+                },
+            ],
+        },
+    ];
+
     return useSnackQuery({
         queryFn: () => getBudgetForCampaign(id, params),
         queryKey: ['budget', 'campaign', id],
-        options: { enabled: Boolean(id) },
+        options: {
+            enabled: Boolean(id),
+            select: (data: Partial<Budget>) => {
+                const newData = {
+                    ...data,
+                    timeline: { categories: mockBudgetTimeline },
+                };
+
+                return newData;
+            },
+        },
     });
 };
 
@@ -106,3 +240,5 @@ export const useGetWorkflowStatesForDropdown = (): UseQueryResult<
         },
     });
 };
+
+export const useGetBudgetTimeline = () => {};

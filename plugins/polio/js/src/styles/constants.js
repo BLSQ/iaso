0a77d39eb36f319b6bc76246bd13056f6faa7f2b
axel <tavormina.axel@gmail.com>
2022-11-02 17:01:15 +0100

    remove unused style constants

--- plugins/polio/js/src/styles/constants.js
+++ plugins/polio/js/src/styles/constants.js
@@ -2,10 +2,6 @@ export const OK_COLOR = 'green';
 export const FAIL_COLOR = 'red';
 export const WARNING_COLOR = '#FFD835';
 
-export const BUDGET_COMPLETED_COLOR = '#008000';
-export const BUDGET_PENDING_COLOR = '#90EE90';
-export const BUDGET_INACTIVE_COLOR = 'grey';
-
 // Scope map values
 export const selectedPathOptions = {
     color: 'lime',

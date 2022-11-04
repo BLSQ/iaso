9012e650f4878eb3ab44e6af593edd0a968bf668
Quang Son Le <qle@bluesquarehub.com>
2022-11-04 16:41:45 +0100

    POLIO-670: format migration

--- plugins/polio/migrations/0109_destruction.py
+++ plugins/polio/migrations/0109_destruction.py
@@ -7,19 +7,27 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('polio', '0108_shipment_comment'),
+        ("polio", "0108_shipment_comment"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Destruction',
+            name="Destruction",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('vials_destroyed', models.IntegerField(blank=True, null=True)),
-                ('date_report_received', models.DateField(blank=True, null=True)),
-                ('date_report', models.DateField(blank=True, null=True)),
-                ('comment', models.TextField(blank=True, null=True)),
-                ('round', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='destruction', to='polio.round')),
+                ("id", models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("vials_destroyed", models.IntegerField(blank=True, null=True)),
+                ("date_report_received", models.DateField(blank=True, null=True)),
+                ("date_report", models.DateField(blank=True, null=True)),
+                ("comment", models.TextField(blank=True, null=True)),
+                (
+                    "round",
+                    models.ForeignKey(
+                        null=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="destruction",
+                        to="polio.round",
+                    ),
+                ),
             ],
         ),
     ]

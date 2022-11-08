d770a3958eef37e656af9956e93f26800c5ca1d5
Nicolas Noé <nicolas@niconoe.eu>
2022-11-08 11:31:33 +0100

    Split requirements.txt into runtime and dev versions, removed unused dependencies

--- runiasodev.sh
+++ runiasodev.sh
@@ -4,7 +4,7 @@
 # You can run the database and webpack with django-compose and it will connect to the by default
 # By running:  django-compose up db webpack
 
-# You will need to create a venv locally by running: venv . && pip install -r requirements.txt
+# You will need to create a venv locally by running: venv . && pip install -r requirements-dev.txt
 
 #source bin/activate
 export SECRET_KEY="secret"

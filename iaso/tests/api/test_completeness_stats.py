673c8d9da9493c6fca65f49534fc5bbe4e611f13
Quang Son Le <qle@bluesquarehub.com>
2022-11-14 10:32:11 +0100

    IA-1611: test first draft

--- iaso/tests/api/test_completeness_stats.py
+++ iaso/tests/api/test_completeness_stats.py
@@ -1,29 +1,38 @@
-from iaso.models import Account, Form, Project
+from iaso.models import Account, Form, Project, OrgUnitType, OrgUnit
 from iaso.test import APITestCase
+from django.contrib.auth.models import User, Permission
 
 
 class CompletenessStatsAPITestCase(APITestCase):
+    fixtures = ["user.yaml", "orgunit.yaml"]
+
     @classmethod
     def setUpTestData(cls):
-        cls.star_wars = Account.objects.create(name="Star Wars")
-        cls.yoda = cls.create_user_with_profile(
-            username="yoda", account=cls.star_wars, permissions=["iaso_completeness_stats"]
-        )
+        cls.user = User.objects.get(username="test")
+        cls.account = Account.objects.get(name="test")
+        # cls.star_wars = Account.objects.create(name="Star Wars")
+        # cls.yoda = cls.create_user_with_profile(
+        #     username="yoda", account=cls.star_wars, permissions=["iaso_completeness_stats"]
+        # )
 
         # Another user that doesn't have the permission for this feature
-        cls.another_user = cls.create_user_with_profile(username="yoda2", account=cls.star_wars)
+        cls.another_user = cls.create_user_with_profile(username="yoda2", account=cls.account)
 
         # We create two forms for the user (we also need a project to create the asssociation)
         cls.form_1 = Form.objects.create(name="Hydroponics study")
         cls.form_2 = Form.objects.create(name="Hydroponics study 2")
-        cls.project_1 = Project.objects.create(
-            name="Hydroponic gardens", app_id="stars.empire.agriculture.hydroponics", account=cls.star_wars
-        )
+        cls.form_3 = Form.objects.create(name="Hydroponics study 3")
+        cls.project_1 = cls.account.project_set.first()
         cls.project_1.forms.add(cls.form_1)
         cls.project_1.forms.add(cls.form_2)
         cls.project_1.save()
-
-        # Some OrgUnits
+        cls.org_unit_type_1 = OrgUnitType.objects.get(pk=5)#alternate pk: 4
+        cls.org_unit_type_2 = OrgUnitType.objects.get(pk=4)#alternate pk: 4
+        cls.form_1.org_unit_types.add(cls.org_unit_type_1)
+        cls.form_2.org_unit_types.add(cls.org_unit_type_1)
+        cls.form_3.org_unit_types.add(cls.org_unit_type_2) 
+        cls.org_unit = OrgUnit.objects.filter(org_unit_type=cls.org_unit_type_1).first()
+        cls.create_form_instance(form=cls.form_1, org_unit=cls.org_unit)
 
     def test_row_listing_anonymous(self):
         """An anonymous user should not be able to access the API"""
@@ -38,11 +47,14 @@ class CompletenessStatsAPITestCase(APITestCase):
         self.assertEqual(response.status_code, 403)
 
     def test_base_row_listing(self):
-        self.client.force_authenticate(self.yoda)
+        self.user.user_permissions.add(Permission.objects.get(codename="iaso_completeness_stats"))
+        self.client.force_authenticate(self.user)
         # TODO: Test the basics of row listing (status code, data line in simple cases, ...)
 
         response = self.client.get("/api/completeness_stats/")
-        self.assertEqual(response.status_code, 200)
+        j = self.assertJSONResponse(response, 200)
+        print(j)
+        self.assertEqual(j, {})
 
     # TODO: Test the data is filtered by account
     # TODO: Test that data can be filtered by OU type

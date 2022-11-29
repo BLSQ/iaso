d7b138463813105ca285b584bd6f64a5318ca633
Olivier Le Thanh Duong <olivier@lethanh.be>
2022-11-29 16:22:05 +0100

    Fix bug in the api when editing instance without existing period
    Submissions without period where not editable anymore, you couldn't correct and add a period on it
    because of a bug in the validation

--- iaso/api/instances.py
+++ iaso/api/instances.py
@@ -67,9 +67,11 @@ class InstanceSerializer(serializers.ModelSerializer):
         Check if period is of self.instance.form.period_type.
         """
 
-        if self.instance.period and (periods.detect(value) == self.instance.form.period_type):
+        if periods.detect(value) == self.instance.form.period_type:
             return value
-        raise serializers.ValidationError("Wrong period type")
+        raise serializers.ValidationError(
+            f"Wrong period type, expecting: {self.instance.form.period_type}. Received type: {periods.detect(value)}"
+        )
 
 
 class HasInstancePermission(permissions.BasePermission):

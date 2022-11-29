a0b8b438cb968b6162b0c8826b2d70cbfeff7f7b
Olivier Le Thanh Duong <olivier@lethanh.be>
2022-11-29 15:57:29 +0100

    Make the dialog logic only load on display
    copy logic from CreateOverrideStep.tsx and  bluesquare-components/src/components/Modal/ModalWithButton.tsx
    
    we should refactor to use ModalWithButton directly

--- hat/assets/js/apps/Iaso/domains/instances/components/CreateReAssignDialogComponent.tsx
+++ hat/assets/js/apps/Iaso/domains/instances/components/CreateReAssignDialogComponent.tsx
@@ -1,12 +1,12 @@
-import React, { FunctionComponent, useCallback, useRef } from 'react';
-import ConfirmCancelDialogComponent from '../../../components/dialogs/ConfirmCancelDialogComponent';
+import React, { FunctionComponent, useState } from 'react';
+import { Typography } from '@material-ui/core';
+// @ts-ignore
+import { ConfirmCancelModal, useSafeIntl } from 'bluesquare-components';
 import { Period } from '../../periods/models';
 import { isValidPeriod } from '../../periods/utils';
 import MESSAGES from '../messages';
 import { OrgUnitTreeviewModal } from '../../orgUnits/components/TreeView/OrgUnitTreeviewModal';
 import PeriodPicker from '../../periods/components/PeriodPicker';
-import { useSafeIntl } from 'bluesquare-components';
-import { Typography } from '@material-ui/core';
 
 type Props = {
     titleMessage: any;
@@ -19,27 +19,28 @@ type Props = {
     currentInstance?: {
         id: number;
         period?: string;
+        // eslint-disable-next-line camelcase
         org_unit?: any;
     };
-    onCreateOrReAssign: (_: any, _: any) => any;
-    renderTrigger: (any) => any;
+    // eslint-disable-next-line no-unused-vars
+    onCreateOrReAssign: (_: any, __: any) => any;
     orgUnitTypes: number[];
+    isOpen: boolean;
+    closeDialog: () => void;
 };
 
 const CreateReAssignDialogComponent: FunctionComponent<Props> = ({
     titleMessage,
     confirmMessage = MESSAGES.ok,
     cancelMessage = MESSAGES.cancel,
-    renderTrigger,
     formType,
     currentInstance,
     onCreateOrReAssign,
     orgUnitTypes = [],
+    isOpen,
+    closeDialog,
 }) => {
     const { formatMessage } = useSafeIntl();
-    const currentFormOrInstanceProp = currentInstance || formType;
-    const currentFormOrInstance = { ...currentFormOrInstanceProp };
-
     const [fieldValue, setFieldValue] = React.useState(() => {
         let initialPeriod = currentInstance?.period;
         // if this is a new Submission or there isn't a current permission calculate
@@ -69,93 +70,110 @@ const CreateReAssignDialogComponent: FunctionComponent<Props> = ({
         formType.periodType,
     );
 
-    // copying the current value of the state to restore it on cancel
-    const orgUnitCopy = useRef(currentFormOrInstance.org_unit);
-    const periodCopy = useRef(currentFormOrInstance.period);
-
-    const onCancel = useCallback(
-        closeDialog => {
-            setFieldValue({
-                ...fieldValue,
-                orgUnit: { ...fieldValue.orgUnit, value: orgUnitCopy.current },
-            });
-            closeDialog();
-        },
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        [fieldValue, orgUnitCopy.current],
-    );
-
-    const onConfirm = closeDialog => {
-        onCreateOrReAssign(currentFormOrInstance, {
+    const onConfirm = () => {
+        const currentFormOrInstanceProp = currentInstance || formType;
+        onCreateOrReAssign(currentFormOrInstanceProp, {
             period: fieldValue.period.value,
             org_unit: fieldValue.orgUnit.value?.id,
         });
-        orgUnitCopy.current = fieldValue.orgUnit.value;
-        periodCopy.current = fieldValue.period.value;
         closeDialog();
     };
 
-    const isPeriodDisabled = formType.periodType;
+    const isPeriodRequired = Boolean(formType.periodType);
     return (
-        <ConfirmCancelDialogComponent
-            renderTrigger={renderTrigger}
-            titleMessage={titleMessage}
-            onConfirm={onConfirm}
-            onCancel={onCancel}
-            confirmMessage={confirmMessage}
-            cancelMessage={cancelMessage}
-            maxWidth="xs"
-            allowConfirm={
-                Boolean(fieldValue.orgUnit.value) &&
-                (Boolean(isPeriodDisabled) ||
-                    (!isPeriodDisabled && Boolean(fieldValue.period.value)))
-            }
-        >
-            {!isOriginalPeriodValid && (
-                <Typography color="error">
-                    Current period on Instance is invalid:
-                    {currentInstance?.period}
-                </Typography>
-            )}
-            {formType.periodType && (
-                <PeriodPicker
-                    title={formatMessage(MESSAGES.period)}
-                    periodType={formType.periodType}
-                    keyName="period"
-                    onChange={value => {
-                        setFieldValue({
-                            ...fieldValue,
-                            period: {
-                                ...fieldValue.period,
-                                value,
-                            },
-                        });
-                    }}
-                    activePeriodString={fieldValue.period.value}
-                />
-            )}
-            <>
-                <OrgUnitTreeviewModal
-                    required
-                    clearable={false}
-                    titleMessage={MESSAGES.selectedOrgUnit}
-                    toggleOnLabelClick={false}
-                    onConfirm={orgUnit => {
-                        setFieldValue({
-                            ...fieldValue,
-                            orgUnit: {
-                                ...fieldValue.orgUnit,
-                                value: orgUnit,
-                            },
-                        });
-                    }}
-                    multiselect={false}
-                    initialSelection={fieldValue.orgUnit.value}
-                    allowedTypes={orgUnitTypes}
-                />
-            </>
-        </ConfirmCancelDialogComponent>
+        <>
+            <ConfirmCancelModal
+                titleMessage={titleMessage}
+                onConfirm={onConfirm}
+                open={isOpen}
+                confirmMessage={confirmMessage}
+                cancelMessage={cancelMessage}
+                maxWidth="xs"
+                allowConfirm={
+                    Boolean(fieldValue.orgUnit.value) &&
+                    isPeriodRequired &&
+                    Boolean(fieldValue.period.value)
+                }
+                closeDialog={closeDialog}
+                onCancel={closeDialog}
+            >
+                {!isOriginalPeriodValid && (
+                    <Typography color="error">
+                        Current period on Instance is invalid:
+                        {currentInstance?.period}
+                    </Typography>
+                )}
+                {isPeriodRequired && (
+                    <PeriodPicker
+                        title={formatMessage(MESSAGES.period)}
+                        periodType={formType.periodType}
+                        keyName="period"
+                        onChange={value => {
+                            setFieldValue({
+                                ...fieldValue,
+                                period: {
+                                    ...fieldValue.period,
+                                    value,
+                                },
+                            });
+                        }}
+                        activePeriodString={fieldValue.period.value}
+                    />
+                )}
+                <>
+                    <OrgUnitTreeviewModal
+                        required
+                        clearable={false}
+                        titleMessage={MESSAGES.selectedOrgUnit}
+                        toggleOnLabelClick={false}
+                        onConfirm={orgUnit => {
+                            setFieldValue({
+                                ...fieldValue,
+                                orgUnit: {
+                                    ...fieldValue.orgUnit,
+                                    value: orgUnit,
+                                },
+                            });
+                        }}
+                        multiselect={false}
+                        initialSelection={fieldValue.orgUnit.value}
+                        allowedTypes={orgUnitTypes}
+                    />
+                </>
+            </ConfirmCancelModal>
+        </>
     );
 };
 
-export default CreateReAssignDialogComponent;
+type DialogProps = {
+    renderTrigger: any;
+    defaultOpen: boolean;
+    iconProps: any;
+} & Props;
+export const CreateReAssignDialog: FunctionComponent<DialogProps> = ({
+    renderTrigger,
+    defaultOpen = false,
+    // eslint-disable-next-line no-unused-vars,@typescript-eslint/no-unused-vars
+    iconProps = {}, // FIXME: Use when converting render trigger
+    ...modalProps
+}) => {
+    const [openModal, setOpenModal] = useState<boolean>(defaultOpen ?? false);
+    return (
+        <>
+            {/* // FIXME change render trigger in calling component */}
+            {renderTrigger({
+                openDialog: () => setOpenModal(true),
+            })}
+            {openModal && (
+                <CreateReAssignDialogComponent
+                    /* eslint-disable-next-line react/jsx-props-no-spreading */
+                    {...modalProps}
+                    closeDialog={() => setOpenModal(false)}
+                    isOpen={openModal}
+                />
+            )}
+        </>
+    );
+};
+
+export default CreateReAssignDialog;

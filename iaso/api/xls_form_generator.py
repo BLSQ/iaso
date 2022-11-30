973b405edd0568e0f4887f3f178857efe1bad3b0
Simon Dusausoy <sdusausoy@bluesquarehub.com>
2022-11-30 09:43:20 +0100

    black

--- iaso/api/xls_form_generator.py
+++ iaso/api/xls_form_generator.py
@@ -181,8 +181,12 @@ class XlsFormGeneratorViewSet(ModelViewSet):
                     if k not in key_added_to_choice_list:
                         # Add the ou type to the select choices of the xls form
                         key_added_to_choice_list.append(k)
-                        q_sheet[cell[get_column_position("type", q_sheet) - 1] + str(starting_row)] = f"select_one ou_{str(k).lower()}"
-                        q_sheet[cell[get_column_position("name", q_sheet) - 1] + str(starting_row)] = f"ou_{str(k).lower()}"
+                        q_sheet[
+                            cell[get_column_position("type", q_sheet) - 1] + str(starting_row)
+                        ] = f"select_one ou_{str(k).lower()}"
+                        q_sheet[
+                            cell[get_column_position("name", q_sheet) - 1] + str(starting_row)
+                        ] = f"ou_{str(k).lower()}"
                         q_sheet[cell[get_column_position("label", q_sheet) - 1] + str(starting_row)] = f"Select {k}"
                         q_sheet[cell[get_column_position("required", q_sheet) - 1] + str(starting_row)] = "yes"
                         if ou_hierarchy_list.index(k.lower()) != 0:

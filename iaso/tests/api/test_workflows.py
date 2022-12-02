87c5591692a9e2311bdd19fc09cbf79c9acff1c6
Matt Vadash <m@dcy.io>
2022-12-02 11:29:30 +0100

    WIP Tests

--- iaso/tests/api/test_workflows.py
+++ iaso/tests/api/test_workflows.py
@@ -1,5 +1,7 @@
 import typing
 
+import jsonschema
+
 from django.utils.timezone import now
 from django.contrib.auth.models import AnonymousUser
 
@@ -22,158 +24,152 @@ class WorkflowsAPITestCase(APITestCase):
     def setUpTestData(cls):
         cls.now = now()
 
-        star_wars_adults = m.Account.objects.create(name="Star Wars Adults")
-        star_wars_children = m.Account.objects.create(name="Star Wars Children")
-        marvel = m.Account.objects.create(name="Marvel")
+        blue_adults = m.Account.objects.create(name="Blue Adults")
+        blue_children = m.Account.objects.create(name="Blue Children")
+        red_adults = m.Account.objects.create(name="Red Adults")
+        red_children = m.Account.objects.create(name="Red Children")
 
         cls.anon = AnonymousUser()
-
-        # Adult Starwarsian
-        cls.yoda = cls.create_user_with_profile(
-            username="yoda", account=star_wars_adults, permissions=["iaso_workflows"]
+        cls.blue_adult_1 = cls.create_user_with_profile(
+            username="blue_adult_1", account=blue_adults, permissions=["iaso_workflows"]
         )
 
-        # Child Starwarsian
-        cls.anakin = cls.create_user_with_profile(
-            username="anakin", account=star_wars_children, permissions=["iaso_workflows"]
+        cls.blue_child_1 = cls.create_user_with_profile(
+            username="blue_child_1", account=blue_children, permissions=["iaso_workflows"]
         )
 
         # He doesn't have permissions
-        cls.darthvader = cls.create_user_with_profile(username="darthvader", account=star_wars_adults)
+        cls.blue_adult_np = cls.create_user_with_profile(username="blue_adult_np", account=blue_adults)
 
-        cls.iron_man = cls.create_user_with_profile(username="iron_man", account=marvel, permissions=["iaso_workflows"])
-
-        cls.project_starwars_adults = m.Project.objects.create(
-            name="Hydroponic gardens Adults", app_id="stars.empire.agriculture", account=star_wars_adults
+        cls.red_adult_1 = cls.create_user_with_profile(
+            username="red_adult_1", account=red_adults, permissions=["iaso_workflows"]
         )
 
-        cls.project_starwars_children = m.Project.objects.create(
-            name="Hydroponic gardens Children", app_id="stars.empire.agriculture", account=star_wars_children
+        cls.project_blue_adults = m.Project.objects.create(
+            name="Blue Adults Project", app_id="blue.adults.project", account=blue_adults
         )
 
-        cls.project_marvel = m.Project.objects.create(
-            name="SHIELD helicarrier", app_id="marvel.shield.fleet", account=marvel
+        cls.project_blue_children = m.Project.objects.create(
+            name="Blue Children Project", app_id="blue.children.project", account=blue_children
         )
 
-        cls.form_children_starwars = m.Form.objects.create(
-            name="Children Form", form_id="children_form_starwars", created_at=cls.now
-        )
-        cls.form_adults_starwars = m.Form.objects.create(
-            name="Adults Form", form_id="adults_form_starwars", created_at=cls.now
+        cls.project_red_adults = m.Project.objects.create(
+            name="Red Adult Project", app_id="red.adults.project", account=red_adults
         )
 
-        cls.form_children_marvel = m.Form.objects.create(
-            name="Children Form", form_id="children_form_marvel", created_at=cls.now
+        cls.form_children_blue = m.Form.objects.create(
+            name="Blue Children Form", form_id="children_form_blue", created_at=cls.now
         )
-        cls.form_adults_marvel = m.Form.objects.create(
-            name="Adults Form", form_id="adults_form_marvel", created_at=cls.now
+        cls.form_adults_blue = m.Form.objects.create(
+            name="Blue Adults Form", form_id="adults_form_blue", created_at=cls.now
         )
 
-        cls.project_starwars_adults.forms.add(cls.form_adults_starwars)
-        cls.project_starwars_adults.save()
+        cls.form_children_red = m.Form.objects.create(
+            name="Red Children Form", form_id="children_form_red", created_at=cls.now
+        )
+        cls.form_adults_red = m.Form.objects.create(
+            name="Red Adults Form", form_id="adults_form_red", created_at=cls.now
+        )
 
-        cls.project_starwars_children.forms.add(cls.form_children_starwars)
-        cls.project_starwars_children.save()
+        cls.project_blue_adults.forms.add(cls.form_adults_blue)
+        cls.project_blue_adults.save()
 
-        cls.project_marvel.forms.add(cls.form_children_marvel)
-        cls.project_marvel.forms.add(cls.form_adults_marvel)
-        cls.project_marvel.save()
+        cls.project_blue_children.forms.add(cls.form_children_blue)
+        cls.project_blue_children.save()
 
-        cls.et_children_starwars = m.EntityType.objects.create(
-            name="Children of Starwars",
+        cls.project_red_adults.forms.add(cls.form_children_red)
+        cls.project_red_adults.forms.add(cls.form_adults_red)
+        cls.project_red_adults.save()
+
+        cls.et_children_blue = m.EntityType.objects.create(
+            name="Children of Blue",
             created_at=cls.now,
-            account=star_wars_children,
-            reference_form=cls.form_children_starwars,
+            account=blue_children,
+            reference_form=cls.form_children_blue,
         )
 
-        cls.et_children_starwars2 = m.EntityType.objects.create(
-            name="Children of Starwars 2",
+        cls.et_children_blue2 = m.EntityType.objects.create(
+            name="Children of Blue 2",
             created_at=cls.now,
-            account=star_wars_children,
-            reference_form=cls.form_children_starwars,
+            account=blue_children,
+            reference_form=cls.form_children_blue,
         )
 
-        cls.et_children_starwars3 = m.EntityType.objects.create(
-            name="Children of Starwars 3",
+        cls.et_children_blue3 = m.EntityType.objects.create(
+            name="Children of Blur 3",
             created_at=cls.now,
-            account=star_wars_children,
-            reference_form=cls.form_children_starwars,
+            account=blue_children,
+            reference_form=cls.form_children_blue,
         )
 
-        cls.et_children_starwars = m.EntityType.objects.create(
-            name="Children of Starwars Inaccessible Reference Form",
+        cls.et_children_blue = m.EntityType.objects.create(
+            name="Children of Blur Inaccessible Reference Form",
             created_at=cls.now,
-            account=star_wars_children,
-            reference_form=cls.form_adults_marvel,
+            account=blue_children,  # <--- Entity inaccessible if you are only blue or red
+            reference_form=cls.form_adults_red,  # <---
         )
-        cls.workflow_et_children_starwars = Workflow.objects.create(entity_type=cls.et_children_starwars)
-        cls.workflow_et_children_starwars2 = Workflow.objects.create(entity_type=cls.et_children_starwars2)
-        cls.workflow_et_children_starwars3 = Workflow.objects.create(entity_type=cls.et_children_starwars3)
+        cls.workflow_et_children_blue = Workflow.objects.create(entity_type=cls.et_children_blue)
+        cls.workflow_et_children_blue2 = Workflow.objects.create(entity_type=cls.et_children_blue2)
+        cls.workflow_et_children_blue3 = Workflow.objects.create(entity_type=cls.et_children_blue3)
 
-        cls.et_children_marvel = m.EntityType.objects.create(
-            name="Children of Marvel", created_at=cls.now, account=marvel, reference_form=cls.form_children_marvel
-        )
-        cls.workflow_et_children_marvel = Workflow.objects.create(entity_type=cls.et_children_marvel)
-
-        cls.et_adults_starwars = m.EntityType.objects.create(
-            name="Adults of Starwars",
+        cls.et_adults_blue = m.EntityType.objects.create(
+            name="Adults of Blue",
             created_at=cls.now,
-            account=star_wars_adults,
-            reference_form=cls.form_adults_starwars,
+            account=blue_adults,
+            reference_form=cls.form_adults_blue,
         )
-        cls.workflow_et_adults_starwars = Workflow.objects.create(entity_type=cls.et_adults_starwars)
+        cls.workflow_et_adults_blue = Workflow.objects.create(entity_type=cls.et_adults_blue)
 
-        cls.et_adults_marvel = m.EntityType.objects.create(
-            name="Adults of Marvel", created_at=cls.now, account=marvel, reference_form=cls.form_adults_marvel
+        cls.et_children_red = m.EntityType.objects.create(
+            name="Children of Red", created_at=cls.now, account=red_children, reference_form=cls.form_children_red
+        )
+        cls.workflow_et_children_red = Workflow.objects.create(entity_type=cls.et_children_red)
+
+        cls.et_adults_red = m.EntityType.objects.create(
+            name="Adults of Red", created_at=cls.now, account=red_adults, reference_form=cls.form_adults_red
         )
 
-        cls.workflow_et_adults_marvel = Workflow.objects.create(entity_type=cls.et_adults_marvel)
+        cls.workflow_et_adults_red = Workflow.objects.create(entity_type=cls.et_adults_red)
 
         # This is not accessible, because Project, Entity Type, Workflow and Workflow version are accessible
-        # by Star Wars Children But the reference_form is only accessible By Star Wars Adults so ... Nobody is both
-        cls.workflow_version_et_children_starwars_inaccessible_reference_form = WorkflowVersion.objects.create(
-            workflow=cls.workflow_et_children_starwars,
-            name="workflow_version_et_children_starwars V1",
-            reference_form=cls.form_adults_starwars,
+        # by Blue Children But the reference_form is only accessible By Blue Adults so ... Nobody is both
+        cls.workflow_version_et_children_blue_inaccessible_reference_form = WorkflowVersion.objects.create(
+            workflow=cls.workflow_et_children_blue,
+            name="workflow_version_et_children_blue V1",
+            reference_form=cls.form_adults_blue,
         )
 
-        cls.workflow_version_et_children_starwars2_inaccessible_followup_form = WorkflowVersion.objects.create(
-            workflow=cls.workflow_et_children_starwars2,
-            name="workflow_version_et_children_starwars2 V1",
-            reference_form=cls.form_children_starwars,
+        cls.workflow_version_et_children_blue2_inaccessible_followup_form = WorkflowVersion.objects.create(
+            workflow=cls.workflow_et_children_blue2,
+            name="workflow_version_et_children_blue2 V1",
+            reference_form=cls.form_children_blue,
         )
 
-        cls.workflow_version_et_children_starwars3_inaccessible_changes = WorkflowVersion.objects.create(
-            workflow=cls.workflow_et_children_starwars3,
-            name="workflow_version_et_children_starwars3 V1",
-            reference_form=cls.form_children_starwars,
+        cls.workflow_version_et_children_blue3_inaccessible_changes = WorkflowVersion.objects.create(
+            workflow=cls.workflow_et_children_blue3,
+            name="workflow_version_et_children_blue3 V1",
+            reference_form=cls.form_children_blue,
         )
 
         cls.inaccessible_follow_up = WorkflowFollowup.objects.create(
-            order=1, condition="true", workflow=cls.workflow_version_et_children_starwars2_inaccessible_followup_form
+            order=1, condition="true", workflow=cls.workflow_version_et_children_blue2_inaccessible_followup_form
         )
 
-        cls.inaccessible_follow_up.forms.add(cls.form_adults_starwars)
+        cls.inaccessible_follow_up.forms.add(cls.form_adults_blue)
         cls.inaccessible_follow_up.save()
 
         cls.inaccessible_change = WorkflowChange.objects.create(
-            form=cls.form_adults_starwars, workflow=cls.workflow_version_et_children_starwars3_inaccessible_changes
+            form=cls.form_adults_blue, workflow=cls.workflow_version_et_children_blue3_inaccessible_changes
         )
 
-        cls.workflow_version_et_adults_starwars = WorkflowVersion.objects.create(
-            workflow=cls.workflow_et_adults_starwars,
-            name="workflow_version_et_adults_starwars V1",
-            reference_form=cls.form_adults_starwars,
+        cls.workflow_version_et_adults_blue = WorkflowVersion.objects.create(
+            workflow=cls.workflow_et_adults_blue,
+            name="workflow_version_et_adults_blue V1",
+            reference_form=cls.form_adults_blue,
         )
 
     def test_user_without_auth(self):
-        response = self.client.get(f"/api/workflow/{self.et_adults_marvel.pk}/")
-
-        var_dump(response)
-        var_dump(response.data)
-
-        print("response.data['detail']", response.data["detail"])
-        print("type response.data['detail']", type(response.data["detail"]))
+        response = self.client.get(f"/api/workflow/{self.et_adults_red.pk}/")
 
         self.assertJSONResponse(response, 403)
         self.assertEqual(response.data["detail"].code, "not_authenticated")
@@ -181,72 +177,130 @@ class WorkflowsAPITestCase(APITestCase):
 
     def test_user_anonymous(self):
         self.client.force_authenticate(self.anon)
-        response = self.client.get(f"/api/workflow/{self.et_adults_marvel.pk}/")
-
-        var_dump(response)
+        response = self.client.get(f"/api/workflow/{self.et_adults_red.pk}/")
 
         self.assertJSONResponse(response, 403)
+        self.assertEqual(response.data["detail"].code, "permission_denied")
+        self.assertEqual(response.data["detail"], "You do not have permission to perform this action.")
 
     def test_user_with_auth_no_permissions(self):
-        self.client.force_authenticate(self.darthvader)
+        self.client.force_authenticate(self.blue_adult_np)
 
-        response = self.client.get(f"/api/workflow/{self.et_children_starwars.pk}/")
-
-        var_dump(response)
+        response = self.client.get(f"/api/workflow/{self.et_children_blue.pk}/")
 
         self.assertJSONResponse(response, 403)
+        self.assertEqual(response.data["detail"].code, "permission_denied")
+        self.assertEqual(response.data["detail"], "You do not have permission to perform this action.")
 
     def test_user_with_auth_no_access_to_entity_type(self):
-        self.client.force_authenticate(self.yoda)
+        self.client.force_authenticate(self.blue_adult_1)
 
-        response = self.client.get(f"/api/workflow/{self.et_adults_marvel.pk}/")
+        response = self.client.get(f"/api/workflow/{self.et_adults_red.pk}/")
 
         self.assertJSONResponse(response, 404)
         assert "User profile account != entity_type account" in response.data
 
     def test_user_with_auth_access_entity_ok_no_access_to_reference_form(self):
-        self.client.force_authenticate(self.anakin)
+        self.client.force_authenticate(self.blue_child_1)
 
         response = self.client.get(
-            f"/api/workflow/{self.et_children_starwars.pk}/?version_id={self.workflow_version_et_children_starwars_inaccessible_reference_form.pk}"
+            f"/api/workflow/{self.et_children_blue.pk}/?version_id={self.workflow_version_et_children_blue_inaccessible_reference_form.pk}"
         )
 
         self.assertJSONResponse(response, 404)
         assert "Cannot Access Reference Form for Workflow Version" in response.data
 
     def test_user_with_auth_access_entity_ok_no_access_to_follow_ups_form(self):
-        self.client.force_authenticate(self.anakin)
+        self.client.force_authenticate(self.blue_child_1)
 
         response = self.client.get(
-            f"/api/workflow/{self.et_children_starwars2.pk}/?version_id={self.workflow_version_et_children_starwars2_inaccessible_followup_form.pk}"
+            f"/api/workflow/{self.et_children_blue2.pk}/?version_id={self.workflow_version_et_children_blue2_inaccessible_followup_form.pk}"
         )
 
         self.assertJSONResponse(response, 404)
         assert "Cannot Access FollowUps Form for Workflow" in response.data
 
     def test_user_with_auth_access_entity_ok_no_access_to_changes_forms(self):
-        self.client.force_authenticate(self.anakin)
+        self.client.force_authenticate(self.blue_child_1)
 
         response = self.client.get(
-            f"/api/workflow/{self.et_children_starwars3.pk}/?version_id={self.workflow_version_et_children_starwars3_inaccessible_changes.pk}"
+            f"/api/workflow/{self.et_children_blue3.pk}/?version_id={self.workflow_version_et_children_blue3_inaccessible_changes.pk}"
         )
 
         self.assertJSONResponse(response, 404)
         assert "Cannot Access Changes Form for Workflow" in response.data
 
     def test_user_all_access_ok(self):
-        self.client.force_authenticate(self.yoda)
+        self.client.force_authenticate(self.blue_adult_1)
 
-        response = self.client.get(f"/api/workflow/{self.et_adults_starwars.pk}/")
+        set_tl_schema = {
+            "type": "object",
+            "properties": {
+                "count": {"type": "number"},
+                "has_next": {"type": "boolean"},
+                "has_previous": {"type": "boolean"},
+                "limit": {"type": "number"},
+                "page": {"type": "number"},
+                "pages": {"type": "number"},
+                "results": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "status": {"type": "string"},
+                            "created_at": {"type": "string"},
+                            "updated_at": {"type": "string"},
+                            "version_id": {"type": "number"},
+                            "name": {"type": "string"},
+                        },
+                    },
+                },
+            },
+            "required": ["count", "has_next", "has_previous", "limit", "page", "pages", "results"],
+        }
+
+        response = self.client.get(f"/api/workflow/{self.et_adults_blue.pk}/")
+
+        var_dump(response)
 
         self.assertJSONResponse(response, 200)
         self.assertEqual(response.json()["count"], 1)  # 1 version available
 
-    def test_view_all_versions(self):
-        pass
+        try:
+            jsonschema.validate(instance=response.data, schema=set_tl_schema)
+        except jsonschema.exceptions.ValidationError as ex:
+            self.fail(msg=str(ex))
 
     def test_view_specific_version(self):
-        pass
+        self.client.force_authenticate(self.blue_adult_1)
+
+        set_tl_schema = {
+            "type": "object",
+            "properties": {
+                "status": {"type": "string"},
+                "name": {"type": "string"},
+                "updated_at": {"type": "string"},
+                "reference_form": {"type": "object"},
+                "entity_type": {
+                    "type": "object",
+                    "properties": {"account": {"type": "number"}, "id": {"type": "number"}, "name": {"type": "string"}},
+                },
+                "follow_ups": {"type": "array"},
+                "version_id": {"type": "number"},
+            },
+            "required": ["status", "name", "updated_at", "reference_form", "entity_type", "follow_ups", "version_id"],
+        }
+
+        response = self.client.get(
+            f"/api/workflow/{self.et_adults_blue.pk}/?version_id={self.workflow_version_et_adults_blue.pk}"
+        )
+
+        self.assertJSONResponse(response, 200)
+
+        try:
+            jsonschema.validate(instance=response.data, schema=set_tl_schema)
+        except jsonschema.exceptions.ValidationError as ex:
+            self.fail(msg=str(ex))
 
     def test_new_version_empty(self):
         pass

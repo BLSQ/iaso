31d46604f0742b30cd2634d9dbdf6b05ea508c0f
Matt Vadash <m@dcy.io>
2022-12-05 15:33:17 +0100

    Fix tests types

--- iaso/models/workflow.py
+++ iaso/models/workflow.py
@@ -1,3 +1,5 @@
+from typing import Set
+
 from django.db import models
 from iaso.models.entity import EntityType, Form
 from ..utils.models.soft_deletable import SoftDeletableModel
@@ -35,7 +37,7 @@ class WorkflowVersionsStatus(models.TextChoices):
     PUBLISHED = "P", "Published"
 
     def is_transition_allowed(self, new_status: "WorkflowVersionsStatus"):
-        allowed_set = WorkflowVersionsStatusAllowedTransitions.get(self.value, {})
+        allowed_set: Set[str] = WorkflowVersionsStatusAllowedTransitions.get(self.value, {})
         return new_status.value in allowed_set
 
 

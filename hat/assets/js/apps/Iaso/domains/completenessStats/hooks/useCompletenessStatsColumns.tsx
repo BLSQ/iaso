d096f9e963dcff266f6c7e16c2c86b0943e7a939
Nicolas Noé <nicolas@niconoe.eu>
2022-12-07 15:48:57 +0100

    Eslint

--- hat/assets/js/apps/Iaso/domains/completenessStats/hooks/useCompletenessStatsColumns.tsx
+++ hat/assets/js/apps/Iaso/domains/completenessStats/hooks/useCompletenessStatsColumns.tsx
@@ -12,65 +12,53 @@ export const useCompletenessStatsColumns = () => {
                 id: 'parent_org_unit__name',
                 accessor: 'parent_org_unit__name',
                 sortable: false,
-                Cell: settings => {
-                    return (
-                        <span>
-                            {settings.row.original.parent_org_unit?.[0].name ??
-                                '--'}
-                        </span>
-                    );
-                },
+                Cell: settings => (
+                    <span>
+                        {settings.row.original.parent_org_unit?.[0].name ??
+                            '--'}
+                    </span>
+                ),
             },
             {
                 Header: formatMessage(MESSAGES.orgUnitType),
                 id: 'org_unit_type__name',
                 accessor: 'org_unit_type__name',
                 sortable: true,
-                Cell: settings => {
-                    return (
-                        <span>
-                            {settings.row.original.org_unit_type?.name ?? '--'}
-                        </span>
-                    );
-                },
+                Cell: settings => (
+                    <span>
+                        {settings.row.original.org_unit_type?.name ?? '--'}
+                    </span>
+                ),
             },
             {
                 Header: formatMessage(MESSAGES.orgUnit),
                 id: 'name',
                 accessor: 'name',
                 sortable: true,
-                Cell: settings => {
-                    return (
-                        <span>
-                            {settings.row.original.org_unit?.name ?? '--'}
-                        </span>
-                    );
-                },
+                Cell: settings => (
+                    <span>{settings.row.original.org_unit?.name ?? '--'}</span>
+                ),
             },
             {
                 Header: formatMessage(MESSAGES.form),
                 id: 'form__name',
                 accessor: 'form__name',
                 sortable: false,
-                Cell: settings => {
-                    return (
-                        <span>{settings.row.original.form?.name ?? '--'}</span>
-                    );
-                },
+                Cell: settings => (
+                    <span>{settings.row.original.form?.name ?? '--'}</span>
+                ),
             },
             {
                 Header: formatMessage(MESSAGES.formsFilledDirect),
                 id: 'forms_filled_direct',
                 accessor: 'forms_filled_direct',
                 sortable: false,
-                Cell: settings => {
-                    return (
-                        <span>
-                            {settings.row.original.forms_filled_direct ?? '--'}/
-                            {settings.row.original.forms_to_fill_direct ?? '--'}
-                        </span>
-                    );
-                },
+                Cell: settings => (
+                    <span>
+                        {settings.row.original.forms_filled_direct ?? '--'}/
+                        {settings.row.original.forms_to_fill_direct ?? '--'}
+                    </span>
+                ),
             },
             {
                 Header: formatMessage(MESSAGES.completenessDirect),
@@ -83,14 +71,12 @@ export const useCompletenessStatsColumns = () => {
                 id: 'forms_filled',
                 accessor: 'forms_filled',
                 sortable: false,
-                Cell: settings => {
-                    return (
-                        <span>
-                            {settings.row.original.forms_filled ?? '--'}/
-                            {settings.row.original.forms_to_fill ?? '--'}
-                        </span>
-                    );
-                },
+                Cell: settings => (
+                    <span>
+                        {settings.row.original.forms_filled ?? '--'}/
+                        {settings.row.original.forms_to_fill ?? '--'}
+                    </span>
+                ),
             },
             {
                 Header: formatMessage(MESSAGES.completenessWithDescendants),

58fe39fa4a4c44709bbd29450dd236b5f6848ce5
Quang Son Le <qle@bluesquarehub.com>
2022-12-07 14:55:34 +0100

    POLIO-683: remove debounce from TextInput

--- plugins/polio/js/src/components/Inputs/TextInput.js
+++ plugins/polio/js/src/components/Inputs/TextInput.js
@@ -1,14 +1,6 @@
-import React, {
-    useState,
-    useEffect,
-    useCallback,
-    useRef,
-    useMemo,
-} from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { useDebounce } from 'use-debounce';
 import { TextField } from '@material-ui/core';
-import { useSkipEffectOnMount } from 'bluesquare-components';
 import { get } from 'lodash';
 
 export const TextInput = ({
@@ -16,46 +8,19 @@ export const TextInput = ({
     form = {},
     value,
     touchOnFocus = true,
-    debounceTime = 500,
     ...props
 } = {}) => {
-    const prevValue = useRef();
-    const prevDebounced = useRef();
-    const [textValue, setTextValue] = useState(value ?? '');
-    const [debouncedValue] = useDebounce(textValue, debounceTime);
-
     const hasError =
         form.errors &&
         Boolean(get(form.errors, field.name) && get(form.touched, field.name));
 
     const handleChangeAndFocus = useCallback(
-        newValue => {
+        e => {
             form?.setFieldTouched(field.name, true);
-            form.setFieldValue(field.name, newValue);
+            field?.onChange(e);
         },
         [form, field],
     );
-
-    const handleChange = useMemo(
-        () => (touchOnFocus ? field.onChange : handleChangeAndFocus),
-        [field.onChange, handleChangeAndFocus, touchOnFocus],
-    );
-
-    // Reset state when value changes to prevent wrongly persisting the state value
-    useEffect(() => {
-        if (value !== prevValue.current) {
-            setTextValue(value ?? '');
-            prevValue.current = value;
-        }
-    }, [value]);
-
-    useSkipEffectOnMount(() => {
-        if (debouncedValue !== prevDebounced.current) {
-            // Only call onChange if debouncedVAlue has been updated to avoid unwanted overwrites
-            prevDebounced.current = debouncedValue;
-            handleChange(debouncedValue);
-        }
-    }, [debouncedValue, handleChange, prevValue.current, handleChange]);
 
     return (
         <TextField
@@ -73,10 +38,8 @@ export const TextInput = ({
                     : undefined
             }
             onBlur={touchOnFocus ? field.onBlur : undefined}
-            onChange={e => {
-                setTextValue(e.target.value);
-            }}
-            value={textValue}
+            onChange={touchOnFocus ? field.onChange : handleChangeAndFocus}
+            value={field.value ?? value ?? ''}
             error={hasError}
             helperText={hasError ? get(form.errors, field.name) : undefined}
         />
@@ -88,7 +51,6 @@ TextInput.defaultProps = {
     form: {},
     value: undefined,
     touchOnFocus: true,
-    debounceTime: 500,
 };
 
 TextInput.propTypes = {
@@ -96,5 +58,4 @@ TextInput.propTypes = {
     form: PropTypes.object,
     value: PropTypes.any,
     touchOnFocus: PropTypes.bool,
-    debounceTime: PropTypes.number,
 };

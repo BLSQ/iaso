d27fe95cbcab08bb63d39d14e9c0e2326246859f
Olivier Le Thanh Duong <olivier@lethanh.be>
2022-12-22 16:48:43 +0100

    fix migration for tests

--- plugins/polio/migrations/0116_fill_budgetstep_to_from_node.py
+++ plugins/polio/migrations/0116_fill_budgetstep_to_from_node.py
@@ -9,6 +9,8 @@ import plugins
 def budgetstep_fill_from_to_nodes(apps, schema_editor):
     BudgetStep = apps.get_model("polio", "BudgetStep")
     workflow = get_workflow()
+    if not workflow:
+        return
     steps_to_fix = BudgetStep.objects.filter(node_key_to__isnull=True, node_key_from__isnull=True)
     logging.info(f"Migration: {steps_to_fix.count()} steps without nodes info")
     step: "plugins.polio.budget.models.BudgetStep"

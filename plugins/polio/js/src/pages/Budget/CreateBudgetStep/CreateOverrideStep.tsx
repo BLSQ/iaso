657a1ca83cb3780b30fad51005d124c7cd1ffd94
Quang Son Le <qle@bluesquarehub.com>
2022-12-23 15:25:23 +0100

    POLIO-766: add OverrideStep modal+button

--- plugins/polio/js/src/pages/Budget/CreateBudgetStep/CreateOverrideStep.tsx
+++ plugins/polio/js/src/pages/Budget/CreateBudgetStep/CreateOverrideStep.tsx
@@ -1,8 +1,13 @@
+/* eslint-disable camelcase */
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/no-unused-prop-types */
-import React, { FunctionComponent } from 'react';
+import React, {
+    FunctionComponent,
+    useCallback,
+    useEffect,
+    useMemo,
+} from 'react';
 import { useFormik, FormikProvider } from 'formik';
-import { isEqual } from 'lodash';
 import {
     // @ts-ignore
     useSafeIntl,
@@ -13,72 +18,99 @@ import {
     // @ts-ignore
     makeFullModal,
 } from 'bluesquare-components';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Chip, Divider, makeStyles, Typography } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 import MESSAGES from '../../../constants/messages';
 import InputComponent from '../../../../../../../hat/assets/js/apps/Iaso/components/forms/InputComponent';
 import { useCurrentUser } from '../../../../../../../hat/assets/js/apps/Iaso/utils/usersUtils';
-
-import { useBudgetStepValidation } from '../hooks/validation';
+import { useUserHasTeam } from '../../../hooks/useGetTeams';
 import {
-    useGetTeamsDropDown,
-    useUserHasTeam,
-} from '../../../hooks/useGetTeams';
-
-import {
-    useTranslatedErrors,
     useApiErrorValidation,
+    useTranslatedErrors,
 } from '../../../../../../../hat/assets/js/apps/Iaso/libs/validation';
-import { useSaveBudgetStep } from '../hooks/api/useSaveBudgetStep';
-import { AddStepButton } from './AddStepButton';
-import { commaSeparatedIdsToArray } from '../../../../../../../hat/assets/js/apps/Iaso/utils/forms';
+import { OverrideStepForm } from '../types';
 import { UserHasTeamWarning } from './UserHasTeamWarning';
+import { AddMultipleLinks } from '../MultipleLinks/AddMultipleLinks';
+import { useOverrideStepValidation } from '../hooks/validation';
+import { redirectToReplace } from '../../../../../../../hat/assets/js/apps/Iaso/routing/actions';
+import { BUDGET_DETAILS } from '../../../constants/routes';
+import { TextArea } from '../../../../../../../hat/assets/js/apps/Iaso/components/forms/TextArea';
+import { useGetRecipientTeams } from '../hooks/api/useGetEmailRecipients';
+import { OverrideStepButton } from './OverrideStepButton';
+import { useSaveOverrideStep } from '../hooks/api/useSaveOverrideStep';
 
 type Props = {
     campaignId: string;
-    transitionKey: string;
-    transitionLabel: string;
     closeDialog: () => void;
     isOpen: boolean;
     id?: string;
     isMobileLayout?: boolean;
+    requiredFields?: string[];
+    params: Record<string, any>;
+    recipients?: number[]; // team ids
 };
+
+const useStyles = makeStyles({
+    alignRight: { textAlign: 'right' },
+});
 
 const CreateOverrideStep: FunctionComponent<Props> = ({
     campaignId,
     closeDialog,
     isOpen,
-    transitionKey,
-    transitionLabel,
-    id = '',
+    id,
+    requiredFields = [],
+    params,
+    recipients = [],
 }) => {
     const currentUser = useCurrentUser();
     const { data: userHasTeam } = useUserHasTeam(currentUser?.user_id);
     const { formatMessage } = useSafeIntl();
-    const { mutateAsync: saveBudgetStep } = useSaveBudgetStep();
-    const { data: teamOptions, isFetching: isFetchingTeams } =
-        useGetTeamsDropDown();
-    // TODO replace with endpoint when available
-    const stepOptions = [];
+    // TODO replace
+    const { mutateAsync: saveOverrideStep } = useSaveOverrideStep();
+    const classes = useStyles();
+    const dispatch = useDispatch();
+    const onSuccess = useCallback(() => {
+        const trimmedParams = { ...params };
+        if (params.quickTransition) {
+            delete trimmedParams.quickTransition;
+        }
+        if (params.previousStep) {
+            delete trimmedParams.previousStep;
+        }
+        dispatch(redirectToReplace(BUDGET_DETAILS, trimmedParams));
+    }, [dispatch, params]);
+
+    // TODO update
+    const { data: recipientTeams } = useGetRecipientTeams(recipients);
 
     const {
         apiErrors,
         payload,
+
         mutation: save,
     } = useApiErrorValidation<Partial<any>, any>({
-        mutationFn: saveBudgetStep,
+        mutationFn: saveOverrideStep,
+        onSuccess,
     });
-    const validationSchema = useBudgetStepValidation(apiErrors, payload);
-    const formik = useFormik({
+    const validationSchema = useOverrideStepValidation(
+        apiErrors,
+        payload,
+        requiredFields,
+    );
+
+    const formik = useFormik<OverrideStepForm>({
         initialValues: {
-            transition_key: transitionKey,
+            new_state_key: undefined,
             campaign: campaignId,
-            comment: null,
-            files: null,
-            links: null,
-            amount: null,
-            target_teams: [],
-            transition: null,
+            comment: undefined,
+            files: undefined,
+            links: undefined,
+            amount: undefined,
+            // this value is for handling non-field arrors from api
             general: null,
+            // This value is to handle error state when either files or links are required
+            attachments: null,
         },
         enableReinitialize: true,
         validateOnBlur: true,
@@ -92,29 +124,53 @@ const CreateOverrideStep: FunctionComponent<Props> = ({
         setFieldTouched,
         errors,
         isValid,
-        initialValues,
         handleSubmit,
         resetForm,
     } = formik;
+    const onChange = useCallback(
+        (keyValue, value) => {
+            setFieldTouched(keyValue, true);
+            setFieldValue(keyValue, value);
+        },
+        [setFieldTouched, setFieldValue],
+    );
 
-    const onChange = (keyValue, value) => {
-        setFieldTouched(keyValue, true);
-        setFieldValue(keyValue, value);
-    };
+    const onCommentChange = useCallback(
+        newValue => onChange('comment', newValue),
+        [onChange],
+    );
     const getErrors = useTranslatedErrors({
         touched,
         errors,
         messages: MESSAGES,
         formatMessage,
     });
+    const attachmentErrors = useMemo(() => {
+        const anyFieldTouched = Object.values(touched).find(value => value);
+        const attachmentsErrors = [errors.attachments] ?? [];
+        if (anyFieldTouched) return attachmentsErrors;
+        return [];
+    }, [errors.attachments, touched]);
 
-    const titleMessage = transitionLabel;
+    useEffect(() => {
+        const formHasBeenTouched = touched.links || touched.amount;
+        if (!formHasBeenTouched) {
+            const { links, amount } = values;
+            if ((links?.length ?? []) > 0) {
+                setFieldTouched('links', true);
+            }
+            if (amount) {
+                setFieldTouched('amount', true);
+            }
+        }
+    }, [setFieldTouched, touched.amount, touched.links, values]);
 
+    const allowConfirm = isValid;
     return (
         <FormikProvider value={formik}>
             <ConfirmCancelModal
-                allowConfirm={isValid && !isEqual(values, initialValues)}
-                titleMessage={titleMessage}
+                allowConfirm={allowConfirm}
+                titleMessage="Override"
                 onConfirm={() => {
                     if (userHasTeam) {
                         handleSubmit();
@@ -130,100 +186,137 @@ const CreateOverrideStep: FunctionComponent<Props> = ({
                 confirmMessage={MESSAGES.send}
                 open={isOpen}
                 closeDialog={closeDialog}
-                id={id}
+                id={id ?? ''}
                 dataTestId="Test-modal"
                 onClose={() => null}
             >
-                {userHasTeam && (
-                    <>
-                        <InputComponent
-                            type="select"
-                            required
-                            keyValue="type"
-                            onChange={(keyValue, value) => {
-                                onChange(keyValue, value);
-                            }}
-                            value={values.type}
-                            errors={getErrors('type')}
-                            label={MESSAGES.step}
-                            options={stepOptions}
-                            loading={false} // TODO replace with query status when available
-                        />
+                <>
+                    <Box mb={2}>
                         <InputComponent
                             type="text"
-                            keyValue="comment"
-                            multiline
+                            keyValue="new_state_key"
                             onChange={onChange}
-                            value={values.comment}
-                            errors={getErrors('comment')}
-                            label={MESSAGES.notes}
-                        />
-                        <InputComponent
-                            type="number"
-                            keyValue="amount"
-                            onChange={onChange}
-                            value={values.amount}
-                            errors={getErrors('amount')}
-                            label={MESSAGES.amount}
-                        />
-
-                        <Box mt={2}>
-                            <FilesUpload
-                                files={values.files ?? []}
-                                onFilesSelect={files => {
-                                    setFieldTouched('files', true);
-                                    setFieldValue('files', files);
-                                }}
-                            />
-                        </Box>
-
-                        <InputComponent
-                            type="text"
-                            keyValue="links"
-                            multiline
-                            onChange={onChange}
-                            value={values.links}
-                            errors={getErrors('links')}
-                            label={MESSAGES.links}
-                        />
-                        <InputComponent
-                            type="select"
+                            value={values.new_state_key}
+                            errors={getErrors('new_state_key')}
+                            label={MESSAGES.newBudgetState}
                             required
-                            keyValue="target_teams"
-                            multi
-                            onChange={(keyValue, value) => {
-                                onChange(
-                                    keyValue,
-                                    commaSeparatedIdsToArray(value),
-                                );
-                            }}
-                            value={values.target_teams}
-                            errors={getErrors('target_teams')}
-                            label={MESSAGES.destination}
-                            options={teamOptions}
-                            loading={isFetchingTeams}
                         />
-                        {/* @ts-ignore */}
-                        {(errors?.general ?? []).length > 0 && (
+                    </Box>
+                    <TextArea
+                        value={values.comment}
+                        errors={getErrors('comment')}
+                        label={formatMessage(MESSAGES.notes)}
+                        onChange={onCommentChange}
+                        required={requiredFields.includes('comment')}
+                        debounceTime={0}
+                    />
+                    <InputComponent
+                        type="number"
+                        keyValue="amount"
+                        onChange={onChange}
+                        value={values.amount}
+                        errors={getErrors('amount')}
+                        label={MESSAGES.amount}
+                        required={requiredFields.includes('amount')}
+                    />
+
+                    <Box mt={2}>
+                        <FilesUpload
+                            files={values.files ?? []}
+                            onFilesSelect={files => {
+                                setFieldTouched('files', true);
+                                setFieldValue('files', files);
+                            }}
+                            required={requiredFields.includes('files')}
+                            errors={getErrors('files')}
+                        />
+                    </Box>
+                    <Box mt={2}>
+                        <Divider />
+                    </Box>
+                    <Box mt={2}>
+                        <AddMultipleLinks
+                            required={requiredFields.includes('links')}
+                        />
+                    </Box>
+                    <Box mt={2} mb={2}>
+                        <Divider />
+                    </Box>
+                    {(recipientTeams ?? []).length > 0 && (
+                        <>
+                            <Box mt={1} mb={1}>
+                                <Typography>
+                                    {formatMessage(MESSAGES.emailWillBeSentTo)}
+                                </Typography>
+                                {recipientTeams?.map(team => {
+                                    return (
+                                        <Box
+                                            mt={1}
+                                            mr={1}
+                                            mb={1}
+                                            display="inline-block"
+                                            key={team}
+                                        >
+                                            <Chip
+                                                label={team}
+                                                variant="outlined"
+                                                color="secondary"
+                                            />
+                                        </Box>
+                                    );
+                                })}
+                            </Box>
+                            <Box mt={2} mb={2}>
+                                <Divider />
+                            </Box>
+                        </>
+                    )}
+
+                    {/* @ts-ignore */}
+                    {(errors?.general ?? []).length > 0 && (
+                        <>
+                            {getErrors('general').map(e => (
+                                <Typography
+                                    key={`${e}-error`}
+                                    color="error"
+                                    className={classes.alignRight}
+                                >
+                                    {e}
+                                </Typography>
+                            ))}
+                        </>
+                    )}
+                    {attachmentErrors.length > 0 &&
+                        (touched.links || touched.files) && (
                             <>
-                                {getErrors('general').map(e => (
+                                {attachmentErrors.map(e => (
                                     <Typography
                                         key={`${e}-error`}
                                         color="error"
+                                        className={classes.alignRight}
                                     >
                                         {e}
                                     </Typography>
                                 ))}
                             </>
                         )}
-                    </>
-                )}
+                    {!touched.links &&
+                        !touched.files &&
+                        requiredFields.includes('attachments') && (
+                            <Typography
+                                color="textSecondary"
+                                className={classes.alignRight}
+                            >
+                                {formatMessage(MESSAGES.linksOrFilesRequired)}
+                            </Typography>
+                        )}
+                </>
                 {!userHasTeam && <UserHasTeamWarning />}
             </ConfirmCancelModal>
         </FormikProvider>
     );
 };
 
-const modalWithButton = makeFullModal(CreateOverrideStep, AddStepButton);
+const modalWithButton = makeFullModal(CreateOverrideStep, OverrideStepButton);
 
 export { modalWithButton as CreateOverrideStep };

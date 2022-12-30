8eb695552866783636328b21a19ac03bec2d25fb
Simon Dusausoy <sdusausoy@bluesquarehub.com>
2022-12-30 13:46:51 +0100

    tests

---
+++ iaso/tests/api/test_reports.py
@@ -0,0 +1,56 @@
+from django.core.files import File
+from django.utils.timezone import now
+
+from iaso import models as m
+from iaso.models import Page, Report, ReportVersion
+from iaso.test import APITestCase
+
+
+class ReportsAPITestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.now = now()
+
+        kame_house = m.Account.objects.create(name="Kame House")
+        konoa = m.Account.objects.create(name="Konoa")
+        cls.sayanj = m.Project.objects.create(account=kame_house, name="sayanJ")
+
+        cls.goku = cls.create_user_with_profile(username="Goku", account=kame_house)
+        cls.kefla = cls.create_user_with_profile(username="Kefla", account=kame_house)
+        cls.kakashin = cls.create_user_with_profile(username="Kakashi", account=konoa)
+
+    def test_get_reports_web(self):
+        self.client.force_authenticate(self.kefla)
+
+        file = File(open("iaso/tests/fixtures/test_user_bulk_create_valid.csv", "rb"))
+
+        report_version = ReportVersion.objects.create(
+            file=file,
+            name="TEST_REPORT_VERSION_A",
+            status="published",
+        )
+
+        Report.objects.create(name="TEST_REPORT_A", published_version=report_version, project=self.sayanj)
+
+        response = self.client.get("/api/reports/")
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.json()[0]["name"], "TEST_REPORT_A")
+
+    def test_get_reports_mobile(self):
+        self.client.force_authenticate(self.kefla)
+
+        file = File(open("iaso/tests/fixtures/test_user_bulk_create_valid.csv", "rb"))
+
+        report_version = ReportVersion.objects.create(
+            file=file,
+            name="TEST_REPORT_VERSION_A",
+            status="published",
+        )
+
+        Report.objects.create(name="TEST_REPORT_A", published_version=report_version, project=self.sayanj)
+
+        response = self.client.get("/api/mobile/reports/")
+
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(response.json()[0]["name"], "TEST_REPORT_A")

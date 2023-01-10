dd0be4cb2a3353b0b9259261e96915cb30f6b3a7
Quang Son Le <qle@bluesquarehub.com>
2023-01-10 10:11:14 +0100

    IA-1808: split LinkValue in own file

---
+++ hat/assets/js/apps/Iaso/domains/links/components/LinkValue.tsx
@@ -0,0 +1,59 @@
+import React, { FunctionComponent } from 'react';
+import { makeStyles } from '@material-ui/core';
+// ts complaints about the import of moment for some reason
+// @ts-ignore
+import moment from 'moment';
+// @ts-ignore
+import { textPlaceholder, useSafeIntl } from 'bluesquare-components';
+import GeoJsonMap from '../../../components/maps/GeoJsonMapComponent';
+import { getOrgUnitParentsString } from '../../orgUnits/utils';
+import { MESSAGES } from '../messages';
+
+const useStyles = makeStyles(theme => ({
+    cellMap: {
+        margin: -theme.spacing(2),
+    },
+}));
+
+type Props = {
+    linkKey: string;
+    value?: any;
+    link?: any;
+};
+
+export const LinkValue: FunctionComponent<Props> = ({
+    linkKey,
+    value = null,
+    link = null,
+}) => {
+    const { formatMessage } = useSafeIntl();
+    const classes = useStyles();
+    if (!value || value.toString().length === 0) return textPlaceholder;
+    switch (linkKey) {
+        case 'geo_json': {
+            return (
+                <div className={classes.cellMap}>
+                    <GeoJsonMap geoJson={value} />
+                </div>
+            );
+        }
+        case 'status': {
+            return value
+                ? formatMessage(MESSAGES.validated)
+                : formatMessage(MESSAGES.notValidated);
+        }
+        case 'groups': {
+            return value.map(g => g.name).join(', ');
+        }
+        case 'created_at':
+        case 'updated_at': {
+            return moment.unix(value).format('LTS');
+        }
+        case 'parent': {
+            return getOrgUnitParentsString(link);
+        }
+
+        default:
+            return value.toString();
+    }
+};
--- hat/assets/js/apps/Iaso/domains/links/components/LinksValueComponent.tsx
+++ hat/assets/js/apps/Iaso/domains/links/components/LinksValueComponent.tsx
@@ -1,15 +1,9 @@
 import React, { FunctionComponent } from 'react';
 import { makeStyles, TableCell, TableRow } from '@material-ui/core';
-// ts complaints about the import of moment for some reason
 // @ts-ignore
-import moment from 'moment';
-// @ts-ignore
-import { textPlaceholder, useSafeIntl } from 'bluesquare-components';
-import GeoJsonMap from '../../../components/maps/GeoJsonMapComponent';
-import { getOrgUnitParentsString } from '../../orgUnits/utils';
-
+import { useSafeIntl } from 'bluesquare-components';
 import MESSAGES from '../../forms/messages';
-import { MESSAGES as LINKS_MESSAGES } from '../messages';
+import { LinkValue } from './LinkValue';
 
 const useStyles = makeStyles(theme => ({
     cell: {
@@ -22,9 +16,6 @@ const useStyles = makeStyles(theme => ({
     isDifferentValidated: {
         backgroundColor: theme.palette.primary.main,
         color: 'white',
-    },
-    cellMap: {
-        margin: -theme.spacing(2),
     },
 }));
 
@@ -39,38 +30,6 @@ const ignoredKeys = [
     'sub_source',
     'source',
 ];
-
-const LinkValue = ({ linkKey, link, value, classes }) => {
-    const { formatMessage } = useSafeIntl();
-    if (!value || value.toString().length === 0) return textPlaceholder;
-    switch (linkKey) {
-        case 'geo_json': {
-            return (
-                <div className={classes.cellMap}>
-                    <GeoJsonMap geoJson={value} />
-                </div>
-            );
-        }
-        case 'status': {
-            return value
-                ? formatMessage(LINKS_MESSAGES.validated)
-                : formatMessage(LINKS_MESSAGES.notValidated);
-        }
-        case 'groups': {
-            return value.map(g => g.name).join(', ');
-        }
-        case 'created_at':
-        case 'updated_at': {
-            return moment.unix(value).format('LTS');
-        }
-        case 'parent': {
-            return getOrgUnitParentsString(link);
-        }
-
-        default:
-            return value.toString();
-    }
-};
 
 type Props = {
     linkKey: string;
@@ -104,12 +63,7 @@ export const LinksValue: FunctionComponent<Props> = ({
                 {!MESSAGES[linkKey] && linkKey}
             </TableCell>
             <TableCell className={className}>
-                <LinkValue
-                    linkKey={linkKey}
-                    value={value}
-                    classes={classes}
-                    link={link}
-                />
+                <LinkValue linkKey={linkKey} value={value} link={link} />
             </TableCell>
         </TableRow>
     );

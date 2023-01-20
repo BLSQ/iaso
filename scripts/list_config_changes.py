2f81ae51ed17d20b8e03014247c2e494b1e1da13
Quang Son Le <qle@bluesquarehub.com>
2023-01-20 16:30:12 +0100

    IA-1847: black formatting

--- scripts/list_config_changes.py
+++ scripts/list_config_changes.py
@@ -5,9 +5,9 @@ import re
 file_regexp = r"\(?<!`)(?<=file:)*([\n|\r\n|\r])"
 admin_regexp = r"\(?<!`)(?<=admin:)*([\n|\r\n|\r])"
 
-file_paths_list = re.findall(file_regexp,sys.argv[1])
-admin_paths_list = re.findall(admin_regexp,sys.argv[1])
+file_paths_list = re.findall(file_regexp, sys.argv[1])
+admin_paths_list = re.findall(admin_regexp, sys.argv[1])
 
-with open(sys.argv[2]+'.txt', "w") as output_file:
-    for index in range (len(file_paths_list)):
-        output_file.write("file: "+file_paths_list[index]+"\n"+"admin: "+admin_paths_list[index]+"\n\n")
+with open(sys.argv[2] + ".txt", "w") as output_file:
+    for index in range(len(file_paths_list)):
+        output_file.write("file: " + file_paths_list[index] + "\n" + "admin: " + admin_paths_list[index] + "\n\n")

b69653f0b22bf5f9cd9e18cb844c416569feedc6
Quang Son Le <qle@bluesquarehub.com>
2023-01-20 18:21:27 +0100

    IA-1847 debug

--- scripts/list_config_changes.py
+++ scripts/list_config_changes.py
@@ -2,19 +2,15 @@
 import sys
 import re
 
-print("script started")
 print(sys.argv[1], sys.argv[2])
 
 file_regexp = re.compile("(?<!`)(?<=file:)*([\n])")
 admin_regexp = re.compile("(?<!`)(?<=admin:)*([\n])")
-print("defined regexes")
 file_paths_list = re.findall(file_regexp, sys.argv[1])
 admin_paths_list = re.findall(admin_regexp, sys.argv[1])
-print("ran findall")
 
 with open(sys.argv[2] + ".txt", "w") as output_file:
-    print(sys.argv[2])
-    # for index in range(len(file_paths_list)):
-    #     print(file_paths_list[index])
-    #     print(admin_paths_list[index])
+    for index in range(len(file_paths_list)):
+        print(file_paths_list[index])
+        print(admin_paths_list[index])
     # output_file.write("file: " + file_paths_list[index] + "\n" + "admin: " + admin_paths_list[index] + "\n\n")

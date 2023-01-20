d554c6607300b7b6a63c5959d6db50b76a7bc9e3
Quang Son Le <qle@bluesquarehub.com>
2023-01-20 16:54:09 +0100

    IA-1847: correction regex

--- scripts/list_config_changes.py
+++ scripts/list_config_changes.py
@@ -2,7 +2,7 @@
 import sys
 import re
 
-file_regexp = r"\(?<!`)(?<=file:)*([\n|\r\n|\r])"
+file_regexp = r"(?<!`)(?<=file:)*([\n|\r\n|\r])"
 admin_regexp = r"\(?<!`)(?<=admin:)*([\n|\r\n|\r])"
 
 file_paths_list = re.findall(file_regexp, sys.argv[1])
@@ -10,4 +10,6 @@ admin_paths_list = re.findall(admin_regexp, sys.argv[1])
 
 with open(sys.argv[2] + ".txt", "w") as output_file:
     for index in range(len(file_paths_list)):
+        print(file_paths_list[index])
+        print(admin_paths_list[index])
         output_file.write("file: " + file_paths_list[index] + "\n" + "admin: " + admin_paths_list[index] + "\n\n")

dbc2dd92d94e65178e47386e5f232516518a7d3e
Quang Son Le <qle@bluesquarehub.com>
2023-01-23 10:02:57 +0100

    IA-1847: add test value for output file

--- scripts/list_config_changes.py
+++ scripts/list_config_changes.py
@@ -9,8 +9,17 @@ admin_regexp = re.compile("(?<!`admin:)(?<=admin:).+(?=\n)")
 file_paths_list = re.findall(file_regexp, sys.argv[1])
 admin_paths_list = re.findall(admin_regexp, sys.argv[1])
 
-with open(sys.argv[2] + ".txt", "w") as output_file:
-    for index in range(len(file_paths_list)):
-        print(file_paths_list[index])
-        print(admin_paths_list[index])
-    output_file.write("file: " + file_paths_list[index] + "\n" + "admin: " + admin_paths_list[index] + "\n\n")
+# test code
+if len(file_paths_list) == 0:
+    file_paths_list = ["test file path"]
+if len(admin_paths_list) == 0:
+    file_paths_list = ["test admin path"]
+
+# end test code
+
+if len(file_paths_list) > 0 and len(admin_paths_list) > 0:
+    with open(sys.argv[2] + ".txt", "w") as output_file:
+        for index in range(len(file_paths_list)):
+            print(file_paths_list[index])
+            print(admin_paths_list[index])
+            output_file.write("file: " + file_paths_list[index] + "\n" + "admin: " + admin_paths_list[index] + "\n\n")

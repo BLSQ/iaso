b9c55b0d7d511f51367c54e63a368c6d554b7496
Quang Son Le <qle@bluesquarehub.com>
2023-01-23 18:06:46 +0100

    IA-1713: refactor InstanceMap to function comp

--- hat/assets/js/apps/Iaso/domains/instances/components/InstancesMap/InstancesMap.tsx
+++ hat/assets/js/apps/Iaso/domains/instances/components/InstancesMap/InstancesMap.tsx
@@ -1,6 +1,5 @@
-import React, { Component, FunctionComponent, useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { FunctionComponent, useCallback, useMemo, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Map, ScaleControl, TileLayer } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 import { Grid, Divider } from '@material-ui/core';
@@ -13,154 +12,136 @@ import {
     defaultZoom,
 } from '../../../../utils/mapUtils';
 
-import { resetMapReducer } from '../../../../redux/mapReducer';
-import { setCurrentInstance } from '../../actions';
-
 import TileSwitch from '../../../../components/maps/tools/TileSwitchComponent';
 import ClusterSwitch from '../../../../components/maps/tools/ClusterSwitchComponent';
 import MarkersListComponent from '../../../../components/maps/markers/MarkersListComponent';
 import InnerDrawer from '../../../../components/nav/InnerDrawer';
 
 import InstancePopupComponent from '../InstancePopupComponent';
-import { warningSnackBar } from '../../../../constants/snackBars';
-import {
-    enqueueSnackbar,
-    closeFixedSnackbar,
-} from '../../../../redux/snackBarsReducer';
 
-import { fetchInstanceDetail } from '../../../../utils/requests';
 import { useShowWarning } from './useShowWarning';
 import { useResetMapReducerOnUnmount } from './useResetMapReducerOnUnmount';
+import { setCurrentInstance } from '../../actions';
+import { fetchInstanceDetail } from '../../../../utils/requests';
 
 const boundsOptions = { padding: [50, 50] };
 
-const snackbarKey = 'noInstancesOnMap';
-
 type Props = {
-    instances:any,
-    currentTile:any,
-    isClusterActive:any,
-    fetching:any, notifications:any[]
-}
+    instances: any;
+    fetching: any;
+};
 
-export const InstancesMap:FunctionComponent<Props> = ({instances, currentTile, isClusterActive,notifications, fetching}) => {
-    const map = useRef()
-    useShowWarning({instances,
-        notifications,
-        fetching,});
+export const InstancesMap: FunctionComponent<Props> = ({
+    instances,
+    fetching,
+}) => {
+    const map: any = useRef<any>();
+    // @ts-ignore
+    const isClusterActive = useSelector(state => state.map.isClusterActive);
+    // @ts-ignore
+    const currentTile = useSelector(state => state.map.currentTile);
+    const notifications = useSelector(state =>
+        // @ts-ignore
+        state.snackBar ? state.snackBar.notifications : [],
+    );
 
-    useResetMapReducerOnUnmount()
- 
+    const dispatch = useDispatch();
+    const dispatchInstance = useCallback(
+        instance => {
+            dispatch(setCurrentInstance(instance));
+        },
+        [dispatch],
+    );
+    // We need redux here for the PopUp. Refactoring the pop up may be complex since it is used in MarkerClusterGroup,
+    // which is itself widely used
+    const fetchAndDispatchDetail = useCallback(
+        instance => {
+            dispatchInstance(null);
+            fetchInstanceDetail(dispatch, instance.id).then(i =>
+                dispatchInstance(i),
+            );
+        },
+        [dispatch, dispatchInstance],
+    );
+    useShowWarning({ instances, notifications, fetching });
 
+    useResetMapReducerOnUnmount();
 
-
-
-    fetchDetail(instance) {
-        const { dispatch } = this.props;
-        this.props.setCurrentInstance(null);
-        fetchInstanceDetail(dispatch, instance.id).then(i =>
-            this.props.setCurrentInstance(i),
-        );
-    }
-
-    fitToBounds() {
-        const { currentTile, instances } = this.props;
+    const fitToBounds = useCallback(() => {
         const bounds = getLatLngBounds(instances);
-        this.map.leafletElement.fitBounds(bounds, {
+        map.current?.leafletElement.fitBounds(bounds, {
             maxZoom: currentTile.maxZoom,
             padding: boundsOptions.padding,
         });
-    }
+    }, [instances, currentTile]);
 
-    render() {
-       
-        if (fetching) return null;
-        const bounds = getLatLngBounds(instances);
-        if (this.map) {
-            this.map.leafletElement.options.maxZoom = currentTile.maxZoom;
+    const bounds = useMemo(() => {
+        if (instances) {
+            return getLatLngBounds(instances);
         }
-        return (
-            <Grid container spacing={0}>
-                <InnerDrawer
-                    withTopBorder
-                    settingsOptionComponent={
-                        <>
-                            <TileSwitch />
-                            <Divider />
-                            <ClusterSwitch />
-                        </>
-                    }
+        return undefined;
+    }, [instances]);
+
+    if (fetching) return null;
+    if (map.current) {
+        map.current.leafletElement.options.maxZoom = currentTile.maxZoom;
+    }
+    return (
+        <Grid container spacing={0}>
+            <InnerDrawer
+                withTopBorder
+                settingsOptionComponent={
+                    <>
+                        <TileSwitch />
+                        <Divider />
+                        <ClusterSwitch />
+                    </>
+                }
+            >
+                <Map
+                    ref={ref => {
+                        map.current = ref;
+                    }}
+                    scrollWheelZoom={false}
+                    maxZoom={currentTile.maxZoom}
+                    style={{ height: '100%' }}
+                    bounds={bounds}
+                    boundsOptions={boundsOptions}
+                    zoom={defaultZoom}
+                    center={defaultCenter}
+                    zoomControl={false}
+                    keyboard={false}
                 >
-                    <Map
-                        ref={ref => {
-                            this.map = ref;
-                        }}
-                        scrollWheelZoom={false}
-                        maxZoom={currentTile.maxZoom}
-                        style={{ height: '100%' }}
-                        bounds={bounds}
-                        boundsOptions={boundsOptions}
-                        zoom={defaultZoom}
-                        center={defaultCenter}
-                        zoomControl={false}
-                        keyboard={false}
-                    >
-                        <ZoomControl fitToBounds={() => this.fitToBounds()} />
-                        <ScaleControl imperial={false} />
-                        <TileLayer
-                            attribution={
-                                currentTile.attribution
-                                    ? currentTile.attribution
-                                    : ''
-                            }
-                            url={currentTile.url}
-                        />
-                        {isClusterActive && (
-                            <MarkerClusterGroup
-                                iconCreateFunction={clusterCustomMarker}
-                            >
-                                <MarkersListComponent
-                                    items={instances}
-                                    onMarkerClick={i => this.fetchDetail(i)}
-                                    PopupComponent={InstancePopupComponent}
-                                />
-                            </MarkerClusterGroup>
-                        )}
-                        {!isClusterActive && (
+                    <ZoomControl fitToBounds={() => fitToBounds()} />
+                    <ScaleControl imperial={false} />
+                    <TileLayer
+                        attribution={
+                            currentTile.attribution
+                                ? currentTile.attribution
+                                : ''
+                        }
+                        url={currentTile.url}
+                    />
+                    {isClusterActive && (
+                        <MarkerClusterGroup
+                            iconCreateFunction={clusterCustomMarker}
+                        >
                             <MarkersListComponent
                                 items={instances}
-                                onMarkerClick={i => this.fetchDetail(i)}
+                                onMarkerClick={fetchAndDispatchDetail}
                                 PopupComponent={InstancePopupComponent}
                             />
-                        )}
-                    </Map>
-                </InnerDrawer>
-            </Grid>
-        );
-    }
-}
-
-InstancesMap.propTypes = {
-    instances: PropTypes.array.isRequired,
-    currentTile: PropTypes.object.isRequired,
-    resetMapReducer: PropTypes.func.isRequired,
-    isClusterActive: PropTypes.bool.isRequired,
-    setCurrentInstance: PropTypes.func.isRequired,
-    dispatch: PropTypes.func.isRequired,
-    fetching: PropTypes.bool.isRequired,
-    notifications: PropTypes.array.isRequired,
+                        </MarkerClusterGroup>
+                    )}
+                    {!isClusterActive && (
+                        <MarkersListComponent
+                            items={instances}
+                            onMarkerClick={fetchAndDispatchDetail}
+                            PopupComponent={InstancePopupComponent}
+                        />
+                    )}
+                </Map>
+            </InnerDrawer>
+        </Grid>
+    );
 };
-
-const MapStateToProps = state => ({
-    currentTile: state.map.currentTile,
-    isClusterActive: state.map.isClusterActive,
-    notifications: state.snackBar ? state.snackBar.notifications : [],
-});
-
-const MapDispatchToProps = dispatch => ({
-    dispatch,
-    resetMapReducer: currentTile => dispatch(resetMapReducer(currentTile)),
-    setCurrentInstance: i => dispatch(setCurrentInstance(i)),
-});
-
-export default connect(MapStateToProps, MapDispatchToProps)(InstancesMap);

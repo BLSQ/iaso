13d4deb406fb36b8b82dca79031d3a11192dbc95
Quang Son Le <qle@bluesquarehub.com>
2023-02-08 12:17:16 +0100

    POLIO-813: change link to procedure

--- plugins/polio/js/src/pages/Budget/BudgetDetails/LinkToProcedure.tsx
+++ plugins/polio/js/src/pages/Budget/BudgetDetails/LinkToProcedure.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 import { useSafeIntl } from 'bluesquare-components';
 import { Tooltip, makeStyles } from '@material-ui/core';
 import HelpIcon from '@material-ui/icons/Help';
-import { WHO_AFRO_PROCEDURE } from '../constants';
+import { BUDGET_PROCEDURE_SCHEMA } from '../constants';
 import MESSAGES from '../../../constants/messages';
 
 const useStyles = makeStyles(theme => ({
@@ -18,7 +18,7 @@ export const LinkToProcedure: FunctionComponent = () => {
     const classes = useStyles();
     return (
         <Tooltip title={formatMessage(MESSAGES.seeProcedure)}>
-            <Link href={WHO_AFRO_PROCEDURE} target="_blank">
+            <Link href={BUDGET_PROCEDURE_SCHEMA} target="_blank">
                 <HelpIcon className={classes.icon} />
             </Link>
         </Tooltip>

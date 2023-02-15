4dfe57b36f4e2bd27a4cd11c9bdd896a335e5a87
Math VDH <mathvdh@gmail.com>
2023-02-15 11:12:22 +0100

    WIP tests

--- iaso/tests/api/test_mobile_form_versions.py
+++ iaso/tests/api/test_mobile_form_versions.py
@@ -12,90 +12,124 @@ from iaso.test import APITestCase
 BASE_URL = "/api/mobile/formversions/"
 
 
+def create_add_form(f_name, o_unit_type, add_to_project):
+    the_form = m.Form.objects.create(
+        name="f_name",  # no form_id yet (no version)
+        period_type="QUARTER",
+        single_per_period=True,
+    )
+    the_form.org_unit_types.add(o_unit_type)
+    the_form.save()
+
+    form_1_file_mock = mock.MagicMock(spec=File)
+    form_1_file_mock.name = "test.xml"
+    with open("iaso/tests/fixtures/odk_form_valid_no_settings.xls", "rb") as xls_file:
+        the_form.form_versions.create(file=form_1_file_mock, xls_file=UploadedFile(xls_file), version_id="2020022401")
+
+    the_form.save()
+
+    add_to_project.add(the_form)
+    add_to_project.save()
+
+    return the_form
+
+
 @override_settings(MEDIA_ROOT=tempfile.mkdtemp())
 class MobileFormsVersionAPITestCase(APITestCase):
     @classmethod
     def setUpTestData(cls):
-        star_wars = m.Account.objects.create(name="Star Wars")
-        dc = m.Account.objects.create(name="DC Comics")
+        blue_account = m.Account.objects.create(name="Blue Account")
+        red_account = m.Account.objects.create(name="Red Account")
 
-        sw_source = m.DataSource.objects.create(name="Evil Empire")
-        cls.sw_source = sw_source
-        sw_version = m.SourceVersion.objects.create(data_source=sw_source, number=1)
-        star_wars.default_version = sw_version
-        star_wars.save()
+        blue_source = m.DataSource.objects.create(name="Blue Source")
+        blue_source_version = m.SourceVersion.objects.create(data_source=blue_source, number=1)
+        blue_source.default_version = blue_source_version
+        blue_source.save()
 
-        cls.yoda = cls.create_user_with_profile(username="yoda", account=star_wars, permissions=["iaso_forms"])
-        cls.batman = cls.create_user_with_profile(username="batman", account=dc, permissions=["iaso_forms"])
-        cls.superman = cls.create_user_with_profile(username="superman", account=dc)
+        cls.blue_with_perms = cls.create_user_with_profile(
+            username="blue_with_perms", account=blue_account, permissions=["iaso_forms"]
+        )
+        cls.red_with_perms = cls.create_user_with_profile(
+            username="red_with_perms", account=red_account, permissions=["iaso_forms"]
+        )
+        cls.red_no_perms = cls.create_user_with_profile(username="red_no_perms", account=red_account)
 
-        cls.sith_council = m.OrgUnitType.objects.create(name="Sith Council", short_name="Cnc")
+        cls.blue_council = m.OrgUnitType.objects.create(name="Blue Council", short_name="BC1")
 
-        cls.project = m.Project.objects.create(
-            name="Hydroponic gardens",
-            app_id="stars.empire.agriculture.hydroponics",
-            account=star_wars,
+        cls.blue_project_need_auth_allow_ro = m.Project.objects.create(
+            name="Blue Project Needs Auth Allow RO",
+            app_id="blue.project_need_auth_allow_ro",
+            account=blue_account,
             needs_authentication=True,
         )
-        cls.project.unit_types.add(cls.sith_council)
-        cls.form_1 = m.Form.objects.create(
-            name="New Land Speeder concept",  # no form_id yet (no version)
-            period_type="QUARTER",
-            single_per_period=True,
-        )
-        cls.form_1.org_unit_types.add(cls.sith_council)
-        cls.form_1.save()
-        form_1_file_mock = mock.MagicMock(spec=File)
-        form_1_file_mock.name = "test.xml"
-        with open("iaso/tests/fixtures/odk_form_valid_no_settings.xls", "rb") as xls_file:
-            cls.form_1.form_versions.create(
-                file=form_1_file_mock, xls_file=UploadedFile(xls_file), version_id="2020022401"
-            )
+        cls.blue_project_need_auth_allow_ro.unit_types.add(cls.blue_council)
 
-        cls.form_1.save()
-
-        cls.project.forms.add(cls.form_1)
-        cls.project.save()
-
-        cls.project_no_read_only = m.Project.objects.create(
-            name="Hydroponic gardens No RO", app_id="stars.empire.agriculture.hydroponics_noro", account=star_wars
+        cls.blue_project_need_auth_not_allow_ro = m.Project.objects.create(
+            name="Blue Project Needs Auth Not Allow RO",
+            app_id="blue.project_need_auth_not_allow_ro",
+            account=blue_account,
+            needs_authentication=True,
         )
 
-        cls.project_no_read_only.unit_types.add(cls.sith_council)
-        cls.project_no_read_only.feature_flags.add(m.FeatureFlag.objects.get(code="FORM_VERSIONS_NO_READ_ONLY"))
-
-        cls.form_2 = m.Form.objects.create(
-            name="Death Start survey", form_id="sample2", period_type="MONTH", single_per_period=False
+        cls.blue_project_need_auth_not_allow_ro.feature_flags.add(
+            m.FeatureFlag.objects.get(code="FORM_VERSIONS_NO_READ_ONLY")
         )
-        cls.form_2.org_unit_types.add(cls.sith_council)
+        cls.blue_project_need_auth_not_allow_ro.unit_types.add(cls.blue_council)
+        cls.blue_project_need_auth_not_allow_ro.save()
 
-        form_2_file_mock = mock.MagicMock(spec=File)
-        form_2_file_mock.name = "test.xml"
-        with open("iaso/tests/fixtures/odk_form_valid_no_settings.xls", "rb") as xls_file:
-            cls.form_2.form_versions.create(
-                file=form_2_file_mock, xls_file=UploadedFile(xls_file), version_id="2020022401"
-            )
+        cls.blue_project_no_need_auth_allow_ro = m.Project.objects.create(
+            name="Blue Project  No Needs Auth Allow RO",
+            app_id="blue.project_no_need_auth_allow_ro",
+            account=blue_account,
+            needs_authentication=False,
+        )
 
-        cls.project_no_read_only.forms.add(cls.form_2)
-        cls.project_no_read_only.save()
+        cls.blue_project_no_need_auth_allow_ro.unit_types.add(cls.blue_council)
+        cls.blue_project_no_need_auth_allow_ro.save()
+
+        cls.blue_project_no_need_auth_not_allow_ro = m.Project.objects.create(
+            name="Blue Project  No Needs Auth Allow RO",
+            app_id="blue.project_no_need_auth_not_allow_ro",
+            account=blue_account,
+            needs_authentication=False,
+        )
+
+        cls.blue_project_no_need_auth_not_allow_ro.feature_flags.add(
+            m.FeatureFlag.objects.get(code="FORM_VERSIONS_NO_READ_ONLY")
+        )
+        cls.blue_project_no_need_auth_not_allow_ro.unit_types.add(cls.blue_council)
+        cls.blue_project_no_need_auth_not_allow_ro.save()
+
+        cls.form_need_auth_allow_ro = create_add_form(
+            "Form need_auth_allow_ro", cls.blue_council, cls.blue_project_need_auth_allow_ro
+        )
+        cls.form_need_auth_not_allow_ro = create_add_form(
+            "Form need_auth_not_allow_ro", cls.blue_council, cls.blue_project_need_auth_not_allow_ro
+        )
+        cls.form_no_need_auth_allow_ro = create_add_form(
+            "Form no_need_auth_allow_ro", cls.blue_council, cls.blue_project_no_need_auth_allow_ro
+        )
+        cls.form_no_need_auth_not_allow_ro = create_add_form(
+            "Form no_need_auth_not_allow_ro", cls.blue_council, cls.blue_project_no_need_auth_not_allow_ro
+        )
 
     def test_form_versions_list_without_auth(self):
-        """GET /mobile/formversions/: without auth: 403"""
+        """GET /mobile/formversions/: without auth should not be allowed"""
 
         response = self.client.get(BASE_URL)
         self.assertJSONResponse(response, 403)
 
     def test_form_versions_list_wrong_permission(self):
-        """GET /mobile/formversions/: with auth but without the iaso_forms permission"""
+        """GET /mobile/formversions/: without the right permission should not be allowed"""
 
-        self.client.force_authenticate(self.superman)
+        self.client.force_authenticate(self.red_no_perms)
         response = self.client.get(BASE_URL)
         self.assertJSONResponse(response, 403)
 
     def test_form_versions_list(self):
         """GET /mobile/formversions/: allowed"""
 
-        self.client.force_authenticate(self.yoda)
+        self.client.force_authenticate(self.blue_with_perms)
         response = self.client.get(BASE_URL)
         self.assertJSONResponse(response, 200)
         form_versions_data = response.json()["form_versions"]
@@ -104,32 +138,37 @@ class MobileFormsVersionAPITestCase(APITestCase):
             self.assertValidFormVersionData(form_version_data)
             self.assertNotIn("descriptor", form_version_data)
 
-    def test_form_versions_retrieve_ok_user_in_account(self):
+    def test_form_need_auth_allow_ro(self):
         """GET /mobile/formversions/<form_id> This form is linked to a project which allow read only: ok"""
 
-        self.client.force_authenticate(self.yoda)  # anyone should be able to read a form version
-        response = self.client.get(
-            f"{BASE_URL}{self.form_1.form_versions.first().id}/?fields=:all&app_id=stars.empire.agriculture.hydroponics"
-        )
+        self.client.force_authenticate(self.blue_with_perms)
+        response = self.client.get(f"{BASE_URL}{self.form_need_auth_allow_ro.id}/")
         self.assertJSONResponse(response, 200)
-        form_version_data = response.json()
+        form_version_data = response.json()["form_version"]
         self.assertValidFormVersionData(form_version_data)
-        self.assertHasField(form_version_data, "descriptor", dict)
 
-    def test_form_versions_retrieve_not_ok_no_appid(self):
-        """GET /mobile/formversions/<form_id> No app id is provided"""
+    def test_form_need_auth_not_allow_ro(self):
+        """GET /mobile/formversions/<form_id> This form is linked to a project which does not allow read only: 403"""
 
-        self.client.force_authenticate(self.superman)
-        response = self.client.get(f"{BASE_URL}{self.form_1.form_versions.first().id}/?fields=:all")
+        self.client.force_authenticate(self.blue_with_perms)
+        response = self.client.get(f"{BASE_URL}{self.form_need_auth_not_allow_ro.id}/")
         self.assertJSONResponse(response, 403)
 
-    def test_form_versions_retrieve_not_ok_project_has_noro_feature_flag(self):
-        """GET /mobile/formversions/<form_id> This form is linked to a project which doesn't allow read only: ok"""
+    def test_form_no_need_auth_allow_ro(self):
+        """GET /mobile/formversions/<form_id> This form is linked to a project which allow read only: ok"""
 
-        self.client.force_authenticate(self.yoda)
-        response = self.client.get(
-            f"{BASE_URL}{self.form_2.form_versions.first().id}/?fields=:allapp_id=stars.empire.agriculture.hydroponics_noro"
-        )
+        self.client.force_authenticate(self.blue_with_perms)
+        response = self.client.get(f"{BASE_URL}{self.form_no_need_auth_allow_ro.id}/")
+        self.assertJSONResponse(response, 200)
+        form_version_data = response.json()["form_version"]
+        self.assertValidFormVersionData(form_version_data)
+        self.assertIn("descriptor", form_version_data)
+
+    def test_form_no_need_auth_not_allow_ro(self):
+        """GET /mobile/formversions/<form_id> This form is linked to a project which does not allow read only: 403"""
+
+        self.client.force_authenticate(self.blue_with_perms)
+        response = self.client.get(f"{BASE_URL}{self.form_no_need_auth_not_allow_ro.id}/")
         self.assertJSONResponse(response, 403)
 
     def assertValidFormVersionData(

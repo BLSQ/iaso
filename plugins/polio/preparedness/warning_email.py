f9ec1f2078f7cea8c82f4c3c4b8d36c7ceb46e86
Quang Son Le <qle@bluesquarehub.com>
2023-03-08 14:32:08 +0100

    POLIO-874: correct fix

--- plugins/polio/preparedness/warning_email.py
+++ plugins/polio/preparedness/warning_email.py
@@ -14,9 +14,9 @@ from plugins.polio.preparedness.summary import get_or_set_preparedness_cache_for
 
 
 def valid_indicator(x):
-    if isinstance(x, str):
-        if x is None:
+    if x is None:
             return True
+    if isinstance(x, str):
         return x.strip() in ["NA", "N/A"]
     if not isinstance(x, (int, float)):
         return False

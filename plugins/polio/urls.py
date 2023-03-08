4e1506427253680dd348b4a1ccd75db6ad4ba29e
Math VDH <mathvdh@gmail.com>
2023-03-08 10:59:22 +0100

    Its working but not sure about the args needed

--- plugins/polio/urls.py
+++ plugins/polio/urls.py
@@ -2,4 +2,4 @@ from django.urls import path
 
 from .views import refresh_preparedness_data
 
-urlpatterns = [path("refresh_preparedness_data/", refresh_preparedness_data)]
+urlpatterns = [path("refresh_preparedness_data/<launcher_user_id>/", refresh_preparedness_data)]
--- plugins/polio/views.py
+++ plugins/polio/views.py
@@ -1,13 +1,15 @@
 import pprint
+
+from django.shortcuts import get_object_or_404
 from plugins.polio.tasks.refresh_preparedness_data import refresh_data
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
 
 @csrf_exempt
-def refresh_preparedness_data(request):
+def refresh_preparedness_data(request, launcher_user_id):
+    the_user = get_object_or_404(User, pk=launcher_user_id)
+    the_task = refresh_data(user=the_user)
 
-    the_task = refresh_data()
-    print(f"Task {the_task} created")
-    the_task()
-    print(f"Task {the_task} launched")
-    pprint.pprint(the_task.as_dict())
+    return JsonResponse({"status": "OK", "task": the_task.as_dict()})

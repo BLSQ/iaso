d12197e963982a04ca7f76e1b32246132ec9f4e4
Axel Tavormina <tavormina.axel@gmail.com>
2023-03-10 16:23:47 +0100

    update logStructure array

--- plugins/polio/js/src/components/campaignHistory/constants.ts
+++ plugins/polio/js/src/components/campaignHistory/constants.ts
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import MESSAGES from '../../constants/messages';
 
-export const LogStructure = [
+export const logStructure = [
     {
         key: 'id',
     },
@@ -23,6 +23,24 @@ export const LogStructure = [
             {
                 key: 'started_at',
                 getLogValue: log => moment(log.created_at).format('LTS'),
+            },
+        ],
+    },
+    {
+        key: 'scopes',
+        type: 'array',
+        childrenLabel: MESSAGES.scope,
+        children: [
+            {
+                key: 'id',
+            },
+            {
+                key: 'group',
+                type: 'object',
+                childrenLabel: MESSAGES.group,
+                children: {
+                    key: 'id',
+                },
             },
         ],
     },

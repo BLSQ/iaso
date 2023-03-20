653f963b0db569eb5d84cb150f9d687284a3560b
Axel Tavormina <tavormina.axel@gmail.com>
2023-03-20 17:40:42 +0100

    hook for children labels

--- plugins/polio/js/src/hooks/useGetCampaignFieldLabel.ts
+++ plugins/polio/js/src/hooks/useGetCampaignFieldLabel.ts
@@ -2,6 +2,7 @@
 // @ts-ignore
 
 import { useSafeIntl } from 'bluesquare-components';
+import { IntlMessage } from '../../../../../hat/assets/js/apps/Iaso/types/intl';
 
 import MESSAGES from '../constants/messages';
 
@@ -10,6 +11,17 @@ export const useGetCampaignFieldLabel = (): ((fieldKey: string) => string) => {
     const getLabel = (fieldKey): any => {
         // temporary displaying key instead of label not yet added to translations
         return formatMessage(MESSAGES[fieldKey]) || fieldKey;
+    };
+
+    return getLabel;
+};
+
+export const useGetChildrenLabel = (): ((
+    childrenLabel: IntlMessage,
+) => string) => {
+    const { formatMessage } = useSafeIntl();
+    const getLabel = (childrenLabel): string => {
+        return formatMessage(childrenLabel);
     };
 
     return getLabel;

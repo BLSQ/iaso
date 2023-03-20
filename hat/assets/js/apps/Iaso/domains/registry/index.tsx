1587660afa2830c8995b7a89fb498a9bf1846361
Christophe Gerard <c@eduz.be>
2023-03-20 08:58:52 +0100

    working tree

--- hat/assets/js/apps/Iaso/domains/registry/index.tsx
+++ hat/assets/js/apps/Iaso/domains/registry/index.tsx
@@ -1,30 +1,81 @@
-import React, { FunctionComponent } from 'react';
-import { makeStyles, Box } from '@material-ui/core';
-import { commonStyles, useSafeIntl } from 'bluesquare-components';
+import React, { useState, FunctionComponent } from 'react';
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+    TreeViewWithSearch,
+    useSafeIntl,
+    commonStyles,
+} from 'bluesquare-components';
+
 import TopBar from '../../components/nav/TopBarComponent';
 import MESSAGES from './messages';
+import {
+    getRootData,
+    getChildrenData,
+    searchOrgUnits,
+} from '../orgUnits/components/TreeView/requests';
+import { OrgUnitLabel, getOrgUnitAncestors } from '../orgUnits/utils';
+import {
+    tooltip,
+    makeTreeviewLabel,
+} from '../orgUnits/components/TreeView/utils';
 
 const useStyles = makeStyles(theme => ({
     ...commonStyles(theme),
 }));
 
-// type Props = {
-//     params: PlanningParams;
-// };
-// const baseUrl = baseUrls.registry;
 export const Registry: FunctionComponent = () => {
     const classes: Record<string, string> = useStyles();
     const { formatMessage } = useSafeIntl();
 
+    const [selectedOrgUnits, setSelectedOrgUnits] = useState();
+
+    const [selectedOrgUnitsIds, setSelectedOrgUnitsIds] = useState([]);
+    const [selectedOrgUnitParents, setSelectedOrgUnitParents] = useState(
+        new Map(),
+    );
+    const onUpdate = (orgUnitIds, parentsData, orgUnits) => {
+        setSelectedOrgUnitsIds(orgUnitIds);
+        setSelectedOrgUnitParents(parentsData);
+        if (orgUnits) {
+            setSelectedOrgUnits(orgUnits);
+        }
+        console.log('orgUnits', orgUnits);
+    };
+    console.log('selectedOrgUnits', selectedOrgUnits);
     return (
         <>
             <TopBar
                 title={formatMessage(MESSAGES.title)}
                 displayBackButton={false}
             />
-
             <Box className={classes.containerFullHeightNoTabPadded}>
-                REGISTRY
+                <TreeViewWithSearch
+                    getChildrenData={getChildrenData}
+                    getRootData={getRootData}
+                    label={makeTreeviewLabel(classes, true)}
+                    toggleOnLabelClick={false}
+                    request={(value, count) => {
+                        //  @ts-ignore
+                        return searchOrgUnits({ value, count });
+                    }}
+                    makeDropDownText={orgUnit => (
+                        <OrgUnitLabel
+                            orgUnit={orgUnit}
+                            withType
+                            withSource={false}
+                        />
+                    )}
+                    toolTip={tooltip}
+                    parseNodeIds={getOrgUnitAncestors}
+                    preselected={selectedOrgUnitsIds}
+                    preexpanded={selectedOrgUnitParents}
+                    selectedData={selectedOrgUnits}
+                    onUpdate={onUpdate}
+                    // allowSelection={false}
+                    multiselect={false}
+                />
+                {selectedOrgUnits && selectedOrgUnits[0]?.name}
             </Box>
         </>
     );
--- hat/assets/js/apps/Iaso/domains/registry/messages.ts
+++ hat/assets/js/apps/Iaso/domains/registry/messages.ts
@@ -5,6 +5,30 @@ const MESSAGES = defineMessages({
         defaultMessage: 'Registry',
         id: 'iaso.registy.title',
     },
+    selectSingle: {
+        id: 'blsq.treeview.label.selectSingle',
+        defaultMessage: 'Select an org unit',
+    },
+    selectMultiple: {
+        id: 'blsq.treeview.label.selectMultiple',
+        defaultMessage: 'Select org unit(s)',
+    },
+    clear: {
+        id: 'blsq.treeview.search.options.label.clear',
+        defaultMessage: 'Clear',
+    },
+    confirm: {
+        id: 'blsq.treeview.search.confirm',
+        defaultMessage: 'Confirm',
+    },
+    cancel: {
+        id: 'blsq.treeview.search.cancel',
+        defaultMessage: 'Cancel',
+    },
+    error: {
+        id: 'blsq.treeview.error',
+        defaultMessage: 'Please select an org unit',
+    },
 });
 
 export default MESSAGES;

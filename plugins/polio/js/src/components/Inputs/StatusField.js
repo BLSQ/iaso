7cbfd5b3792fb77fc2b415124e3344ca79f4f0e3
Quang Son Le <qle@bluesquarehub.com>
2023-03-29 17:51:23 +0200

    POLIO-920: auto fill detection status

--- plugins/polio/js/src/components/Inputs/StatusField.js
+++ plugins/polio/js/src/components/Inputs/StatusField.js
@@ -5,34 +5,6 @@ import { Select } from './Select';
 import MESSAGES from '../../constants/messages';
 import { RA_STATUSES } from '../../constants/statuses.ts';
 
-const statuses = [
-    {
-        value: 'PENDING',
-        label: MESSAGES.pending,
-    },
-    {
-        value: 'ONGOING',
-        label: MESSAGES.ongoing,
-    },
-    {
-        value: 'FINISHED',
-        label: MESSAGES.finished,
-    },
-];
-
-export const StatusField = props => {
-    const { formatMessage } = useSafeIntl();
-    const options = useTranslatedOptions(statuses);
-
-    return (
-        <Select
-            label={formatMessage(MESSAGES.status)}
-            options={options}
-            {...props}
-        />
-    );
-};
-
 const RABudgetstatuses = RA_STATUSES.map(state => ({
     value: state,
     label: MESSAGES[state],

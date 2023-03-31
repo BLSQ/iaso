4cbf0442464927e6cad23d79b17ef5955764a7e7
Axel Tavormina <tavormina.axel@gmail.com>
2023-03-31 15:38:58 +0200

    fix label for budget fields

--- plugins/polio/js/src/hooks/useGetCampaignFieldLabel.ts
+++ plugins/polio/js/src/hooks/useGetCampaignFieldLabel.ts
@@ -2,13 +2,18 @@
 // @ts-ignore
 
 import { useSafeIntl } from 'bluesquare-components';
-import { IntlMessage } from '../../../../../hat/assets/js/apps/Iaso/types/intl';
 
 import MESSAGES from '../constants/messages';
 
 export const useGetCampaignFieldLabel = (): ((fieldKey: string) => string) => {
     const { formatMessage } = useSafeIntl();
+    const WORKFLOW_SUFFIX = '_at_WFEDITABLE';
+
     const getLabel = (fieldKey): any => {
+        if (fieldKey.includes(WORKFLOW_SUFFIX)) {
+            const budgetFieldKey = fieldKey.replace(WORKFLOW_SUFFIX, '');
+            return formatMessage(MESSAGES[budgetFieldKey]);
+        }
         // temporary displaying key instead of label not yet added to translations
         return formatMessage(MESSAGES[fieldKey]) || fieldKey;
     };

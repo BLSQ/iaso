428428d11ba41252b8687a3733dc288c9a8f236a
Axel Tavormina <tavormina.axel@gmail.com>
2023-04-18 15:41:36 +0200

    add max value for % covered target population

--- plugins/polio/js/src/hooks/useFormValidator.js
+++ plugins/polio/js/src/hooks/useFormValidator.js
@@ -544,7 +544,12 @@ const useRoundShape = () => {
         shipments: yup.array(shipment).nullable(),
         vaccines: yup.array(vaccine).nullable(),
         destructions: yup.array(destruction).nullable(),
-        percentage_covered_target_population: yup.number().nullable().min(0).integer(),
+        percentage_covered_target_population: yup
+            .number()
+            .nullable()
+            .min(0)
+            .max(100)
+            .integer(),
     });
 };
 

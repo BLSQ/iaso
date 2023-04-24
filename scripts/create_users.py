6d3d83618cd08e26bdda7db02e62656f7b4f08b6
Math VDH <mathvdh@gmail.com>
2023-04-24 15:35:02 +0200

    Last requests

--- scripts/create_users.py
+++ scripts/create_users.py
@@ -381,106 +381,168 @@ from django.contrib.gis.geos import Point
 
 # Adding more POIs and Assignments
 
-orgunit_master = OrgUnit.objects.get(id=1074010)  # ll Province Lualaba
-print("Master OrgUnit: " + orgunit_master.name)
+# orgunit_master = OrgUnit.objects.get(id=1074010)  # ll Province Lualaba
+# print("Master OrgUnit: " + orgunit_master.name)
+
+# account = Account.objects.get(id=22)
+# print("Account: " + account.name)
+
+
+# children = OrgUnit.objects.filter(parent=orgunit_master)
+# print("Children count : " + str(len(children)))
+
+
+# profiles = Profile.objects.filter(account=account)
+# print("Profiles count : " + str(len(profiles)))
+
+
+# the_planning = Planning.objects.get(id=48)
+# print("Planning: " + the_planning.name)
+# # Canevas
+# centre_de_sante_type = OrgUnitType.objects.get(projects__account=account, short_name="FoSa")
+# print("Centre De Santé Type: " + centre_de_sante_type.short_name)
+
+# aire_de_sante_type = OrgUnitType.objects.get(projects__account=account, short_name="AS")
+# print("Aire De Santé Type: " + aire_de_sante_type.short_name)
+
+# point_of_interest_type = OrgUnitType.objects.get(projects__account=account, short_name="POI")
+# print("Point Of Interest Type: " + point_of_interest_type.short_name)
+
+
+# def get_location(grand_child, child):
+#     val = None
+#     if grand_child.geom and grand_child.geom.centroid:
+#         val = grand_child.geom.centroid
+#     elif grand_child.location:
+#         val = grand_child.location
+#     elif child.geom and child.geom.centroid:
+#         val = child.geom.centroid
+#     elif child.location:
+#         val = child.location
+
+#     if val is not None:
+#         return Point(val.x, val.y, 0)
+#     else:
+#         return None
+
+
+# for child in children:
+
+#     grand_children = OrgUnit.objects.filter(parent=child, org_unit_type=aire_de_sante_type)
+
+#     # for all the areas in the zone "child"
+#     for g in grand_children.all():
+
+#         print(f"OrgUnit {g.name}")
+
+#         # create 30 (ask claire) POI per area (you will probably need to set a point to the poi in the center of the area)
+
+#         try:
+#             corresp_profile = profiles.get(org_units=g, account=account)
+#         except:
+#             corresp_profile = None
+
+#         try:
+#             corresp_team = Team.objects.get(name=child.name)
+#         except:
+#             corresp_team = None
+
+#         if corresp_profile is None or corresp_team is None:
+#             print(f"!! Didnt find a profile or a team for {g.name}")
+#             print(f"Profile {corresp_profile}")
+#             print(f"Team {corresp_team}")
+#         else:
+
+#             for i in range(30, 100):
+#                 # create 30 more POI per area
+
+#                 print("Location : " + str(get_location(g, child)))
+#                 print("Type of location : " + str(type(get_location(g, child))))
+
+#                 ou, created = OrgUnit.objects.get_or_create(
+#                     name=f"POI_{i:02d}",  # format avec 0 devant
+#                     parent=g,
+#                     org_unit_type=point_of_interest_type,
+#                     version=account.default_version,
+#                     validation_status=OrgUnit.VALIDATION_VALID,
+#                     defaults={"location": get_location(g, child)},
+#                 )
+
+#                 print("Created? " + str(created))
+
+#                 print(
+#                     f"Created POI_{i:02d} with parent {g.name} type {point_of_interest_type.short_name} location {get_location(g, child)}"
+#                 )
+
+#                 # create assignments in the existing plannong of each POIs  to people in the current area
+
+#                 correct_assignment = Assignment.objects.get_or_create(
+#                     planning=the_planning, org_unit=ou, user=corresp_profile.user
+#                 )
+
+#                 print(f"Created assignment for {corresp_profile.user.username} for POI_{i:02d}")
+
 
 account = Account.objects.get(id=22)
 print("Account: " + account.name)
 
 
-children = OrgUnit.objects.filter(parent=orgunit_master)
-print("Children count : " + str(len(children)))
-
-
 profiles = Profile.objects.filter(account=account)
 print("Profiles count : " + str(len(profiles)))
 
+# Zone_Sante_Base_Id = 1074168  # ll Fungurume Zone de Santé
+Zone_Sante_Base_Id = 1074597  # ll Lualaba Zone de Santé
+zs_base = OrgUnit.objects.get(id=Zone_Sante_Base_Id)
 
-the_planning = Planning.objects.get(id=48)
-print("Planning: " + the_planning.name)
-# Canevas
+print("Zone de Santé Base: " + zs_base.name)
+
 centre_de_sante_type = OrgUnitType.objects.get(projects__account=account, short_name="FoSa")
 print("Centre De Santé Type: " + centre_de_sante_type.short_name)
 
 aire_de_sante_type = OrgUnitType.objects.get(projects__account=account, short_name="AS")
 print("Aire De Santé Type: " + aire_de_sante_type.short_name)
 
-point_of_interest_type = OrgUnitType.objects.get(projects__account=account, short_name="POI")
-print("Point Of Interest Type: " + point_of_interest_type.short_name)
 
+the_planning = Planning.objects.get(id=50)  # Digitalisation canevas papier
 
-def get_location(grand_child, child):
-    val = None
-    if grand_child.geom and grand_child.geom.centroid:
-        val = grand_child.geom.centroid
-    elif grand_child.location:
-        val = grand_child.location
-    elif child.geom and child.geom.centroid:
-        val = child.geom.centroid
-    elif child.location:
-        val = child.location
+print("Planning: " + the_planning.name)
 
-    if val is not None:
-        return Point(val.x, val.y, 0)
+aire_de_santes = OrgUnit.objects.filter(parent=zs_base, org_unit_type=aire_de_sante_type)
+
+print("Aire de Santé count: " + str(aire_de_santes.count()))
+
+for air_sa in aire_de_santes.all():
+
+    try:
+        corresp_profile = profiles.get(org_units=air_sa, account=account)
+    except:
+        corresp_profile = None
+
+    try:
+        corresp_team = Team.objects.get(name=zs_base.name)
+    except:
+        corresp_team = None
+
+    if corresp_profile is None or corresp_team is None:
+        print(f"!! Didnt find a profile or a team for {air_sa.name}")
+        print(f"Profile {corresp_profile}")
+        print(f"Team {corresp_team}")
     else:
-        return None
 
+        correct_assignment = Assignment.objects.get_or_create(
+            planning=the_planning, org_unit=air_sa, user=corresp_profile.user
+        )
 
-for child in children:
+        print(f"Created assignment for {corresp_profile.user.username} for {air_sa.name}")
 
-    grand_children = OrgUnit.objects.filter(parent=child, org_unit_type=aire_de_sante_type)
+        centre_de_santes = OrgUnit.objects.filter(parent=air_sa, org_unit_type=centre_de_sante_type)
+        for centre_sa in centre_de_santes.all():
 
-    # for all the areas in the zone "child"
-    for g in grand_children.all():
+            correct_assignment = Assignment.objects.get_or_create(
+                planning=the_planning, org_unit=centre_sa, user=corresp_profile.user
+            )
 
-        print(f"OrgUnit {g.name}")
-
-        # create 30 (ask claire) POI per area (you will probably need to set a point to the poi in the center of the area)
-
-        try:
-            corresp_profile = profiles.get(org_units=g, account=account)
-        except:
-            corresp_profile = None
-
-        try:
-            corresp_team = Team.objects.get(name=child.name)
-        except:
-            corresp_team = None
-
-        if corresp_profile is None or corresp_team is None:
-            print(f"!! Didnt find a profile or a team for {g.name}")
-            print(f"Profile {corresp_profile}")
-            print(f"Team {corresp_team}")
-        else:
-
-            for i in range(30, 100):
-                # create 30 more POI per area
-
-                print("Location : " + str(get_location(g, child)))
-                print("Type of location : " + str(type(get_location(g, child))))
-
-                ou, created = OrgUnit.objects.get_or_create(
-                    name=f"POI_{i:02d}",  # format avec 0 devant
-                    parent=g,
-                    org_unit_type=point_of_interest_type,
-                    version=account.default_version,
-                    validation_status=OrgUnit.VALIDATION_VALID,
-                    defaults={"location": get_location(g, child)},
-                )
-
-                print("Created? " + str(created))
-
-                print(
-                    f"Created POI_{i:02d} with parent {g.name} type {point_of_interest_type.short_name} location {get_location(g, child)}"
-                )
-
-                # create assignments in the existing plannong of each POIs  to people in the current area
-
-                correct_assignment = Assignment.objects.get_or_create(
-                    planning=the_planning, org_unit=ou, user=corresp_profile.user
-                )
-
-                print(f"Created assignment for {corresp_profile.user.username} for POI_{i:02d}")
+            print(f">> Created assignment for {corresp_profile.user.username} for {centre_sa.name}")
 
 
 # for deleting the wrong assignments created

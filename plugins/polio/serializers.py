63bb49a6109bd58c0c5ad77f8f837ad18e62e200
Christophe Gerard <c@eduz.be>
2023-05-08 13:51:38 +0200

    roll-back

--- plugins/polio/serializers.py
+++ plugins/polio/serializers.py
@@ -483,25 +483,24 @@ class SurgePreviewSerializer(serializers.Serializer):
         return instance
 
 
-class ParentOrgUnitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = OrgUnit
-        fields = ["id", "name", "org_unit_type"]
-
-
 class OrgUnitSerializer(serializers.ModelSerializer):
     country_parent = serializers.SerializerMethodField()
     root = serializers.SerializerMethodField()  # type: ignore
+
+    def __init__(self, *args, **kwargs):
+        for field in kwargs.pop("hidden_fields", []):
+            self.fields.pop(field)
+        super().__init__(*args, **kwargs)
 
     def get_country_parent(self, instance: OrgUnit):
         countries = instance.country_ancestors()
         if countries is not None and len(countries) > 0:
             country = countries[0]
-            return ParentOrgUnitSerializer(instance=country).data
+            return OrgUnitSerializer(instance=country, hidden_fields=["country_parent", "root"]).data
 
     def get_root(self, instance: OrgUnit):
         root = instance.root()
-        return ParentOrgUnitSerializer(instance=root).data if root else None
+        return OrgUnitSerializer(instance=root, hidden_fields=["country_parent", "root"]).data if root else None
 
     class Meta:
         model = OrgUnit

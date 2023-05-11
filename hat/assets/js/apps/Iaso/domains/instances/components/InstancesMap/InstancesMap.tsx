2128dce23c6ae02acfced657ba6a90341d4dfc5e
Christophe Gerard <c@eduz.be>
2023-05-11 12:45:47 +0200

    toggle cluster legend

--- hat/assets/js/apps/Iaso/domains/instances/components/InstancesMap/InstancesMap.tsx
+++ hat/assets/js/apps/Iaso/domains/instances/components/InstancesMap/InstancesMap.tsx
@@ -4,23 +4,20 @@ import React, {
     useMemo,
     useState,
 } from 'react';
+import { commonStyles } from 'bluesquare-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { MapContainer, ScaleControl } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
-import { Grid, Divider } from '@material-ui/core';
+import { Box, makeStyles } from '@material-ui/core';
 
 import {
     getLatLngBounds,
     clusterCustomMarker,
-    // ZoomControl,
     defaultCenter,
     defaultZoom,
 } from '../../../../utils/mapUtils';
 
-import TileSwitch from '../../../../components/maps/tools/TileSwitchComponent';
-import ClusterSwitch from '../../../../components/maps/tools/ClusterSwitchComponent';
 import MarkersListComponent from '../../../../components/maps/markers/MarkersListComponent';
-import InnerDrawer from '../../../../components/nav/InnerDrawer';
 import { useShowWarning } from './useShowWarning';
 import { useResetMapReducerOnUnmount } from './useResetMapReducerOnUnmount';
 import { setCurrentInstance } from '../../actions';
@@ -31,6 +28,8 @@ import { Tile } from '../../../../components/maps/tools/TileSwitch';
 import { TilesSwitchDialog } from '../../../../components/maps/tools/TilesSwitchDialog';
 import { CustomTileLayer } from '../../../../components/maps/CustomTileLayer';
 import { CustomZoomControl } from '../../../../components/maps/CustomZoomControl';
+import { ToggleCluster } from '../../../../components/maps/tools/ToggleCluster';
+
 import tiles from '../../../../constants/mapTiles';
 
 const boundsOptions = { padding: [50, 50] };
@@ -45,13 +44,18 @@ type PartialReduxState = {
     snackBar: { notifications: any[] };
 };
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        ...commonStyles(theme).mapContainer,
+        position: 'relative',
+    },
+}));
 export const InstancesMap: FunctionComponent<Props> = ({
     instances,
     fetching,
 }) => {
-    const isClusterActive = useSelector(
-        (state: PartialReduxState) => state.map.isClusterActive,
-    );
+    const classes = useStyles();
+    const [isClusterActive, setIsClusterActive] = useState<boolean>(true);
     const [currentTile, setCurrentTile] = useState<Tile>(tiles.osm);
     const notifications = useSelector((state: PartialReduxState) =>
         state.snackBar ? state.snackBar.notifications : [],
@@ -88,59 +92,52 @@ export const InstancesMap: FunctionComponent<Props> = ({
 
     if (fetching) return null;
     return (
-        <Grid container spacing={0}>
-            <InnerDrawer
-                withTopBorder
-                settingsOptionComponent={
-                    <>
-                        <TileSwitch />
-                        <Divider />
-                        <ClusterSwitch />
-                    </>
-                }
+        <Box className={classes.root}>
+            <ToggleCluster
+                isClusterActive={isClusterActive}
+                setIsClusterActive={setIsClusterActive}
+            />
+            <TilesSwitchDialog
+                currentTile={currentTile}
+                setCurrentTile={setCurrentTile}
+            />
+            <MapContainer
+                scrollWheelZoom={false}
+                maxZoom={currentTile.maxZoom}
+                style={{ height: '80vh' }}
+                bounds={bounds}
+                boundsOptions={boundsOptions}
+                zoom={defaultZoom}
+                center={defaultCenter}
+                zoomControl={false}
+                keyboard={false}
             >
-                <TilesSwitchDialog
-                    currentTile={currentTile}
-                    setCurrentTile={setCurrentTile}
-                />
-                <MapContainer
-                    scrollWheelZoom={false}
-                    maxZoom={currentTile.maxZoom}
-                    style={{ height: '100%' }}
+                <ScaleControl imperial={false} />
+                <CustomTileLayer currentTile={currentTile} />
+                <CustomZoomControl
                     bounds={bounds}
                     boundsOptions={boundsOptions}
-                    zoom={defaultZoom}
-                    center={defaultCenter}
-                    zoomControl={false}
-                    keyboard={false}
-                >
-                    <ScaleControl imperial={false} />
-                    <CustomTileLayer currentTile={currentTile} />
-                    <CustomZoomControl
-                        bounds={bounds}
-                        boundsOptions={boundsOptions}
-                        fitOnLoad
-                    />
-                    {isClusterActive && (
-                        <MarkerClusterGroup
-                            iconCreateFunction={clusterCustomMarker}
-                        >
-                            <MarkersListComponent
-                                items={instances}
-                                onMarkerClick={fetchAndDispatchDetail}
-                                PopupComponent={InstancePopup}
-                            />
-                        </MarkerClusterGroup>
-                    )}
-                    {!isClusterActive && (
+                    fitOnLoad
+                />
+                {isClusterActive && (
+                    <MarkerClusterGroup
+                        iconCreateFunction={clusterCustomMarker}
+                    >
                         <MarkersListComponent
                             items={instances}
                             onMarkerClick={fetchAndDispatchDetail}
                             PopupComponent={InstancePopup}
                         />
-                    )}
-                </MapContainer>
-            </InnerDrawer>
-        </Grid>
+                    </MarkerClusterGroup>
+                )}
+                {!isClusterActive && (
+                    <MarkersListComponent
+                        items={instances}
+                        onMarkerClick={fetchAndDispatchDetail}
+                        PopupComponent={InstancePopup}
+                    />
+                )}
+            </MapContainer>
+        </Box>
     );
 };

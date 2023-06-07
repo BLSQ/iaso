b7acc6ce717a1c752e6788300b9641beff2ea360
Quang Son Le <qle@bluesquarehub.com>
2023-06-07 14:28:59 +0200

    POLIO-1037: add ReasonForDelayPopper (WIP)
    - still uses placeholder text i.o backend data

---
+++ plugins/polio/js/src/components/Rounds/ReasonForDelayModal/ReasonForDelayPopper.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from 'react';
+import { InfoPopper } from '../../../../../../../hat/assets/js/apps/Iaso/domains/app/components/InfoPopper';
+
+export const ReasonForDelayPopper: FunctionComponent = () => {
+    return (
+        <InfoPopper placement="top-end">
+            <span>Historical data bla bla bla</span>
+        </InfoPopper>
+    );
+};

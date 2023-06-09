327164c3f82e8abffb23d4c9c058a60b679caea5
Quang Son Le <qle@bluesquarehub.com>
2023-06-09 18:14:27 +0200

    IA-2213: add messages

---
+++ hat/assets/js/apps/Iaso/domains/users/components/WarningModal/messages.ts
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+export const MESSAGES = defineMessages({
+    createUserWithoutPerm: {
+        id: 'iaso.users.labels.createUserWithoutPerm',
+        defaultMessage: 'Create user with no permissions?',
+    },
+    cancel: {
+        id: 'iaso.label.cancel',
+        defaultMessage: 'Cancel',
+    },
+    confirm: {
+        id: 'iaso.mappings.confirm',
+        defaultMessage: 'Confirm',
+    },
+});

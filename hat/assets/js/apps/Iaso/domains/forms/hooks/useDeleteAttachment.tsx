f306e1579d5d95412dc560f33f0be74dd42932fd
Quang Son Le <qle@bluesquarehub.com>
2023-06-12 15:10:59 +0200

    IA-2197: add useDeleteTableRow hooks fro reuse

--- hat/assets/js/apps/Iaso/domains/forms/hooks/useDeleteAttachment.tsx
+++ hat/assets/js/apps/Iaso/domains/forms/hooks/useDeleteAttachment.tsx
@@ -1,36 +1,26 @@
-import { UseMutationResult, useQueryClient } from 'react-query';
-import { useDispatch } from 'react-redux';
+import { UseMutationResult } from 'react-query';
 import { deleteRequest } from '../../../libs/Api';
 import { useSnackMutation } from '../../../libs/apiHooks';
-import { redirectToReplace } from '../../../routing/actions';
 import { baseUrls } from '../../../constants/urls';
+import { useDeleteTableRow } from '../../../components/tables/TableWithDeepLink';
 
 const deleteAttachment = (id: number) =>
     deleteRequest(`/api/formattachments/${id}`);
 
 export const useDeleteAttachment = (
-    params: any,
+    params: Record<string, any>,
     count: number,
 ): UseMutationResult => {
-    const queryClient = useQueryClient();
-    const dispatch = useDispatch();
-    const { attachmentsPage, attachmentsPageSize } = params;
-    const onSuccess = () => {
-        const page = parseInt(attachmentsPage, 10);
-        const pageSize = parseInt(attachmentsPageSize, 10);
-        const newCount = count - 1;
-        if (newCount / pageSize > 1) {
-            const newParams = {
-                ...params,
-                attachmentsPage: `${page - 1}`,
-            };
-            dispatch(redirectToReplace(baseUrls.formDetail, newParams));
-        }
-        queryClient.invalidateQueries(['formAttachments']);
-    };
+    const onSuccess = useDeleteTableRow({
+        params,
+        pageKey: 'attachmentsPage',
+        pageSizeKey: 'attachmentsPageSize',
+        count,
+        invalidateQueries: ['formAttachments'],
+        baseUrl: baseUrls.formDetail,
+    });
     return useSnackMutation({
         mutationFn: deleteAttachment,
         options: { onSuccess },
-        // invalidateQueryKey: ['formAttachments'],
     });
 };

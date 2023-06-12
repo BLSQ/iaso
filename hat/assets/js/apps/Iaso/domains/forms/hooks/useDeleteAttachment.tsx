a52709aa24b8744ea3f1795de52d3cfab6905d28
Quang Son Le <qle@bluesquarehub.com>
2023-06-12 12:49:37 +0200

    IA-2197: fix row deletion bug in form attachment

--- hat/assets/js/apps/Iaso/domains/forms/hooks/useDeleteAttachment.tsx
+++ hat/assets/js/apps/Iaso/domains/forms/hooks/useDeleteAttachment.tsx
@@ -1,13 +1,36 @@
-import { UseMutationResult } from 'react-query';
+import { UseMutationResult, useQueryClient } from 'react-query';
+import { useDispatch } from 'react-redux';
 import { deleteRequest } from '../../../libs/Api';
 import { useSnackMutation } from '../../../libs/apiHooks';
+import { redirectToReplace } from '../../../routing/actions';
+import { baseUrls } from '../../../constants/urls';
 
 const deleteAttachment = (id: number) =>
     deleteRequest(`/api/formattachments/${id}`);
 
-export const useDeleteAttachment = (): UseMutationResult => {
+export const useDeleteAttachment = (
+    params: any,
+    count: number,
+): UseMutationResult => {
+    const queryClient = useQueryClient();
+    const dispatch = useDispatch();
+    const { attachmentsPage, attachmentsPageSize } = params;
+    const onSuccess = () => {
+        const page = parseInt(attachmentsPage, 10);
+        const pageSize = parseInt(attachmentsPageSize, 10);
+        const newCount = count - 1;
+        if (newCount / pageSize > 1) {
+            const newParams = {
+                ...params,
+                attachmentsPage: `${page - 1}`,
+            };
+            dispatch(redirectToReplace(baseUrls.formDetail, newParams));
+        }
+        queryClient.invalidateQueries(['formAttachments']);
+    };
     return useSnackMutation({
         mutationFn: deleteAttachment,
-        invalidateQueryKey: ['formAttachments'],
+        options: { onSuccess },
+        // invalidateQueryKey: ['formAttachments'],
     });
 };
--- hat/assets/js/apps/Iaso/domains/forms/hooks/useGetAttachments.tsx
+++ hat/assets/js/apps/Iaso/domains/forms/hooks/useGetAttachments.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable camelcase */
 import { UseQueryResult } from 'react-query';
-
+import { Pagination } from 'bluesquare-components';
 import { useSnackQuery } from '../../../libs/apiHooks';
 import { getRequest } from '../../../libs/Api';
 import { FormAttachment, FormParams } from '../types/forms';
 import { makeUrlWithParams } from '../../../libs/utils';
-import { Pagination } from '../../../types/table';
 
 export interface FormAttachmentsApiResult extends Pagination {
     results: FormAttachment[];

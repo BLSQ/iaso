fcefd832e9bcec0aa2dc695fa881aa830e00e436
Quang Son Le <qle@bluesquarehub.com>
2023-06-19 16:03:40 +0200

    POLIO_1070 cleanup console

--- plugins/polio/js/src/components/MapComponent/MapComponent.js
+++ plugins/polio/js/src/components/MapComponent/MapComponent.js
@@ -53,13 +53,10 @@ export const MapComponent = ({
         if (bounds_list.length === 0) {
             return null;
         }
-        console.log('bounds_list', bounds_list);
         const newBounds = bounds_list[0];
         newBounds.extend(bounds_list);
         return newBounds;
     }, [mainLayer, fitBoundsToBackground, backgroundLayer]);
-    console.log('bounds', bounds);
-    console.log('map bounds', map.current?.leafletElement.getBounds());
 
     return (
         <MapContainer

952e28e351bcd36f6a87b8eb9946f12f4d80715c
Olivier Le Thanh Duong <olivier@lethanh.be>
2023-06-29 11:37:19 +0200

    Better doc string

--- hat/assets/js/apps/Iaso/routing/routing.ts
+++ hat/assets/js/apps/Iaso/routing/routing.ts
@@ -1,6 +1,6 @@
 import { formatPattern, Router } from 'react-router';
 
-/* Generate a new url for the current page with the modified parameters */
+/* Modify the url for the current page and return the new url */
 const genUrl = (router: Router, newParams: Record<string, string>): string =>
     formatPattern(router.routes[0].path, { ...router.params, ...newParams });
 

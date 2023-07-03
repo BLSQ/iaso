fbc240b5b604fbe7c5c8dd9b1e68a22c9d9ac39f
Olivier Le Thanh Duong <olivier@lethanh.be>
2023-07-03 11:51:06 +0200

    POLIO-847 API Add a field on round  districts_count_calculated with the count of district

--- plugins/polio/serializers.py
+++ plugins/polio/serializers.py
@@ -337,6 +337,7 @@ class RoundSerializer(serializers.ModelSerializer):
     shipments = ShipmentSerializer(many=True, required=False)
     destructions = DestructionSerializer(many=True, required=False)
     datelogs = RoundDateHistoryEntrySerializer(many=True, required=False)
+    districts_count_calculated = serializers.IntegerField(read_only=True)
 
     @atomic
     def create(self, validated_data):

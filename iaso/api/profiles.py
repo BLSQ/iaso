2a84dd2d84466331d9fbe76a2dd2a41be534f7fa
Benjamin Monjoie <bmo@appkers.com>
2023-07-18 15:45:51 +0200

    Add merge migration
    IA-1940

--- iaso/api/profiles.py
+++ iaso/api/profiles.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth import models
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.contrib.sites.shortcuts import get_current_site
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import send_mail
 from django.core.paginator import Paginator
 from django.db.models import Q

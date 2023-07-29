6f53a697acf63a51f419c0a591129bdf4f7eedfd
Martin De Wulf <mdewulf@bluesquarehub.com>
2023-07-29 10:07:18 +0200

    WFP ETL V1

--- plugins/wfp/admin.py
+++ plugins/wfp/admin.py
@@ -1,1 +1,13 @@
+from django.contrib import admin
 
+from .models import (
+    Beneficiary,
+    Journey,
+    Step,
+    Visit,
+)
+
+admin.site.register(Beneficiary)
+admin.site.register(Journey)
+admin.site.register(Step)
+admin.site.register(Visit)

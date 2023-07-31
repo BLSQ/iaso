e2ce017432ddf4de428cf47ba405be7ceb7a56f2
beygorghor <cgerard@bluesquarehub.com>
2023-07-31 13:15:58 +0200

    add refresh page button (#738)

--- plugins/polio/js/src/components/LQAS-IM/Filters.tsx
+++ plugins/polio/js/src/components/LQAS-IM/Filters.tsx
@@ -4,10 +4,12 @@ import { Select, useSafeIntl } from 'bluesquare-components';
 import { withRouter } from 'react-router';
 import { useDispatch } from 'react-redux';
 import { replace } from 'react-router-redux';
+import RefreshIcon from '@material-ui/icons/Refresh';
 
-import { Box, Grid, IconButton } from '@material-ui/core';
+import { Box, Grid, IconButton, Button } from '@material-ui/core';
 
 import OpenInNewIcon from '@material-ui/icons/OpenInNew';
+import { useQueryClient } from 'react-query';
 import MESSAGES from '../../constants/messages';
 
 import { useGetCountries } from '../../hooks/useGetCountries';
@@ -79,6 +81,7 @@ const Filters: FunctionComponent<Props> = ({
         ? `/dashboard/polio/list/campaignId/${campaignObj.id}/search/${campaignObj.obr_name}`
         : null;
 
+    const queryClient = useQueryClient();
     return (
         <Box mt={2} width="100%">
             <Grid container item spacing={2}>
@@ -122,6 +125,21 @@ const Filters: FunctionComponent<Props> = ({
                         </IconButton>
                     </Grid>
                 )}
+                <Grid item md={campaignLink ? 3 : 4}>
+                    <Box display="flex" justifyContent="flex-end" width="100%">
+                        <Button
+                            size="small"
+                            variant="contained"
+                            color="primary"
+                            onClick={() => queryClient.resetQueries()}
+                        >
+                            <Box mr={1} pt={1}>
+                                <RefreshIcon fontSize="small" />
+                            </Box>
+                            {formatMessage(MESSAGES.refreshPage)}
+                        </Button>
+                    </Box>
+                </Grid>
             </Grid>
         </Box>
     );

fdad3f0601140b5632eb5dbedcf256cdc9b13bda
Christophe Gerard <c@eduz.be>
2023-08-24 10:19:07 +0200

    some more fixes

--- plugins/polio/js/src/constants/messages.js
+++ plugins/polio/js/src/constants/messages.js
@@ -320,10 +320,6 @@ const MESSAGES = defineMessages({
         id: 'iaso.polio.label.detection_responsible',
         defaultMessage: 'Detection responsible',
     },
-    risk_assessment_rrt_oprtt_approval_at: {
-        id: 'iaso.polio.form.label.rrtOprttApproval',
-        defaultMessage: 'RRT/ORPG-Approval',
-    },
     ag_nopv_group_met_at: {
         id: 'iaso.polio.form.label.agNopvGroup',
         defaultMessage: 'AG/nOPV Group',
@@ -1956,6 +1952,10 @@ const MESSAGES = defineMessages({
     rrtOprttApproval: {
         id: 'iaso.polio.form.label.rrtOprttApproval',
         defaultMessage: 'RRT/OPRTT Approval',
+    },
+    risk_assessment_rrt_oprtt_approval_at: {
+        id: 'iaso.polio.form.label.risk_assessment_rrt_oprtt_approval_at',
+        defaultMessage: 'RRT/ORPG-Approval',
     },
     loading: {
         id: 'iaso.label.loading',

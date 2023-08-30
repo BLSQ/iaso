cddcf5b2d941222c58f3d0adfb094cc56e8cd2fa
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2023-08-30 13:42:27 +0200

    add html template invitation email to be able to highlight the userName and add some other html tags

--- iaso/api/profiles.py
+++ iaso/api/profiles.py
@@ -546,13 +546,13 @@ The {site_name} Team.
 
     CREATE_PASSWORD_HTML_MESSAGE_EN = """<p>Hello,<br><br>
 
-You have been invited to access IASO - <a href="{{protocol}}://{{domain}}" target="_blank">{{account_name}}</a>.<br><br>
+You have been invited to access IASO - <a href="{protocol}://{domain}" target="_blank">{account_name}</a>.<br><br>
 
-Username: <strong>{{userName}}</strong><br><br>
+Username: <strong>{userName}</strong><br><br>
 
 Please click on the link below to create your password:<br><br>
 
-<a href="{{url}}" target="_blank">{{url}}</a><br><br>
+<a href="{url}" target="_blank">{url}</a><br><br>
 
 If clicking the link above doesn't work, please copy and paste the URL in a new browser<br>
 window instead.<br><br>
@@ -583,20 +583,20 @@ L'équipe {site_name}.
 
     CREATE_PASSWORD_HTML_MESSAGE_FR = """<p>Bonjour,<br><br>
 
-Vous avez été invité à accéder à l'IASO - <a href="{{protocol}}://{{domain}}" target="_blank">{{account_name}}</a>.<br><br>
+Vous avez été invité à accéder à l'IASO - <a href="{protocol}://{domain}" target="_blank">{account_name}</a>.<br><br>
 
-Nom d'utilisateur: <strong>{{userName}}</strong><br><br>
+Nom d'utilisateur: <strong>{userName}</strong><br><br>
 
 Pour configurer un mot de passe pour votre compte, merci de cliquer sur le lien ci-dessous :<br><br>
 
-<a href="{{url}}" target="_blank">{{url}}</a><br><br>
+<a href="{url}" target="_blank">{url}</a><br><br>
 
 Si le lien ne fonctionne pas, merci de copier et coller l'URL dans une nouvelle fenêtre de votre navigateur.<br><br>
 
-Si vous n'avez pas demandé de compte sur {{account_name}}, vous pouvez ignorer cet e-mail - aucun mot de passe ne sera créé.<br><br>
+Si vous n'avez pas demandé de compte sur {account_name}, vous pouvez ignorer cet e-mail - aucun mot de passe ne sera créé.<br><br>
 
 Cordialement,<br>
-L'équipe {{site_name}}.</p>
+L'équipe {site_name}.</p>
     """
 
     EMAIL_SUBJECT_FR = "Configurer un mot de passe pour votre nouveau compte sur {dns_domain}"

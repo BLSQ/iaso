0e603d40bd20c78e7ff46e662879f1bbaf3f260e
kemar <marc@marcarea.com>
2023-08-30 17:13:10 +0200

    Ignore typing in `settings_test`

--- hat/settings_test.py
+++ hat/settings_test.py
@@ -4,4 +4,4 @@ from .settings import *  # noqa: F401,F403
 # Django serializes the whole database into a SQL string at the start of a test run, which can take a few seconds.
 # https://docs.djangoproject.com/en/3.2/topics/testing/overview/#test-case-serialized-rollback
 # Fixed in Django 4.0 https://code.djangoproject.com/ticket/32446
-DATABASES["default"]["TEST"] = {"SERIALIZE": False}
+DATABASES["default"]["TEST"] = {"SERIALIZE": False}  # type: ignore

4def5c08cb38380642c96b0ff75ff48a339c5054
Simon Dusausoy <sdusausoy@bluesquarehub.com>
2023-09-11 16:41:51 +0200

    move to tasks

--- plugins/polio/tasks/vaccine_authorizations_mail_alerts.py
+++ plugins/polio/tasks/vaccine_authorizations_mail_alerts.py
@@ -6,19 +6,17 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 
+from beanstalk_worker import task_decorator
 from hat import settings
 from iaso.models import Team
 from plugins.polio.models import VaccineAuthorization
+from logging import getLogger
 
 
-def vaccine_authorizations_60_days_expiration_email_alert():
-    future_date = dt.date.today() + timedelta(days=60)
-    vaccine_auths = VaccineAuthorization.objects.filter(expiration_date=future_date)
+logger = getLogger(__name__)
 
-    team = get_object_or_404(Team, name="nOPV2 vaccine authorization alerts")
 
-    mailing_list = [user.email for user in User.objects.filter(pk__in=team.users.all())]
-
+def vaccine_authorizations_60_days_expiration_email_alert(vaccine_auths, mailing_list):
     mail_sent = False
 
     for obj in vaccine_auths:
@@ -68,14 +66,35 @@ def vaccine_authorizations_60_days_expiration_email_alert():
     return {"vacc_auth_mail_sent_to": mailing_list} if mail_sent else "no_vacc_auth_mail_sent"
 
 
-def expired_vaccine_authorizations_email_alert():
-    past_date = dt.date.today() - timedelta(days=1)
-    vaccine_auths = VaccineAuthorization.objects.filter(expiration_date=past_date)
+@task_decorator(task_name="vaccine_authorizations_60_days_expiration_email_alert")
+def send_email_vaccine_authorizations_60_days_expiration_alert(task=None):
+    future_date = dt.date.today() + timedelta(days=60)
+    vaccine_auths = VaccineAuthorization.objects.filter(expiration_date=future_date)
+    total = vaccine_auths.count()
 
     team = get_object_or_404(Team, name="nOPV2 vaccine authorization alerts")
 
     mailing_list = [user.email for user in User.objects.filter(pk__in=team.users.all())]
+    email_sent = 0
 
+    for i, vacc_auth in enumerate(vaccine_auths):
+        task.report_progress_and_stop_if_killed(
+            progress_value=i,
+            end_value=total,
+            progress_message=f"Campaign {vacc_auth.pk} started",
+        )
+
+        logger.info(f"Email for {vacc_auth}")
+        status = vaccine_authorizations_60_days_expiration_email_alert(vaccine_auths, mailing_list)
+        if not status:
+            logger.info(f"... skipped")
+        else:
+            email_sent += 1
+
+    task.report_success(f"Finished sending {email_sent} nOPV2 vaccine authorization alertsemail(s)")
+
+
+def expired_vaccine_authorizations_email_alert(vaccine_auths, mailing_list):
     mail_sent = False
 
     for obj in vaccine_auths:
@@ -123,9 +142,54 @@ def expired_vaccine_authorizations_email_alert():
     return {"vacc_auth_mail_sent_to": mailing_list} if mail_sent else "no_vacc_auth_mail_sent"
 
 
-def vaccine_authorization_update_expired_entries():
-    expired_vacc_auth = VaccineAuthorization.objects.filter(expiration_date=datetime.date.today() - timedelta(days=1))
+@task_decorator(task_name="expired_vaccine_authorizations_email_alert")
+def send_email_expired_vaccine_authorizations_alert(task=None):
+    past_date = dt.date.today() - timedelta(days=1)
+    vaccine_auths = VaccineAuthorization.objects.filter(expiration_date=past_date)
+    team = get_object_or_404(Team, name="nOPV2 vaccine authorization alerts")
+    mailing_list = [user.email for user in User.objects.filter(pk__in=team.users.all())]
+    total = vaccine_auths.count()
 
-    for vacc_auth in expired_vacc_auth:
+    email_sent = 0
+
+    for i, vacc_auth in enumerate(vaccine_auths):
+        task.report_progress_and_stop_if_killed(
+            progress_value=i,
+            end_value=total,
+            progress_message=f"Campaign {vacc_auth.pk} started",
+        )
+
+        logger.info(f"Email for {vacc_auth}")
+        status = vaccine_authorizations_60_days_expiration_email_alert(vaccine_auths, mailing_list)
+        if not status:
+            logger.info(f"... skipped")
+        else:
+            email_sent += 1
+
+    task.report_success(f"Finished sending {email_sent} nOPV2 vaccine authorization alert email(s)")
+
+
+@task_decorator(task_name="vaccine_authorization_update_expired_entries")
+def vaccine_authorization_update_expired_entries(task=None):
+    expired_vacc_auth = VaccineAuthorization.objects.filter(expiration_date=datetime.date.today() - timedelta(days=1))
+    total = expired_vacc_auth.count()
+
+    expired_auth = 0
+
+    for i, vacc_auth in enumerate(expired_vacc_auth):
+        task.report_progress_and_stop_if_killed(
+            progress_value=i,
+            end_value=total,
+            progress_message=f"Campaign {vacc_auth.pk} started",
+        )
+
         vacc_auth.status = "EXPIRED"
         vacc_auth.save()
+
+        logger.info(f"Campaign {vacc_auth} has expired.")
+        if total == 0:
+            logger.info(f"... skipped")
+        else:
+            expired_auth += 1
+
+    task.report_success(f"{expired_auth} expired nOPV2 vaccine authorization.")

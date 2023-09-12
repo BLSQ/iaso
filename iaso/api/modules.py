2732cd81b5cb64d822457b3da90093381848d406
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2023-09-12 15:40:27 +0200

    add api/modules get list

---
+++ iaso/api/modules.py
@@ -0,0 +1,63 @@
+from rest_framework import permissions, serializers
+from django.db.models import Q, QuerySet
+from django.contrib.auth.models import Permission
+from iaso.models import Module, Account
+from .common import ModelViewSet, HasPermission
+from hat.menupermissions import models as permission
+
+
+class PermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        fields = ("id", "name", "codename")
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ("id", "name")
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    permissions = serializers.SerializerMethodField("get_permissions")
+    account = serializers.SerializerMethodField("get_account")
+
+    class Meta:
+        model = Module
+        fields = ["name", "permissions", "account"]
+
+    def get_permissions(self, obj):
+        return PermissionSerializer(Permission.objects.filter(iaso_module_permission__module=obj), many=True).data
+
+    def get_account(self, obj):
+        account = self.context["request"].user.iaso_profile.account
+        return AccountSerializer(obj.account_modules.filter(id=account.id), many=True).data
+
+
+class ModulesViewSet(ModelViewSet):
+    f"""Modules API
+
+    This API is restricted to authenticated users having the "{permission.MODULES}" permission for reading only
+
+    GET /api/modules/
+    """
+
+    permission_classes = [permissions.IsAuthenticated, HasPermission(permission.MODULES)]  # type: ignore
+    serializer_class = ModuleSerializer
+    http_method_names = ["get"]
+
+    def get_queryset(self) -> QuerySet[Module]:
+        user = self.request.user
+        queryset = Module.objects.all()  # type: ignore
+        search = self.request.GET.get("search", None)
+        orders = self.request.GET.get("order", "name").split(",")
+        account_id = self.request.GET.get("account_id", None)
+
+        if account_id:
+            queryset = queryset.filter(account_modules=user.iaso_profile.account)
+        if search:
+            queryset = queryset.filter(Q(name__icontains=search)).distinct()
+        if orders:
+            queryset = queryset.order_by(*orders)
+
+        return queryset
--- iaso/urls.py
+++ iaso/urls.py
@@ -85,6 +85,7 @@ from .api.workflows.import_export import export_workflow, import_workflow
 from .api.org_unit_validation_status import ValidationStatusViewSet
 from .dhis2.authentication import dhis2_callback  # type: ignore
 from .api.user_roles import UserRolesViewSet
+from .api.modules import ModulesViewSet
 
 URL = Union[URLPattern, URLResolver]
 URLList = List[URL]
@@ -164,6 +165,7 @@ router.register(r"datastore", DataStoreViewSet, basename="datastore")
 router.register(r"validationstatus", ValidationStatusViewSet, basename="validationstatus")
 
 router.register(r"mobile/metadata/lastupdates", LastUpdatesViewSet, basename="lastupdates")
+router.register(r"modules", ModulesViewSet, basename="modules")
 
 router.registry.extend(plugins_router.registry)
 

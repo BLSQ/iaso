df2d89db1331ad8144b344e2a9229dfea96bb5db
Math VDH <mathvdh@gmail.com>
2023-09-15 15:11:04 +0100

    Changes after martin's comments

--- iaso/api/stocks.py
+++ iaso/api/stocks.py
@@ -35,13 +35,13 @@ class StockItemViewSet(ModelViewSet):
         qs = StockItem.objects.filter(account=profile.account).order_by("id")
         return qs
 
-    def destroy(self, request, pk=None):
-        profile = request.user.iaso_profile
-        existing = StockItem.objects.get(id=pk)
-        if existing.account != profile.account:
-            raise ValueError("You can only delete stock items from your own account")
+    # def destroy(self, request, pk=None):
+    #     profile = request.user.iaso_profile
+    #     existing = StockItem.objects.get(id=pk)
+    #     if existing.account != profile.account:
+    #         raise ValueError("You can only delete stock items from your own account")
 
-        return super().destroy(request, pk)
+    #     return super().destroy(request, pk)
 
 
 class StockMovementSerializer(serializers.ModelSerializer):
@@ -51,14 +51,14 @@ class StockMovementSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         profile = self.context["request"].user.iaso_profile
-        wfv = StockMovement.objects.create(account=profile.account, **validated_data)
-        return wfv
+        stock_move = StockMovement.objects.create(account=profile.account, **validated_data)
+        return stock_move
 
 
 class StockMovementViewSet(ModelViewSet):
     f"""Stock Movements API
 
-    This API is restricted to authenticated authenticated users having the permission "{permission.STOCKS}"
+    This API is restricted to authenticated users having the permission "{permission.STOCKS}"
     """
 
     permission_classes = [

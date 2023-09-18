30025516e4cc6c7240a7531e381646f62791d833
Quang Son Le <qle@bluesquarehub.com>
2023-09-18 12:22:35 +0200

    POLIO-1124: add translations

--- plugins/polio/js/src/constants/messages.js
+++ plugins/polio/js/src/constants/messages.js
@@ -2359,6 +2359,38 @@ const MESSAGES = defineMessages({
         defaultMessage:
             'The latest campaign is the campaign with the round that started the most recently within the chosen time frame',
     },
+    latestManualRefresh: {
+        id: 'iaso.polio.label.latestManualRefresh',
+        defaultMessage: 'Latest manual refresh',
+    },
+    SUCCESS: {
+        id: 'iaso.task.label.SUCCESS',
+        defaultMessage: 'Success',
+    },
+    ERRORED: {
+        id: 'iaso.task.label.ERRORED',
+        defaultMessage: 'Errored',
+    },
+    RUNNING: {
+        id: 'iaso.task.label.RUNNING',
+        defaultMessage: 'Running',
+    },
+    QUEUED: {
+        id: 'iaso.task.label.QUEUED',
+        defaultMessage: 'Queued',
+    },
+    SKIPPED: {
+        id: 'iaso.task.label.SKIPPED',
+        defaultMessage: 'Skipped',
+    },
+    KILLED: {
+        id: 'iaso.task.label.KILLED',
+        defaultMessage: 'Killed',
+    },
+    EXPORTED: {
+        id: 'iaso.task.label.EXPORTED',
+        defaultMessage: 'Exported',
+    },
 });
 
 export default MESSAGES;

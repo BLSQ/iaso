90c0d5a706b71b87b5ff7ea00cf575ebaee8f428
Martin De Wulf <mdewulf@bluesquarehub.com>
2023-09-21 20:07:04 +0200

    Making the new ETL available as a task

--- plugins/wfp/management/commands/wfp_etl.py
+++ plugins/wfp/management/commands/wfp_etl.py
@@ -1,10 +1,10 @@
 from ...models import *  # type: ignore
 from django.core.management.base import BaseCommand
-from ...tasks import etl
+from ...tasks import ETL
 
 
 class Command(BaseCommand):
     help = "Transform WFP collected data in a format usable for analytics"
 
     def handle(self, *args, **options):
-        etl()
+        ETL().run()

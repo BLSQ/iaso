b2de512a007ba70ca2be5def2f0d48272cb74143
Matteo <m.cafarotti@gmail.com>
2023-09-24 10:16:38 +0200

    test celery beat

--- hat/settings.py
+++ hat/settings.py
@@ -551,4 +551,5 @@ CACHES = {"default": {"BACKEND": "django.core.cache.backends.db.DatabaseCache", 
 
 # sample celery configuration
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+# CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379")
+CELERY_RESULT_BACKEND = "django-db"

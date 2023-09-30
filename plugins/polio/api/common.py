2b6878e71bfa1c8f47f590607bbe1c9804658d9e
Martin De Wulf <mdewulf@bluesquarehub.com>
2023-09-30 20:57:32 +0200

    Making the criteria on response id name match BEFORE checking the dates

--- plugins/polio/api/common.py
+++ plugins/polio/api/common.py
@@ -67,9 +67,10 @@ def find_lqas_im_campaign(campaigns, today, country, round_number: Optional[int]
 
 
 def find_campaign_on_day(campaigns, day, response_id=None):
+    exact_match = campaigns.filter(obr_name=response_id).first()
+    if exact_match:
+        return exact_match
     for c in campaigns:
-        if c.obr_name == response_id:
-            return c
         if not c.start_date:
             continue
         start_date = c.start_date

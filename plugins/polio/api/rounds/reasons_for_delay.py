1e8ed5497387543319dfbbaa51ab583f0192dfc5
Quang Son Le <qle@bluesquarehub.com>
2023-10-05 12:10:50 +0200

    POLIO_1217: add reasons for delay endpoint

---
+++ plugins/polio/api/rounds/reasons_for_delay.py
@@ -0,0 +1,19 @@
+from plugins.polio.models import ReasonForDelay
+from rest_framework import serializers
+from iaso.api.common import ModelViewSet
+from rest_framework import filters, permissions
+
+
+class ReasonForDelaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReasonForDelay
+        fields = ["id", "name_fr", "name_en", "key_name"]
+
+
+class ReasonForDelayViewSet(ModelViewSet):
+    http_method_names = ["get"]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = ReasonForDelaySerializer
+
+    def get_queryset(self):
+        return ReasonForDelay.objects.filter(deleted_at__isnull=True)

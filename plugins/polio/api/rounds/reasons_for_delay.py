b8ce2e729386a85f24f2dee5b57f3987dc2c8be9
Quang Son Le <qle@bluesquarehub.com>
2023-10-06 09:50:16 +0200

    POLIO-1217: make date fields read-only

--- plugins/polio/api/rounds/reasons_for_delay.py
+++ plugins/polio/api/rounds/reasons_for_delay.py
@@ -1,11 +1,12 @@
 from plugins.polio.models import ReasonForDelay
 from rest_framework import serializers
-from iaso.api.common import ModelViewSet
+from iaso.api.common import ModelViewSet,HasPermission
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from hat.menupermissions import models as permission
 
-
+#TODO switch serializers and @action decorator
 class ReasonForDelaySerializer(serializers.ModelSerializer):
     class Meta:
         model = ReasonForDelay
@@ -16,7 +17,9 @@ class ReasonForDelayTableSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReasonForDelay
         fields = ["id", "name_fr", "name_en", "key_name", "created_at", "updated_at", "times_selected"]
-
+        
+        read_only_fields = ["created_at", "updated_at"]
+        
     times_selected = serializers.SerializerMethodField()
 
     def get_times_selected(self, reason_for_delay):
@@ -32,7 +35,7 @@ class ReasonForDelayViewSet(ModelViewSet):
         account = self.request.user.iaso_profile.account
         return ReasonForDelay.objects.filter(deleted_at__isnull=True).filter(account=account)
 
-    @action(methods=["GET"], detail=False)
+    @action(methods=["GET"], detail=False, permission_classes = [HasPermission(permission.POLIO_CONFIG)])
     def tableview(self, request):
         queryset = self.get_queryset()
         reasons_for_delay = ReasonForDelayTableSerializer(queryset, many=True).data

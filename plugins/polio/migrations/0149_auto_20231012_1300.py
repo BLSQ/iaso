ca36edb38c80c1b2bf9dbcd7223e0bb9bfb618a7
Math VDH <mathvdh@gmail.com>
2023-10-12 14:16:07 +0100

    Black formatting migration file

--- plugins/polio/migrations/0149_auto_20231012_1300.py
+++ plugins/polio/migrations/0149_auto_20231012_1300.py
@@ -4,31 +4,30 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('polio', '0148_merge_20231002_1610'),
+        ("polio", "0148_merge_20231002_1610"),
     ]
 
     operations = [
         migrations.AlterUniqueTogether(
-            name='roundvaccine',
+            name="roundvaccine",
             unique_together=None,
         ),
         migrations.RemoveField(
-            model_name='roundvaccine',
-            name='round',
+            model_name="roundvaccine",
+            name="round",
         ),
         migrations.RemoveField(
-            model_name='shipment',
-            name='round',
+            model_name="shipment",
+            name="round",
         ),
         migrations.DeleteModel(
-            name='Destruction',
+            name="Destruction",
         ),
         migrations.DeleteModel(
-            name='RoundVaccine',
+            name="RoundVaccine",
         ),
         migrations.DeleteModel(
-            name='Shipment',
+            name="Shipment",
         ),
     ]

a499b8f6538c4f10163be9818515d7d9c0c1fdbe
butofleury <fleurybu@gmail.com>
2023-10-16 17:01:56 +0200

    Move the common functions in separate files

--- plugins/wfp/management/commands/wfp_etl.py
+++ plugins/wfp/management/commands/wfp_etl.py
@@ -1,10 +1,11 @@
-from ...models import *  # type: ignore
 from django.core.management.base import BaseCommand
-from ...tasks import ETL
+from wfp_etl_Under5 import Under5
+from wfp_etl_pbwg import PBWG
 
 
 class Command(BaseCommand):
     help = "Transform WFP collected data in a format usable for analytics"
 
     def handle(self, *args, **options):
-        ETL().run()
+        Under5.run()
+        PBWG.run()

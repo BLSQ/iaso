a0ffd9cab8d99fb52c6353e306a30739aa83c9dd
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2023-10-17 16:29:58 +0200

    fix black formatting error in iaso/migrations/0238_alter_account_modules.py

--- iaso/migrations/0238_alter_account_modules.py
+++ iaso/migrations/0238_alter_account_modules.py
@@ -5,15 +5,33 @@ import iaso.models.base
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('iaso', '0237_add_modules_to_all_existing_accounts'),
+        ("iaso", "0237_add_modules_to_all_existing_accounts"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='account',
-            name='modules',
-            field=iaso.models.base.ChoiceArrayField(base_field=models.CharField(choices=[('DATA_COLLECTION_FORMS', 'Data collection - Forms'), ('DEFAULT', 'Default'), ('DHIS2_MAPPING', 'DHIS2 mapping'), ('EMBEDDED_LINKS', 'Embedded links'), ('ENTITIES', 'Entities'), ('EXTERNAL_STORAGE', 'External storage'), ('PLANNING', 'Planning'), ('POLIO_PROJECT', 'Polio project'), ('REGISTRY', 'Registry')], max_length=100), blank=True, default=list, null=True, size=None),
+            model_name="account",
+            name="modules",
+            field=iaso.models.base.ChoiceArrayField(
+                base_field=models.CharField(
+                    choices=[
+                        ("DATA_COLLECTION_FORMS", "Data collection - Forms"),
+                        ("DEFAULT", "Default"),
+                        ("DHIS2_MAPPING", "DHIS2 mapping"),
+                        ("EMBEDDED_LINKS", "Embedded links"),
+                        ("ENTITIES", "Entities"),
+                        ("EXTERNAL_STORAGE", "External storage"),
+                        ("PLANNING", "Planning"),
+                        ("POLIO_PROJECT", "Polio project"),
+                        ("REGISTRY", "Registry"),
+                    ],
+                    max_length=100,
+                ),
+                blank=True,
+                default=list,
+                null=True,
+                size=None,
+            ),
         ),
     ]

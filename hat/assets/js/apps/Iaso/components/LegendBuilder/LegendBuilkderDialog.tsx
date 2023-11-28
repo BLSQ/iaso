c1f4799aed815a79197d4d17bac1d8c61866cae1
Christophe Gerard <c@eduz.be>
2023-11-28 14:32:42 +0100

    import issue

--- hat/assets/js/apps/Iaso/components/LegendBuilder/LegendBuilkderDialog.tsx
+++ hat/assets/js/apps/Iaso/components/LegendBuilder/LegendBuilkderDialog.tsx
@@ -15,7 +15,7 @@ import { Box, Button } from '@material-ui/core';
 import Add from '@material-ui/icons/Add';
 import { MESSAGES } from './messages';
 import { ScaleThreshold } from './types';
-import { LegendBuilder } from '.';
+import { LegendBuilder } from './LegendBuilder';
 import { getRangeValues, getScaleThreshold } from './utils';
 
 type Props = {

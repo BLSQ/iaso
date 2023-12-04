f9f8a87ff863ebc325a103fe60d2981a357444eb
Bram Jans <ik@bramjans.com>
2023-12-04 14:30:29 +0100

    TRIA-7 Add middleware from Trypelim to fetch current_user

--- hat/settings.py
+++ hat/settings.py
@@ -223,6 +223,7 @@ MIDDLEWARE += [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "hat.middleware.ThreadLocalMiddleware",
 ]
 
 ROOT_URLCONF = "hat.urls"

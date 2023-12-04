601b057e995743b3f63123f87dc9889581a9689d
Bram Jans <ik@bramjans.com>
2023-12-04 14:37:47 +0100

    TRIA-7 Add some JS config to support Trypelim

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,9 +1,11 @@
 import polioConfig from './polio/js/config';
 import registryConfig from './registry/js/config';
 import testAppConfig from './test/js/config';
+import trypelimConfig from './trypelim/assets/js/config';
 
 export default {
     test: testAppConfig,
     polio: polioConfig,
     registry: registryConfig,
+    trypelim: trypelimConfig,
 };

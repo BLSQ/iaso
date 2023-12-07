9d19dbec5051901d6cbabb5be55b0efd5cc754a9
Quang Son Le <qle@bluesquarehub.com>
2023-12-07 13:28:29 +0100

    chore: typo

--- plugins/polio/js/src/domains/VaccineModule/StockManagement/hooks/api.ts
+++ plugins/polio/js/src/domains/VaccineModule/StockManagement/hooks/api.ts
@@ -64,7 +64,7 @@ export const useGetVaccineStockList = (
 
 // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 const getUsableVials = async (queryString: string) => {
-    console.log('unusable vials', queryString);
+    console.log('usable vials', queryString);
     await waitFor(750);
     return mockUsableVials;
 };

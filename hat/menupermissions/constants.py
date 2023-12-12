971c1787104adf6e6bb10001cecdeac5b894879b
Quang Son Le <qle@bluesquarehub.com>
2023-12-12 16:27:14 +0100

    POLIO-1339: use supply chain permissions
    - replace iaso-polio with supply chain read and write
    - block edition if no write access
    - block access if no read access (treating write as write only i. o read/write)

--- hat/menupermissions/constants.py
+++ hat/menupermissions/constants.py
@@ -24,7 +24,14 @@ MODULE_PERMISSIONS = {
     "ENTITIES": ["iaso_entities", "iaso_workflows", "iaso_entity_duplicates_write", "iaso_entity_duplicates_read"],
     "EXTERNAL_STORAGE": ["iaso_storages"],
     "PLANNING": ["iaso_assignments"],
-    "POLIO_PROJECT": ["iaso_polio_config", "iaso_polio", "iaso_polio_budget_admin", "iaso_polio_budget"],
+    "POLIO_PROJECT": [
+        "iaso_polio_config",
+        "iaso_polio",
+        "iaso_polio_budget_admin",
+        "iaso_polio_budget",
+        "iaso_polio_vaccine_supply_chain_read",
+        "iaso_polio_vaccine_supply_chain_write",
+    ],
     "REGISTRY": ["iaso_registry", "iaso_test", "iaso_testtt", "iaso_testttr", "iaso_testttrr"],
 }
 

14bff500dcdb9464d67ae578d4f02c6ee484ba73
Marc Hertzog <marc@marcarea.com>
2023-12-13 14:29:37 +0100

    Remove superfluous condition
    Co-authored-by: Benjamin Monjoie <bmo@appkers.com>

--- iaso/api/org_unit_change_requests/permissions.py
+++ iaso/api/org_unit_change_requests/permissions.py
@@ -5,8 +5,6 @@ from hat.menupermissions import models as iaso_permission
 
 class HasOrgUnitsChangeRequestPermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_superuser:
-            return True
         return request.user.is_authenticated
 
 

0ef9b1cbf0903efe586f4073b5e4c623ac1f77b4
kemar <marc@marcarea.com>
2023-12-14 10:27:30 +0100

    Extract columns in a hook to void cluttering the table component

--- plugins/polio/js/src/domains/Notifications/Table/NotificationsTable.tsx
+++ plugins/polio/js/src/domains/Notifications/Table/NotificationsTable.tsx
@@ -1,20 +1,16 @@
 import React, { FunctionComponent } from 'react';
-import { Column, useSafeIntl } from 'bluesquare-components';
 
-import { DateCell } from '../../../../../../../hat/assets/js/apps/Iaso/components/Cells/DateTimeCell';
 import { TableWithDeepLink } from '../../../../../../../hat/assets/js/apps/Iaso/components/tables/TableWithDeepLink';
 import { handleTableDeepLink } from '../../../../../../../hat/assets/js/apps/Iaso/utils/table';
 
-import MESSAGES from '../messages';
-import { DeleteNotificationModal } from '../Modals/NotificationsDeleteModal';
-import { EditNotificationModal } from '../Modals/NotificationsCreateEditModal';
-import { NOTIFICATIONS_BASE_URL } from '../../../constants/routes';
-import { useGetNotifications } from '../hooks/api';
 import {
     ApiNotificationsParams,
     DropdownsContent,
     NotificationsParams,
 } from '../types';
+import { NOTIFICATIONS_BASE_URL } from '../../../constants/routes';
+import { useGetNotifications } from '../hooks/api';
+import { useNotificationsTableColumns } from './useNotificationsTableColumns';
 
 type Props = { params: NotificationsParams; dropdownContent: DropdownsContent };
 
@@ -29,98 +25,7 @@ export const NotificationsTable: FunctionComponent<Props> = ({
         page: params.page || '1',
     };
     const { data, isFetching } = useGetNotifications(apiParams);
-    const { formatMessage } = useSafeIntl();
-    const columns: Array<Column> = [
-        {
-            Header: formatMessage(MESSAGES.labelId),
-            id: 'id',
-            accessor: 'id',
-        },
-        {
-            Header: formatMessage(MESSAGES.labelEpid),
-            id: 'epid_number',
-            accessor: 'epid_number',
-        },
-        {
-            Header: formatMessage(MESSAGES.labelVdpvCategory),
-            id: 'vdpv_category',
-            accessor: 'get_vdpv_category_display',
-        },
-        {
-            Header: formatMessage(MESSAGES.labelSource),
-            id: 'source',
-            accessor: 'get_source_display',
-        },
-        {
-            Header: formatMessage(MESSAGES.labelVdpvNucleotideDiffSabin2),
-            id: 'vdpv_nucleotide_diff_sabin2',
-            accessor: 'vdpv_nucleotide_diff_sabin2',
-        },
-        {
-            Header: formatMessage(MESSAGES.labelCountry),
-            id: 'country',
-            accessor: 'country',
-            sortable: false,
-        },
-        {
-            Header: formatMessage(MESSAGES.labelProvince),
-            id: 'province',
-            accessor: 'province',
-            sortable: false,
-        },
-        {
-            Header: formatMessage(MESSAGES.labelDistrict),
-            id: 'district',
-            accessor: 'district',
-            sortable: false,
-        },
-        {
-            Header: formatMessage(MESSAGES.labelSiteName),
-            id: 'site_name',
-            accessor: 'site_name',
-        },
-        {
-            Header: formatMessage(MESSAGES.labelLineage),
-            id: 'lineage',
-            accessor: 'lineage',
-        },
-        {
-            Header: formatMessage(MESSAGES.labelDateOfOnset),
-            id: 'date_of_onset',
-            accessor: 'date_of_onset',
-            Cell: DateCell,
-        },
-        {
-            Header: formatMessage(MESSAGES.labelClosestMatchVdpv2),
-            id: 'closest_match_vdpv2',
-            accessor: 'closest_match_vdpv2',
-        },
-        {
-            Header: formatMessage(MESSAGES.labelDateResultsReceived),
-            id: 'date_results_received',
-            accessor: 'date_results_received',
-            Cell: DateCell,
-        },
-        {
-            Header: formatMessage(MESSAGES.labelActions),
-            sortable: false,
-            Cell: settings => {
-                return (
-                    <>
-                        {/* @ts-ignore */}
-                        <EditNotificationModal
-                            dropdownContent={dropdownContent}
-                            notification={settings.row.original}
-                        />
-                        {/* @ts-ignore */}
-                        <DeleteNotificationModal
-                            notification={settings.row.original}
-                        />
-                    </>
-                );
-            },
-        },
-    ];
+    const columns = useNotificationsTableColumns(dropdownContent);
     return (
         <TableWithDeepLink
             baseUrl={NOTIFICATIONS_BASE_URL}

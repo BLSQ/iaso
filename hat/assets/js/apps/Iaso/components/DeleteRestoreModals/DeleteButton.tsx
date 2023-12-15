326b36c2b3f2cb5118e8a92ef3dd6bf76cf1ad16
Quang Son Le <qle@bluesquarehub.com>
2023-12-15 19:49:01 +0100

    IA-2570: add delete and restore modals

---
+++ hat/assets/js/apps/Iaso/components/DeleteRestoreModals/DeleteButton.tsx
@@ -0,0 +1,28 @@
+import React, { FunctionComponent } from 'react';
+import { IntlMessage, useSafeIntl } from 'bluesquare-components';
+import { defineMessages } from 'react-intl';
+import { Button } from '@material-ui/core';
+
+const messages = defineMessages({
+    delete: {
+        id: 'iaso.label.delete',
+        defaultMessage: 'Delete',
+    },
+});
+
+type Props = {
+    onClick: () => void;
+    message?: IntlMessage;
+};
+
+export const DeleteButton: FunctionComponent<Props> = ({
+    onClick,
+    message,
+}) => {
+    const { formatMessage } = useSafeIntl();
+    return (
+        <Button onClick={onClick} variant="contained" color="primary">
+            {message ?? formatMessage(messages.delete)}
+        </Button>
+    );
+};
---
+++ hat/assets/js/apps/Iaso/components/DeleteRestoreModals/YesNoButtons.tsx
@@ -0,0 +1,45 @@
+import { Button, Grid } from '@material-ui/core';
+import React, { FunctionComponent } from 'react';
+import { useSafeIntl } from 'bluesquare-components';
+import { MESSAGES } from './messages';
+
+type Props = {
+    closeDialog: () => void;
+    onConfirm: () => void;
+    onCancel?: () => void;
+};
+
+export const YesNoButtons: FunctionComponent<Props> = ({
+    closeDialog,
+    onConfirm,
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    onCancel = () => {},
+}) => {
+    const { formatMessage } = useSafeIntl();
+
+    return (
+        <Grid container justifyContent="flex-end" spacing={2}>
+            <Grid>
+                <Button
+                    onClick={() => {
+                        onCancel();
+                        closeDialog();
+                    }}
+                    color="primary"
+                    data-test="cancel-button"
+                >
+                    {formatMessage(MESSAGES.no)}
+                </Button>
+                <Button
+                    data-test="confirm-button"
+                    onClick={() => {
+                        onConfirm();
+                    }}
+                    color="primary"
+                >
+                    {formatMessage(MESSAGES.yes)}
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};

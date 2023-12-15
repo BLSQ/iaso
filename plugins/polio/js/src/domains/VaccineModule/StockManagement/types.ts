fb725215b99d84f5f5dea631c9bf6e6ecb59cee3
Quang Son Le <38907762+quang-le@users.noreply.github.com>
2023-12-15 12:49:47 +0100

    Polio-1328: stock management details table(mock data) (#965)
    * POLIO-1328: update mockData
    
    * POLIO-1328: add route, translations, types
    
    * POLIO-1328: add useTabs
    
    * POLIO-1328: add details view (WIP)
    
    * POLIO-1328: add messages, types
    
    * POLIO-1328: extract PaperTableRow from BeneficiaryBaseInfo
    
    * POLIO-1328: details view with mock data
    
    * POLIO-1328: log mock api params in console
    
    * POLIO-1328: improve params conversion
    
    * chore: typo
    
    * POLIO-1328: fix table pageSize bug
    
    * chore: remove log
    
    * POLIO-1328: split details table in 2
    
    * POLIO-1328: add missing translations
    
    * POLIO-1328: add trads, improve table layout
    
    * POLIO-1328: code review
    - move tabs
    - remove extra padding
    
    * POLIO-1328: fix translations
    
    ---------
    
    Co-authored-by: Christophe Gerard <c@eduz.be>

--- plugins/polio/js/src/domains/VaccineModule/StockManagement/types.ts
+++ plugins/polio/js/src/domains/VaccineModule/StockManagement/types.ts
@@ -8,3 +8,17 @@ export type StockManagementListParams = {
     country_id?: string; // number as string
     vaccine_type?: string;
 };
+
+export type TabValue = 'usableVials' | 'unusableVials';
+
+export type StockManagementDetailsParams = {
+    accountId: string; // number as string
+    id: string; // number as string
+    tab: TabValue;
+    unusableVialsPageSize: string; // number as string
+    unusableVialsPage: string; // number as string
+    unusableVialsOrder: string; // number as string
+    usableVialsPageSize: string; // number as string
+    usableVialsPage: string; // number as string
+    usableVialsOrder: string; // number as string
+};

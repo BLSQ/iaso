ec440665d4a4b6a91953d19718fd9ba30b35edfb
kemar <marc@marcarea.com>
2023-12-18 11:24:05 +0100

    Align the filter button

--- plugins/polio/js/src/domains/Notifications/Filters/NotificationsFilters.tsx
+++ plugins/polio/js/src/domains/Notifications/Filters/NotificationsFilters.tsx
@@ -29,71 +29,80 @@ export const NotificationsFilters: FunctionComponent<Props> = ({
     const { formatMessage } = useSafeIntl();
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12} md={4} lg={3}>
-                <InputComponent
-                    type="select"
-                    clearable
-                    keyValue="country"
-                    value={filters.country}
-                    onChange={handleChange}
-                    options={notificationsMetaData.country}
-                    labelString={formatMessage(MESSAGES.labelCountry)}
-                />
-            </Grid>
-            <Grid item xs={12} md={4} lg={3}>
-                <InputComponent
-                    type="select"
-                    clearable
-                    keyValue="vdpv_category"
-                    value={filters.vdpv_category}
-                    onChange={handleChange}
-                    options={notificationsMetaData.vdpv_category}
-                    labelString={formatMessage(MESSAGES.labelVdpvCategory)}
-                />
-            </Grid>
-            <Grid item xs={12} md={4} lg={3}>
-                <InputComponent
-                    type="select"
-                    clearable
-                    keyValue="source"
-                    value={filters.source}
-                    onChange={handleChange}
-                    options={notificationsMetaData.source}
-                    labelString={formatMessage(MESSAGES.labelSource)}
-                />
-            </Grid>
-            <Grid item xs={12} md={4} lg={3}>
-                <DatesRange
-                    onChangeDate={handleChange}
-                    dateFrom={filters.date_of_onset_after}
-                    dateTo={filters.date_of_onset_before}
-                    labelFrom={MESSAGES.labelDateOfOnsetAfter}
-                    labelTo={MESSAGES.labelDateOfOnsetBefore}
-                    keyDateFrom="date_of_onset_after"
-                    keyDateTo="date_of_onset_before"
-                />
-            </Grid>
-            <Grid container item xs={12} md={12} justifyContent="flex-end">
-                <Box mt={2}>
-                    <FilterButton
-                        disabled={!filtersUpdated}
-                        onFilter={handleSearch}
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={4} lg={4}>
+                    <InputComponent
+                        type="select"
+                        clearable
+                        keyValue="country"
+                        value={filters.country}
+                        onChange={handleChange}
+                        options={notificationsMetaData.country}
+                        labelString={formatMessage(MESSAGES.labelCountry)}
                     />
-                </Box>
+                </Grid>
+                <Grid item xs={12} md={4} lg={4}>
+                    <InputComponent
+                        type="select"
+                        clearable
+                        keyValue="vdpv_category"
+                        value={filters.vdpv_category}
+                        onChange={handleChange}
+                        options={notificationsMetaData.vdpv_category}
+                        labelString={formatMessage(MESSAGES.labelVdpvCategory)}
+                    />
+                </Grid>
+                <Grid item xs={12} md={4} lg={4}>
+                    <InputComponent
+                        type="select"
+                        clearable
+                        keyValue="source"
+                        value={filters.source}
+                        onChange={handleChange}
+                        options={notificationsMetaData.source}
+                        labelString={formatMessage(MESSAGES.labelSource)}
+                    />
+                </Grid>
+                <Grid item xs={12} md={9} lg={6}>
+                    <DatesRange
+                        onChangeDate={handleChange}
+                        dateFrom={filters.date_of_onset_after}
+                        dateTo={filters.date_of_onset_before}
+                        labelFrom={MESSAGES.labelDateOfOnsetAfter}
+                        labelTo={MESSAGES.labelDateOfOnsetBefore}
+                        keyDateFrom="date_of_onset_after"
+                        keyDateTo="date_of_onset_before"
+                    />
+                </Grid>
+                <Grid
+                    container
+                    item
+                    xs={12}
+                    md={3}
+                    lg={6}
+                    justifyContent="flex-end"
+                >
+                    <Box mt={2}>
+                        <FilterButton
+                            disabled={!filtersUpdated}
+                            onFilter={handleSearch}
+                        />
+                    </Box>
+                </Grid>
             </Grid>
             <Grid container item xs={12} md={12} justifyContent="flex-end">
-                <Box ml={2}>
+                <Box ml={2} mt={2}>
                     {/* @ts-ignore */}
                     <BulkImportNotificationModal />
                 </Box>
-                <Box ml={2}>
+                <Box ml={2} mt={2}>
                     <CreateNotificationModal
                         iconProps={{ message: MESSAGES.modalAddTitle }}
                         notificationsMetaData={notificationsMetaData}
                     />
                 </Box>
             </Grid>
-        </Grid>
+        </>
     );
 };

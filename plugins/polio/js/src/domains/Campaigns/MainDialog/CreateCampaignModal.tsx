26a423baa6b54782a379911dc88faf6fae566ca4
Quang Son Le <qle@bluesquarehub.com>
2023-12-19 13:02:52 +0100

    POLIO-1357: refactor CampaignModal WIP

---
+++ plugins/polio/js/src/domains/Campaigns/MainDialog/CreateCampaignModal.tsx
@@ -0,0 +1,42 @@
+import React, { FunctionComponent, useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { AddButton, useSafeIntl } from 'bluesquare-components';
+import { redirectTo } from '../../../../../../../hat/assets/js/apps/Iaso/routing/actions';
+import { genUrl } from '../../../../../../../hat/assets/js/apps/Iaso/routing/routing';
+import MESSAGES from '../../../constants/messages';
+import { PolioCreateEditDialog } from './CreateEditDialog';
+import { Router } from '../../../../../../../hat/assets/js/apps/Iaso/types/general';
+
+type Props = { router?: Router; params?: any };
+
+export const CreateCampaignModal: FunctionComponent<Props> = ({
+    router,
+    params,
+}) => {
+    const { formatMessage } = useSafeIntl();
+    const dispatch = useDispatch();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const closeDialog = useCallback(() => {
+        setIsOpen(false);
+        const url = genUrl(router, {
+            campaignId: undefined,
+        });
+        dispatch(redirectTo(url));
+    }, [dispatch, router]);
+
+    return (
+        <>
+            <AddButton
+                onClick={() => {
+                    setIsOpen(true);
+                }}
+            >
+                {formatMessage(MESSAGES.create)}
+            </AddButton>
+            {isOpen && (
+                <PolioCreateEditDialog isOpen={isOpen} onClose={closeDialog} />
+            )}
+        </>
+    );
+};

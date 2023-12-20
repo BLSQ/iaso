13febfad27223564b144e4cea3fd5ef43c659f17
kemar <marc@marcarea.com>
2023-12-20 15:22:08 +0100

    Display Polio Notification Import details in the tasks UI

---
+++ hat/assets/js/apps/Iaso/domains/tasks/components/NotificationImportDetailButton.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent } from 'react';
+import { IconButton } from 'bluesquare-components';
+
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+import MESSAGES from '../messages';
+
+type IconButtonProps = {
+    onClick: () => void;
+    dataTestId?: string;
+};
+
+export const NotificationImportDetailButton: FunctionComponent<IconButtonProps> =
+    ({ onClick, dataTestId = 'roundHistoryButton' }) => {
+        return (
+            <IconButton
+                dataTestId={dataTestId}
+                onClick={onClick}
+                overrideIcon={VisibilityIcon}
+                tooltipMessage={MESSAGES.polioNotificationImportDetails}
+            />
+        );
+    };
---
+++ hat/assets/js/apps/Iaso/domains/tasks/components/NotificationImportDetailModal.tsx
@@ -0,0 +1,78 @@
+import React, { FunctionComponent } from 'react';
+
+import {
+    LoadingSpinner,
+    makeFullModal,
+    useSafeIntl,
+} from 'bluesquare-components';
+import { AlertModal } from '../../../components/AlertModal/AlertModal';
+
+import MESSAGES from '../messages';
+import { NotificationImportDetailButton } from './NotificationImportDetailButton';
+import { Task } from '../types';
+import { useGetPolioNotificationImport } from '../hooks/api';
+
+type Props = {
+    task: Task<any>;
+    isOpen: boolean;
+    closeDialog: () => void;
+};
+
+const NotificationImportDetailModal: FunctionComponent<Props> = ({
+    task,
+    isOpen,
+    closeDialog,
+}) => {
+    const { formatMessage } = useSafeIntl();
+
+    const {
+        data: notificationImport,
+        isFetching: isFetchingNotificationImport,
+    } = useGetPolioNotificationImport(task.polio_notification_id);
+
+    return (
+        <AlertModal
+            isOpen={isOpen}
+            closeDialog={closeDialog}
+            titleMessage={formatMessage(
+                MESSAGES.polioNotificationImportDetails,
+            )}
+            maxWidth="md"
+        >
+            {isFetchingNotificationImport && <LoadingSpinner />}
+            {!isFetchingNotificationImport && (
+                <>
+                    <p>{task.result.message}</p>
+                    {notificationImport &&
+                        notificationImport.errors.length > 0 && (
+                            <>
+                                <p>
+                                    {formatMessage(
+                                        MESSAGES.polioNotificationImportErrors,
+                                        {
+                                            count: notificationImport.errors
+                                                .length,
+                                        },
+                                    )}
+                                </p>
+                                <pre style={{ maxHeight: '550px' }}>
+                                    {JSON.stringify(
+                                        notificationImport.errors,
+                                        null,
+                                        ' ',
+                                    )}
+                                </pre>
+                            </>
+                        )}
+                </>
+            )}
+        </AlertModal>
+    );
+};
+
+const modalWithIconButton = makeFullModal(
+    NotificationImportDetailModal,
+    NotificationImportDetailButton,
+);
+
+export { modalWithIconButton as NotificationImportDetailModal };

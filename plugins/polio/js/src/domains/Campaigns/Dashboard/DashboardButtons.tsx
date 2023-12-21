4ba437d0de2e86a346de968bdcb3596796e1da6d
Quang Son Le <qle@bluesquarehub.com>
2023-12-21 15:36:02 +0100

    POLIO-1253: fix dashboard buttons positioning

--- plugins/polio/js/src/domains/Campaigns/Dashboard/DashboardButtons.tsx
+++ plugins/polio/js/src/domains/Campaigns/Dashboard/DashboardButtons.tsx
@@ -27,23 +27,21 @@ export const DashboardButtons: FunctionComponent<Props> = ({
     const classes: Record<string, string> = useStyles();
     const isUserAdmin = userHasPermission('iaso_polio_config', currentUser);
     return (
-        <Box mr={1}>
-            <Grid
-                container
-                className={classes.marginBottom}
-                spacing={2}
-                justifyContent="flex-end"
-                alignItems="center"
-            >
-                {isUserAdmin && (
-                    <Box mr={2}>
-                        <CreateCampaignModal router={router} />
-                    </Box>
-                )}
-                <LinkButton icon={DownloadIcon} url={exportToCSV}>
-                    {formatMessage(MESSAGES.csv)}
-                </LinkButton>
-            </Grid>
-        </Box>
+        <Grid
+            container
+            className={classes.marginBottom}
+            spacing={2}
+            justifyContent="flex-end"
+            alignItems="center"
+        >
+            {isUserAdmin && (
+                <Box mr={2}>
+                    <CreateCampaignModal router={router} />
+                </Box>
+            )}
+            <LinkButton icon={DownloadIcon} url={exportToCSV}>
+                {formatMessage(MESSAGES.csv)}
+            </LinkButton>
+        </Grid>
     );
 };

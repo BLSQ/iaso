4301d1aaf25785fd766b374a9726d35114231c8e
Bram Jans <ik@bramjans.com>
2023-12-27 11:14:23 +0100

    feat: Add Trypelim crontab and couchdb proxypass
    Also a few ENV vars and a small update on the Iaso API client.
    Note: the ETL script runs hourly.

---
+++ .platform/confighooks/postdeploy/set_up_couchdb_proxy_pass.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+################################################################################
+# Note: this is the exact same file as in the .platform/hooks folder.
+# The script in confighooks is executed upon deploy. This file is executed on 
+# configuration changes such as changing ENV variables. 
+# We need to run this in both situations and I couldn't find a way to do it with 1 file.
+################################################################################
+
+# Since we want to use the env var COUCHDB_URL, we cannot create this file directly
+# in the nginx config.
+
+# Create new file in tmp with nginx config.
+cat <<'EOF' > /tmp/02_trypelim_couchdb.conf
+location /_couchdb {
+  proxy_set_header Host $host;
+  proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+  rewrite /_couchdb/(.*) /$1 break;
+  proxy_pass ${COUCHDB_URL};
+}
+EOF
+
+# Now create the file in the desired location with the env var subsituted for the
+# real couchdb URL.
+envsubst '$COUCHDB_URL' < /tmp/02_trypelim_couchdb.conf > /etc/nginx/conf.d/elasticbeanstalk/02_trypelim_couchdb.conf
+
+# End by restarting nginx
+systemctl restart nginx

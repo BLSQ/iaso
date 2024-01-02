5d8c6ebd17c8baae128da5f457bf275f724c5790
Bram Jans <ik@bramjans.com>
2024-01-02 11:31:11 +0100

    TRIA-7 Remove some links from main navigation that aren't relevant for Trypelim

--- hat/assets/js/apps/Iaso/constants/menu.tsx
+++ hat/assets/js/apps/Iaso/constants/menu.tsx
@@ -161,20 +161,20 @@ const menuItems = (
                     permissions: paths.userRolesPath.permissions,
                     icon: props => <GroupsIcon {...props} />,
                 },
-                {
-                    label: formatMessage(MESSAGES.teams),
-                    permissions: paths.teamsPath.permissions,
-                    key: 'teams',
-                    icon: props => <Diversity3Icon {...props} />,
-                },
+                // {
+                //     label: formatMessage(MESSAGES.teams),
+                //     permissions: paths.teamsPath.permissions,
+                //     key: 'teams',
+                //     icon: props => <Diversity3Icon {...props} />,
+                // },
             ],
         },
-        {
-            label: formatMessage(MESSAGES.monitoring),
-            key: 'devices',
-            permissions: paths.devicesPath.permissions,
-            icon: props => <ImportantDevicesRoundedIcon {...props} />,
-        },
+        // {
+        //     label: formatMessage(MESSAGES.monitoring),
+        //     key: 'devices',
+        //     permissions: paths.devicesPath.permissions,
+        //     icon: props => <ImportantDevicesRoundedIcon {...props} />,
+        // },
     ];
     if (currentUser.is_staff || currentUser.is_superuser) {
         settingsSubMenu.push({

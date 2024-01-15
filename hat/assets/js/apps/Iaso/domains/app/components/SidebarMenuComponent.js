4d19ac74c6ec09f49e1a2ad230c515e8735ca0bb
Quang Son Le <qle@bluesquarehub.com>
2024-01-15 12:01:17 +0100

    IA-2609: update FR user manual link

--- hat/assets/js/apps/Iaso/domains/app/components/SidebarMenuComponent.js
+++ hat/assets/js/apps/Iaso/domains/app/components/SidebarMenuComponent.js
@@ -29,7 +29,7 @@ import MenuItem from './MenuItemComponent';
 import { Logo } from './Logo.tsx';
 import LanguageSwitch from './LanguageSwitchComponent';
 
-import { useMenuItems } from '../../../constants/menu.tsx';
+import { useMenuItems, RDC_USER_MANUAL } from '../../../constants/menu.tsx';
 
 import MESSAGES from './messages';
 
@@ -89,9 +89,10 @@ const styles = theme => ({
 
 // not removing the function (or the locale argument) because we will want to link to localized version of the doc website
 // eslint-disable-next-line no-unused-vars
+
 const localizedManualUrl = (locale, account) => {
     if (locale === 'fr' && account.name === 'RDC') {
-        return 'https://docs.google.com/document/d/1lKyhbKDLZpHtAsf3K6pRs0_EAXWdSDsL76Ohv0cyZQc/edit';
+        return RDC_USER_MANUAL;
     }
 
     return account.user_manual_path

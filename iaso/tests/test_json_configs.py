42d0e4e5c6db98e8825a2953677a7fc06b11d5c7
Quang Son Le <qle@bluesquarehub.com>
2024-01-25 13:01:00 +0100

    POLIO-1439: fix tests

--- iaso/tests/test_json_configs.py
+++ iaso/tests/test_json_configs.py
@@ -11,14 +11,14 @@ from iaso.models.json_config import Config
 content = {"beurk": "lol"}
 
 
-class PolioConfigTestCase(APITestCase):
+class ConfigTestCase(APITestCase):
     config: Config
     authorized_user: User
     unauthorized_user: User
     account: Account
 
     def setUp(cls) -> None:
-        cls.account = Account.objects.create(name="Polio account")
+        cls.account = Account.objects.create(name="account")
         cls.authorized_user = cls.create_user_with_profile(username="authorized", account=cls.account)
         cls.unauthorized_user = cls.create_user_with_profile(username="unAuthorized", account=cls.account)
         cls.config = Config.objects.create(slug="test", content=json.dumps(content))
@@ -26,29 +26,29 @@ class PolioConfigTestCase(APITestCase):
 
     def test_anonymous_user(self):
         c = APIClient()
-        response = c.get("/api/polio/configs/", accept="application/json")
+        response = c.get("/api/configs/", accept="application/json")
         self.assertEqual(response.status_code, 403)
-        response = c.get("/api/polio/configs/test/", accept="application/json")
+        response = c.get("/api/configs/test/", accept="application/json")
         self.assertEqual(response.status_code, 403)
 
     def test_unauthorized_user(self):
         c = APIClient()
         c.force_authenticate(self.unauthorized_user)
-        response = c.get("/api/polio/configs/", accept="application/json")
+        response = c.get("/api/configs/", accept="application/json")
         self.assertEqual(response.status_code, 200)
         json_response = json.loads(response.content)
         self.assertEqual(len(json_response["results"]), 0)
-        response = c.get("/api/polio/configs/test/", accept="application/json")
+        response = c.get("/api/configs/test/", accept="application/json")
         self.assertEqual(response.status_code, 404)
 
     def test_authorized_user(self):
         c = APIClient()
         c.force_authenticate(user=self.authorized_user)
-        response = c.get("/api/polio/configs/", accept="application/json")
+        response = c.get("/api/configs/", accept="application/json")
         self.assertEqual(response.status_code, 200)
         json_response = json.loads(response.content)
         self.assertEqual(json_response["results"][0]["key"], "test")
-        response = c.get("/api/polio/configs/test/", accept="application/json")
+        response = c.get("/api/configs/test/", accept="application/json")
         self.assertEqual(response.status_code, 200)
         json_response = json.loads(response.content)
         self.assertEqual(json_response["key"], "test")

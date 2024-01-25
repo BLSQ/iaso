d33a46e57d61c1465612b94263d0ef8a88e5b9d1
Quang Son Le <qle@bluesquarehub.com>
2024-01-25 16:44:13 +0100

    POLIO-1439: add doc in docstring

--- iaso/models/json_config.py
+++ iaso/models/json_config.py
@@ -3,6 +3,14 @@ from django.db import models
 
 
 class Config(models.Model):
+    """A json that can be used to store configs that we want to be able to change without having to deploy,
+    e.g: url to a 3rd party server.
+    
+    It's meant to be accessed mainly by iaso's backend but it can be exposed by an API. In that case, access not granted
+    via permissions but on a user basis, with the `users` field.
+    
+    The slug acts identifier and can be used to generate the API url
+    """
     slug = models.SlugField(unique=True)
     content = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)

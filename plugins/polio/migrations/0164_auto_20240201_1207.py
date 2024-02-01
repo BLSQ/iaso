474851070a740763e64f97b523257a1be237bf95
Math VDH <mathvdh@gmail.com>
2024-02-01 12:12:39 +0000

    POLIO-1448 Fix black format of migratio

--- plugins/polio/migrations/0164_auto_20240201_1207.py
+++ plugins/polio/migrations/0164_auto_20240201_1207.py
@@ -5,19 +5,20 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('polio', '0163_auto_20240123_1435'),
+        ("polio", "0163_auto_20240123_1435"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='destructionreport',
-            name='lot_number',
+            model_name="destructionreport",
+            name="lot_number",
         ),
         migrations.AddField(
-            model_name='destructionreport',
-            name='lot_numbers',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(blank=True, max_length=200), default=list, size=None),
+            model_name="destructionreport",
+            name="lot_numbers",
+            field=django.contrib.postgres.fields.ArrayField(
+                base_field=models.CharField(blank=True, max_length=200), default=list, size=None
+            ),
         ),
     ]

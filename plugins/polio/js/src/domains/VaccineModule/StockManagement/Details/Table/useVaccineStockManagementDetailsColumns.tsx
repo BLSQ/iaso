009922fea3fa30966950d64f59015e2c13eb5035
Quang Son Le <qle@bluesquarehub.com>
2024-02-01 17:10:55 +0100

    POLIO-1462: add text placeholder in empty table rows

--- plugins/polio/js/src/domains/VaccineModule/StockManagement/Details/Table/useVaccineStockManagementDetailsColumns.tsx
+++ plugins/polio/js/src/domains/VaccineModule/StockManagement/Details/Table/useVaccineStockManagementDetailsColumns.tsx
@@ -1,5 +1,6 @@
-import { useMemo } from 'react';
-import { Column, useSafeIntl } from 'bluesquare-components';
+/* eslint-disable camelcase */
+import React, { useMemo } from 'react';
+import { Column, textPlaceholder, useSafeIntl } from 'bluesquare-components';
 import { DateCell } from '../../../../../../../../../hat/assets/js/apps/Iaso/components/Cells/DateTimeCell';
 import MESSAGES from '../../messages';
 import { NumberCell } from '../../../../../../../../../hat/assets/js/apps/Iaso/components/Cells/NumberCell';
@@ -23,28 +24,51 @@ export const useVaccineStockManagementDetailsColumns = (): Column[] => {
             {
                 Header: formatMessage(MESSAGES.vials_in),
                 accessor: 'vials_in',
-                Cell: NumberCell,
+                Cell: settings => {
+                    const { vials_in } = settings.row.original;
+                    if (!vials_in) {
+                        return <span>{textPlaceholder}</span>;
+                    }
+                    return <NumberCell value={vials_in} />;
+                },
             },
             {
                 Header: formatMessage(MESSAGES.doses_in),
                 accessor: 'doses_in',
-                Cell: NumberCell,
+                Cell: settings => {
+                    const { doses_in } = settings.row.original;
+                    if (!doses_in) {
+                        return <span>{textPlaceholder}</span>;
+                    }
+
+                    return <NumberCell value={doses_in} />;
+                },
             },
             {
                 Header: formatMessage(MESSAGES.vials_out),
                 accessor: 'vials_out',
-                Cell: NumberCell,
+                Cell: settings => {
+                    const { vials_out } = settings.row.original;
+                    if (!vials_out) {
+                        return <span>{textPlaceholder}</span>;
+                    }
+
+                    return <NumberCell value={vials_out} />;
+                },
             },
             {
                 Header: formatMessage(MESSAGES.doses_out),
                 accessor: 'doses_out',
-                Cell: NumberCell,
+                Cell: settings => {
+                    const { doses_out } = settings.row.original;
+                    if (!doses_out) {
+                        return <span>{textPlaceholder}</span>;
+                    }
+
+                    return <NumberCell value={doses_out} />;
+                },
             },
         ];
         return columns;
-        // if (tab === USABLE_VIALS) {
-        //     return columns;
-        // }
-        // return columns.filter(col => !col.accessor.includes('doses'));
     }, [formatMessage]);
 };

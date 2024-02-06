e9512f83071a27cc5d30484961a9d9d0299b849b
Beygorghor <c@eduz.be>
2024-02-06 12:46:28 +0100

    relayout part 3

--- hat/assets/js/apps/Iaso/domains/assignments/configs/AssignmentsListTabColumns.tsx
+++ hat/assets/js/apps/Iaso/domains/assignments/configs/AssignmentsListTabColumns.tsx
@@ -1,8 +1,15 @@
-import React, { FunctionComponent, useMemo } from 'react';
+import React, {
+    FunctionComponent,
+    MouseEvent,
+    useCallback,
+    useMemo,
+} from 'react';
 import get from 'lodash/get';
 import { useSafeIntl, Column } from 'bluesquare-components';
 
-import { AssignmentsApi } from '../types/assigment';
+import { Checkbox, Box } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { AssignmentParams, AssignmentsApi } from '../types/assigment';
 import { AssignmentUnit } from '../types/locations';
 import { DropdownTeamsOptions, Team } from '../types/team';
 
@@ -11,17 +18,11 @@ import { Profile } from '../../../utils/usersUtils';
 
 import { UsersTeamsCell } from '../components/UsersTeamsCell';
 import { LinkToOrgUnit } from '../../orgUnits/components/LinkToOrgUnit';
+import { redirectTo } from '../../../routing/actions';
 
 import MESSAGES from '../messages';
 import { OrgUnit, ParentOrgUnit } from '../../orgUnits/types/orgUnit';
-
-type Props = {
-    orgUnits: Array<AssignmentUnit>;
-    assignments: AssignmentsApi;
-    teams: DropdownTeamsOptions[] | undefined;
-    profiles: Profile[] | undefined;
-    currentTeam?: Team;
-};
+import { baseUrls } from '../../../constants/urls';
 
 const getParentCount = (
     orgUnit: AssignmentUnit | ParentOrgUnit,
@@ -37,31 +38,81 @@ const getParentCount = (
 type ParentHeadCellProps = {
     settings: any;
     index: number;
+    params: AssignmentParams;
 };
+
+const baseUrl = baseUrls.assignments;
 
 const ParentHeadCell: FunctionComponent<ParentHeadCellProps> = ({
     settings,
     index,
+    params,
 }) => {
     const { formatMessage } = useSafeIntl();
+    const dispatch = useDispatch();
     const orgUnit = settings.data[index] as OrgUnit;
     const parent = get(
         orgUnit,
         `parent${'.parent'.repeat(index)}`,
     ) as ParentOrgUnit;
-    return parent
-        ? parent.org_unit_type_name
-        : formatMessage(MESSAGES.orgUnitsParent, {
-              index: index + 1,
-          });
+    const isActive = `${parent?.org_unit_type_id}` === params.parentOrgunitType;
+    const handleClick = useCallback(
+        (e: MouseEvent<HTMLElement>) => {
+            e.stopPropagation();
+            const newParams = {
+                ...params,
+            };
+            if (params.parentPicking === 'true') {
+                if (isActive) {
+                    newParams.parentPicking = 'false';
+                } else {
+                    newParams.parentOrgunitType = `${parent.org_unit_type_id}`;
+                }
+            } else {
+                newParams.parentPicking = 'true';
+                newParams.parentOrgunitType = `${parent.org_unit_type_id}`;
+            }
+            dispatch(redirectTo(baseUrl, newParams));
+        },
+        [dispatch, isActive, params, parent.org_unit_type_id],
+    );
+    return (
+        <>
+            {!parent &&
+                formatMessage(MESSAGES.orgUnitsParent, {
+                    index: index + 1,
+                })}
+            {parent && (
+                <Box>
+                    {parent.org_unit_type_name}
+                    <Checkbox
+                        sx={{ position: 'relative', top: -2 }}
+                        size="small"
+                        checked={isActive && params.parentPicking === 'true'}
+                        onClick={handleClick}
+                    />
+                </Box>
+            )}
+        </>
+    );
 };
 
+type Props = {
+    orgUnits: Array<AssignmentUnit>;
+    assignments: AssignmentsApi;
+    teams: DropdownTeamsOptions[] | undefined;
+    profiles: Profile[] | undefined;
+    currentTeam?: Team;
+
+    params: AssignmentParams;
+};
 export const useColumns = ({
     orgUnits,
     assignments,
     teams,
     profiles,
     currentTeam,
+    params,
 }: Props): Column[] => {
     const { formatMessage } = useSafeIntl();
     const firstOrgunit: AssignmentUnit = orgUnits[0];
@@ -84,7 +135,11 @@ export const useColumns = ({
                 columns.push({
                     // @ts-ignore
                     Header: settings => (
-                        <ParentHeadCell index={index} settings={settings} />
+                        <ParentHeadCell
+                            index={index}
+                            settings={settings}
+                            params={params}
+                        />
                     ),
                     id: `parent__${'parent__'.repeat(index)}name`,
                     accessor: `parent.${'parent.'.repeat(index)}name`,
@@ -127,6 +182,7 @@ export const useColumns = ({
         formatMessage,
         parentCount,
         currentTeam?.type,
+        params,
         assignments,
         teams,
         profiles,

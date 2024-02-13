c45426f555f990eb0a94f3f228edfb6cfd689ce7
Beygorghor <c@eduz.be>
2024-02-13 17:04:43 +0100

    adding permissions and payment creation script

--- iaso/models/payments.py
+++ iaso/models/payments.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
+
 class Payment(models.Model):
     """
     Model to store the status of payments linked to multiple OrgUnitChangeRequest by the same user.
@@ -13,9 +14,21 @@ class Payment(models.Model):
         REJECTED = "rejected", _("Rejected")
 
     status = models.CharField(choices=Statuses.choices, default=Statuses.PENDING, max_length=40)
-    change_requests = models.ManyToManyField('OrgUnitChangeRequest', related_name="payment_statuses")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payment_statuses")
-    created_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name="payment_created_set")
-    updated_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name="payment_updated_set")
+    change_requests = models.ManyToManyField("OrgUnitChangeRequest", related_name="payment")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="payment")
+    created_by = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.SET_NULL, related_name="payment_created_set"
+    )
+    updated_by = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.SET_NULL, related_name="payment_updated_set"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "%s - %s - %s - %s" % (
+            self.id,
+            self.status,
+            self.user,
+            self.updated_at.strftime("%Y-%m-%d %H:%M:%S"),
+        )

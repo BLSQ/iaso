39b7c090a088692cb0d8a0df81a41f2f7c6b7c88
Martin De Wulf <mdewulf@bluesquarehub.com>
2024-02-13 17:58:49 +0100

    Fix: when saving a datasource without url, an error 500 should not happen

--- iaso/dhis2/url_helper.py
+++ iaso/dhis2/url_helper.py
@@ -3,7 +3,7 @@ from typing import Union
 
 def clean_url(dhis2_url: Union[str, None]) -> Union[str, None]:
     if not dhis2_url:
-        return None
+        return ""
     cleaned_url = dhis2_url.replace("dhis-web-commons/security/login.action", "")
     cleaned_url = cleaned_url.strip("/")
     return cleaned_url

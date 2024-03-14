c5ef38223749dcea37c8b7d9f83722cb18162527
Fleury Butoyi <fleurybu@gmail.com>
2024-03-14 09:13:51 +0200

    Adding unit test for get_creator_name function

---
+++ iaso/tests/utils/test_common.py
@@ -0,0 +1,15 @@
+from iaso.test import TestCase
+from iaso.utils.models.common import get_creator_name
+
+
+class CommonTestCase(TestCase):
+
+    def test_get_creator_name(self):
+        user = {"username": "yoda", "first_name": "Yo", "last_name": "Da"}
+        creator = get_creator_name(
+            None,
+            user.get("username", ""),
+            user.get("first_name", ""),
+            user.get("last_name", ""),
+        )
+        self.assertEqual(creator, "yoda (Yo Da)")

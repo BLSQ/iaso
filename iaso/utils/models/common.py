ca1b0f4b0116025f29fb30651ce79a8361c4e933
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2024-03-14 09:17:28 +0200

    fix black formatting on iaso/utils/models/common.py

--- iaso/utils/models/common.py
+++ iaso/utils/models/common.py
@@ -3,7 +3,6 @@ from iaso.models.base import User
 
 def get_creator_name(creator: User = None, username: str = "", first_name: str = "", last_name: str = "") -> str:
     if isinstance(creator, User):
-
         username = creator.username
         first_name = creator.first_name
         last_name = creator.last_name

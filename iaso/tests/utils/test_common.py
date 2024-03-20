dd4ca79a1f2913613bdc804adac6a18161dd86fd
Fleury Butoyi <fleurybu@gmail.com>
2024-03-20 14:42:17 +0200

    Adding Unit test for parent source ref function

--- iaso/tests/utils/test_common.py
+++ iaso/tests/utils/test_common.py
@@ -1,5 +1,5 @@
 from iaso.test import TestCase
-from iaso.utils.models.common import get_creator_name
+from iaso.utils.models.common import get_creator_name, get_org_unit_parents_ref
 
 
 class CommonTestCase(TestCase):
@@ -12,3 +12,38 @@ class CommonTestCase(TestCase):
             user.get("last_name", ""),
         )
         self.assertEqual(creator, "yoda (Yo Da)")
+
+    def test_get_org_unit_parents_ref(self):
+        org_unit = {
+            "id": 9354,
+            "name": "Test 2",
+            "org_unit_type__name": "FosaPlay",
+            "version__data_source__name": "reference_play_test2.40.3",
+            "validation_status": "NEW",
+            "parent__name": "Parent",
+            "parent__parent__name": "Parent 1",
+            "parent__parent__parent__name": "Parent 2",
+            "parent__parent__source_ref": "fdc6uOvgoji",
+            "parent__parent__parent__source_ref": "ImspTQPwCqd",
+            "parent__id": 7513,
+            "parent__parent__id": 7417,
+            "parent__parent__parent__id": 6688,
+        }
+        parent_source_ref_field_names = [
+            "parent__source_ref",
+            "parent__parent__source_ref",
+            "parent__parent__parent__source_ref",
+            "parent__parent__parent__parent__source_ref",
+        ]
+
+        parent_field_ids = [
+            "parent__id",
+            "parent__parent__id",
+            "parent__parent__parent__id",
+            "parent__parent__parent__parent__id",
+        ]
+        parents_source_ref = [
+            get_org_unit_parents_ref(field_name, org_unit, parent_source_ref_field_names, parent_field_ids)
+            for field_name in parent_source_ref_field_names
+        ]
+        self.assertEqual(parents_source_ref, ["iaso#7513", "fdc6uOvgoji", "ImspTQPwCqd", None])

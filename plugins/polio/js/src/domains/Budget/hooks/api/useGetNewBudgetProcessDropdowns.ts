19f2e1ba36eb86220ec8d2f84048dfffc0a33fe1
kemar <marc@marcarea.com>
2024-03-20 15:20:16 +0100

    Trigger GET's `select` only once via `useCallback`

--- plugins/polio/js/src/domains/Budget/hooks/api/useGetNewBudgetProcessDropdowns.ts
+++ plugins/polio/js/src/domains/Budget/hooks/api/useGetNewBudgetProcessDropdowns.ts
@@ -1,29 +1,38 @@
 import { UseQueryResult } from 'react-query';
+import { useCallback } from 'react';
+
 import { getRequest } from '../../../../../../../../hat/assets/js/apps/Iaso/libs/Api';
 import { useSnackQuery } from '../../../../../../../../hat/assets/js/apps/Iaso/libs/apiHooks';
 
-import { DropdownOptions } from '../../types';
+import { DropdownOptions, OptionsRounds } from '../../types';
 import { formatRoundNumber } from '../../utils';
 
 export const useGetNewBudgetProcessDropdowns = (): UseQueryResult<
     DropdownOptions,
     Error
 > => {
+    const select = useCallback((data: DropdownOptions) => {
+        const formattedRounds = data.rounds.map((round: OptionsRounds) => {
+            return {
+                id: round.id,
+                name: formatRoundNumber(round.name),
+                campaign_id: round.campaign_id,
+            };
+        });
+        return {
+            countries: data.countries,
+            campaigns: data.campaigns,
+            rounds: formattedRounds,
+        };
+    }, []);
+
     return useSnackQuery({
         queryKey: ['new_budget_process_dropdowns'],
         queryFn: () =>
             getRequest('/api/polio/budget/new_budget_process_dropdowns/'),
         options: {
-            select: data => {
-                if (!data) return [];
-
-                console.log(data);
-
-                // data.rounds.forEach(
-                //     round => (round.name = formatRoundNumber(round.name)),
-                // );
-                return data;
-            },
+            keepPreviousData: true,
+            select,
         },
     });
 };

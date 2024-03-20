a540fc9c4cffc36435fd742f7398d7888439713e
kemar <marc@marcarea.com>
2024-03-20 15:58:07 +0100

    Add yup validation schema

--- plugins/polio/js/src/domains/Budget/messages.ts
+++ plugins/polio/js/src/domains/Budget/messages.ts
@@ -13,17 +13,29 @@ const MESSAGES = defineMessages({
         id: 'iaso.label.save',
         defaultMessage: 'Save',
     },
-    labelCountry: {
-        id: 'iaso.polio.budget.label.country',
-        defaultMessage: 'Country',
-    },
     labelCampaign: {
         id: 'iaso.polio.budget.label.campaign',
         defaultMessage: 'Campaign',
     },
+    labelCountry: {
+        id: 'iaso.polio.budget.label.country',
+        defaultMessage: 'Country',
+    },
     labelRound: {
         id: 'iaso.polio.budget.label.round',
         defaultMessage: 'Round',
+    },
+    requiredField: {
+        id: 'iaso.forms.error.fieldRequired',
+        defaultMessage: 'This field is required',
+    },
+    requiredPositiveInteger: {
+        id: 'iaso.forms.error.positiveInteger',
+        defaultMessage: 'Please use a positive integer',
+    },
+    requiredUuid: {
+        id: 'iaso.forms.error.requiredUuid',
+        defaultMessage: 'Please use an UUID',
     },
 });
 

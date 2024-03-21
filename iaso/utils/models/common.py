a8949a81a7a221b2206ce298d94266686aadac19
Fleury Butoyi <fleurybu@gmail.com>
2024-03-21 14:14:12 +0200

    Explaining the reason to prefix parent_ref with iaso#

--- iaso/utils/models/common.py
+++ iaso/utils/models/common.py
@@ -19,6 +19,7 @@ def get_org_unit_parents_ref(field_name, org_unit, parent_source_ref_field_names
     else:
         parent_index = parent_source_ref_field_names.index(field_name)
         parent_ref = org_unit.get(parent_field_ids[parent_index])
+        """if the external reference id is missing, prefix with iaso the internal id. e.g: 'iaso#1475'"""
         if parent_ref:
             return f"iaso#{parent_ref}"
         return None

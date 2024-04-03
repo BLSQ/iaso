d41399ed66a663a50ee50e75e06693c8e5a70421
Beygorghor <c@eduz.be>
2024-04-03 15:02:23 +0200

    code review

--- hat/assets/js/apps/Iaso/hooks/useSortedItems.ts
+++ hat/assets/js/apps/Iaso/hooks/useSortedItems.ts
@@ -3,40 +3,22 @@ import { get, orderBy } from 'lodash';
 import { useMemo } from 'react';
 
 type SortOrder = 'asc' | 'desc';
-type Sort = {
-    sortKey: string;
-    sortOrder: SortOrder;
-};
 
 export const useSortedItems = <T>(
     items: T[] | undefined,
     columns: Column[],
     order: string,
 ): T[] | undefined => {
-    const { sortKey, sortOrder } = useMemo<Sort>(() => {
-        const isDescending = order.startsWith('-');
-        const key = isDescending ? order.substring(1) : order;
-        return {
-            sortKey: key,
-            sortOrder: isDescending ? 'desc' : 'asc',
-        };
-    }, [order]);
+    const isDescending = order.startsWith('-');
+    const sortKey = isDescending ? order.substring(1) : order;
+    const sortOrder: SortOrder = isDescending ? 'desc' : 'asc';
+    const columnAccessor: string | undefined = columns?.find(
+        item => item.id === sortKey,
+    )?.accessor;
 
-    const columnAccessor: string | undefined = useMemo(() => {
-        return columns?.find(item => item.id === sortKey)?.accessor as
-            | string
-            | undefined;
-    }, [sortKey, columns]);
-
-    return useMemo(
-        () =>
-            columnAccessor
-                ? orderBy(
-                      items,
-                      [item => get(item, columnAccessor)],
-                      [sortOrder],
-                  )
-                : items,
-        [items, columnAccessor, sortOrder],
-    );
+    return useMemo(() => {
+        return columnAccessor
+            ? orderBy(items, [item => get(item, columnAccessor)], [sortOrder])
+            : items;
+    }, [items, columnAccessor, sortOrder]);
 };

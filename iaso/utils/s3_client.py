57d1a25f2a32282e1f966421f2f448a9ce332ae5
Bram Jans <ik@bramjans.com>
2024-04-10 16:14:12 +0200

    SLEEP-1337 Bulk upload v1

--- iaso/utils/s3_client.py
+++ iaso/utils/s3_client.py
@@ -39,7 +39,6 @@ def generate_presigned_url_from_s3(object_name, expires_in=3600):
 
 def download_file(object_name):
     file_name = object_name.split("/")[-1]
-    print("file_name ", file_name)
     destination_file_name = "/tmp/" + file_name
 
     boto3.client("s3", region_name=settings.AWS_S3_REGION_NAME).download_file(

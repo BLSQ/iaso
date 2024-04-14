a0428f0235aa85bf66e59fb08b450cace699ad5d
r.verma <wfp-ravindra@WFP-Ravindras-MacBook-Pro.local>
2024-04-14 20:40:50 +0530

    bug (245784) : Updated the Build Pipeline to include values for Health URL and testing

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # This script is used during deployement to fill the version info so we can see them from python
+
 cat > hat/__version__.py << EOF
 # Generated at deploy
-VERSION='$VERSION_NAME'
+VERSION='Dev'
 DEPLOYED_ON="$(date --rfc-3339=seconds)"
-DEPLOYED_BY="$GITHUB_ACTOR"
+DEPLOYED_BY="$(git log --format='%an' -n 1 "$BUILD_SOURCEVERSION")"
 EOF
+
+cat hat/__version__.py

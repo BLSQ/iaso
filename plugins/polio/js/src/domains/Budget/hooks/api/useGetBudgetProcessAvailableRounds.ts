4ea009d7a43790827dd02be9a3a98d92fd4ef451
kemar <marc@marcarea.com>
2024-04-15 10:46:20 +0200

    Remove `useCallback`

--- plugins/polio/js/src/domains/Budget/hooks/api/useGetBudgetProcessAvailableRounds.ts
+++ plugins/polio/js/src/domains/Budget/hooks/api/useGetBudgetProcessAvailableRounds.ts
@@ -1,5 +1,4 @@
 import { UseQueryResult } from 'react-query';
-import { useCallback } from 'react';
 
 import { getRequest } from '../../../../../../../../hat/assets/js/apps/Iaso/libs/Api';
 import { useSnackQuery } from '../../../../../../../../hat/assets/js/apps/Iaso/libs/apiHooks';
@@ -7,60 +6,60 @@ import { useSnackQuery } from '../../../../../../../../hat/assets/js/apps/Iaso/l
 import { DropdownOptions, OptionsRounds } from '../../types';
 import { formatRoundNumber } from '../../utils';
 
+const filterAvailableRoundsForCreate = (data: DropdownOptions) => {
+    const result: DropdownOptions = {
+        countries: [],
+        campaigns: [],
+        rounds: [],
+    };
+    if (data.rounds !== undefined) {
+        const formattedRounds = data.rounds.map((round: OptionsRounds) => {
+            return {
+                value: round.value,
+                label: formatRoundNumber(round.label),
+                campaign_id: round.campaign_id,
+            };
+        });
+        result.countries = data.countries;
+        result.campaigns = data.campaigns;
+        result.rounds = formattedRounds;
+    }
+    return result;
+};
+
 export const useAvailableRoundsForCreate = (): UseQueryResult<
     DropdownOptions,
     Error
 > => {
-    const select = useCallback((data: DropdownOptions) => {
-        const result: DropdownOptions = {
-            countries: [],
-            campaigns: [],
-            rounds: [],
-        };
-        if (data.rounds !== undefined) {
-            const formattedRounds = data.rounds.map((round: OptionsRounds) => {
-                return {
-                    value: round.value,
-                    label: formatRoundNumber(round.label),
-                    campaign_id: round.campaign_id,
-                };
-            });
-            result.countries = data.countries;
-            result.campaigns = data.campaigns;
-            result.rounds = formattedRounds;
-        }
-        return result;
-    }, []);
-
     return useSnackQuery({
         queryKey: ['new_budget_process_dropdowns'],
         queryFn: () =>
             getRequest('/api/polio/budget/available_rounds_for_create/'),
         options: {
             keepPreviousData: true,
-            select,
+            select: data => filterAvailableRoundsForCreate(data),
         },
     });
+};
+
+const filterAvailableRoundsForUpdate = (data: OptionsRounds[]) => {
+    let result: OptionsRounds[] = [];
+    if (Array.isArray(data)) {
+        result = data.map((round: OptionsRounds) => {
+            return {
+                value: round.value,
+                label: formatRoundNumber(round.label),
+                campaign_id: round.campaign_id,
+            };
+        });
+    }
+    return result;
 };
 
 export const useAvailableRoundsForUpdate = (
     campaignID: string,
     budgetProcessId: number,
 ): UseQueryResult<OptionsRounds[], Error> => {
-    const select = useCallback((data: OptionsRounds[]) => {
-        let result: OptionsRounds[] = [];
-        if (Array.isArray(data)) {
-            result = data.map((round: OptionsRounds) => {
-                return {
-                    value: round.value,
-                    label: formatRoundNumber(round.label),
-                    campaign_id: round.campaign_id,
-                };
-            });
-        }
-        return result;
-    }, []);
-
     return useSnackQuery({
         queryKey: ['new_budget_process_dropdowns'],
         queryFn: () =>
@@ -69,7 +68,7 @@ export const useAvailableRoundsForUpdate = (
             ),
         options: {
             keepPreviousData: true,
-            select,
+            select: data => filterAvailableRoundsForUpdate(data),
         },
     });
 };

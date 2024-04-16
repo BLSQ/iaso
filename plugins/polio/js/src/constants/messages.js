1aa325d94139427dea2451a4d1afd8d23d6737bb
Martin De Wulf <mdewulf@bluesquarehub.com>
2024-04-16 15:01:04 +0200

    [POLIOPM-51] Splitting LQAS results in 4 categories

--- plugins/polio/js/src/constants/messages.js
+++ plugins/polio/js/src/constants/messages.js
@@ -826,6 +826,14 @@ const MESSAGES = defineMessages({
         id: 'iaso.polio.label.passed',
         defaultMessage: 'Passed',
     },
+    poor: {
+        id: 'iaso.polio.label.poor',
+        defaultMessage: 'Poor',
+    },
+    moderate: {
+        id: 'iaso.polio.label.moderate',
+        defaultMessage: 'Moderate',
+    },
     countryPassing: {
         id: 'iaso.polio.label.countryPassing',
         defaultMessage: '80% or more districts passed',
@@ -852,7 +860,15 @@ const MESSAGES = defineMessages({
     },
     '3lqasFail': {
         id: 'iaso.polio.label.failed',
-        defaultMessage: 'Failed',
+        defaultMessage: 'Very poor',
+    },
+    '3lqasmoderate': {
+        id: 'iaso.polio.label.moderate',
+        defaultMessage: 'Moderate',
+    },
+    '3lqaspoor': {
+        id: 'iaso.polio.label.poor',
+        defaultMessage: 'Poor',
     },
     inScope: {
         id: 'iaso.polio.label.inScope',

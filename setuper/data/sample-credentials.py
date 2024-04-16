4df756d364b8c269979ee6d2253b2e40a139e4da
LaurePontis <lpontis@bluesquarehub.com>
2024-04-16 17:52:15 +0200

    User Guide update

--- setuper/data/sample-credentials.py
+++ setuper/data/sample-credentials.py
@@ -1,4 +1,4 @@
-SERVER = "http://localhost:8081"
+SERVER = "https://iaso-staging.bluesquare.org/"
 API_URL = f"{SERVER}/api/"  # with a trailing slash !
-ADMIN_USER_NAME = "testemail2.39.3.1"
-ADMIN_PASSWORD = "testemail2.39.3.1"
+ADMIN_USER_NAME = "laure-blsq-microplanning"
+ADMIN_PASSWORD = "laure-blsq-microplanning"

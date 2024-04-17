66dba57de11ce1f53665c42e077f710c8540c25b
r.verma <wfp-ravindra@WFP-Ravindras-MacBook-Pro.local>
2024-04-17 16:42:14 +0530

    bug (245784) : Updated the shell to have version no as tag and branch

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script is used during deployement to fill the version info so we can see them from python
-
+BRANCH_NAME=$(git symbolic-ref --short HEAD)
 cat > hat/__version__.py << EOF
 # Generated at deploy
-VERSION='Dev'
+VERSION=$(git describe --tags)--$BRANCH_NAME
 DEPLOYED_ON="$(date --rfc-3339=seconds)"
 DEPLOYED_BY="$(git log --format='%an' -n 1 "$BUILD_SOURCEVERSION")"
 EOF

25dec090277f62fc01e1ea3b2a251a7a5e38cb44
Quang Son Le <qle@bluesquarehub.com>
2024-04-29 17:05:42 +0200

    IA-2937: fix tables and filters for Change requests

--- hat/assets/js/apps/Iaso/domains/orgUnits/reviewChanges/ReviewOrgUnitChanges.tsx
+++ hat/assets/js/apps/Iaso/domains/orgUnits/reviewChanges/ReviewOrgUnitChanges.tsx
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { commonStyles, getTableUrl, useSafeIntl } from 'bluesquare-components';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo } from 'react';
 import DownloadButtonsComponent from '../../../components/DownloadButtonsComponent';
 import TopBar from '../../../components/nav/TopBarComponent';
 import { ReviewOrgUnitChangesFilter } from './Filter/ReviewOrgUnitChangesFilter';
@@ -67,18 +67,32 @@ export const ReviewOrgUnitChanges: FunctionComponent = () => {
     const classes: Record<string, string> = useStyles();
     const { formatMessage } = useSafeIntl();
     const endPointUrl = 'orgunits/changes/export_to_csv';
-    const csv_params = {
-        parent_id: params.parent_id,
-        groups: params.groups,
-        org_unit_type_id: params.org_unit_type_id,
-        status: params.status,
-        created_at_after: params.created_at_after,
-        created_at_before: params.created_at_before,
-        forms: params.forms,
-        users: params.userIds,
-        user_roles: params.userRoles,
-        with_location: params.withLocation,
-    };
+    const csv_params = useMemo(
+        () => ({
+            parent_id: params.parent_id,
+            groups: params.groups,
+            org_unit_type_id: params.org_unit_type_id,
+            status: params.status,
+            created_at_after: params.created_at_after,
+            created_at_before: params.created_at_before,
+            forms: params.forms,
+            users: params.userIds,
+            user_roles: params.userRoles,
+            with_location: params.withLocation,
+        }),
+        [
+            params.created_at_after,
+            params.created_at_before,
+            params.forms,
+            params.groups,
+            params.org_unit_type_id,
+            params.parent_id,
+            params.status,
+            params.userIds,
+            params.userRoles,
+            params.withLocation,
+        ],
+    );
 
     const csv_url = getTableUrl(endPointUrl, csv_params);
 

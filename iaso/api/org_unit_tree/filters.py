9d4015cae16eb315dc047718d13d0ceacd4f4e69
kemar <marc@marcarea.com>
2024-04-30 17:52:47 +0200

    Improve query

--- iaso/api/org_unit_tree/filters.py
+++ iaso/api/org_unit_tree/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 from django.db.models.query import QuerySet
+from django.shortcuts import get_object_or_404
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework.exceptions import ValidationError
@@ -11,7 +12,6 @@ class OrgUnitTreeFilter(django_filters.rest_framework.FilterSet):
     default_version = django_filters.BooleanFilter(method="filter_default_version", label=_("Default version"))
     ignore_empty_names = django_filters.BooleanFilter(method="filter_empty_names", label=_("Ignore empty names"))
     parent_id = django_filters.NumberFilter(method="filter_parent_id", label=_("Parent ID"))
-    roots_for_user = django_filters.BooleanFilter(method="filter_roots_for_user", label=_("Roots for user"))
     source_id = django_filters.NumberFilter(method="filter_source_id", label=_("Source ID"))
     validation_status = django_filters.ChoiceFilter(
         method="filter_validation_status", label=_("Validation status"), choices=OrgUnit.VALIDATION_STATUS_CHOICES
@@ -21,30 +21,26 @@ class OrgUnitTreeFilter(django_filters.rest_framework.FilterSet):
         model = OrgUnit
         fields = ["version"]
 
+    @property
+    def qs(self):
+        parent_qs = super().qs
+        # If `parent_id` is not provided, return the tree's first level.
+        parent_id = self.form.cleaned_data["parent_id"]
+        if not parent_id:
+            return parent_qs.filter(parent__isnull=True)
+        return parent_qs
+
     def filter_default_version(self, queryset: QuerySet, _, use_default_version: bool) -> QuerySet:
         user = self.request.user
-        if user.is_anonymous or not use_default_version:
+        if not use_default_version or user.is_anonymous:
             return queryset
         return queryset.filter(version=user.iaso_profile.account.default_version)
 
     def filter_empty_names(self, queryset: QuerySet, _, use_empty_names: bool) -> QuerySet:
         return queryset.exclude(name="") if use_empty_names else queryset
 
-    def filter_parent_id(self, queryset: QuerySet, _, parent_id: int) -> QuerySet:
-        try:
-            parent = OrgUnit.objects.get(id=parent_id)
-        except OrgUnit.DoesNotExist:
-            raise ValidationError({"parent_id": [f"OrgUnit with id {parent_id} does not exist."]})
-        return queryset.filter(parent=parent.pk)
-
-    def filter_roots_for_user(self, queryset: QuerySet, _, use_roots_for_user: bool) -> QuerySet:
-        user = self.request.user
-        if user.is_anonymous or not use_roots_for_user:
-            return queryset
-        org_unit_ids_for_profile = user.iaso_profile.org_units.only("id")
-        if org_unit_ids_for_profile and not user.is_superuser:
-            return queryset.filter(id__in=org_unit_ids_for_profile)
-        return queryset.filter(parent__isnull=True)
+    def filter_parent_id(self, queryset: QuerySet, _, parent_id: str) -> QuerySet:
+        return queryset.filter(parent=parent_id)
 
     def filter_source_id(self, queryset: QuerySet, _, source_id: int) -> QuerySet:
         try:
@@ -56,22 +52,44 @@ class OrgUnitTreeFilter(django_filters.rest_framework.FilterSet):
         return queryset.filter(version__data_source_id=source_id)
 
     def filter_validation_status(self, queryset: QuerySet, _, validation_status: str) -> QuerySet:
-        if validation_status == OrgUnit.VALIDATION_VALID:
-            # Fetch children of `REJECTED` org units which are not themselves `REJECTED`.
-            children_of_rejected_org_units_sql = """
-WITH RECURSIVE rejected AS (
-    SELECT id, validation_status
-    FROM iaso_orgunit
-    WHERE validation_status = 'REJECTED'
-    UNION
-    SELECT descendant.id, descendant.validation_status
-    FROM iaso_orgunit as descendant, rejected
-    WHERE descendant.parent_id = rejected.id
-)
-SELECT 1 as id, array_agg(id) as ids
-FROM rejected
-WHERE validation_status != 'REJECTED';"""
-            ids_to_exclude = OrgUnit.objects.raw(children_of_rejected_org_units_sql)[0].ids
-            queryset = queryset.exclude(id__in=ids_to_exclude)
+        """
+        If we filter on VALID or NEW, the children of a REJECTED org unit should not be returned.
+        """
+        parent_id = self.form.cleaned_data["parent_id"]
+        if parent_id and validation_status in [OrgUnit.VALIDATION_NEW, OrgUnit.VALIDATION_VALID]:
+            try:
+                # Get parent's top ancestor.
+                top_ancestor = OrgUnit.objects.raw(
+                    (
+                        "SELECT id, path FROM iaso_orgunit "
+                        "WHERE path = SUBPATH((SELECT path FROM iaso_orgunit WHERE id = %s), 0, 1)"
+                    ),
+                    [parent_id],
+                )[0]
+            except IndexError:
+                raise ValidationError({"parent_id": [f"OrgUnit with id {parent_id} does not exist."]})
+
+            # Find the children of `REJECTED` org units which are not `REJECTED` themselves.
+            rejected_children = OrgUnit.objects.raw(
+                """
+                WITH RECURSIVE rejected AS (
+                    SELECT id, validation_status
+                    FROM iaso_orgunit
+                    WHERE path <@ %s  -- Limit to all descendants of the top_ancestor node (including self).
+                    AND validation_status = 'REJECTED'
+                    UNION
+                    SELECT descendant.id, descendant.validation_status
+                    FROM iaso_orgunit as descendant, rejected
+                    WHERE descendant.parent_id = rejected.id
+                )
+                SELECT 1 as id, array_agg(id) as ids
+                FROM rejected
+                WHERE validation_status != 'REJECTED';
+                """,
+                [str(top_ancestor.path)],
+            )[0]
+
+            if rejected_children.ids:
+                queryset = queryset.exclude(id__in=rejected_children.ids)
 
         return queryset.filter(validation_status=validation_status)
--- iaso/api/org_unit_tree/views.py
+++ iaso/api/org_unit_tree/views.py
@@ -1,10 +1,9 @@
 import django_filters
 from django.db.models import Count
 
-from rest_framework import filters, viewsets
+from rest_framework import filters, permissions, viewsets
 
 from iaso.api.org_unit_tree.filters import OrgUnitTreeFilter
-from iaso.api.org_unit_tree.pagination import OrgUnitTreePagination
 from iaso.api.org_unit_tree.serializers import OrgUnitTreeSerializer
 from iaso.models import OrgUnit
 
@@ -14,11 +13,14 @@ class OrgUnitTreeViewSet(viewsets.ModelViewSet):
     filterset_class = OrgUnitTreeFilter
     http_method_names = ["get", "options", "head", "trace"]
     ordering_fields = ["id", "name"]
-    pagination_class = OrgUnitTreePagination
+    permission_classes = [permissions.AllowAny]
     serializer_class = OrgUnitTreeSerializer
 
     def get_queryset(self):
-        qs = OrgUnit.objects.filter_for_user(self.request.user)
+        if self.request.user.is_anonymous:
+            qs = OrgUnit.objects.all()
+        else:
+            qs = OrgUnit.objects.filter_for_user(self.request.user)
         qs = qs.only("id", "name", "validation_status", "version", "org_unit_type", "parent")
         qs = qs.annotate(children_count=Count("orgunit__id"))
         return qs

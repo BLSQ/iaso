68c8e47a192f76d61fa8d8f065bac6a4b507ecb5
Quang Son Le <qle@bluesquarehub.com>
2024-05-03 15:43:31 +0200

    IA-2955 convert OrgUnitDisplay, OrgUnitSourceRefDisplay to TS
    - also use LinkWithLocation

---
+++ hat/assets/js/apps/Iaso/domains/orgUnits/components/OrgUnitDisplay.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react';
+import { makeStyles } from '@mui/styles';
+import { textPlaceholder } from 'bluesquare-components';
+import { OrgUnitLabel } from '../utils';
+import { LinkWithLocation } from '../../../components/nav/LinkWithLocation';
+import { baseUrls } from '../../../constants/urls';
+
+const useStyles = makeStyles(() => ({
+    link: {
+        cursor: 'pointer',
+    },
+}));
+
+type Props = {
+    orgUnit?: Record<string, any>;
+    withType?: boolean;
+};
+
+// TODO refactor with strings as children of span> or <p>
+const OrgUnitDisplay = ({
+    orgUnit,
+    withType = true,
+}: Props): string | ReactElement => {
+    const classes = useStyles();
+    if (!orgUnit) {
+        return textPlaceholder;
+    }
+    return (
+        <LinkWithLocation
+            target="_blank"
+            className={classes.link}
+            to={`/${baseUrls.orgUnitDetails}/orgUnitId/${orgUnit.id}`}
+        >
+            {OrgUnitLabel({ orgUnit, withType })}
+        </LinkWithLocation>
+    );
+};
+
+export default OrgUnitDisplay;

5b3d4dbec8ee73587d51decafa4f10b5c5fcfd49
Fleury Butoyi <fleurybu@gmail.com>
2024-05-08 15:54:23 +0200

    Adding default account feature flags as constant

--- hat/constants.py
+++ hat/constants.py
@@ -1,1 +1,7 @@
 DATE_FORMAT = "%Y-%m-%d"
+
+ACCOUNT_FEATURE_FLAGS = [
+    {"code": "SHOW_HOME_ONLINE", "name": "Display plugin online home page"},
+    {"code": "SHOW_BENEFICIARY_TYPES_IN_LIST_MENU", "name": "Display beneficiaries by types in side menu"},
+    {"code": "SHOW_PAGES", "name": "Show page menu"},
+]

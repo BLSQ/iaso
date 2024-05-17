905cfcaeef47b2dbee1d9840be7880036f2eeae3
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2024-05-17 15:26:20 +0200

    fix conflict in hat/menupermissions/constants.py

--- hat/menupermissions/constants.py
+++ hat/menupermissions/constants.py
@@ -61,3 +61,69 @@ MODULES = [
     {"name": "Registry", "codename": "REGISTRY"},
     {"name": "Payments", "codename": "PAYMENTS"},
 ]
+
+FEATUREFLAGES_TO_EXCLUDE = {
+    "PLANNING": ["PLANNING"],
+    "ENTITIES": [
+        "REPORTS",
+        "ENTITY",
+        "MOBILE_ENTITY_WARN_WHEN_FOUND",
+        "MOBILE_ENTITY_LIMITED_SEARCH",
+        "MOBILE_ENTITY_NO_CREATION",
+        "WRITE_ON_NFC_CARDS",
+    ],
+}
+
+PERMISSIONS_PRESENTATION = {
+    "forms": [
+        "iaso_forms",
+        "iaso_update_submission",
+        "iaso_submissions",
+        "iaso_completeness",
+        "iaso_completeness_stats",
+    ],
+    "org_units": [
+        "iaso_org_units",
+        "iaso_org_unit_types",
+        "iaso_org_unit_groups",
+        "iaso_sources",
+        "iaso_write_sources",
+        "iaso_links",
+        "iaso_registry",
+        "iaso_org_unit_change_request_review",
+    ],
+    "entities": [
+        "iaso_entities",
+        "iaso_workflows",
+        "iaso_entity_duplicates_write",
+        "iaso_entity_duplicates_read",
+        "iaso_entity_type_write",
+    ],
+    "payments": ["iaso_payments"],
+    "dhis2_mapping": ["iaso_mappings"],
+    "external_storage": ["iaso_storages"],
+    "planning": ["iaso_assignments", "iaso_planning"],
+    "embedded_links": ["iaso_pages", "iaso_page_write"],
+    "polio": [
+        "iaso_polio_config",
+        "iaso_polio",
+        "iaso_polio_budget_admin",
+        "iaso_polio_budget",
+        "iaso_polio_vaccine_supply_chain_read",
+        "iaso_polio_vaccine_supply_chain_write",
+        "iaso_polio_vaccine_stock_management_read",
+        "iaso_polio_vaccine_stock_management_write",
+        "iaso_polio_notifications",
+        "iaso_polio_vaccine_authorizations_read_only",
+        "iaso_polio_vaccine_authorizations_admin",
+    ],
+    "admin": [
+        "iaso_data_tasks",
+        "iaso_reports",
+        "iaso_projects",
+        "iaso_users",
+        "iaso_user_roles",
+        "iaso_teams",
+        "iaso_modules",
+    ],
+}

35655f2b266f4ec702eb59f27f55d07bcba3581f
Quang Son Le <qle@bluesquarehub.com>
2024-05-22 10:36:50 +0200

    IA-2966: add js doc for LinkTo, useParamsObject

--- hat/assets/js/apps/Iaso/routing/hooks/useParamsObject.tsx
+++ hat/assets/js/apps/Iaso/routing/hooks/useParamsObject.tsx
@@ -1,6 +1,11 @@
 import { useParamsObject as useLibraryParams } from 'bluesquare-components';
 import { useParamsConfig } from '../routing';
 
+/**
+ * @description a wrapper to the hook from bluesquare-components. It passes the configs from iaso to the library hook, so it can be used in iaso by only passing the url
+ * @param baseUrl the url of which the params should be retrieved
+ * @returns a dictionary with all the params values as strings, so `array` and `object` params may require additional parsing
+ */
 export const useParamsObject = (
     baseUrl: string,
 ): Record<string, string | Record<string, unknown> | undefined> => {

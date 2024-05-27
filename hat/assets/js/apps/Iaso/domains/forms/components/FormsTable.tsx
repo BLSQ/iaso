716e266073a4efece040d6f6c1a3699db2d7b1db
Quang Son Le <qle@bluesquarehub.com>
2024-05-27 17:12:45 +0200

    IA-2976: update FormsTable

--- hat/assets/js/apps/Iaso/domains/forms/components/FormsTable.tsx
+++ hat/assets/js/apps/Iaso/domains/forms/components/FormsTable.tsx
@@ -1,53 +1,29 @@
-import React, { FunctionComponent, useMemo } from 'react';
+import React, { FunctionComponent } from 'react';
 import { Column } from 'bluesquare-components';
 import { useFormsTableColumns } from '../config';
 import { TableWithDeepLink } from '../../../components/tables/TableWithDeepLink';
 import { useGetForms } from '../hooks/useGetForms';
+import { usePrefixedParams } from '../../../routing/hooks/usePrefixedParams';
 
 type Props = {
     baseUrl: string;
     params: Record<string, any>;
-    orgUnitId?: string; // number as string
     defaultPageSize?: number;
     paramsPrefix?: string;
-};
-
-const decapitalize = (word: string) => {
-    const split = word.split('');
-    if (split.length === 0) {
-        return word;
-    }
-    const [first, ...rest] = split;
-    return [first.toLocaleLowerCase(), ...rest].join('');
 };
 
 export const FormsTable: FunctionComponent<Props> = ({
     baseUrl,
     params,
-    orgUnitId,
     defaultPageSize = 50,
     paramsPrefix,
 }) => {
     const columns = useFormsTableColumns({
         showDeleted: params?.showDeleted === 'true',
-        orgUnitId,
+        orgUnitId: params?.orgUnitId,
     }) as Column[];
-    const apiParams = useMemo(() => {
-        if (!paramsPrefix) {
-            return params;
-        }
-        const newParams = { ...params };
-        Object.keys(params)
-            .filter(paramKey => paramKey.includes(paramsPrefix))
-            .forEach(prefixedKey => {
-                // eslint-disable-next-line no-unused-vars
-                const [_, upperCaseKey] = prefixedKey.split(paramsPrefix);
-                const formattedKey = decapitalize(upperCaseKey);
-                newParams[formattedKey] = newParams[prefixedKey];
-                delete newParams[prefixedKey];
-            });
-        return newParams;
-    }, [params, paramsPrefix]);
+
+    const apiParams = usePrefixedParams(paramsPrefix, params);
 
     const { data: forms, isLoading: isLoadingForms } = useGetForms(apiParams);
     return (

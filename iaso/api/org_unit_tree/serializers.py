05ac9a5aa142fb159a25f4c133d1148a1ab5863a
Beygorghor <c@eduz.be>
2024-05-27 16:32:05 +0200

    adding parent to serializer

--- iaso/api/org_unit_tree/serializers.py
+++ iaso/api/org_unit_tree/serializers.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+from django.db.models import Count
 from rest_framework import serializers
 
 from iaso.api.common import DynamicFieldsModelSerializer
@@ -9,6 +10,7 @@ from iaso.models import OrgUnit
 class OrgUnitTreeSerializer(DynamicFieldsModelSerializer, serializers.ModelSerializer):
     has_children = serializers.SerializerMethodField()
     org_unit_type_short_name = serializers.SerializerMethodField()
+    parent = serializers.SerializerMethodField()
 
     @classmethod
     def get_has_children(cls, org_unit: OrgUnit) -> bool:
@@ -16,6 +18,14 @@ class OrgUnitTreeSerializer(DynamicFieldsModelSerializer, serializers.ModelSeria
 
     def get_org_unit_type_short_name(self, org_unit: OrgUnit) -> Union[str, None]:
         return org_unit.org_unit_type.short_name if org_unit.org_unit_type else None
+
+    def get_parent(self, org_unit: OrgUnit):
+        if org_unit.parent:
+            parent_with_annotation = (
+                OrgUnit.objects.filter(pk=org_unit.parent.pk).annotate(children_count=Count("orgunit")).first()
+            )
+            return OrgUnitTreeSerializer(parent_with_annotation, context=self.context).data
+        return None
 
     class Meta:
         model = OrgUnit
@@ -26,6 +36,7 @@ class OrgUnitTreeSerializer(DynamicFieldsModelSerializer, serializers.ModelSeria
             "has_children",
             "org_unit_type_id",
             "org_unit_type_short_name",
+            "parent",
         ]
         default_fields = [
             "id",
@@ -34,4 +45,5 @@ class OrgUnitTreeSerializer(DynamicFieldsModelSerializer, serializers.ModelSeria
             "has_children",
             "org_unit_type_id",
             "org_unit_type_short_name",
+            "parent",
         ]

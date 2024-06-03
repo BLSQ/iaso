49baff792aa974cd50611e37add926b0d10640f0
Quang Son Le <qle@bluesquarehub.com>
2024-06-03 11:47:34 +0200

    IA-2976: remove SingleTable from Devices

--- hat/assets/js/apps/Iaso/domains/devices/config.js
+++ hat/assets/js/apps/Iaso/domains/devices/config.js
@@ -1,46 +1,52 @@
+import { useMemo } from 'react';
+import { useSafeIntl } from 'bluesquare-components';
 import MESSAGES from './messages';
-import { DateTimeCell } from '../../components/Cells/DateTimeCell';
+import { DateTimeCell } from '../../components/Cells/DateTimeCell.tsx';
 import { YesNoCell } from '../../components/Cells/YesNoCell';
 
-const devicesTableColumns = formatMessage => [
-    {
-        Header: formatMessage(MESSAGES.imei),
-        sortable: false,
-        accessor: 'imei',
-    },
-    {
-        Header: formatMessage(MESSAGES.test_device),
-        sortable: false,
-        accessor: 'test_device',
-        Cell: YesNoCell,
-    },
-    {
-        Header: formatMessage(MESSAGES.last_owner),
-        sortable: false,
-        accessor: 'last_owner',
-        Cell: settings =>
-            settings.value
-                ? `${settings.value.first_name} ${settings.value.last_name} ${settings.value.user_name}`
-                : null,
-    },
-    {
-        Header: formatMessage(MESSAGES.timeSynched),
-        sortable: false,
-        accessor: 'synched_at',
-        Cell: DateTimeCell,
-    },
-    {
-        Header: formatMessage(MESSAGES.timeCreated),
-        sortable: false,
-        accessor: 'created_at',
-        Cell: DateTimeCell,
-    },
-    {
-        Header: formatMessage(MESSAGES.timeUpdated),
-        sortable: false,
-        accessor: 'updated_at',
-        Cell: DateTimeCell,
-    },
-];
-
-export default devicesTableColumns;
+export const useDevicesTableColumns = () => {
+    const { formatMessage } = useSafeIntl();
+    return useMemo(
+        () => [
+            {
+                Header: formatMessage(MESSAGES.imei),
+                sortable: false,
+                accessor: 'imei',
+            },
+            {
+                Header: formatMessage(MESSAGES.test_device),
+                sortable: false,
+                accessor: 'test_device',
+                Cell: YesNoCell,
+            },
+            {
+                Header: formatMessage(MESSAGES.last_owner),
+                sortable: false,
+                accessor: 'last_owner',
+                Cell: settings =>
+                    settings.value
+                        ? `${settings.value.first_name} ${settings.value.last_name} ${settings.value.user_name}`
+                        : null,
+            },
+            {
+                Header: formatMessage(MESSAGES.timeSynched),
+                sortable: false,
+                accessor: 'synched_at',
+                Cell: DateTimeCell,
+            },
+            {
+                Header: formatMessage(MESSAGES.timeCreated),
+                sortable: false,
+                accessor: 'created_at',
+                Cell: DateTimeCell,
+            },
+            {
+                Header: formatMessage(MESSAGES.timeUpdated),
+                sortable: false,
+                accessor: 'updated_at',
+                Cell: DateTimeCell,
+            },
+        ],
+        [formatMessage],
+    );
+};
--- hat/assets/js/apps/Iaso/domains/devices/index.js
+++ hat/assets/js/apps/Iaso/domains/devices/index.js
@@ -1,35 +1,51 @@
 import React from 'react';
-
-import { useSafeIntl } from 'bluesquare-components';
-import SingleTable from '../../components/tables/SingleTable';
+import { useSafeIntl, commonStyles } from 'bluesquare-components';
+import { Box } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import TopBar from '../../components/nav/TopBarComponent';
-
-import { baseUrls } from '../../constants/urls';
-
-import devicesTableColumns from './config';
+import { baseUrls } from '../../constants/urls.ts';
+import { useDevicesTableColumns } from './config';
 import MESSAGES from './messages';
-import { fetchDevicesAsDict } from '../../utils/requests';
+import { TableWithDeepLink } from '../../components/tables/TableWithDeepLink.tsx';
+import { tableDefaults, useGetDevices } from './hooks/api/useGetDevices.tsx';
+import { useParamsObject } from '../../routing/hooks/useParamsObject.tsx';
 
 const baseUrl = baseUrls.devices;
 
+const useStyles = makeStyles(theme => {
+    return {
+        containerFullHeightNoTabPadded:
+            commonStyles(theme).containerFullHeightNoTabPadded,
+    };
+});
+
 const Devices = () => {
-    const intl = useSafeIntl();
+    const params = useParamsObject(baseUrl);
+    const classes = useStyles();
+    const { formatMessage } = useSafeIntl();
+    const columns = useDevicesTableColumns();
+    const { data, isFetching: loading } = useGetDevices(params);
 
     return (
         <>
             <TopBar
-                title={intl.formatMessage(MESSAGES.devices)}
+                title={formatMessage(MESSAGES.devices)}
                 displayBackButton={false}
             />
-            <SingleTable
-                baseUrl={baseUrl}
-                endPointPath="devices"
-                dataKey="devices"
-                fetchItems={fetchDevicesAsDict}
-                columns={devicesTableColumns(intl.formatMessage)}
-                hideGpkg
-                exportButtons={false}
-            />
+            <Box className={classes.containerFullHeightNoTabPadded}>
+                <TableWithDeepLink
+                    baseUrl={baseUrl}
+                    columns={columns}
+                    data={data?.devices ?? []}
+                    defaultSorted={[{ id: 'name', desc: false }]}
+                    count={data?.count ?? 0}
+                    pages={data?.pages ?? 0}
+                    extraProps={{
+                        defaultPageSize: data?.limit ?? tableDefaults.limit,
+                        loading,
+                    }}
+                />
+            </Box>
         </>
     );
 };

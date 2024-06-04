828ab0b8ce1258813de120be57aaf40bf90a3696
Fleury Butoyi <fleurybu@gmail.com>
2024-06-04 08:00:10 +0200

    Fix test for update org unit sub type

--- iaso/tests/gpkg/test_import_with_sub_org_unit_type.py
+++ iaso/tests/gpkg/test_import_with_sub_org_unit_type.py
@@ -1,6 +1,7 @@
 from iaso.gpkg.import_gpkg import import_gpkg_file
 from iaso.models import Account, Project
 from iaso.test import APITestCase
+from setuper.pyramid import update_org_unit_sub_type
 
 
 class OrgUnitImportFromGPKG(APITestCase):
@@ -21,25 +22,10 @@ class OrgUnitImportFromGPKG(APITestCase):
         )
         self.client.force_authenticate(self.user_test)
         response = self.client.get("/api/v2/orgunittypes/")
-        org_unit_types = response.json()["orgUnitTypes"]
+        self.assertJSONResponse(response, 200)
+        response_data = response.json()
 
-        for org_unit_type in org_unit_types:
-            current_level = org_unit_type["depth"]
-            current_id = org_unit_type["id"]
-            sub_unit_type_ids = [
-                org_unit_type["id"] for org_unit_type in org_unit_types if org_unit_type["depth"] == (current_level + 1)
-            ]
+        updated_with_sub_types = update_org_unit_sub_type(self.client, self.project.id, response_data["orgUnitTypes"])
 
-            if len(sub_unit_type_ids) > 0:
-                current_type = {
-                    "name": org_unit_type["name"],
-                    "short_name": org_unit_type["short_name"],
-                    "project_ids": [self.project.id],
-                    "sub_unit_type_ids": sub_unit_type_ids,
-                }
-                response = self.client.patch(f"/api/v2/orgunittypes/{current_id}/", data=current_type, format="json")
-                self.assertJSONResponse(response, 200)
-                self.assertHasField(response.json(), "sub_unit_types", list)
-                new_sub_units = response.json()["sub_unit_types"]
-                new_sub_unit_ids = [sub_unit_type["id"] for sub_unit_type in new_sub_units]
-                self.assertEqual(new_sub_unit_ids, sub_unit_type_ids)
+        for org_unit_type_with in updated_with_sub_types:
+            self.assertJSONResponse(org_unit_type_with, 200)

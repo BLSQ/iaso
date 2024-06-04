70f954803bb733705ceacb88b0dbb62e3d5c07af
Fleury Butoyi <fleurybu@gmail.com>
2024-06-04 07:58:31 +0200

    Refactoring update sub type function

--- setuper/pyramid.py
+++ setuper/pyramid.py
@@ -1,4 +1,4 @@
-def setup_orgunits(account_name, iaso_client):
+def setup_orgunits(iaso_client):
     project_id = iaso_client.get("/api/projects/")["projects"][0]["id"]
     sources = iaso_client.get("/api/datasources/")["sources"]
     data_source_id = sources[0]["id"]
@@ -27,13 +27,14 @@ def setup_orgunits(account_name, iaso_client):
         "searches": [{"validation_status": "all", "color": "f4511e", "source": data_source_id}],
     }
     task = iaso_client.post("/api/tasks/create/orgunitsbulkupdate/", json=data)
-    update_org_unit_sub_type(iaso_client, project_id)
+    org_unit_types = iaso_client.get("/api/v2/orgunittypes/")["orgUnitTypes"]
+    update_org_unit_sub_type(iaso_client, project_id, org_unit_types)
     iaso_client.wait_task_completion(task)
 
 
-def update_org_unit_sub_type(iaso_client, project_id):
+def update_org_unit_sub_type(iaso_client, project_id, org_unit_types):
     print("-- Updating org unit sub type")
-    org_unit_types = iaso_client.get("/api/v2/orgunittypes/")["orgUnitTypes"]
+    updated_with_sub_types = []
     for org_unit_type in org_unit_types:
         org_unit_type_level = org_unit_type["depth"]
         org_unit_type_id = org_unit_type["id"]
@@ -50,4 +51,7 @@ def update_org_unit_sub_type(iaso_client, project_id):
                 "sub_unit_type_ids": sub_unit_type_ids,
             }
             # Updating default sub type
-            iaso_client.patch(f"/api/v2/orgunittypes/{org_unit_type_id}/", json=current_type)
+            updated_with_sub_types.append(
+                iaso_client.patch(f"/api/v2/orgunittypes/{org_unit_type_id}/", json=current_type)
+            )
+    return updated_with_sub_types

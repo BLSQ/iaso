77a123c22f513b0ebefef195f420a1cb58b51dce
kemar <marc@marcarea.com>
2024-06-11 11:16:54 +0200

    Test the number of queries

--- iaso/tests/api/org_unit_change_requests/test_org_unit_change_requests.py
+++ iaso/tests/api/org_unit_change_requests/test_org_unit_change_requests.py
@@ -180,7 +180,8 @@ class OrgUnitChangeRequestAPITestCase(APITestCase):
             "org_unit_id": self.org_unit.id,
             "new_name": "Bar",
         }
-        response = self.client.post("/api/orgunits/changes/?app_id=foo.bar.baz", data=data, format="json")
+        with self.assertNumQueries(14):
+            response = self.client.post("/api/orgunits/changes/?app_id=foo.bar.baz", data=data, format="json")
         self.assertEqual(response.status_code, 201)
         change_request = m.OrgUnitChangeRequest.objects.get(uuid=data["uuid"])
         self.assertEqual(change_request.new_name, data["new_name"])

6def9dcabe9cea6db29da6d93230727b05ce5880
kemar <marc@marcarea.com>
2024-06-14 10:53:17 +0200

    Fix `distinct()` without argument

--- iaso/api/org_unit_search.py
+++ iaso/api/org_unit_search.py
@@ -231,7 +231,7 @@ def build_org_units_queryset(queryset, params, profile):
     queryset = queryset.prefetch_related("parent__parent__parent")
     queryset = queryset.prefetch_related("parent__parent__parent__parent")
 
-    return queryset.distinct()
+    return queryset.distinct("id")
 
 
 def annotate_query(queryset, count_instances, count_per_form, forms):

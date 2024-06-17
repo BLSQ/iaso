c80691b25ede84240d195225f2224a2c360fdff3
Christophe Gerard <c@eduz.be>
2024-06-17 15:47:58 +0200

    remove only

--- hat/assets/js/cypress/integration/11 - submissions/list.spec.js
+++ hat/assets/js/cypress/integration/11 - submissions/list.spec.js
@@ -148,7 +148,7 @@ describe('Submissions', () => {
         });
     });
 
-    it.only('change filters should deep link and call api with correct params', () => {
+    it('change filters should deep link and call api with correct params', () => {
         goToPage();
         cy.intercept(
             'GET',

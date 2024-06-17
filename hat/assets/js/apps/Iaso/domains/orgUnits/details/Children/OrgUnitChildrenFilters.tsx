e1a090127a2f2806ff46fbc2befc96b40b179a72
Quang Son Le <qle@bluesquarehub.com>
2024-06-17 13:58:27 +0200

    IA-2976: code review

--- hat/assets/js/apps/Iaso/domains/orgUnits/details/Children/OrgUnitChildrenFilters.tsx
+++ hat/assets/js/apps/Iaso/domains/orgUnits/details/Children/OrgUnitChildrenFilters.tsx
@@ -47,94 +47,90 @@ export const OrgUnitChildrenFilters: FunctionComponent<Props> = ({
     const groupOptions = useGroupsOptions(groups);
 
     return (
-        <Box>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={3}>
-                    <InputComponent
-                        type="search"
-                        keyValue={`${OU_CHILDREN_PREFIX}Search`}
-                        value={filters[`${OU_CHILDREN_PREFIX}Search`]}
-                        onChange={handleChange}
-                        onEnterPressed={handleSearch}
-                        label={MESSAGES.textSearch}
-                    />
-                    <InputComponent
-                        type="select"
-                        keyValue={`${OU_CHILDREN_PREFIX}OrgUnitTypeId`}
-                        value={filters[`${OU_CHILDREN_PREFIX}OrgUnitTypeId`]}
-                        onChange={handleChange}
-                        multi
-                        options={orgUnitTypeOptions}
-                        loading={isLoadingTypes}
-                        label={MESSAGES.org_unit_type_id}
-                    />
-                    <InputComponent
-                        type="checkbox"
-                        keyValue={`${OU_CHILDREN_PREFIX}OnlyDirectChildren`}
-                        value={showDirectChildren}
-                        onChange={handleShowDirectChildren}
-                        label={MESSAGES.onlyDirectChildren}
-                    />
-                </Grid>
-                <Grid item xs={12} md={3}>
-                    <InputComponent
-                        type="select"
-                        keyValue={`${OU_CHILDREN_PREFIX}WithLocation`}
-                        value={filters[`${OU_CHILDREN_PREFIX}WithLocation`]}
-                        onChange={handleChange}
-                        options={locationOptions}
-                        label={MESSAGES.location}
-                    />
-                    <InputComponent
-                        type="select"
-                        keyValue={`${OU_CHILDREN_PREFIX}WithShape`}
-                        value={filters[`${OU_CHILDREN_PREFIX}WithShape`]}
-                        onChange={handleChange}
-                        options={locationOptions}
-                        label={MESSAGES.shape}
-                    />
-                </Grid>
-                <Grid item xs={12} md={3}>
-                    <InputComponent
-                        type="select"
-                        keyValue={`${OU_CHILDREN_PREFIX}Group`}
-                        value={filters[`${OU_CHILDREN_PREFIX}Group`]}
-                        onChange={handleChange}
-                        multi
-                        options={groupOptions}
-                        label={MESSAGES.group}
-                    />
-
-                    <InputComponent
-                        type="select"
-                        keyValue={`${OU_CHILDREN_PREFIX}HasInstances`}
-                        value={filters[`${OU_CHILDREN_PREFIX}HasInstances`]}
-                        onChange={handleChange}
-                        options={instancesOptions}
-                        label={MESSAGES.hasInstances}
-                    />
-                    <InputComponent
-                        type="select"
-                        keyValue={`${OU_CHILDREN_PREFIX}Validation_status`}
-                        value={
-                            filters[`${OU_CHILDREN_PREFIX}Validation_status`]
-                        }
-                        onChange={handleChange}
-                        options={validationStatusOptions}
-                        loading={isLoadingStatuses}
-                        clearable={false}
-                        label={MESSAGES.validationStatus}
-                    />
-                </Grid>
-                <Grid container item xs={12} md={3} justifyContent="flex-end">
-                    <Box mt={2}>
-                        <SearchButton
-                            onSearch={handleSearch}
-                            disabled={!filtersUpdated}
-                        />
-                    </Box>
-                </Grid>
+        <Grid container spacing={2}>
+            <Grid item xs={12} md={3}>
+                <InputComponent
+                    type="search"
+                    keyValue={`${OU_CHILDREN_PREFIX}Search`}
+                    value={filters[`${OU_CHILDREN_PREFIX}Search`]}
+                    onChange={handleChange}
+                    onEnterPressed={handleSearch}
+                    label={MESSAGES.textSearch}
+                />
+                <InputComponent
+                    type="select"
+                    keyValue={`${OU_CHILDREN_PREFIX}OrgUnitTypeId`}
+                    value={filters[`${OU_CHILDREN_PREFIX}OrgUnitTypeId`]}
+                    onChange={handleChange}
+                    multi
+                    options={orgUnitTypeOptions}
+                    loading={isLoadingTypes}
+                    label={MESSAGES.org_unit_type_id}
+                />
+                <InputComponent
+                    type="checkbox"
+                    keyValue={`${OU_CHILDREN_PREFIX}OnlyDirectChildren`}
+                    value={showDirectChildren}
+                    onChange={handleShowDirectChildren}
+                    label={MESSAGES.onlyDirectChildren}
+                />
             </Grid>
-        </Box>
+            <Grid item xs={12} md={3}>
+                <InputComponent
+                    type="select"
+                    keyValue={`${OU_CHILDREN_PREFIX}WithLocation`}
+                    value={filters[`${OU_CHILDREN_PREFIX}WithLocation`]}
+                    onChange={handleChange}
+                    options={locationOptions}
+                    label={MESSAGES.location}
+                />
+                <InputComponent
+                    type="select"
+                    keyValue={`${OU_CHILDREN_PREFIX}WithShape`}
+                    value={filters[`${OU_CHILDREN_PREFIX}WithShape`]}
+                    onChange={handleChange}
+                    options={locationOptions}
+                    label={MESSAGES.shape}
+                />
+            </Grid>
+            <Grid item xs={12} md={3}>
+                <InputComponent
+                    type="select"
+                    keyValue={`${OU_CHILDREN_PREFIX}Group`}
+                    value={filters[`${OU_CHILDREN_PREFIX}Group`]}
+                    onChange={handleChange}
+                    multi
+                    options={groupOptions}
+                    label={MESSAGES.group}
+                />
+
+                <InputComponent
+                    type="select"
+                    keyValue={`${OU_CHILDREN_PREFIX}HasInstances`}
+                    value={filters[`${OU_CHILDREN_PREFIX}HasInstances`]}
+                    onChange={handleChange}
+                    options={instancesOptions}
+                    label={MESSAGES.hasInstances}
+                />
+                <InputComponent
+                    type="select"
+                    keyValue={`${OU_CHILDREN_PREFIX}Validation_status`}
+                    value={filters[`${OU_CHILDREN_PREFIX}Validation_status`]}
+                    onChange={handleChange}
+                    options={validationStatusOptions}
+                    loading={isLoadingStatuses}
+                    clearable={false}
+                    label={MESSAGES.validationStatus}
+                />
+            </Grid>
+            <Grid container item xs={12} md={3} justifyContent="flex-end">
+                <Box mt={2}>
+                    <SearchButton
+                        onSearch={handleSearch}
+                        disabled={!filtersUpdated}
+                    />
+                </Box>
+            </Grid>
+        </Grid>
     );
 };

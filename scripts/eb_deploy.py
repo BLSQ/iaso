bcf5501697cff79e57ac17d8682d8b42b7d4376a
Bram Jans <ik@bramjans.com>
2024-06-19 15:59:35 +0200

    TRIA-7 Temp: Change eb_deploy.py to allow deployment...
    of branches other than main.

--- scripts/eb_deploy.py
+++ scripts/eb_deploy.py
@@ -11,37 +11,36 @@ def eb_deploy(env_name, version_name):
     # for Action debugging: print(f"eb deploy {env_name}")
 
 
-if __name__ == "__main__":
-    # Connect to aws to get the list of environment linked to the application
-    client = boto3.client("elasticbeanstalk")
-    desc = client.describe_environments(ApplicationName="Iaso")
-    eb_envs = {x["EnvironmentName"]: x for x in desc["Environments"]}
+# Connect to aws to get the list of environment linked to the application
+client = boto3.client("elasticbeanstalk")
+desc = client.describe_environments(ApplicationName="Iaso")
+eb_envs = {x["EnvironmentName"]: x for x in desc["Environments"]}
 
-    if "VERSION_NAME" not in os.environ:
-        exit("Mission VERSION environment variable")
-    version = os.environ["VERSION_NAME"]
+if "VERSION_NAME" not in os.environ:
+    exit("Mission VERSION environment variable")
+version = os.environ["VERSION_NAME"]
 
-    # If the argument correspond to one of the env, deploy to it
-    if sys.argv[1].lower() in [x.lower() for x in eb_envs.keys()]:
-        exit(eb_deploy(sys.argv[1], version_name=version))
+# If the argument correspond to one of the env, deploy to it
+if sys.argv[1].lower() in [x.lower() for x in eb_envs.keys()]:
+    exit(eb_deploy(sys.argv[1], version_name=version))
 
-    # otherwise consider it's a tag and update all the environment with the same `env` tag
-    tag_envs = {}
-    target_envs = []
-    for env_name, env_details in eb_envs.items():
-        if env_details["Status"] not in ("Ready", "Updating"):
-            print("Env {} ({}) is not ready for deploy, skipping".format(env_name, env_details["Status"]))
-            continue
-        raw_tags = client.list_tags_for_resource(ResourceArn=env_details["EnvironmentArn"])
+# otherwise consider it's a tag and update all the environment with the same `env` tag
+tag_envs = {}
+target_envs = []
+for env_name, env_details in eb_envs.items():
+    if env_details["Status"] not in ("Ready", "Updating"):
+        print("Env {} ({}) is not ready for deploy, skipping".format(env_name, env_details["Status"]))
+        continue
+    raw_tags = client.list_tags_for_resource(ResourceArn=env_details["EnvironmentArn"])
 
-        tags = {x["Key"]: x["Value"] for x in raw_tags.get("ResourceTags")}
-        tag_envs[env_name] = tags
-        if "env" in tags and tags["env"].lower() == sys.argv[1].lower():
-            target_envs.append(env_name)
+    tags = {x["Key"]: x["Value"] for x in raw_tags.get("ResourceTags")}
+    tag_envs[env_name] = tags
+    if "env" in tags and tags["env"].lower() == sys.argv[1].lower():
+        target_envs.append(env_name)
 
-    if len(target_envs) == 0:
-        exit(f"No target env found for {sys.argv[1]}")
-    else:
-        print(f"Will deploy to environments : {', '.join(target_envs)}")
-        for e in target_envs:
-            r = eb_deploy(e, version_name=version)
+if len(target_envs) == 0:
+    exit(f"No target env found for {sys.argv[1]}")
+else:
+    print(f"Will deploy to environments : {', '.join(target_envs)}")
+    for e in target_envs:
+        r = eb_deploy(e, version_name=version)

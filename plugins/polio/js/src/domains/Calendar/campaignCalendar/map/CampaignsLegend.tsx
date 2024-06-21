9b1dc964520d11a1d42e692b87f4af1dd99cccf1
Christophe Gerard <c@eduz.be>
2024-06-21 13:56:57 +0200

    review a bit campaign legend ui

--- plugins/polio/js/src/domains/Calendar/campaignCalendar/map/CampaignsLegend.tsx
+++ plugins/polio/js/src/domains/Calendar/campaignCalendar/map/CampaignsLegend.tsx
@@ -9,8 +9,9 @@ import {
 } from '@mui/material';
 import { useSafeIntl } from 'bluesquare-components';
 import React, { FunctionComponent } from 'react';
-import { useStyles } from '../Styles';
+import { vaccineOpacity } from '../Styles';
 
+import { SxStyles } from '../../../../../../../../hat/assets/js/apps/Iaso/types/general';
 import MESSAGES from '../../../../constants/messages';
 import { MappedCampaign } from '../types';
 
@@ -18,48 +19,81 @@ type Props = {
     campaigns: MappedCampaign[];
 };
 
+const styles: SxStyles = {
+    title: {
+        height: theme => theme.spacing(6),
+        minHeight: theme => theme.spacing(6),
+        '&.Mui-expanded': {
+            minHeight: theme => theme.spacing(6),
+        },
+    },
+    details: {
+        paddingTop: 0,
+    },
+    mapLegendCampaigns: {
+        width: 165,
+    },
+    mapLegendLabel: {
+        textAlign: 'right',
+    },
+    roundColor: {
+        margin: '4px 0',
+        borderRadius: theme => theme.spacing(3),
+        height: theme => theme.spacing(3),
+        width: theme => theme.spacing(3),
+        opacity: vaccineOpacity,
+        // @ts-ignore
+        border: theme => `2px solid ${theme.palette.ligthGray.border}`,
+    },
+    label: {
+        overflow: 'hidden',
+        whiteSpace: 'normal',
+        width: '100%',
+        justifyContent: 'flex-start',
+        height: '100%',
+        alignItems: 'center',
+        display: 'flex',
+        wordBreak: 'break-all',
+    },
+    mapLegendCampaignTitle: {
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+};
 export const CampaignsLegend: FunctionComponent<Props> = ({ campaigns }) => {
-    const classes = useStyles();
     const { formatMessage } = useSafeIntl();
     return (
-        <Accordion elevation={1} className={classes.mapLegendCampaigns}>
-            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Accordion elevation={1} sx={styles.mapLegendCampaigns}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />} sx={styles.title}>
                 <Typography
                     variant="subtitle1"
-                    className={classes.mapLegendCampaignTitle}
+                    sx={styles.mapLegendCampaignTitle}
                 >
                     {formatMessage(MESSAGES.campaigns)}
                 </Typography>
             </AccordionSummary>
-            <AccordionDetails>
+            <AccordionDetails sx={styles.details}>
                 <Box display="block">
                     {campaigns.length === 0 &&
                         formatMessage(MESSAGES.noCampaign)}
                     {campaigns.map(c => (
-                        <Grid container spacing={1} key={c.id}>
+                        <Grid container spacing={0} key={c.id}>
                             <Grid
                                 item
                                 sm={3}
                                 container
-                                className={classes.mapLegendLabel}
+                                sx={styles.mapLegendLabel}
                                 justifyContent="flex-start"
                             >
-                                <span
-                                    className={classes.roundColor}
-                                    style={{
+                                <Box
+                                    sx={{
+                                        ...styles.roundColor,
                                         borderColor: c.color,
-                                        margin: '2px',
                                     }}
                                 />
                             </Grid>
-                            <Grid
-                                item
-                                sm={9}
-                                container
-                                justifyContent="flex-end"
-                                alignItems="center"
-                            >
-                                {c.name}
+                            <Grid item sm={9}>
+                                <Box sx={styles.label}>{c.name}</Box>
                             </Grid>
                         </Grid>
                     ))}

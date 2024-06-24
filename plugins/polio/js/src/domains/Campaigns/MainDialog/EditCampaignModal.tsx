0849775b1ab176074bf367291a8a0e15eef4b198
Christophe Gerard <c@eduz.be>
2024-06-24 16:38:24 +0200

    open edit dialg if cmapaing id in the url is the same

--- plugins/polio/js/src/domains/Campaigns/MainDialog/EditCampaignModal.tsx
+++ plugins/polio/js/src/domains/Campaigns/MainDialog/EditCampaignModal.tsx
@@ -11,7 +11,9 @@ export const EditCampaignModal: FunctionComponent<Props> = ({
     campaignId,
 }) => {
     const redirectTo = useRedirectTo();
-    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isOpen, setIsOpen] = useState<boolean>(
+        Boolean(params.campaignId) && params.campaignId === campaignId,
+    );
 
     const openDialog = useCallback(() => {
         redirectTo(DASHBOARD_BASE_URL, { ...params, campaignId });

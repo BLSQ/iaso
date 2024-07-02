f748013fda5cb49d2480b64bea211f802f233dbf
kemar <marc@marcarea.com>
2024-07-02 17:28:40 +0200

    Add chronogram list

---
+++ plugins/polio/js/src/domains/Chronogram/Filters/ChronogramFilters.tsx
@@ -0,0 +1,93 @@
+import React, { FunctionComponent } from 'react';
+import { Box, Grid } from '@mui/material';
+
+import { useSafeIntl } from 'bluesquare-components';
+
+import InputComponent from '../../../../../../../hat/assets/js/apps/Iaso/components/forms/InputComponent';
+import { FilterButton } from '../../../../../../../hat/assets/js/apps/Iaso/components/FilterButton';
+import { useFilterState } from '../../../../../../../hat/assets/js/apps/Iaso/hooks/useFilterState';
+
+import MESSAGES from '../messages';
+import { ChronogramParams } from '../types';
+import { baseUrls } from '../../../constants/urls';
+import { useGetCountries } from '../../../hooks/useGetCountries';
+
+type Props = {
+    params: ChronogramParams;
+};
+
+const baseUrl = baseUrls.chronogram;
+
+export const ChronogramFilters: FunctionComponent<Props> = ({ params }) => {
+    const { formatMessage } = useSafeIntl();
+
+    const { filters, handleSearch, handleChange, filtersUpdated } =
+        useFilterState({ baseUrl, params });
+
+    const { countriesData, isFetchingCountriesData: isFetchingCountries } =
+        useGetCountries();
+    const countriesOptions = (countriesData && countriesData.orgUnits) || [];
+
+    const onTimeOptions = [
+        {
+            label: formatMessage(MESSAGES.yes),
+            value: 'true',
+        },
+        {
+            label: formatMessage(MESSAGES.no),
+            value: 'false',
+        },
+    ];
+
+    return (
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={4} lg={4}>
+                    <InputComponent
+                        type="search"
+                        keyValue="search"
+                        value={filters.search}
+                        onChange={handleChange}
+                        onEnterPressed={handleSearch}
+                        label={MESSAGES.filterLabelSearch}
+                    />
+                </Grid>
+                <Grid item xs={12} md={4} lg={4}>
+                    <InputComponent
+                        loading={isFetchingCountries}
+                        keyValue="country"
+                        multi
+                        clearable
+                        onChange={handleChange}
+                        value={filters.countries}
+                        type="select"
+                        options={countriesOptions.map(c => ({
+                            label: c.name,
+                            value: c.id,
+                        }))}
+                        label={MESSAGES.filterLabelCountry}
+                    />
+                </Grid>
+                <Grid item xs={12} md={4} lg={4}>
+                    <InputComponent
+                        type="select"
+                        clearable
+                        keyValue="on_time"
+                        value={filters.on_time}
+                        onChange={handleChange}
+                        options={onTimeOptions}
+                        label={MESSAGES.filterLabelOnTime}
+                    />
+                </Grid>
+            </Grid>
+            <Grid container item justifyContent="flex-end">
+                <Box mt={2}>
+                    <FilterButton
+                        disabled={!filtersUpdated}
+                        onFilter={handleSearch}
+                    />
+                </Box>
+            </Grid>
+        </>
+    );
+};
--- plugins/polio/js/src/domains/Chronogram/index.tsx
+++ plugins/polio/js/src/domains/Chronogram/index.tsx
@@ -1,5 +1,55 @@
 import React, { FunctionComponent } from 'react';
+import { Box } from '@mui/material';
+
+import { LoadingSpinner, useSafeIntl } from 'bluesquare-components';
+
+import TopBar from '../../../../../../hat/assets/js/apps/Iaso/components/nav/TopBarComponent';
+import { baseUrls } from '../../constants/urls';
+import { useParamsObject } from '../../../../../../hat/assets/js/apps/Iaso/routing/hooks/useParamsObject';
+
+import MESSAGES from './messages';
+import { ChronogramFilters } from './Filters/ChronogramFilters';
+import { ChronogramParams, ChronogramTaskMetaData } from './types';
+import { ChronogramTable } from './Table/ChronogramTable';
+import { useOptionChronogramTask } from './api/useOptionChronogramTask';
+import { useStyles } from '../../styles/theme';
 
 export const Chronogram: FunctionComponent = () => {
-    return <h1>Chronograms list</h1>;
+    const params = useParamsObject(baseUrls.chronogram) as ChronogramParams;
+
+    const paramsNew: ChronogramParams = {
+        ...params,
+        pageSize: params.pageSize || '20',
+        order: params.order || 'id',
+        page: params.page || '1',
+    };
+
+    const { data: chronogramTaskMetaData, isFetching: isFetchingMetaData } =
+        useOptionChronogramTask();
+
+    const classes: Record<string, string> = useStyles();
+    const { formatMessage } = useSafeIntl();
+
+    return (
+        <>
+            {isFetchingMetaData && <LoadingSpinner />}
+            {!isFetchingMetaData && (
+                <>
+                    <TopBar
+                        title={formatMessage(MESSAGES.chronogramTitle)}
+                        displayBackButton={false}
+                    />
+                    <Box className={classes.containerFullHeightNoTabPadded}>
+                        <ChronogramFilters params={paramsNew} />
+                        <ChronogramTable
+                            params={paramsNew}
+                            chronogramTaskMetaData={
+                                chronogramTaskMetaData as ChronogramTaskMetaData
+                            }
+                        />
+                    </Box>
+                </>
+            )}
+        </>
+    );
 };

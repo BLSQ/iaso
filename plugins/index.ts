21006211dbc3749bffc27735ed3da96b0415df2a
Beygorghor <c@eduz.be>
2024-07-02 14:29:29 +0200

    adding a new plugin for public registry

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,7 +1,9 @@
-import testAppConfig from './test/js/config';
 import polioConfig from './polio/js/config';
+import publicRegistryConfig from './public_registry/js/config';
+import testAppConfig from './test/js/config';
 
 export default {
     test: testAppConfig,
     polio: polioConfig,
+    public_registry: publicRegistryConfig,
 };

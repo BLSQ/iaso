a91272be4b1ce4c1c32f5836be4cd8ea3d7ea27c
kemar <marc@marcarea.com>
2024-07-04 17:04:39 +0200

    Allow to delete a default chronogram task

---
+++ plugins/polio/js/src/domains/Chronogram/ChronogramTemplateTask/Modals/ChronogramTemplateTaskDeleteModal.tsx
@@ -0,0 +1,57 @@
+import React, { FunctionComponent, useCallback } from 'react';
+import {
+    ConfirmCancelModal,
+    makeFullModal,
+    useSafeIntl,
+} from 'bluesquare-components';
+
+import MESSAGES from '../messages';
+
+import { ChronogramTemplateTask } from '../types';
+
+import { DeleteIconButton } from '../../../../../../../../hat/assets/js/apps/Iaso/components/Buttons/DeleteIconButton';
+import { useDeleteChronogramTemplateTask } from '../api/useDeleteChronogramTemplateTask';
+
+type Props = {
+    isOpen: boolean;
+    closeDialog: () => void;
+    chronogramTemplateTask: ChronogramTemplateTask;
+};
+
+const ChronogramTemplateTaskDeleteModal: FunctionComponent<Props> = ({
+    isOpen,
+    closeDialog,
+    chronogramTemplateTask,
+}) => {
+    const { formatMessage } = useSafeIntl();
+    const { mutate: deleteChronogramTemplateTask } =
+        useDeleteChronogramTemplateTask();
+    const onDelete = useCallback(
+        () => deleteChronogramTemplateTask(chronogramTemplateTask.id),
+        [chronogramTemplateTask.id, deleteChronogramTemplateTask],
+    );
+    return (
+        <ConfirmCancelModal
+            open={isOpen}
+            closeDialog={closeDialog}
+            onClose={() => null}
+            id="delete-notification"
+            dataTestId="delete-notification"
+            titleMessage={MESSAGES.modalDeleteTitle}
+            onConfirm={onDelete}
+            onCancel={() => null}
+            confirmMessage={MESSAGES.modalDeleteYes}
+            cancelMessage={MESSAGES.modalDeleteNo}
+        >
+            <p>{formatMessage(MESSAGES.modalDeleteConfirm)}</p>
+            <p>{chronogramTemplateTask.description}</p>
+        </ConfirmCancelModal>
+    );
+};
+
+const modalWithButton = makeFullModal(
+    ChronogramTemplateTaskDeleteModal,
+    DeleteIconButton,
+);
+
+export { modalWithButton as DeleteChronogramTemplateTask };
--- plugins/polio/js/src/domains/Chronogram/ChronogramTemplateTask/Table/useChronogramTemplateTaskTableColumns.tsx
+++ plugins/polio/js/src/domains/Chronogram/ChronogramTemplateTask/Table/useChronogramTemplateTaskTableColumns.tsx
@@ -1,7 +1,10 @@
-import { useMemo } from 'react';
+import React, { useMemo } from 'react';
+import { Box } from '@mui/material';
+
 import { Column, useSafeIntl } from 'bluesquare-components';
 
 import MESSAGES from '../messages';
+import { DeleteChronogramTemplateTask } from '../Modals/ChronogramTemplateTaskDeleteModal';
 
 export const useChronogramTemplateTaskTableColumns = (): Column[] => {
     const { formatMessage } = useSafeIntl();
@@ -27,6 +30,20 @@ export const useChronogramTemplateTaskTableColumns = (): Column[] => {
                 Header: formatMessage(MESSAGES.labelStartOffsetInDays),
                 id: 'start_offset_in_days',
                 accessor: 'start_offset_in_days',
+            },
+            {
+                Header: formatMessage(MESSAGES.actions),
+                sortable: false,
+                Cell: settings => {
+                    return (
+                        <Box display="inline-flex">
+                            {/* @ts-ignore */}
+                            <DeleteChronogramTemplateTask
+                                chronogramTemplateTask={settings.row.original}
+                            />
+                        </Box>
+                    );
+                },
             },
         ];
     }, [formatMessage]);

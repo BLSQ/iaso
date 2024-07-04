07f45d2034fb3f831066415394653370a5eab01f
Bram Jans <ik@bramjans.com>
2024-07-04 14:18:51 +0200

    SLEEP-1448 Bulk upload: support form update and bugfix
    - Iterate over the instances.json file instead of the folders to avoid
    files not being found in case of a form that already exists. In this
    case the instance already exists in the db, but potentially with a
    different file_name if it was uploaded from a different tablet (nearby
    share does not preserve filenames).
    - If during processing we get a form that already exists, an for which the
    bulk upload contains a newer form (with a more recent `updated_at`
    timestamp), we update the form and set the new `source_updated_at`.
    - Ignore .xml files when processing attachments to circumvent a bug in
    the mobile app

--- iaso/tests/tasks/test_process_mobile_bulk_upload.py
+++ iaso/tests/tasks/test_process_mobile_bulk_upload.py
@@ -3,21 +3,32 @@ import zipfile
 
 from django.contrib.auth.models import User
 from django.test import TestCase
-from unittest import mock
+from unittest import mock, skip
 
 from hat.api_import.models import APIImport
 from iaso import models as m
 from iaso.tasks.process_mobile_bulk_upload import process_mobile_bulk_upload
 
-ZIP_FIXTURE_DIR = "iaso/tests/fixtures/mobile_bulk_uploads/one_test_with_image_zip"
+CATT_TABLET_DIR = "catt_one_test_with_image"
+LABO_TABLET_DIR = "labo_update_registration_form"
+
+DISASI_MAKULO_REGISTRATION = "3f0ed68e-bfcf-4395-a2a5-a5821390ae1b"
+DISASI_MAKULO_CATT = "a5362052-408f-44f8-8abc-2a520c01ea10"
+PATRICE_AKAMBU_REGISTRATION = "90619ebe-4aa5-4eca-ae66-bf989bfb1539"
+PATRICE_AKAMBU_CATT = "f55b0eff-b353-49ea-93b9-0257b6b807c4"
+
 CORRECT_FILES_FOR_ZIP = [
-    "3f0ed68e-bfcf-4395-a2a5-a5821390ae1b",  # Disasi Makulo CATT
-    "a5362052-408f-44f8-8abc-2a520c01ea10",  # Disasi Makulo registration
-    "f55b0eff-b353-49ea-93b9-0257b6b807c4",  # Patrice Akambu CATT
-    "90619ebe-4aa5-4eca-ae66-bf989bfb1539",  # Patrice Akambu registration
+    DISASI_MAKULO_REGISTRATION,
+    DISASI_MAKULO_CATT,
+    PATRICE_AKAMBU_REGISTRATION,
+    PATRICE_AKAMBU_CATT,
     "instances.json",
     "orgUnits.json",
 ]
+
+
+def zip_fixture_dir(tablet_dir):
+    return f"iaso/tests/fixtures/mobile_bulk_uploads/{tablet_dir}"
 
 
 def add_to_zip(zipf, directory, subset):
@@ -40,7 +51,7 @@ class ProcessMobileBulkUploadTest(TestCase):
         self.api_import = APIImport.objects.create(
             user=self.user,
             import_type="bulk",
-            json_body={"file": "one_test_with_image_zip"},
+            json_body={"file": CATT_TABLET_DIR},
         )
         self.task = m.Task.objects.create(
             name="process_mobile_bulk_upload",
@@ -58,10 +69,10 @@ class ProcessMobileBulkUploadTest(TestCase):
         # Create the zip file: we create it on the fly to be able to clearly
         # see the contents in our repo. We then mock the file download method
         # to return the filepath to this zip.
-        with zipfile.ZipFile("/tmp/one_test_with_image.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
-            add_to_zip(zipf, ZIP_FIXTURE_DIR, CORRECT_FILES_FOR_ZIP)
+        with zipfile.ZipFile(f"/tmp/{CATT_TABLET_DIR}.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
+            add_to_zip(zipf, zip_fixture_dir(CATT_TABLET_DIR), CORRECT_FILES_FOR_ZIP)
 
-        mock_download_file.return_value = "/tmp/one_test_with_image.zip"
+        mock_download_file.return_value = f"/tmp/{CATT_TABLET_DIR}.zip"
 
         self.assertEquals(m.Entity.objects.count(), 0)
         self.assertEquals(m.Instance.objects.count(), 0)
@@ -91,18 +102,18 @@ class ProcessMobileBulkUploadTest(TestCase):
 
         # Instances (Submissions) + Entity were created
         self.assertEquals(m.Entity.objects.count(), 2)
-        entity_disasi = m.Entity.objects.get(uuid="3f0ed68e-bfcf-4395-a2a5-a5821390ae1b")
-        entity_patrice = m.Entity.objects.get(uuid="90619ebe-4aa5-4eca-ae66-bf989bfb1539")
+        entity_disasi = m.Entity.objects.get(uuid=DISASI_MAKULO_REGISTRATION)
+        entity_patrice = m.Entity.objects.get(uuid=PATRICE_AKAMBU_REGISTRATION)
         self.assertEquals(m.Instance.objects.count(), 4)
         self.assertEquals(m.InstanceFile.objects.count(), 2)
 
         # Entity 1: Disasi Makulo
-        reg_instance = m.Instance.objects.get(uuid="3f0ed68e-bfcf-4395-a2a5-a5821390ae1b")
+        reg_instance = m.Instance.objects.get(uuid=DISASI_MAKULO_REGISTRATION)
         self.assertEquals(reg_instance.json.get("_full_name"), "Disasi Makulo")
         self.assertEquals(reg_instance.entity, entity_disasi)
         self.assertEquals(reg_instance.instancefile_set.count(), 0)
 
-        catt_instance = m.Instance.objects.get(uuid="a5362052-408f-44f8-8abc-2a520c01ea10")
+        catt_instance = m.Instance.objects.get(uuid=DISASI_MAKULO_CATT)
         self.assertEquals(catt_instance.json.get("result"), "positive")
         self.assertEquals(catt_instance.entity, entity_disasi)
         self.assertEquals(catt_instance.instancefile_set.count(), 1)
@@ -110,12 +121,12 @@ class ProcessMobileBulkUploadTest(TestCase):
         self.assertEquals(image.name, "1712326156339.webp")
 
         # Entity 2: Patrice Akambu
-        reg_instance = m.Instance.objects.get(uuid="90619ebe-4aa5-4eca-ae66-bf989bfb1539")
+        reg_instance = m.Instance.objects.get(uuid=PATRICE_AKAMBU_REGISTRATION)
         self.assertEquals(reg_instance.json.get("_full_name"), "Patrice Akambu")
         self.assertEquals(reg_instance.entity, entity_patrice)
         self.assertEquals(reg_instance.instancefile_set.count(), 0)
 
-        catt_instance = m.Instance.objects.get(uuid="f55b0eff-b353-49ea-93b9-0257b6b807c4")
+        catt_instance = m.Instance.objects.get(uuid=PATRICE_AKAMBU_CATT)
         self.assertEquals(catt_instance.json.get("result"), "positive")
         self.assertEquals(catt_instance.entity, entity_patrice)
         self.assertEquals(catt_instance.instancefile_set.count(), 1)
@@ -124,11 +135,16 @@ class ProcessMobileBulkUploadTest(TestCase):
         self.assertEquals(image.name, "1712326156339.webp")
 
     def test_fail_in_the_middle_of_import(self, mock_download_file):
-        # Add all correct files as well as a wrong one. The job will fail, then
-        # verify that nothing was created.
-        with zipfile.ZipFile("/tmp/one_test_with_image.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
-            add_to_zip(zipf, ZIP_FIXTURE_DIR, CORRECT_FILES_FOR_ZIP + ["incorrect-folder"])
-        mock_download_file.return_value = "/tmp/one_test_with_image.zip"
+        # Org unit doesn't exist. The job will fail, then verify that
+        # nothing was created.
+        INCORRECT_FILES_FOR_ZIP = ["instances.json"]
+        with zipfile.ZipFile(f"/tmp/{CATT_TABLET_DIR}.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
+            add_to_zip(
+                zipf,
+                zip_fixture_dir(CATT_TABLET_DIR),
+                INCORRECT_FILES_FOR_ZIP,
+            )
+        mock_download_file.return_value = f"/tmp/{CATT_TABLET_DIR}.zip"
 
         self.assertEquals(m.Entity.objects.count(), 0)
         self.assertEquals(m.Instance.objects.count(), 0)
@@ -155,3 +171,68 @@ class ProcessMobileBulkUploadTest(TestCase):
         self.assertEquals(m.Entity.objects.count(), 0)
         self.assertEquals(m.Instance.objects.count(), 0)
         self.assertEquals(m.InstanceFile.objects.count(), 0)
+
+    # SLEEP-1448: Update an existing registration form (with a different file path
+    # on the already created instance)
+    def test_reference_form_update(self, mock_download_file):
+        # Do an import with the CATT tablet first to already create Disasi Makulo
+        with zipfile.ZipFile(f"/tmp/{CATT_TABLET_DIR}.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
+            add_to_zip(zipf, zip_fixture_dir(CATT_TABLET_DIR), CORRECT_FILES_FOR_ZIP)
+        mock_download_file.return_value = f"/tmp/{CATT_TABLET_DIR}.zip"
+        process_mobile_bulk_upload(
+            api_import_id=self.api_import.id,
+            project_id=self.project.id,
+            task=self.task,
+            _immediate=True,
+        )
+
+        instance_disasi = m.Instance.objects.get(uuid=DISASI_MAKULO_REGISTRATION)
+        instance_patrice = m.Instance.objects.get(uuid=PATRICE_AKAMBU_REGISTRATION)
+        self.assertEquals(instance_disasi.source_updated_at.date().isoformat(), "2024-04-05")
+        self.assertEquals(instance_disasi.json["is_confirmed_positive"], "0")
+        self.assertEquals(instance_patrice.source_updated_at.date().isoformat(), "2024-04-05")
+        self.assertEquals(instance_patrice.json["is_confirmed_positive"], "0")
+
+        # Now import with the LABO tablet to update Disasi Makulo.
+        # Also contains Patrice Akambu, but with the same updated_at timestamp.
+        task_2 = m.Task.objects.create(
+            name="process_mobile_bulk_upload",
+            launcher=self.user,
+            account=m.Account.objects.first(),
+        )
+        api_import = APIImport.objects.create(
+            user=self.user,
+            import_type="bulk",
+            json_body={"file": LABO_TABLET_DIR},
+        )
+
+        with zipfile.ZipFile(f"/tmp/{LABO_TABLET_DIR}.zip", "w", zipfile.ZIP_DEFLATED) as zipf:
+            add_to_zip(zipf, zip_fixture_dir(LABO_TABLET_DIR), CORRECT_FILES_FOR_ZIP)
+        mock_download_file.return_value = f"/tmp/{LABO_TABLET_DIR}.zip"
+        process_mobile_bulk_upload(
+            api_import_id=api_import.id,
+            project_id=self.project.id,
+            task=task_2,
+            _immediate=True,
+        )
+
+        # check Task status and result
+        task_2.refresh_from_db()
+        self.assertEquals(task_2.status, m.SUCCESS)
+
+        self.assertEquals(APIImport.objects.count(), 2)
+        api_import = APIImport.objects.last()
+        self.assertEquals(api_import.import_type, "bulk")
+        self.assertFalse(api_import.has_problem)
+
+        # Verify that only Disasi was changed
+        instance_disasi.refresh_from_db()
+        instance_patrice.refresh_from_db()
+        self.assertEquals(instance_disasi.source_updated_at.date().isoformat(), "2024-04-17")
+        self.assertEquals(instance_disasi.json["is_confirmed_positive"], "1")
+        self.assertEquals(instance_patrice.source_updated_at.date().isoformat(), "2024-04-05")
+        self.assertEquals(instance_patrice.json["is_confirmed_positive"], "0")
+
+        # Bug with extra .xml files of other form submissions being in the same
+        # folder. Make sure they are not processed.
+        self.assertEquals(instance_disasi.instancefile_set.count(), 0)

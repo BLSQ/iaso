6bb20815d9b309e3766b8019e141115130d39217
Christophe Gerard <c@eduz.be>
2024-07-05 13:57:16 +0200

    source config can have empty user

--- hat/assets/js/apps/Iaso/domains/orgUnits/components/TreeView/useSourceConfig.ts
+++ hat/assets/js/apps/Iaso/domains/orgUnits/components/TreeView/useSourceConfig.ts
@@ -1,8 +1,8 @@
 import { useMemo } from 'react';
 import { User, useCurrentUser } from '../../../../utils/usersUtils';
+import { useGetDataSource } from '../../../dataSources/hooks/useGetDataSource';
 import { useGetDataSourceVersion } from '../../../dataSources/hooks/useGetDataSourceVersion';
 import { OrgUnitStatus } from '../../types/orgUnit';
-import { useGetDataSource } from '../../../dataSources/hooks/useGetDataSource';
 
 export type SourceInfos = {
     sourceName?: string;
@@ -31,7 +31,7 @@ export const useSourceConfig = (
         useGetDataSourceVersion(versionId ? `${versionId}` : undefined);
     return useMemo(() => {
         const defaultUserConfig =
-            currentUser.account.default_version?.data_source
+            currentUser?.account.default_version?.data_source
                 ?.tree_config_status_fields;
 
         let sourceSettings = DEFAULT_CONFIG;
@@ -64,7 +64,7 @@ export const useSourceConfig = (
             if (defaultUserConfig?.length > 0) {
                 sourceSettings = defaultUserConfig;
             }
-            const defaultVersion = currentUser.account.default_version;
+            const defaultVersion = currentUser?.account.default_version;
             sourceInfos = {
                 sourceName: defaultVersion?.data_source?.name,
                 sourceId: defaultVersion?.data_source?.id,
@@ -78,7 +78,7 @@ export const useSourceConfig = (
             sourceInfos,
         };
     }, [
-        currentUser.account.default_version,
+        currentUser?.account.default_version,
         sourceId,
         source,
         isFetchingSource,

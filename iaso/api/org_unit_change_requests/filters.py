48a6cee350f307318956e5a967e9402709d19dfe
Quang Son Le <qle@bluesquarehub.com>
2024-07-31 10:00:29 +0200

    IA-3258: remove duplicate parse_comma_separated_numeric_values methods

--- iaso/api/org_unit_change_requests/filters.py
+++ iaso/api/org_unit_change_requests/filters.py
@@ -4,7 +4,7 @@ from django.db.models import Q
 from django.db.models.query import QuerySet
 from django.utils.translation import gettext_lazy as _
 from rest_framework.exceptions import ValidationError
-
+from iaso.api.common import parse_comma_separated_numeric_values
 from iaso.models import OrgUnit, OrgUnitChangeRequest
 from iaso.models.payments import PaymentStatuses
 
@@ -32,17 +32,6 @@ class OrgUnitChangeRequestListFilter(django_filters.rest_framework.FilterSet):
     status = django_filters.CharFilter(method="filter_status", label=_("Status (comma-separated)"))
     payment_status = django_filters.CharFilter(method="filter_payment_status", label=_("Payment status"))
 
-    @staticmethod
-    def parse_comma_separated_numeric_values(value: str, field_name: str) -> list:
-        """
-        Parses a comma-separated string of numeric values and returns a list of integers.
-        Raises a ValidationError if the input is not valid.
-        """
-        ids = [val for val in value.split(",") if val.isnumeric()]
-        if not ids:
-            raise ValidationError({field_name: ["Invalid value."]})
-        return [int(val) for val in ids]
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.form.fields["created_at"].fields[0].input_formats = settings.API_DATE_INPUT_FORMATS
@@ -62,27 +51,27 @@ class OrgUnitChangeRequestListFilter(django_filters.rest_framework.FilterSet):
             raise ValidationError({"parent_id": [f"OrgUnit with id {value} does not exist."]})
 
     def filter_org_unit_type_id(self, queryset: QuerySet, name: str, value: str) -> QuerySet:
-        org_unit_type_ids = self.parse_comma_separated_numeric_values(value, name)
+        org_unit_type_ids = parse_comma_separated_numeric_values(value, name)
         return queryset.filter(
             Q(old_org_unit_type__id__in=org_unit_type_ids) | Q(new_org_unit_type__id__in=org_unit_type_ids)
         )
 
     def filter_groups(self, queryset: QuerySet, name: str, value: str) -> QuerySet:
-        groups_ids = self.parse_comma_separated_numeric_values(value, name)
+        groups_ids = parse_comma_separated_numeric_values(value, name)
         return queryset.filter(Q(old_groups__in=groups_ids) | Q(new_groups__in=groups_ids))
 
     def filter_forms(self, queryset: QuerySet, name: str, value: str) -> QuerySet:
-        forms_ids = self.parse_comma_separated_numeric_values(value, name)
+        forms_ids = parse_comma_separated_numeric_values(value, name)
         return queryset.filter(
             Q(old_reference_instances__form_id__in=forms_ids) | Q(new_reference_instances__form_id__in=forms_ids)
         )
 
     def filter_users(self, queryset: QuerySet, name: str, value: str) -> QuerySet:
-        users_ids = self.parse_comma_separated_numeric_values(value, name)
+        users_ids = parse_comma_separated_numeric_values(value, name)
         return queryset.filter(Q(created_by_id__in=users_ids) | Q(updated_by__in=users_ids))
 
     def filter_user_roles(self, queryset: QuerySet, name: str, value: str) -> QuerySet:
-        users_roles_ids = self.parse_comma_separated_numeric_values(value, name)
+        users_roles_ids = parse_comma_separated_numeric_values(value, name)
         return queryset.filter(
             Q(created_by__iaso_profile__user_roles__id__in=users_roles_ids)
             | Q(updated_by__iaso_profile__user_roles__id__in=users_roles_ids)

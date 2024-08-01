4341222b3bd61a928e3bf43d548c116494742e14
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2024-08-01 17:04:14 +0200

    fix conflict in iaso/api/query_params.py

--- hat/assets/js/apps/Iaso/constants/urls.ts
+++ hat/assets/js/apps/Iaso/constants/urls.ts
@@ -297,7 +297,7 @@ export const baseRouteConfigs: Record<string, RouteConfig> = {
     },
     orgUnitTypes: {
         url: 'orgunits/types',
-        params: ['accountId', 'search', ...paginationPathParams],
+        params: ['accountId', 'search', 'projectIds', ...paginationPathParams],
     },
     entities: {
         url: 'entities/list',

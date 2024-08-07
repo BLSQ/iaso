5d96b080574b9cc71b78b9fc8ddbdbe7069f0faf
kemar <marc@marcarea.com>
2024-08-07 14:01:53 +0200

    Use only one import line

--- plugins/polio/js/src/domains/Chronogram/Chronogram/Table/useChronogramTableColumns.tsx
+++ plugins/polio/js/src/domains/Chronogram/Chronogram/Table/useChronogramTableColumns.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import { useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { IconButton, Column, useSafeIntl } from 'bluesquare-components';
 
 import {

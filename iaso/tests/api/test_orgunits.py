c63034d62c8ef4db1c40e36c78f918beae2f3844
Stéphan Mestach <smestach@bluesquare.org>
2024-08-21 16:08:14 +0200

    IA-3032 Fix api so if remove the geojson we both empty geom and simplified_geom

--- iaso/tests/api/test_orgunits.py
+++ iaso/tests/api/test_orgunits.py
@@ -2,7 +2,7 @@ import typing
 
 from django.contrib.gis.geos import Polygon, Point, MultiPolygon, GEOSGeometry
 from django.db import connection
-
+from iaso.utils.gis import simplify_geom
 from hat.audit.models import Modification
 from iaso import models as m
 from iaso.models import OrgUnitType, OrgUnit
@@ -933,6 +933,30 @@ class OrgUnitAPITestCase(APITestCase):
         self.assertQuerySetEqual(ou.groups.all().order_by("name"), [group_a, group_b])
         self.assertEqual(ou.geom.wkt, MultiPolygon(Polygon([(0, 0), (0, 1), (1, 1), (0, 0)])).wkt)
         self.assertEqual(response.data["reference_instances"], [])
+
+    def test_edit_org_unit_partial_update_remove_geojson_geom_simplified_geom_should_be_consistent(self):
+        """Check that if we remove the geojson both simplified_geom and geom are empty"""
+        ou = m.OrgUnit(version=self.sw_version_1)
+        ou.name = "test ou"
+        ou.source_ref = "b"
+        ou.geom = MultiPolygon(Polygon([(0, 0), (0, 1), (1, 1), (0, 0)]))
+        ou.simplified_geom = simplify_geom(MultiPolygon(Polygon([(0, 0), (0, 1), (1, 1), (0, 0)])))
+        ou.save()
+        old_modification_date = ou.updated_at
+        self.client.force_authenticate(self.yoda)
+        data = {"geo_json": None}
+        response = self.client.patch(
+            f"/api/orgunits/{ou.id}/",
+            format="json",
+            data=data,
+        )
+        self.assertJSONResponse(response, 200)
+        ou.refresh_from_db()
+        # check the orgunit has not beee modified
+        self.assertGreater(ou.updated_at, old_modification_date)
+        self.assertEqual(ou.name, "test ou")
+        self.assertIsNone(ou.geom)
+        self.assertIsNone(ou.simplified_geom)
 
     def test_edit_org_unit_edit_bad_group_fail(self):
         """Check for a previous bug if an org unit is already member of a bad group

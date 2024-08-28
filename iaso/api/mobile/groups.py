3f4543cf6ca0291efe7202979726a20c66688578
Benjamin Monjoie <bmo@appkers.com>
2024-08-28 11:10:12 +0200

    Allow the mobile app to download all groups
    This adds a query parameter `showDeleted` that allows the mobile app to download
    groups from all source_version.
    
    IA-3379

--- iaso/api/mobile/groups.py
+++ iaso/api/mobile/groups.py
@@ -1,3 +1,5 @@
+from typing import Dict, Any
+
 from django.db.models.query import QuerySet
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
@@ -8,17 +10,34 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from iaso.api.query_params import APP_ID
+from iaso.api.query_params import APP_ID, SHOW_DELETED
 from iaso.api.serializers import AppIdSerializer
-from iaso.models import Group, Project
+from iaso.models import Group, Project, SourceVersion
 
 
 class MobileGroupSerializer(serializers.ModelSerializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.source_version = kwargs["context"].get("source_version")
+
+    erased = serializers.SerializerMethodField()
+
+    def get_erased(self, obj: Group):
+        return obj.source_version.id != self.source_version
+
+    @staticmethod
+    def include_source_version(
+        context: Dict[str, Any], source_version: SourceVersion
+    ) -> Dict[str, Any]:
+        context["source_version"] = source_version.id
+        return context
+
     class Meta:
         model = Group
         fields = [
             "id",
             "name",
+            "erased",
         ]
 
 
@@ -43,19 +62,49 @@ class MobileGroupsViewSet(ListModelMixin, GenericViewSet):
         type=openapi.TYPE_STRING,
     )
 
+    show_deleted_param = openapi.Parameter(
+        name=SHOW_DELETED,
+        in_=openapi.IN_QUERY,
+        required=False,
+        description="Include deleted groups",
+        type=openapi.TYPE_BOOLEAN,
+        default=False,
+    )
+
     @swagger_auto_schema(
         responses={
             200: f"List of groups for the given '{APP_ID}'.",
             400: f"Parameter '{APP_ID}' is required.",
             404: f"Project for given '{APP_ID}' doesn't exist.",
         },
-        manual_parameters=[app_id_param],
+        manual_parameters=[app_id_param, show_deleted_param],
     )
     def list(self, request: Request, *args, **kwargs) -> Response:
         return super().list(request, *args, **kwargs)
 
+    def get_serializer_context(self) -> Dict[str, Any]:
+        context = super().get_serializer_context()
+        return self.serializer_class.include_source_version(
+            context, self.get_project().account.default_version
+        )
+
+    def get_project(self):
+        app_id = AppIdSerializer(data=self.request.query_params).get_app_id(
+            raise_exception=True
+        )
+        project_qs = Project.objects.select_related(
+            "account__default_version__data_source"
+        )
+        return get_object_or_404(project_qs, app_id=app_id)
+
     def get_queryset(self) -> QuerySet:
-        app_id = AppIdSerializer(data=self.request.query_params).get_app_id(raise_exception=True)
-        project_qs = Project.objects.select_related("account__default_version")
-        project = get_object_or_404(project_qs, app_id=app_id)
-        return Group.objects.filter(source_version=project.account.default_version)
+        objects = Group.objects
+        if self.request.query_params.get(SHOW_DELETED) != "true":
+            objects = objects.filter(
+                source_version=self.get_project().account.default_version
+            )
+        else:
+            objects = objects.filter(
+                source_version__data_source=self.get_project().account.default_version.data_source
+            )
+        return objects

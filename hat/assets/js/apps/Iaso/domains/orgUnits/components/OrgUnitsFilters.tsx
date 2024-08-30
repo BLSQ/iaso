655181388cc42254de6a6df2153755be5e48bf8f
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2024-08-30 11:43:10 +0200

    Move filter projects to the right

--- hat/assets/js/apps/Iaso/domains/orgUnits/components/OrgUnitsFilters.tsx
+++ hat/assets/js/apps/Iaso/domains/orgUnits/components/OrgUnitsFilters.tsx
@@ -267,16 +267,6 @@ export const OrgUnitFilters: FunctionComponent<Props> = ({
                         }
                     />
                 </Box>
-                <InputComponent
-                    type="select"
-                    disabled={isFetchingProjects}
-                    keyValue="project"
-                    onChange={handleChange}
-                    value={!isFetchingProjects && projectId}
-                    label={MESSAGES.project}
-                    options={projects}
-                    loading={isFetchingProjects}
-                />
                 <InputWithInfos infos={formatMessage(MESSAGES.searchParams)}>
                     <InputComponent
                         keyValue="search"
@@ -289,6 +279,16 @@ export const OrgUnitFilters: FunctionComponent<Props> = ({
                         onErrorChange={hasError => setTextSearchError(hasError)}
                     />
                 </InputWithInfos>
+                <InputComponent
+                    type="select"
+                    disabled={isFetchingProjects}
+                    keyValue="project"
+                    onChange={handleChange}
+                    value={!isFetchingProjects && projectId}
+                    label={MESSAGES.project}
+                    options={projects}
+                    loading={isFetchingProjects}
+                />
                 <InputComponent
                     type="select"
                     disabled={isFetchingDataSources}

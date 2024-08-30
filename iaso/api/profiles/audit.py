6f1853a43ca3b03d04bf6f0ee2ba6d32786d26ea
Quang Son Le <qle@bluesquarehub.com>
2024-08-30 17:58:51 +0200

    IA-3365: log whether password has been updated

--- iaso/api/profiles/audit.py
+++ iaso/api/profiles/audit.py
@@ -18,14 +18,19 @@ class NestedUserRoleSerializer(serializers.ModelSerializer):
 
 class NestedUserAuditSerializer(serializers.ModelSerializer):
     user_permissions = serializers.SerializerMethodField()
+    # password_updated = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ["id", "username", "first_name", "last_name", "email", "user_permissions"]
+        # We're serializing the password to be able to determine if it has been changed. but we remove them before saving the log itself
+        # DO NOT reuse this serializer if you're going to send the payload to the front-end
+        fields = ["id", "username", "first_name", "last_name", "email", "user_permissions", "password"]
 
-    # TODO optimize DB queries
     def get_user_permissions(self, user):
         return [permission.codename for permission in user.user_permissions.all()]
+
+    # def get_password_updated(self,user):
+    #     return user.password is not None
 
 
 class ProfileAuditSerializer(serializers.ModelSerializer):
@@ -46,19 +51,29 @@ class ProfileAuditLogger(AuditLogger):
         source = source if source else self.default_source
         if not old_data_dump:
             old_data_dump = []
+        new_value = self.serialize_instance(instance)
+        password_updated = (
+            old_data_dump[0]["user"]["password"] != new_value[0]["user"]["password"] if old_data_dump else True
+        )
+        if old_data_dump:
+            del old_data_dump[0]["user"]["password"]
+        del new_value[0]["user"]["password"]
+        new_value[0]["user"]["password_updated"] = password_updated
         Modification.objects.create(
             user=request_user,
             past_value=old_data_dump,
-            new_value=self.serialize_instance(instance),
+            new_value=new_value,
             content_object=instance,
             source=source,
         )
 
     def log_hard_deletion(self, instance, request_user, source=None):
         source = source if source else self.default_source
+        past_value = self.serialize_instance(instance)
+        del past_value[0]["user"]["password"]
         Modification.objects.create(
             user=request_user,
-            past_value=self.serialize_instance(instance),
+            past_value=past_value,
             new_value=[],
             content_object=instance,
             source=source,

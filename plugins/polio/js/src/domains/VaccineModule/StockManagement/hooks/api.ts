7df95c72bf33f9c21921bdc686beba456b3d5069
Math VDH <mathvdh@gmail.com>
2024-09-06 10:06:34 +0100

    POLIO-1653 Fix for sorting not working on API for Vaccine Stock Management (#1608)
    * POLIO-1653 FIx for sorting not working on API
    
    * POLIO-1653 Fix frontend
    
    * POLIO-1653: fix default sort in stock variation
    
    - Pass default values to useUrlParams
    
    * POLIO-1653 adding tests
    
    ---------
    
    Co-authored-by: Quang Son Le <qle@bluesquarehub.com>

--- plugins/polio/js/src/domains/VaccineModule/StockManagement/hooks/api.ts
+++ plugins/polio/js/src/domains/VaccineModule/StockManagement/hooks/api.ts
@@ -164,12 +164,20 @@ export const useGetFormAList = (
         formaPageSize: pageSize,
         id: vaccine_stock,
     } = params;
-    const safeParams = useUrlParams({
-        order,
-        page,
-        pageSize,
-        vaccine_stock,
-    } as Partial<UrlParams>);
+
+    const safeParams = useUrlParams(
+        {
+            order,
+            page,
+            pageSize,
+            vaccine_stock,
+        } as Partial<UrlParams>,
+        {
+            order: '-form_a_reception_date',
+            pageSize: 20,
+        },
+    );
+
     const apiParams = useApiParams(safeParams);
     const queryString = new URLSearchParams(apiParams).toString();
     return useSnackQuery({
@@ -192,12 +200,18 @@ export const useGetDestructionList = (
         destructionPageSize: pageSize,
         id: vaccine_stock,
     } = params;
-    const safeParams = useUrlParams({
-        order,
-        page,
-        pageSize,
-        vaccine_stock,
-    } as Partial<UrlParams>);
+    const safeParams = useUrlParams(
+        {
+            order,
+            page,
+            pageSize,
+            vaccine_stock,
+        } as Partial<UrlParams>,
+        {
+            order: '-rrt_destruction_report_reception_date',
+            pageSize: 20,
+        },
+    );
     const apiParams = useApiParams(safeParams);
     const queryString = new URLSearchParams(apiParams).toString();
     return useSnackQuery({
@@ -220,12 +234,18 @@ export const useGetIncidentList = (
         incidentPageSize: pageSize,
         id: vaccine_stock,
     } = params;
-    const safeParams = useUrlParams({
-        order,
-        page,
-        pageSize,
-        vaccine_stock,
-    } as Partial<UrlParams>);
+    const safeParams = useUrlParams(
+        {
+            order,
+            page,
+            pageSize,
+            vaccine_stock,
+        } as Partial<UrlParams>,
+        {
+            order: '-incident_report_received_by_rrt',
+            pageSize: 20,
+        },
+    );
     const apiParams = useApiParams(safeParams);
     const queryString = new URLSearchParams(apiParams).toString();
     return useSnackQuery({

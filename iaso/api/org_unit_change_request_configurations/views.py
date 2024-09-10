98cfbf319514e27799c6afbc2f5078c5d49465ec
Thibault Dethier <tdethier@bluesquarehub.com>
2024-09-10 16:24:15 +0200

    [IA-3386] Added check_availability endpoint

--- iaso/api/org_unit_change_request_configurations/views.py
+++ iaso/api/org_unit_change_request_configurations/views.py
@@ -1,17 +1,22 @@
 import django_filters
-from rest_framework import viewsets, filters
+from django.db.models import Q
+from rest_framework import viewsets, filters, status, serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+from hat.audit.audit_mixin import AuditMixin
 from iaso.api.org_unit_change_request_configurations.pagination import OrgUnitChangeRequestConfigurationPagination
 from iaso.api.org_unit_change_request_configurations.serializers import (
     OrgUnitChangeRequestConfigurationListSerializer,
     OrgUnitChangeRequestConfigurationRetrieveSerializer,
     OrgUnitChangeRequestConfigurationWriteSerializer,
     OrgUnitChangeRequestConfigurationUpdateSerializer,
+    OrgUnitTypeNestedSerializer,
 )
-from iaso.models import OrgUnitChangeRequestConfiguration
+from iaso.models import OrgUnitChangeRequestConfiguration, OrgUnitType, Project
 
 
-class OrgUnitChangeRequestConfigurationViewSet(viewsets.ModelViewSet):
+class OrgUnitChangeRequestConfigurationViewSet(viewsets.ModelViewSet, AuditMixin):
     filter_backends = [filters.OrderingFilter, django_filters.rest_framework.DjangoFilterBackend]
     ordering_fields = [
         "id",
@@ -51,3 +56,32 @@ class OrgUnitChangeRequestConfigurationViewSet(viewsets.ModelViewSet):
             return OrgUnitChangeRequestConfigurationRetrieveSerializer
         if self.action == "partial_update":
             return OrgUnitChangeRequestConfigurationUpdateSerializer
+
+    @action(detail=False)
+    def check_availability(self, request, *args, **kwargs):
+        user = request.user
+        if user and user.is_anonymous:
+            raise serializers.ValidationError("You must be logged in")
+
+        # Not checking if this is indeed a number -> with django filters?
+        project_id = self.request.query_params.get("project_id")
+        if not project_id:
+            raise serializers.ValidationError("Parameter project_id is missing")
+
+        if not Project.objects.filter(id=project_id).exists():
+            raise serializers.ValidationError("This project does not exist")
+
+        # It seems there is currently no constraints on projects, but it will happen in the near future
+        # user_projects = user.iaso_profile.projects.all()
+        # if user_projects and project_id not in user_projects:
+        #     raise serializers.ValidationError("You don't have access to this project")
+
+        org_unit_types_in_configs = OrgUnitChangeRequestConfiguration.objects.filter(project_id=project_id).values_list(
+            "org_unit_type_id", flat=True
+        )
+        available_org_unit_types = OrgUnitType.objects.filter(
+            Q(projects__id=project_id) & ~Q(id__in=org_unit_types_in_configs)
+        ).order_by("id")
+
+        serializer = OrgUnitTypeNestedSerializer(available_org_unit_types, many=True)
+        return Response(serializer.data)

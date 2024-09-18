ad1d33df655f6a62a2e6051e27d95f216419b113
Stéphan Mestach <smestach@bluesquare.org>
2024-09-18 13:04:06 +0200

    fix review comments

--- iaso/management/commands/delete_project.py
+++ iaso/management/commands/delete_project.py
@@ -11,10 +11,7 @@ class Command(BaseCommand):
         parser.add_argument("--account-to-keep", type=int)
         parser.add_argument("--project-app-id-to-delete", type=str)
 
-    def handle(self, *args, **options):
-        print("delete Project")
-
-        account_id_to_keep = options.get("account_to_keep")
+    def _get_account(self, account_id_to_keep):
         if account_id_to_keep is None:
             for account in Account.objects.order_by("id").all():
                 print(account.id, account.name)
@@ -24,14 +21,16 @@ class Command(BaseCommand):
         account_to_keep = Account.objects.get(pk=account_id_to_keep)
         print("*****")
         print("keeping", account_id_to_keep, account_to_keep)
+        return account_to_keep
 
-        project_app_id_to_delete = options.get("project_app_id_to_delete")
+    def _get_project(self, account_to_keep, project_app_id_to_delete):
+        for project in account_to_keep.project_set.order_by("id").all():
+            print(project.id, "\t", project.name, "\t", project.app_id)
 
-        for project in account.project_set.order_by("id").all():
-            print(project.id, project.name, project.app_id)
+        project_to_delete = account_to_keep.project_set.get(app_id=project_app_id_to_delete)
+        return project_to_delete
 
-        project = account_to_keep.project_set.get(app_id=project_app_id_to_delete)
-
+    def _delete_project(self, project):
         print("deleting vector_control_apiimport")
         Form.objects.raw(
             f"delete vector_control_apiimport where headers->>'QUERY_STRING' like 'app_id={project.app_id}%'"
@@ -43,7 +42,10 @@ class Command(BaseCommand):
             InstanceFile.objects.filter(instance__in=Instance.objects.filter(project=project)).delete(),
         )
         print("Instance delete", Instance.objects.filter(project=project).delete())
-        print("InstanceFile delete", InstanceFile.objects.filter(instance__form__in=forms).delete())
+        print(
+            "InstanceFile delete",
+            InstanceFile.objects.filter(instance__form__in=forms).delete(),
+        )
         print("Instance delete", Instance.objects.filter(form__in=forms).delete())
         print("Mapping", Mapping.objects.filter(form__in=forms).delete())
         print("Forms", [f.name for f in forms])
@@ -58,3 +60,9 @@ class Command(BaseCommand):
                 )
 
         print("project", project.delete())
+
+    def handle(self, *args, **options):
+        print("delete Project")
+        account_to_keep = self._get_account(options.get("account_to_keep"))
+        project = self._get_project(account_to_keep, options.get("project_app_id_to_delete"))
+        self._delete_project(project)

cbab0c0f3f33999135e416b77fa0bba95ec0b6f3
Christophe Gerard <c@eduz.be>
2024-09-19 14:38:25 +0200

    make it pretty

--- hat/assets/js/apps/Iaso/components/nav/AccountSwitch.tsx
+++ hat/assets/js/apps/Iaso/components/nav/AccountSwitch.tsx
@@ -1,16 +1,16 @@
-import React, { FunctionComponent, useState } from 'react';
-
-import { Menu, MenuItem, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import {
+    ClickAwayListener,
+    Grow,
+    MenuItem,
+    MenuList,
+    Paper,
+    Popper,
+    Typography,
+} from '@mui/material';
+import React, { FunctionComponent, useEffect, useRef, useState } from 'react';
 
 import { useSwitchAccount } from '../../hooks/useSwitchAccount';
 import { useCurrentUser } from '../../utils/usersUtils';
-
-const useStyles = makeStyles(theme => ({
-    accountSwitchButton: {
-        padding: theme.spacing(0),
-    },
-}));
 
 type Props = {
     color?: 'inherit' | 'primary' | 'secondary';
@@ -20,52 +20,128 @@ export const AccountSwitch: FunctionComponent<Props> = ({
     color = 'inherit',
 }) => {
     const currentUser = useCurrentUser();
-    const classes = useStyles();
 
-    const [anchorEl, setAnchorEl] = useState(null);
-    const handleClickListItem = event => {
-        setAnchorEl(event.currentTarget);
-    };
+    const [open, setOpen] = useState(false);
+    const anchorRef = useRef<HTMLDivElement>(null);
 
     const { mutateAsync: switchAccount } = useSwitchAccount(() => {
-        setAnchorEl(null);
+        setOpen(false);
         window.location.href = '/';
     });
 
-    const handleClose = () => {
-        setAnchorEl(null);
+    const handleToggle = () => {
+        setOpen(prevOpen => !prevOpen);
     };
 
-    if (currentUser.other_accounts.length > 0) {
+    const handleClose = (event: Event | React.SyntheticEvent) => {
+        if (
+            anchorRef.current &&
+            anchorRef.current.contains(event.target as HTMLElement)
+        ) {
+            return;
+        }
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event: React.KeyboardEvent) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        } else if (event.key === 'Escape') {
+            setOpen(false);
+        }
+    }
+
+    // Return focus to the button when we transitioned from !open -> open
+    const prevOpen = useRef(open);
+    useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current?.focus();
+        }
+        prevOpen.current = open;
+    }, [open]);
+    const menuListKeyDownHandler = React.useCallback(handleListKeyDown, []);
+
+    if (currentUser.other_accounts.length === 0) {
         return (
-            <>
-                <Typography
-                    variant="body2"
-                    color={color}
-                    onClick={handleClickListItem}
-                    className={classes.accountSwitchButton}
-                >
-                    {currentUser.account.name}
-                </Typography>
-                <Menu
-                    id="lock-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                >
-                    {currentUser.other_accounts.map(account => (
-                        <MenuItem
-                            key={account.id}
-                            selected={account.id === currentUser.account.id}
-                            onClick={() => switchAccount(account.id)}
-                        >
-                            {account.name}
-                        </MenuItem>
-                    ))}
-                </Menu>
-            </>
+            <Typography
+                variant="body2"
+                color={color}
+                sx={{
+                    padding: theme => theme.spacing(0),
+                    fontSize: 16,
+                }}
+            >
+                {currentUser.account.name}
+            </Typography>
         );
     }
-    return null;
+
+    return (
+        <>
+            <Typography
+                ref={anchorRef}
+                variant="body2"
+                color={color}
+                onClick={handleToggle}
+                sx={{
+                    padding: theme => theme.spacing(0),
+                    cursor: 'pointer',
+                    fontSize: 16,
+                    '&:hover': {
+                        color: theme => theme.palette.secondary.main,
+                    },
+                }}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-expanded={open ? 'true' : undefined}
+                aria-haspopup="true"
+            >
+                {currentUser.account.name}
+            </Typography>
+            <Popper
+                open={open}
+                anchorEl={anchorRef.current}
+                role={undefined}
+                placement="bottom-end"
+                transition
+                disablePortal
+            >
+                {({ TransitionProps }) => (
+                    <Grow
+                        // eslint-disable-next-line react/jsx-props-no-spreading
+                        {...TransitionProps}
+                        style={{
+                            transformOrigin: 'right bottom',
+                        }}
+                    >
+                        <Paper>
+                            <ClickAwayListener onClickAway={handleClose}>
+                                <MenuList
+                                    autoFocusItem={open}
+                                    id="account-menu"
+                                    aria-labelledby="account-button"
+                                    onKeyDown={menuListKeyDownHandler}
+                                >
+                                    {currentUser.other_accounts.map(account => (
+                                        <MenuItem
+                                            key={account.id}
+                                            selected={
+                                                account.id ===
+                                                currentUser.account.id
+                                            }
+                                            onClick={() =>
+                                                switchAccount(account.id)
+                                            }
+                                        >
+                                            {account.name}
+                                        </MenuItem>
+                                    ))}
+                                </MenuList>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                )}
+            </Popper>
+        </>
+    );
 };

8cce31fe21d39d14c948150b275fb39199162b06
Christophe Gerard <c@eduz.be>
2024-09-20 11:05:03 +0200

    fix user creation and missing iaso_profile

--- iaso/api/profiles/profiles.py
+++ iaso/api/profiles/profiles.py
@@ -1,13 +1,13 @@
 import copy
 from typing import Any, List, Optional, Union
+
 from django.conf import settings
 from django.contrib.auth import login, models, update_session_auth_hash
 from django.contrib.auth.models import Permission, User
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import BadRequest, ObjectDoesNotExist
 from django.core.mail import send_mail
 from django.core.paginator import Paginator
-from django.core.exceptions import BadRequest
 from django.db.models import Q, QuerySet
 from django.http import HttpResponse, JsonResponse, StreamingHttpResponse
 from django.shortcuts import get_object_or_404
@@ -16,19 +16,20 @@ from django.urls import reverse
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.utils.translation import gettext as _
-from hat.audit.models import PROFILE_API
-from iaso.api.profiles.audit import ProfileAuditLogger
-from iaso.utils import is_mobile_request
 from phonenumber_field.phonenumber import PhoneNumber
 from rest_framework import permissions, status, viewsets
 from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.response import Response
+
 from hat.api.export_utils import Echo, generate_xlsx, iter_items
+from hat.audit.models import PROFILE_API
 from hat.menupermissions import models as permission
 from hat.menupermissions.models import CustomPermissionSupport
-from iaso.api.profiles.bulk_create_users import BULK_CREATE_USER_COLUMNS_LIST
 from iaso.api.common import CONTENT_TYPE_CSV, CONTENT_TYPE_XLSX, FileFormatEnum
+from iaso.api.profiles.audit import ProfileAuditLogger
+from iaso.api.profiles.bulk_create_users import BULK_CREATE_USER_COLUMNS_LIST
 from iaso.models import OrgUnit, Profile, Project, TenantUser, UserRole
+from iaso.utils import is_mobile_request
 from iaso.utils.module_permissions import account_module_permissions
 
 PK_ME = "me"
@@ -653,40 +654,46 @@ class ProfilesViewSet(viewsets.ViewSet):
 
         if not username:
             return JsonResponse({"errorKey": "user_name", "errorMessage": _("Nom d'utilisateur requis")}, status=400)
+        else:
+            existing_user = User.objects.filter(username__iexact=username)
+            if existing_user:
+                return JsonResponse(
+                    {"errorKey": "user_name", "errorMessage": _("Nom d'utilisateur existant")}, status=400
+                )
         if not password and not send_email_invitation:
             return JsonResponse({"errorKey": "password", "errorMessage": _("Mot de passe requis")}, status=400)
-
+        main_user = None
         try:
             existing_user = User.objects.get(username__iexact=username)
-            main_user = None
-            if existing_user:
-                user_in_same_account = (
-                    existing_user.iaso_profile and existing_user.iaso_profile.account == current_account
+            user_in_same_account = False
+            try:
+                if existing_user.iaso_profile and existing_user.iaso_profile.account == current_account:
+                    user_in_same_account = True
+            except User.iaso_profile.RelatedObjectDoesNotExist:
+                # User doesn't have an iaso_profile, so they're not in the same account
+                pass
+
+            if user_in_same_account:
+                return JsonResponse(
+                    {"errorKey": "user_name", "errorMessage": _("Nom d'utilisateur existant")}, status=400
                 )
-                if user_in_same_account:
-                    return JsonResponse(
-                        {"errorKey": "user_name", "errorMessage": _("Nom d'utilisateur existant")}, status=400
-                    )
-                else:
-                    # TODO: invitation
-                    # TODO what if no iaso_profile?
-                    # TODO what if already main user?
-                    old_username = username
-                    username = f"{username}_{current_account.name.lower().replace(' ', '_')}"
+            else:
+                # TODO: invitation
+                # TODO what if already main user?
+                old_username = username
+                username = f"{username}_{current_account.name.lower().replace(' ', '_')}"
 
-                    # duplicate existing_user into main user and account user
-                    main_user = copy.copy(existing_user)
+                # duplicate existing_user into main user and account user
+                main_user = copy.copy(existing_user)
 
-                    existing_user.username = (
-                        f"{old_username}_{existing_user.iaso_profile.account.name.lower().replace(' ', '_')}"
-                    )
-                    existing_user.set_unusable_password()
-                    existing_user.save()
+                existing_user.username = f"{old_username}_{'unknown' if not hasattr(existing_user, 'iaso_profile') else existing_user.iaso_profile.account.name.lower().replace(' ', '_')}"
+                existing_user.set_unusable_password()
+                existing_user.save()
 
-                    main_user.pk = None
-                    main_user.save()
+                main_user.pk = None
+                main_user.save()
 
-                    TenantUser.objects.create(main_user=main_user, account_user=existing_user)
+                TenantUser.objects.create(main_user=main_user, account_user=existing_user)
         except User.DoesNotExist:
             pass  # no existing user, simply create a new user
 

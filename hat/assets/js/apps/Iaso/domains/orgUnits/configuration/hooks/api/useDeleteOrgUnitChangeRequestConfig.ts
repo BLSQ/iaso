fff00209e5078a678be3a16c52e2f92308e419ce
Christophe Gerard <c@eduz.be>
2024-09-26 14:49:25 +0200

    fix delete

--- hat/assets/js/apps/Iaso/domains/orgUnits/configuration/hooks/api/useDeleteOrgUnitChangeRequestConfig.ts
+++ hat/assets/js/apps/Iaso/domains/orgUnits/configuration/hooks/api/useDeleteOrgUnitChangeRequestConfig.ts
@@ -1,14 +1,14 @@
 import { UseMutationResult } from 'react-query';
 import { useSnackMutation } from '../../../../../libs/apiHooks';
 
+import { deleteRequest } from '../../../../../libs/Api';
 import { apiUrlOUCRC } from '../../constants';
 import { OrgUnitChangeRequestConfigurationFull } from '../../types';
-import { deleteRequest } from '../../../../../libs/Api';
 
 const deleteOrgUnitChangeRequestConfigs = (
     config: OrgUnitChangeRequestConfigurationFull,
 ) => {
-    return deleteRequest(`${apiUrlOUCRC}/${config.id}/`) as Promise<boolean>;
+    return deleteRequest(`${apiUrlOUCRC}${config.id}/`) as Promise<boolean>;
 };
 
 export const useDeleteOrgUnitChangeRequestConfig = (): UseMutationResult =>

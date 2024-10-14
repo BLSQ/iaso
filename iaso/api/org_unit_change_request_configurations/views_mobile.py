5c33fedef2e9927fc54265819f30d07bc6d7e6d7
kemar <marc@marcarea.com>
2024-10-14 16:17:40 +0200

    Handle `Profile.editable_org_unit_types` in the mobile org unit change request configuration API

--- iaso/api/org_unit_change_request_configurations/views_mobile.py
+++ iaso/api/org_unit_change_request_configurations/views_mobile.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 import django_filters
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
@@ -17,7 +19,7 @@ from iaso.api.org_unit_change_request_configurations.serializers import (
 )
 from iaso.api.query_params import APP_ID
 from iaso.api.serializers import AppIdSerializer
-from iaso.models import OrgUnitChangeRequestConfiguration
+from iaso.models import OrgUnitChangeRequestConfiguration, Project
 
 
 class MobileOrgUnitChangeRequestConfigurationViewSet(ListModelMixin, viewsets.GenericViewSet):
@@ -35,14 +37,56 @@ class MobileOrgUnitChangeRequestConfigurationViewSet(ListModelMixin, viewsets.Ge
     )
 
     def get_queryset(self):
+        """
+        Because some Org Unit Type restrictions are also configurable at the `Profile` level,
+        we implement the following logic here:
+
+        1. If `Profile.editable_org_unit_types` empty:
+
+            - return `OrgUnitChangeRequestConfiguration`
+
+        2. If `Profile.editable_org_unit_types` not empty:
+
+            2a. for org_unit_type not in `Profile.editable_org_unit_types`:
+                - return a dynamic configuration that says `org_units_editable: False`
+                - regardless of any existing `OrgUnitChangeRequestConfiguration`
+
+            2b. for org_unit_type in `Profile.editable_org_unit_types`:
+                - return either the existing `OrgUnitChangeRequestConfiguration` or nothing
+
+        """
         app_id = AppIdSerializer(data=self.request.query_params).get_app_id(raise_exception=True)
-        return (
+
+        org_unit_change_request_configurations = (
             OrgUnitChangeRequestConfiguration.objects.filter(project__app_id=app_id)
             .select_related("org_unit_type")
             .prefetch_related(
                 "possible_types", "possible_parent_types", "group_sets", "editable_reference_forms", "other_groups"
             )
-            .order_by("id")
+            .order_by("org_unit_type_id")
+        )
+
+        user_editable_org_unit_type_ids = set(
+            self.request.user.iaso_profile.editable_org_unit_types.values_list("id", flat=True)
+        )
+
+        if not user_editable_org_unit_type_ids:
+            return org_unit_change_request_configurations
+
+        project_org_unit_types = set(Project.objects.get(app_id=app_id).unit_types.values_list("id", flat=True))
+
+        non_editable_org_unit_type_ids = project_org_unit_types - user_editable_org_unit_type_ids
+
+        dynamic_configurations = [
+            OrgUnitChangeRequestConfiguration(org_unit_type_id=org_unit_type_id, org_units_editable=False)
+            for org_unit_type_id in non_editable_org_unit_type_ids
+        ]
+
+        return list(
+            chain(
+                org_unit_change_request_configurations.exclude(org_unit_type__in=non_editable_org_unit_type_ids),
+                dynamic_configurations,
+            )
         )
 
     @swagger_auto_schema(manual_parameters=[app_id_param])
--- iaso/tests/api/org_unit_change_request_configurations/test_org_unit_change_request_configs_mobile.py
+++ iaso/tests/api/org_unit_change_request_configurations/test_org_unit_change_request_configs_mobile.py
@@ -1,5 +1,8 @@
+import json
+
 from rest_framework import status
 
+from iaso import models as m
 from iaso.tests.api.org_unit_change_request_configurations.common_base_with_setup import OUCRCAPIBase
 
 
@@ -12,18 +15,120 @@ class MobileOrgUnitChangeRequestConfigurationAPITestCase(OUCRCAPIBase):
 
     def test_list_ok(self):
         self.client.force_authenticate(self.user_ash_ketchum)
-        with self.assertNumQueries(7):
+        with self.assertNumQueries(8):
             # get_queryset
-            #   1. COUNT(*) OrgUnitChangeRequestConfiguration
-            #   2. SELECT OrgUnitChangeRequestConfiguration
-            #   3. PREFETCH OrgUnitChangeRequestConfiguration.possible_types
-            #   4. PREFETCH OrgUnitChangeRequestConfiguration.possible_parent_types
-            #   5. PREFETCH OrgUnitChangeRequestConfiguration.group_sets
-            #   6. PREFETCH OrgUnitChangeRequestConfiguration.editable_reference_forms
-            #   7. PREFETCH OrgUnitChangeRequestConfiguration.other_groups
+            #   1. SELECT user_editable_org_unit_type_ids
+            #   2. COUNT(*) OrgUnitChangeRequestConfiguration
+            #   3. SELECT OrgUnitChangeRequestConfiguration
+            #   4. PREFETCH OrgUnitChangeRequestConfiguration.possible_types
+            #   5. PREFETCH OrgUnitChangeRequestConfiguration.possible_parent_types
+            #   6. PREFETCH OrgUnitChangeRequestConfiguration.group_sets
+            #   7. PREFETCH OrgUnitChangeRequestConfiguration.editable_reference_forms
+            #   8. PREFETCH OrgUnitChangeRequestConfiguration.other_groups
             response = self.client.get(f"{self.MOBILE_OUCRC_API_URL}?app_id={self.app_id}")
             self.assertJSONResponse(response, status.HTTP_200_OK)
             self.assertEqual(3, len(response.data["results"]))  # the 3 OUCRCs from setup
+
+    def test_list_ok_with_restricted_write_permission_for_user(self):
+        # Add new Org Unit Types.
+        new_org_unit_type_1 = m.OrgUnitType.objects.create(name="Hospital")
+        new_org_unit_type_1.projects.add(self.project_johto)
+        new_org_unit_type_2 = m.OrgUnitType.objects.create(name="Health facility")
+        new_org_unit_type_2.projects.add(self.project_johto)
+        new_org_unit_type_3 = m.OrgUnitType.objects.create(name="District")
+        new_org_unit_type_3.projects.add(self.project_johto)
+        self.assertEqual(self.project_johto.unit_types.count(), 6)
+
+        self.user_ash_ketchum.iaso_profile.editable_org_unit_types.set(
+            # Only org units of this type are now writable for this user.
+            [self.ou_type_fire_pokemons, new_org_unit_type_3]
+        )
+
+        self.client.force_authenticate(self.user_ash_ketchum)
+
+        response = self.client.get(f"{self.MOBILE_OUCRC_API_URL}?app_id={self.app_id}")
+        self.assertJSONResponse(response, status.HTTP_200_OK)
+        results = response.data["results"]
+
+        # The user has write access on `new_org_unit_type_3` at his Profile level
+        # and there is no existing configuration for `new_org_unit_type_3`, so this
+        # Org Unit Type should not be in the response meaning the user has full
+        # write perms on this type.
+        new_org_unit_type_3_config = next(
+            (config for config in results if config["org_unit_type_id"] == new_org_unit_type_3.pk), None
+        )
+        self.assertEqual(new_org_unit_type_3_config, None)
+
+        self.assertEqual(5, len(results))  # 3 OUCRCs from setup + 2 dynamic OUCRCs.
+        self.assertEqual(
+            json.loads(json.dumps(response.data["results"])),
+            [
+                {
+                    "created_at": self.oucrc_type_fire.created_at.timestamp(),
+                    "editable_fields": ["name", "aliases", "location", "opening_date", "closing_date"],
+                    "editable_reference_form_ids": list(
+                        self.oucrc_type_fire.editable_reference_forms.values_list("id", flat=True)
+                    ),
+                    "group_set_ids": list(self.oucrc_type_fire.group_sets.values_list("id", flat=True)),
+                    "org_unit_type_id": self.ou_type_fire_pokemons.pk,
+                    "org_units_editable": True,
+                    "other_group_ids": list(self.oucrc_type_fire.other_groups.values_list("id", flat=True)),
+                    "possible_parent_type_ids": list(
+                        self.oucrc_type_fire.possible_parent_types.values_list("id", flat=True)
+                    ),
+                    "possible_type_ids": list(self.oucrc_type_fire.possible_types.values_list("id", flat=True)),
+                    "updated_at": self.oucrc_type_fire.updated_at.timestamp(),
+                },
+                {
+                    "created_at": None,
+                    "editable_fields": [],
+                    "editable_reference_form_ids": [],
+                    "group_set_ids": [],
+                    "org_unit_type_id": self.ou_type_rock_pokemons.pk,
+                    "org_units_editable": False,
+                    "other_group_ids": [],
+                    "possible_parent_type_ids": [],
+                    "possible_type_ids": [],
+                    "updated_at": None,
+                },
+                {
+                    "created_at": None,
+                    "editable_fields": [],
+                    "editable_reference_form_ids": [],
+                    "group_set_ids": [],
+                    "org_unit_type_id": self.ou_type_water_pokemons.pk,
+                    "org_units_editable": False,
+                    "other_group_ids": [],
+                    "possible_parent_type_ids": [],
+                    "possible_type_ids": [],
+                    "updated_at": None,
+                },
+                {
+                    "created_at": None,
+                    "editable_fields": [],
+                    "editable_reference_form_ids": [],
+                    "group_set_ids": [],
+                    "org_unit_type_id": new_org_unit_type_1.id,
+                    "org_units_editable": False,
+                    "other_group_ids": [],
+                    "possible_parent_type_ids": [],
+                    "possible_type_ids": [],
+                    "updated_at": None,
+                },
+                {
+                    "created_at": None,
+                    "editable_fields": [],
+                    "editable_reference_form_ids": [],
+                    "group_set_ids": [],
+                    "org_unit_type_id": new_org_unit_type_2.id,
+                    "org_units_editable": False,
+                    "other_group_ids": [],
+                    "possible_parent_type_ids": [],
+                    "possible_type_ids": [],
+                    "updated_at": None,
+                },
+            ],
+        )
 
     def test_list_without_auth(self):
         response = self.client.get(f"{self.MOBILE_OUCRC_API_URL}?app_id={self.app_id}")

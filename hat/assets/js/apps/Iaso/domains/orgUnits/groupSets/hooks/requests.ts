5cdd6ff35306798118b793d6c975641393fcb07d
Christophe Gerard <c@eduz.be>
2024-10-15 12:25:23 +0200

    IA-3558: align queryKey to reload dropdown values

--- hat/assets/js/apps/Iaso/domains/orgUnits/groupSets/hooks/requests.ts
+++ hat/assets/js/apps/Iaso/domains/orgUnits/groupSets/hooks/requests.ts
@@ -43,7 +43,7 @@ export const useGetGroupSets = params => {
         newParams as Record<string, string>,
     );
     return useSnackQuery(
-        ['group_sets', searchParams.toString()],
+        ['groupSets', searchParams.toString()],
         () =>
             getRequest(
                 `/api/group_sets/?${searchParams.toString()}&fields=id,name,groups,created_at,updated_at`,
@@ -58,7 +58,7 @@ export const useGetGroupSets = params => {
 
 export const useGetGroupSet = groupSetId => {
     return useSnackQuery(
-        ['group_sets', groupSetId],
+        ['groupSet', groupSetId],
         () => {
             // if create
             if (groupSetId === 'new') {
@@ -80,7 +80,7 @@ export const useSaveGroupSet = (): UseMutationResult<any, any, any, any> =>
         },
         undefined,
         undefined,
-        ['group_sets'],
+        ['groupSets', 'groupSet'],
     );
 
 export const useDeleteGroupSet = () =>
@@ -88,7 +88,7 @@ export const useDeleteGroupSet = () =>
         body => deleteRequest(`/api/group_sets/${body.id}/`),
         MESSAGES.deleteSuccess,
         MESSAGES.deleteError,
-        ['group_sets'],
+        ['groupSets', 'groupSet'],
     );
 
 const mapChoices = choices =>

6c4358c3bc6356f72bd721c9e4c550fce9ed6eb4
Bram Jans <ik@bramjans.com>
2024-10-15 14:08:22 +0200

    IA-3516 Better error-handling and tests

--- iaso/serializers.py
+++ iaso/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
 
 from iaso.models import Project
 
@@ -8,24 +9,33 @@ from iaso.models import Project
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         """
-        Override this method to be able to take into account the app_id query param.
+        Override this method to be able to handle multi-account users.
 
         If the user is a multi-account user, we return a token for the correct
-        "account user" (based on the `app_id`) instead of the main user that was
-        used for logging in.
+        "account user" (based on the `app_id` query params) instead of the
+        main user that was used for logging in.
         """
         data = super().validate(attrs)
+
+        err_msg = "No active account found with the given credentials"
 
         if self.user.tenant_users.exists():
             request = self.context.get("request")
             if request:
                 app_id = request.query_params.get("app_id", None)
-                project = Project.objects.get(app_id=app_id)
+
+                try:
+                    project = Project.objects.get(app_id=app_id)
+                except Project.DoesNotExist:
+                    raise AuthenticationFailed(err_msg)
 
                 account_user = User.objects.filter(
                     tenant_user__main_user=self.user,
                     iaso_profile__account=project.account,
                 ).first()
+
+                if account_user is None:
+                    raise AuthenticationFailed(err_msg)
 
                 refresh = self.get_token(account_user)
 

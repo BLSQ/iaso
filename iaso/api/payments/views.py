a4e60733d3b6ed0a476ff7290991cd0a67a7b63e
Quang Son Le <qle@bluesquarehub.com>
2024-10-31 17:14:47 +0100

    IA-3607: filter payments by account

--- iaso/api/payments/views.py
+++ iaso/api/payments/views.py
@@ -100,6 +100,8 @@ class PaymentLotsViewSet(ModelViewSet):
     def get_queryset(self):
         # Filter out PaymentLot with task because they're still being created by the worker
         # queryset = PaymentLot.objects.filter(task__isnull=True)
+        payments = Payment.objects.filter(user__iaso_profile__account=self.request.user.iaso_profile.account).values_list("payment_lot_id",flat=True).distinct()
+        queryset = PaymentLot.objects.filter(id__in=payments)
 
         change_requests_prefetch = Prefetch(
             "payments__change_requests",

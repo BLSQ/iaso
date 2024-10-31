8b065eafc88ba0575aefc73afa6e5510ad7afb23
Quang Son Le <qle@bluesquarehub.com>
2024-11-01 00:09:31 +0100

    IA-3609: prevent 403 on user save
    - If user has restricted user permission, don't send projects in PATCH request body

--- hat/assets/js/apps/Iaso/domains/users/hooks/useSaveProfile.js
+++ hat/assets/js/apps/Iaso/domains/users/hooks/useSaveProfile.js
@@ -1,13 +1,33 @@
+import { useCallback } from 'react';
 import { useSnackMutation } from 'Iaso/libs/apiHooks.ts';
 import { postRequest, patchRequest } from 'Iaso/libs/Api.ts';
+import { useCurrentUser } from '../../../utils/usersUtils';
+import { userHasPermission } from '../utils';
+import { USERS_ADMIN, USERS_MANAGEMENT } from '../../../utils/permissions';
 
-export const useSaveProfile = () =>
-    useSnackMutation(
-        body =>
-            body.id
-                ? patchRequest(`/api/profiles/${body.id}/`, body)
-                : postRequest('/api/profiles/', body),
-        undefined,
-        undefined,
-        ['profiles', 'usersHistoryList'],
+export const useSaveProfile = () => {
+    const currentUser = useCurrentUser();
+    const hasRestrictedPerm =
+        userHasPermission(USERS_MANAGEMENT, currentUser) &&
+        !userHasPermission(USERS_ADMIN, currentUser);
+
+    const saveProfile = useCallback(
+        body => {
+            if (!body.id) {
+                return postRequest('/api/profiles/', body);
+            }
+            if (!hasRestrictedPerm) {
+                return patchRequest(`/api/profiles/${body.id}/`, body);
+            }
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+            const { projects, ...toSend } = body;
+            return patchRequest(`/api/profiles/${body.id}/`, toSend);
+        },
+        [hasRestrictedPerm],
     );
+
+    return useSnackMutation(saveProfile, undefined, undefined, [
+        'profiles',
+        'usersHistoryList',
+    ]);
+};

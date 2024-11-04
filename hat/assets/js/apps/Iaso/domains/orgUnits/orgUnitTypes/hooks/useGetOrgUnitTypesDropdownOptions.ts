2dce32ae91d8c8045d8617f8372b12014161ca50
Quang Son Le <qle@bluesquarehub.com>
2024-11-04 12:15:20 +0100

    IA-3629: improve org unit type dropdown perf
    - Add new dropdown endpoint that doesn't call count() and only returns name, depth and id
    - Replace existing API calls to get dropdowns with the new one
    - Remove and replace duplicate useGetOrgUnitTypes dropdowns

--- hat/assets/js/apps/Iaso/domains/orgUnits/orgUnitTypes/hooks/useGetOrgUnitTypesDropdownOptions.ts
+++ hat/assets/js/apps/Iaso/domains/orgUnits/orgUnitTypes/hooks/useGetOrgUnitTypesDropdownOptions.ts
@@ -5,58 +5,9 @@ import { useSnackQuery } from '../../../../libs/apiHooks';
 
 import { DropdownOptions } from '../../../../types/utils';
 import { useCheckUserHasWriteTypePermission } from '../../../../utils/usersUtils';
-import { OrgunitTypes, OrgunitTypesApi } from '../../types/orgunitTypes';
+import { OrgunitTypes } from '../../types/orgunitTypes';
 
-const getOrgunitTypes = (projectId?: number): Promise<OrgunitTypesApi> => {
-    return getRequest(
-        projectId
-            ? `/api/v2/orgunittypes/?project=${projectId}`
-            : '/api/v2/orgunittypes/',
-    );
-};
-// @deprecated
-export const useGetOrgUnitTypesDropdownOptionsLegacy = (
-    projectId?: number,
-    onlyWriteAccess = false,
-): UseQueryResult<DropdownOptions<string>[], Error> => {
-    const queryKey: any[] = ['orgunittypes-dropdown-legacy', projectId];
-    const checkUserHasWriteTypePermission =
-        useCheckUserHasWriteTypePermission();
-    return useSnackQuery(
-        queryKey,
-        () => getOrgunitTypes(projectId),
-        undefined,
-        {
-            keepPreviousData: true,
-            staleTime: 1000 * 60 * 15, // in MS
-            cacheTime: 1000 * 60 * 5,
-            select: data => {
-                if (!data) return [];
-                let { orgUnitTypes } = data;
-                if (onlyWriteAccess) {
-                    orgUnitTypes = orgUnitTypes.filter(orgunitType =>
-                        checkUserHasWriteTypePermission(orgunitType.id),
-                    );
-                }
-                return orgUnitTypes
-                    .sort((orgunitType1, orgunitType2) => {
-                        const depth1 = orgunitType1.depth ?? 0;
-                        const depth2 = orgunitType2.depth ?? 0;
-                        return depth1 < depth2 ? -1 : 1;
-                    })
-                    .map(orgunitType => {
-                        return {
-                            value: orgunitType.id.toString(),
-                            label: orgunitType.name,
-                            original: orgunitType,
-                        };
-                    });
-            },
-        },
-    );
-};
-
-const getOrgunitTypesV3 = (projectId?: number): Promise<OrgunitTypes> => {
+const getOrgunitTypes = (projectId?: number): Promise<OrgunitTypes> => {
     return getRequest(
         projectId
             ? `/api/v2/orgunittypes/dropdown/?project=${projectId}`
@@ -73,7 +24,7 @@ export const useGetOrgUnitTypesDropdownOptions = (
         useCheckUserHasWriteTypePermission();
     return useSnackQuery({
         queryKey,
-        queryFn: () => getOrgunitTypesV3(projectId),
+        queryFn: () => getOrgunitTypes(projectId),
         options: {
             keepPreviousData: true,
             staleTime: 1000 * 60 * 15, // in MS
@@ -96,6 +47,7 @@ export const useGetOrgUnitTypesDropdownOptions = (
                         return {
                             value: orgunitType.id.toString(),
                             label: orgunitType.name,
+                            original: orgunitType,
                         };
                     });
             },

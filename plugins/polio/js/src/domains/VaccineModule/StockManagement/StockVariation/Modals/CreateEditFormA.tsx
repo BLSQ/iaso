df1e49e885baddabddcfabae750a2b86d8511e2f
Math VDH <mathvdh@gmail.com>
2024-11-07 13:01:13 +0000

    POLIO-1689 Fix for initial value of round in front

--- plugins/polio/js/src/domains/VaccineModule/StockManagement/StockVariation/Modals/CreateEditFormA.tsx
+++ plugins/polio/js/src/domains/VaccineModule/StockManagement/StockVariation/Modals/CreateEditFormA.tsx
@@ -51,7 +51,7 @@ export const CreateEditFormA: FunctionComponent<Props> = ({
         initialValues: {
             id: formA?.id,
             campaign: formA?.campaign,
-            round: formA?.rounds,
+            round: formA?.round,
             // lot_numbers: formA?.lot_numbers ?? '',
             report_date: formA?.report_date,
             form_a_reception_date: formA?.form_a_reception_date,

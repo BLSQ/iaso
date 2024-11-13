93277fe67e1edfe5aff49525c2dcd342e738a924
Quang Son Le <qle@bluesquarehub.com>
2024-11-13 19:00:04 +0100

    POLIO-1716: add add_stock_history_command

--- plugins/polio/tasks/archive_vaccine_stock_for_rounds.py
+++ plugins/polio/tasks/archive_vaccine_stock_for_rounds.py
@@ -5,9 +5,38 @@ from plugins.polio.api.vaccines.stock_management import VaccineStockCalculator
 from plugins.polio.models import Round, VaccineStock, VaccineStockHistory
 from plugins.polio.models.base import VACCINES
 from django.db.models import Q
+from django.db.utils import IntegrityError
 
 
 logger = logging.getLogger(__name__)
+
+
+def archive_stock_for_round(round, vaccine_stock, reference_date, country=None):
+    if not country:
+        vaccine_stock_for_vaccine = vaccine_stock.filter(country=round.campaign.country.id)
+
+    if vaccine_stock_for_vaccine.exists():
+        vaccine_stock_for_vaccine = vaccine_stock_for_vaccine.first()
+        calculator = VaccineStockCalculator(vaccine_stock_for_vaccine)
+        total_usable_vials_in, total_usable_doses_in = calculator.get_total_of_usable_vials(reference_date)
+        total_unusable_vials_in, total_unusable_doses_in = calculator.get_total_of_unusable_vials(reference_date)
+        try:
+            VaccineStockHistory.objects.create(
+                vaccine_stock=vaccine_stock_for_vaccine,
+                round=round,
+                usable_vials_in=total_usable_vials_in,
+                usable_doses_in=total_usable_doses_in,
+                unusable_vials_in=total_unusable_vials_in,
+                unusable_doses_in=total_unusable_doses_in,
+            )
+        except IntegrityError:
+            print("--------------------------------------------")
+            print(f"Error with {round.campaign.obr_name}, round {round.number}")
+            print(f"usable_vials_in={total_usable_vials_in}")
+            print(f"usable_doses_in={total_usable_doses_in}")
+            print(f"unusable_vials_in={total_unusable_vials_in}")
+            print(f"unusable_doses_in={total_unusable_doses_in}")
+            print("--------------------------------------------")
 
 
 @task_decorator(task_name="archive_vaccine_stock")
@@ -43,25 +72,7 @@ def archive_vaccine_stock_for_rounds(date=None, country=None, campaign=None, vac
         vaccine_stock = VaccineStock.objects.filter(vaccine=vax)
         if country:
             vaccine_stock = VaccineStock.objects.filter(country__id=country)
-
         for r in vax_rounds_qs:
-            vaccine_stock_for_vaccine = vaccine_stock.filter(vaccine=vax)
-            if not country:
-                vaccine_stock_for_vaccine = vaccine_stock_for_vaccine.filter(country=r.campaign.country.id)
-
-            if vaccine_stock_for_vaccine.exists():
-                vaccine_stock_for_vaccine = vaccine_stock_for_vaccine.first()
-                calculator = VaccineStockCalculator(vaccine_stock_for_vaccine)
-                total_usable_vials_in, total_usable_doses_in = calculator.get_total_of_usable_vials(reference_date)
-                total_unusable_vials_in, total_unusable_doses_in = calculator.get_total_of_unusable_vials(
-                    reference_date
-                )
-
-                VaccineStockHistory.objects.create(
-                    vaccine_stock=vaccine_stock_for_vaccine,
-                    round=r,
-                    usable_vials_in=total_usable_vials_in,
-                    usable_doses_in=total_usable_doses_in,
-                    unusable_vials_in=total_unusable_vials_in,
-                    unusable_doses_in=total_unusable_doses_in,
-                )
+            archive_stock_for_round(
+                round=r, reference_date=reference_date, vaccine_stock=vaccine_stock, vaccine=vax, country=country
+            )

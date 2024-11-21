0e19ccafe888c3db23f0938f598d473237a3574f
Christophe Gerard <c@eduz.be>
2024-11-21 10:49:35 +0100

    Add sub table component to use in a cell

---
+++ hat/assets/js/apps/Iaso/components/Cells/SubTable.tsx
@@ -0,0 +1,62 @@
+import { Table, TableBody, TableCell, TableRow } from '@mui/material';
+import { textPlaceholder } from 'bluesquare-components';
+import React, { FunctionComponent } from 'react';
+import { SxStyles } from '../../types/general';
+
+type SubTableProps = {
+    values: any[];
+    renderValue: (value: any) => string;
+};
+
+const styles: SxStyles = {
+    table: {
+        borderCollapse: 'collapse',
+        width: '100%',
+        height: '100%',
+    },
+    tableCell: {
+        textAlign: 'center',
+        padding: '0px',
+        height: '30px',
+    },
+};
+
+export const SubTable: FunctionComponent<SubTableProps> = ({
+    values,
+    renderValue,
+}) => {
+    return (
+        <Table sx={styles.table}>
+            <TableBody>
+                {values.length === 0 ? (
+                    <TableRow>
+                        <TableCell
+                            sx={{
+                                ...styles.tableCell,
+                                borderBottom: 'none',
+                            }}
+                        >
+                            {textPlaceholder}
+                        </TableCell>
+                    </TableRow>
+                ) : (
+                    values.map((value, index) => (
+                        <TableRow key={`${renderValue(value)}${index}`}>
+                            <TableCell
+                                sx={{
+                                    ...styles.tableCell,
+                                    borderBottom:
+                                        index === values.length - 1
+                                            ? 'none'
+                                            : '1px solid rgba(224, 224, 224, 1)',
+                                }}
+                            >
+                                {renderValue(value) ?? textPlaceholder}
+                            </TableCell>
+                        </TableRow>
+                    ))
+                )}
+            </TableBody>
+        </Table>
+    );
+};

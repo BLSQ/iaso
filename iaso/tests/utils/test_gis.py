41b2660484b0b4880004e96f697393f0be4c8fc9
Thibault Dethier <tdethier@bluesquarehub.com>
2024-11-28 15:50:00 +0100

    Fixed simplified geom

--- iaso/tests/utils/test_gis.py
+++ iaso/tests/utils/test_gis.py
@@ -13,8 +13,11 @@ class SimplifyGeomTestCase(TestCase):
         self.assertEqual(simplified_geom.num_coords, 678)
 
     def test_simplify_as_essomba_geometry(self):
+        """
+        This tests shows that simplifying a simple shape returns an identical shape or something very similar.
+        """
         with open("iaso/tests/fixtures/geometry/as_essomba_multipolygon.txt") as multipolygon_file:
             geom = GEOSGeometry(multipolygon_file.read(), srid=4326)
         simplified_geom = simplify_geom(geom)
         self.assertEqual(geom.num_coords, 91)
-        self.assertEqual(simplified_geom.num_coords, 91)  # Same number of coords.
+        self.assertEqual(simplified_geom.num_coords, 90)  # Very similar number of coords

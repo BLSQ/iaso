e5b0558f620f40d6aaaa47830b0217a8789d3eed
Quang Son Le <qle@bluesquarehub.com>
2024-11-29 11:50:30 +0100

    POLIO-1768: fix scopes migration

--- plugins/polio/migrations/0206_delete_unused_scopes.py
+++ plugins/polio/migrations/0206_delete_unused_scopes.py
@@ -5,17 +5,28 @@ from django.db import migrations
 
 def delete_unused_scopes(apps, schema_editor):
     Round = apps.get_model("polio", "Round")
+    Campaign = apps.get_model("polio", "Campaign")
+    CampaignScope = apps.get_model("polio", "CampaignScope")
+    RoundScope = apps.get_model("polio", "RoundScope")
 
-    rounds = Round.objects.all().select_related("campaign").prefetch_related("campaign__scopes", "scopes")
+    round_scopes = RoundScope.objects.all().prefetch_related("round", "round__campaign")
+    campaign_scopes = CampaignScope.objects.all().prefetch_related("campaign")
 
-    for rnd in rounds:
-        # These rounds should be deleted in another migration once we confirmed that it is safe to do so
-        if not rnd.campaign:
+    campaign_scopes_ids = []
+    round_scopes_ids = []
+
+    for scope in campaign_scopes:
+        if scope.campaign.separate_scopes_per_round:
+            campaign_scopes_ids.append(scope.id)
+    for scope in round_scopes:
+        # Rounds without campaigns should be deleted in separate migration
+        if not scope.round.campaign:
             continue
-        if rnd.campaign.separate_scopes_per_round:
-            rnd.campaign.scopes.set([])
-        else:
-            rnd.scopes.set([])
+        if not scope.round.campaign.separate_scopes_per_round:
+            round_scopes_ids.append(scope.id)
+
+    CampaignScope.objects.filter(id__in=campaign_scopes_ids).delete()
+    RoundScope.objects.filter(id__in=round_scopes_ids).delete()
 
 
 class Migration(migrations.Migration):

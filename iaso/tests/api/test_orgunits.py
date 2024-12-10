5a4c28fbccfe8e7e95a6336224a4da4caff1c19b
kemar <marc@marcarea.com>
2024-12-10 11:42:23 +0100

    Fix tests

--- iaso/tests/api/test_orgunits.py
+++ iaso/tests/api/test_orgunits.py
@@ -1118,10 +1118,16 @@ class OrgUnitAPITestCase(APITestCase):
         self.assertJSONResponse(response, 200)
         self.assertCreated({Modification: 1})
         ou = m.OrgUnit.objects.get(id=old_ou.id)
+
         # Verify group was not modified but the rest was modified
-        self.assertQuerySetEqual(
-            ou.groups.all().order_by("name"), ["<Group:  | Evil Empire  1 >", "<Group: bad | None >"], transform=repr
-        )
+        ou_groups = ou.groups.all().order_by("name")
+        self.assertEqual(ou_groups.count(), 2)
+        self.assertEqual(ou_groups[0].name, "")
+        self.assertEqual(ou_groups[0].source_version.data_source.name, "Evil Empire")
+        self.assertEqual(ou_groups[0].source_version.number, 1)
+        self.assertEqual(ou_groups[1].name, "bad")
+        self.assertEqual(ou_groups[1].source_version, None)
+
         self.assertEqual(ou.id, old_ou.id)
         self.assertEqual(ou.name, "new name")
         self.assertEqual(ou.parent, old_ou.parent)

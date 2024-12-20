2215a66fcc822ead31dc2ba519bc0481119911ee
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2024-12-20 15:29:58 +0200

    launch task

--- hat/assets/js/apps/Iaso/domains/instances/components/PushGpsDialogComponent.tsx
+++ hat/assets/js/apps/Iaso/domains/instances/components/PushGpsDialogComponent.tsx
@@ -1,12 +1,14 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useCallback, useState } from 'react';
 import { Grid, Typography } from '@mui/material';
-import { useSafeIntl } from 'bluesquare-components';
+import { useRedirectTo, useSafeIntl } from 'bluesquare-components';
 import ConfirmCancelDialogComponent from '../../../components/dialogs/ConfirmCancelDialogComponent';
 import MESSAGES from '../messages';
 import { Instance } from '../types/instance';
 import { Selection } from '../../orgUnits/types/selection';
 import { useGetCheckBulkGpsPush } from '../hooks/useGetCheckBulkGpsPush';
 import PushBulkGpsWarning from './PushBulkGpsWarning';
+import { useInstanceBulkgpspush } from '../hooks/useInstanceBulkgpspush';
+import { baseUrls } from '../../../constants/urls';
 
 type Props = {
     renderTrigger: (openDialog: boolean) => void;
@@ -21,16 +23,41 @@ const PushGpsDialogComponent: FunctionComponent<Props> = ({
         useState<boolean>(false);
     const [approveSubmissionNoHasGps, setApproveSubmissionNoHasGps] =
         useState<boolean>(false);
-    const onConfirm = closeDialog => {
-        return null;
-    };
+    const { mutateAsync: bulkgpspush } = useInstanceBulkgpspush();
+    const select_all = selection.selectAll;
+    const selected_ids = selection.selectedItems;
+    const unselected_ids = selection.unSelectedItems;
+    const instancebulkgpspush = useCallback(async () => {
+        await bulkgpspush({
+            select_all,
+            selected_ids: selected_ids.map(item => item.id),
+            unselected_ids: unselected_ids.map(item => item.id),
+        });
+    }, [bulkgpspush, select_all, selected_ids, unselected_ids]);
+
+    const onConfirm = useCallback(
+        async closeDialog => {
+            await instancebulkgpspush();
+            closeDialog();
+        },
+        [instancebulkgpspush],
+    );
+    const redirectTo = useRedirectTo();
+    const onConfirmAndSeeTask = useCallback(
+        async closeDialog => {
+            await instancebulkgpspush();
+            closeDialog();
+            redirectTo(baseUrls.tasks, {
+                order: '-created_at',
+            });
+        },
+        [instancebulkgpspush, redirectTo],
+    );
 
     const { data: checkBulkGpsPush, isError } = useGetCheckBulkGpsPush({
-        selected_ids: selection.selectedItems.map(item => item.id).join(','),
-        select_all: selection.selectAll,
-        unselected_ids: selection.unSelectedItems
-            .map(item => item.id)
-            .join(','),
+        selected_ids: selected_ids.map(item => item.id).join(','),
+        select_all,
+        unselected_ids: unselected_ids.map(item => item.id).join(','),
     });
 
     const onClosed = () => {
@@ -66,14 +93,16 @@ const PushGpsDialogComponent: FunctionComponent<Props> = ({
         <ConfirmCancelDialogComponent
             renderTrigger={({ openDialog }) => renderTrigger(openDialog)}
             titleMessage={title}
-            onConfirm={onConfirm}
+            onConfirm={closeDialog => onConfirm(closeDialog)}
             confirmMessage={MESSAGES.launch}
             onClosed={onClosed}
             cancelMessage={MESSAGES.cancel}
             maxWidth="sm"
             additionalButton
             additionalMessage={MESSAGES.goToCurrentTask}
-            onAdditionalButtonClick={onConfirm}
+            onAdditionalButtonClick={closeDialog =>
+                onConfirmAndSeeTask(closeDialog)
+            }
             allowConfimAdditionalButton
         >
             <Grid container spacing={4} alignItems="center">

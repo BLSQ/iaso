ba208e4a74c300af75917c6319d7225cfe5a10be
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2024-12-20 10:20:37 +0200

    Move warning code into different file

--- hat/assets/js/apps/Iaso/domains/instances/components/PushGpsDialogComponent.tsx
+++ hat/assets/js/apps/Iaso/domains/instances/components/PushGpsDialogComponent.tsx
@@ -1,62 +1,16 @@
 import React, { FunctionComponent, useState } from 'react';
-import { Button, Grid, Typography } from '@mui/material';
-import { LinkWithLocation, useSafeIntl } from 'bluesquare-components';
+import { Grid, Typography } from '@mui/material';
+import { useSafeIntl } from 'bluesquare-components';
 import ConfirmCancelDialogComponent from '../../../components/dialogs/ConfirmCancelDialogComponent';
 import MESSAGES from '../messages';
 import { Instance } from '../types/instance';
 import { Selection } from '../../orgUnits/types/selection';
 import { useGetCheckBulkGpsPush } from '../hooks/useGetCheckBulkGpsPush';
+import PushBulkGpsWarning from './PushBulkGpsWarning';
 
 type Props = {
     renderTrigger: (openDialog: boolean) => void;
     selection: Selection<Instance>;
-};
-
-const WarningSection = ({
-    condition,
-    message,
-    linkTo,
-    approveCondition,
-    onApproveClick,
-}) => {
-    const { formatMessage } = useSafeIntl();
-    if (!condition) return null;
-
-    return (
-        <Grid
-            item
-            xs={12}
-            container
-            spacing={2}
-            alignItems="center"
-            direction="row"
-        >
-            <Grid item xs={8}>
-                <Typography component="ul" sx={{ color: 'warning.main' }}>
-                    <Typography component="li">
-                        {formatMessage(message)}
-                    </Typography>
-                </Typography>
-            </Grid>
-            <Grid item xs={2} display="flex" justifyContent="flex-start">
-                <LinkWithLocation to={linkTo}>
-                    {formatMessage(MESSAGES.seeAll)}
-                </LinkWithLocation>
-            </Grid>
-            <Grid item xs={2} display="flex" justifyContent="flex-end">
-                <Button
-                    data-test="search-button"
-                    variant="outlined"
-                    color={approveCondition ? 'primary' : 'warning'}
-                    onClick={onApproveClick}
-                >
-                    {formatMessage(
-                        approveCondition ? MESSAGES.approved : MESSAGES.approve,
-                    )}
-                </Button>
-            </Grid>
-        </Grid>
-    );
 };
 
 const PushGpsDialogComponent: FunctionComponent<Props> = ({
@@ -135,7 +89,7 @@ const PushGpsDialogComponent: FunctionComponent<Props> = ({
                               })}
                     </Typography>
                 </Grid>
-                <WarningSection
+                <PushBulkGpsWarning
                     condition={
                         (checkBulkGpsPush?.warning_no_location?.length ?? 0) >
                             0 && (checkBulkGpsPush?.error_ids?.length ?? 0) <= 0
@@ -145,7 +99,7 @@ const PushGpsDialogComponent: FunctionComponent<Props> = ({
                     approveCondition={approveSubmissionNoHasGps}
                     onApproveClick={() => onApprove('instanceNoGps')}
                 />
-                <WarningSection
+                <PushBulkGpsWarning
                     condition={
                         (checkBulkGpsPush?.warning_overwrite?.length ?? 0) >
                             0 && (checkBulkGpsPush?.error_ids?.length ?? 0) <= 0

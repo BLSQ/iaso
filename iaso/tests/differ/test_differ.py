58f1474aba3618ce6c858d10045d55746f3875e0
kemar <marc@marcarea.com>
2024-12-20 15:49:48 +0100

    Fix tests

--- iaso/tests/differ/test_differ.py
+++ iaso/tests/differ/test_differ.py
@@ -501,7 +501,7 @@ class DifferTestCase(TestCase):
         region_diff = next((diff for diff in diffs if diff.org_unit.org_unit_type == self.org_unit_type_region), None)
         self.assertEqual(region_diff.status, "modified")
         region_diff_comparisons = [comparison.as_dict() for comparison in region_diff.comparisons]
-        self.assertEqual(5, len(region_diff_comparisons))
+        self.assertEqual(8, len(region_diff_comparisons))
         self.assertDictEqual(
             region_diff_comparisons[0],
             {
@@ -552,13 +552,43 @@ class DifferTestCase(TestCase):
                 "distance": None,
             },
         )
+        self.assertDictEqual(
+            region_diff_comparisons[5],
+            {
+                "field": "group:group-a:Group A",
+                "before": [],
+                "after": [],
+                "status": "same",
+                "distance": 0,
+            },
+        )
+        self.assertDictEqual(
+            region_diff_comparisons[6],
+            {
+                "field": "group:group-b:Group B",
+                "before": [],
+                "after": [],
+                "status": "same",
+                "distance": 0,
+            },
+        )
+        self.assertDictEqual(
+            region_diff_comparisons[7],
+            {
+                "field": "group:group-c:Group C",
+                "before": [],
+                "after": [],
+                "status": "same",
+                "distance": 0,
+            },
+        )
 
         district_diff = next(
             (diff for diff in diffs if diff.org_unit.org_unit_type == self.org_unit_type_district), None
         )
         self.assertEqual(district_diff.status, "modified")
         district_diff_comparisons = [comparison.as_dict() for comparison in district_diff.comparisons]
-        self.assertEqual(5, len(district_diff_comparisons))
+        self.assertEqual(8, len(district_diff_comparisons))
         self.assertDictEqual(
             district_diff_comparisons[0],
             {
@@ -607,5 +637,35 @@ class DifferTestCase(TestCase):
                 "after": datetime.date(2025, 12, 28),
                 "status": "modified",
                 "distance": None,
+            },
+        )
+        self.assertDictEqual(
+            district_diff_comparisons[5],
+            {
+                "field": "group:group-a:Group A",
+                "before": [],
+                "after": [],
+                "status": "same",
+                "distance": 0,
+            },
+        )
+        self.assertDictEqual(
+            district_diff_comparisons[6],
+            {
+                "field": "group:group-b:Group B",
+                "before": [],
+                "after": [],
+                "status": "same",
+                "distance": 0,
+            },
+        )
+        self.assertDictEqual(
+            region_diff_comparisons[7],
+            {
+                "field": "group:group-c:Group C",
+                "before": [],
+                "after": [],
+                "status": "same",
+                "distance": 0,
             },
         )

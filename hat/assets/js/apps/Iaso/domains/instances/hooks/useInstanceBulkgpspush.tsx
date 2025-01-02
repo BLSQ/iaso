7a8d2684a76b78f73434551b3d0847ea6020d2a1
HAKIZIMANA Franck <fhakizimana@bluesquarehub.com>
2025-01-02 14:39:35 +0200

    add snackBar message on launch task

--- hat/assets/js/apps/Iaso/domains/instances/hooks/useInstanceBulkgpspush.tsx
+++ hat/assets/js/apps/Iaso/domains/instances/hooks/useInstanceBulkgpspush.tsx
@@ -1,6 +1,7 @@
 import { UseMutationResult } from 'react-query';
 import { postRequest } from '../../../libs/Api';
 import { useSnackMutation } from '../../../libs/apiHooks';
+import MESSAGES from '../messages';
 
 type BulkGpsPush = {
     select_all: boolean;
@@ -8,16 +9,15 @@ type BulkGpsPush = {
     unselected_ids: string[];
 };
 export const useInstanceBulkgpspush = (): UseMutationResult => {
-    return useSnackMutation({
-        mutationFn: (data: BulkGpsPush) => {
-            const { select_all, selected_ids, unselected_ids } = data;
-
+    return useSnackMutation(
+        ({ select_all, selected_ids, unselected_ids }) => {
             return postRequest('/api/tasks/create/instancebulkgpspush/', {
                 select_all,
                 selected_ids,
                 unselected_ids,
             });
         },
-        showSucessSnackBar: false,
-    });
+        MESSAGES.pushGpsTaskPlanned,
+        MESSAGES.pushGpsTaskError,
+    );
 };

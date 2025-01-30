fb6e0fc987e187cd850e4f88b6aa2c08151001f3
Math VDH <mathvdh@gmail.com>
2025-01-30 16:00:55 +0100

    IA-3893 WIP

--- plugins/polio/permissions.py
+++ plugins/polio/permissions.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-_PREFIX = "menupermissions."
+_PREFIX = "polio."
 
 _POLIO = "iaso_polio"
 _POLIO_BUDGET = "iaso_polio_budget"

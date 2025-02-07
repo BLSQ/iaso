acd40433dd704e6e49277566de3edc913fbd4f94
Bram Jans <ik@bramjans.com>
2025-02-07 18:26:42 +0100

    Add MetricType and MetricValue models

--- iaso/models/__init__.py
+++ iaso/models/__init__.py
@@ -6,6 +6,7 @@ from .device import Device, DeviceOwnership, DevicePosition
 from .entity import Entity, EntityType
 from .forms import Form, FormAttachment, FormPredefinedFilter, FormVersion
 from .import_gpkg import ImportGPKG
+from .metric import MetricType, MetricValue
 from .microplanning import Planning, Team
 from .org_unit import OrgUnit, OrgUnitChangeRequest, OrgUnitType
 from .org_unit_change_request_configuration import OrgUnitChangeRequestConfiguration
@@ -39,6 +40,8 @@ __all__ = [
     "FormPredefinedFilter",
     "FormVersion",
     "ImportGPKG",
+    "MetricType",
+    "MetricValue",
     "OrgUnit",
     "OrgUnitChangeRequest",
     "OrgUnitChangeRequestConfiguration",

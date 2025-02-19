bc11f42c0642a0a82891dae7e62d80b937022fdb
Bram Jans <ik@bramjans.com>
2025-02-19 14:24:13 +0100

    wip

--- iaso/admin.py
+++ iaso/admin.py
@@ -1,16 +1,17 @@
 from typing import Any, Protocol
 
 from django import forms as django_forms
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.admin import widgets, SimpleListFilter
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from django.contrib.auth.models import User
 from django.contrib.gis import admin, forms
 from django.contrib.gis.db import models as geomodels
 from django.contrib.postgres.fields import ArrayField
-from django.db import models
+from django.db import models, transaction
 from django.db.models import Exists, OuterRef
 from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import path, reverse
 from django.utils.html import format_html, format_html_join
 from django.utils.safestring import mark_safe
@@ -18,7 +19,6 @@ from django_json_widget.widgets import JSONEditorWidget
 
 from hat.audit.models import DJANGO_ADMIN
 from iaso.models.json_config import Config  # type: ignore
-from iaso.utils import is_multi_account_user
 from iaso.utils.admin.custom_filters import (
     DuplicateUUIDFilter,
     EntityEmptyAttributesFilter,
@@ -452,13 +452,28 @@ class UserProfileInline(admin.StackedInline):
     raw_id_fields = ("org_units",)
 
 
+class MultiAccountForm(forms.Form):
+    accounts = forms.ModelMultipleChoiceField(
+        queryset=Account.objects.none(),  # Set empty queryset by default
+        widget=forms.CheckboxSelectMultiple,
+        required=True,
+        label="Select Accounts",
+    )
+
+    def __init__(self, *args, **kwargs):
+        existing_accounts = kwargs.pop("existing_accounts", [])
+        super().__init__(*args, **kwargs)
+
+        # Set the queryset to exclude existing accounts
+        account_ids = [account.pk for account in existing_accounts]
+        self.fields["accounts"].queryset = Account.objects.exclude(pk__in=account_ids)
+
+
 @admin_attr_decorator
 class UserAdmin(AuthUserAdmin):
     list_display = AuthUserAdmin.list_display + ("accounts",)
 
-    inlines = [
-        UserProfileInline,
-    ]
+    inlines = [UserProfileInline]
 
     def accounts(self, user):
         if user.tenant_users.exists():  # Multi-account user
@@ -497,6 +512,83 @@ class UserAdmin(AuthUserAdmin):
             UserProfileInline,
         ]
         return super(UserAdmin, self).change_view(*args, **kwargs)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "<int:user_id>/edit-multi-account/",
+                self.admin_site.admin_view(self.edit_multi_account_user),
+                name="edit-multi-account",
+            ),
+        ]
+        return custom_urls + urls
+
+    def edit_multi_account_user(self, request, user_id):
+        user = get_object_or_404(User, pk=user_id)
+
+        if request.method == "POST":
+            form = MultiAccountForm(request.POST)
+            if form.is_valid():
+                if not user.tenant_users.exists():  # User is not yet multi-account
+                    # Move existing account to tenant_user structure
+                    existing_profile = user.iaso_profile
+                    existing_account = existing_profile.account
+                    new_account_user = User.objects.create(
+                        username=f"{user.username}_account_{str(existing_account.pk)}",
+                        first_name=user.first_name,
+                        last_name=user.last_name,
+                        email=user.email,
+                        is_superuser=user.is_superuser,
+                        is_staff=user.is_staff,
+                    )
+                    existing_profile.user = new_account_user
+                    existing_profile.save()
+                    TenantUser.objects.create(
+                        main_user=user,
+                        account_user=new_account_user,
+                    )
+
+                for account in form.cleaned_data["accounts"]:
+                    with transaction.atomic():
+                        new_account_user = User.objects.create(
+                            username=f"{user.username}_account_{str(account.pk)}",
+                            first_name=user.first_name,
+                            last_name=user.last_name,
+                            email=user.email,
+                            is_superuser=user.is_superuser,
+                            is_staff=user.is_staff,
+                        )
+                        Profile.objects.create(account=account, user=new_account_user)
+                        TenantUser.objects.create(
+                            main_user=user,
+                            account_user=new_account_user,
+                        )
+                        # Profile
+                        # TenantUser
+
+                print("####")
+                print("####")
+                # print("accounts", accounts)
+
+                # Implement your logic to link the user to the selected accounts
+                # Example: user.iaso_profile.account.set(accounts)
+                messages.success(request, f"User {user.username} has been linked to selected accounts.")
+                return redirect("admin:auth_user_change", user.id)
+
+            return redirect("admin:auth_user_change", user_id=user.id)
+
+        else:
+            if user.tenant_users.exists():  # Multi-account user
+                linked_accounts = [
+                    tu.account_user.iaso_profile and tu.account_user.iaso_profile.account
+                    for tu in user.tenant_users.all()
+                ]
+            else:  # Regular user
+                linked_accounts = [user.iaso_profile.account]
+            form = MultiAccountForm(existing_accounts=linked_accounts)
+
+        return render(request, "admin/edit_multi_account_user.html", {"form": form, "user": user})
 
 
 # unregister old user admin

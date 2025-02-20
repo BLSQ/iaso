8cc3ba776b638055d2e38f9b17ebf45a26535c69
Christophe Gerard <cgerard@bluesquarehub.com>
2025-02-20 14:10:58 +0100

    roll back and set endpoint to public

--- plugins/polio/api/dashboards/launch_powerbi.py
+++ plugins/polio/api/dashboards/launch_powerbi.py
@@ -1,7 +1,5 @@
-from django.utils.decorators import method_decorator
-from django.views.decorators.clickjacking import xframe_options_exempt
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import serializers, status, viewsets
+from rest_framework import permissions, serializers, status, viewsets
 from rest_framework.response import Response
 
 from iaso.utils.powerbi import launch_dataset_refresh
@@ -15,8 +13,9 @@ class PowerBIRefreshSerializer(serializers.Serializer):
 @swagger_auto_schema()
 class LaunchPowerBIRefreshViewSet(viewsets.ViewSet):
     serializer_class = PowerBIRefreshSerializer
+    # Open to all to make it work while calling this api from a iframe in RRT
+    permission_classes = [permissions.AllowAny]
 
-    @method_decorator(xframe_options_exempt)
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -24,14 +23,7 @@ class LaunchPowerBIRefreshViewSet(viewsets.ViewSet):
         data_set_id = serializer.validated_data["data_set_id"]
         group_id = serializer.validated_data["group_id"]
         # Perform actions using uuid1 and uuid2
+        response_data = {"message": f"Received data_set_id: {data_set_id}, group_id: {group_id}"}
         launch_dataset_refresh(group_id, data_set_id)
 
-        response_data = {"message": f"Received data_set_id: {data_set_id}, group_id: {group_id}"}
-        response = Response(response_data, status=status.HTTP_201_CREATED)
-        response["Content-Security-Policy"] = "frame-ancestors 'self' afro-rrt-who.hub.arcgis.com"
-
-        # Set SameSite attribute for cookies
-        response.set_cookie("sessionid", request.COOKIES.get("sessionid"), samesite="None", secure=True)
-        response.set_cookie("csrftoken", request.COOKIES.get("csrftoken"), samesite="None", secure=True)
-
-        return response
+        return Response(response_data, status=status.HTTP_201_CREATED)

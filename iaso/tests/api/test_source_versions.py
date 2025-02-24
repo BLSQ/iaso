5743c4a9e3f51e3f970dfa5087e962cb9d8a81ce
Fleury Butoyi <fleurybu@gmail.com>
2025-02-24 10:17:55 +0200

    Testing drop down datasource endpoint with user from another account

--- iaso/tests/api/test_source_versions.py
+++ iaso/tests/api/test_source_versions.py
@@ -23,6 +23,16 @@ class SourceVersionAPITestCase(APITestCase):
         cls.project = m.Project.objects.create(name="Project", account=cls.account, app_id="foo.bar.baz")
         cls.data_source.projects.set([cls.project])
 
+        cls.account2, cls.data_source2, cls.version2, cls.project2 = cls.create_account_datasource_version_project(
+            source_name="GDHF source", account_name="GDHF", project_name="GDHF campaign"
+        )
+        cls.project2.account = cls.account2
+        cls.data_source2.account = cls.account2
+        cls.project2.app_id = "GDHF.campaign"
+        cls.project2.save()
+        cls.data_source2.save()
+        cls.user2 = cls.create_user_with_profile(username="GDHF", account=cls.account2)
+
     def test_list_unauthorized_without_auth(self):
         response = self.client.get("/api/sourceversions/")
         self.assertJSONResponse(response, 401)
@@ -114,3 +124,14 @@ class SourceVersionAPITestCase(APITestCase):
         self.client.force_authenticate(self.user)
         response = self.client.get("/api/sourceversions/dropdown/")
         self.assertJSONResponse(response, 403)
+
+    def test_dropdown_sourceversions_with_user_from_another_account(self):
+        self.user2.user_permissions.set([Permission.objects.get(codename=permission._SOURCES)])
+        self.client.force_authenticate(self.user2)
+        response = self.client.get("/api/sourceversions/dropdown/")
+        data = self.assertJSONResponse(response, 200)
+        version = data[0]
+        self.assertEqual(version["id"], self.version2.pk)
+        self.assertEqual(version["data_source"], self.data_source2.pk)
+        self.assertEqual(version["number"], self.version2.number)
+        self.assertEqual(version["data_source_name"], self.data_source2.name)

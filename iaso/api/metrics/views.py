38a661c44d8453f645ce59d5e5c555b1c5201136
Bram Jans <ik@bramjans.com>
2025-02-26 22:57:20 +0100

    Allow JSONlogic filtering on the value of MetricValue
    Prepare for flexibility with the automatic filter selection.

--- iaso/api/metrics/views.py
+++ iaso/api/metrics/views.py
@@ -1,8 +1,10 @@
+import json
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
 
 
 from iaso.models import MetricType, MetricValue
+from iaso.utils.jsonlogic import jsonlogic_to_q
 from .serializers import MetricTypeSerializer, MetricValueSerializer
 
 # TODO for both viewsets: permission_classes
@@ -17,10 +19,21 @@ class MetricTypeViewSet(viewsets.ModelViewSet):
         return MetricType.objects.filter(account=self.request.user.iaso_profile.account)
 
 
+class ValueFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        json_filter = request.query_params.get("json_filter")
+
+        if json_filter:
+            q = jsonlogic_to_q(jsonlogic=json.loads(json_filter))
+            queryset = queryset.filter(q)
+
+        return queryset
+
+
 class MetricValueViewSet(viewsets.ModelViewSet):
     serializer_class = MetricValueSerializer
     queryset = MetricValue.objects.all()
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, ValueFilterBackend]
     filterset_fields = ["metric_type_id", "org_unit_id"]
 
     def get_queryset(self):

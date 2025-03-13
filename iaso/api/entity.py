ab96746663f61f98df3bbd6ed03cfb83df8a97bd
Bram Jans <ik@bramjans.com>
2025-03-13 13:57:13 +0100

    feat: TEMP: Add button on entity to link to migrated patient
    Temporary feature:
    If the entity is linked to a migrated Trypelim patient, we show the
    button. This will make testing easier.

--- iaso/api/entity.py
+++ iaso/api/entity.py
@@ -54,6 +54,7 @@ class EntitySerializer(serializers.ModelSerializer):
             "org_unit",
             "duplicates",
             "nfc_cards",
+            "migration_source",
         ]
 
     entity_type_name = serializers.SerializerMethodField()
@@ -62,6 +63,7 @@ class EntitySerializer(serializers.ModelSerializer):
     org_unit = serializers.SerializerMethodField()
     duplicates = serializers.SerializerMethodField()
     nfc_cards = serializers.SerializerMethodField()
+    migration_source = serializers.SerializerMethodField()
 
     def get_attributes(self, entity: Entity):
         if entity.attributes:
@@ -91,6 +93,17 @@ class EntitySerializer(serializers.ModelSerializer):
     @staticmethod
     def get_entity_type_name(obj: Entity):
         return obj.entity_type.name if obj.entity_type else None
+
+    def get_migration_source(self, obj: Entity):
+        if not obj.attributes:
+            return None
+
+        if obj.attributes.patient_set.exists():
+            patient = obj.attributes.patient_set.first()
+            if patient:
+                return patient.id
+
+        return None
 
 
 def _get_duplicates(entity):

3eb5ca48c0d18f6fdc8b6c9f71eb52368b375dca
Christophe Gerard <cgerard@bluesquarehub.com>
2025-03-31 16:00:29 +0200

    code review

--- hat/assets/js/apps/Iaso/domains/dataSources/messages.js
+++ hat/assets/js/apps/Iaso/domains/dataSources/messages.js
@@ -204,7 +204,7 @@ const MESSAGES = defineMessages({
     syncMessage: {
         id: 'iaso.datasources.title.syncMessage',
         defaultMessage:
-            'This might create a lot of changes requests to align the two versions.',
+            'This might create a lot of changes requests to align the two versions. This dialog will stay frozen until the preview is done.',
     },
     default: {
         id: 'iaso.datasources.options.label.default',

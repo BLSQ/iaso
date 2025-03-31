6dbe1c074b0bce0e24988dfc5d30bcdb3fb11c4c
Christophe Gerard <cgerard@bluesquarehub.com>
2025-03-31 15:39:30 +0200

    chevron

--- plugins/polio/js/src/domains/Calendar/campaignCalendar/cells/CountryStaticFields.tsx
+++ plugins/polio/js/src/domains/Calendar/campaignCalendar/cells/CountryStaticFields.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react';
 
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Box, Tooltip } from '@mui/material';
 import { useSafeIntl } from 'bluesquare-components';
 
@@ -97,7 +97,7 @@ export const CountryStaticFields: FunctionComponent<Props> = ({
                             {subActivitiesExpanded ? (
                                 <ExpandLessIcon sx={{ fontSize: 16 }} />
                             ) : (
-                                <ExpandMoreIcon sx={{ fontSize: 16 }} />
+                                <ChevronRightIcon sx={{ fontSize: 16 }} />
                             )}
                         </Box>
                     </Box>

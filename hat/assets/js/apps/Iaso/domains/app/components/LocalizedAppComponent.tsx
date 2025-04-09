46dbb86220f6ae6b03f215f4399fc506458f2552
Christophe Gerard <cgerard@bluesquarehub.com>
2025-04-09 11:00:45 +0200

    prepare the field for next steps

--- hat/assets/js/apps/Iaso/domains/app/components/LocalizedAppComponent.tsx
+++ hat/assets/js/apps/Iaso/domains/app/components/LocalizedAppComponent.tsx
@@ -6,6 +6,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import enLibrary from 'bluesquare-components/dist/locale/en.json';
 import frLibrary from 'bluesquare-components/dist/locale/fr.json';
 import { IntlProvider } from 'react-intl';
+import { useCurrentUser } from 'Iaso/utils/usersUtils';
 import translations from 'IasoModules/translations/configs';
 import { PluginsContext } from '../../../plugins/context';
 import { Plugin } from '../../../plugins/types';
@@ -28,12 +29,18 @@ const LocalizedAppComponent: FunctionComponent<Props> = ({ children }) => {
     const { plugins } = useContext(PluginsContext);
     const frPlugins = extractTranslations(plugins, 'fr');
     const enPlugins = extractTranslations(plugins, 'en');
+    const currentUser = useCurrentUser();
+    console.log('currentUser', currentUser);
+    // TODO: add default langages to the account filed, with fr and en as default
+    // TODO: handle the case where we don't have user at all, public routes and use only en and fr
+    // TODO: quid of bluesquare-components translations ?
+
     const messages = {
         fr: { ...translations.fr, ...frLibrary, ...frPlugins },
         en: { ...translations.en, ...enLibrary, ...enPlugins },
     };
     const { locale } = useLocale();
-    const onError = (msg: string): void => console.warn(msg);
+    const onError = (err: any): void => console.warn(err);
 
     return (
         <IntlProvider

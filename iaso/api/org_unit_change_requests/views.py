305ca81713d0d863997fcc55fd5763a36cba49d7
Christophe Gerard <cgerard@bluesquarehub.com>
2025-04-11 15:58:58 +0200

    remove old doc and adapt csv export

--- iaso/api/org_unit_change_requests/views.py
+++ iaso/api/org_unit_change_requests/views.py
@@ -49,6 +49,35 @@ class OrgUnitChangeRequestViewSet(viewsets.ModelViewSet):
         "Created by",
         "Updated",
         "Updated by",
+        "Status of Change Request",
+        "Name before change",
+        "Name after change",
+        "Name conclusion",
+        "Parent 1 before change",
+        "Parent 1 after change",
+        "Ref Ext parent 1 before change",
+        "Ref Ext parent 1 after change",
+        "Ref Ext parent 1 conclusion",
+        "Ref Ext parent 2 before change",
+        "Ref Ext parent 2 after change",
+        "Ref Ext parent 2 conclusion",
+        "Ref Ext parent 3 before change",
+        "Ref Ext parent 3 after change",
+        "Ref Ext parent 3 conclusion",
+        "Opening date before change",
+        "Opening date after change",
+        "Opening date conclusion",
+        "Closing date before change",
+        "Closing date after change",
+        "Closing date conclusion",
+        "Groups before change",
+        "Groups after change",
+        "Groups conclusion",
+        "Localisation before change",
+        "Localisation after change",
+        "Localisation conclusion",
+        "Reference submission before",
+        "Reference form after",
     ]
 
     filter_backends = [filters.OrderingFilter, django_filters.rest_framework.DjangoFilterBackend]
@@ -218,20 +247,150 @@ class OrgUnitChangeRequestViewSet(viewsets.ModelViewSet):
         writer.writerow(self.CSV_HEADER_COLUMNS)
 
         for change_request in filtered_org_unit_changes_requests:
+            # Helper function to determine if a field has changed
+            def get_conclusion(old_value, new_value):
+                if old_value == new_value:
+                    return "same"
+                return "updated"
+
+            # Get parent reference extensions
+            def get_parent_ref_ext(parent, level):
+                if not parent:
+                    return None
+
+                # Get ancestors up to the specified level
+                ancestors = list(parent.ancestors().order_by("path"))
+                if level <= len(ancestors):
+                    return ancestors[level - 1].source_ref
+                return None
+
+            # Get location string
+            def get_location_str(location):
+                if not location:
+                    return None
+                return f"{location.y}, {location.x}"
+
+            # Get reference instance IDs
+            def get_reference_instance_ids(instances):
+                if not instances.exists():
+                    return ""
+                return ",".join(str(instance.id) for instance in instances.all())
+
+            # Basic row data
             row = [
                 change_request.id,
                 change_request.org_unit_id,
                 change_request.org_unit.source_ref,
                 change_request.org_unit.name,
                 change_request.org_unit.parent.name if change_request.org_unit.parent else None,
-                change_request.org_unit.org_unit_type.name,
+                change_request.org_unit.org_unit_type.name if change_request.org_unit.org_unit_type else None,
                 ",".join(group.name for group in change_request.org_unit.groups.all()),
                 change_request.get_status_display(),
                 datetime.strftime(change_request.created_at, "%Y-%m-%d"),
                 get_creator_name(change_request.created_by) if change_request.created_by else None,
                 datetime.strftime(change_request.updated_at, "%Y-%m-%d"),
                 get_creator_name(change_request.updated_by) if change_request.updated_by else None,
+                change_request.get_status_display(),
             ]
+
+            # Name changes
+            name_before = change_request.old_name if change_request.kind == change_request.Kind.ORG_UNIT_CHANGE else ""
+            name_after = change_request.new_name if change_request.new_name else change_request.org_unit.name
+            name_conclusion = get_conclusion(name_before, name_after)
+
+            row.extend([name_before, name_after, name_conclusion])
+
+            # Parent changes
+            parent_before = change_request.old_parent.name if change_request.old_parent else ""
+            parent_after = (
+                change_request.new_parent.name
+                if change_request.new_parent
+                else change_request.org_unit.parent.name
+                if change_request.org_unit.parent
+                else None
+            )
+
+            row.extend([parent_before, parent_after])
+
+            # Reference extensions for parents
+            for level in range(1, 4):
+                parent_before = change_request.old_parent if change_request.old_parent else None
+                parent_after = (
+                    change_request.new_parent if change_request.new_parent else change_request.org_unit.parent
+                )
+
+                ref_ext_before = get_parent_ref_ext(parent_before, level)
+                ref_ext_after = get_parent_ref_ext(parent_after, level)
+                ref_ext_conclusion = get_conclusion(ref_ext_before, ref_ext_after)
+
+                row.extend([ref_ext_before, ref_ext_after, ref_ext_conclusion])
+
+            # Opening date changes
+            opening_date_before = (
+                change_request.old_opening_date.strftime("%Y-%m-%d") if change_request.old_opening_date else ""
+            )
+            opening_date_after = (
+                change_request.new_opening_date.strftime("%Y-%m-%d")
+                if change_request.new_opening_date
+                else (
+                    change_request.org_unit.opening_date.strftime("%Y-%m-%d")
+                    if change_request.org_unit.opening_date
+                    else None
+                )
+            )
+            opening_date_conclusion = get_conclusion(opening_date_before, opening_date_after)
+
+            row.extend([opening_date_before, opening_date_after, opening_date_conclusion])
+
+            # Closing date changes
+            closing_date_before = (
+                change_request.old_closed_date.strftime("%Y-%m-%d") if change_request.old_closed_date else ""
+            )
+            closing_date_after = (
+                change_request.new_closed_date.strftime("%Y-%m-%d")
+                if change_request.new_closed_date
+                else (
+                    change_request.org_unit.closed_date.strftime("%Y-%m-%d")
+                    if change_request.org_unit.closed_date
+                    else None
+                )
+            )
+            closing_date_conclusion = get_conclusion(closing_date_before, closing_date_after)
+
+            row.extend([closing_date_before, closing_date_after, closing_date_conclusion])
+
+            # Groups changes
+            groups_before = ",".join(group.name for group in change_request.old_groups.all())
+            groups_after = (
+                ",".join(group.name for group in change_request.new_groups.all())
+                if change_request.new_groups.exists()
+                else ",".join(group.name for group in change_request.org_unit.groups.all())
+            )
+            groups_conclusion = get_conclusion(groups_before, groups_after)
+
+            row.extend([groups_before, groups_after, groups_conclusion])
+
+            # Location changes
+            location_before = get_location_str(change_request.old_location)
+            location_after = (
+                get_location_str(change_request.new_location)
+                if change_request.new_location
+                else get_location_str(change_request.org_unit.location)
+            )
+            location_conclusion = get_conclusion(location_before, location_after)
+
+            row.extend([location_before, location_after, location_conclusion])
+
+            # Reference instances changes
+            reference_before = get_reference_instance_ids(change_request.old_reference_instances)
+            reference_after = (
+                get_reference_instance_ids(change_request.new_reference_instances)
+                if change_request.new_reference_instances.exists()
+                else get_reference_instance_ids(change_request.org_unit.reference_instances)
+            )
+
+            row.extend([reference_before, reference_after])
+
             writer.writerow(row)
         filename = filename + ".csv"
         response["Content-Disposition"] = "attachment; filename=" + filename

b60606b1e317af41c51c9f2b2669f8016ddd9453
Christophe Gerard <cgerard@bluesquarehub.com>
2025-04-28 12:38:18 +0200

    fix datetime cell

--- hat/assets/js/apps/Iaso/components/Cells/DateTimeCell.tsx
+++ hat/assets/js/apps/Iaso/components/Cells/DateTimeCell.tsx
@@ -21,8 +21,7 @@ export const DateTimeCell = (cellInfo: {
         LANGUAGE_CONFIGS[currentLocale]?.dateFormats?.LTS ||
         LANGUAGE_CONFIGS.en?.dateFormats?.LTS ||
         'DD/MM/YYYY HH:mm';
-
-    return moment(cellInfo.value).format(format);
+    return moment(cellInfo.value * 1000).format(format);
 };
 
 export const convertToDateTimeRfc = (

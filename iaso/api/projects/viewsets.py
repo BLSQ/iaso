7c99e309e8d6b29ea7be56c191e98a08011dbc23
kemar <marc@marcarea.com>
2025-05-13 16:59:40 +0200

    Add an endpoint to list all projects by skipping restrictions

--- iaso/api/projects/viewsets.py
+++ iaso/api/projects/viewsets.py
@@ -3,7 +3,10 @@ from qr_code.qrcode.maker import make_qr_code_image
 from qr_code.qrcode.utils import QRCodeOptions
 from rest_framework import filters, permissions, status
 from rest_framework.decorators import action
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
 
+from hat.menupermissions import models as permission
 from iaso.models import Project
 
 from ..common import ModelViewSet
@@ -17,6 +20,7 @@ class ProjectsViewSet(ModelViewSet):
 
     GET /api/projects/
     GET /api/projects/<id>
+    GET /api/projects/list_all/
     """
 
     permission_classes = [permissions.IsAuthenticated]
@@ -26,12 +30,14 @@ class ProjectsViewSet(ModelViewSet):
     results_key = "projects"
     http_method_names = ["get", "head", "options", "trace"]
 
-    def get_queryset(self):
-        """Always filter the base queryset by account"""
+    def get_queryset(self, filter_on_user_projects=True):
+        # Always filter the base queryset by account.
+        projects = Project.objects.filter(account=self.request.user.iaso_profile.account)
 
-        return Project.objects.filter(account=self.request.user.iaso_profile.account).filter_on_user_projects(
-            self.request.user
-        )
+        if filter_on_user_projects:
+            projects = projects.filter_on_user_projects(self.request.user)
+
+        return projects
 
     @action(detail=True, methods=["get"])
     def qr_code(self, request, *args, **kwargs):
@@ -45,3 +51,21 @@ class ProjectsViewSet(ModelViewSet):
                 qr_code_options=QRCodeOptions(size="S", image_format="png", error_correction="L"),
             ),
         )
+
+    @action(detail=False, methods=["get"])
+    def list_all(self, request):
+        """
+        List all projects by skipping restrictions.
+        """
+        if not request.user.has_perm(permission.USERS_ADMIN):
+            raise PermissionDenied(f"{permission.USERS_ADMIN} permission is required.")
+
+        projects = self.filter_queryset(self.get_queryset(filter_on_user_projects=False))
+
+        page = self.paginate_queryset(projects)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(projects, many=True)
+        return Response(serializer.data)

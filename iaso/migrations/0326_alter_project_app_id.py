48062e60ab2101aa03a35cf64d6db9e7f047b490
kemar <marc@marcarea.com>
2025-05-21 16:05:55 +0200

    Fix migrations

--- iaso/migrations/0326_alter_project_app_id.py
+++ iaso/migrations/0326_alter_project_app_id.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("iaso", "0324_auto_20250514_1244"),
+        ("iaso", "0325_merge_0324_auto_20250514_1244_0324_project_color"),
     ]
 
     operations = [

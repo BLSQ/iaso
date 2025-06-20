c21d4bede724d27e0bae62f86a60b97e7149284b
kemar <marc@marcarea.com>
2025-06-20 16:37:58 +0200

    Factorize `calculate_md5` and fix migrations

--- iaso/enketo/__init__.py
+++ iaso/enketo/__init__.py
@@ -9,13 +9,11 @@ from .enketo_xml import (
     inject_instance_id_in_form,
     to_xforms_xml,
 )
-from .md5_file import calculate_file_md5
 
 
 __all__ = [
     "ENKETO_FORM_ID_SEPARATOR",
     "EnketoError",
-    "calculate_file_md5",
     "enketo_settings",
     "enketo_url_for_edition",
     "extract_xml_instance_from_form_xml",

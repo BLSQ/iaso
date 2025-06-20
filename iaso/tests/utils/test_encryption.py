548b33f8eaf30ab8cac70a2330467ea5c7785733
kemar <marc@marcarea.com>
2025-06-20 17:01:03 +0200

    Fix test

--- iaso/tests/utils/test_encryption.py
+++ iaso/tests/utils/test_encryption.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import SimpleTestCase
 
@@ -6,7 +8,8 @@ from iaso.utils.encryption import calculate_md5
 
 class EncryptionTestCase(SimpleTestCase):
     def test_calculate_md5(self):
-        zip_file = SimpleUploadedFile("file.zip", b"Some file content")
+        content = b"Some file content"
+        zip_file = SimpleUploadedFile("file.zip", content)
+        expected_md5 = hashlib.md5(content).hexdigest()
         md5 = calculate_md5(zip_file)
-        expected_md5 = "92bbcf620ceb5f5bf38f08e9a1f31e7b"
         self.assertEqual(md5, expected_md5)

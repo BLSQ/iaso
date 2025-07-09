b299fe4e70096c2c59666d277be844840a4afe38
Benjamin Monjoie <bmo@appkers.com>
2025-07-09 16:17:08 +0200

    Fix `configuration_schema` cannot be removed in admin

--- iaso/models/feature_flags.py
+++ iaso/models/feature_flags.py
@@ -79,7 +79,7 @@ class FeatureFlag(models.Model):
     requires_authentication = models.BooleanField(default=False)
     description = models.TextField(blank=True)
     category = models.TextField(choices=FeatureFlagCategory.choices, default=FeatureFlagCategory.NA)
-    configuration_schema = models.JSONField(null=True, default=None)
+    configuration_schema = models.JSONField(null=True, blank=True, default=None)
     order = models.PositiveSmallIntegerField(default=0)
     is_dangerous = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)

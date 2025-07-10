ae1541458882c0f79abc3470ac730466b765544e
kemar <marc@marcarea.com>
2025-07-10 15:22:58 +0200

    Delete dead code

--- iaso/models/tenant_users.py
+++ iaso/models/tenant_users.py
@@ -139,31 +139,11 @@ class TenantUser(models.Model):
             pass
         return f"{self.main_user} -- {self.account_user} ({account_name})"
 
-    @property
-    def account(self):
-        try:
-            return self.account_user.iaso_profile.account if self.account_user.iaso_profile else None
-        except User.iaso_profile.RelatedObjectDoesNotExist:
-            return None
-
     def get_all_account_users(self):
         return [tu.account_user for tu in self.main_user.tenant_users.all()]
 
     def get_other_accounts(self):
         return [tu.account for tu in self.main_user.tenant_users.exclude(pk=self.pk) if tu.account]
-
-    def as_dict(self):
-        account_dict = None
-        try:
-            if self.account_user.iaso_profile:
-                account_dict = self.account_user.iaso_profile.account.as_dict()
-        except User.iaso_profile.RelatedObjectDoesNotExist:
-            pass
-        return {
-            "id": self.id,
-            "main_user_id": self.main_user_id,
-            "account": account_dict,
-        }
 
     @classmethod
     def is_multi_account_user(cls, user: User) -> bool:

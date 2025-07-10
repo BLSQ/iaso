ee7c41dee99cebebe106d90dd7ed8ffbb980db2b
Bewi <benjamin.wilfart@gmail.com>
2025-07-10 13:11:13 +0200

    feat: Mobile API - Filter out planning without started_date null (#2265)
    refs:  IA-4313

--- iaso/api/microplanning.py
+++ iaso/api/microplanning.py
@@ -301,6 +301,10 @@ class PlanningSerializer(serializers.ModelSerializer):
         ):
             validation_errors["started_at"] = "startDateAfterEndDate"
             validation_errors["ended_at"] = "EndDateBeforeStartDate"
+
+        if validated_data.get("published_at") and validated_data.get("started_at") is None:
+            validation_errors["started_at"] = "publishedWithoutStartDate"
+
         project = validated_data.get("project", self.instance.project if self.instance else None)
 
         team = validated_data.get("team", self.instance.team if self.instance else None)
@@ -591,6 +595,7 @@ class MobilePlanningViewSet(ModelViewSet):
         return (
             Planning.objects.filter(assignment__user=user)
             .exclude(published_at__isnull=True)
+            .exclude(started_at__isnull=True)
             .filter(deleted_at__isnull=True)
             .distinct()
         )

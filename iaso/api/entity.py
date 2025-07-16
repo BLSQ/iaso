d03fceb2c969f1f7b210d50e2188673f63c1b5bf
Christophe Gerard <cgerard@bluesquarehub.com>
2025-07-16 16:24:15 +0200

    missing import

--- iaso/api/entity.py
+++ iaso/api/entity.py
@@ -19,6 +19,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend  # type: ignore
 from rest_framework import filters, permissions, serializers
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
 

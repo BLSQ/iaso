f53409e3d2d7751bd503f9f300af0a8152c3ebcc
Christophe Gerard <cgerard@bluesquarehub.com>
2025-07-17 13:23:08 +0200

    wip on azure static storage

--- iaso/storage.py
+++ iaso/storage.py
@@ -1,6 +1,18 @@
+from storages.backends.azure_storage import AzureStorage  # type: ignore
 from storages.backends.s3boto3 import S3Boto3Storage  # type: ignore
 
 
 class StaticStorage(S3Boto3Storage):
     location = "iasostatics"
     default_acl = "public-read"
+
+
+class AzureStaticStorage(AzureStorage):
+    azure_container = "static"
+    expiration_secs = None
+    default_acl = "public-read"
+
+
+class AzureMediaStorage(AzureStorage):
+    azure_container = "media"
+    expiration_secs = None

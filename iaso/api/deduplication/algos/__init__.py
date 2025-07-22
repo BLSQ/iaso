2444b43e7131e468b993fff9f4868f1ceecb41e7
kemar <marc@marcarea.com>
2025-07-22 16:13:19 +0200

    Remove `run_algo`

--- iaso/api/deduplication/algos/__init__.py
+++ iaso/api/deduplication/algos/__init__.py
@@ -1,35 +1,5 @@
-from typing import List
-
-from ..common import PotentialDuplicate
-from . import levenshtein as levenshtein
 from .base import DeduplicationAlgorithm
 
 
 POSSIBLE_ALGORITHMS = [[k, k] for k in DeduplicationAlgorithm.ALGORITHMS.keys()]
 DEFAULT_ALGORITHM = POSSIBLE_ALGORITHMS[0][0]
-
-
-def run_algo(algo_name, algo_params, task=None) -> List[PotentialDuplicate]:
-    """Runs the given algorithm and returns the results.
-
-    :param algo_name: The name of the algorithm to run.
-    :param algo_params: The parameters to pass to the algorithm.
-    :param task: The task in which the algorithm is run.
-    :return: A list of PotentialDuplicates returned by the algorithm.
-    """
-    from iaso.api.workflows.serializers import find_question_by_name
-    from iaso.models import EntityType
-
-    if algo_name in DeduplicationAlgorithm.ALGORITHMS:
-        algo = DeduplicationAlgorithm.ALGORITHMS[algo_name]()
-
-        possible_fields = EntityType.objects.values_list("reference_form__possible_fields", flat=True).get(
-            id=algo_params["entity_type_id"]
-        )
-
-        updated_fields = [find_question_by_name(field, possible_fields) for field in algo_params.get("fields", [])]
-
-        algo_params["fields"] = updated_fields
-
-        return algo.run(algo_params, task)
-    raise ValueError(f"Unknown algorithm {algo_name}")
--- iaso/tasks/run_deduplication_algo.py
+++ iaso/tasks/run_deduplication_algo.py
@@ -3,7 +3,9 @@ import logging
 from datetime import datetime
 
 from beanstalk_worker import task_decorator
-from iaso.api.deduplication.algos import run_algo  # type: ignore
+from iaso.api.deduplication.algos.base import DeduplicationAlgorithm
+from iaso.api.workflows.serializers import find_question_by_name
+from iaso.models import EntityType
 
 
 logger = logging.getLogger(__name__)
@@ -14,10 +16,21 @@ def run_deduplication_algo(algo_name=None, algo_params=None, task=None):
     """Background Task to run deduplication algo."""
     started_at = datetime.now()
 
-    results = run_algo(algo_name, algo_params, task)
+    if algo_name not in DeduplicationAlgorithm.ALGORITHMS:
+        raise ValueError(f"Unknown algorithm {algo_name}")
+
+    algo = DeduplicationAlgorithm.ALGORITHMS[algo_name]()
+
+    possible_fields = EntityType.objects.values_list("reference_form__possible_fields", flat=True).get(
+        id=algo_params["entity_type_id"]
+    )
+
+    updated_fields = [find_question_by_name(field, possible_fields) for field in algo_params.get("fields", [])]
+
+    algo_params["fields"] = updated_fields
+
+    algo.run(algo_params, task)
 
     finished_at = datetime.now()
     the_duration = (finished_at - started_at).total_seconds()
     task.report_success_with_result(f"Finished in {the_duration} seconds")
-
-    return results

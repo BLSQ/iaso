5ee2290fc30b1fc18405a4ba088e9d3faeb0efea
kemar <marc@marcarea.com>
2025-07-23 11:46:43 +0200

    Use `bulk_create`

--- iaso/api/deduplication/algos/finalize.py
+++ iaso/api/deduplication/algos/finalize.py
@@ -1,29 +1,36 @@
+from itertools import islice
 from typing import List
 
-from django.apps import apps
 from django.utils.timezone import now
 
 from iaso.api.deduplication.common import PotentialDuplicate
+from iaso.models import EntityDuplicate
 from iaso.models.base import Task
 
 
 def finalize_from_task(the_task: Task, potential_duplicates: List[PotentialDuplicate]):
     eda = the_task.entity_duplicate_analyzis.first()
+
     if not eda:
         raise Exception("No entity duplicate analyze found for task %s" % the_task)
 
-    ed_model = apps.get_model("iaso", "EntityDuplicate")
+    batch_size = 100
 
-    for pot_dup in potential_duplicates:
-        ed, _ = ed_model.objects.get_or_create(
+    entity_duplicates = (
+        EntityDuplicate(
             entity1_id=int(pot_dup["entity1_id"]),
             entity2_id=int(pot_dup["entity2_id"]),
             analyze=eda,
-            defaults={
-                "similarity_score": pot_dup["score"],
-            },
+            similarity_score=pot_dup["score"],
         )
-        ed.save()
+        for pot_dup in potential_duplicates
+    )
+
+    while True:
+        batch = list(islice(entity_duplicates, batch_size))
+        if not batch:
+            break
+        EntityDuplicate.objects.bulk_create(batch, batch_size, ignore_conflicts=True)
 
     eda.finished_at = now()
     eda.save()

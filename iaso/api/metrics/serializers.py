16cd256b0d3ab32411a4e09fbb7054457aab26f8
Bewi <benjamin.wilfart@gmail.com>
2025-07-31 15:45:12 +0200

    SNT-Malaria: Filter org units based on Exists json logic
    Associated commits:
    - 1192ee8 
    - 8c4b762 
    - e58296c 
    - 2a8d460 
    - d540ebf 
    - 1f22a93 
    - af2be6d 
    - c7b9a72 
    - 7bc90bd 
    - f046953

--- iaso/api/metrics/serializers.py
+++ iaso/api/metrics/serializers.py
@@ -28,9 +28,14 @@ class MetricTypeSerializer(serializers.ModelSerializer):
             "updated_at",
         ]
 
-
 class MetricValueSerializer(serializers.ModelSerializer):
     class Meta:
         model = MetricValue
         fields = ["id", "metric_type", "org_unit", "year", "value", "string_value"]
         read_only_fields = fields
+
+class OrgUnitIdSerializer(serializers.Serializer):
+    org_unit_id = serializers.IntegerField()
+
+    def to_representation(self, instance):
+        return {"org_unit_id": instance}
--- iaso/api/metrics/views.py
+++ iaso/api/metrics/views.py
@@ -1,10 +1,12 @@
 import json
 
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
 
 from iaso.models import MetricType, MetricValue
-from .serializers import MetricTypeSerializer, MetricValueSerializer
+from iaso.utils.jsonlogic import jsonlogic_to_exists_q_clauses, jsonlogic_to_q
+
+from .serializers import MetricTypeSerializer, MetricValueSerializer, OrgUnitIdSerializer
 
 
 # TODO for both viewsets: permission_classes
@@ -19,11 +21,52 @@ class MetricTypeViewSet(viewsets.ModelViewSet):
         return MetricType.objects.filter(account=self.request.user.iaso_profile.account)
 
 
+class ValueFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        json_filter = request.query_params.get("json_filter")
+
+        if json_filter:
+            q = jsonlogic_to_q(jsonlogic=json.loads(json_filter))
+            queryset = queryset.filter(q)
+
+        return queryset
+
+
+class ValueAndTypeFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        json_filter = request.query_params.get("json_filter")
+        group_by_field_name = "org_unit_id"
+        if not json_filter:
+            return queryset.values(group_by_field_name).distinct().values_list(group_by_field_name, flat=True)
+
+        q = jsonlogic_to_exists_q_clauses(
+            json.loads(json_filter), MetricValue.objects, "metric_type_id", group_by_field_name
+        )
+
+        qs = queryset.filter(q).values_list(group_by_field_name, flat=True).distinct()
+        return qs
+
+
 class MetricValueViewSet(viewsets.ModelViewSet):
     serializer_class = MetricValueSerializer
     queryset = MetricValue.objects.all()
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, ValueFilterBackend]
     filterset_fields = ["metric_type_id", "org_unit_id"]
+
+    def get_queryset(self):
+        return MetricValue.objects.filter(metric_type__account=self.request.user.iaso_profile.account)
+
+
+class MetricOrgUnitsViewSet(viewsets.ModelViewSet):
+    """
+    This viewset is used to retrieve the org units for a given metric type.
+    It is used in the frontend to display the org units for a given metric type.
+    """
+
+    serializer_class = OrgUnitIdSerializer
+    queryset = MetricValue.objects.all()
+    filter_backends = [DjangoFilterBackend, ValueAndTypeFilterBackend]
+    filterset_fields = ["metric_type_id"]
 
     def get_queryset(self):
         return MetricValue.objects.filter(metric_type__account=self.request.user.iaso_profile.account)

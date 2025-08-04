ab9c424d6b6f2977f84827d6e897214cb0647e15
Quang Son Le <qle@bluesquarehub.com>
2025-08-04 18:12:43 +0200

    POLIO-1983: add lqas data manager
    - convert json data to model instances
    - scaffold api endpoint by overriding perform create and perform update

---
+++ plugins/polio/api/lqas_im/lqas_data_manager.py
@@ -0,0 +1,305 @@
+from logging import getLogger
+
+from django.db.utils import IntegrityError
+
+from iaso.models.org_unit import OrgUnit
+from plugins.polio.models import Round
+from plugins.polio.models.lqas_im import (
+    LqasAbsenceStats,
+    LqasActivityStats,
+    LqasCareGiverStats,
+    LqasEntry,
+    LqasNoMarkStats,
+    LqasStatuses,
+)
+
+
+class LqasDataManager:
+    def __init__(self):
+        self.logger = getLogger(__name__)
+
+    def parse_json_and_create_lqas_activities(self, lqas_json_data):
+        data_to_parse = lqas_json_data["stats"]
+        rounds, districts = self._prefetch_rounds_and_districts(lqas_json_data)
+        for obr_name, campaign_data in data_to_parse.items():
+            for round_data in campaign_data.get("rounds", []):
+                try:
+                    round_obj = rounds.get(number=round_data["number"], campaign__obr_name=obr_name)
+                    # Create lqas models for round here
+                    self._create_lqas_activity(round_obj, obr_name, round_data)
+                    self._create_lqas_no_marks(round_obj, obr_name, round_data)
+                    self._create_lqas_absences(round_obj, obr_name, round_data)
+                    # Create district data for current round
+                    districts_results = round_data.get("data", {})
+                    for district_id_string, district_data in districts_results.items():
+                        try:
+                            district = districts.get(id=int(district_id_string))
+                            lqas_entry = self._create_lqas_entry(
+                                round_obj=round_obj, district=district, district_data=district_data
+                            )
+                            self._create_caregiver_stats(lqas_entry, district_data)
+                        except OrgUnit.DoesNotExist as e:
+                            self.logger.warning(f"No district found for id {district_id_string}")
+                            continue
+                except Round.DoesNotExist as e:
+                    self.logger.warning(
+                        f"Could not add LQAS data for {obr_name} Round {round_data['number']}: Round not found"
+                    )
+                    self.logger.warning(e)
+                    continue
+                except IntegrityError as e:
+                    self.logger.warning(e)
+                    continue
+
+    def parse_json_and_update_lqas_activities(self, lqas_json_data):
+        data_to_parse = lqas_json_data["stats"]
+        rounds, districts = self._prefetch_rounds_and_districts(lqas_json_data)
+        for obr_name, campaign_data in data_to_parse.items():
+            for round_data in campaign_data.get("rounds", []):
+                try:
+                    round_obj = rounds.get(number=round_data["number"], campaign__obr_name=obr_name)
+                    # Create lqas models for round here
+                    self._update_lqas_activity(round_obj, obr_name, round_data)
+                    self._update_lqas_no_marks(round_obj, obr_name, round_data)
+                    self._update_lqas_absences(round_obj, obr_name, round_data)
+                    # Update district data
+                    districts_results = round_data.get("data", {})
+                    for district_id_string, district_data in districts_results.items():
+                        try:
+                            district = districts.get(id=int(district_id_string))
+                            lqas_entry = self._update_lqas_entry(
+                                round_obj=round_obj, district=district, district_data=district_data
+                            )
+                            self._update_caregiver_stats(lqas_entry, district_data)
+                        except OrgUnit.DoesNotExist as e:
+                            self.logger.warning(f"No district found for id {district_id_string}")
+                            continue
+                except Round.DoesNotExist as e:
+                    self.logger.warning(
+                        f"Could not update LQAS data for {obr_name} Round {round_data['number']}: Round not found"
+                    )
+                    self.logger.warning(e)
+                    continue
+
+    def _prefetch_rounds_and_districts(self, lqas_json_data):
+        data_to_parse = lqas_json_data["stats"]
+        obr_names = data_to_parse.keys()
+        rounds = Round.objects.filter(campaign__obr_name__in=obr_names).prefetch_related(
+            "campaign",
+            "campaign__scopes",
+            "campaign__scopes__group__org_units",
+            "subactivities",
+            "subactivities__scopes",
+            "subactivities__scopes__group__org_units",
+        )
+        all_district_ids = set()
+        for campaign_data in data_to_parse.values():
+            for round_data in campaign_data.get("rounds", []):
+                for district_code in round_data.get("data", {}).keys():
+                    all_district_ids.add(int(district_code))
+
+        districts = (
+            OrgUnit.objects.filter(org_unit_type__name="DISTRICT")
+            .filter(id__in=all_district_ids)
+            .prefetch_related("parent")
+        )
+        return rounds, districts
+
+    def _create_lqas_activity(self, round_obj: Round, obr_name: str, round_data, subactivity=None):
+        try:
+            LqasActivityStats.objects.create(
+                round=round_obj,
+                subactivity=subactivity,
+                lqas_failed=round_data.get("lqas_failed", 0),
+                lqas_passed=round_data.get("lqas_passed", 0),
+                lqas_no_data=round_data.get("lqas_no_data", 0),
+                status=round_data.get("status", LqasStatuses.INSCOPE),
+            )
+
+        except IntegrityError as e:
+            self.logger.warning(f"LQAS data already exists for {obr_name} Round {round_data['number']}")
+            self.logger.warning(e)
+            raise e
+
+    def _update_lqas_activity(self, round_obj: Round, obr_name: str, round_data, subactivity=None):
+        update_values = {
+            "lqas_failed": round_data.get("lqas_failed", 0),
+            "lqas_passed": round_data.get("lqas_passed", 0),
+            "lqas_no_data": round_data.get("lqas_no_data", 0),
+            "status": round_data.get("status", LqasStatuses.INSCOPE),
+        }
+        return self._safe_update_or_create(
+            model_class=LqasActivityStats,
+            lookup_kwargs={"round": round_obj, "subactivity": subactivity},
+            update_values=update_values,
+        )
+
+    def _create_lqas_no_marks(self, round_obj: Round, obr_name: str, round_data, subactivity=None):
+        no_mark_data = round_data.get("nfm_stats", None)
+        if no_mark_data:
+            try:
+                LqasNoMarkStats.objects.create(
+                    round=round_obj,
+                    subactivity=subactivity,
+                    vaccinated_but_not_fm=no_mark_data.get("vaccinated_but_not_fm", 0),
+                    child_is_a_visitor=no_mark_data.get("child_is_a_visitor", 0),
+                    house_not_visited=no_mark_data.get("house_not_visited", 0),
+                    child_was_asleep=no_mark_data.get("child_was_asleep", 0),
+                    non_compliance=no_mark_data.get("non_compliance", 0),
+                    child_absent=no_mark_data.get("child_absent", 0),
+                    other=no_mark_data.get("other", 0),
+                )
+
+            except IntegrityError as e:
+                self.logger.warning(f"LQAS care giver data already exists for {obr_name} Round {round_data['number']}")
+                self.logger.warning(e)
+                raise e
+
+    def _update_lqas_no_marks(self, round_obj: Round, round_data, subactivity=None):
+        no_mark_data = round_data.get("nfm_stats", None)
+        if no_mark_data:
+            update_values = {
+                "vaccinated_but_not_fm": no_mark_data.get("vaccinated_but_not_fm", 0),
+                "child_is_a_visitor": no_mark_data.get("child_is_a_visitor", 0),
+                "house_not_visited": no_mark_data.get("house_not_visited", 0),
+                "child_was_asleep": no_mark_data.get("child_was_asleep", 0),
+                "non_compliance": no_mark_data.get("non_compliance", 0),
+                "child_absent": no_mark_data.get("child_absent", 0),
+                "other": no_mark_data.get("other", 0),
+            }
+            return self._safe_update_or_create(
+                model_class=LqasNoMarkStats,
+                lookup_kwargs={"round": round_obj, "subactivity": subactivity},
+                update_values=update_values,
+            )
+
+    def _create_lqas_absences(self, round_obj: Round, obr_name: str, round_data, subactivity=None):
+        no_mark_data = round_data.get("nfm_abs_stats", None)
+        if no_mark_data:
+            try:
+                LqasAbsenceStats.objects.create(
+                    round=round_obj,
+                    subactivity=subactivity,
+                    farm=no_mark_data.get("farm", 0),
+                    other=no_mark_data.get("other", 0),
+                    market=no_mark_data.get("market", 0),
+                    school=no_mark_data.get("school", 0),
+                    travelled=no_mark_data.get("travelled", 0),
+                    in_playground=no_mark_data.get("in_playground", 0),  # To do match casing to form
+                    unknown=no_mark_data.get("unknown", 0),
+                )
+
+            except IntegrityError as e:
+                self.logger.warning(f"LQAS care giver data already exists for {obr_name} Round {round_data['number']}")
+                self.logger.warning(e)
+                raise e
+
+    def _update_lqas_absences(self, round_obj: Round, round_data, subactivity=None):
+        absence_data = round_data.get("nfm_abs_stats", None)
+        if absence_data:
+            update_values = {
+                "farm": absence_data.get("farm", 0),
+                "other": absence_data.get("other", 0),
+                "market": absence_data.get("market", 0),
+                "school": absence_data.get("school", 0),
+                "travelled": absence_data.get("travelled", 0),
+                "in_playground": absence_data.get("in_playground", 0),
+                "unknown": absence_data.get("unknown", 0),
+            }
+            return self._safe_update_or_create(
+                model_class=LqasAbsenceStats,
+                lookup_kwargs={"round": round_obj, "subactivity": subactivity},
+                update_values=update_values,
+            )
+
+    def _create_lqas_entry(self, round_obj: Round, district: OrgUnit, district_data, subactivity=None):
+        try:
+            lqas_entry = LqasEntry.objects.create(
+                round=round_obj,
+                subactivity=subactivity,
+                district=district,
+                total_children_fmd=district_data.get("total_child_fmd", 0),
+                total_children_checked=district_data.get("total_children_checked", 0),
+                total_sites_visited=district_data.get("total_chitotal_sites_visitedld_fmd", 0),
+                status=district_data.get("status", LqasStatuses.INSCOPE),
+            )
+            return lqas_entry
+
+        except IntegrityError as e:
+            self.logger.warning(
+                f"LQAS entry already exists for {round.campaign.obr_name} Round {round.number} district {district.name}"
+            )
+            self.logger.warning(e)
+            raise e
+
+    def _update_lqas_entry(self, round_obj: Round, district: OrgUnit, district_data, subactivity=None):
+        update_values = {
+            "total_children_fmd": district_data.get("total_child_fmd", 0),
+            "total_children_checked": district_data.get("total_children_checked", 0),
+            "total_sites_visited": district_data.get("total_chitotal_sites_visitedld_fmd", 0),
+        }
+        return self._safe_update_or_create(
+            model_class=LqasEntry,
+            lookup_kwargs={"round": round_obj, "subactivity": subactivity, "district": district},
+            update_values=update_values,
+        )
+
+    def _create_caregiver_stats(self, lqas_entry: LqasEntry, district_data):
+        try:
+            caregiver_data = district_data.get("care_giver_stats", None)
+            if caregiver_data:
+                best_info_source_data = [
+                    item
+                    for item in caregiver_data.items()
+                    if item[0] not in ["ratio", "caregivers_informed", "caregivers_informed_ratio"]
+                ]  # TODO Test thoroughly
+                best_info_source, best_info_source_ratio = best_info_source_data[0]
+                LqasCareGiverStats.objects.create(
+                    lqas_entry=lqas_entry,
+                    ratio=caregiver_data.get("ratio", 0),
+                    caregivers_informed=caregiver_data.get("caregivers_informed", 0),
+                    caregivers_informed_ratio=caregiver_data.get("caregivers_informed_ratio", 0),
+                    best_info_source=best_info_source,
+                    best_info_ratio=best_info_source_ratio,
+                )
+
+        except IntegrityError as e:
+            self.logger.warning(f"Care Giver stats already exist for LQAS entry already exists for {lqas_entry}")
+            self.logger.warning(e)
+            raise e
+
+    def _update_caregiver_stats(self, lqas_entry: LqasEntry, district_data):
+        caregiver_data = district_data.get("care_giver_stats", None)
+        if caregiver_data:
+            best_info_source_data = [
+                item
+                for item in caregiver_data.items()
+                if item[0] not in ["ratio", "caregivers_informed", "caregivers_informed_ratio"]
+            ]  # TODO Test thoroughly
+            best_info_source, best_info_source_ratio = best_info_source_data[0]
+            update_values = {
+                "ratio": caregiver_data.get("ratio", 0),
+                "caregivers_informed": caregiver_data.get("total_children_checked", 0),
+                "caregivers_informed_ratio": caregiver_data.get("total_chitotal_sites_visitedld_fmd", 0),
+                "best_info_source": best_info_source,
+                "best_info_ratio": best_info_source_ratio,
+            }
+
+            return self._safe_update_or_create(
+                model_class=LqasCareGiverStats,
+                lookup_kwargs={"lqas_entry": lqas_entry},
+                update_values=update_values,
+            )
+
+    def _safe_update_or_create(self, model_class, lookup_kwargs, update_values):
+        """Safely update or create a model instance, handling race conditions."""
+        try:
+            instance, created = model_class.objects.update_or_create(defaults=update_values, **lookup_kwargs)
+            return instance
+        except IntegrityError:
+            # Handle the case where another process created it first
+            instance = model_class.objects.get(**lookup_kwargs)
+            for key, value in update_values.items():
+                setattr(instance, key, value)
+            instance.save()
+            return instance

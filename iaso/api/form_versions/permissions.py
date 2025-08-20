13143c7ea05108bc3c0f198e15e768553e4ee857
Thibault Dethier <84660492+tdethier@users.noreply.github.com>
2025-08-20 16:00:26 +0200

    Update iaso/api/form_versions/permissions.py
    Co-authored-by: Marc Hertzog <marc@marcarea.com>

--- iaso/api/form_versions/permissions.py
+++ iaso/api/form_versions/permissions.py
@@ -5,9 +5,6 @@ from iaso.models import Form
 
 class HasFormVersionPermission(HasFormPermission):
     def has_object_permission(self, request, view, obj) -> bool:
-        if not self.has_permission(request, view):
-            return False
-
         ok_forms = Form.objects_include_deleted.filter_for_user_and_app_id(
             request.user, request.query_params.get(APP_ID)
         )

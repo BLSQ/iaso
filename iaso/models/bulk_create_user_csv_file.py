1b9e7a4e31b1a964d5d9da627fd0bebe61d96931
Thibault Dethier <tdethier@bluesquarehub.com>
2025-08-29 18:48:02 +0200

    change ReportVersion.file upload_to + refactor

--- iaso/models/bulk_create_user_csv_file.py
+++ iaso/models/bulk_create_user_csv_file.py
@@ -8,7 +8,7 @@ from django.db import models
 from iaso.models import Account
 
 
-def bulk_create_user_csv_file_upload_to_for_file_field(bulk_create, filename):
+def bulk_create_user_csv_file_upload_to(bulk_create, filename):
     account_name = "unknown_account"  #  shouldn't be unknown, but the model is nullable
 
     today = date.today()
@@ -27,7 +27,7 @@ def bulk_create_user_csv_file_upload_to_for_file_field(bulk_create, filename):
 
 
 class BulkCreateUserCsvFile(models.Model):
-    file = models.FileField(upload_to=bulk_create_user_csv_file_upload_to_for_file_field, blank=False, null=False)
+    file = models.FileField(upload_to=bulk_create_user_csv_file_upload_to, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     account = models.ForeignKey(Account, on_delete=models.PROTECT, null=True)

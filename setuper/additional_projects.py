143de13318f666304044a44537651adecb9acf40
Fleury Butoyi <fleurybu@gmail.com>
2025-09-01 15:57:01 +0200

    Setuper: Fix form update when adding project to form

--- setuper/additional_projects.py
+++ setuper/additional_projects.py
@@ -65,7 +65,11 @@ def get_project_ids(created_or_updated_projects, iaso_client):
     existing_projects = iaso_client.get("/api/projects/")["projects"]
     project_ids = []
     for current_project in created_or_updated_projects:
-        project_id = [project for project in existing_projects if project["app_id"] == current_project["id"]]
+        project_id = [
+            project
+            for project in existing_projects
+            if project["app_id"] == current_project["id"]
+        ]
         project_ids.append(project_id[0]["id"])
     return project_ids
 
@@ -77,21 +81,29 @@ def update_org_unit_types_with_new_projects(iaso_client, project_ids):
         linked_project_ids = [project["id"] for project in org_unit_type["projects"]]
         org_unit_type["project_ids"] = linked_project_ids
         org_unit_type["project_ids"].extend(project_ids)
-        type = iaso_client.patch(f"/api/v2/orgunittypes/{org_unit_type['id']}/", json=org_unit_type)
+        type = iaso_client.patch(
+            f"/api/v2/orgunittypes/{org_unit_type['id']}/", json=org_unit_type
+        )
         updated_types.append(type)
     return updated_types
 
 
 def link_created_projects_to_main_data_source(account_name, iaso_client, project_ids):
     data_sources = iaso_client.get("/api/datasources/")["sources"]
-    data_source = [data_source for data_source in data_sources if data_source["name"] == account_name]
+    data_source = [
+        data_source
+        for data_source in data_sources
+        if data_source["name"] == account_name
+    ]
 
     if len(data_source) > 0:
         default_project = data_source[0]["projects"][0][0]["id"]
         project_ids.append(default_project)
         data_source[0]["project_ids"] = project_ids
         data_source[0]["default_version_id"] = data_source[0]["default_version"]["id"]
-        iaso_client.put(f"/api/datasources/{data_source[0]['id']}/", json=data_source[0])
+        iaso_client.put(
+            f"/api/datasources/{data_source[0]['id']}/", json=data_source[0]
+        )
 
 
 def create_projects(account_name, iaso_client):
@@ -102,7 +114,11 @@ def create_projects(account_name, iaso_client):
 
     for project in projects:
         project["linked_forms"] = None
-        project["feature_flags"] = [flag for flag in flags["featureflags"] if flag["code"] in project["feature_flags"]]
+        project["feature_flags"] = [
+            flag
+            for flag in flags["featureflags"]
+            if flag["code"] in project["feature_flags"]
+        ]
         new_project = iaso_client.post("/api/apps/", json=project)
         created_projects.append(new_project)
 
@@ -115,13 +131,27 @@ def create_projects(account_name, iaso_client):
 def link_forms_to_new_projects(projects, forms, iaso_client):
     existing_forms = iaso_client.get("/api/forms/")["forms"]
     for form in forms:
-        project_ids = [project["id"] for project in projects if form in project["linked_forms"]]
-        form_to_link_to_project = [current_form for current_form in existing_forms if current_form["name"] == form]
+        project_ids = [
+            project["id"] for project in projects if form in project["linked_forms"]
+        ]
+        form_to_link_to_project = [
+            current_form
+            for current_form in existing_forms
+            if current_form["name"] == form
+        ]
 
         if len(form_to_link_to_project) > 0:
             current_form = form_to_link_to_project[0]
-            current_form["project_ids"] = project_ids
-            iaso_client.patch(f"/api/forms/{current_form['id']}/", json=current_form)
+            form_to_update = {
+                "project_ids": project_ids,
+                "name": current_form["name"],
+                "org_unit_type_ids": current_form["org_unit_type_ids"],
+                "derived": current_form["derived"],
+                "single_per_period": current_form["single_per_period"],
+                "periods_before_allowed": current_form["periods_before_allowed"],
+                "periods_after_allowed": current_form["periods_after_allowed"],
+            }
+            iaso_client.patch(f"/api/forms/{current_form['id']}/", json=form_to_update)
 
 
 def forms_mapper(projects, iaso_client, account_name):
@@ -138,7 +168,9 @@ def link_new_projects_to_main_data_source(account_name, iaso_client):
     all_projects = []
     for project in projects_mapped:
         current_project = [
-            current_project for current_project in projects if project["name"] == current_project["name"]
+            current_project
+            for current_project in projects
+            if project["name"] == current_project["name"]
         ]
         if len(current_project) > 0:
             project["id"] = current_project[0]["id"]
@@ -159,7 +191,9 @@ def add_new_dhis2_data_source(account_name, iaso_client):
             "dhis_password": "district",
         },
     }
-    projects = iaso_client.get(f"/api/projects/?app_id={account_name}.children")["projects"]
+    projects = iaso_client.get(f"/api/projects/?app_id={account_name}.children")[
+        "projects"
+    ]
     project = [project for project in projects if project["name"] == "Vaccination"]
     if len(project) > 0:
         data_source["project_ids"] = [project[0]["id"]]

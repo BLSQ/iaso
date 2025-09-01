aeec4817e93523cc050b843ac72ac15b7fe2931b
Stéphan Mestach <smestach@bluesquare.org>
2025-09-01 10:18:18 +0200

    use epilog instead of comments in code

--- iaso/management/commands/parquet_export.py
+++ iaso/management/commands/parquet_export.py
@@ -1,3 +1,5 @@
+import argparse
+
 from django.core.management.base import BaseCommand
 
 import iaso.models as m
@@ -9,6 +11,7 @@ class Command(BaseCommand):
     help = "Export instances to parquet"
 
     def add_arguments(self, parser):
+        parser.formatter_class = argparse.RawTextHelpFormatter
         parser.add_argument("--form-id", type=int, required=False)
         parser.add_argument("--version-id", type=int, required=False)
         parser.add_argument(
@@ -17,12 +20,25 @@ class Command(BaseCommand):
             required=True,
             help="Choose export mode: pyramid or submission",
         )
+        parser.epilog = """
+        allow to export as parquet files
+           - pyramid (for a given source version-id)
+           - submissions (for a given form-id)
+
+        exemple in dev environnement :
+            docker compose run iaso manage parquet_export --mode pyramid --version-id 2
+            duckdb -c 'describe "./media/pyramid.parquet"; select * from "./media/pyramid.parquet";'
+
+            docker compose run iaso manage parquet_export --mode submissions --form-id 12
+            duckdb -c 'describe "./media/submissions.parquet"; select * from "./media/submissions.parquet";'
+
+            duckdb -c ' select  level_2_name, level_3_name, level_4_name, org_unit_name,  submission_org_unit_name from "./media/submissions.parquet"  left outer join "./media/pyramid.parquet" on "submission_org_unit_id"="org_unit_id";'
+            duckdb -box -c 'DESCRIBE select * from "./media/submissions.parquet"  left outer join "./media/pyramid.parquet" on "submission_org_unit_id"="org_unit_id";'
+"""
 
     def handle(self, *args, **options):
         mode = options["mode"]
 
-        # docker compose run iaso manage parquet_export --mode pyramid --version-id 2
-        # duckdb -c 'describe "./media/pyramid.parquet"; select * from "./media/pyramid.parquet";'
         if mode == "pyramid":
             version_id = options["version_id"]
             parquet.export_django_query_to_parquet_via_duckdb(
@@ -30,17 +46,9 @@ class Command(BaseCommand):
                 "./media/pyramid.parquet",
             )
 
-        # docker compose run iaso manage parquet_export --mode submissions --form-id 12
-        # duckdb -c 'describe "./media/submissions.parquet"; select * from "./media/submissions.parquet";'
-
         if mode == "submissions":
             form_id = options["form_id"]
 
             parquet.export_django_query_to_parquet_via_duckdb(
                 parquet.build_submissions_queryset(m.Instance.objects, form_id), "./media/submissions.parquet"
             )
-
-        # once the 2 files exist
-
-        # duckdb -c ' select  level_2_name, level_3_name, level_4_name, org_unit_name,  submission_org_unit_name from "./media/submissions.parquet"  left outer join "./media/pyramid.parquet" on "submission_org_unit_id"="org_unit_id";'
-        # duckdb -box -c 'DESCRIBE select * from "./media/submissions.parquet"  left outer join "./media/pyramid.parquet" on "submission_org_unit_id"="org_unit_id";'

6448e2d10613be8599272d3dd5b8c627be94ea3f
Martin De Wulf <mdewulf@bluesquarehub.com>
2025-09-07 22:09:24 +0200

    Merging all iaso migrations in a single one
    + wfp plugin

--- iaso/migrations/0271_upgrade_postgis_extensions_20240325_1139.py
+++
@@ -1,9 +0,0 @@
-from django.db import migrations
-
-
-class Migration(migrations.Migration):
-    dependencies = [
-        ("iaso", "0270_auto_20240314_1515"),
-    ]
-
-    operations = []  # There used to be something here, but it was a bad idea, and we only kept an empty migration

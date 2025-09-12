a126d6b296dc51243453afb18176b4f187b165aa
Stéphan Mestach <smestach@bluesquare.org>
2025-09-12 15:15:44 +0200

    use context manager for connection handling

--- iaso/exports/duckdb_util.py
+++ iaso/exports/duckdb_util.py
@@ -22,29 +22,29 @@ def export_django_query_to_parquet_via_duckdb(qs: QuerySet, output_file_path: st
     # initially was full_sql = sql % tuple(map(adapt_param, params)) but supporting all types is complicated
     dsn = connection.get_connection_params()
 
-    con = duckdb.connect()
-    attach_sql = f"""
-        INSTALL postgres;
-        LOAD postgres;
-        ATTACH 'dbname={dsn["dbname"]} host={dsn["host"]} user={dsn["user"]} password={dsn["password"]} port={dsn["port"]}' AS pg (TYPE postgres, READ_ONLY);
+    with duckdb.connect() as duckdb_connection:
+        attach_sql = f"""
+            INSTALL postgres;
+            LOAD postgres;
+            ATTACH 'dbname={dsn["dbname"]} host={dsn["host"]} user={dsn["user"]} password={dsn["password"]} port={dsn["port"]}' AS pg (TYPE postgres, READ_ONLY);
 
-    """
-    con.execute(attach_sql)
+        """
+        duckdb_connection.execute(attach_sql)
 
-    logger.info(f"exporting parquet : {output_file_path} \n\n {full_sql}")
-    parquet_export_sql = f"""
-        COPY (
-            SELECT * FROM postgres_query('pg', $$ {full_sql} $$)
-        ) TO '{output_file_path}' (FORMAT PARQUET)
-    """
+        logger.info(f"exporting parquet : {output_file_path} \n\n {full_sql}")
+        parquet_export_sql = f"""
+            COPY (
+                SELECT * FROM postgres_query('pg', $$ {full_sql} $$)
+            ) TO '{output_file_path}' (FORMAT PARQUET)
+        """
 
-    con.execute(parquet_export_sql)
+        duckdb_connection.execute(parquet_export_sql)
 
-    row_count = con.execute(f"SELECT COUNT(*) FROM '{output_file_path}'").fetchone()[0]
-    col_count = len(con.execute(f"DESCRIBE SELECT * FROM '{output_file_path}'").fetchall())
-    con.close()
+        row_count = duckdb_connection.execute(f"SELECT COUNT(*) FROM '{output_file_path}'").fetchone()[0]
+        col_count = len(duckdb_connection.execute(f"DESCRIBE SELECT * FROM '{output_file_path}'").fetchall())
+
     duration = time.perf_counter() - start
     size_mb = os.path.getsize(output_file_path) / (1024 * 1024)
-    logger.warn(
+    logger.warning(
         f"dumped to {output_file_path} took {duration:.3f} seconds for {row_count} records and {col_count} columns, final file size {size_mb:.2f} Mb"
     )

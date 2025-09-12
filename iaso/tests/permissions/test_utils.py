0e0798ef0f0783d8e34cb55c1c0e0109f7894dd2
Thibault Dethier <tdethier@bluesquarehub.com>
2025-09-12 15:35:56 +0200

    added tests on permission utils

--- iaso/tests/permissions/test_utils.py
+++ iaso/tests/permissions/test_utils.py
@@ -2,8 +2,13 @@ import unittest
 
 from types import SimpleNamespace
 
-from iaso.permissions import base
-from iaso.permissions.utils import load_permissions_from_module
+from django.contrib.auth.models import Permission
+from rest_framework.exceptions import PermissionDenied
+
+from iaso.permissions.base import ALL_PERMISSIONS
+from iaso.permissions.core_permissions import CORE_USERS_ADMIN_PERMISSION
+from iaso.permissions.utils import load_permissions_from_module, raise_error_if_user_lacks_admin_permission
+from iaso.test import IasoTestCaseMixin, TestCase
 
 
 class TestLoadPermissions(unittest.TestCase):
@@ -16,8 +21,6 @@ class TestLoadPermissions(unittest.TestCase):
 
     def setUp(self):
         # Reset globals before each test
-        base.ALL_PERMISSIONS.clear()
-        base.PERMISSION_CLASSES.clear()
         self.mock_module = SimpleNamespace(
             permissions=self.core_permissions,
             permission_models=self.core_permission_models,
@@ -29,14 +32,14 @@ class TestLoadPermissions(unittest.TestCase):
         This simulates the loading of permissions from the core module.
         """
         # Checking initial state: {}
-        self.assertEqual(base.ALL_PERMISSIONS, {})
-        self.assertEqual(base.PERMISSION_CLASSES, [])
+        permissions = {}
+        permission_classes = []
 
-        load_permissions_from_module(self.mock_module, base.ALL_PERMISSIONS, base.PERMISSION_CLASSES)
+        load_permissions_from_module(self.mock_module, permissions, permission_classes)
 
         # Checking result
-        self.assertEqual(base.ALL_PERMISSIONS, self.core_permissions)
-        self.assertEqual(base.PERMISSION_CLASSES, self.core_permission_models)
+        self.assertEqual(permissions, self.core_permissions)
+        self.assertEqual(permission_classes, self.core_permission_models)
 
     def test_load_more_permissions(self):
         """
@@ -44,7 +47,9 @@ class TestLoadPermissions(unittest.TestCase):
         This simulates the loading of permissions from a plugin once the core permissions are loaded.
         """
         # Preload with some initial permissions
-        load_permissions_from_module(self.mock_module, base.ALL_PERMISSIONS, base.PERMISSION_CLASSES)
+        permissions = {}
+        permission_classes = []
+        load_permissions_from_module(self.mock_module, permissions, permission_classes)
 
         plugin_permissions = {"plugin": "a plugin permission"}
         plugin_permission_models = ["PluginModel", "AnotherPluginModel"]
@@ -54,14 +59,14 @@ class TestLoadPermissions(unittest.TestCase):
             permission_models=plugin_permission_models,
         )
 
-        load_permissions_from_module(mock_plugin_permissions, base.ALL_PERMISSIONS, base.PERMISSION_CLASSES)
+        load_permissions_from_module(mock_plugin_permissions, permissions, permission_classes)
 
         # Expecting dicts to be merged
         expected_permissions = self.core_permissions | plugin_permissions
         expected_permission_models = self.core_permission_models + plugin_permission_models
 
-        self.assertEqual(base.ALL_PERMISSIONS, expected_permissions)
-        self.assertEqual(base.PERMISSION_CLASSES, expected_permission_models)
+        self.assertEqual(permissions, expected_permissions)
+        self.assertEqual(permission_classes, expected_permission_models)
 
     def test_load_more_permissions_with_conflict(self):
         """
@@ -71,7 +76,9 @@ class TestLoadPermissions(unittest.TestCase):
         conflicting permissions.
         """
         # Preload with some initial permissions
-        load_permissions_from_module(self.mock_module, base.ALL_PERMISSIONS, base.PERMISSION_CLASSES)
+        permissions = {}
+        permission_classes = []
+        load_permissions_from_module(self.mock_module, permissions, permission_classes)
 
         plugin_permissions = {
             "core": "uh oh this is going to fail"  # conflicting key - permissions shouldn't have the same name
@@ -83,5 +90,47 @@ class TestLoadPermissions(unittest.TestCase):
         )
 
         with self.assertRaises(ValueError) as e:
-            load_permissions_from_module(mock_plugin_permissions, base.ALL_PERMISSIONS, base.PERMISSION_CLASSES)
+            load_permissions_from_module(mock_plugin_permissions, permissions, permission_classes)
             self.assertEqual(str(e.exception), "Duplicate permission name: core")
+
+
+class TestRaiseErrorOnAdminPermission(TestCase, IasoTestCaseMixin):
+    def setUp(self):
+        # Preparing test data
+        self.account, _, _, _ = self.create_account_datasource_version_project("source", "account", "project")
+        self.user = self.create_user_with_profile(account=self.account, username="user 1")
+
+    def test_no_error_if_not_requesting_admin_permission(self):
+        permissions = ALL_PERMISSIONS
+        permissions.pop(CORE_USERS_ADMIN_PERMISSION.name)
+        requested_permission_names = list(permissions.keys())
+
+        # Should not raise any error
+        raise_error_if_user_lacks_admin_permission(self.user, requested_permission_names)
+
+        self.assertEqual(self.user.user_permissions.count(), 0)
+
+    def test_error_missing_permission(self):
+        permissions = ALL_PERMISSIONS
+        requested_permission_names = list(permissions.keys())
+
+        self.assertEqual(self.user.user_permissions.count(), 0)
+
+        with self.assertRaises(PermissionDenied) as e:
+            raise_error_if_user_lacks_admin_permission(self.user, requested_permission_names)
+            self.assertEqual(
+                str(e.exception),
+                f"Only users with {CORE_USERS_ADMIN_PERMISSION} permission can grant {CORE_USERS_ADMIN_PERMISSION} permission",
+            )
+
+    def test_no_error_with_admin_permission(self):
+        permissions = ALL_PERMISSIONS
+        requested_permission_names = list(permissions.keys())
+
+        django_perm = Permission.objects.get(codename=CORE_USERS_ADMIN_PERMISSION.name)
+        self.user.user_permissions.set([django_perm])
+
+        # Should not raise any error
+        raise_error_if_user_lacks_admin_permission(self.user, requested_permission_names)
+
+        self.assertEqual(self.user.user_permissions.count(), 1)

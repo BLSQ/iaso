ecf5a577822fa789cce2578c9e54461882d00df0
Christophe Gerard <cgerard@bluesquarehub.com>
2025-09-15 18:01:54 +0200

    error handling on list page

--- hat/assets/js/apps/Iaso/domains/openHexa/index.tsx
+++ hat/assets/js/apps/Iaso/domains/openHexa/index.tsx
@@ -1,7 +1,9 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { Box } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { commonStyles, useSafeIntl } from 'bluesquare-components';
+import PageError from 'Iaso/components/errors/PageError';
+import { DjangoError } from 'Iaso/types/general';
 import TopBar from '../../components/nav/TopBarComponent';
 import { TableWithDeepLink } from '../../components/tables/TableWithDeepLink';
 import { baseUrls } from '../../constants/urls';
@@ -18,31 +20,45 @@ const baseUrl = baseUrls.pipelineList;
 export const PipelineList: FunctionComponent = () => {
     const classes: Record<string, string> = useStyles();
     const { formatMessage } = useSafeIntl();
-    const { data, isFetching } = useGetPipelines();
+    const [error, setError] = useState<DjangoError | null>(null);
+    const { data, isFetching } = useGetPipelines(setError);
     const defaultSorted = [{ id: 'id', desc: true }];
 
     const columns = useColumns();
 
     return (
         <>
-            <TopBar
-                title={formatMessage(MESSAGES.pipelines)}
-                displayBackButton={false}
-            />
-            <Box className={classes.containerFullHeightNoTabPadded}>
-                {/* @ts-ignore */}
-                <TableWithDeepLink
-                    baseUrl={baseUrl}
-                    data={data ?? []}
-                    pages={1}
-                    showPagination={false}
-                    defaultSorted={defaultSorted}
-                    columns={columns}
-                    count={data?.length ?? 0}
-                    params={{}}
-                    extraProps={{ loading: isFetching }}
-                />
-            </Box>
+            {error && (
+                <Box>
+                    <PageError
+                        errorCode={`${error.status}`}
+                        displayMenuButton
+                        customMessage={error.details.error}
+                    />
+                </Box>
+            )}
+            {!error && (
+                <>
+                    <TopBar
+                        title={formatMessage(MESSAGES.pipelines)}
+                        displayBackButton={false}
+                    />
+                    <Box className={classes.containerFullHeightNoTabPadded}>
+                        {/* @ts-ignore */}
+                        <TableWithDeepLink
+                            baseUrl={baseUrl}
+                            data={data ?? []}
+                            pages={1}
+                            showPagination={false}
+                            defaultSorted={defaultSorted}
+                            columns={columns}
+                            count={data?.length ?? 0}
+                            params={{}}
+                            extraProps={{ loading: isFetching }}
+                        />
+                    </Box>
+                </>
+            )}
         </>
     );
 };

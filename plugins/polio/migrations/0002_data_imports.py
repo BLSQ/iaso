44b51de08a50cd5c7faa955953db43154f1f3f1f
Martin De Wulf <mdewulf@bluesquarehub.com>
2025-09-18 17:31:41 +0200

    Adding migrations that are creating required data

--- plugins/polio/migrations/0002_data_imports.py
+++ plugins/polio/migrations/0002_data_imports.py
@@ -3,7 +3,12 @@ from django.db import migrations
 
 def create_initial_campaign_types(apps, schema_editor):
     CampaignType = apps.get_model("polio", "CampaignType")
-    CampaignType.objects.create(name="Polio")
+    CampaignType.objects.get_or_create(name="Polio", slug="polio")
+    CampaignType.objects.get_or_create(name="Measles", slug="measles")
+    CampaignType.objects.get_or_create(name="PIRI", slug="piri")
+    CampaignType.objects.get_or_create(name="Vitamin A", slug="vitamin-a")
+    CampaignType.objects.get_or_create(name="Rubella", slug="rubella")
+    CampaignType.objects.get_or_create(name="Deworming", slug="deworming")
 
 
 class Migration(migrations.Migration):

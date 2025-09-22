3600b6b56ff9c09028fe765a5cdf4fe690e1ff09
Philippe Vanderlinden <git@philv.me>
2025-09-22 13:06:22 +0000

    Disable isSearchActive for entities page list

--- hat/assets/js/apps/Iaso/constants/menu.tsx
+++ hat/assets/js/apps/Iaso/constants/menu.tsx
@@ -82,9 +82,7 @@ const menuItems = (
             isActive: pathname =>
                 pathname?.includes(`/entityTypeIds/${entityType.value}/`) &&
                 pathname?.includes(`entities/list/`),
-            extraPath:
-                `/entityTypeIds/${entityType.value}/locationLimit/1000/order/-last_saved_instance/pageSize/20/page/1` +
-                `/isSearchActive/true`,
+            extraPath: `/entityTypeIds/${entityType.value}/locationLimit/1000/order/-last_saved_instance/pageSize/20/page/1`,
         }));
     }
     const settingsSubMenu = [

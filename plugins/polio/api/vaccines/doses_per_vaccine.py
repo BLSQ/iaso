a806d860f7f6c4f6c3f47fddc64cdd06cda83fae
Quang Son Le <qle@bluesquarehub.com>
2025-10-28 13:58:44 +0100

    POLIO-2019: add API to get doses per vial
        - based on hard-coded Config slug
        - read only
        - require polio or vaccine perm

---
+++ plugins/polio/api/vaccines/doses_per_vaccine.py
@@ -0,0 +1,53 @@
+from rest_framework import permissions
+from rest_framework.mixins import ListModelMixin
+from rest_framework.viewsets import GenericViewSet
+
+from iaso.api.common import HasPermission
+from iaso.api.config import ConfigSerializer
+from iaso.models.json_config import Config
+from plugins.polio.permissions import (
+    POLIO_CONFIG_PERMISSION,
+    POLIO_PERMISSION,
+    POLIO_VACCINE_STOCK_EARMARKS_ADMIN_PERMISSION,
+    POLIO_VACCINE_STOCK_EARMARKS_NONADMIN_PERMISSION,
+    POLIO_VACCINE_STOCK_EARMARKS_READ_ONLY_PERMISSION,
+    POLIO_VACCINE_STOCK_MANAGEMENT_READ_ONLY_PERMISSION,
+    POLIO_VACCINE_STOCK_MANAGEMENT_READ_PERMISSION,
+    POLIO_VACCINE_STOCK_MANAGEMENT_WRITE_PERMISSION,
+    POLIO_VACCINE_SUPPLY_CHAIN_READ_ONLY_PERMISSION,
+    POLIO_VACCINE_SUPPLY_CHAIN_READ_PERMISSION,
+    POLIO_VACCINE_SUPPLY_CHAIN_WRITE_PERMISSION,
+)
+
+
+class DosesPerVaccineViewset(ListModelMixin, GenericViewSet):
+    """
+    Super custom endpoint to send the possible vaccine presentation (doses per vial) to the front end
+
+    A config with a slug == self.CONFIG_SLUG  is required (create it via the django admin)
+
+    The config data should have 1 key per available vaccine, and the value should be a list of positive integers, eg: 'bOPV':[10,20]
+
+    """
+
+    permission_classes = [
+        permissions.IsAuthenticated,
+        HasPermission(
+            POLIO_CONFIG_PERMISSION,
+            POLIO_PERMISSION,
+            POLIO_VACCINE_SUPPLY_CHAIN_READ_ONLY_PERMISSION,
+            POLIO_VACCINE_SUPPLY_CHAIN_READ_PERMISSION,
+            POLIO_VACCINE_SUPPLY_CHAIN_WRITE_PERMISSION,
+            POLIO_VACCINE_STOCK_MANAGEMENT_READ_ONLY_PERMISSION,
+            POLIO_VACCINE_STOCK_MANAGEMENT_READ_PERMISSION,
+            POLIO_VACCINE_STOCK_MANAGEMENT_WRITE_PERMISSION,
+            POLIO_VACCINE_STOCK_EARMARKS_READ_ONLY_PERMISSION,
+            POLIO_VACCINE_STOCK_EARMARKS_NONADMIN_PERMISSION,
+            POLIO_VACCINE_STOCK_EARMARKS_ADMIN_PERMISSION,
+        ),
+    ]
+    serializer_class = ConfigSerializer
+    CONFIG_SLUG = "vaccine_doses_per_vial"
+
+    def get_queryset(self):
+        return Config.objects.filter(slug=self.CONFIG_SLUG)

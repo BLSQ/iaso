88e4ac19befe7fe985c2245e3c908e5810da172a
Christophe Gerard <cgerard@bluesquarehub.com>
2025-11-06 10:33:29 +0100

    use api colors only in front

--- hat/assets/js/apps/Iaso/domains/orgUnits/groups/config.js
+++ hat/assets/js/apps/Iaso/domains/orgUnits/groups/config.js
@@ -6,12 +6,13 @@ import {
     IconButton,
     textPlaceholder,
     LinkWithLocation,
+    useSafeIntl,
 } from 'bluesquare-components';
 import { useNavigate } from 'react-router-dom';
 import { DateTimeCell } from '../../../components/Cells/DateTimeCell.tsx';
 import DeleteDialog from '../../../components/dialogs/DeleteDialogComponent';
 import { baseUrls } from '../../../constants/urls';
-import { filterOrgUnitsByGroupUrl } from '../utils';
+import { useFilterOrgUnitsByGroupUrl } from '../utils';
 import GroupsDialog from './components/GroupsDialog';
 import MESSAGES from './messages';
 
@@ -24,94 +25,99 @@ const useStyles = makeStyles(() => ({
 export const baseUrl = baseUrls.groups;
 const groupSetUrl = baseUrls.groupSetDetail;
 
-const TableColumns = (formatMessage, params, deleteGroup, saveGroup) => [
-    {
-        Header: 'Id',
-        accessor: 'id',
-    },
-    {
-        Header: formatMessage(MESSAGES.name),
-        accessor: 'name',
-    },
-    {
-        Header: formatMessage(MESSAGES.updatedAt),
-        accessor: 'updated_at',
-        Cell: DateTimeCell,
-    },
-    {
-        Header: formatMessage(MESSAGES.sourceVersion),
-        accessor: 'source_version',
-        Cell: settings =>
-            settings.value !== null
-                ? `${settings.value.data_source.name} - ${settings.value.number}`
-                : textPlaceholder,
-    },
-    {
-        Header: formatMessage(MESSAGES.sourceRef),
-        accessor: 'source_ref',
-    },
-    {
-        Header: formatMessage(MESSAGES.groupSet),
-        accessor: 'group_sets',
-        Cell: settings => {
-            const classes = useStyles();
-            const navigate = useNavigate();
-            return (
-                <span>
-                    {settings.row.original.group_sets.map(g => (
-                        <Chip
-                            className={classes.groupSetChip}
-                            label={g.name}
-                            color="primary"
-                            key={g.id}
-                            onClick={() => {
-                                navigate(`/${groupSetUrl}/groupSetId/${g.id}`);
-                            }}
-                        />
-                    ))}
-                </span>
-            );
+export const useTableColumns = (params, deleteGroup, saveGroup) => {
+    const { formatMessage } = useSafeIntl();
+    const filterOrgUnitsByGroupUrl = useFilterOrgUnitsByGroupUrl();
+    return [
+        {
+            Header: 'Id',
+            accessor: 'id',
         },
-    },
-    {
-        Header: formatMessage(MESSAGES.orgUnit),
-        accessor: 'org_unit_count',
-        Cell: settings => (
-            <LinkWithLocation
-                to={filterOrgUnitsByGroupUrl(settings.row.original.id)}
-            >
-                {formatThousand(settings.value)}
-            </LinkWithLocation>
-        ),
-    },
-    {
-        Header: formatMessage(MESSAGES.actions),
-        accessor: 'actions',
-        resizable: false,
-        sortable: false,
-        Cell: settings => (
-            <section>
-                <GroupsDialog
-                    renderTrigger={({ openDialog }) => (
-                        <IconButton
-                            onClick={openDialog}
-                            icon="edit"
-                            tooltipMessage={MESSAGES.edit}
-                        />
-                    )}
-                    initialData={settings.row.original}
-                    titleMessage={MESSAGES.update}
-                    params={params}
-                    saveGroup={saveGroup}
-                />
-                <DeleteDialog
-                    keyName="group"
-                    titleMessage={MESSAGES.delete}
-                    message={MESSAGES.deleteWarning}
-                    onConfirm={() => deleteGroup(settings.row.original)}
-                />
-            </section>
-        ),
-    },
-];
-export default TableColumns;
+        {
+            Header: formatMessage(MESSAGES.name),
+            accessor: 'name',
+        },
+        {
+            Header: formatMessage(MESSAGES.updatedAt),
+            accessor: 'updated_at',
+            Cell: DateTimeCell,
+        },
+        {
+            Header: formatMessage(MESSAGES.sourceVersion),
+            accessor: 'source_version',
+            Cell: settings =>
+                settings.value !== null
+                    ? `${settings.value.data_source.name} - ${settings.value.number}`
+                    : textPlaceholder,
+        },
+        {
+            Header: formatMessage(MESSAGES.sourceRef),
+            accessor: 'source_ref',
+        },
+        {
+            Header: formatMessage(MESSAGES.groupSet),
+            accessor: 'group_sets',
+            Cell: settings => {
+                const classes = useStyles();
+                const navigate = useNavigate();
+                return (
+                    <span>
+                        {settings.row.original.group_sets.map(g => (
+                            <Chip
+                                className={classes.groupSetChip}
+                                label={g.name}
+                                color="primary"
+                                key={g.id}
+                                onClick={() => {
+                                    navigate(
+                                        `/${groupSetUrl}/groupSetId/${g.id}`,
+                                    );
+                                }}
+                            />
+                        ))}
+                    </span>
+                );
+            },
+        },
+        {
+            Header: formatMessage(MESSAGES.orgUnit),
+            accessor: 'org_unit_count',
+            Cell: settings => (
+                <LinkWithLocation
+                    to={filterOrgUnitsByGroupUrl(settings.row.original.id)}
+                >
+                    {formatThousand(settings.value)}
+                </LinkWithLocation>
+            ),
+        },
+        {
+            Header: formatMessage(MESSAGES.actions),
+            accessor: 'actions',
+            resizable: false,
+            sortable: false,
+            Cell: settings => (
+                <section>
+                    <GroupsDialog
+                        renderTrigger={({ openDialog }) => (
+                            <IconButton
+                                onClick={openDialog}
+                                icon="edit"
+                                tooltipMessage={MESSAGES.edit}
+                            />
+                        )}
+                        initialData={settings.row.original}
+                        titleMessage={MESSAGES.update}
+                        params={params}
+                        saveGroup={saveGroup}
+                    />
+                    <DeleteDialog
+                        keyName="group"
+                        titleMessage={MESSAGES.delete}
+                        message={MESSAGES.deleteWarning}
+                        onConfirm={() => deleteGroup(settings.row.original)}
+                    />
+                </section>
+            ),
+        },
+    ];
+};
--- hat/assets/js/apps/Iaso/hooks/useGetColors.ts
+++ hat/assets/js/apps/Iaso/hooks/useGetColors.ts
@@ -1,11 +1,25 @@
 import { UseQueryResult } from 'react-query';
 import { getRequest } from 'Iaso/libs/Api';
 import { useSnackQuery } from 'Iaso/libs/apiHooks';
+import { makeUrlWithParams } from 'Iaso/libs/utils';
 
-export const useGetColors = (): UseQueryResult<string[], Error> => {
+type Color = {
+    value: string;
+    label: string;
+};
+
+const getColors = async (dispersed: boolean): Promise<Color[]> => {
+    const url = makeUrlWithParams('/api/colors/', {
+        dispersed,
+    });
+    return getRequest(url) as Promise<Color[]>;
+};
+export const useGetColors = (
+    dispersed = false,
+): UseQueryResult<string[], Error> => {
     return useSnackQuery({
-        queryKey: ['colors'],
-        queryFn: () => getRequest('/api/colors/'),
+        queryKey: ['colors', { dispersed }],
+        queryFn: () => getColors(dispersed),
         options: {
             select: data => data.map(color => color.value),
             staleTime: Infinity,
@@ -14,8 +28,14 @@ export const useGetColors = (): UseQueryResult<string[], Error> => {
     });
 };
 
-export const getColor = (i: number, colors?: string[]): string => {
+export const getColor = (
+    i: number,
+    colors?: string[],
+    usedColors?: string[],
+): string => {
     if (!colors) return 'transparent';
     if (colors.length === 0) return 'transparent';
-    return colors[i % colors.length];
+    return colors.filter(color => !usedColors?.includes(color))[
+        i % colors.length
+    ];
 };

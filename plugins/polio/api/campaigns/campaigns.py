19453cc5a7eb23f856d4eef670a791468c7b6889
Christophe Gerard <cgerard@bluesquarehub.com>
2025-11-10 14:07:25 +0100

    remove unused serializer

--- plugins/polio/api/campaigns/campaigns.py
+++ plugins/polio/api/campaigns/campaigns.py
@@ -542,58 +542,6 @@ class AnonymousCampaignSerializer(CampaignSerializer):
         read_only_fields = fields
 
 
-class SmallCampaignSerializer(CampaignSerializer):
-    campaign_types = CampaignTypeSerializer(many=True, required=False)
-
-    class Meta:
-        model = Campaign
-        # TODO: refactor to avoid duplication with AnonymousCampaignSerializer?
-        fields = [
-            "id",
-            "epid",
-            "obr_name",
-            "gpei_coordinator",
-            "gpei_email",
-            "description",
-            "initial_org_unit",
-            "creation_email_send_at",
-            "onset_at",
-            "cvdpv2_notified_at",
-            "pv_notified_at",
-            "pv2_notified_at",
-            "virus",
-            "vaccines",
-            "detection_status",
-            "detection_responsible",
-            "detection_first_draft_submitted_at",
-            "risk_assessment_status",
-            "risk_assessment_responsible",
-            "investigation_at",
-            "risk_assessment_first_draft_submitted_at",
-            "risk_assessment_rrt_oprtt_approval_at",
-            "ag_nopv_group_met_at",
-            "dg_authorized_at",
-            "verification_score",
-            "budget_status",
-            "who_disbursed_to_co_at",
-            "who_disbursed_to_moh_at",
-            "unicef_disbursed_to_co_at",
-            "unicef_disbursed_to_moh_at",
-            "no_regret_fund_amount",
-            "payment_mode",
-            "created_at",
-            "updated_at",
-            "district_count",
-            "top_level_org_unit_name",
-            "top_level_org_unit_id",
-            "is_preventive",
-            "account",
-            "outbreak_declaration_date",
-            "campaign_types",
-        ]
-        read_only_fields = fields
-
-
 class CalendarCampaignSerializer(CampaignSerializer):
     """This serializer contains juste enough data for the Calendar view in the web ui. Read only.
     Used by both anonymous and non-anonymous user"""

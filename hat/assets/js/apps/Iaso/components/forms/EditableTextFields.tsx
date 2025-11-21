d8359daa52c471fb3ec93c48562aad36dac274a8
Quang Son Le <qle@bluesquarehub.com>
2025-11-21 14:34:51 +0100

    IA-4621: convert EditableFields to TS

---
+++ hat/assets/js/apps/Iaso/components/forms/EditableTextFields.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from 'react';
+import InputComponent from './InputComponent';
+import { IntlMessage } from 'bluesquare-components';
+
+type Props = {
+    fields: {
+        keyValue: string;
+        password: boolean;
+        label: IntlMessage;
+        value: any;
+        errors: any[];
+        onChange: (key: string, bvalue: any) => void;
+    }[];
+};
+
+export const EditableTextFields: FunctionComponent<Props> = ({ fields }) => {
+    return fields.map(field => (
+        <InputComponent
+            key={field.keyValue}
+            clearable={false}
+            type={field.password ? 'password' : 'text'}
+            keyValue={field.keyValue}
+            label={field.label}
+            value={field.value}
+            errors={field.errors ? field.errors : []}
+            onChange={(key, value) => field.onChange(key, value)}
+        />
+    ));
+};

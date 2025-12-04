8067c27e93d389736e8a71a895af3f81486384f2
Fleury Butoyi <fleurybu@gmail.com>
2025-12-04 11:51:01 +0200

    Refactoring datasources and projects and users update functions

--- iaso/tasks/setuper_sandbox.py
+++ iaso/tasks/setuper_sandbox.py
@@ -4,113 +4,96 @@ import subprocess
 
 from datetime import datetime
 
+from django.db.models import Q
+
 from beanstalk_worker import task_decorator
-from iaso.models import Account, DataSource, Profile, Project
+from iaso.models import Account, DataSource, Profile, Project, Task
 
 
 logger = logging.getLogger(__name__)
 
 
-def change_user_name_and_disable_user(profile, new_account_name):
-    user_name = profile.user.username.split(".")
-    new_user_name = new_account_name
-    if len(user_name) > 1:
-        new_user_name = f"{new_account_name}.{user_name[1]}"
-    profile.user.username = new_user_name
-    profile.user.is_active = False
-    profile.user.save()
-    return profile.user
-
-
 def update_users_profiles(profiles, new_account_name):
-    return list(
-        map(
-            lambda profile: change_user_name_and_disable_user(profile, new_account_name),
-            profiles,
-        )
-    )
+    for profile in profiles:
+        user_name = profile.user.username.split(".")
+        new_user_name = new_account_name
+        if len(user_name) > 1:
+            new_user_name = f"{new_account_name}.{user_name[1]}"
+        profile.user.username = new_user_name
+        profile.user.is_active = False
+        profile.user.save()
+    return profiles
 
 
-def map_project(projects, new_account_name):
-    return list(
-        map(
-            lambda project: change_project_app_id(project, new_account_name),
-            projects,
-        )
-    )
-
-
-def change_project_app_id(project, new_account_name):
-    current_project = Project.objects.filter(app_id=project.app_id).first()
-    app_id = project.app_id.split(".")
-    current_project.app_id = new_account_name
-    if len(app_id) > 1:
-        current_project.app_id = f"{new_account_name}.{app_id[1]}"
-    return current_project
-
-
-def map_data_source(data_sources, current_timestamp):
-    return list(
-        map(
-            lambda data_source: change_data_source_name(data_source, current_timestamp),
-            data_sources,
-        )
-    )
-
-
-def change_data_source_name(data_source, current_timestamp):
-    data_source.name = f"{data_source.name}{current_timestamp}"
-    data_source.save()
-    return data_source
+def change_projects_app_id(projects, new_account_name):
+    for project in projects:
+        app_id = project.app_id.split(".")
+        if len(app_id) > 1:
+            project.app_id = f"{new_account_name}.{app_id[1]}"
+        else:
+            project.app_id = new_account_name
+        project.save()
+    return projects
 
 
 def recreate_account(account_name):
+    command = ["python3", "setuper/setuper.py", "--additional_projects", "-n", account_name]
+    logger.info(f"Executing command: {' '.join(command)}")
     try:
-        command = ["python3", "setuper/setuper.py", "--additional_projects", "-n", account_name]
         process = subprocess.run(command, capture_output=True, text=True)
         logger.info(f"Stdout:\n{process.stdout}")
-        if process.stderr:
-            logger.error(f"Stderr:\n{process.stderr}")
+        return True
     except subprocess.CalledProcessError as e:
         logger.exception(f"Error executing script '{e}':")
         logger.exception(f"Return Code: {e.returncode}")
+        logger.exception(f"Stderr:\n{e.stderr}")
+        return False
     except Exception as e:
         logger.exception(f"An unexpected error occurred: {e}")
+        return False
 
 
 @task_decorator(task_name="setuper_sandbox")
-def setuper_sandbox(name="admin", task=None):
+def setuper_sandbox(name="admin", task=Task):
     ENABLE_SETUPER_SANDBOX = os.environ.get("ENABLE_SETUPER_SANDBOX", "false").lower() == "true"
     if ENABLE_SETUPER_SANDBOX:
         logger.warning(f" .... Resetting {name} account and its data! ...")
-        current_datetime = int(datetime.now().timestamp())
+        current_timestamp = int(datetime.now().timestamp())
         account_name = name
         if isinstance(name, list):
             account_name = name[0]
-        new_name = f"{account_name}{current_datetime}"
+        new_name = f"{account_name}{current_timestamp}"
         logger.info(f"Renaming current account {account_name} to {new_name}")
         account = Account.objects.filter(name=account_name).first()
         if account is not None:
             account.name = new_name
             account.save()
-        profiles = Profile.objects.filter(account=account)
+
+        profiles = Profile.objects.filter(Q(account=account) | Q(user__username=account_name))
         logger.info(f"Renaming and deactivating all {len(profiles)} users belong to account {account_name}")
+
         updated_users = update_users_profiles(profiles, new_name)
         logger.info(f"Disabled {len(updated_users)} users")
 
         projects = Project.objects.filter(account=account)
         logger.info(f"Renaming app id for all {len(projects)} projects belong to account {account_name}")
-        projects_to_updated = map_project(projects, new_name)
+        projects_to_updated = change_projects_app_id(projects, new_name)
         updated_projects = Project.objects.bulk_update(projects_to_updated, ["app_id"])
         logger.info(f"Renamed app id for all {updated_projects} projects")
 
         data_sources = DataSource.objects.filter(projects__account=account).distinct()
         logger.info(f"Renaming all {len(data_sources)} data_sources belong to account {account_name}")
-        rename_data_sources = map_data_source(data_sources, current_datetime)
-        logger.info(f"Renamed all {len(rename_data_sources)} data_sources")
+        for data_source in data_sources:
+            data_source.name = f"{data_source.name}{current_timestamp}"
+            data_source.save()
+        logger.info(f"Renamed all {len(data_sources)} data_sources")
 
-        logger.warning(f"Reset {account_name} account!")
-
-        recreate_account(account_name)
+        logger.info(f"Reset {account_name} account!")
+        result = recreate_account(account_name)
+        message = f"Sandbox {account_name} account"
+        if result:
+            task.report_success(f"{message} created")
+        else:
+            task.terminate_with_error(f"{message} task failed")
     else:
         logger.info("No setuper sandbox task")

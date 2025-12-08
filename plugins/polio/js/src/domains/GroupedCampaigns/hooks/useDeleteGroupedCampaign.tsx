13366a91c26dafb27580fb79e7be561ad9accb27
Crebert08 <benicrebertbaransegeta@gmail.com>
2025-12-08 18:53:38 +0200

    Refactor useDelete in Grouped campaigns module

--- plugins/polio/js/src/domains/GroupedCampaigns/hooks/useDeleteGroupedCampaign.tsx
+++ plugins/polio/js/src/domains/GroupedCampaigns/hooks/useDeleteGroupedCampaign.tsx
@@ -1,15 +1,31 @@
 import { UseMutationResult } from 'react-query';
 import { deleteRequest } from '../../../../../../../hat/assets/js/apps/Iaso/libs/Api';
 import { useSnackMutation } from '../../../../../../../hat/assets/js/apps/Iaso/libs/apiHooks';
+import { useDeleteTableRow } from 'Iaso/components/tables/TableWithDeepLink';
+import { baseUrls } from '../../../constants/urls';
 
 const deleteGroupedCampaign = async id =>
     deleteRequest(`/api/polio/campaignsgroup/${id}/`);
 
-export const useDeleteGroupedCampaign = (): UseMutationResult => {
-    return useSnackMutation(
-        id => deleteGroupedCampaign(id),
-        undefined,
-        undefined,
-        ['groupedCampaigns'],
-    );
+type useDeleteArgs = {
+    params: any;
+    count: number;
+}
+
+export const useDeleteGroupedCampaign = ({params, count}: useDeleteArgs): UseMutationResult => {
+    const onSuccess = useDeleteTableRow({
+        count,
+        params,
+        pageKey: 'page',
+        pageSizeKey: 'pageSize',
+        invalidateQueries: ['groupedCampaigns'],
+        baseUrl: baseUrls.groupedCampaigns,
+    });
+
+    return useSnackMutation({
+        mutationFn: id => deleteGroupedCampaign(id),
+        options: {
+            onSuccess,
+        }
+    });
 };

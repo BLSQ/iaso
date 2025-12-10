78dface78d28f200581e96f4c08b5f35539e9d3e
Crebert08 <benicrebertbaransegeta@gmail.com>
2025-12-10 14:18:19 +0200

    Refactor useDelete in the Forms module

--- hat/assets/js/apps/Iaso/domains/forms/hooks/useDeleteForm.tsx
+++ hat/assets/js/apps/Iaso/domains/forms/hooks/useDeleteForm.tsx
@@ -1,16 +1,45 @@
-import { UseMutationResult } from 'react-query';
+import { UseMutationResult, useQueryClient } from 'react-query';
 import { deleteRequest } from '../../../libs/Api';
 import { useSnackMutation } from '../../../libs/apiHooks';
 import MESSAGES from '../messages';
+import { useDeleteTableRow } from 'Iaso/components/tables/TableWithDeepLink';
+import { baseUrls } from '../../../constants/urls';
 
 const deleteForm = (id: number) => {
     return deleteRequest(`/api/forms/${id}/`);
 };
 
-export const useDeleteForm = (): UseMutationResult => {
+type useDeleteArgs = {
+    params: any;
+    count: number;
+}
+
+export const useDeleteForm = ({params, count}: useDeleteArgs): UseMutationResult => {
+    const queryClient = useQueryClient();
+
+    console.log('DEBUG (useDeleteForm): Hook Initialized');
+    console.log('DEBUG (useDeleteForm): Received Params:', params);
+    console.log('DEBUG (useDeleteForm): Received Count:', count);
+
+    const onSuccessNavigateOrInvalidate = useDeleteTableRow({
+        count,
+        params,
+        pageKey: 'page',
+        pageSizeKey: 'pageSize',
+        baseUrl: baseUrls.forms,
+    })
+
     return useSnackMutation({
-        mutationFn: deleteForm,
+        mutationFn: body => deleteForm(body.id),
         snackSuccessMessage: MESSAGES.formDeleted,
-        invalidateQueryKey: ['forms'],
+        options:{
+            onSuccess:()=>{
+                queryClient.invalidateQueries(['forms']);
+                
+                // 2. Run the navigation/invalidation logic calculated by useDeleteTableRow.
+                // (This function will trigger the redirect to page 1).
+                onSuccessNavigateOrInvalidate(); 
+            }
+        }
     });
 };

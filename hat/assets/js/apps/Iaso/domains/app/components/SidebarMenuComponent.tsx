2af70c245e4e1dbb15b144915e7f17daae493960
edil3ra <edil3ra@gmail.com>
2025-12-11 09:55:55 +0100

    feat: Add discussion board link to sidebar

--- hat/assets/js/apps/Iaso/domains/app/components/SidebarMenuComponent.tsx
+++ hat/assets/js/apps/Iaso/domains/app/components/SidebarMenuComponent.tsx
@@ -1,13 +1,17 @@
 import React, { FunctionComponent } from 'react';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ChatIcon from '@mui/icons-material/Chat';
 import ExitIcon from '@mui/icons-material/ExitToApp';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 import {
-    Box,
     Button,
     Divider,
     Drawer,
     IconButton,
     List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
     Tooltip,
     Typography,
     useMediaQuery,
@@ -75,6 +79,17 @@ const useStyles = makeStyles(theme => ({
         display: 'flex',
         alignItems: 'center',
     },
+    bottomLinkItem: {
+        color: 'inherit',
+    },
+    drawerContent: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+    },
+    mainContent: {
+        flex: 1,
+    },
 }));
 
 type Props = { location: any };
@@ -98,101 +113,90 @@ const SidebarMenu: FunctionComponent<Props> = ({ location }) => {
 
     return (
         <Drawer anchor="left" open={isOpen} onClose={toggleSidebar}>
-            <div className={classes.toolbar}>
-                <Link className={classes.homeLink} to={`/${baseUrls.home}`}>
-                    <LogoAndTitle />
-                </Link>
-                <IconButton
-                    className={classes.menuButton}
-                    color="inherit"
-                    aria-label="Menu"
-                    onClick={toggleSidebar}
-                >
-                    <ArrowBackIcon />
-                </IconButton>
-            </div>
-            <Divider />
-            <List className={classes.list}>
-                {menuItems.map(menuItem => (
-                    <MenuItem
-                        location={location}
-                        key={menuItem.key}
-                        menuItem={menuItem}
-                        onClick={(_, url) => onClick(url)}
-                        url={menuItem.url}
-                        target="_blank"
-                    />
-                ))}
-            </List>
-            <Box className={classes.user}>
-                {isMobileLayout && (
-                    <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className={classes.userName}
+            <div className={classes.drawerContent}>
+                <div className={classes.toolbar}>
+                    <Link className={classes.homeLink} to={`/${baseUrls.home}`}>
+                        <LogoAndTitle />
+                    </Link>
+                    <IconButton
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="Menu"
+                        onClick={toggleSidebar}
                     >
-                        {currentUser.user_name}
-                    </Typography>
-                )}
-
-                {currentUser.account && isMobileLayout && (
-                    <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className={classes.userName}
+                        <ArrowBackIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+                <List className={`${classes.list} ${classes.mainContent}`}>
+                    {menuItems.map(menuItem => (
+                        <MenuItem
+                            location={location}
+                            key={menuItem.key}
+                            menuItem={menuItem}
+                            onClick={(_, url) => onClick(url)}
+                            url={menuItem.url}
+                            target="_blank"
+                        />
+                    ))}
+                </List>
+                <List className={`${classes.list} ${classes.user}`}>
+                    <Tooltip
+                        classes={{ popper: classes.popperFixed }}
+                        placement="top-start"
+                        title={formatMessage(MESSAGES.viewUserManual)}
                     >
-                        {defaultSourceVersion && (
-                            <Tooltip
-                                classes={{ popper: classes.popperFixed }}
-                                placement="bottom"
-                                title={`${formatMessage(MESSAGES.source)}: ${
-                                    (defaultSourceVersion.source &&
-                                        defaultSourceVersion.source.name) ||
-                                    '-'
-                                }, ${formatMessage(MESSAGES.version)} ${
-                                    (defaultSourceVersion.version &&
-                                        defaultSourceVersion.version.number) ||
-                                    '-'
-                                }`}
-                            >
-                                <span>{currentUser.account.name}</span>
-                            </Tooltip>
-                        )}
-                        {!defaultSourceVersion && (
-                            <span>{currentUser.account.name}</span>
-                        )}
-                    </Typography>
-                )}
-                <Tooltip
-                    classes={{ popper: classes.popperFixed }}
-                    placement="bottom-start"
-                    title={formatMessage(MESSAGES.viewUserManual)}
-                >
-                    <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        className={`${classes.userName} ${classes.userManual}`}
-                    >
-                        <a
+                        <ListItem
+                            className={classes.bottomLinkItem}
+                            button
+                            component="a"
                             href={userGuideUrl}
                             target="_blank"
                             rel="noreferrer"
-                            className={classes.link}
                         >
-                            {formatMessage(MESSAGES.userManual)}
-                        </a>
-                    </Typography>
-                </Tooltip>
-                <Button
-                    size="small"
-                    color="inherit"
-                    href="/logout-iaso"
-                    aria-label={formatMessage(MESSAGES.logout)}
-                >
-                    <ExitIcon className={classes.smallButtonIcon} />
-                    <FormattedMessage {...MESSAGES.logout} />
-                </Button>
-            </Box>
+                            <ListItemIcon>
+                                <MenuBookIcon />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary={
+                                    <Typography variant="body2">
+                                        <FormattedMessage
+                                            {...MESSAGES.userManual}
+                                        />
+                                    </Typography>
+                                }
+                            />
+                        </ListItem>
+                    </Tooltip>
+                    <Tooltip
+                        classes={{ popper: classes.popperFixed }}
+                        placement="top-start"
+                        title={formatMessage(MESSAGES.viewDiscussionBoard)}
+                    >
+                        <ListItem
+                            className={classes.bottomLinkItem}
+                            button
+                            component="a"
+                            href="https://forum.openiaso.com"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <ListItemIcon>
+                                <ChatIcon />
+                            </ListItemIcon>
+                            <ListItemText
+                                primary={
+                                    <Typography variant="body2">
+                                        <FormattedMessage
+                                            {...MESSAGES.discussionBoard}
+                                        />
+                                    </Typography>
+                                }
+                            />
+                        </ListItem>
+                    </Tooltip>
+                </List>
+            </div>
         </Drawer>
     );
 };

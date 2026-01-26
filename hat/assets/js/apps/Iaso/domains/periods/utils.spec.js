2b38160366c1f6fded9266966ef26124c43bcf3f
Stéphan Mestach <mestachs@users.noreply.github.com>
2026-01-26 15:53:44 +0100

    feat: add WEEK frequency form support (#2686)
    refs: WC2-870

--- hat/assets/js/apps/Iaso/domains/periods/utils.spec.js
+++ hat/assets/js/apps/Iaso/domains/periods/utils.spec.js
@@ -1,5 +1,26 @@
 import { textPlaceholder } from 'bluesquare-components';
-import { getPrettyPeriod } from './utils';
+import { getNumberOfIsoWeeksInYear, getPrettyPeriod } from './utils';
+
+function testFormatMessage(msg, values) {
+  values = values || {};
+  var template =
+    typeof msg === 'string'
+      ? msg
+      : (msg && msg.defaultMessage) || '';
+
+  return template.replace(/\{(\w+)\}/g, function (_m, key) {
+    if (!Object.prototype.hasOwnProperty.call(values, key)) return '{' + key + '}';
+    return String(values[key]);
+  });
+}
+
+// optional strict version (fails if a placeholder is missing)
+function strictTestFormatMessage(msg, values) {
+  var out = testFormatMessage(msg, values);
+  if (/\{\w+\}/.test(out)) throw new Error('Unreplaced placeholder in: ' + out);
+  return out;
+}
+
 
 describe('getPrettyPeriod', () => {
     it('should return textPlaceholder if no periodString', () => {
@@ -13,4 +34,18 @@ describe('getPrettyPeriod', () => {
     it('for year should return a year', () => {
         expect(getPrettyPeriod('2020', x => x.defaultMessage)).to.eql('2020');
     });
+
+    it('for year should return a year', () => {
+        expect(getPrettyPeriod('2020W53', testFormatMessage)).to.eql('Week N° 53 (28/12/2020 - 03/01/2021)');
+    });
+
 });
+
+
+describe('getNumberOfIsoWeeksInYear', () => {
+      it('should handle week leap years', () => {
+        expect(getNumberOfIsoWeeksInYear(2025)).to.eql(52)
+        expect(getNumberOfIsoWeeksInYear(2026)).to.eql(53)
+        expect(getNumberOfIsoWeeksInYear(2027)).to.eql(52)
+      })
+})

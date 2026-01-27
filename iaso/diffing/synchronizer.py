5b57d628d7302876a26e9d96276e271ac3c30b4d
Christophe Gerard <cgerard@bluesquarehub.com>
2026-01-27 13:57:06 +0100

    make it clearer

--- iaso/diffing/synchronizer.py
+++ iaso/diffing/synchronizer.py
@@ -326,6 +326,7 @@ class DataSourceVersionsSynchronizer:
         changes = {
             comparison["field"]: comparison["after"]
             for comparison in diff["comparisons"]
+            # if comparison["status"] == Differ.STATUS_MODIFIED
             if comparison["status"] in [Differ.STATUS_MODIFIED, Differ.STATUS_NEW, Differ.STATUS_NOT_IN_ORIGIN]
             and comparison["field"] in ["name", "parent", "opening_date", "closed_date"]
         }

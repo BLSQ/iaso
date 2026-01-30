a8b0120263367592f83098ca20ac7819c16bf000
Crebert08 <benicrebertbaransegeta@gmail.com>
2026-01-30 14:51:53 +0200

    Feat: Adding the view and urls of the new debug summary page

--- plugins/wfp/urls.py
+++ plugins/wfp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import debug, delete_all_instances_and_entities, delete_beneficiaries_analytics
+from .views import debug, debug_summary, delete_all_instances_and_entities, delete_beneficiaries_analytics
 
 
 urlpatterns = [
     path("debug/<int:id>/", debug, name="wfp_debug"),
+    path("debug_summary/", debug_summary, name="wfp_debug_summary"),
     path("delete_beneficiaries_analytics/", delete_beneficiaries_analytics, name="delete_beneficiaries_analytics"),
     path(
         "delete_all_instances_and_entities/",

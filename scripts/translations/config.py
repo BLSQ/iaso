4b96b5b538d4ee207cfdf5cfdfe4b0e18495c9af
quang-le <qle@bluesquarehub.com>
2026-02-02 17:24:50 +0100

    IA-4076: remove remaining references to cypress

--- scripts/translations/config.py
+++ scripts/translations/config.py
@@ -1,7 +1,6 @@
 IGNORE_ARGS = [
     "--ignore=.venv",
     "--ignore=venv",
-    "--ignore=cypress",
     "--ignore=node_modules",
     "--ignore=beanstalk_worker",
     "--ignore=django_sql_dashboard_export",

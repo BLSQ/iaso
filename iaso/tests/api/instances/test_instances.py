279a98ad92d1d893f3715a0152c5393aa9116d72
Benjamin Monjoie <bmo@appkers.com>
2026-02-11 17:01:25 +0100

    Fix too precise accuracy fails import
    When the accuracy is too precise, rounding is not enough.
    We have to take into consideration what the actual size will be before validating.
    
    IA-4769

--- iaso/tests/api/instances/test_instances.py
+++ iaso/tests/api/instances/test_instances.py
@@ -444,6 +444,35 @@ class InstancesAPITestCase(TaskAPITestCase):
         last_instance = m.Instance.objects.get(uuid=instance_uuid)
         self.assertEqual(Decimal("12.35"), last_instance.accuracy)
 
+    def test_instance_create_with_accuracy_rounded_with_long_number(self):
+        """POST /api/instances/ with accuracy having more than 2 decimal places should be rounded"""
+        from decimal import Decimal
+
+        instance_uuid = str(uuid4())
+        body = [
+            {
+                "id": instance_uuid,
+                "created_at": 1565258153704,
+                "updated_at": 1565258153709,
+                "orgUnitId": self.jedi_council_corruscant.id,
+                "formId": self.form_1.id,
+                "period": "202002",
+                "latitude": 50.2,
+                "longitude": 4.4,
+                "accuracy": 48.622852,
+                "altitude": 100,
+                "file": "\/storage\/emulated\/0\/odk\/instances\/test_accuracy_rounded\/test.xml",
+                "name": "test_accuracy_rounded",
+            }
+        ]
+        response = self.client.post(
+            "/api/instances/?app_id=stars.empire.agriculture.hydroponics", data=body, format="json"
+        )
+        self.assertEqual(response.status_code, 200)
+
+        last_instance = m.Instance.objects.get(uuid=instance_uuid)
+        self.assertEqual(Decimal("48.62"), last_instance.accuracy)
+
     def test_instance_create_with_invalid_accuracy(self):
         """POST /api/instances/ with invalid accuracy value should fail the import"""
         instance_uuid = str(uuid4())

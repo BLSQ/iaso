0ea70f295d1116c471b8d8638ab24af7f38ed17e
Thibault Dethier <tdethier@bluesquarehub.com>
2026-02-18 16:20:50 +0100

    PR review

--- iaso/utils/models/color.py
+++ iaso/utils/models/color.py
@@ -1,26 +1,23 @@
 import re
 
-from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 HEX_COLOR_REGEX = re.compile(r"#[0-9A-Fa-f]{6}$")
 
 
-def validate_hex_color(value):
-    if not HEX_COLOR_REGEX.match(value):
-        raise ValidationError(f"{value} is not a valid hex color code. It should be in the format #RRGGBB.")
-
-
 class ColorField(models.CharField):
     description = "A color field that accepts hex color codes (e.g., #RRGGBB)."
+    default_validators = [
+        RegexValidator(
+            regex=HEX_COLOR_REGEX,
+            message="%(value)s is not a valid hex color code. It should be in the format #RRGGBB.",
+        ),
+    ]
 
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("max_length", 7)  # Length for hex color code (#RRGGBB)
-        validators = kwargs.get("validators", [])
-        if not validators or validate_hex_color not in validators:
-            validators.append(validate_hex_color)
-        kwargs["validators"] = validators
         super().__init__(*args, **kwargs)
 
     def to_python(self, value):

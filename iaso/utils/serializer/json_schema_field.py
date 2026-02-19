56e068143f7c4f4c2a8c9fe02d4bddc46f936a6e
Thibault Dethier <tdethier@bluesquarehub.com>
2026-02-19 15:08:38 +0100

    improve JSONSchemaField + tests

--- iaso/utils/serializer/json_schema_field.py
+++ iaso/utils/serializer/json_schema_field.py
@@ -5,16 +5,18 @@ from rest_framework import serializers
 
 
 class JSONSchemaField(serializers.JSONField):
-    def __init__(self, *args, **kwargs):
-        self.schema = kwargs.pop("schema", None)
+    def __init__(self, schema, *args, **kwargs):
+        if not schema:
+            raise ValueError("A JSON schema must be provided for validation.")
+        self.schema = schema
+
         super().__init__(*args, **kwargs)
 
     def to_internal_value(self, value):
         data = super().to_internal_value(value)
         try:
             jsonschema.validate(data, self.schema)
-        except jsonschema.exceptions.ValidationError as e:
+        except jsonschema.ValidationError as e:
             raise ValidationError(e.message) from e
-        except jsonschema.exceptions.SchemaError as e:
-            raise ValidationError(e.message) from e
+
         return data

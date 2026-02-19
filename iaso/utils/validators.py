ba635e08750a384d6554270990ffe3a6129dffa3
Thibault Dethier <tdethier@bluesquarehub.com>
2026-02-19 14:40:23 +0100

    improve JSONSchemaValidator + tests

--- iaso/utils/validators.py
+++ iaso/utils/validators.py
@@ -1,18 +1,34 @@
 import jsonschema
 
 from django.core.exceptions import ValidationError
-from django.core.validators import BaseValidator
+from django.utils.deconstruct import deconstructible
 
 
-class JSONSchemaValidator(BaseValidator):
-    def __init__(self, schema, message=None):
+@deconstructible
+class JSONSchemaValidator:
+    message = "Provide a valid JSON schema - validation failed: %(error)s"
+    code = "invalid_json_schema"
+
+    def __init__(self, schema, message=None, code=None):
+        if schema is None:
+            raise ValueError("A JSON schema must be provided for validation.")
         self.schema = schema
-        super().__init__(limit_value=None, message=message)
+
+        if message is not None:
+            self.message = message
+        if code is not None:
+            self.code = code
 
     def __call__(self, value):
         try:
             jsonschema.validate(value, self.schema)
         except jsonschema.ValidationError as e:
-            raise ValidationError(e.message) from e
-        except jsonschema.exceptions.SchemaError as e:
-            raise ValidationError(e.message) from e
+            raise ValidationError(self.message, code=self.code, params={"error": e.message}) from e
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, self.__class__)
+            and self.schema == other.schema
+            and self.message == other.message
+            and self.code == other.code
+        )

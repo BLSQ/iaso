acf40ea9789b9a75a2806ce3f8f95d4657d201c5
Bewi <benjamin.wilfart@gmail.com>
2026-03-06 13:40:26 +0100

    Mock ResizeObserver and set as global for tests

--- hat/assets/js/tests/setup.ts
+++ hat/assets/js/tests/setup.ts
@@ -1,6 +1,17 @@
 import '@testing-library/jest-dom';
 
 // fix for react-pdf
-import DOMMatrix from "@thednp/dommatrix";
+import DOMMatrix from '@thednp/dommatrix';
+import { vi } from 'vitest';
 // @ts-ignore
-global.DOMMatrix = DOMMatrix as unknown as typeof DOMMatrix
+global.DOMMatrix = DOMMatrix as unknown as typeof DOMMatrix;
+
+// Mock the ResizeObserver
+const ResizeObserverMock = vi.fn(() => ({
+    observe: vi.fn(),
+    unobserve: vi.fn(),
+    disconnect: vi.fn(),
+}));
+
+// Stub the global ResizeObserver
+vi.stubGlobal('ResizeObserver', ResizeObserverMock);

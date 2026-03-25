2f9b5c843e9f2ef20926b0bc5df01dc73505ea9e
Michael Wendland <mwendland@bluesquarehub.com>
2026-03-25 17:15:06 +0100

    fix: SNT-392 subclass querycount to fix KeyError (#2855)
    refs: SNT-392
    
    * fix: subclass querycount to fix KeyError
    
    This fixes a KeyError of querycount when using dynamically configured DB connection by overwriting querycount's static stats dict with a defaultdict
    
    * refactor: mv to middleware.py

--- hat/settings.py
+++ hat/settings.py
@@ -258,7 +258,7 @@ MIDDLEWARE += [
 ]
 if DEBUG:
     MIDDLEWARE += [
-        "querycount.middleware.QueryCountMiddleware",
+        "iaso.middleware.SafeQueryCountMiddleware",
     ]
 
 MIDDLEWARE += [

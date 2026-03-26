bc9ea5e55681afaab21b1f196e6e196b56a6520a
quang-le <qle@bluesquarehub.com>
2026-03-26 17:50:51 +0100

    IA-4853: adapt permission for superuser

--- iaso/api/validation_workflows_nodes/permissions.py
+++ iaso/api/validation_workflows_nodes/permissions.py
@@ -5,8 +5,11 @@ from iaso.permissions.core_permissions import CORE_VALIDATION_WORKFLOW_PERMISSIO
 
 class HasValidationWorkflowPermission(BasePermission):
     def has_permission(self, request, view):
+
         if request.method in ["HEAD", "OPTIONS"]:
             return True
         if not request.user or not request.user.is_authenticated:
             return False
+        if request.user.is_superuser:
+            return True
         return request.user.has_perm(CORE_VALIDATION_WORKFLOW_PERMISSION.full_name())

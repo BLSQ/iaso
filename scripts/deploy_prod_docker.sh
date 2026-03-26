a094fe80ddaf32750181e902f1051d77e6007bf3
oscar <ochavez@bluesquarehub.com>
2026-03-26 10:00:20 +0100

    chore: Restore current .ebextensions to avoid conflicts. Add new post deploy commands

--- scripts/deploy_prod_docker.sh
+++ scripts/deploy_prod_docker.sh
@@ -3,10 +3,19 @@ set -euo pipefail
 
 
 PROD_COMPOSE="docker/prod/docker-compose.prod.yml"
+PROD_EB_EXTENSIONS="docker/prod/.ebextensions"
+DEV_EB_EXTENSIONS=".ebextensions"
+DEV_EB_EXTENSIONS_BAK=".ebextensions.bak"
 DEV_COMPOSE="docker-compose.yml"
 DEV_COMPOSE_BAK="docker-compose.dev.yml"
 
 EB_ENVIRONMENT="${1}"
+
+if [ -z "$EB_ENVIRONMENT" ]; then
+    echo "==> EB_ENVIRONMENT is not set"
+    echo "Usage: $0 <environment>"
+    exit 1
+fi
 
 
 echo "
@@ -25,7 +34,12 @@ cleanup() {
     echo "==> Restoring docker-compose for dev..."
     mv "$DEV_COMPOSE" "$PROD_COMPOSE"
     mv "$DEV_COMPOSE_BAK" "$DEV_COMPOSE"
+    
+    echo "==> Restoring eb extensions for dev..."
+    mv "$DEV_EB_EXTENSIONS" "$PROD_EB_EXTENSIONS"
+    mv "$DEV_EB_EXTENSIONS_BAK" "$DEV_EB_EXTENSIONS"
     git checkout -- "$DEV_COMPOSE"
+    git checkout -- "$DEV_EB_EXTENSIONS"
 }
 
 # Swap docker-compose files 
@@ -33,19 +47,26 @@ echo "==> Swapping docker-compose to prod"
 mv "$DEV_COMPOSE" "$DEV_COMPOSE_BAK"
 mv "$PROD_COMPOSE" "$DEV_COMPOSE"
 
-# Stage the swapped file so `eb deploy --staged` picks it up
+# Swap eb extensions files
+echo "==> Swapping eb extensions to prod"
+mv "$DEV_EB_EXTENSIONS" "$DEV_EB_EXTENSIONS_BAK"
+mv "$PROD_EB_EXTENSIONS" "$DEV_EB_EXTENSIONS"
+
+# Stage the swapped files so `eb deploy --staged` picks it up
 git add "$DEV_COMPOSE"
+git add "$DEV_EB_EXTENSIONS"
 
 # Rollback even if deploy failed
 trap cleanup EXIT
 
 # Create version from tags
-export VERSION_NAME=$(git describe --tags --match "v[[:digit:]]*")
+VERSION_NAME=$(git describe --tags --match "v[[:digit:]]*")
+export "VERSION_NAME=${VERSION_NAME}"
 echo "==> Version name/tag ${VERSION_NAME}"
 
 # Deploy to prod
 echo "==> Deploying ${EB_ENVIRONMENT}"
-python "scripts/eb_deploy.py" "${EB_ENVIRONMENT}"
+# python "scripts/eb_deploy.py" "${EB_ENVIRONMENT}"
 
 # Timestamp
 date

b291d638d67fadd7f0425be4abff3a639de1d2fe
Crebert08 <benicrebertbaransegeta@gmail.com>
2026-04-15 12:31:11 +0200

    Removing the get_object_or_404 method from the file

--- iaso/api/user_roles.py
+++ iaso/api/user_roles.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import Group, Permission
 from django.db.models import Q, QuerySet
-from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema
 from rest_framework import permissions, serializers, status
 from rest_framework.response import Response

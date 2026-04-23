fc2e13a23d967badf2ad3ad992909112a9f5005f
Thibault Dethier <tdethier@bluesquarehub.com>
2026-04-23 19:35:56 +0200

    refactor

--- iaso/tests/api/notifications/test_views.py
+++ iaso/tests/api/notifications/test_views.py
@@ -1,11 +1,15 @@
 from datetime import datetime, timedelta
 
+from rest_framework import status
+
 from hat.api_import.models import APIImport
 from iaso import test
 from iaso.models import Account, Project
 
 
 class TestNotificationsViewSet(test.APITestCase):
+    BASE_URL = "/api/notifications/"
+
     def setUp(self):
         super().setUp()
         self.account1 = account1 = Account.objects.create(name="account1")
@@ -14,10 +18,8 @@ class TestNotificationsViewSet(test.APITestCase):
         self.project1 = Project.objects.create(name="project1", app_id="P1", account=account1)
         self.project2 = Project.objects.create(name="project2", app_id="P2", account=account2)
 
-        self.staff = staff = self.create_user_with_profile(
-            username="staff", account=account1, permissions=[], is_staff=True
-        )
-        self.superuser = superuser = self.create_user_with_profile(
+        self.staff = self.create_user_with_profile(username="staff", account=account1, permissions=[], is_staff=True)
+        self.superuser = self.create_user_with_profile(
             username="superuser",
             account=account1,
             permissions=[],
@@ -26,42 +28,42 @@ class TestNotificationsViewSet(test.APITestCase):
         self.user = self.create_user_with_profile(username="user", account=account1, permissions=[])
 
     def test_get_notifications_anonymously(self):
-        response = self.client.get("/api/notifications/")
-        self.assertJSONResponse(response, 401)
+        response = self.client.get(self.BASE_URL)
+        self.assertJSONResponse(response, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_notifications_user(self):
         APIImport.objects.create(app_id="P1", has_problem=True, json_body={})
         self.client.force_authenticate(user=self.user)
-        response = self.client.get("/api/notifications/")
-        json_response = self.assertJSONResponse(response, 200)
+        response = self.client.get(self.BASE_URL)
+        json_response = self.assertJSONResponse(response, status.HTTP_200_OK)
         self.assertEqual(len(json_response), 0)
 
     def test_get_notifications_superuser_problem_in_account(self):
         APIImport.objects.create(app_id="P1", has_problem=True, json_body={})
         self.client.force_authenticate(user=self.superuser)
-        response = self.client.get("/api/notifications/")
-        json_response = self.assertJSONResponse(response, 200)
+        response = self.client.get(self.BASE_URL)
+        json_response = self.assertJSONResponse(response, status.HTTP_200_OK)
         self.assertEqual(len(json_response), 1)
 
     def test_get_notifications_superuser_problem_in_other_account(self):
         APIImport.objects.create(app_id="P2", has_problem=True, json_body={})
         self.client.force_authenticate(user=self.superuser)
-        response = self.client.get("/api/notifications/")
-        json_response = self.assertJSONResponse(response, 200)
+        response = self.client.get(self.BASE_URL)
+        json_response = self.assertJSONResponse(response, status.HTTP_200_OK)
         self.assertEqual(len(json_response), 0)
 
     def test_get_notifications_staff_problem_in_account(self):
         APIImport.objects.create(app_id="P1", has_problem=True, json_body={})
         self.client.force_authenticate(user=self.staff)
-        response = self.client.get("/api/notifications/")
-        json_response = self.assertJSONResponse(response, 200)
+        response = self.client.get(self.BASE_URL)
+        json_response = self.assertJSONResponse(response, status.HTTP_200_OK)
         self.assertEqual(len(json_response), 1)
 
     def test_get_notifications_staff_problem_in_other_account(self):
         APIImport.objects.create(app_id="P2", has_problem=True, json_body={})
         self.client.force_authenticate(user=self.staff)
-        response = self.client.get("/api/notifications/")
-        json_response = self.assertJSONResponse(response, 200)
+        response = self.client.get(self.BASE_URL)
+        json_response = self.assertJSONResponse(response, status.HTTP_200_OK)
         self.assertEqual(len(json_response), 1)
 
     def test_get_notifications_staff_problem_not_older_than_30_days(self):
@@ -69,8 +71,8 @@ class TestNotificationsViewSet(test.APITestCase):
         apiimport.created_at = datetime.now() - timedelta(days=28)
         apiimport.save()
         self.client.force_authenticate(user=self.staff)
-        response = self.client.get("/api/notifications/")
-        json_response = self.assertJSONResponse(response, 200)
+        response = self.client.get(self.BASE_URL)
+        json_response = self.assertJSONResponse(response, status.HTTP_200_OK)
         self.assertEqual(len(json_response), 1)
         self.assertJSONEqual(
             response.content,
@@ -82,6 +84,6 @@ class TestNotificationsViewSet(test.APITestCase):
         apiimport.created_at = datetime.now() - timedelta(days=31)
         apiimport.save()
         self.client.force_authenticate(user=self.staff)
-        response = self.client.get("/api/notifications/")
-        json_response = self.assertJSONResponse(response, 200)
+        response = self.client.get(self.BASE_URL)
+        json_response = self.assertJSONResponse(response, status.HTTP_200_OK)
         self.assertEqual(len(json_response), 0)

8d181d5b6a2d6ee446722a90be7a680c8471d223
Stéphan Mestach <smestach@bluesquare.org>
2026-04-29 15:37:27 +0200

    IA-4947 fix assets to be relative for form preview

--- docker/odk-preview/vite.config.js
+++ docker/odk-preview/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [vue()],
-    base: '/static/odk-preview/',
+    base: './',
     build: {
         target: 'esnext',
         outDir: '../../iaso/static/odk-preview',

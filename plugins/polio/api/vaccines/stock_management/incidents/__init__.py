478bb10be7e1b4fca884fb8ffef53878826a9b6b
Quang Son Le <38907762+quang-le@users.noreply.github.com>
2026-05-04 11:02:02 +0200

    feat: Temporary Form A (backend) (#2956)
    refs: POLIO-2125

---
+++ plugins/polio/api/vaccines/stock_management/incidents/__init__.py
@@ -0,0 +1,5 @@
+from plugins.polio.api.vaccines.stock_management.incidents.serializers import IncidentReportSerializer
+from plugins.polio.api.vaccines.stock_management.incidents.views import IncidentReportViewSet
+
+
+__all__ = ["IncidentReportSerializer", "IncidentReportViewSet"]

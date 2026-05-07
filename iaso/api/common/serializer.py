af78e1d038fcf40eadee770b0616ca9c11989bca
Abdelrahman Meroual <62496@etu.he2b.be>
2026-05-07 10:26:56 +0200

    fix format

--- iaso/api/common/serializer.py
+++ iaso/api/common/serializer.py
@@ -7,6 +7,7 @@ from iaso.models import UserRole
 
 from .serializer_fields import UserRoleNameField
 
+
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.CharField(source="get_full_name")
 
@@ -35,12 +36,14 @@ class ModelSerializerFieldMappingMixin:
 class ModelSerializer(ModelSerializerFieldMappingMixin, serializers.ModelSerializer):
     pass
 
+
 class UserRoleNameSerializer(ModelSerializer):
     name = UserRoleNameField(source="group.name", read_only=True)
 
     class Meta:
         model = UserRole
         fields = ["name", "id"]
+
 
 class DropdownOptionsSerializer(serializers.Serializer):
     value = serializers.CharField()

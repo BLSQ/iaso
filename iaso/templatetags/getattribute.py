c963c2459de19210f69e68f2644e6e3704197fe2
quang-le <qle@bluesquarehub.com>
2026-05-08 15:42:12 +0200

    IA-5044: add templatetags folder

---
+++ iaso/templatetags/getattribute.py
@@ -0,0 +1,23 @@
+import re
+
+from django import template
+from django.conf import settings
+
+
+numeric_test = re.compile("^\d+$")
+register = template.Library()
+
+
+def getattribute(value, arg):
+    """Gets an attribute of an object dynamically from a string name"""
+
+    if hasattr(value, str(arg)):
+        return getattr(value, arg)
+    if hasattr(value, "has_key") and value.has_key(arg):
+        return value[arg]
+    if numeric_test.match(str(arg)) and len(value) > int(arg):
+        return value[int(arg)]
+    return settings.TEMPLATE_STRING_IF_INVALID
+
+
+register.filter("getattribute", getattribute)

8994e02d1aa2c46f291c3b7747d3385281a9af66
Crebert08 <benicrebertbaransegeta@gmail.com>
2026-05-28 17:08:21 +0200

    adding the new field in the initialValues

--- hat/assets/js/apps/Iaso/domains/users/components/useInitialUser.ts
+++ hat/assets/js/apps/Iaso/domains/users/components/useInitialUser.ts
@@ -129,6 +129,10 @@ export const useInitialUser = (
                 value: get(initialData, 'coordination', null),
                 errors: [],
             },
+            institution: {
+                value: get(initialData, 'institution', null),
+                errors: [],
+            },
         };
     }, [initialData]);
     const [user, setUser] = useState<UserDialogData>(initialUser);

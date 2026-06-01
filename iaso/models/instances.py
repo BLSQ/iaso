7fca0e4e1c2d7dda3363549259b1f2bfd642989d
Philippe Vanderlinden <git@philv.me>
2026-06-01 12:19:12 +0000

    Add user id to instance created_by payload
    refs: SLEEP-1766

--- iaso/models/instances.py
+++ iaso/models/instances.py
@@ -786,6 +786,7 @@ class Instance(ValidationWorkflowArtefact):
             "deleted": self.deleted,
             "created_by": (
                 {
+                    "id": self.created_by.id,
                     "username": self.created_by.username,
                     "first_name": self.created_by.first_name,
                     "last_name": self.created_by.last_name,

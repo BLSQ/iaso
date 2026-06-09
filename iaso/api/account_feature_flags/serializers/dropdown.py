6253945dc987396c93bb13803d65a607a7d76410
hugo093 <hugo.093@hotmail.com>
2026-06-09 11:51:39 +0200

    fix: don't hit db when initializing serializer

--- iaso/api/account_feature_flags/serializers/dropdown.py
+++ iaso/api/account_feature_flags/serializers/dropdown.py
@@ -6,10 +6,12 @@ from iaso.models import AccountFeatureFlag
 
 class AccountFeatureFlagDropdownSerializer(ModelSerializer):
     label = serializers.CharField(read_only=True, source="name")
-    value = serializers.ChoiceField(
-        read_only=True, source="code", choices=list(AccountFeatureFlag.objects.values_list("code", flat=True))
-    )
+    value = serializers.ChoiceField(read_only=True, source="code", choices=[])
 
     class Meta:
         model = AccountFeatureFlag
         fields = ["label", "value"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["value"].choices = list(AccountFeatureFlag.objects.values_list("code", flat=True))

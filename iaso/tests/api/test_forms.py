43e52aa474a87dea482e9e6df774a876283b00d3
Martin De Wulf <mdewulf@bluesquarehub.com>
2026-06-15 00:21:29 +0200

    Update iaso/tests/api/test_forms.py
    Co-authored-by: hugo-s <59597930+hugo093@users.noreply.github.com>

--- iaso/tests/api/test_forms.py
+++ iaso/tests/api/test_forms.py
@@ -514,9 +514,7 @@ class FormsAPITestCase(APITestCase):
             },
             format="json",
         )
-        self.assertJSONResponse(response, 201)
-
-        response_data = response.json()
+        response_data = self.assertJSONResponse(response, 201)
         form = m.Form.objects.get(pk=response_data["id"])
         self.assertEqual(0, form.org_unit_groups.count())
 

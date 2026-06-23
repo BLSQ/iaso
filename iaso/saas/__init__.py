c49b739609567bede4aa6bbcc11be04b690009f8
hugo093 <hugo.093@hotmail.com>
2026-06-23 12:25:41 +0200

    fix: import error

--- iaso/saas/__init__.py
+++ iaso/saas/__init__.py
@@ -1,2 +1,5 @@
-from .quotas import disk_space, project, submissions, user  # noqa: F401
-from .signals import disk_space, project, submissions, user  # noqa: F401
+try:
+    from .quotas import disk_space, project, submissions, user  # noqa: F401
+    from .signals import disk_space, project, submissions, user  # noqa: F401
+except ImportError:
+    pass

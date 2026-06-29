8c4b7a5aa9eedd89336a89485c59de9c1ea9afdb
quang-le <qle@bluesquarehub.com>
2026-06-29 12:45:15 +0200

    IA_5117: update serializer to enable f-e reuse

--- iaso/api/instance_diff/views.py
+++ iaso/api/instance_diff/views.py
@@ -1,7 +1,7 @@
 from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
-from rest_framework.exceptions import NotFound
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
@@ -11,7 +11,7 @@ from iaso.api.common import HasPermission
 from iaso.api.common.mixin import CustomPaginationListModelMixin
 from iaso.api.instance_diff.filters import InstanceDiffFilter
 from iaso.api.instance_diff.pagination import InstanceDiffPaginator
-from iaso.api.instance_diff.serializers import ModificationSerializer
+from iaso.api.instance_diff.serializers import InstanceModificationSerializer
 from iaso.models import Instance
 from iaso.permissions.core_permissions import CORE_SUBMISSIONS_PERMISSION
 
@@ -26,15 +26,26 @@ class InstanceDiffViewSet(CustomPaginationListModelMixin, GenericViewSet):
     http_method_names = ["get"]
     pagination_class = InstanceDiffPaginator
 
+    def get_instance(self):
+        if not hasattr(self, "_instance"):
+            self._instance = get_object_or_404(
+                InstanceModificationSerializer.instance_queryset(Instance.objects.filter_for_user(self.request.user)),
+                pk=self.kwargs["instance_id"],
+            )
+        return self._instance
+
     def get_queryset(self):
-        if not Instance.objects.filter_for_user(self.request.user).filter(id=self.kwargs["instance_id"]).exists():
-            raise NotFound
         instance_content_type = ContentType.objects.get_for_model(Instance)
         return Modification.objects.select_related("content_type").filter(
-            content_type=instance_content_type, object_id=self.kwargs.get("instance_id")
+            content_type=instance_content_type, object_id=str(self.get_instance().pk)
         )
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["instance"] = self.get_instance()
+        return context
 
     def get_serializer_class(self):
         if self.action == "list":
-            return ModificationSerializer
+            return InstanceModificationSerializer
         raise NotImplementedError(f"Serializer not implemented for {self.action}")

b5bbf92396fe77c83186bf2ec6fc0b9ebefed0d0
quang-le <qle@bluesquarehub.com>
2026-06-30 23:13:07 +0200

    IA-5117: enable save

--- hat/assets/js/apps/Iaso/domains/instances/validate/components/ValidationPaper/StepInfo.tsx
+++ hat/assets/js/apps/Iaso/domains/instances/validate/components/ValidationPaper/StepInfo.tsx
@@ -1,12 +1,88 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useCallback, useMemo } from 'react';
 import { Box, Paper, Typography } from '@mui/material';
-import { useSafeIntl } from 'bluesquare-components';
+import {
+    DropdownOptions,
+    textPlaceholder,
+    UrlParams,
+    useRedirectTo,
+    useSafeIntl,
+} from 'bluesquare-components';
+import InputComponent from 'Iaso/components/forms/InputComponent';
+import { baseUrls } from 'Iaso/constants/urls';
+import {
+    Timeline,
+    ValidationNodeRetrieveResponse,
+} from 'Iaso/domains/validationWorkflowsConfiguration/types/validationNodes';
+import { useParamsObject } from 'Iaso/routing/hooks/useParamsObject';
 import MESSAGES from '../../messages';
 
-type Props = { formName: string };
+type Props = {
+    formName: string;
+    workflow: ValidationNodeRetrieveResponse | undefined;
+    isLoading: boolean;
+};
 
-export const StepInfo: FunctionComponent<Props> = ({ formName }) => {
+type Params = {
+    accountId: string;
+    instanceId: string;
+    selectedStep?: string;
+} & Partial<UrlParams>;
+
+export const StepInfo: FunctionComponent<Props> = ({
+    formName,
+    workflow,
+    isLoading,
+}) => {
+    const params: Params = useParamsObject(
+        baseUrls.instanceValidation,
+    ) as Params;
     const { formatMessage } = useSafeIntl();
+
+    const { selectedStep } = params ?? {};
+    const redirectTo = useRedirectTo();
+
+    const activeSteps: Timeline[] = useMemo(() => {
+        return (
+            workflow?.submissions?.[0].timeline
+                .filter((el: Timeline) => el.user_can_do_actions)
+                .filter(
+                    (el: Timeline) =>
+                        el.status !== 'ACCEPTED' && el.status !== 'SKIPPED',
+                ) ?? []
+        );
+    }, [workflow]);
+
+    const stepOptions: DropdownOptions<string>[] = useMemo(() => {
+        return activeSteps.map(step => ({
+            label: step.name,
+            value: `${step.id}`,
+        }));
+    }, [activeSteps]);
+
+    const stepToDisplay =
+        stepOptions.find(option => option.value === selectedStep)?.label ??
+        textPlaceholder;
+
+    const bypassed_steps =
+        activeSteps
+            .filter((_: any, i: number) => {
+                const index = activeSteps.findIndex(
+                    step => step.name === stepToDisplay,
+                );
+                return i > index;
+            })
+            .map(step => step.name)
+            .join(', ') || textPlaceholder;
+
+    const onStepSelect = useCallback(
+        (_: string, value: string) => {
+            redirectTo(baseUrls.instanceValidation, {
+                ...params,
+                selectedStep: value,
+            });
+        },
+        [redirectTo, params],
+    );
     return (
         <Paper
             elevation={1}
@@ -24,11 +100,23 @@ export const StepInfo: FunctionComponent<Props> = ({ formName }) => {
                 </Typography>
                 <Typography>{formName}</Typography>
             </Box>
+            <Box mb={2}>
+                <InputComponent
+                    type="select"
+                    clearable={false}
+                    keyValue="selectedStep"
+                    options={stepOptions}
+                    value={selectedStep}
+                    onChange={onStepSelect}
+                    labelString={formatMessage(MESSAGES.step)}
+                    loading={isLoading}
+                />
+            </Box>
             <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
                 <Typography sx={{ fontWeight: 'bold' }}>
-                    {formatMessage(MESSAGES.step)}
+                    {formatMessage(MESSAGES.bypassedSteps)}
                 </Typography>
-                <Typography>{formName}</Typography>
+                <Typography>{bypassed_steps}</Typography>
             </Box>
         </Paper>
     );
--- hat/assets/js/apps/Iaso/domains/instances/validate/components/ValidationPaper/ValidationPaper.tsx
+++ hat/assets/js/apps/Iaso/domains/instances/validate/components/ValidationPaper/ValidationPaper.tsx
@@ -1,6 +1,9 @@
 import React, { FunctionComponent } from 'react';
-import { useSafeIntl } from 'bluesquare-components';
+import { UrlParams, useSafeIntl } from 'bluesquare-components';
 import WidgetPaper from 'Iaso/components/papers/WidgetPaperComponent';
+import { baseUrls } from 'Iaso/constants/urls';
+import { useGetSubmissionValidationStatus } from 'Iaso/domains/instances/components/ValidationWorkflow/useGetSubmissionValidationStatus';
+import { useParamsObject } from 'Iaso/routing/hooks/useParamsObject';
 import MESSAGES from '../../messages';
 import { ApprovalForm } from './ApprovalForm';
 import { PreviousRejection } from './PreviousRejection';
@@ -8,16 +11,32 @@ import { StepInfo } from './StepInfo';
 
 type Props = { formName: string; instanceId: number };
 
+type Params = {
+    accountId: string;
+    instanceId: string;
+    selectedStep?: string;
+} & Partial<UrlParams>;
+
 export const ValidationPaper: FunctionComponent<Props> = ({
     formName,
     instanceId,
 }) => {
+    const params: Params = useParamsObject(
+        baseUrls.instanceValidation,
+    ) as Params;
     const { formatMessage } = useSafeIntl();
+    const { data: currentWorkflow, isLoading: isLoadingWorkflow } =
+        useGetSubmissionValidationStatus(parseInt(params.instanceId, 10));
+
     return (
         <WidgetPaper title={formatMessage(MESSAGES.validation)}>
-            <StepInfo formName={formName} />
+            <StepInfo
+                formName={formName}
+                workflow={currentWorkflow}
+                isLoading={isLoadingWorkflow}
+            />
             <PreviousRejection instanceId={instanceId} />
-            <ApprovalForm />
+            <ApprovalForm workflow={currentWorkflow} />
         </WidgetPaper>
     );
 };

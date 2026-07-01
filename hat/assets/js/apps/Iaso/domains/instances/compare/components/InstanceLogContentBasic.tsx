e85a5dc99478dd0df6fe1de4181c2e7428cd4701
quang-le <qle@bluesquarehub.com>
2026-07-01 14:25:23 +0200

    IA-5117: set table max height

--- hat/assets/js/apps/Iaso/domains/instances/compare/components/InstanceLogContentBasic.tsx
+++ hat/assets/js/apps/Iaso/domains/instances/compare/components/InstanceLogContentBasic.tsx
@@ -1,7 +1,9 @@
 import React, { FunctionComponent } from 'react';
 import {
+    Box,
     Table,
     TableCell,
+    TableContainer,
     TableRow,
     TableHead,
     Typography,
@@ -12,8 +14,9 @@ import {
     IntlFormatMessage,
     IntlMessage,
 } from 'bluesquare-components';
-
 import classNames from 'classnames';
+import { getStickyTableHeadStyles } from 'Iaso/styles/utils';
+
 import { FileContent } from '../../types/instance';
 import MESSAGES from '../messages';
 import InstanceLogContentBodyTable from './InstanceLogContentBodyTable';
@@ -22,12 +25,15 @@ type Props = {
     fileContent: FileContent;
     headerA?: IntlMessage;
     headerB?: IntlMessage;
+    tableMaxHeight?: string;
 };
+
+export const DEFAULT_TABLE_MAX_HEIGHT = 'calc(100vh - 200px)';
 
 const useStyles = makeStyles(theme => ({
     tableCellHead: {
         fontWeight: 'bold',
-        backgroundColor: 'transparent',
+        backgroundColor: theme.palette.background.paper,
         borderTop: 'none !important',
         borderLeft: 'none !important',
         // @ts-ignore
@@ -51,57 +57,66 @@ export const InstanceLogContentBasic: FunctionComponent<Props> = ({
     fileContent,
     headerA = MESSAGES.instanceLogsVersionA,
     headerB = MESSAGES.instanceLogsVersionB,
+    tableMaxHeight = DEFAULT_TABLE_MAX_HEIGHT,
 }) => {
     const { formatMessage }: { formatMessage: IntlFormatMessage } =
         useSafeIntl();
     const classes: Record<string, string> = useStyles();
 
     return (
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell
-                        align="left"
-                        className={classNames(
-                            classes.tableCellHead,
-                            classes.labelTableCellFixWith,
-                        )}
-                    >
-                        {formatMessage(MESSAGES.label)}
-                    </TableCell>
-                    <TableCell
-                        align="left"
-                        className={classNames(
-                            classes.tableCellHead,
-                            classes.versionValueTableCellFix,
-                        )}
-                    >
-                        <Typography
-                            color={
-                                fileContent?.logA?.deleted ? 'error' : 'inherit'
-                            }
-                        >
-                            {formatMessage(headerA)}
-                        </Typography>
-                    </TableCell>
-                    <TableCell
-                        align="left"
-                        className={classNames(
-                            classes.tableCellHead,
-                            classes.versionValueTableCellFix,
-                        )}
-                    >
-                        <Typography
-                            color={
-                                fileContent?.logB?.deleted ? 'error' : undefined
-                            }
-                        >
-                            {formatMessage(headerB)}
-                        </Typography>
-                    </TableCell>
-                </TableRow>
-            </TableHead>
-            <InstanceLogContentBodyTable fileContent={fileContent} />
-        </Table>
+        <Box sx={getStickyTableHeadStyles(tableMaxHeight)}>
+            <TableContainer>
+                <Table stickyHeader>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell
+                                align="left"
+                                className={classNames(
+                                    classes.tableCellHead,
+                                    classes.labelTableCellFixWith,
+                                )}
+                            >
+                                {formatMessage(MESSAGES.label)}
+                            </TableCell>
+                            <TableCell
+                                align="left"
+                                className={classNames(
+                                    classes.tableCellHead,
+                                    classes.versionValueTableCellFix,
+                                )}
+                            >
+                                <Typography
+                                    color={
+                                        fileContent?.logA?.deleted
+                                            ? 'error'
+                                            : 'inherit'
+                                    }
+                                >
+                                    {formatMessage(headerA)}
+                                </Typography>
+                            </TableCell>
+                            <TableCell
+                                align="left"
+                                className={classNames(
+                                    classes.tableCellHead,
+                                    classes.versionValueTableCellFix,
+                                )}
+                            >
+                                <Typography
+                                    color={
+                                        fileContent?.logB?.deleted
+                                            ? 'error'
+                                            : undefined
+                                    }
+                                >
+                                    {formatMessage(headerB)}
+                                </Typography>
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <InstanceLogContentBodyTable fileContent={fileContent} />
+                </Table>
+            </TableContainer>
+        </Box>
     );
 };

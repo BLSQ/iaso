861bc06dd74ac5d7e8acf75256200d2cb669a64c
Crebert08 <benicrebertbaransegeta@gmail.com>
2026-07-07 14:39:17 +0200

    Changing the form ai index file back

--- hat/assets/js/apps/Iaso/domains/formAI/index.tsx
+++ hat/assets/js/apps/Iaso/domains/formAI/index.tsx
@@ -10,6 +10,10 @@ import { useGetFormsDropdownOptions } from '../forms/hooks/useGetFormsDropdownOp
 import { ChatPanel } from './components/ChatPanel';
 import { FormPreview } from './components/FormPreview';
 import { SaveFormDialog } from './components/SaveFormDialog';
+<<<<<<< Updated upstream
+=======
+// import { useGetFormsDropdown } from './hooks/requests/useGetFormsDropdown';
+>>>>>>> Stashed changes
 import { useLoadForm } from './hooks/requests/useLoadForm';
 import { useSendMessage } from './hooks/requests/useSendMessage';
 import MESSAGES from './messages';
@@ -94,7 +98,11 @@ const FormAI: FunctionComponent = () => {
     const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
 
     const { mutate: sendMessage, isLoading } = useSendMessage();
+<<<<<<< Updated upstream
     const { data: forms } = useGetFormsDropdownOptions({
+=======
+    const { data: formOptions = [] } = useGetFormsDropdownOptions({
+>>>>>>> Stashed changes
         extraFields: ['form_id', 'latest_form_version'],
     });
     const { mutate: loadForm, isLoading: isLoadingForm } = useLoadForm();

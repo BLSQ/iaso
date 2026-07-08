d208f05348d4ec4ecdb9589c1b90413167ff2068
beygorghor <cgerard@bluesquarehub.com>
2026-07-08 10:30:50 +0200

    fix: limit profiles api
    refs: POLIO-2174 (#3176)

--- iaso/api/profiles/views.py
+++ iaso/api/profiles/views.py
@@ -63,11 +63,9 @@ class ProfilesViewSet(ModelViewSet):
     f"""Profiles API
 
     This API is restricted to authenticated users having the "{CORE_USERS_ADMIN_PERMISSION}" or "{CORE_USERS_MANAGED_PERMISSION}"
-    permission for write core_permissions.
-    Read access is accessible to any authenticated users as it necessary to list profile or display a particular one in
-    the interface.
-
-    Any logged user can also edit his profile to set his language.
+    permission for write operations and for listing, retrieving, or exporting other users.
+    Users without those permissions can only read and update their own profile via `/me`, and use the dropdown endpoint
+    which returns limited information.
 
     GET /api/profiles/
     GET /api/profiles/me => current user

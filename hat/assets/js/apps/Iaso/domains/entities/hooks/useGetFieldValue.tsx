cd61afdbc4afed25eae71999ed818a96bc8fbf94
Christophe Gerard <cgerard@bluesquarehub.com>
2026-08-07 12:35:57 +0200

    fix select on list view

--- hat/assets/js/apps/Iaso/domains/entities/hooks/useGetFieldValue.tsx
+++ hat/assets/js/apps/Iaso/domains/entities/hooks/useGetFieldValue.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { Box } from '@mui/material';
 import { textPlaceholder, useSafeIntl } from 'bluesquare-components';
@@ -64,98 +64,108 @@ export const useGetFieldValue = (
     type: FieldType,
 ) => string | number | React.ReactNode) => {
     const { formatMessage } = useSafeIntl();
-    const getValue = (
-        fieldKey: string,
-        fileContent: FieldSource,
-        type: FieldType,
-    ): string | number | React.ReactNode => {
-        switch (type) {
-            case 'text':
-            case 'calculate':
-            case 'integer':
-            case 'decimal':
-            case 'barcode':
-            case 'note': {
-                const value = getRawFieldValue(fileContent, fieldKey);
-                return (
-                    (value as string | number | undefined) || textPlaceholder
-                );
-            }
-            case 'date': {
-                const rawDate = getRawFieldString(fileContent, fieldKey);
-                return rawDate ? moment(rawDate).format('L') : textPlaceholder;
-            }
-            case 'start':
-            case 'end':
-            case 'dateTime': {
-                const rawDateTime = getRawFieldString(fileContent, fieldKey);
-                return rawDateTime
-                    ? moment(rawDateTime).format('LTS')
-                    : textPlaceholder;
-            }
-            case 'select one':
-            case 'select_one': {
-                return getDescriptorValue(
-                    fieldKey,
-                    fileContent,
-                    formDescriptors,
-                );
-            }
-            case 'select_all_that_apply':
-            case 'select all that apply':
-            case 'select_multiple':
-            case 'select multiple': {
-                const listValues = getDescriptorListValues(
-                    fieldKey,
-                    fileContent,
-                    formDescriptors,
-                );
-                return listValues.length > 0
-                    ? listValues.join(' - ')
-                    : textPlaceholder;
-            }
+    return useCallback(
+        (
+            fieldKey: string,
+            fileContent: FieldSource,
+            type: FieldType,
+        ): string | number | React.ReactNode => {
+            switch (type) {
+                case 'text':
+                case 'calculate':
+                case 'integer':
+                case 'decimal':
+                case 'barcode':
+                case 'note': {
+                    const value = getRawFieldValue(fileContent, fieldKey);
+                    return (
+                        (value as string | number | undefined) ||
+                        textPlaceholder
+                    );
+                }
+                case 'date': {
+                    const rawDate = getRawFieldString(fileContent, fieldKey);
+                    return rawDate
+                        ? moment(rawDate).format('L')
+                        : textPlaceholder;
+                }
+                case 'start':
+                case 'end':
+                case 'dateTime': {
+                    const rawDateTime = getRawFieldString(
+                        fileContent,
+                        fieldKey,
+                    );
+                    return rawDateTime
+                        ? moment(rawDateTime).format('LTS')
+                        : textPlaceholder;
+                }
+                case 'select one':
+                case 'select_one': {
+                    return getDescriptorValue(
+                        fieldKey,
+                        fileContent,
+                        formDescriptors,
+                    );
+                }
+                case 'select_all_that_apply':
+                case 'select all that apply':
+                case 'select_multiple':
+                case 'select multiple': {
+                    const listValues = getDescriptorListValues(
+                        fieldKey,
+                        fileContent,
+                        formDescriptors,
+                    );
+                    return listValues.length > 0
+                        ? listValues.join(' - ')
+                        : textPlaceholder;
+                }
 
-            case 'geopoint': {
-                const rawGeo = getRawFieldString(fileContent, fieldKey);
-                if (!rawGeo) return textPlaceholder;
-                const latitude = Number(rawGeo.split(' ')[0]);
-                const longitude = Number(rawGeo.split(' ')[1]);
-                return (
-                    <Box width="100%" height="100%">
-                        <MarkerMap
-                            longitude={
-                                Number.isFinite(longitude)
-                                    ? longitude
-                                    : undefined
-                            }
-                            latitude={
-                                Number.isFinite(latitude) ? latitude : undefined
-                            }
-                            maxZoom={8}
-                            mapHeight={200}
-                        />
-                    </Box>
-                );
+                case 'geopoint': {
+                    const rawGeo = getRawFieldString(fileContent, fieldKey);
+                    if (!rawGeo) return textPlaceholder;
+                    const latitude = Number(rawGeo.split(' ')[0]);
+                    const longitude = Number(rawGeo.split(' ')[1]);
+                    return (
+                        <Box width="100%" height="100%">
+                            <MarkerMap
+                                longitude={
+                                    Number.isFinite(longitude)
+                                        ? longitude
+                                        : undefined
+                                }
+                                latitude={
+                                    Number.isFinite(latitude)
+                                        ? latitude
+                                        : undefined
+                                }
+                                maxZoom={8}
+                                mapHeight={200}
+                            />
+                        </Box>
+                    );
+                }
+                case 'time': {
+                    // ODK/XLSForm times look like "10:30:00.000+02:00" (time + offset, no date).
+                    const rawTime = getRawFieldString(fileContent, fieldKey);
+                    if (!rawTime) return textPlaceholder;
+                    const parsedTime = moment.parseZone(rawTime, [
+                        'HH:mm:ss.SSSZ',
+                        'HH:mm:ss.SSSZZ',
+                        'HH:mm:ssZ',
+                        'HH:mm:ss.SSS',
+                        'HH:mm:ss',
+                        'HH:mm',
+                    ]);
+                    return parsedTime.isValid()
+                        ? parsedTime.format('LT')
+                        : textPlaceholder;
+                }
+                default:
+                    return formatMessage(MESSAGES.typeNotSupported, { type });
             }
-            case 'time': {
-                // ODK/XLSForm times look like "10:30:00.000+02:00" (time + offset, no date).
-                const rawTime = getRawFieldString(fileContent, fieldKey);
-                if (!rawTime) return textPlaceholder;
-                const parsedTime = moment.parseZone(rawTime, [
-                    'HH:mm:ss.SSSZ',
-                    'HH:mm:ss.SSSZZ',
-                    'HH:mm:ssZ',
-                    'HH:mm:ss.SSS',
-                    'HH:mm:ss',
-                    'HH:mm',
-                ]);
-                return parsedTime.isValid()
-                    ? parsedTime.format('LT')
-                    : textPlaceholder;
-            }
-            default:
-                return formatMessage(MESSAGES.typeNotSupported, { type });
-        }
-    };
-    return getValue;
+        },
+        [formDescriptors, formatMessage],
+    );
 };

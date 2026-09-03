804abc0964f71eabab7e4144cbafe46ede243c10
philv <git@philv.me>
2026-09-03 14:55:33 +0000

    Hide the org unit reject button from the users without permission

--- hat/assets/js/apps/Iaso/domains/orgUnits/components/ActionCell.tsx
+++ hat/assets/js/apps/Iaso/domains/orgUnits/components/ActionCell.tsx
@@ -9,6 +9,9 @@ import { IconButton, ConfirmCancelModal } from 'bluesquare-components';
 import { FormattedMessage } from 'react-intl';
 import { baseUrls } from '../../../constants/urls';
 import { isValidCoordinate } from '../../../utils/map/mapUtils';
+import { ORG_UNITS } from '../../../utils/permissions';
+import { useCurrentUser } from '../../../utils/usersUtils';
+import { userHasPermission } from '../../users/utils';
 import { useSaveOrgUnit } from '../hooks';
 import MESSAGES from '../messages';
 import { OrgUnit } from '../types/orgUnit';
@@ -18,11 +21,14 @@ type Props = {
 
 export const ActionCell: FunctionComponent<Props> = ({ orgUnit }) => {
     const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+    const currentUser = useCurrentUser();
     const { mutateAsync: saveOu, isLoading: isSaving } = useSaveOrgUnit(
         undefined,
         ['orgunits'],
         MESSAGES.orgUnitRejected,
     );
+
+    const hasOrgUnitsPermission = userHasPermission(ORG_UNITS, currentUser);
 
     const handleRejectOrgUnit = useCallback(() => {
         saveOu({
@@ -53,37 +59,38 @@ export const ActionCell: FunctionComponent<Props> = ({ orgUnit }) => {
                     icon="history"
                     tooltipMessage={MESSAGES.history}
                 />
-                {orgUnit.validation_status !== 'REJECTED' && (
-                    <>
-                        <IconButton
-                            onClick={() => setIsConfirmModalOpen(true)}
-                            icon="delete"
-                            tooltipMessage={MESSAGES.rejectOrgUnit}
-                            disabled={isSaving}
-                        />
-                        <ConfirmCancelModal
-                            open={isConfirmModalOpen}
-                            closeDialog={() => setIsConfirmModalOpen(false)}
-                            onClose={() => null}
-                            id={`reject-orgunit-${orgUnit.id}`}
-                            dataTestId={`reject-orgunit-modal-${orgUnit.id}`}
-                            titleMessage={MESSAGES.rejectOrgUnit}
-                            onConfirm={handleRejectOrgUnit}
-                            onCancel={() => setIsConfirmModalOpen(false)}
-                            confirmMessage={MESSAGES.yes}
-                            cancelMessage={MESSAGES.no}
-                        >
-                            <DialogContentText id="alert-dialog-description">
-                                <FormattedMessage
-                                    {...MESSAGES.rejectOrgUnitQuestion}
-                                    values={{
-                                        name: orgUnit.name,
-                                    }}
-                                />
-                            </DialogContentText>
-                        </ConfirmCancelModal>
-                    </>
-                )}
+                {orgUnit.validation_status !== 'REJECTED' &&
+                    hasOrgUnitsPermission && (
+                        <>
+                            <IconButton
+                                onClick={() => setIsConfirmModalOpen(true)}
+                                icon="delete"
+                                tooltipMessage={MESSAGES.rejectOrgUnit}
+                                disabled={isSaving}
+                            />
+                            <ConfirmCancelModal
+                                open={isConfirmModalOpen}
+                                closeDialog={() => setIsConfirmModalOpen(false)}
+                                onClose={() => null}
+                                id={`reject-orgunit-${orgUnit.id}`}
+                                dataTestId={`reject-orgunit-modal-${orgUnit.id}`}
+                                titleMessage={MESSAGES.rejectOrgUnit}
+                                onConfirm={handleRejectOrgUnit}
+                                onCancel={() => setIsConfirmModalOpen(false)}
+                                confirmMessage={MESSAGES.yes}
+                                cancelMessage={MESSAGES.no}
+                            >
+                                <DialogContentText id="alert-dialog-description">
+                                    <FormattedMessage
+                                        {...MESSAGES.rejectOrgUnitQuestion}
+                                        values={{
+                                            name: orgUnit.name,
+                                        }}
+                                    />
+                                </DialogContentText>
+                            </ConfirmCancelModal>
+                        </>
+                    )}
             </section>
         );
     }, [
@@ -96,6 +103,7 @@ export const ActionCell: FunctionComponent<Props> = ({ orgUnit }) => {
         handleRejectOrgUnit,
         isConfirmModalOpen,
         isSaving,
+        hasOrgUnitsPermission,
     ]);
     return cell;
 };

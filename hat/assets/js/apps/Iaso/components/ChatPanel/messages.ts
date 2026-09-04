c2e20678d5f2b708e375235274be9e1b4d81dde6
Bewi <benjamin.wilfart@gmail.com>
2026-09-04 10:06:26 +0200

    feat(Chat AI): Add revert action for assistant messages in ChatPanel (#3297)
    * feat: Add revert action for assistant messages in ChatPanel
    
    * feat: Prevent revert action during active requests
    
    * Remove unecessary type casting
    
    refs: SNT-577

--- hat/assets/js/apps/Iaso/components/ChatPanel/messages.ts
+++ hat/assets/js/apps/Iaso/components/ChatPanel/messages.ts
@@ -17,6 +17,31 @@ const MESSAGES = defineMessages({
         defaultMessage: 'Type a message...',
         id: 'iaso.chatPanel.placeholder',
     },
+    revert: {
+        defaultMessage: 'Revert this change',
+        id: 'iaso.chatPanel.revert',
+    },
+    reverted: {
+        defaultMessage: 'Reverted',
+        id: 'iaso.chatPanel.reverted',
+    },
+    revertConfirmTitle: {
+        defaultMessage: 'Revert this change?',
+        id: 'iaso.chatPanel.revertConfirmTitle',
+    },
+    revertConfirmMessage: {
+        defaultMessage:
+            'This cannot be undone. Every change made after this one will be reverted as well.',
+        id: 'iaso.chatPanel.revertConfirmMessage',
+    },
+    revertConfirmAccept: {
+        defaultMessage: 'Revert',
+        id: 'iaso.chatPanel.revertConfirmAccept',
+    },
+    revertConfirmCancel: {
+        defaultMessage: 'Cancel',
+        id: 'iaso.chatPanel.revertConfirmCancel',
+    },
     sendAnswers: {
         defaultMessage: 'Send answers',
         id: 'iaso.chatPanel.sendAnswers',
